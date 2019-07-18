cca62b5e72c9e65c5b01109b9f620353d8c582a1
Aaron DeRuvo <aaron.deruvo@gmail.com>
2019-07-18 13:43:33 -0700

    Improve UI for faulting and funding by taking advantage of new subscription method for monitoring state of fauceting requests

---
+++ packages/web/src/fauceting/RequestFunds.tsx
@@ -0,0 +1,223 @@
+import * as React from 'react'
+import ReCAPTCHA from 'react-google-recaptcha'
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
+import {
+  getCaptchaKey,
+  RequestState,
+  requestStatusToState,
+  validateBeneficary,
+} from 'src/fauceting/utils'
+import { TextInput } from 'src/forms/FormComponents'
+import Button, { BTN, SIZE } from 'src/shared/Button.3'
+import { postForm } from 'src/shared/Form'
+import { colors, fonts, standardStyles, textStyles } from 'src/styles'
+import {
+  RequestRecord,
+  RequestStatus,
+  RequestType,
+  subscribeRequest,
+} from '../../server/FirebaseClient'
+import { I18nProps, NameSpaces, withNamespaces } from 'src/i18n'
+
+function send(beneficiary: string, kind: RequestType, captchaToken: string) {
+  const route = kind === RequestType.Invite ? '/invite' : '/faucet'
+  return postForm(route, { captchaToken, beneficiary })
+}
+
+function formatNumber(number: string) {
+  if (number.length === 1 && number.startsWith('+')) {
+    return ''
+  }
+  if (number.startsWith('+')) {
+    return number
+  } else {
+    return `+${number}`
+  }
+}
+
+interface State {
+  beneficiary: string
+  country: string
+  captchaOK: boolean
+  requestState: RequestState
+  dollarTxHash?: string
+  goldTxHash?: string
+}
+
+interface Props {
+  kind: RequestType
+}
+
+class RequestFunds extends React.PureComponent<Props & I18nProps, State> {
+  state: State = {
+    beneficiary: '',
+    country: '',
+    requestState: RequestState.Initial,
+    captchaOK: false,
+  }
+  recaptchaRef = React.createRef<ReCAPTCHA>()
+
+  setBeneficiary = ({ nativeEvent }) => {
+    const { value } = nativeEvent.target
+    const beneficiary = this.props.kind === RequestType.Invite ? formatNumber(value) : value
+    this.setState({
+      beneficiary,
+      requestState:
+        this.state.requestState !== RequestState.Started
+          ? RequestState.Initial
+          : this.state.requestState,
+    })
+  }
+
+  onCaptcha = (value: string | null) => {
+    this.setState({ captchaOK: !!value })
+  }
+  resetCaptcha = () => {
+    this.recaptchaRef.current.reset()
+  }
+  getCaptchaToken = () => {
+    return this.recaptchaRef.current.getValue()
+  }
+
+  onSubmit = async () => {
+    if (!validateBeneficary(this.state.beneficiary, this.props.kind)) {
+      this.setState({ requestState: RequestState.Invalid })
+      return
+    }
+    const res = await this.startRequest()
+    const { status, key } = await res.json()
+
+    this.updateStatus(status)
+    this.subscribe(key)
+    this.resetCaptcha()
+  }
+
+  startRequest = () => {
+    this.setState({ requestState: RequestState.Started })
+    return send(this.state.beneficiary, this.props.kind, this.getCaptchaToken())
+  }
+
+  updateStatus = (status: RequestStatus) => {
+    this.setState({ requestState: requestStatusToState(status) })
+  }
+
+  subscribe = (key: string) => {
+    subscribeRequest(key, this.onUpdates)
+  }
+
+  onUpdates = (record: RequestRecord) => {
+    const { status, dollarTxHash, goldTxHash } = record
+    this.setState({ requestState: requestStatusToState(status), dollarTxHash, goldTxHash })
+  }
+
+  isFaucet = () => {
+    return this.props.kind === RequestType.Faucet
+  }
+
+  getPlaceholder = () => {
+    return this.isFaucet() ? this.props.t('testnetAddress') : '+1 555 555 5555'
+  }
+  buttonText = () => {
+    const isComplete = this.state.requestState === RequestState.Completed
+    const { t } = this.props
+
+    if (this.isFaucet()) {
+      return isComplete ? t('funded') : t('getDollars')
+    } else {
+      return isComplete ? t('accountCreated') : t('requestInvite')
+    }
+  }
+
+  render() {
+    const { requestState } = this.state
+    const isInvalid = requestState === RequestState.Invalid
+    const isStarted = requestState === RequestState.Started
+    return (
+      <View style={standardStyles.elementalMargin}>
+        <View style={standardStyles.elementalMarginBottom}>
+          <ReCAPTCHA sitekey={getCaptchaKey()} onChange={this.onCaptcha} ref={this.recaptchaRef} />
+        </View>
+        <TextInput
+          type={this.isFaucet() ? 'tel' : 'text'}
+          focusStyle={
+            this.isFaucet() ? standardStyles.inputFocused : standardStyles.inputDarkFocused
+          }
+          name="beneficiary"
+          style={[
+            standardStyles.input,
+            !this.isFaucet() && standardStyles.inputDarkMode,
+            isInvalid && styles.error,
+          ]}
+          placeholder={this.getPlaceholder()}
+          onChange={this.setBeneficiary}
+          value={this.state.beneficiary}
+        />
+        <ContextualInfo
+          requestState={this.state.requestState}
+          t={this.props.t}
+          isFaucet={this.isFaucet()}
+        />
+        <View style={[standardStyles.row, standardStyles.elementalMarginTop]}>
+          <Button
+            disabled={isInvalid || !this.state.captchaOK || isStarted}
+            kind={isStarted ? BTN.SECONDARY : BTN.PRIMARY}
+            text={this.buttonText()}
+            onPress={this.onSubmit}
+            iconLeft={isStarted && <ActivityIndicator color={colors.primary} size={'large'} />}
+            align="flex-start"
+            size={this.isFaucet() ? SIZE.normal : SIZE.big}
+          />
+        </View>
+      </View>
+    )
+  }
+}
+
+export default withNamespaces(NameSpaces.faucet)(RequestFunds)
+
+interface InfoProps {
+  requestState: RequestState
+  t: I18nProps['t']
+  isFaucet: boolean
+}
+
+const BAD_STATES = new Set([RequestState.Failed, RequestState.Invalid])
+
+function ContextualInfo({ requestState, t, isFaucet }: InfoProps) {
+  const contextStyle = [
+    fonts.small,
+    !isFaucet && textStyles.invert,
+    BAD_STATES.has(requestState) && textStyles.error,
+  ]
+
+  const text = isFaucet ? faucetText({ requestState, t }) : inviteText({ requestState, t })
+
+  return <Text style={contextStyle}>{text}</Text>
+}
+
+function faucetText({ requestState, t }) {
+  return (
+    {
+      [RequestState.Failed]: t('faucetError'),
+      [RequestState.Invalid]: t('invalidAddress'),
+      [RequestState.Completed]: t('faucetCompleted'),
+    }[requestState] || 'eg. c0000fab00d0000000000f00b0e0000c00c00ebd'
+  )
+}
+
+function inviteText({ requestState, t }) {
+  return (
+    {
+      [RequestState.Failed]: t('inviteError'),
+      [RequestState.Invalid]: t('invalidNumber'),
+      [RequestState.Completed]: t('accountCreated'),
+    }[requestState] || ''
+  )
+}
+
+const styles = StyleSheet.create({
+  error: {
+    borderColor: colors.error,
+    borderWidth: 1,
+  },
+})
---
+++ packages/web/src/fauceting/utils.ts
@@ -0,0 +1,46 @@
+import getConfig from 'next/config'
+import { RequestStatus, RequestType } from '../../server/FirebaseClient'
+
+export function getCaptchaKey() {
+  return getConfig().publicRuntimeConfig.RECAPTCHA
+}
+
+export function validateBeneficary(addressOrE164: string, kind: RequestType) {
+  if (kind === RequestType.Invite) {
+    return validateNumber(addressOrE164)
+  } else {
+    return validateAddress(addressOrE164)
+  }
+}
+
+function validateNumber(number: string) {
+  //  TODO use our phone utils from @celo/utils
+  const E164RegEx = /^\+[1-9][0-9]{1,14}$/
+  return E164RegEx.test(number)
+}
+
+// TODO actually validate
+export function validateAddress(address: string) {
+  return address.length > 0
+}
+
+export function requestStatusToState(status: RequestStatus) {
+  switch (status) {
+    case RequestStatus.Done:
+      return RequestState.Completed
+    case RequestStatus.Failed:
+      return RequestState.Failed
+    case RequestStatus.Working:
+      return RequestState.Started
+    case RequestStatus.Pending:
+      return RequestState.Started
+  }
+}
+
+export enum RequestState {
+  Initial,
+  Invalid,
+  Started,
+  Completed,
+  Failed,
+}
