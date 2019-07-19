37978ef8efc993cb29536e57b43002c3489bebc8
Martín Volpe <martin.volpe@gmail.com>
2019-07-19 14:57:14 +0200

    Added timeout

--- packages/mobile/src/redux/store.ts
+++ packages/mobile/src/redux/store.ts
@@ -7,12 +7,18 @@ import thunk from 'redux-thunk'
 import rootReducer from 'src/redux/reducers'
 import { rootSaga } from 'src/redux/sagas'
 
+let extrSettings = {}
+if (__DEV__) {
+  extrSettings = { timeout: 10000 }
+}
+
 const persistConfig = {
   key: 'root',
   version: -1, // default is -1, increment as we make migrations
   storage,
   blacklist: ['home', 'geth', 'exchange', 'networkInfo', 'alert'],
   stateReconciler: autoMergeLevel2,
+  ...extrSettings,
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
