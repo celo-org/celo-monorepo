217e86398a2746d8ddea39e96acea15a21839c35
J M Rossy <jm.rossy@gmail.com>
2019-07-24 12:15:58 +0200

    Add redux migration v0

---
+++ packages/mobile/src/redux/migrations.ts
@@ -0,0 +1,16 @@
+export const migrations = {
+  0: (state: any) => {
+    return {
+      ...state,
+      invite: {
+        ...state.invite,
+        redeemComplete: state.app.inviteCodeEntered,
+      },
+      identity: {
+        startedVerification: state.app.numberVerified,
+        askedContactsPermission: state.app.numberVerified,
+        isLoadingImportContacts: false,
+      },
+    }
+  },
+}
