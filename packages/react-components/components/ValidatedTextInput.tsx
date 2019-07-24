bf0eed935f99a6af383c0cfab49bdd99cf056ac9
Derrick Lee <derrick@dlee.dev>
2019-07-24 09:50:12 -0700

    Move ValidatedTextInput logic to utils

--- packages/react-components/components/ValidatedTextInput.tsx
+++ packages/react-components/components/ValidatedTextInput.tsx
@@ -1,10 +1,12 @@
-import { ParsedPhoneNumber, parsePhoneNumber } from '@celo/utils/src/phoneNumbers'
+import { validateInput } from '@celo/utils/src/inputValidation'
+import { ParsedPhoneNumber } from '@celo/utils/src/phoneNumbers'
 import * as React from 'react'
 import { TextInput, TextInputProps } from 'react-native'
 
 interface OwnProps {
   value: string
   numberOfDecimals?: number
+  placeholder?: string
 }
 
 // Required props when validator type is phone
@@ -12,7 +14,7 @@ interface PhoneValidatorProps {
   validator: 'phone'
   countryCallingCode: string
   onChangePhoneNumber: (input: string, phoneDetails: ParsedPhoneNumber) => void
-  // Following props unused but required to be defined
+  // Following props unused w/ 'phone' validator but required to be defined
   onChangeText?: NumberValidatorProps['onChangeText']
   customValidator?: CustomValidatorProps['customValidator']
 }
@@ -20,7 +22,7 @@ interface PhoneValidatorProps {
 interface NumberValidatorProps {
   validator?: 'integer' | 'decimal'
   onChangeText: (input: string) => void
-  // Following props unused but required to be defined
+  // Following props unused w/ number validators but required to be defined
   countryCallingCode?: string
   onChangePhoneNumber?: PhoneValidatorProps['onChangePhoneNumber']
   customValidator?: CustomValidatorProps['customValidator']
@@ -30,7 +32,7 @@ interface CustomValidatorProps {
   validator: 'custom'
   customValidator: (input: string) => string
   onChangeText: NumberValidatorProps['onChangeText']
-  // Following props unused but required to be defined
+  // Following props unused w/ 'custom' but required to be defined
   countryCallingCode?: string
   onChangePhoneNumber?: PhoneValidatorProps['onChangePhoneNumber']
 }
@@ -44,63 +46,18 @@ interface RefProps {
 type Props = OwnProps & ValidatorProps & RefProps & TextInputProps
 
 class ValidatedTextInput extends React.Component<Props> {
-  validateInteger = (input: string): string => {
-    return input.replace(/[^0-9]/, '')
-  }
-
-  validateDecimal = (input: string): string => {
-    return input
-      .replace(/[^0-9.]/, '')
-      .replace(/\./, 'b') // Keep only the first decimal
-      .replace(/\./g, '')
-      .replace(/b/, '.')
-  }
-
-  validatePhone = (input: string): string => {
-    input = input.replace(/[^0-9()-]/, '')
-    // countryCallingCode must be defined as this is reachable only via phone validator
-    const phoneDetails = parsePhoneNumber(input, this.props.countryCallingCode!)
-
-    if (!phoneDetails) {
-      return input
-    }
-
-    if (this.props.onChangePhoneNumber) {
-      this.props.onChangePhoneNumber(input, phoneDetails)
-    }
-
-    return phoneDetails.displayNumber
-  }
-
-  validateInput = (input: string): string => {
-    if (!this.props.validator && !this.props.customValidator) {
-      return input
-    }
-
-    switch (this.props.validator) {
-      case 'decimal':
-        return this.validateDecimal(input)
-      case 'integer':
-        return this.validateInteger(input)
-      case 'phone':
-        return this.validatePhone(input)
-      case 'custom':
-        return this.props.customValidator(input)
-    }
-
-    throw new Error('Unhandled input validator')
-  }
-
   onChangeText = (input: string): void => {
-    const validated = this.validateInput(input)
-    // pass the validated text to parent
-    if (input === validated) {
+    const validated = validateInput(input, this.props)
+    // Don't propagate change if new change is invalid
+    if (this.props.value === validated) {
       return
     }
 
-    if (this.props.onChangeText) {
-      this.props.onChangeText(validated)
+    if (!this.props.onChangeText) {
+      return
     }
+
+    this.props.onChangeText(validated)
   }
 
   getMaxLength = () => {
