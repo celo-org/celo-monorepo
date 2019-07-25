e8b4a9341bc13cf7bfeea9fe91ccfb081346998f
Derrick Lee <derrick@dlee.dev>
2019-07-25 14:19:40 -0700

    Add es test for comma inputValidation

--- packages/utils/test/inputValidation.test.ts
+++ packages/utils/test/inputValidation.test.ts
@@ -1,9 +1,10 @@
-import { validateInput } from '../src/inputValidation'
+import { BaseProps, validateInput } from '../src/inputValidation'
 
 describe('inputValidation', () => {
   function validateFunction(
     desc: string,
     itStr: string,
+    props: BaseProps,
     inputs: string[],
     validator: 'phone' | 'integer' | 'decimal',
     expected: string
@@ -11,7 +12,7 @@ describe('inputValidation', () => {
     describe(desc, () => {
       inputs.forEach((input) =>
         it(`${itStr}: ${input}`, () => {
-          const result = validateInput(input, { validator, countryCallingCode: '1' })
+          const result = validateInput(input, { validator, countryCallingCode: '1', ...props })
           expect(result).toEqual(expected)
         })
       )
@@ -21,22 +22,29 @@ describe('inputValidation', () => {
   validateFunction(
     'validateInteger',
     'Removes invalid characters',
+    {},
     ['bu123n', '123', '-123', '1b2u3n', '1.2.3.'],
     'integer',
     '123'
   )
 
+  const decimals = ['bu1.23n', '1.2.3', '1.23', '1.2.-_[`/,zx3.....', '1.b.23']
+
+  validateFunction('validateDecimal', 'Removes invalid characters', {}, decimals, 'decimal', '1.23')
+
   validateFunction(
     'validateDecimal',
-    'Removes invalid characters',
-    ['bu1.23n', '1.2.3', '1.23', '1.2.-_[`/,zx3.....', '1.b.23'],
+    'Supports commas',
+    { lng: 'es-AR' },
+    decimals.map((val) => val.replace('.', ',')),
     'decimal',
-    '1.23'
+    '1,23'
   )
 
   validateFunction(
     'validatePhone',
     'Removes invalid characters and formats numbers',
+    {},
     [
       '4023939889',
       '(402)3939889',
