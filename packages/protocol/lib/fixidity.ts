17c4d191e891b1bd9b6a6a01c591098d00591691
Martin Chrzanowski <martin@celo.org>
2019-07-25 12:13:01 -0700

    Add Fixidity TS helpers

---
+++ packages/protocol/lib/fixidity.ts
@@ -0,0 +1,8 @@
+import BigNumber from 'bignumber.js'
+
+export const digits = new BigNumber('24')
+export const fixed1 = new BigNumber('1000000000000000000000000')
+
+export const toFixed = (n: number | BigNumber) => {
+  return fixed1.times(n).integerValue(BigNumber.ROUND_FLOOR)
+}
