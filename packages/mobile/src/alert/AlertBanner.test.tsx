1179444efbecd33740830bb036a033ff81d827c6
Jean Regisser <jean.regisser@gmail.com>
2019-08-07 15:03:54 +0200

    Remove dead comment

--- packages/mobile/src/alert/AlertBanner.test.tsx
+++ packages/mobile/src/alert/AlertBanner.test.tsx
@@ -3,33 +3,6 @@ import 'react-native'
 import { render } from 'react-native-testing-library'
 import { AlertBanner } from 'src/alert/AlertBanner'
 
-/*
-import SmartTopAlert, { NotificationTypes } from '@celo/react-components/components/SmartTopAlert'
-import * as React from 'react'
-import { render } from 'react-native-testing-library'
-
-describe('SmartTopAlert', () => {
-  beforeAll(() => {
-    jest.useRealTimers()
-  })
-
-  it('renders correctly', async () => {
-    const { toJSON } = render(
-      <SmartTopAlert
-        dismissAfter={5}
-        title={'Smart Top Alert'}
-        text="dont get funny"
-        onPress={jest.fn()}
-        type={NotificationTypes.MESSAGE}
-      />
-    )
-
-    expect(toJSON()).toMatchSnapshot()
-  })
-})
-
-*/
-
 describe('AlertBanner', () => {
   const baseProps = {
     hideAlert: jest.fn(),
