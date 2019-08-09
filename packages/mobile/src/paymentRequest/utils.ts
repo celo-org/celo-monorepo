a8dc0789650792a46803a0cc93d74d1ed4326d7e
J M Rossy <jm.rossy@gmail.com>
2019-08-09 18:49:57 +0200

    Have payment requests pay back directly to requert's address

---
+++ packages/mobile/src/paymentRequest/utils.ts
@@ -0,0 +1,24 @@
+import { PaymentRequest } from 'src/account'
+import { NumberToRecipient, Recipient, RecipientKind } from 'src/recipients/recipient'
+
+export function getRecipientFromPaymentRequest(
+  paymentRequest: PaymentRequest,
+  recipientCache: NumberToRecipient
+): Recipient {
+  const cachedRecipient = paymentRequest.requesterE164Number
+    ? recipientCache[paymentRequest.requesterE164Number]
+    : null
+
+  if (cachedRecipient) {
+    return {
+      ...cachedRecipient,
+      address: paymentRequest.requesterAddress,
+    }
+  } else {
+    return {
+      kind: RecipientKind.Address,
+      address: paymentRequest.requesterAddress,
+      displayName: paymentRequest.requesterAddress,
+    }
+  }
+}
