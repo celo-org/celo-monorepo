1d447f0c87cf0e8bb730c53c6b7eb7c04d815bbb
Shelly Grossman <shellygr151@gmail.com>
2019-08-09 22:39:18 +0300

    All Certora run configurations file

---
+++ packages/protocol/CertoraRunConfigurations.sh
@@ -0,0 +1,15 @@
+# Run bonded deposits simple
+cp certora_verify.json.bonded certora_verify.json
+certoraRun.py pecs/harnesses/BondedDepositsHarness.sol --solc solc-5.10.exe
+
+# Run bonded deposits linked
+cp certora_verify.json.bonded.linked certora_verify.json
+certoraRun.py specs/harnesses/RegistryHarness.sol specs/harnesses/BondedDepositsHarness.sol --solc solc-5.10.exe --link BondedDepositsHarness:registry=RegistryHarness
+
+# Run governance simple
+cp certora_verify.json.govern certora_verify.json
+certoraRun.py contracts/governance/IntegerSortedLinkedList.sol specs/harnesses/GovernanceHarness.sol --solc solc-5.10.exe --link GovernanceHarness:queue=IntegerSortedLinkedList --settings -assumeUnwindCond
+
+# Run lists
+cp certora_verify.json.list certora_verify.json
+certoraRun.py specs/harnesses/SortedListCheck.sol specs/harnesses/IntegerSortedListCheck.sol specs/harnesses/AddressSortedListCheck.sol --solc solc-5.10.exe
