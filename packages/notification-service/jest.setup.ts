26625da1c4d6ff4aeb3fa05cd1a7dfbf8a3b10c1
J M Rossy <jm.rossy@gmail.com>
2019-08-09 16:26:00 +0200

    Attempt to fix test failures on CI

--- packages/notification-service/jest.setup.ts
+++ packages/notification-service/jest.setup.ts
@@ -1,6 +1,7 @@
 jest.mock('./src/config', () => ({
+  ...jest.requireActual('./src/config'),
   BLOCKSCOUT_API: '',
   STABLE_TOKEN_ADDRESS: '',
   GOLD_TOKEN_ADDRESS: '',
-  ...jest.requireActual('./src/config'),
+  NOTIFICATIONS_DISABLED: true,
 }))
