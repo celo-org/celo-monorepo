1dec1b87c2ca892d337d042074e9c4128d99868f
Mariano Cortesi <mcortesi@gmail.com>
2019-08-13 10:58:36 -0300

    Generate contract obj for contractkit & Cleanup
    Changes:
    
    - `yarn build` on protocol compiles sm, generate types for protocol &
    contractkit, and ts factories for contractkit
    - contractkit has a devDependency with protocol so files are generated
    before build
    - cleanup protocol build and compile structure.ts
    - `yarn build:gen` build protocol, and it's used before publishing to
    npm

---
+++ packages/contractkit/src/index.ts
@@ -0,0 +1,21 @@
+import Web3 from 'web3'
+
+export { newAttestations } from './generated/Attestations'
+export { newBondedDeposits } from './generated/BondedDeposits'
+export { newEscrow } from './generated/Escrow'
+export { newExchange } from './generated/Exchange'
+export { newGasCurrencyWhitelist } from './generated/GasCurrencyWhitelist'
+export { newGasPriceMinimum } from './generated/GasPriceMinimum'
+export { newGoldToken } from './generated/GoldToken'
+export { newGovernance } from './generated/Governance'
+export { newMultiSig } from './generated/MultiSig'
+export { newRandom } from './generated/Random'
+export { newRegistry } from './generated/Registry'
+export { newReserve } from './generated/Reserve'
+export { newSortedOracles } from './generated/SortedOracles'
+export { newStableToken } from './generated/StableToken'
+export { newValidators } from './generated/Validators'
+
+export function newWeb3(url: string) {
+  return new Web3(url)
+}
