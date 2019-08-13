46c7961df5f8d15e4156dff1fa5805d311891a98
Audrey Penven <audrey@celo.org>
2019-08-13 18:09:43 +0200

    add AddressRegistry class and a function to get the address for a given contract

--- packages/contractkit/src/AddressRegistry.ts
+++ packages/contractkit/src/AddressRegistry.ts
@@ -1,11 +1,43 @@
+import { newRegistry } from 'src/generated/Registry'
+import { Registry } from 'src/generated/types/Registry'
 import { Address, CeloContract } from './base'
 import { ContractKit } from './kit'
 
-export class AddressRegistry {
-  constructor(readonly kit: ContractKit) {}
+// Registry contract is always predeployed to this address
+const REGISTRY_CONTRACT_ADDRESS = '0x000000000000000000000000000000000000ce10'
 
-  async addressFor(_contract: CeloContract): Promise<Address> {
-    // TODO implement
-    return '0x0000000'
+// This is what's returned from the registry when a contract isn't found
+const ADDRESS_NOT_FOUND = '0x0000000000000000000000000000000000000000'
+
+export class AddressRegistry {
+  private readonly registry: Registry
+  private readonly cache: Map<CeloContract, Address> = new Map()
+
+  constructor(readonly kit: ContractKit) {
+    this.cache.set(CeloContract.Registry, REGISTRY_CONTRACT_ADDRESS)
+    this.registry = newRegistry(kit.web3, REGISTRY_CONTRACT_ADDRESS)
   }
+
+  async addressFor(contract: CeloContract): Promise<Address> {
+    if (!this.cache.has(contract)) {
+      const address = await this.registry.methods.getAddressFor(contract).call()
+
+      if (!address || address === ADDRESS_NOT_FOUND) {
+        throw new Error(`Failed to get address for ${contract} from the Registry`)
+      }
+      this.cache.set(contract, address)
+    }
+    return this.cache.get(contract)!
+  }
+
+  // async allAddresses(): Promise<Map<CeloContract, Address>> {
+  //   const AllContracts = Object.keys(CeloContract).map((contract) => {
+  //     if (typeof contract === string) {
+  //       return CeloContract[contract]
+  //     }
+  //   }) as CeloContract[]
+  //   AllContracts.forEach(async (contract) => {
+  //     await this.addressFor(contract)
+  //   })
+  // }
 }
