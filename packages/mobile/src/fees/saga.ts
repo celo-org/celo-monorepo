71530178e4b5fce06047661ec2d4b1da45980227
annakaz <annakaz@users.noreply.github.com>
2019-08-14 13:06:15 -0700

    Revert saga.ts to match master

--- packages/mobile/src/fees/saga.ts
+++ packages/mobile/src/fees/saga.ts
@@ -1,12 +1,11 @@
 import { getStableTokenContract } from '@celo/walletkit'
-import { call, put, select, spawn, takeLeading } from 'redux-saga/effects'
+import { call, put, spawn, takeLeading } from 'redux-saga/effects'
 import { getReclaimEscrowFee } from 'src/escrow/saga'
 import { Actions, EstimateFeeAction, feeEstimated, FeeType } from 'src/fees/actions'
 import { getInvitationVerificationFee } from 'src/invite/saga'
 import { getSendFee } from 'src/send/saga'
 import { CeloDefaultRecipient } from 'src/send/Send'
 import Logger from 'src/utils/Logger'
-import { currentAccountSelector } from 'src/web3/selectors'
 
 const TAG = 'fees/saga'
 
@@ -14,8 +13,6 @@ export function* estimateFeeSaga({ feeType }: EstimateFeeAction) {
   Logger.debug(TAG + '@estimateFeeSaga', `updating for ${feeType}`)
 
   // TODO: skip fee update if it was calculated recently
-
-  const account = yield select(currentAccountSelector)
 
   let feeInWei
 
@@ -25,7 +22,7 @@ export function* estimateFeeSaga({ feeType }: EstimateFeeAction) {
       break
     case FeeType.SEND:
       // Just use default values here since it doesn't matter for fee estimation
-      feeInWei = yield call(getSendFee, account, getStableTokenContract, {
+      feeInWei = yield call(getSendFee, CeloDefaultRecipient.address, getStableTokenContract, {
         recipientAddress: CeloDefaultRecipient.address,
         amount: '1',
         comment: 'Coffee or Tea?',
