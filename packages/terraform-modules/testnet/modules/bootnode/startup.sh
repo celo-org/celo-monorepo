e7510c6eecd12e034893dfd7bb7323042f6d2f08
Trevor Porter <trkporter@ucdavis.edu>
2019-08-14 11:01:10 -0700

    Use celotool/Terraform to deploy a VM-based testnet (#332)
    * VM testnet works, celotool initial and destroy works
    
    * Make names of resources specific to a celo_env
    
    * Remove hardcoded vars for bootnode
    
    * Add small READMEs to terraform modules
    
    * Fix failing ci-lint

---
+++ packages/terraform-modules/testnet/modules/bootnode/startup.sh
@@ -0,0 +1,44 @@
+#! /bin/sh
+
+echo "Installing Docker..."
+
+# TODO(trevor): investigate how to pull this into a separate file so
+# other startup scripts can use it
+apt update && apt upgrade
+apt install -y apt-transport-https ca-certificates curl software-properties-common gnupg2
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt update
+apt install -y docker-ce
+systemctl start docker
+
+echo "Configuring Docker..."
+gcloud auth configure-docker
+
+BOOTNODE_VERBOSITY=1
+
+CELOTOOL_DOCKER_IMAGE=${celotool_docker_image_repository}:${celotool_docker_image_tag}
+GETH_BOOTNODE_DOCKER_IMAGE=${geth_bootnode_docker_image_repository}:${geth_bootnode_docker_image_tag}
+
+# Get the node key from the celotool docker container
+
+echo "Pulling celotool..."
+docker pull $CELOTOOL_DOCKER_IMAGE
+
+echo "Generating node key..."
+NODE_KEY=`docker run --rm $CELOTOOL_DOCKER_IMAGE \
+  celotooljs.sh generate bip32 --mnemonic "${mnemonic}" \
+  --accountType load_testing --index 0`
+
+echo "Pulling bootnode..."
+docker pull $GETH_BOOTNODE_DOCKER_IMAGE
+
+echo "Starting bootnode..."
+docker run -p 30301:30301/udp --net=host -d $GETH_BOOTNODE_DOCKER_IMAGE /bin/sh -c "\
+  set -euo pipefail && \
+  mkdir /etc/bootnode && \
+  echo $NODE_KEY > /etc/bootnode/node.key && \
+  /usr/local/bin/bootnode \
+    --nat=extip:${ip_address} \
+    --nodekey=/etc/bootnode/node.key \
+    --verbosity=$BOOTNODE_VERBOSITY"
