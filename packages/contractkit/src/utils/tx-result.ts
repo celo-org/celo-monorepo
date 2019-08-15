2988af0f2acc1c82205556a0a290a9eb6eb4250c
Mariano Cortesi <mcortesi@gmail.com>
2019-08-15 19:11:07 -0300

    Remove confirmation from txResult

--- packages/contractkit/src/utils/tx-result.ts
+++ packages/contractkit/src/utils/tx-result.ts
@@ -12,7 +12,6 @@ export function toTxResult(pe: PromiEvent<any>) {
 export class TransactionResult {
   private hashFuture = new ExternalPromise<string>()
   private receiptFuture = new ExternalPromise<TransactionReceipt>()
-  private confirmationsFuture = new ExternalPromise<boolean>()
 
   constructor(pe: PromiEvent<any>) {
     pe.on('transactionHash', (hash: string) => {
@@ -23,16 +22,7 @@ export class TransactionResult {
         debug('receipt: %O', receipt)
         this.receiptFuture.resolve(receipt)
       })
-      .on('confirmation', (confirmationNumber: number) => {
-        if (confirmationNumber > 1) {
-          // "confirmation" event is called for 24 blocks.
-          // if check to avoid polluting the logs and trying to remove
-          // the standby notification more than once
-          return
-        }
-        debug('tx confirmed')
-        this.confirmationsFuture.resolve(true)
-      })
+
       .on('error', ((error: any, receipt: TransactionReceipt | false) => {
         if (!receipt) {
           debug('send-error: %o', error)
@@ -41,7 +31,6 @@ export class TransactionResult {
           debug('mining-error: %o, %O', error, receipt)
         }
         this.receiptFuture.reject(error)
-        this.confirmationsFuture.reject(error)
       }) as any)
   }
 
@@ -51,9 +40,5 @@ export class TransactionResult {
 
   waitReceipt() {
     return this.receiptFuture
-  }
-
-  waitConfirmation() {
-    return this.confirmationsFuture
   }
 }
