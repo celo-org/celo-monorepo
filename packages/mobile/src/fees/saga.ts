6c313ab19bc5145eb6a5fc7bd177bde0ce7292f5
J M Rossy <jm.rossy@gmail.com>
2019-08-16 16:46:14 +0200

    Fix fees for accounts with low balances

--- packages/mobile/src/fees/saga.ts
+++ packages/mobile/src/fees/saga.ts
@@ -1,5 +1,8 @@
 import { getStableTokenContract } from '@celo/walletkit'
 import { call, put, select, spawn, takeLeading } from 'redux-saga/effects'
+import { showError } from 'src/alert/actions'
+import { ErrorMessages } from 'src/app/ErrorMessages'
+import { ALERT_BANNER_DURATION } from 'src/config'
 import { getReclaimEscrowFee } from 'src/escrow/saga'
 import { Actions, EstimateFeeAction, feeEstimated, FeeType } from 'src/fees/actions'
 import { getInvitationVerificationFee } from 'src/invite/saga'
@@ -10,42 +13,47 @@ import { currentAccountSelector } from 'src/web3/selectors'
 
 const TAG = 'fees/saga'
 
+// TODO: skip fee update if it was calculated recently
 export function* estimateFeeSaga({ feeType }: EstimateFeeAction) {
-  Logger.debug(TAG + '@estimateFeeSaga', `updating for ${feeType}`)
+  Logger.debug(`${TAG}/estimateFeeSaga`, `updating for ${feeType}`)
 
-  // TODO: skip fee update if it was calculated recently
-  const account = yield select(currentAccountSelector)
+  try {
+    const account = yield select(currentAccountSelector)
 
-  let feeInWei
+    let feeInWei
 
-  switch (feeType) {
-    case FeeType.INVITE:
-      feeInWei = yield call(getInvitationVerificationFee)
-      break
-    case FeeType.SEND:
-      // Just use default values here since it doesn't matter for fee estimation
-      feeInWei = yield call(getSendFee, account, getStableTokenContract, {
-        recipientAddress: CeloDefaultRecipient.address,
-        amount: '1',
-        comment: 'Coffee or Tea?',
-      })
-      break
-    case FeeType.EXCHANGE:
-      // TODO
-      break
-    case FeeType.RECLAIM_ESCROW:
-      // Just use default values here since it doesn't matter for fee estimation
-      feeInWei = yield call(
-        getReclaimEscrowFee,
-        CeloDefaultRecipient.address,
-        CeloDefaultRecipient.address
-      )
-      break
-  }
+    switch (feeType) {
+      case FeeType.INVITE:
+        feeInWei = yield call(getInvitationVerificationFee)
+        break
+      case FeeType.SEND:
+        // Just use default values here since it doesn't matter for fee estimation
+        feeInWei = yield call(getSendFee, account, getStableTokenContract, {
+          recipientAddress: CeloDefaultRecipient.address,
+          amount: '0.001',
+          comment: 'Coffee or Tea?',
+        })
+        break
+      case FeeType.EXCHANGE:
+        // TODO
+        break
+      case FeeType.RECLAIM_ESCROW:
+        // Just use default values here since it doesn't matter for fee estimation
+        feeInWei = yield call(
+          getReclaimEscrowFee,
+          CeloDefaultRecipient.address,
+          CeloDefaultRecipient.address
+        )
+        break
+    }
 
-  if (feeInWei) {
-    Logger.debug(`${TAG}/estimateFeeSaga`, `New fee is: ${feeInWei}`)
-    yield put(feeEstimated(feeType, feeInWei))
+    if (feeInWei) {
+      Logger.debug(`${TAG}/estimateFeeSaga`, `New fee is: ${feeInWei}`)
+      yield put(feeEstimated(feeType, feeInWei))
+    }
+  } catch (error) {
+    Logger.error(`${TAG}/estimateFeeSaga`, 'Error estimating fee', error)
+    yield put(showError(ErrorMessages.CALCULATE_FEE_FAILED, ALERT_BANNER_DURATION))
   }
 }
 
