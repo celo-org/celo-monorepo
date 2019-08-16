f3c7973fb27e49b0a337a05db6e4311f59fafcf3
Aaron DeRuvo <aaron.deruvo@gmail.com>
2019-08-16 12:05:05 -0700

    No tests yet :(

--- packages/web/src/header/BlueBanner.test.tsx
+++
@@ -1,6 +0,0 @@
-// FIXME jest failing on TSX
-
-// import { TEXT } from './BlueBanner'
-// test('Announcement Text is less than 75 characters so that it fits on small phones', () => {
-//   expect(TEXT.length).toBeLessThanOrEqual(75)
-// })
