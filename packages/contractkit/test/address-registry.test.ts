6b039e509d33177806ede79676ef738f1b6f0fbb
Audrey Penven <audrey@celo.org>
2019-08-16 17:47:19 +0200

    finish address-registry tests

--- packages/contractkit/test/address-registry.test.ts
+++ packages/contractkit/test/address-registry.test.ts
@@ -1,39 +1,91 @@
 import { AddressRegistry } from '../src/address-registry'
-import { CeloContract } from '../src/base'
+import { AllContracts, CeloContract, NULL_ADDRESS } from '../src/base'
 import { newKit } from '../src/kit'
 import { createMockContract } from './utils'
 
-let mockContractAddress = 'MOCK_CONTRACT_ADDRESS'
+const mockContractAddress = '0x00000000000_FAKE_ADDRESS'
 
-const getAddressFor = jest.fn((_contract: CeloContract) => {
-  return mockContractAddress //`0x000${contract}`
-})
+const mockedGetAddressFor = jest.fn()
 
 jest.mock('../src/generated/Registry', () => ({
-  newRegistry: () => createMockContract({ getAddressFor }),
+  newRegistry: () => createMockContract({ getAddressFor: mockedGetAddressFor }),
 }))
 
 describe('AddressRegistry', () => {
-  afterEach(() => {
-    getAddressFor.mockReset()
+  beforeEach(() => {
+    mockedGetAddressFor.mockReturnValue(mockContractAddress)
   })
+
+  afterEach(() => {
+    mockedGetAddressFor.mockClear()
+  })
+
   describe('addressFor', () => {
     it('returns the address from the Registry contract', async () => {
-      mockContractAddress = 'ADDRESS_FOR_GOLD_TOKEN'
-      const kit = newKit('')
-      const addressRegistry = new AddressRegistry(kit)
+      const addressRegistry = new AddressRegistry(newKit(''))
       const addr = await addressRegistry.addressFor(CeloContract.GoldToken)
-      console.info(addr)
       expect(addr).toEqual(mockContractAddress)
     })
 
     it('only calls the Registry contract once per contract', async () => {
-      mockContractAddress = 'ADDRESS_FOR_GOLD_TOKEN'
-      const kit = newKit('')
-      const addressRegistry = new AddressRegistry(kit)
+      const addressRegistry = new AddressRegistry(newKit(''))
       await addressRegistry.addressFor(CeloContract.GoldToken)
       await addressRegistry.addressFor(CeloContract.GoldToken)
-      expect(getAddressFor).toHaveBeenCalledTimes(1)
+      expect(mockedGetAddressFor).toHaveBeenCalledTimes(1)
+
+      await addressRegistry.addressFor(CeloContract.StableToken)
+      expect(mockedGetAddressFor).toHaveBeenCalledTimes(2)
+    })
+
+    describe('getting the address for the Registry contract', () => {
+      it('does not call the Registry contract because this is known, and hard-coded', async () => {
+        const addressRegistry = new AddressRegistry(newKit(''))
+        await addressRegistry.addressFor(CeloContract.Registry)
+        expect(mockedGetAddressFor).toHaveBeenCalledTimes(0)
+      })
+
+      it('returns the address that the Registry is always deployed to', async () => {
+        const addressRegistry = new AddressRegistry(newKit(''))
+        expect(await addressRegistry.addressFor(CeloContract.Registry)).toEqual(
+          '0x000000000000000000000000000000000000ce10'
+        )
+      })
+    })
+
+    describe('when the address cannot be found', () => {
+      beforeEach(() => {
+        mockedGetAddressFor.mockReturnValueOnce(NULL_ADDRESS)
+      })
+
+      it('raises an error', async () => {
+        const addressRegistry = new AddressRegistry(newKit(''))
+        await expect(addressRegistry.addressFor(CeloContract.GoldToken)).rejects.toThrow(
+          'Failed to get address for GoldToken from the Registry'
+        )
+      })
+    })
+  })
+
+  describe('allAddresses', () => {
+    it('calls getAddressFor on the Registry once per contract in AllContracts', async () => {
+      const addressRegistry = new AddressRegistry(newKit(''))
+      await addressRegistry.allAddresses()
+      expect(mockedGetAddressFor).toHaveBeenCalledTimes(AllContracts.length - 1)
+    })
+
+    it('calls to the Registry only happen once per contract', async () => {
+      const addressRegistry = new AddressRegistry(newKit(''))
+      await addressRegistry.allAddresses()
+      await addressRegistry.allAddresses()
+      expect(mockedGetAddressFor).toHaveBeenCalledTimes(AllContracts.length - 1)
+    })
+
+    it('returns an object with entries for each contract', async () => {
+      const addressRegistry = new AddressRegistry(newKit(''))
+      const result = await addressRegistry.allAddresses()
+      for (const contract of AllContracts) {
+        expect(result).toHaveProperty(contract)
+      }
     })
   })
 })
