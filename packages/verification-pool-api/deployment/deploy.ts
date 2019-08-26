95b1aae36326c941548b23749678f3ca80925121
Rohit <8ohit.dua@gmail.com>
2019-08-26 17:08:40 +0200

    [Infrastructure] cloudbuild file for verification pool api, ci/cd

--- packages/verification-pool-api/deployment/deploy.ts
+++ packages/verification-pool-api/deployment/deploy.ts
@@ -2,9 +2,16 @@
 // To use call ts-node deploy.ts --celo-env YOUR_ENV_NAME --celo-proj YOUR_PROJECT_NAME
 
 import * as util from '@celo/verification-pool-api/deployment/deployment-utils'
-import * as parseArgs from 'minimist'
+import parseArgs from 'minimist'
 
-const envArg = parseArgs(process.argv.slice(2))['celo-env']
-util.setProject(parseArgs(process.argv.slice(2))['celo-proj'])
-util.setEnv(envArg)
-util.deploy(envArg)
+async function main() {
+  const envArg = parseArgs(process.argv.slice(2))['celo-env']
+  try {
+    await util.setProject(parseArgs(process.argv.slice(2))['celo-proj'])
+  } catch {
+    await util.setProject('celo-testnet')
+  }
+  await util.setEnv(envArg)
+  await util.deploy(envArg)
+}
+main()
