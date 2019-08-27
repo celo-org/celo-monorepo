e8168793738dbeb4f5633fea1fb3895ae97d25fd
Aaron DeRuvo <aaron.deruvo@gmail.com>
2019-08-27 10:30:12 -0700

    SEO optimize Headings (#681)

--- packages/web/flags.js
+++ packages/web/flags.js
@@ -1,14 +1,17 @@
 const FLAGS = {
   development: {
     ECOFUND: true,
+    ENV: 'development',
     SDK: true,
   },
   production: {
     ECOFUND: false,
+    ENV: 'production',
     SDK: false,
   },
   staging: {
     ECOFUND: false,
+    ENV: 'staging',
     SDK: false,
   },
 }
