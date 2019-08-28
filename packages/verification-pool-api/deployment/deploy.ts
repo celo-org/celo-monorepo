11d6387e5502c2f1449a1dfd2bc77e23404c64ba
Rohit <8ohit.dua@gmail.com>
2019-08-28 11:37:50 +0200

    update package.json verification-pool-api

--- packages/verification-pool-api/deployment/deploy.ts
+++ packages/verification-pool-api/deployment/deploy.ts
@@ -1,8 +1,8 @@
 // Script used for deploying to firebase
 // To use call ts-node deploy.ts --celo-env YOUR_ENV_NAME --celo-proj YOUR_PROJECT_NAME
 
-import * as util from '@celo/verification-pool-api/deployment/deployment-utils'
 import parseArgs from 'minimist'
+import * as util from './deployment-utils'
 
 async function main() {
   const envArg = parseArgs(process.argv.slice(2))['celo-env']
