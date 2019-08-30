07c70618afc83e9a02ae8b991cd937461b29f409
Rohit <8ohit.dua@gmail.com>
2019-08-30 13:37:44 +0200

    add firebase token option verification pool api

--- packages/verification-pool-api/deploy.sh
+++ packages/verification-pool-api/deploy.sh
@@ -11,11 +11,13 @@ NETWORK=""
 while getopts 'a:n:' flag; do
   case "${flag}" in
     n) NETWORK="$OPTARG" ;;
+    t) TOKEN="$OPTARG" ;;
     *) error "Unexpected option ${flag}" ;;
   esac
 done
 
 [ -z "$NETWORK" ] && echo "Need to set the NETWORK via the -n flag" && exit 1;
+[ -z "$TOKEN" ] && echo "Need to set the TOKEN via the -t flag" && exit 1;
 
 echo "Starting verification pool api deployment."
 
@@ -24,6 +26,6 @@ yarn run firebase use celo-testnet
 yarn set-env $NETWORK
 yarn run build
 yarn firebase-bolt schema.bolt
-yarn run firebase deploy --only "database,functions:handleVerificationRequest$NETWORK" --token $_FIREBASE_TOKEN
+yarn run firebase deploy --only "database,functions:handleVerificationRequest$NETWORK" --token $TOKEN
 
 echo "Done deployment."
