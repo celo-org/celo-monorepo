9992cf06422dde257696cc09925fd8d90f4c9146
Asa Oines <a@celo.org>
2019-09-03 14:43:03 -0700

    Remove file that shouldn't have changed

--- packages/web/src/community/CodeOfConduct.tsx
+++ packages/web/src/community/CodeOfConduct.tsx
@@ -113,15 +113,13 @@ export class CodeOfConduct extends React.Component<I18nProps> {
                   text="Geek Feminism Anti-Harassment Policy"
                   kind={BTN.INLINE}
                   target={'_new'}
-                />
-                , and the{' '}
+                />, and the{' '}
                 <Button
                   href={'https://www.contributor-covenant.org/version/1/4/code-of-conduct.html'}
                   text="Contributor Covenant"
                   kind={BTN.INLINE}
                   target={'_new'}
-                />
-                .
+                />.
               </Trans>
             }
           />
