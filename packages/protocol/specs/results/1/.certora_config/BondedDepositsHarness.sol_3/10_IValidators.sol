bcafef57a3197fd01778a8983edd2a5c0a605463
Shelly Grossman <shellygr151@gmail.com>
2019-09-03 10:42:05 +0300

    removing redundant files

--- packages/protocol/specs/results/1/.certora_config/BondedDepositsHarness.sol_3/10_IValidators.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.5.8;
-
-
-interface IValidators {
-  function isVoting(address) external view returns (bool);
-  function isValidating(address) external view returns (bool);
-  function getValidators() external view returns (address[] memory);
-}
