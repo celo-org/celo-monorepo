8f3c48a994c6ff40e5c5a5c42c4d713dd81fdca9
Jean Regisser <jean.regisser@gmail.com>
2019-09-05 15:11:44 +0200

    Use our blockchain-api new currency conversion query

--- packages/mobile/src/localCurrency/sagas.ts
+++ packages/mobile/src/localCurrency/sagas.ts
@@ -1,4 +1,6 @@
+import gql from 'graphql-tag'
 import { call, delay, put, spawn } from 'redux-saga/effects'
+import { apolloClient } from 'src/apollo'
 import { waitForRehydrate } from 'src/app/saga'
 import { LOCAL_CURRENCY_SYMBOL } from 'src/config'
 import {
@@ -16,25 +18,31 @@ const POLLING_DELAY = 3600 * 1000 // 1 hour
 const RETRY_DELAY = 10 * 1000 // 10 secs
 
 async function fetchExchangeRate(symbol: string): Promise<number> {
-  // TODO: replace by our own endpoint once it's ready
-  const response = await fetch(`https://api.exchangeratesapi.io/latest?base=USD&symbols=${symbol}`)
-  if (response.status !== 200) {
-    throw new Error(`Invalid response status ${response.status}`)
+  const response = await apolloClient.query({
+    query: gql`
+    {    
+      currencyConversion(currencyCode: "${symbol}") {
+        rate
+      }
+    }
+  `,
+    fetchPolicy: 'network-only',
+    errorPolicy: 'all',
+  })
+
+  const { data } = response
+  if (typeof data !== 'object') {
+    throw new Error(`Invalid response data ${data}`)
   }
 
-  const json = await response.json()
-  if (typeof json !== 'object') {
-    throw new Error(`Invalid response ${json}`)
+  const { currencyConversion } = data
+  if (typeof currencyConversion !== 'object') {
+    throw new Error(`Invalid response data ${data}`)
   }
 
-  const { rates } = json
-  if (typeof rates !== 'object') {
-    throw new Error(`Invalid response ${json}`)
-  }
-
-  const rate = rates[symbol]
+  const { rate } = currencyConversion
   if (typeof rate !== 'number') {
-    throw new Error(`Invalid response ${json}`)
+    throw new Error(`Invalid response data ${data}`)
   }
 
   return rate
