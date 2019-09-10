3f2a5d8702c52ca0d023f63dddb86f3499337272
Aaron DeRuvo <aaron.deruvo@gmail.com>
2019-09-10 11:19:09 -0700

    WIP — rendering placeholders rather than null

--- packages/web/src/community/Articles.tsx
+++ packages/web/src/community/Articles.tsx
@@ -4,19 +4,37 @@ import FadeIn from 'react-lazyload-fadein'
 import { StyleSheet, View } from 'react-native'
 import { Cell, GridRow, Spans } from 'src/layout/GridRow'
 import Card from 'src/shared/Card'
+import { standardStyles } from 'src/styles'
 
-export default function Articles(props: Props) {
-  const { articles } = props
+interface OwnProps {
+  loading: boolean
+}
+
+type voidFunc = () => void
+
+export default function Articles(props: Props & OwnProps) {
+  const { articles, loading } = props
+
+  if (loading) {
+    return (
+      <GridRow allStyle={standardStyles.centered}>
+        <Cell span={Spans.third} style={standardStyles.centered}>
+          {/* {} */}
+        </Cell>
+      </GridRow>
+    )
+  }
+
   return (
     <FadeIn>
-      {(onLoad) => {
+      {(onImageLoad: voidFunc) => {
         return (
           <View>
             <GridRow allStyle={styles.body}>
               {articles.map((article, key) => {
                 return (
                   <Cell key={key} span={Spans.third} tabletSpan={Spans.half} style={styles.cell}>
-                    <Card {...article} onLoad={onLoad} />
+                    <Card {...article} onLoad={onImageLoad} />
                   </Cell>
                 )
               })}
