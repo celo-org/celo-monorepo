e228f624305c48a0ac56c3a06f984f79f7507889
Aaron DeRuvo <aaron.deruvo@gmail.com>
2019-09-11 17:55:42 -0700

    New plan

--- packages/web/src/fauceting/PhoneInput.tsx
+++ packages/web/src/fauceting/PhoneInput.tsx
@@ -1,5 +1,6 @@
 import { Countries, PhoneNumberUtils } from '@celo/utils'
 import * as React from 'react'
+import Autosuggest from 'react-autosuggest'
 import {
   NativeSyntheticEvent,
   StyleSheet,
@@ -8,7 +9,6 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native'
-import Autocomplete from 'react-native-autocomplete-input'
 import { TextInput } from 'src/forms/FormComponents'
 import { colors, fonts, standardStyles, textStyles } from 'src/styles'
 
@@ -22,7 +22,7 @@ interface State {
   phoneNumber: string
 }
 
-const COUNTRIES = new Countries('en')
+const COUNTRIES = new Countries('en-us')
 
 class PhoneInput extends React.PureComponent<Props, State> {
   state: State = {
@@ -40,9 +40,11 @@ class PhoneInput extends React.PureComponent<Props, State> {
   }
 
   setNumber = (event: NativeSyntheticEvent<TextInputChangeEventData>) => {
-    const phone = event.nativeEvent.text
+    // remove not phone number characters
+    const phone = event.nativeEvent.text.replace(/[^\d\(\)-\s]/g, '')
 
-    const phoneInfo = PhoneNumberUtils.parsePhoneNumber(phone, this.state.countryCallingCode)
+    const phoneInfo =
+      phone.length && PhoneNumberUtils.parsePhoneNumber(phone, this.state.countryCallingCode)
 
     if (phoneInfo) {
       this.setState({ phoneNumber: phoneInfo.displayNumber })
@@ -55,16 +57,21 @@ class PhoneInput extends React.PureComponent<Props, State> {
 
   renderTextInput = (props: any) => (
     <TextInput
+      style={[standardStyles.input, standardStyles.inputDarkMode]}
       focusStyle={standardStyles.inputDarkFocused}
       {...props}
       value={this.state.countryQuery}
-      placeholderTextColor={colors.inactive}
     />
   )
 
   renderItem = (countryCode: string) => {
-    const { displayName, emoji } = COUNTRIES.getCountryByCode(countryCode)
-    const onPress = () => this.onChangeCountryQuery(displayName)
+    // @ts-ignore
+    const { displayName, emoji, countryCallingCodes } = COUNTRIES.getCountryByCode(countryCode)
+
+    const onPress = () => {
+      this.setState({ countryCallingCode: countryCallingCodes[0] })
+      this.onChangeCountryQuery(displayName)
+    }
 
     return (
       <TouchableOpacity onPress={onPress}>
@@ -77,29 +84,61 @@ class PhoneInput extends React.PureComponent<Props, State> {
   }
 
   getPlaceholder = () => {
-    return 'phone number'
+    return 'Phone number'
+  }
+
+  updateSuggestions = ({ value }) => {
+    this.onChangeCountryQuery(value)
+  }
+
+  // TODO set the caling code if counry name is just typed
+  //  TODO figure out what on Suggestion Selected is for and if we need it
+
+  onInputChange = (_, { newValue }) => {
+    this.onChangeCountryQuery(newValue)
+  }
+
+  onSuggestionSelected = (x) => {
+    debugger
+    console.log(x)
+  }
+
+  renderSuggestionsContainer = ({ containerProps, children, query }) => {
+    const { className, ...otherProps } = containerProps
+    return (
+      <View
+        {...otherProps}
+        style={{ backgroundColor: colors.white, position: 'absolute', zIndex: 100 }}
+      >
+        {children}
+      </View>
+    )
   }
 
   render() {
+    const inputProps = {
+      placeholder: 'Country',
+      value: this.state.countryQuery,
+      onChange: this.onInputChange,
+    }
+
     return (
       <>
-        <Autocomplete
-          autoCapitalize="none"
-          autoCorrect={false}
-          // listContainerStyle={style.autoCompleteDropDown}
-          // inputContainerStyle={[style.borderedBox, style.inputBox, style.inputCountry]}
-          // listStyle={[style.borderedBox, style.listAutocomplete]}
-          data={this.filteredCountries()}
-          defaultValue={this.state.countryQuery}
-          onChangeText={this.onChangeCountryQuery}
-          // onEndEditing={this.props.onEndEditingCountryCode}
-          placeholder={'country'}
-          renderItem={this.renderItem}
-          renderTextInput={this.renderTextInput}
+        <Autosuggest
+          alwaysRenderSuggestions={true}
+          suggestions={this.filteredCountries()}
+          getSuggestionValue={getSuggestionValue}
+          onSuggestionsFetchRequested={this.updateSuggestions}
+          onSuggestionSelected={this.onSuggestionSelected}
+          renderSuggestion={this.renderItem}
+          renderInputComponent={this.renderTextInput}
+          renderSuggestionsContainer={this.renderSuggestionsContainer}
+          inputProps={inputProps}
+          highlightFirstSuggestion={true}
         />
         <View style={[standardStyles.row, styles.fakeInputBorder]}>
           <Text style={[fonts.legal, textStyles.invert, textStyles.center, styles.countryCode]}>
-            {this.state.countryCallingCode}+00
+            {this.state.countryCallingCode || '+00'}
           </Text>
           <View style={styles.line} />
           <TextInput
@@ -117,6 +156,8 @@ class PhoneInput extends React.PureComponent<Props, State> {
     )
   }
 }
+
+const getSuggestionValue = (suggestion) => suggestion
 
 const styles = StyleSheet.create({
   line: {
