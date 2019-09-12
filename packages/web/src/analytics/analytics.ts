3446a7d306fb471fcff813c7842122f3f55d73fb
Aaron DeRuvo <aaron.deruvo@gmail.com>
2019-09-12 16:03:19 -0700

    Fixes #938 cookies
    Key points
    
    Disagreeing no longer redirects users away from site, instead it is noted that they responded and said not to track.
    
    Agreeing: Cookies are set that they have responded to the consent form and tracking is enabled.
    
    When tracking is enabled Segment and Sentry are both initialized.
    
    If we `canTrack` they are agreed to on page load.
    
    `canTrack` is defined as either the visitor NOT being identified as likely being in EU or being in EU and agreeing to being tracked.

--- packages/web/src/analytics/analytics.ts
+++ packages/web/src/analytics/analytics.ts
@@ -1,18 +1,23 @@
 import Cookies from 'js-cookie'
 import getConfig from 'next/config'
+const isInEU = require('@segment/in-eu')
 
 let analytics
 const ALLOW_ANALYTICS_COOKIE_NAME = '__allow__analytics__cookie__'
-const DISALLOW_COOKIES_REDIRECT_LOCATION = 'https://google.com'
+const RESPONDED_TO_CONSENT = '__responded_to_consent__'
 
 declare var process: any
 
-export const hasUserGivenCookiesAgreement = () => {
-  return !!Cookies.get(ALLOW_ANALYTICS_COOKIE_NAME)
+export function canTrack(): boolean {
+  return !!Cookies.get(ALLOW_ANALYTICS_COOKIE_NAME) || !isInEU()
+}
+
+export function showVisitorCookieConsent(): boolean {
+  return isInEU() && !Cookies.get(RESPONDED_TO_CONSENT)
 }
 
 const initializeAnalytics = () => {
-  if (hasUserGivenCookiesAgreement() && process.browser) {
+  if (process.browser && canTrack()) {
     const Segment = require('load-segment')
     const { publicRuntimeConfig } = getConfig()
     analytics = Segment({ key: publicRuntimeConfig.__SEGMENT_KEY__ })
@@ -27,12 +32,16 @@ const initializeAnalytics = () => {
 initializeAnalytics()
 
 export const agree = () => {
-  Cookies.set(ALLOW_ANALYTICS_COOKIE_NAME, true)
+  Cookies.set(ALLOW_ANALYTICS_COOKIE_NAME, true, { expires: EXPIRE_DAYS })
+  Cookies.set(RESPONDED_TO_CONSENT, true, { expires: EXPIRE_DAYS })
   initializeAnalytics()
 }
 
 export const disagree = () => {
-  location.href = DISALLOW_COOKIES_REDIRECT_LOCATION
+  Cookies.set(ALLOW_ANALYTICS_COOKIE_NAME, false, { expires: EXPIRE_DAYS })
+  Cookies.set(RESPONDED_TO_CONSENT, true, { expires: EXPIRE_DAYS })
 }
+
+const EXPIRE_DAYS = 365
 
 export default analytics
