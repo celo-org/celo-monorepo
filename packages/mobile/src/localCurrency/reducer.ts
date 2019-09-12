af1a72027327eb5ac55f166d7b0d02d69d7b289d
Jean Regisser <jean.regisser@gmail.com>
2019-09-12 10:57:43 +0200

    Fix local currency persisted state not reset when LOCAL_CURRENCY_SYMBOL changes

--- packages/mobile/src/localCurrency/reducer.ts
+++ packages/mobile/src/localCurrency/reducer.ts
@@ -24,7 +24,8 @@ export const reducer = (
 
       // Make sure we don't use the persisted exchange rate if the symbol is now different
       if (persistedState && persistedState.symbol !== LOCAL_CURRENCY_SYMBOL) {
-        return initialState
+        // return a copy on purpose otherwise it's ignored
+        return { ...initialState }
       }
 
       // Ignore some persisted properties
