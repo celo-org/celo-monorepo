ca646f815cae46ce42a1532df8ce5da5d69f5a9b
Sami Mäkelä <mrsmkl@gmail.com>
2019-09-13 09:26:13 +0300

    small changes

--- packages/protocol/test/common/linkedlist.ts
+++ packages/protocol/test/common/linkedlist.ts
@@ -17,67 +17,69 @@ contract('LinkedListTest', () => {
 
   describe('#insert()', () => {
     const NULL_KEY = '0x00'
-    const first_key = '0x01'
+    const firstKey = '0x01'
     const keys = ['0x01', '0x02', '0x03']
-    const middle_key = '0x02'
-    const last_key = '0x03'
-    const added_key = '0x04'
+    const middleKey = '0x02'
+    const lastKey = '0x03'
+    const addedKey = '0x04'
 
-    it('should revert if previous is equal to key (empty list)', async () => {
-      await assertRevert(linkedListTest.insert(added_key, added_key, NULL_KEY))
+    describe('when inserting to empty list', () => {
+      it('should revert if previous is equal to key', async () => {
+        await assertRevert(linkedListTest.insert(addedKey, addedKey, NULL_KEY))
+      })
+
+      it('should revert if next is equal to key', async () => {
+        await assertRevert(linkedListTest.insert(addedKey, NULL_KEY, addedKey))
+      })
     })
 
-    it('should revert if next is equal to key (empty list)', async () => {
-      await assertRevert(linkedListTest.insert(added_key, NULL_KEY, added_key))
-    })
-
-    describe('singleton', () => {
+    describe('when inserting to singleton', () => {
       beforeEach(async () => {
-        await linkedListTest.insert(first_key, NULL_KEY, NULL_KEY)
+        await linkedListTest.insert(firstKey, NULL_KEY, NULL_KEY)
       })
 
-      it('should revert if next is equal to key (singleton)', async () => {
-        await assertRevert(linkedListTest.insert(added_key, first_key, added_key))
+      it('should revert if next is equal to key', async () => {
+        await assertRevert(linkedListTest.insert(addedKey, firstKey, addedKey))
       })
 
-      it('should revert if previous is equal to key (singleton)', async () => {
-        await assertRevert(linkedListTest.insert(added_key, added_key, first_key))
+      it('should revert if previous is equal to key', async () => {
+        await assertRevert(linkedListTest.insert(addedKey, addedKey, firstKey))
       })
     })
 
-    describe('list with more items', () => {
+    describe('when inserting to a list with more items', () => {
       beforeEach(async () => {
-        await linkedListTest.insert(first_key, NULL_KEY, NULL_KEY)
+        await linkedListTest.insert(firstKey, NULL_KEY, NULL_KEY)
         for (let i = 1; i < keys.length; i++)
           await linkedListTest.insert(keys[i], NULL_KEY, keys[i - 1])
       })
 
       it('should revert if next is equal to key (beginning)', async () => {
-        await assertRevert(linkedListTest.insert(added_key, first_key, added_key))
+        await assertRevert(linkedListTest.insert(addedKey, firstKey, addedKey))
       })
 
       it('should revert if previous is equal to key (beginning)', async () => {
-        await assertRevert(linkedListTest.insert(added_key, added_key, first_key))
+        await assertRevert(linkedListTest.insert(addedKey, addedKey, firstKey))
       })
 
       it('should revert if next is equal to key (end)', async () => {
-        await assertRevert(linkedListTest.insert(added_key, last_key, added_key))
+        await assertRevert(linkedListTest.insert(addedKey, lastKey, addedKey))
       })
 
       it('should revert if previous is equal to key (end)', async () => {
-        await assertRevert(linkedListTest.insert(added_key, added_key, last_key))
+        await assertRevert(linkedListTest.insert(addedKey, addedKey, lastKey))
       })
 
       it('should revert if next is equal to key (middle)', async () => {
-        await assertRevert(linkedListTest.insert(added_key, middle_key, added_key))
+        await assertRevert(linkedListTest.insert(addedKey, middleKey, addedKey))
       })
 
       it('should revert if previous is equal to key (middle)', async () => {
-        await assertRevert(linkedListTest.insert(added_key, added_key, middle_key))
+        await assertRevert(linkedListTest.insert(addedKey, addedKey, middleKey))
       })
 
       it('should revert if next and previous equal to key', async () => {
-        await assertRevert(linkedListTest.insert(added_key, added_key, added_key))
+        await assertRevert(linkedListTest.insert(addedKey, addedKey, addedKey))
       })
     })
   })
