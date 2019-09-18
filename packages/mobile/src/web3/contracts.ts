7b7cad119fc4b11a4d55b3b146be9f09c1eb2d17
Jean Regisser <jean.regisser@gmail.com>
2019-09-18 16:13:42 +0200

    Restore using web3 ipc provider on Android

--- packages/mobile/src/web3/contracts.ts
+++ packages/mobile/src/web3/contracts.ts
@@ -1,5 +1,6 @@
-// import { DocumentDirectoryPath } from 'react-native-fs'
-// import * as net from 'react-native-tcp'
+import { Platform } from 'react-native'
+import { DocumentDirectoryPath } from 'react-native-fs'
+import * as net from 'react-native-tcp'
 import Logger from 'src/utils/Logger'
 import { DEFAULT_TESTNET, Testnets } from 'src/web3/testnets'
 import Web3 from 'web3'
@@ -7,39 +8,38 @@ import Web3 from 'web3'
 // Logging tag
 const tag = 'web3/contracts'
 
-export const getWeb3Provider = (testnet: Testnets) => {
+const getWeb3IpcProvider = (testnet: Testnets) => {
   Logger.debug(tag, 'creating IPCProvider...')
 
-  // const ipcProvider = new Web3.providers.IpcProvider(
-  //   `${DocumentDirectoryPath}/.${testnet}/geth.ipc`,
-  //   net
-  // )
-  const ipcProvider = new Web3.providers.HttpProvider('http://localhost:8545')
+  const ipcProvider = new Web3.providers.IpcProvider(
+    `${DocumentDirectoryPath}/.${testnet}/geth.ipc`,
+    net
+  )
   Logger.debug(tag, 'created IPCProvider')
 
   // More details on the IPC objects can be seen via this
   // console.debug("Ipc connection object is " + Object.keys(ipcProvider.connection));
   // console.debug("Ipc data handle is " + ipcProvider.connection._events['data']);
   // @ts-ignore
-  // const ipcProviderConnection: any = ipcProvider.connection
-  // const dataResponseHandlerKey: string = 'data'
-  // const oldDataHandler = ipcProviderConnection._events[dataResponseHandlerKey]
-  // // Since we are modifying internal properties of IpcProvider, it is best to add this check to ensure that
-  // // any future changes to IpcProvider internals will cause an error instead of a silent failure.
-  // if (oldDataHandler === 'undefined') {
-  //   throw new Error('Data handler is not defined')
-  // }
-  // ipcProviderConnection._events[dataResponseHandlerKey] = (data: any) => {
-  //   if (data.toString().indexOf('"message":"no suitable peers available"') !== -1) {
-  //     // This is Crude check which can be improved. What we are trying to match is
-  //     // {"jsonrpc":"2.0","id":2,"error":{"code":-32000,"message":"no suitable peers available"}}
-  //     Logger.debug(tag, `Error suppressed: ${data}`)
-  //     return true
-  //   } else {
-  //     // Logger.debug(tag, `Received data over IPC: ${data}`)
-  //     oldDataHandler(data)
-  //   }
-  // }
+  const ipcProviderConnection: any = ipcProvider.connection
+  const dataResponseHandlerKey: string = 'data'
+  const oldDataHandler = ipcProviderConnection._events[dataResponseHandlerKey]
+  // Since we are modifying internal properties of IpcProvider, it is best to add this check to ensure that
+  // any future changes to IpcProvider internals will cause an error instead of a silent failure.
+  if (oldDataHandler === 'undefined') {
+    throw new Error('Data handler is not defined')
+  }
+  ipcProviderConnection._events[dataResponseHandlerKey] = (data: any) => {
+    if (data.toString().indexOf('"message":"no suitable peers available"') !== -1) {
+      // This is Crude check which can be improved. What we are trying to match is
+      // {"jsonrpc":"2.0","id":2,"error":{"code":-32000,"message":"no suitable peers available"}}
+      Logger.debug(tag, `Error suppressed: ${data}`)
+      return true
+    } else {
+      // Logger.debug(tag, `Received data over IPC: ${data}`)
+      oldDataHandler(data)
+    }
+  }
 
   // In the future, we might decide to over-ride the error handler via the following code.
   // ipcProvider.on("error", () => {
@@ -47,6 +47,18 @@ export const getWeb3Provider = (testnet: Testnets) => {
   // })
   return ipcProvider
 }
+
+const getWeb3HttpProvider = (testnet: Testnets) => {
+  Logger.debug(tag, 'creating HttpProvider...')
+
+  const httpProvider = new Web3.providers.HttpProvider('http://localhost:8545')
+  Logger.debug(tag, 'created HttpProvider')
+
+  return httpProvider
+}
+
+// Use Http provider on iOS until we add support for local socket on iOS in react-native-tcp
+export const getWeb3Provider = Platform.OS === 'ios' ? getWeb3HttpProvider : getWeb3IpcProvider
 
 export const setWeb3Provider = (testnet: Testnets) => {
   web3.setProvider(getWeb3Provider(testnet))
