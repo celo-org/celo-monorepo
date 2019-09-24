34a29d9b50b018fc45f16a9d486e02e18d31935b
J M Rossy <jm.rossy@gmail.com>
2019-09-24 14:14:12 +0200

    [Wallet] Pin Setup Flow v2 (#1054)
    * Implement new Secure Wallet onboarding designs
    * Fix custom pincode flow (wasn't setting account password correctly)
    * Fix navigation to send flow from tab bar icon
    * Add caching of custom pincodes
    * Convert pincode thunks to Saga and add tests
    * Rename Pincode.tsx -> PincodeSet.tsx (and mostly redo)
    * Rename SystemAuth.tsx -> PincodeEducation.tsx
    * Add migration for new redux state around pincodeType
    * Add a Number Keypad component for in-app keypad
    * Add a backspace icon
    * Add an obfuscated Pincode text component
    * Improve error handling of pin setup screens

--- packages/mobile/src/navigator/Navigator.tsx
+++ packages/mobile/src/navigator/Navigator.tsx
@@ -29,8 +29,9 @@ import { Screens, Stacks } from 'src/navigator/Screens'
 import TabNavigator from 'src/navigator/TabNavigator'
 import PaymentRequestConfirmation from 'src/paymentRequest/PaymentRequestConfirmation'
 import PaymentRequestListScreen from 'src/paymentRequest/PaymentRequestListScreen'
-import Pincode from 'src/pincode/Pincode'
 import PincodeConfirmation from 'src/pincode/PincodeConfirmation'
+import PincodeEducation from 'src/pincode/PincodeEducation'
+import PincodeSet from 'src/pincode/PincodeSet'
 import QRCode from 'src/qrcode/QRCode'
 import QRScanner from 'src/qrcode/QRScanner'
 import FeeEducation from 'src/send/FeeEducation'
@@ -66,7 +67,8 @@ const NuxStack = createStackNavigator(
   {
     [Screens.Language]: { screen: Language },
     [Screens.JoinCelo]: { screen: JoinCelo },
-    [Screens.Pincode]: { screen: Pincode },
+    [Screens.PincodeEducation]: { screen: PincodeEducation },
+    [Screens.PincodeSet]: { screen: PincodeSet },
     [Screens.EnterInviteCode]: { screen: EnterInviteCode },
     [Screens.ImportWallet]: { screen: ImportWallet },
     [Screens.ImportContacts]: { screen: ImportContacts },
@@ -150,7 +152,6 @@ const AppStack = createStackNavigator(
     [Screens.QRScanner]: { screen: QRScanner },
     [Screens.GoldEducation]: { screen: GoldEducation },
     [Screens.Backup]: { screen: Backup },
-    [Screens.Pincode]: { screen: Pincode },
     [Screens.PaymentRequestListScreen]: { screen: PaymentRequestListScreen },
     [Screens.ReclaimPaymentConfirmationScreen]: { screen: ReclaimPaymentConfirmationScreen },
     ...commonScreens,
