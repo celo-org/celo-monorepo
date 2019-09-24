5e69160dbac12494b4ad107179a988a7d41ae8f0
Aaron DeRuvo <aaron.deruvo@gmail.com>
2019-09-24 13:05:26 -0700

    Test can be smaller

--- packages/web/server/EventHelpers.test.ts
+++ packages/web/server/EventHelpers.test.ts
@@ -22,17 +22,6 @@ const example = [
     endDate: undefined,
   },
   {
-    name: 'SF Blockchain Meetup with Oasis Labs, Celo, Harmony, and Blockdaemon',
-    link:
-      'https://www.eventbrite.com/e/sf-blockchain-meetup-with-oasis-labs-celo-and-blockdaemon-tickets-72551965883',
-    celoHosted: false,
-    celoSpeaking: true,
-    description: undefined,
-    location: 'San Francisco, California',
-    startDate: '2019-10-02',
-    endDate: undefined,
-  },
-  {
     name: 'IAMTN Global Summit 2019: Cross Boarder Payments',
     link:
       'https://www.iamtn.org/amp/stablecoins-a-safer-bet-for-cross-border-payments-and-remittances',
@@ -44,16 +33,6 @@ const example = [
     endDate: '2019-10-03',
   },
   {
-    name: 'Grace Hopper',
-    link: 'https://ghc.anitab.org/',
-    celoHosted: false,
-    celoSpeaking: true,
-    description: undefined,
-    location: 'Orlando, Florida',
-    startDate: '2019-10-01',
-    endDate: '2019-10-04',
-  },
-  {
     name: 'Stories From Mexico: The entrepreneurial spirit & need for opportunity',
     link: 'https://www.meetup.com/blockchainforsocialimpactsf/events/264197099/',
     celoHosted: true,
@@ -61,26 +40,6 @@ const example = [
     description: undefined,
     location: 'San Francisco, California',
     startDate: '2019-09-26',
-    endDate: undefined,
-  },
-  {
-    name: 'University of Waterloo Career Fair',
-    link: 'https://uwaterloo.ca/hire/p4e-career-fair-job-fair',
-    celoHosted: false,
-    celoSpeaking: true,
-    description: undefined,
-    location: 'Ontario, Canada',
-    startDate: '2019-09-25',
-    endDate: undefined,
-  },
-  {
-    name: 'UX for Non-Designers',
-    link: 'cryptosprings.org',
-    celoHosted: false,
-    celoSpeaking: true,
-    description: 'A facilitated conversation ',
-    location: 'Palm Springs, California',
-    startDate: '2019-09-24',
     endDate: undefined,
   },
 ]
@@ -124,37 +83,7 @@ const RESULT = [
   },
   {
     celoHosted: false,
-
-    name: 'SF Blockchain Meetup with Oasis Labs, Celo, Harmony, and Blockdaemon',
-
-    startDate: '2019-10-02',
-  },
-  {
-    celoHosted: false,
-
     name: 'IAMTN Global Summit 2019: Cross Boarder Payments',
-
     startDate: '2019-10-02',
-  },
-  {
-    celoHosted: false,
-
-    name: 'Grace Hopper',
-
-    startDate: '2019-10-01',
-  },
-  {
-    celoHosted: false,
-
-    name: 'University of Waterloo Career Fair',
-
-    startDate: '2019-09-25',
-  },
-  {
-    celoHosted: false,
-
-    name: 'UX for Non-Designers',
-
-    startDate: '2019-09-24',
   },
 ]
