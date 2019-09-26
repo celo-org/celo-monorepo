473a31ecc0c236ddadb9629362a56f10ea26b919
Martín Volpe <martin.volpe@gmail.com>
2019-09-26 20:23:19 +0200

    Do not run the emulator and the build at the same time

--- packages/mobile/scripts/start_emulator.sh
+++ packages/mobile/scripts/start_emulator.sh
@@ -3,4 +3,4 @@
 
 #$ANDROID_SDK_ROOT/emulator/emulator -avd `$ANDROID_SDK_ROOT/emulator/emulator -list-avds | grep 'x86' | head -n 1`
 
-$ANDROID_SDK_ROOT/emulator/emulator -avd $AVD_NAME -noaudio -no-boot-anim -no-window -accel on -verbose &
+emulator -avd $AVD_NAME -noaudio -no-boot-anim -no-window -verbose &
