8cdc4c9796e451f7e1d3495a6e9fb7a5f91df486
annakaz <annakaz@users.noreply.github.com>
2019-09-26 15:05:47 -0700

    Switch to contractkit

--- packages/notification-service/src/exchange/exchangeQuery.ts
+++ packages/notification-service/src/exchange/exchangeQuery.ts
@@ -1,5 +1,5 @@
+import { ContractKit, newKitFromWeb3 } from '@celo/contractkit'
 import { CURRENCY_ENUM } from '@celo/utils'
-import { ContractUtils } from '@celo/walletkit' // To be updated to contractkit when new version is published on npm
 import BigNumber from 'bignumber.js'
 import Web3 from 'web3'
 import { WEB3_PROVIDER_URL } from '../config'
@@ -12,34 +12,35 @@ const SELL_AMOUNTS = {
 }
 
 export async function handleExchangeQuery() {
-  const web3Instance = await getWeb3Instance()
+  const contractKitInstance = await getContractKit()
   const fetchTime = Date.now().toString()
   const [dollarMakerRate, goldMakerRate] = await Promise.all([
-    getExchangeRate(CURRENCY_ENUM.DOLLAR, web3Instance),
-    getExchangeRate(CURRENCY_ENUM.GOLD, web3Instance),
+    getExchangeRate(CURRENCY_ENUM.DOLLAR, contractKitInstance),
+    getExchangeRate(CURRENCY_ENUM.GOLD, contractKitInstance),
   ])
 
   writeExchangeRatePair(CURRENCY_ENUM.DOLLAR, dollarMakerRate.toString(), fetchTime)
   writeExchangeRatePair(CURRENCY_ENUM.GOLD, goldMakerRate.toString(), fetchTime)
 }
 
-export async function getExchangeRate(makerToken: CURRENCY_ENUM, web3Instance: Web3) {
-  const rate = await ContractUtils.getExchangeRate(
-    web3Instance,
-    makerToken,
-    SELL_AMOUNTS[makerToken]
+export async function getExchangeRate(makerToken: CURRENCY_ENUM, contractKitInstance: ContractKit) {
+  const exchange = await contractKitInstance.contracts.getExchange()
+  const rate = await exchange.getExchangeRate(
+    SELL_AMOUNTS[makerToken],
+    makerToken === CURRENCY_ENUM.GOLD
   )
   return rate
 }
 
-let web3: Web3
-export function getWeb3Instance(): Web3 {
-  if (web3 && web3.eth.net.isListening()) {
+let contractKit: ContractKit
+export function getContractKit(): ContractKit {
+  if (contractKit && contractKit.isListening()) {
     // Already connected
-    return web3
+    return contractKit
   } else {
     const httpProvider = new Web3.providers.HttpProvider(WEB3_PROVIDER_URL)
-    web3 = new Web3(httpProvider)
-    return web3
+    const web3 = new Web3(httpProvider)
+    contractKit = newKitFromWeb3(web3)
+    return contractKit
   }
 }
