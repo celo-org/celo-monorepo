fc8c9599e8cd279249dcd6637f4f450b5ec87682
Rohit <8ohit.dua@gmail.com>
2019-09-30 12:53:30 +0200

    split long lines

--- packages/analytics/client_log_exporter.py
+++ packages/analytics/client_log_exporter.py
@@ -5,6 +5,8 @@ import logging
 import re
 import json
 import urllib2
+import sys
+import textwrap
 
 import apache_beam as beam
 from apache_beam.io.gcp.pubsub import ReadFromPubSub
@@ -53,19 +55,8 @@ class ReadGCSNotifications(beam.PTransform):
       'phone': urllib2.unquote(name_comps[0]).decode('utf8'),
       'filepath': filepath
     }
- #   except:
- #     logging.warn("Couldn't read metadata for %s", filepath)
- #     return []
-
+    self.logline_metadata = logline_metadata
     logging.info('Got file: %s with %s', filepath, logline_metadata)
-
-    severity_pattern = re.compile('^([A-Za-z]+)')
-    severity_remappings = {
-      'TRACE' : 'DEBUG',
-      'LOG'   : 'DEBUG',
-      'WARN'  : 'WARNING',
-      'CRIT'  : 'CRITICAL'
-    }
 
     if not self.gcs:
       # These imports have to be nested (ugh) because the constructor and the 
@@ -79,32 +70,43 @@ class ReadGCSNotifications(beam.PTransform):
     # we can do in dataflow right now.
 
     with self.gcs.open('gs://%s/%s' % (bucket, filepath), mime_type='text/plain') as infile:
-
       for line in infile:
-        
-        # Build log element from message, and labels from metadata
-        log_element = dict(logline_metadata)
-        log_element['msg'] = line
-
-        # Try to parse out the severity from the start of the line
-        # And try and make sure it maps to a valid SD severity
-        match = severity_pattern.match(line)
-        if match:     
-          log_severity = match.group(1).upper()
-          log_severity = severity_remappings.get(log_severity, log_severity)
-
-          try:
-            # Write the struct to SD using the hopefully valid severity
-            self.logger.log_struct(log_element, severity=log_severity) 
-          except:
-            # Write the struct to SD without a severity 
-            self.logger.log_struct(log_element) 
-
+        if sys.getsizeof(line) > 1000:
+          lines = textwrap.wrap(line, 1000, break_long_words=False)
+          for text in lines:
+            self.writeLog(text)
         else:
-          # Write the struct to SD without a severity 
-          self.logger.log_struct(log_element) 
-          
+          self.writeLog(line)
     return []
+
+  def writeLog(self, text):
+    severity_pattern = re.compile('^([A-Za-z]+)')
+    severity_remappings = {
+      'TRACE' : 'DEBUG',
+      'LOG'   : 'DEBUG',
+      'WARN'  : 'WARNING',
+      'CRIT'  : 'CRITICAL'
+    }
+    # Build log element from message, and labels from metadata
+    log_element = dict(self.logline_metadata)
+    log_element['msg'] = text
+
+    # Try to parse out the severity from the start of the line
+    # And try and make sure it maps to a valid SD severity
+    match = severity_pattern.match(text)
+    if match:     
+      log_severity = match.group(1).upper()
+      log_severity = severity_remappings.get(log_severity, log_severity)
+      try:
+        # Write the struct to SD using the hopefully valid severity
+        self.logger.log_struct(log_element, severity=log_severity) 
+      except:
+        # Write the struct to SD without a severity 
+        self.logger.log_struct(log_element) 
+    else:
+      # Write the struct to SD without a severity 
+      self.logger.log_struct(log_element) 
+    
 
 
   def expand(self, pcoll):
