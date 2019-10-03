27e91047a014d187067bae9eca68a97675821b70
Aaron DeRuvo <aaron.deruvo@gmail.com>
2019-10-02 17:44:08 -0700

    Use bookLayout component

--- packages/web/src/about/PressMedia.tsx
+++ packages/web/src/about/PressMedia.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react'
 import { Text } from 'react-native'
-import { I18nProps, withNamespaces, NameSpaces } from 'src/i18n'
-import SideTitledSection from 'src/layout/SideTitledSection'
+import BookLayout from 'src/about/BookLayout'
+import { I18nProps, NameSpaces, withNamespaces } from 'src/i18n'
 import Button, { BTN } from 'src/shared/Button.3'
-import { fonts, textStyles } from 'src/styles'
+import { fonts } from 'src/styles'
 
 export default withNamespaces(NameSpaces.about)(function PressAndMedia({ t }: I18nProps) {
   return (
-    <SideTitledSection title={t('Press and Media')}>
-      <Text style={[fonts.p, textStyles.center]}>
+    <BookLayout label={t('Press and Media')} endBlock={true}>
+      <Text style={[fonts.p]}>
         {t('pressText')}{' '}
         <Button kind={BTN.INLINE} text={'press@celo.org'} href="mailto:press@celo.org" />.
       </Text>
-    </SideTitledSection>
+    </BookLayout>
   )
 })
