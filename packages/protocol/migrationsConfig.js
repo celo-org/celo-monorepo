e1a0501ab0ca567a7aa13699adbe9cdb11cd0895
Sami Mäkelä <mrsmkl@gmail.com>
2019-10-03 09:43:23 +0300

    removed commented code

--- packages/protocol/migrationsConfig.js
+++ packages/protocol/migrationsConfig.js
@@ -99,7 +99,6 @@ const linkedLibraries = {
   IntegerSortedLinkedList: ['Governance', 'IntegerSortedLinkedListTest'],
   AddressSortedLinkedListWithMedian: ['SortedOracles', 'AddressSortedLinkedListWithMedianTest'],
   Signatures: ['LockedGold', 'Escrow'],
-  //  ExtractFunctionSignature: ['Governance', 'ExtractFunctionSignatureTest'],
 }
 
 const argv = minimist(process.argv.slice(2), {
