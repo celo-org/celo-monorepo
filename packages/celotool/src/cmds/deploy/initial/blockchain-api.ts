17aad5b3533e326046e66c0cad96152df7a02b97
annakaz <annakaz@users.noreply.github.com>
2019-10-15 13:22:16 -0700

    Add script to update faucet addresses in blockchain api

--- packages/celotool/src/cmds/deploy/initial/blockchain-api.ts
+++ packages/celotool/src/cmds/deploy/initial/blockchain-api.ts
@@ -12,6 +12,14 @@ type BlockchainApiArgv = UpgradeArgv
 export const handler = async (argv: BlockchainApiArgv) => {
   await switchToClusterFromEnv()
   const testnetProjectName = fetchEnv(envVar.TESTNET_PROJECT_NAME)
+  console.info(`updating blockchain-api yaml file for env ${argv.celoEnv}`)
+  await execCmd(
+    `yarn --cwd ../blockchain-api new_address=$(celotooljs generate address-from-env --index 0 --accountType validator --celo-env ${
+      argv.celoEnv
+    }) | sed -i.bak 's/FAUCET_ADDRESS: .*$/FAUCET_ADDRESS: \"'"$new_address"'\"/g' app.${
+      argv.celoEnv
+    }.yaml `
+  )
   console.info(`deploying blockchain-api for env ${argv.config} to ${testnetProjectName}`)
   await execCmd(
     `yarn --cwd ../blockchain-api run deploy -p ${testnetProjectName} -n ${argv.celoEnv}`
