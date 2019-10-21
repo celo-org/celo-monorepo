940768f4fc6f531aff0019aa711832fd34d9ca16
Connor McEwen <connor.mcewen@gmail.com>
2019-10-21 11:21:37 -0700

    Begin e2e attestations test

---
+++ packages/celotool/src/e2e-tests/attestations.ts
@@ -0,0 +1,62 @@
+import { CeloContract, ContractKit, newKit } from '@celo/contractkit'
+import { AttestationsWrapper } from '@celo/contractkit/lib/wrappers/Attestations'
+import { GoldTokenWrapper } from '@celo/contractkit/lib/wrappers/GoldTokenWrapper'
+import { getContext, GethTestConfig, sleep } from './utils'
+
+const validatorAddress = '0x47e172f6cfb6c7d01c1574fa3e2be7cc73269d95'
+
+describe('governance tests', () => {
+  const gethConfig: GethTestConfig = {
+    migrate: true,
+    instances: [
+      { name: 'validator0', validating: true, syncmode: 'full', port: 30303, rpcport: 8545 },
+      { name: 'validator1', validating: true, syncmode: 'full', port: 30305, rpcport: 8547 },
+      { name: 'validator2', validating: true, syncmode: 'full', port: 30307, rpcport: 8549 },
+      { name: 'validator3', validating: true, syncmode: 'full', port: 30309, rpcport: 8551 },
+      { name: 'validator4', validating: true, syncmode: 'full', port: 30311, rpcport: 8553 },
+    ],
+  }
+
+  const context: any = getContext(gethConfig)
+  let contractKit: ContractKit
+  let Attestations: AttestationsWrapper
+  let GoldToken: GoldTokenWrapper
+
+  before(async function(this: any) {
+    this.timeout(0)
+    await context.hooks.before()
+  })
+
+  after(context.hooks.after)
+
+  const restart = async () => {
+    await context.hooks.restart()
+    contractKit = newKit('http://localhost:8545')
+    contractKit.defaultAccount = validatorAddress
+
+    // TODO(mcortesi): magic sleep. without it unlockAccount sometimes fails
+    await sleep(2)
+    // Assuming empty password
+    await contractKit.web3.eth.personal.unlockAccount(validatorAddress, '', 1000000)
+    Attestations = await contractKit.contracts.getAttestations()
+    GoldToken = await contractKit.contracts.getGoldToken()
+  }
+
+  describe('Attestations', () => {
+    before(async function() {
+      this.timeout(10000)
+      await restart()
+    })
+
+    it('request an attestation', async () => {
+      console.log('BALANCE: ', (await GoldToken.balanceOf(validatorAddress)).toString())
+
+      const approve = await Attestations.approveAttestationFee(CeloContract.GoldToken, 1)
+      const approveRes = await approve.sendAndWaitForReceipt()
+      console.log('APPROVE RES:', approveRes)
+      const tx = await Attestations.request('+15555555555', 1, CeloContract.GoldToken)
+      const res = await tx.sendAndWaitForReceipt()
+      console.log('RES:', res)
+    })
+  })
+})
