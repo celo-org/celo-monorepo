6ba6499e84b37b52e409c7f708165143467b0291
Jean Regisser <jean.regisser@gmail.com>
2019-10-22 14:24:43 +0200

    Throw error if user address in the db doesn't match the persisted address
    This is to ensure that there's not a logic error that breaks our current
    assumptions in the future

--- packages/mobile/src/firebase/firebase.ts
+++ packages/mobile/src/firebase/firebase.ts
@@ -21,6 +21,10 @@ export const initializeAuth = async (app: Firebase, address: string) => {
   await userRef.child(user.user.uid).transaction((userData) => {
     if (userData == null) {
       return { address }
+    } else if (userData.address !== undefined && userData.address !== address) {
+      // This shouldn't happen! If this is thrown it means the firebase user is reused
+      // with different addresses (which we don't want) or the db was incorrectly changed remotely!
+      throw new Error("User address in the db doesn't match persisted address")
     }
   })
   Logger.info(TAG, 'Firebase Auth initialized successfully')
