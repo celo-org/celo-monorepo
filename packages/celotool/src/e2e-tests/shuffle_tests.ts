d073db9858d02e9e3590613ddca0d911a011a510
Connor McEwen <connor.mcewen@gmail.com>
2019-10-24 11:09:10 -0700

    Try to align on epoch

--- packages/celotool/src/e2e-tests/shuffle_tests.ts
+++ packages/celotool/src/e2e-tests/shuffle_tests.ts
@@ -1,4 +1,5 @@
 import { ContractKit, newKit } from '@celo/contractkit'
+import { assert } from 'chai'
 import { getContext, GethTestConfig, sleep } from './utils'
 
 const validatorAddress = '0x47e172f6cfb6c7d01c1574fa3e2be7cc73269d95'
@@ -9,6 +10,36 @@ const validatorAddresses = [
   '0xA42c9b0d1A30722AEa8b81E72957134897E7A11a',
   '0x848920B14154b6508B8d98e7eE8159AA84b579A4',
   '0x2ffe970257D93eae9d6B134f528b93b262C31030',
+]
+
+const EPOCH = 10
+
+const VALIDATOR_ORDER = [
+  '0x2ffe970257D93eae9d6B134f528b93b262C31030',
+  '0x47e172F6CfB6c7D01C1574fa3E2Be7CC73269D95',
+  '0xa0Af2E71cECc248f4a7fD606F203467B500Dd53B',
+  '0xA42c9b0d1A30722AEa8b81E72957134897E7A11a',
+  '0x2ffe970257D93eae9d6B134f528b93b262C31030',
+  '0x47e172F6CfB6c7D01C1574fa3E2Be7CC73269D95',
+  '0xa0Af2E71cECc248f4a7fD606F203467B500Dd53B',
+  '0xA42c9b0d1A30722AEa8b81E72957134897E7A11a',
+  '0x2ffe970257D93eae9d6B134f528b93b262C31030',
+  '0x47e172F6CfB6c7D01C1574fa3E2Be7CC73269D95',
+  '0xa0Af2E71cECc248f4a7fD606F203467B500Dd53B',
+  '0xA42c9b0d1A30722AEa8b81E72957134897E7A11a',
+  '0x848920B14154b6508B8d98e7eE8159AA84b579A4',
+  '0x2ffe970257D93eae9d6B134f528b93b262C31030',
+  '0x47e172F6CfB6c7D01C1574fa3E2Be7CC73269D95',
+  '0xa0Af2E71cECc248f4a7fD606F203467B500Dd53B',
+  '0xA42c9b0d1A30722AEa8b81E72957134897E7A11a',
+  '0x848920B14154b6508B8d98e7eE8159AA84b579A4',
+  '0x2ffe970257D93eae9d6B134f528b93b262C31030',
+  '0x47e172F6CfB6c7D01C1574fa3E2Be7CC73269D95',
+  '0xa0Af2E71cECc248f4a7fD606F203467B500Dd53B',
+  '0xA42c9b0d1A30722AEa8b81E72957134897E7A11a',
+  '0x848920B14154b6508B8d98e7eE8159AA84b579A4',
+  '0x2ffe970257D93eae9d6B134f528b93b262C31030',
+  '0x47e172F6CfB6c7D01C1574fa3E2Be7CC73269D95',
 ]
 
 describe('governance tests', () => {
@@ -51,21 +82,24 @@ describe('governance tests', () => {
     it('requests an attestation', async function(this: any) {
       this.timeout(100000)
 
-      const latestBlock = (await contractKit.web3.eth.getBlock('latest')).number
+      const latestBlockNumber = (await contractKit.web3.eth.getBlock('latest')).number
+      const epochRemainder = latestBlockNumber % EPOCH
+      const nextEpoch = latestBlockNumber + (epochRemainder === 0 ? 0 : EPOCH - epochRemainder)
 
-      while ((await contractKit.web3.eth.getBlock('latest')).number < latestBlock + 25) {
+      while ((await contractKit.web3.eth.getBlock('latest')).number < nextEpoch + 25) {
         await sleep(2)
         console.log('waiting for 25 blocks')
       }
 
       const blocks = await Promise.all(
         Array.from(Array(25).keys()).map(async (num) =>
-          contractKit.web3.eth.getBlock(num + latestBlock)
+          contractKit.web3.eth.getBlock(num + nextEpoch)
         )
       )
-      const miners = blocks.map((block) => block.miner)
+      const validators = blocks.map((block) => block.miner)
 
-      console.log(miners)
+      console.log(validators)
+      assert.equal(validators, VALIDATOR_ORDER)
     })
   })
 })
