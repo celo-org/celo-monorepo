1c82ae62e1880c7b6d7f4aa8a76c88b6e25b4e05
Anna K <anna.kazlauskas@gmail.com>
2019-10-24 09:47:56 -0700

    [Wallet] Wallet can switch between hosted and local node (#1419)

--- packages/mobile/src/web3/selectors.ts
+++ packages/mobile/src/web3/selectors.ts
@@ -1,6 +1,8 @@
 import { RootState } from 'src/redux/reducers'
 
 export const currentAccountSelector = (state: RootState) => state.web3.account
+export const currentAccountInWeb3KeystoreSelector = (state: RootState) =>
+  state.web3.accountInWeb3Keystore
 export const zeroSyncSelector = (state: RootState) => state.web3.zeroSyncMode
 
 export const privateCommentKeySelector = (state: RootState) => state.web3.commentKey
