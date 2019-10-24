7eea3cbc4aa410a93bc45057d8d12d1957c4e29b
annakaz <annakaz@users.noreply.github.com>
2019-10-24 16:57:00 -0700

    Read local currency from utils

--- packages/mobile/src/localCurrency/consts.ts
+++ packages/mobile/src/localCurrency/consts.ts
@@ -1,18 +1,4 @@
-// Supported local currency codes
-export enum LocalCurrencyCode {
-  USD = 'USD',
-  CAD = 'CAD',
-  EUR = 'EUR',
-  MXN = 'MXN',
-  PHP = 'PHP',
-}
-
-export enum LocalCurrencySymbol {
-  USD = '$',
-  CAD = '$',
-  EUR = '€',
-  MXN = '$',
-  PHP = '₱',
-}
+// Re-export from utils for convinience since we use these often
+export { LocalCurrencyCode, LocalCurrencySymbol } from '@celo/utils/src/currencies'
 
 export const LOCAL_CURRENCY_CODES = Object.values(LocalCurrencyCode)
