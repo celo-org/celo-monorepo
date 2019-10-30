5e44aa479bd579dda6db346a78357f993cf73958
Aaron DeRuvo <aaron.deruvo@gmail.com>
2019-10-29 17:31:36 -0700

    That sweet baking

--- packages/web/src/dev/Cover.tsx
+++ packages/web/src/dev/Cover.tsx
@@ -1,26 +1,28 @@
 import * as React from 'react'
-import { Image, StyleSheet, View } from 'react-native'
+import LazyLoad from 'react-lazyload'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import Fade from 'react-reveal/Fade'
 import LeaderBoard from 'src/dev/LeaderBoard'
 import Transceive from 'src/dev/Transceive'
 import { H2, H4 } from 'src/fonts/Fonts'
 import { I18nProps, withNamespaces } from 'src/i18n'
 import { Cell, GridRow, Spans } from 'src/layout/GridRow'
+import Button, { BTN } from 'src/shared/Button.3'
 import { HEADER_HEIGHT } from 'src/shared/Styles'
-import { colors, standardStyles, textStyles } from 'src/styles'
+import { colors, fonts, standardStyles, textStyles } from 'src/styles'
 const DELAY = 100
 const DURATION = 400
 
 const LEADERS = [
-  { points: 83, identity: 'Rex' },
+  { points: 8, identity: 'Rex' },
   { points: 840, identity: '100%_that_validator_who_is_an_absolute_diva' },
-  { points: 321, identity: 'Heisenberg' },
+  { points: 3200, identity: 'Heisenberg' },
 
-  { points: 764, identity: 'DX-West' },
-  { points: 500, identity: 'Simone2148' },
-  { points: 497, identity: '$celo-$' },
-  { points: 1020, identity: '77zepher' },
-  { points: 940, identity: 'Gatsby & Durben' },
+  { points: 7600, identity: 'DX-West' },
+  { points: 5000, identity: 'Simone2148' },
+  { points: 4009, identity: '$celo-$' },
+  { points: 1002, identity: '77zepher' },
+  { points: 9400, identity: 'Gatsby & Durben' },
 ]
 
 const CoverComponent = React.memo(function Cover({ t }: I18nProps) {
@@ -40,23 +42,45 @@ const CoverComponent = React.memo(function Cover({ t }: I18nProps) {
         <TitleAndSubTitle title={t('greatStakeOff')} subtitle={t('greatStakeOffSubtitle')}>
           <Fade delay={DELAY} duration={DURATION}>
             <Image
-              style={{ height: 151, width: 169 }}
+              style={styles.baking}
               source={require('src/dev/bakeoff.png')}
               resizeMode={'contain'}
             />
           </Fade>
         </TitleAndSubTitle>
       </GridRow>
-      <GridRow>
-        <Cell span={Spans.full}>
-          <LeaderBoard leaders={LEADERS} />
-        </Cell>
-      </GridRow>
-      <GridRow>
-        <Cell span={Spans.full}>
+      <LazyLoad>
+        <GridRow
+          desktopStyle={standardStyles.blockMarginBottom}
+          tabletStyle={standardStyles.blockMarginBottomTablet}
+          mobileStyle={standardStyles.blockMarginBottomMobile}
+        >
+          <Cell span={Spans.full}>
+            <LeaderBoard leaders={LEADERS} />
+          </Cell>
+        </GridRow>
+      </LazyLoad>
+      <GridRow
+        allStyle={standardStyles.centered}
+        desktopStyle={standardStyles.sectionMarginBottom}
+        tabletStyle={standardStyles.sectionMarginBottomTablet}
+        mobileStyle={standardStyles.sectionMarginBottomMobile}
+      >
+        <Cell span={Spans.half} style={standardStyles.centered}>
           <H4 style={[textStyles.center, textStyles.invert, standardStyles.elementalMarginBottom]}>
-            {t('')}
+            {t('aboutTheChallenge')}
           </H4>
+          <Text
+            style={[
+              fonts.p,
+              textStyles.center,
+              textStyles.invert,
+              standardStyles.elementalMarginBottom,
+            ]}
+          >
+            {t('aboutChallengeText')}
+          </Text>
+          <Button text={t('aboutTheChallengeButton')} kind={BTN.PRIMARY} href={'/TODO'} />
         </Cell>
       </GridRow>
     </View>
@@ -116,13 +140,14 @@ const styles = StyleSheet.create({
     overflow: 'hidden',
   },
   phone: {
-    height: '27vh',
+    height: '33vh',
     minHeight: 250,
     marginTop: HEADER_HEIGHT,
   },
   gap: {
     width: 20,
   },
+  baking: { height: 151, width: 169 },
   button: {
     marginHorizontal: 10,
   },
