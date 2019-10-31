ca440c130f7a10d084948a17eb613ab17c847842
J M Rossy <jm.rossy@gmail.com>
2019-10-31 18:42:11 +0100

    Set up new screens for verification

--- packages/mobile/src/verify/VerificationInputScreen.tsx
+++ packages/mobile/src/verify/VerificationInputScreen.tsx
@@ -4,32 +4,19 @@ import { withNamespaces, WithNamespaces } from 'react-i18next'
 import { ScrollView, StyleSheet } from 'react-native'
 import SafeAreaView from 'react-native-safe-area-view'
 import componentWithAnalytics from 'src/analytics/wrapper'
-import Carousel, { CarouselItem } from 'src/components/Carousel'
 import DevSkipButton from 'src/components/DevSkipButton'
 import { Namespaces } from 'src/i18n'
 import { Screens } from 'src/navigator/Screens'
 
-class VerificationLoadingScreen extends React.Component<WithNamespaces> {
+class VerificationInputScreen extends React.Component<WithNamespaces> {
   static navigationOptions = null
 
   render() {
     // const { t } = this.props
-    const items: CarouselItem[] = [
-      {
-        text: 'Test 1',
-      },
-      {
-        text: 'Test 2',
-      },
-      {
-        text: 'Test 3',
-      },
-    ]
     return (
       <SafeAreaView style={styles.container}>
         <ScrollView contentContainerStyle={styles.scrollContainer}>
           <DevSkipButton nextScreen={Screens.WalletHome} />
-          <Carousel containerStyle={styles.carouselContainer} items={items} />
         </ScrollView>
       </SafeAreaView>
     )
@@ -40,7 +27,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'space-between',
-    backgroundColor: colors.backgroundDarker,
+    backgroundColor: colors.background,
   },
   scrollContainer: {
     flex: 1,
@@ -49,11 +36,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  carouselContainer: {
-    marginVertical: 30,
-  },
 })
 
 export default componentWithAnalytics(
-  withNamespaces(Namespaces.nuxVerification2)(VerificationLoadingScreen)
+  withNamespaces(Namespaces.nuxVerification2)(VerificationInputScreen)
 )
