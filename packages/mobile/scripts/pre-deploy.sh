bc0c033e635655b305e6f921ce802b28f75e7a4b
J M Rossy <jm.rossy@gmail.com>
2019-10-31 11:16:47 +0100

    Cleanup package.json test-licenses scripts and a few others in mobile Add circle ci test for checking licenses

--- packages/mobile/scripts/pre-deploy.sh
+++ packages/mobile/scripts/pre-deploy.sh
@@ -10,7 +10,7 @@ yarn run react-native-version
 echo "===Done updating versions. Note: you may need to update the VERSION_CODE in gradle.properties as well if deploying manually==="
 
 echo "===Update license list and disclaimer==="
-yarn update-disclaimer
+yarn deploy:update-disclaimer
 echo "===Done updating licenses==="
 
 echo "Pre-deploy steps complete"
