bfb25cb612043817c6c03396024af1f2291e672c
Martín Volpe <martin.volpe@gmail.com>
2019-11-05 16:14:56 +0100

    Finished up with Connor's help

--- packages/verifier/android/app/src/main/java/org/celo/verifier/MainActivity.java
+++ packages/verifier/android/app/src/main/java/org/celo/verifier/MainActivity.java
@@ -4,6 +4,7 @@ import android.util.Log;
 import android.os.Bundle;
 import android.content.Intent;
 import com.google.firebase.iid.FirebaseInstanceId;
+import org.devio.rn.splashscreen.SplashScreen;	
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -12,6 +13,7 @@ public class MainActivity extends ReactActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);	
         super.onCreate(savedInstanceState);
 
         try {
