dd623ee90b6f48c8f2df297d8d3268cf2a567444
Jean Regisser <jean.regisser@gmail.com>
2019-11-06 12:19:29 +0100

    Fix build and test on enviroments without decryption keys

--- packages/blockchain-api/src/config.ts
+++ packages/blockchain-api/src/config.ts
@@ -1,12 +1,21 @@
 import dotenv from 'dotenv'
-import * as secretsFile from './secrets.json'
 
 // Load environment variables from .env file
 dotenv.config()
 
+function getSecrets(deployEnv: string) {
+  try {
+    return require('./secrets.json')[deployEnv]
+  } catch {
+    // Secrets are only available on trusted environments
+  }
+
+  return {}
+}
+
 export const DEPLOY_ENV = (process.env.DEPLOY_ENV as string).toLowerCase()
 export const EXCHANGE_RATES_API = (process.env.EXCHANGE_RATES_API as string).toLowerCase()
-export const { EXCHANGE_RATES_API_ACCESS_KEY } = (secretsFile as any)[DEPLOY_ENV]
+export const { EXCHANGE_RATES_API_ACCESS_KEY } = getSecrets(DEPLOY_ENV)
 export const BLOCKSCOUT_API = (process.env.BLOCKSCOUT_API as string).toLowerCase()
 export const FAUCET_ADDRESS = (process.env.FAUCET_ADDRESS as string).toLowerCase()
 export const VERIFICATION_REWARDS_ADDRESS = (process.env
