50e73c80081333601462d0ab2452a8e41c016383
Sami Mäkelä <mrsmkl@gmail.com>
2019-11-06 11:19:26 -0800

    Adds a governable parameter controlling how much extra gas gasCurrency transactions use (#1253)

--- packages/protocol/contracts/governance/BlockchainParameters.sol
+++ packages/protocol/contracts/governance/BlockchainParameters.sol
@@ -16,7 +16,10 @@ contract BlockchainParameters is Ownable, Initializable {
 
   ClientVersion private minimumClientVersion;
 
+  uint256 public intrinsicGasForAlternativeGasCurrency;
+
   event MinimumClientVersionSet(uint256 major, uint256 minor, uint256 patch);
+  event IntrinsicGasForAlternativeGasCurrencySet(uint gas);
 
   /**
    * @notice Initializes critical variables.
@@ -30,13 +33,13 @@ contract BlockchainParameters is Ownable, Initializable {
   function initialize(
     uint256 major,
     uint256 minor,
-    uint256 patch
-  )
-    external
-    initializer
+    uint256 patch,
+    uint256 _gasForNonGoldCurrencies
+  ) external initializer
   {
     _transferOwnership(msg.sender);
     setMinimumClientVersion(major, minor, patch);
+    setIntrinsicGasForAlternativeGasCurrency(_gasForNonGoldCurrencies);
   }
 
   /**
@@ -52,6 +55,11 @@ contract BlockchainParameters is Ownable, Initializable {
     minimumClientVersion.minor = minor;
     minimumClientVersion.patch = patch;
     emit MinimumClientVersionSet(major, minor, patch);
+  }
+
+  function setIntrinsicGasForAlternativeGasCurrency(uint256 gas) public onlyOwner {
+    intrinsicGasForAlternativeGasCurrency = gas;
+    emit IntrinsicGasForAlternativeGasCurrencySet(gas);
   }
 
   /** @notice Query minimum client version.
