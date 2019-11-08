3838e8f008baee0aac02f1f70cb68547569616d4
J M Rossy <jm.rossy@gmail.com>
2019-11-08 16:30:04 +0100

    Undo backup phrase change

--- packages/mobile/src/backup/BackupPhraseContainer.tsx
+++ packages/mobile/src/backup/BackupPhraseContainer.tsx
@@ -35,7 +35,7 @@ type Props = {
 
 export class BackupPhraseContainer extends React.Component<Props> {
   async componentDidMount() {
-    if (Platform.OS === 'android' && false) {
+    if (Platform.OS === 'android') {
       FlagSecure.activate()
     } else if (Platform.OS === 'ios') {
       // TODO add iOS support
@@ -43,7 +43,7 @@ export class BackupPhraseContainer extends React.Component<Props> {
   }
 
   componentWillUnmount() {
-    if (Platform.OS === 'android' && false) {
+    if (Platform.OS === 'android') {
       FlagSecure.deactivate()
     } else if (Platform.OS === 'ios') {
       // TODO add iOS support
