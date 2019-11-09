f5649084f1d465f4776f3bb3699edef4ffd788a7
Connor McEwen <connor.mcewen@gmail.com>
2019-11-09 14:46:59 +0000

    New invite script

--- packages/celotool/src/cmds/account/invite.ts
+++ packages/celotool/src/cmds/account/invite.ts
@@ -1,8 +1,8 @@
 /* tslint:disable no-console */
-import { downloadArtifacts } from 'src/lib/artifacts'
-import { switchToClusterFromEnv } from 'src/lib/cluster'
+import { newKit } from '@celo/contractkit'
+import { StableTokenWrapper } from '@celo/contractkit/lib/wrappers/StableTokenWrapper'
+import { BigNumber } from 'bignumber.js'
 import { portForwardAnd } from 'src/lib/port_forward'
-import { execCmd } from 'src/lib/utils'
 import { Argv } from 'yargs'
 import { AccountArgv } from '../account'
 
@@ -16,35 +16,85 @@ interface InviteArgv extends AccountArgv {
 }
 
 export const builder = (yargs: Argv) => {
-  return yargs
-    .option('phone', {
-      type: 'string',
-      description: 'Phone number to send invite code,',
-      demand: 'Please specify phone number to send invite code',
-    })
-    .option('fast', {
-      type: 'boolean',
-      default: false,
-      description: "Don't download artifacts, use this for repeated invocations",
-      demand: 'Please specify phone number to send invite code',
-    })
+  return yargs.option('phone', {
+    type: 'string',
+    description: 'Phone number to send invite code,',
+    demand: 'Please specify phone number to send invite code',
+  })
+}
+
+export async function convertToContractDecimals(
+  value: number | BigNumber,
+  contract: StableTokenWrapper
+) {
+  const decimals = new BigNumber(await contract.decimals())
+  const one = new BigNumber(10).pow(decimals.toNumber())
+  return one.times(value)
 }
 
 export const handler = async (argv: InviteArgv) => {
-  console.log(`Sending invitation code to ${argv.phone}`)
+  const phone = argv.phone
+
+  console.log(`Sending invitation code to ${phone}`)
   const cb = async () => {
-    await execCmd(
-      `yarn --cwd ../protocol run invite -n ${argv.celoEnv} -p ${argv.phone} -f ${argv.fast}`
+    const kit = newKit('http://localhost:8545')
+    const account = (await kit.web3.eth.getAccounts())[0]
+    console.log(`Using account: ${account}`)
+    kit.defaultAccount = account
+
+    // TODO(asa): This number was made up
+    const verificationGasAmount = new BigNumber(10000000)
+    // TODO: this default gas price might not be accurate
+    const gasPrice = 100000000000
+
+    const temporaryWalletAccount = await kit.web3.eth.accounts.create()
+    const temporaryAddress = temporaryWalletAccount.address
+    // Buffer.from doesn't expect a 0x for hex input
+    const privateKeyHex = temporaryWalletAccount.privateKey.substring(2)
+    const inviteCode = Buffer.from(privateKeyHex, 'hex').toString('base64')
+
+    const [goldToken, stableToken, attestations, escrow] = await Promise.all([
+      kit.contracts.getGoldToken(),
+      kit.contracts.getStableToken(),
+      kit.contracts.getAttestations(),
+      kit.contracts.getEscrow(),
+    ])
+    const verificationFee = new BigNumber(
+      await attestations.attestationRequestFees(stableToken.address)
     )
+    const goldAmount = verificationGasAmount.times(gasPrice).toString()
+    const stableTokenInviteAmount = verificationFee.times(10).toString()
+    const stableTokenEscrowAmount = (await convertToContractDecimals(5, stableToken)).toString()
+
+    const phoneHash: string = kit.web3.utils.soliditySha3({
+      type: 'string',
+      value: phone,
+    })
+
+    await stableToken.approve(escrow.address, stableTokenEscrowAmount).sendAndWaitForReceipt()
+    const expirySeconds = 60 * 60 * 24 * 5 // 5 days
+
+    console.log(
+      `Transferring ${goldAmount} Gold, ${stableTokenInviteAmount} StableToken, and escrowing ${stableTokenEscrowAmount}`
+    )
+    await Promise.all([
+      goldToken.transfer(temporaryAddress, goldAmount).sendAndWaitForReceipt(),
+      stableToken.transfer(temporaryAddress, stableTokenInviteAmount).sendAndWaitForReceipt(),
+      escrow
+        .transfer(
+          phoneHash,
+          stableToken.address,
+          stableTokenEscrowAmount,
+          expirySeconds,
+          temporaryAddress,
+          0
+        )
+        .sendAndWaitForReceipt(),
+    ])
+    console.log(`Temp address: ${temporaryAddress}`)
+    console.log(`Invite code: ${inviteCode}`)
   }
   try {
-    if (argv.fast) {
-      console.log(`Fast mode is on, cluster won't be switched, artifacts won't be downloaded`)
-    } else {
-      console.log(`Fast mode is off, artifacts will be downloaded`)
-      await switchToClusterFromEnv(false)
-      await downloadArtifacts(argv.celoEnv)
-    }
     await portForwardAnd(argv.celoEnv, cb)
   } catch (error) {
     console.error(`Unable to send invitation code to ${argv.phone}`)
