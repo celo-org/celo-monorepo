471735d3562d8d123c7d15dc9d94b91b52620a05
Sami Mäkelä <mrsmkl@gmail.com>
2019-11-08 22:42:55 -0800

    Added parameter to set the block gas limit (#1245)

--- packages/protocol/contracts/governance/BlockchainParameters.sol
+++ packages/protocol/contracts/governance/BlockchainParameters.sol
@@ -14,11 +14,12 @@ contract BlockchainParameters is Ownable, Initializable {
   }
 
   ClientVersion private minimumClientVersion;
-
+  uint256 public blockGasLimit;
   uint256 public intrinsicGasForAlternativeGasCurrency;
 
   event MinimumClientVersionSet(uint256 major, uint256 minor, uint256 patch);
   event IntrinsicGasForAlternativeGasCurrencySet(uint256 gas);
+  event BlockGasLimitSet(uint256 limit);
 
   /**
    * @notice Initializes critical variables.
@@ -28,13 +29,19 @@ contract BlockchainParameters is Ownable, Initializable {
    * minor version.
    * @param patch Minimum client version that can be used in the chain,
    * patch level.
+   * @param _gasForNonGoldCurrencies Intrinsic gas for non-gold gas currencies.
+   * @param gasLimit Block gas limit.
    */
-  function initialize(uint256 major, uint256 minor, uint256 patch, uint256 _gasForNonGoldCurrencies)
-    external
-    initializer
-  {
+  function initialize(
+    uint256 major,
+    uint256 minor,
+    uint256 patch,
+    uint256 _gasForNonGoldCurrencies,
+    uint256 gasLimit
+  ) external initializer {
     _transferOwnership(msg.sender);
     setMinimumClientVersion(major, minor, patch);
+    setBlockGasLimit(gasLimit);
     setIntrinsicGasForAlternativeGasCurrency(_gasForNonGoldCurrencies);
   }
 
@@ -53,12 +60,26 @@ contract BlockchainParameters is Ownable, Initializable {
     emit MinimumClientVersionSet(major, minor, patch);
   }
 
+  /**
+   * @notice Sets the block gas limit.
+   * @param gasLimit New block gas limit.
+   */
+  function setBlockGasLimit(uint256 gasLimit) public onlyOwner {
+    blockGasLimit = gasLimit;
+    emit BlockGasLimitSet(gasLimit);
+  }
+
+  /**
+   * @notice Sets the intrinsic gas for non-gold gas currencies.
+   * @param gas Intrinsic gas for non-gold gas currencies.
+   */
   function setIntrinsicGasForAlternativeGasCurrency(uint256 gas) public onlyOwner {
     intrinsicGasForAlternativeGasCurrency = gas;
     emit IntrinsicGasForAlternativeGasCurrencySet(gas);
   }
 
-  /** @notice Query minimum client version.
+  /**
+   * @notice Query minimum client version.
    * @return Returns major, minor, and patch version numbers.
    */
   function getMinimumClientVersion()
