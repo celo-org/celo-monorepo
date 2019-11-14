c00d2064e6d4e8f04ab181553dbb4fb84fbbc733
diminator <dimi.dejonghe@gmail.com>
2019-11-14 09:19:02 +0100

    - local scripts for validator and validatorgroup

---
+++ packages/celotool/register_validatorgroup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Usage: register.sh [data dir] [geth dir]
+DATA_DIR=${1:-"/tmp/e2e/group"}
+FAUCET=${2:-"0x47e172f6cfb6c7d01c1574fa3e2be7cc73269d95"}
+GETH_DIR=${3:-"/home/dimi/celo/celo-blockchain"}
+
+mkdir -p ${DATA_DIR}/keystore
+
+export VALIDATOR=`./bin/celotooljs.sh geth create-account -e local --data-dir ${DATA_DIR} --geth-dir ${GETH_DIR} --password "" | grep 0x | cut -f4 -d' '`
+echo ${VALIDATOR}
+ACCOUNT_NOZEROX=`echo ${VALIDATOR} | cut -c 3-`
+export KEYSTORE_V=`find ${DATA_DIR} -type f -iname "*${ACCOUNT_NOZEROX}"`
+echo ${KEYSTORE_V}
+
+FROM=${FAUCET} TO=${VALIDATOR} AMOUNT=3e22 NUM_TX=1 ts-node src/helpers/transfer.ts
+
+export VALIDATORGROUP=`./bin/celotooljs.sh geth create-account -e local --data-dir ${DATA_DIR} --geth-dir ${GETH_DIR} --password "" | grep 0x | cut -f4 -d' '`
+echo ${VALIDATORGROUP}
+ACCOUNT_NOZEROX=`echo ${VALIDATORGROUP} | cut -c 3-`
+export KEYSTORE_VG=`find ${DATA_DIR} -type f -iname "*${ACCOUNT_NOZEROX}"`
+echo ${KEYSTORE_VG}
+
+FROM=${FAUCET} TO=${VALIDATORGROUP} AMOUNT=3e22 NUM_TX=1 ts-node src/helpers/transfer.ts
+
+KEYSTORE=${KEYSTORE_V} ts-node src/helpers/account_register.ts
+KEYSTORE=${KEYSTORE_VG} ts-node src/helpers/account_register.ts
+
+KEYSTORE=${KEYSTORE_VG} ts-node src/helpers/validatorgroup_register.ts
+KEYSTORE=${KEYSTORE_V} GROUP=${VALIDATORGROUP} ts-node src/helpers/validator_register.ts
+KEYSTORE=${KEYSTORE_VG} MEMBER=${VALIDATOR} ts-node src/helpers/validatorgroup_add.ts
+KEYSTORE=${KEYSTORE_VG} GROUP=${VALIDATORGROUP} ts-node src/helpers/validatorgroup_vote.ts
+KEYSTORE=${KEYSTORE_V} GROUP=${VALIDATORGROUP} ts-node src/helpers/validatorgroup_vote.ts
--- packages/celotool/src/helpers/transfer.ts
+++ packages/celotool/src/helpers/transfer.ts
@@ -1,131 +1,36 @@
-import { CURRENCY_ENUM } from '@celo/utils'
 import BigNumber from 'bignumber.js'
 import Web3 from 'web3'
-import { Tx } from 'web3/eth/types'
-import { erc20Abi, getContractAddress } from '../e2e-tests/utils'
-
-// import LockedGold from './artifacts/LockedGold'
-
-const stableTokenAbi = erc20Abi.concat([
-  {
-    constant: false,
-    inputs: [
-      {
-        name: 'rate',
-        type: 'uint256',
-      },
-      {
-        name: 'updatePeriod',
-        type: 'uint256',
-      },
-    ],
-    name: 'setInflationParameters',
-    outputs: [],
-    payable: false,
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: 'getInflationParameters',
-    outputs: [
-      {
-        name: '',
-        type: 'uint256',
-      },
-      {
-        name: '',
-        type: 'uint256',
-      },
-      {
-        name: '',
-        type: 'uint256',
-      },
-      {
-        name: '',
-        type: 'uint256',
-      },
-    ],
-    payable: false,
-    stateMutability: 'view',
-    type: 'function',
-  },
-])
-const strip0xFromAddress = (addressWith0x: string) => addressWith0x.split('x')[1]
-
-const transferCeloGold = async (
-  fromAddress: string,
-  toAddress: string,
-  amount: BigNumber,
-  txOptions: any = {}
-) => {
-  // Hack to get the node to suggest a price for us.
-  // Otherwise, web3 will suggest the default gold price.
-  if (txOptions.gasCurrency) {
-    txOptions.gasPrice = '0'
-  }
-
-  const tx: Tx = {
-    from: fromAddress,
-    to: toAddress,
-    value: amount.toString(),
-    ...txOptions,
-  }
-  if (!tx.gas) {
-    tx.gas = await web3.eth.estimateGas(tx)
-  }
-  return new Promise(async (resolve, reject) => {
-    try {
-      await web3.eth
-        .sendTransaction(tx)
-        .on('confirmation', (_: any, receipt: any) => resolve(receipt))
-    } catch (err) {
-      reject(err)
-    }
-  })
-}
+import { displaySendTx } from '@celo/celocli/lib/utils/cli'
+import { newKitFromWeb3 } from '@celo/contractkit'
 
 const web3 = new Web3('http://localhost:8545')
-const from: string = strip0xFromAddress(process.env.FROM || '0x0')
-const to: string = strip0xFromAddress(process.env.TO || '0x0')
-const NUM_TX = process.env.NUM_TX || 40
-const AMOUNT = new BigNumber(process.env.AMOUNT || 0)
-
-const randomItem = (list: string[]) => {
-  return list[Math.floor(Math.random() * list.length)]
-}
+const from: string = process.env.FROM || '0x0'
+const to: string = process.env.TO || '0x0'
+const amountInWei = new BigNumber(process.env.AMOUNT || 0)
 ;(async () => {
-  const stableTokenAddress = await getContractAddress('StableTokenProxy')
-  const stableToken = new web3.eth.Contract(stableTokenAbi, stableTokenAddress)
-  const getBalance = async (address: string) => {
-    const balance: any = {}
-    const goldBalance = new BigNumber(await web3.eth.getBalance(address))
-    const dollarBalance = new BigNumber(await stableToken.methods.balanceOf(address).call())
-    balance[CURRENCY_ENUM.GOLD] = goldBalance
-    balance[CURRENCY_ENUM.DOLLAR] = dollarBalance
-    return balance
-  }
+  const kit = newKitFromWeb3(web3)
+  const goldToken = await kit.contracts.getGoldToken()
 
-  const accounts = await web3.eth.getAccounts()
+  // Check the balance before
+  const balanceFromBeforeInWei = await goldToken.balanceOf(to)
+  console.log('balance to  :', balanceFromBeforeInWei.toString())
+  const balanceToBeforeInWei = await goldToken.balanceOf(from)
+  console.log('balance from:', balanceToBeforeInWei.toString())
 
-  for (let k = 0; k < NUM_TX; k++) {
-    const toAddress = to === '0x0' ? strip0xFromAddress(randomItem(accounts)) : to
-    const feeRecipientAddress = randomItem(accounts)
-    const balance = await getBalance(from)
-    let amount = AMOUNT
-      ? AMOUNT
-      : new BigNumber(Math.floor((Math.random() * balance[CURRENCY_ENUM.GOLD]) / 10000))
+  // Perform the transfer
+  await displaySendTx('gold.Transfer', goldToken.transfer(to, amountInWei.toFixed()), {
+    from: from,
+  })
 
-    await transferCeloGold(from, toAddress, amount, {
-      gasCurrency: stableTokenAddress,
-      gasFeeRecipient: feeRecipientAddress,
-    })
-    console.log(
-      k,
-      amount.toString(),
-      balance[CURRENCY_ENUM.GOLD].toString(),
-      balance[CURRENCY_ENUM.DOLLAR].toString()
-    )
-  }
+  // Check the balance after
+  const balanceFromAfterInWei = await goldToken.balanceOf(to)
+  console.log('new balance to  :', balanceFromAfterInWei.toString())
+  const balanceToAfterInWei = await goldToken.balanceOf(from)
+  console.log('new balance from:', balanceToAfterInWei.toString())
+  // Get gas cost
+  const differenceInWei = balanceFromBeforeInWei.minus(balanceFromAfterInWei)
+  const gasCostInWei = differenceInWei.minus(amountInWei)
+
+  // }
+  console.log(`transfer ${amountInWei} from ${from} to ${to} - gas: ${gasCostInWei}`)
 })()
