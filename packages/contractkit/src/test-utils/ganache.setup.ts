6c4b1e9c3d7f98dd9fd3020b145c15a4826e5c3c
Nam Chu Hoai <nambrot@googlemail.com>
2019-11-17 19:27:09 -0800

    Fix build

--- packages/contractkit/src/test-utils/ganache.setup.ts
+++ packages/contractkit/src/test-utils/ganache.setup.ts
@@ -1,5 +1,7 @@
 import baseSetup from '@celo/dev-utils/lib/ganache-setup'
 import * as path from 'path'
+// Has to import the matchers somewhere so that typescript knows the matchers have been made available
+import _unused from '@celo/dev-utils/lib/matchers'
 
 export default function setup() {
   return baseSetup(path.resolve(path.join(__dirname, '../../.devchain')))
