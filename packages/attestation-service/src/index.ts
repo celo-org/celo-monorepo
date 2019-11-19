f14365da6aee186947c4b1a5051ba049121a0668
Nam Chu Hoai <nambrot@googlemail.com>
2019-11-18 19:06:06 -0800

    Add structured logging and Prometheus events

--- packages/attestation-service/src/index.ts
+++ packages/attestation-service/src/index.ts
@@ -1,8 +1,17 @@
 import * as dotenv from 'dotenv'
+
+// We need to load the config before some of the imports for e.g. the logger
+if (process.env.CONFIG) {
+  dotenv.config({ path: process.env.CONFIG })
+}
+
 import express from 'express'
 import RateLimiter from 'express-rate-limit'
+import requestIdMiddleware from 'express-request-id'
+import * as PromClient from 'prom-client'
 import { initializeDB, initializeKit } from './db'
-import { createValidatedHandler } from './request'
+import { logger } from './logger'
+import { createValidatedHandler, loggerMiddleware } from './request'
 import {
   AttestationRequestType,
   getAttestationKey,
@@ -10,12 +19,8 @@ import {
 } from './requestHandlers/attestation'
 import { handleStatusRequest, StatusRequestType } from './requestHandlers/status'
 import { initializeSmsProviders } from './sms'
-async function init() {
-  console.info(process.env.CONFIG)
-  if (process.env.CONFIG) {
-    dotenv.config({ path: process.env.CONFIG })
-  }
 
+async function init() {
   await initializeDB()
   await initializeKit()
   // TODO: Validate that the attestation key has been authorized by the account
@@ -23,15 +28,18 @@ async function init() {
   await initializeSmsProviders()
 
   const app = express()
-  app.use(express.json())
+  app.use([express.json(), requestIdMiddleware(), loggerMiddleware])
   const port = process.env.PORT || 3000
-  app.listen(port, () => console.log(`Server running on ${port}!`))
+  app.listen(port, () => logger.info({ port }, 'Attestation Service started'))
 
   const rateLimiter = new RateLimiter({
     windowMs: 5 * 60 * 100, // 5 minutes
     max: 50,
     // @ts-ignore
     message: { status: false, error: 'Too many requests, please try again later' },
+  })
+  app.get('/metrics', (_req, res) => {
+    res.send(PromClient.register.metrics())
   })
   app.get('/status', rateLimiter, createValidatedHandler(StatusRequestType, handleStatusRequest))
   app.post(
@@ -41,7 +49,6 @@ async function init() {
 }
 
 init().catch((err) => {
-  console.error(`Error occurred while running server, exiting ....`)
-  console.error(err)
+  logger.error({ err })
   process.exit(1)
 })
