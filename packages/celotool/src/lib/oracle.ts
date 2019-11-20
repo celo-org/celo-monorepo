19f0e695f4f4937399d7e2130c79770dbda761a8
Audrey Penven <audrey@celo.org>
2019-11-20 19:18:51 +0100

    move some values into .env. add a dockerfile for a standalone cli image without geth

--- packages/celotool/src/lib/oracle.ts
+++ packages/celotool/src/lib/oracle.ts
@@ -1,19 +1,27 @@
+import { getFornoUrl } from 'src/lib/endpoints'
 import { envVar, fetchEnv } from 'src/lib/env-utils'
 import { installGenericHelmChart } from 'src/lib/helm_deploy'
 
 const helmChartPath = '../helm-charts/oracle'
 
 export async function installHelmChart(celoEnv: string) {
-  return installGenericHelmChart(celoEnv, releaseName(celoEnv), helmChartPath, helmParameters())
+  return installGenericHelmChart(
+    celoEnv,
+    releaseName(celoEnv),
+    helmChartPath,
+    helmParameters(celoEnv)
+  )
 }
 
-function helmParameters() {
+function helmParameters(celoEnv: string) {
   return [
     `--set celotool.image.repository=${fetchEnv('CELOTOOL_DOCKER_IMAGE_REPOSITORY')}`,
     `--set celotool.image.tag=${fetchEnv('CELOTOOL_DOCKER_IMAGE_TAG')}`,
     `--set mnemonic="${fetchEnv(envVar.MNEMONIC)}"`,
     `--set oracle.image.repository=${fetchEnv('ORACLE_DOCKER_IMAGE_REPOSITORY')}`,
-    `--set oracle.image.tag=${fetchEnv('ORACLE_DOCKER_IMAGE_TAG')}`,
+    `--set celocli.nodeUrl=${getFornoUrl(celoEnv)}`,
+    `--set celocli.image.repository=${fetchEnv('CELOCLI_STANDALONE_IMAGE_REPOSITORY')}`,
+    `--set celocli.image.tag=${fetchEnv('CELOCLI_STANDALONE_IMAGE_TAG')}`,
   ]
 }
 
