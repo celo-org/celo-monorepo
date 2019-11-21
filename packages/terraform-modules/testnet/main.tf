ee49ab514f40562b7d86afab9282da009babb5a5
Trevor Porter <trkporter@ucdavis.edu>
2019-11-21 14:52:40 +0100

    Run terraform fmt

--- packages/terraform-modules/testnet/main.tf
+++ packages/terraform-modules/testnet/main.tf
@@ -29,7 +29,7 @@ data "terraform_remote_state" "state" {
 
 locals {
   firewall_target_tags_bootnode = ["${var.celo_env}-bootnode"]
-  firewall_target_tags_node = ["${var.celo_env}-node"]
+  firewall_target_tags_node     = ["${var.celo_env}-node"]
 }
 
 data "google_compute_network" "network" {
@@ -124,24 +124,24 @@ module "bootnode" {
 module "tx_node" {
   source = "./modules/full-node"
   # variables
-  block_time                        = var.block_time
-  bootnode_ip_address               = module.bootnode.ip_address
-  celo_env                          = var.celo_env
-  ethstats_host                     = var.ethstats_host
-  gcloud_secrets_base_path          = var.gcloud_secrets_base_path
-  gcloud_secrets_bucket             = var.gcloud_secrets_bucket
-  gcloud_vm_service_account_email   = var.gcloud_vm_service_account_email
-  genesis_content_base64            = var.genesis_content_base64
+  block_time                            = var.block_time
+  bootnode_ip_address                   = module.bootnode.ip_address
+  celo_env                              = var.celo_env
+  ethstats_host                         = var.ethstats_host
+  gcloud_secrets_base_path              = var.gcloud_secrets_base_path
+  gcloud_secrets_bucket                 = var.gcloud_secrets_bucket
+  gcloud_vm_service_account_email       = var.gcloud_vm_service_account_email
+  genesis_content_base64                = var.genesis_content_base64
   geth_exporter_docker_image_repository = var.geth_exporter_docker_image_repository
-  geth_exporter_docker_image_tag    = var.geth_exporter_docker_image_tag
-  geth_node_docker_image_repository = var.geth_node_docker_image_repository
-  geth_node_docker_image_tag        = var.geth_node_docker_image_tag
-  geth_verbosity                    = var.geth_verbosity
-  in_memory_discovery_table         = var.in_memory_discovery_table
-  name                              = "tx-node"
-  network_id                        = var.network_id
-  network_name                      = data.google_compute_network.network.name
-  node_count                        = var.tx_node_count
+  geth_exporter_docker_image_tag        = var.geth_exporter_docker_image_tag
+  geth_node_docker_image_repository     = var.geth_node_docker_image_repository
+  geth_node_docker_image_tag            = var.geth_node_docker_image_tag
+  geth_verbosity                        = var.geth_verbosity
+  in_memory_discovery_table             = var.in_memory_discovery_table
+  name                                  = "tx-node"
+  network_id                            = var.network_id
+  network_name                          = data.google_compute_network.network.name
+  node_count                            = var.tx_node_count
 }
 
 # used for access by blockscout
@@ -162,24 +162,24 @@ module "tx_node_lb" {
 module "validator" {
   source = "./modules/validator"
   # variables
-  block_time                        = var.block_time
-  bootnode_ip_address               = module.bootnode.ip_address
-  celo_env                          = var.celo_env
-  ethstats_host                     = var.ethstats_host
-  gcloud_secrets_base_path          = var.gcloud_secrets_base_path
-  gcloud_secrets_bucket             = var.gcloud_secrets_bucket
-  gcloud_vm_service_account_email   = var.gcloud_vm_service_account_email
-  genesis_content_base64            = var.genesis_content_base64
+  block_time                            = var.block_time
+  bootnode_ip_address                   = module.bootnode.ip_address
+  celo_env                              = var.celo_env
+  ethstats_host                         = var.ethstats_host
+  gcloud_secrets_base_path              = var.gcloud_secrets_base_path
+  gcloud_secrets_bucket                 = var.gcloud_secrets_bucket
+  gcloud_vm_service_account_email       = var.gcloud_vm_service_account_email
+  genesis_content_base64                = var.genesis_content_base64
   geth_exporter_docker_image_repository = var.geth_exporter_docker_image_repository
-  geth_exporter_docker_image_tag    = var.geth_exporter_docker_image_tag
-  geth_node_docker_image_repository = var.geth_node_docker_image_repository
-  geth_node_docker_image_tag        = var.geth_node_docker_image_tag
-  geth_verbosity                    = var.geth_verbosity
-  in_memory_discovery_table         = var.in_memory_discovery_table
-  istanbul_request_timeout_ms       = var.istanbul_request_timeout_ms
-  network_id                        = var.network_id
-  network_name                      = data.google_compute_network.network.name
-  proxied_validator_count           = var.proxied_validator_count
-  tx_node_count                     = var.tx_node_count
-  validator_count                   = var.validator_count
+  geth_exporter_docker_image_tag        = var.geth_exporter_docker_image_tag
+  geth_node_docker_image_repository     = var.geth_node_docker_image_repository
+  geth_node_docker_image_tag            = var.geth_node_docker_image_tag
+  geth_verbosity                        = var.geth_verbosity
+  in_memory_discovery_table             = var.in_memory_discovery_table
+  istanbul_request_timeout_ms           = var.istanbul_request_timeout_ms
+  network_id                            = var.network_id
+  network_name                          = data.google_compute_network.network.name
+  proxied_validator_count               = var.proxied_validator_count
+  tx_node_count                         = var.tx_node_count
+  validator_count                       = var.validator_count
 }
