9443b86bd9969ccff613a6c4901c9a624c78d14e
Asa Oines <a@celo.org>
2019-11-22 20:53:33 -0800

    Update faucet to pull from the reserve when possible

--- packages/celotool/src/cmds/account/faucet.ts
+++ packages/celotool/src/cmds/account/faucet.ts
@@ -1,5 +1,6 @@
 /* tslint:disable no-console */
 import { newKit } from '@celo/contractkit'
+import BigNumber from 'bignumber.js'
 import { switchToClusterFromEnv } from 'src/lib/cluster'
 import { convertToContractDecimals } from 'src/lib/contract-utils'
 import { portForwardAnd } from 'src/lib/port_forward'
@@ -9,24 +10,37 @@ import { AccountArgv } from '../account'
 
 export const command = 'faucet'
 
-export const describe = 'command for fauceting an address with gold and dollars'
+export const describe = 'command for fauceting an address with gold and/or dollars'
 
 interface FaucetArgv extends AccountArgv {
   account: string
+  gold: number
+  dollars: number
 }
 
 export const builder = (argv: yargs.Argv) => {
-  return argv.option('account', {
-    type: 'string',
-    description: 'Account to faucet',
-    demand: 'Please specify account to faucet',
-    coerce: (address) => {
-      if (!validateAccountAddress(address)) {
-        throw Error(`Receiver Address is invalid: "${address}"`)
-      }
-      return address
-    },
-  })
+  return argv
+    .option('account', {
+      type: 'string',
+      description: 'Account to faucet',
+      demand: 'Please specify account to faucet',
+      coerce: (address) => {
+        if (!validateAccountAddress(address)) {
+          throw Error(`Receiver Address is invalid: "${address}"`)
+        }
+        return address
+      },
+    })
+    .option('dollars', {
+      type: 'number',
+      description: 'Number of dollars to faucet',
+      default: 0,
+    })
+    .option('gold', {
+      type: 'number',
+      description: 'Amount of gold to faucet',
+      default: 0,
+    })
 }
 
 export const handler = async (argv: FaucetArgv) => {
@@ -40,18 +54,27 @@ export const handler = async (argv: FaucetArgv) => {
     console.log(`Using account: ${account}`)
     kit.defaultAccount = account
 
-    const [goldToken, stableToken] = await Promise.all([
+    const [goldToken, stableToken, reserve] = await Promise.all([
       kit.contracts.getGoldToken(),
       kit.contracts.getStableToken(),
+      kit.contracts.getReserve(),
     ])
-    const goldAmount = (await convertToContractDecimals(1, goldToken)).toString()
-    const stableTokenAmount = (await convertToContractDecimals(10, stableToken)).toString()
-
+    const goldAmount = (await convertToContractDecimals(argv.gold, goldToken)).toString()
+    const stableTokenAmount = (await convertToContractDecimals(
+      argv.dollars,
+      stableToken
+    )).toString()
     console.log(`Fauceting ${goldAmount} Gold and ${stableTokenAmount} StableToken to ${address}`)
-    await Promise.all([
-      goldToken.transfer(address, goldAmount).sendAndWaitForReceipt(),
-      stableToken.transfer(address, stableTokenAmount).sendAndWaitForReceipt(),
-    ])
+    if (!new BigNumber(goldAmount).isZero()) {
+      if (await reserve.isSpender(account)) {
+        await reserve.transferGold(address, goldAmount).sendAndWaitForReceipt()
+      } else {
+        await goldToken.transfer(address, goldAmount).sendAndWaitForReceipt()
+      }
+    }
+    if (!new BigNumber(stableTokenAmount).isZero()) {
+      await stableToken.transfer(address, stableTokenAmount).sendAndWaitForReceipt()
+    }
   }
 
   try {
