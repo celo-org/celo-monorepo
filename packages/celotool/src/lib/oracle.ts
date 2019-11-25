cb319f6f24814eaf56ccf320960776656a7f37f1
Audrey Penven <audrey@celo.org>
2019-11-25 19:16:20 +0100

    hardcode a node's IP because forno is broken. fix some bits of the celotool deployment command for the oracle

--- packages/celotool/src/lib/oracle.ts
+++ packages/celotool/src/lib/oracle.ts
@@ -1,4 +1,3 @@
-import { getFornoUrl } from 'src/lib/endpoints'
 import { envVar, fetchEnv } from 'src/lib/env-utils'
 import { installGenericHelmChart } from 'src/lib/helm_deploy'
 
@@ -13,14 +12,15 @@ export async function installHelmChart(celoEnv: string) {
   )
 }
 
-function helmParameters(celoEnv: string) {
+function helmParameters(_celoEnv: string) {
   return [
     `--set celotool.image.repository=${fetchEnv(envVar.CELOTOOL_DOCKER_IMAGE_REPOSITORY)}`,
     `--set celotool.image.tag=${fetchEnv(envVar.CELOTOOL_DOCKER_IMAGE_TAG)}`,
     `--set mnemonic="${fetchEnv(envVar.MNEMONIC)}"`,
     `--set oracle.image.repository=${fetchEnv(envVar.ORACLE_DOCKER_IMAGE_REPOSITORY)}`,
     `--set oracle.image.tag=${fetchEnv(envVar.ORACLE_DOCKER_IMAGE_TAG)}`,
-    `--set celocli.nodeUrl=${getFornoUrl(celoEnv)}`,
+    // `--set celocli.nodeUrl=${getFornoUrl(celoEnv)}`,
+    `--set celocli.nodeUrl='http://34.83.46.9:8545'`,
     `--set celocli.image.repository=${fetchEnv(envVar.CELOCLI_STANDALONE_IMAGE_REPOSITORY)}`,
     `--set celocli.image.tag=${fetchEnv(envVar.CELOCLI_STANDALONE_IMAGE_TAG)}`,
   ]
