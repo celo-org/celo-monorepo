f99fd4e087032e3b23e9a8edf51de4947d52c22e
J M Rossy <jm.rossy@gmail.com>
2019-11-26 18:54:55 +0100

    Get app syncing with baklavastaging

--- packages/mobile/src/invite/saga.ts
+++ packages/mobile/src/invite/saga.ts
@@ -46,7 +46,7 @@ import { zeroSyncSelector } from 'src/web3/selectors'
 
 const TAG = 'invite/saga'
 export const TEMP_PW = 'ce10'
-export const REDEEM_INVITE_TIMEOUT = 1 * 60 * 1000 // 1 minute
+export const REDEEM_INVITE_TIMEOUT = 2 * 60 * 1000 // 2 minutes
 const INVITE_FEE = '0.2'
 
 export async function getInviteTxGas(
@@ -233,7 +233,7 @@ export function* doRedeemInvite(inviteCode: string) {
   yield call(waitWeb3LastBlock)
   try {
     const tempAccount = web3.eth.accounts.privateKeyToAccount(inviteCode).address
-    Logger.debug(`TAG@doRedeemInvite`, 'Invite code contains temp account', tempAccount)
+    Logger.debug(TAG + '@doRedeemInvite', 'Invite code contains temp account', tempAccount)
     const tempAccountBalanceWei: BigNumber = yield call(
       fetchTokenBalanceInWeiWithRetry,
       CURRENCY_ENUM.DOLLAR,
@@ -250,7 +250,7 @@ export function* doRedeemInvite(inviteCode: string) {
     yield put(fetchDollarBalance())
     return true
   } catch (e) {
-    Logger.error(TAG, 'Failed to redeem invite', e)
+    Logger.error(TAG + '@doRedeemInvite', 'Failed to redeem invite', e)
     if (e.message in ErrorMessages) {
       yield put(showError(e.message))
     } else {
--- packages/utils/src/attestations.ts
+++ packages/utils/src/attestations.ts
@@ -46,7 +46,9 @@ export function sanitizeMessageBase64(base64String: string) {
   return base64String.replace(/(¿|§)/gi, '_')
 }
 
-const attestationCodeRegex = new RegExp(/(.* |^)([a-zA-Z0-9=\+\/_-]{87,88})($| .*)/)
+const attestationCodeRegex = new RegExp(
+  /(.* |^)(?:celo:\/\/wallet\/v\/)?([a-zA-Z0-9=_-]{87,88})($| .*)/
+)
 
 export function messageContainsAttestationCode(message: string) {
   return attestationCodeRegex.test(message)
