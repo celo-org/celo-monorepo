727d4e633926fa0569f3dd9b4af1a41784253dd5
Jean Regisser <jean.regisser@gmail.com>
2019-11-26 17:32:41 +0100

    Fix incorrect value echoed for CELO_VALIDATOR_GROUP_ADDRESS

--- scripts/run-docker-validator-network.sh
+++ scripts/run-docker-validator-network.sh
@@ -129,7 +129,7 @@ if [[ $COMMAND == *"accounts"* ]]; then
     
 
     echo -e "\tCELO_VALIDATOR_ADDRESS=$CELO_VALIDATOR_ADDRESS"
-    echo -e "\tCELO_VALIDATOR_GROUP_ADDRESS=$CELO_VALIDATOR_ADDRESS"
+    echo -e "\tCELO_VALIDATOR_GROUP_ADDRESS=$CELO_VALIDATOR_GROUP_ADDRESS"
     echo -e "\tCELO_PROXY_ADDRESS=$CELO_PROXY_ADDRESS"
     
     export CELO_VALIDATOR_BLS_PUBLIC_KEY=$(docker run -v $PWD/validator:/root/.celo --entrypoint /bin/sh -it $CELO_IMAGE -c " printf '%s\n' $DEFAULT_PASSWORD $DEFAULT_PASSWORD | geth account proof-of-possession $CELO_VALIDATOR_ADDRESS $CELO_VALIDATOR_ADDRESS "| tail -1| cut -d' ' -f 5| tr -cd "[:alnum:]\n" )
