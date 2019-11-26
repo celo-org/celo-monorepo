f69c8c30fefbfb5756a8d71a0c2f445de9d0286a
Justin Foutts <justin.foutts@celo.org>
2019-11-25 17:06:28 -0800

    Use FixidityLib for computeTobinTax return

--- packages/protocol/contracts/stability/Reserve.sol
+++ packages/protocol/contracts/stability/Reserve.sol
@@ -28,7 +28,8 @@ contract Reserve is IReserve, Ownable, Initializable, UsingRegistry, ReentrancyG
   address[] private _tokens;
   TobinTaxCache public tobinTaxCache;
   uint256 public tobinTaxStalenessThreshold;
-  uint256 public constant TOBIN_TAX_DENOMINATOR = 1000;
+  uint256 public constant TOBIN_TAX_NUMERATOR = 5000000000000000000000; // 0.005
+  uint256 public constant TOBIN_TAX_DENOMINATOR = 1000000000000000000000000;
   mapping(address => bool) public isSpender;
 
   mapping(address => bool) public isOtherReserveAddress;
@@ -210,7 +211,7 @@ contract Reserve is IReserve, Ownable, Initializable, UsingRegistry, ReentrancyG
   function getOrComputeTobinTax() external nonReentrant returns (uint256, uint256) {
     // solhint-disable-next-line not-rely-on-time
     if (now.sub(tobinTaxCache.timestamp) > tobinTaxStalenessThreshold) {
-      tobinTaxCache.numerator = uint128(computeTobinTax());
+      tobinTaxCache.numerator = uint128(FixidityLib.unwrap(computeTobinTax()));
       tobinTaxCache.timestamp = uint128(now); // solhint-disable-line not-rely-on-time
     }
     return (uint256(tobinTaxCache.numerator), TOBIN_TAX_DENOMINATOR);
@@ -247,7 +248,7 @@ contract Reserve is IReserve, Ownable, Initializable, UsingRegistry, ReentrancyG
    * @notice Computes a tobin tax based on the reserve ratio.
    * @return The numerator of the tobin tax amount, where the denominator is 1000.
    */
-  function computeTobinTax() private view returns (uint256) {
+  function computeTobinTax() private view returns (FixidityLib.Fraction memory) {
     address sortedOraclesAddress = registry.getAddressForOrDie(SORTED_ORACLES_REGISTRY_ID);
     ISortedOracles sortedOracles = ISortedOracles(sortedOraclesAddress);
     uint256 reserveGoldBalance = getReserveGoldBalance();
@@ -266,9 +267,9 @@ contract Reserve is IReserve, Ownable, Initializable, UsingRegistry, ReentrancyG
     // The protocol aims to keep half of the reserve value in gold, thus the reserve ratio
     // is two when the value of gold in the reserve is equal to the total supply of stable tokens.
     if (reserveGoldBalance >= stableTokensValueInGold) {
-      return 0;
+      return FixidityLib.wrap(0);
     } else {
-      return 5;
+      return FixidityLib.wrap(TOBIN_TAX_NUMERATOR);
     }
   }
 
