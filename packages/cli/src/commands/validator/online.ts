fca5fde06a6405eb66ede8757e82d7a1707a2f91
Victor Graf <victor@celo.org>
2019-11-26 09:55:13 -0600

    create validator:online command to check if a validator is alive

---
+++ packages/cli/src/commands/validator/online.ts
@@ -0,0 +1,84 @@
+import { cli } from 'cli-ux'
+import { range } from 'lodash'
+import * as rlp from 'rlp'
+import { Block } from 'web3/eth/types'
+import { BaseCommand } from '../../base'
+import { newCheckBuilder } from '../../utils/checks'
+import { Flags } from '../../utils/command'
+
+export default class ValidatorOnline extends BaseCommand {
+  static description = 'Show information about whether the given signer is online and validating'
+
+  // How many blocks to look back for proposals of this signer.
+  static readonly lookback = 50
+
+  static flags = {
+    ...BaseCommand.flags,
+    signer: Flags.address({
+      required: true,
+      description: 'address of the signer to check if online and validating',
+    }),
+  }
+
+  static examples = ['online --signer 0x5409ED021D9299bf6814279A6A1411A7e866A631']
+
+  requireSynced = true
+
+  async run() {
+    const { flags } = this.parse(ValidatorOnline)
+
+    await newCheckBuilder(this, flags.signer)
+      .signerAccountIsValidator()
+      .runChecks()
+
+    cli.action.start('Querying current validator set')
+    const election = await this.kit.contracts.getElection()
+    const size = await election.numberValidatorsInCurrentSet()
+    const set = await Promise.all(range(size).map(election.validatorAddressFromCurrentSet))
+    cli.action.stop()
+
+    const signerIndex = set.map((a) => a.toLowerCase()).indexOf(flags.signer.toLowerCase())
+    if (signerIndex < 0) {
+      this.error(`Signer ${flags.signer} is not in the validator set for this epoch`)
+    }
+
+    cli.action.start('Searching for proposed blocks')
+    const latest = await this.web3.eth.getBlock('latest')
+    const blocks = await Promise.all(
+      range(1, ValidatorOnline.lookback).map((i) => this.web3.eth.getBlock(latest.number - i))
+    )
+    blocks.splice(0, 0, latest)
+    cli.action.stop()
+
+    for (const [i, block] of blocks.entries()) {
+      if (this.validatorIndexSignedParentHeader(block, signerIndex)) {
+        const parent =
+          i + 1 < blocks.length ? blocks[i + 1] : await this.web3.getBlock(block.number - 1)
+        console.info(
+          `Signer is online and signed block ${parent.number} ${Math.ceil(
+            Date.now() / 1000 - parent.timestamp
+          )} seconds ago`
+        )
+        return
+      }
+    }
+    this.error(
+      `Signer is part of the current validator set, but has not signed any of the last ${
+        ValidatorOnline.lookback
+      } blocks`
+    )
+  }
+
+  // Check the parent seal in the given block header for a bit indicating at the given index.
+  private validatorIndexSignedParentHeader(block: Block, index: number) {
+    // See https://github.com/celo-org/celo-blockchain/blob/master/core/types/istanbul.go
+    // tslint:disable:no-bitwise
+    // @ts-ignore
+    const parentSealBitmap = rlp.decode('0x' + block.extraData.slice(66))[5][0]
+    // @ts-ignore
+    return (
+      (parentSealBitmap[parentSealBitmap.length - 1 - Math.floor(index / 8)] & (1 << index % 8)) > 0
+    )
+    // tslint:enable:no-bitwise
+  }
+}
