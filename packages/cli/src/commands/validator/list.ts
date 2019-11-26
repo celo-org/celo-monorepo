7d62b6f6dc95f2c77ebc3158ae228d5ecf2a8862
Justin Foutts <justin.foutts@celo.org>
2019-11-26 13:46:57 -0800

    Fixes for #1880 and #1874

--- packages/cli/src/commands/validator/list.ts
+++ packages/cli/src/commands/validator/list.ts
@@ -1,3 +1,5 @@
+import { flags } from '@oclif/command'
+import { Accounts } from '@celo/contractkit/lib/wrapper'
 import { cli } from 'cli-ux'
 import { BaseCommand } from '../../base'
 
@@ -7,25 +9,43 @@ export default class ValidatorList extends BaseCommand {
 
   static flags = {
     ...BaseCommand.flags,
+    'no-truncate': flags.boolean({
+      description: "Don't truncate fields to fit line",
+      required: false,
+    }),
   }
 
   static examples = ['list']
 
   async run() {
-    this.parse(ValidatorList)
+    const res = this.parse(ValidatorList)
 
     cli.action.start('Fetching Validators')
+    const accounts = await this.kit.contracts.getAccounts()
     const validators = await this.kit.contracts.getValidators()
     const validatorList = await validators.getRegisteredValidators()
 
+    var names = []
+    for (let i = 0; i < validatorList.length; i++) {
+      names.push(accounts.getName(validatorList[i].address))
+    }
+    for (let i = 0; i < validatorList.length; i++) {
+      const name = await names[i]
+      validatorList[i].name = name ? name : ''
+    }
+
     cli.action.stop()
-    cli.table(validatorList, {
-      address: {},
-      name: {},
-      affiliation: {},
-      score: { get: (v) => v.score.toFixed() },
-      ecdsaPublicKey: {},
-      blsPublicKey: {},
-    })
+    cli.table(
+      validatorList,
+      {
+        address: {},
+        name: {},
+        affiliation: {},
+        score: { get: (v) => v.score.toFixed() },
+        ecdsaPublicKey: {},
+        blsPublicKey: {},
+      },
+      { 'no-truncate': res.flags['no-truncate'] }
+    )
   }
 }
