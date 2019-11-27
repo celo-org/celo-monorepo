b56a709baa7538fad275441f5a12756a835dfcb9
Aaron DeRuvo <aaron.deruvo@gmail.com>
2019-11-27 11:18:36 -0600

    Brand home page is a ok

--- packages/web/src/brandkit/Intro.tsx
+++ packages/web/src/brandkit/Intro.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native'
 import { brandStyles } from 'src/brandkit/common/constants'
 import Page from 'src/brandkit/common/Page'
 import { H1, H4 } from 'src/fonts/Fonts'
-import { I18nProps, Trans, withNamespaces, NameSpaces } from 'src/i18n'
+import { I18nProps, NameSpaces, Trans, withNamespaces } from 'src/i18n'
 import InlineAnchor from 'src/shared/InlineAnchor'
 import menuItems from 'src/shared/menu-items'
 import { fonts, standardStyles } from 'src/styles'
@@ -24,9 +24,9 @@ export default React.memo(function Intro() {
 const Overview = withNamespaces(NameSpaces.brand)(function _Overview({ t }: I18nProps) {
   return (
     <View style={brandStyles.gap}>
-      <H1>{t('home.title')}</H1>
-      <H4 style={standardStyles.blockMarginBottomTablet}>{t('home.introduction')}</H4>
-      <Text style={fonts.h5}>{t('home.useageTitle')}</Text>
+      <H1 style={standardStyles.elementalMarginBottom}>{t('home.title')}</H1>
+      <H4 style={standardStyles.blockMarginBottom}>{t('home.introduction')}</H4>
+      <Text style={[fonts.h5, standardStyles.elementalMarginBottom]}>{t('home.useageTitle')}</Text>
       <Text style={fonts.p}>
         <Trans i18nKey="home.useageText">
           <InlineAnchor href={menuItems.CODE_OF_CONDUCT.link}>Code Of Conduct</InlineAnchor>
