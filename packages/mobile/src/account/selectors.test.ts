0d1232f4c8b112b090b6b9e03671bc9ba1556027
Ivan Sorokin <ivan@sorokin.io>
2019-11-27 16:03:54 +0100

    test for outgoing payment requests

--- packages/mobile/src/account/selectors.test.ts
+++ packages/mobile/src/account/selectors.test.ts
@@ -1,4 +1,4 @@
-import { getIncomingPaymentRequests } from 'src/account/selectors'
+import { getIncomingPaymentRequests, getOutgoingPaymentRequests } from 'src/account/selectors'
 import { PaymentRequestStatus } from 'src/account/types'
 import { paymentRequestDouble } from 'src/paymentRequest/__mocks__'
 
@@ -31,6 +31,37 @@ describe('selectors', () => {
       }
 
       expect(getIncomingPaymentRequests(state)).toEqual([goodRequest])
+    })
+  })
+
+  describe(getOutgoingPaymentRequests, () => {
+    it('excludes declined and completed requests', () => {
+      const state: any = {
+        account: {
+          outgoingPaymentRequests: [
+            paymentRequestDouble({ status: PaymentRequestStatus.DECLINED }),
+            paymentRequestDouble({ status: PaymentRequestStatus.COMPLETED }),
+          ],
+        },
+      }
+      expect(getOutgoingPaymentRequests(state)).toEqual([])
+    })
+
+    it('returns requested payments', () => {
+      const goodRequest = paymentRequestDouble({ status: PaymentRequestStatus.REQUESTED })
+
+      const state: any = {
+        account: {
+          outgoingPaymentRequests: [
+            paymentRequestDouble({
+              status: PaymentRequestStatus.COMPLETED,
+            }),
+            goodRequest,
+          ],
+        },
+      }
+
+      expect(getOutgoingPaymentRequests(state)).toEqual([goodRequest])
     })
   })
 })
