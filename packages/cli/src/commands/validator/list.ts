5a4b888d3870ce96ce26d2727b61900176b38021
Connor McEwen <connor.mcewen@gmail.com>
2019-12-02 21:58:26 -0800

    Add validator table to cli commands

--- packages/cli/src/commands/validator/list.ts
+++ packages/cli/src/commands/validator/list.ts
@@ -1,6 +1,16 @@
+import { Validator } from '@celo/contractkit/src/wrappers/Validators'
 import { flags } from '@oclif/command'
 import { cli } from 'cli-ux'
 import { BaseCommand } from '../../base'
+
+export const validatorTable = {
+  address: {},
+  name: {},
+  affiliation: {},
+  score: { get: (v: Validator) => v.score.toFixed() },
+  ecdsaPublicKey: {},
+  blsPublicKey: {},
+}
 
 export default class ValidatorList extends BaseCommand {
   static description =
@@ -24,17 +34,6 @@ export default class ValidatorList extends BaseCommand {
     const validatorList = await validators.getRegisteredValidators()
 
     cli.action.stop()
-    cli.table(
-      validatorList,
-      {
-        address: {},
-        name: {},
-        affiliation: {},
-        score: { get: (v) => v.score.toFixed() },
-        ecdsaPublicKey: {},
-        blsPublicKey: {},
-      },
-      { 'no-truncate': res.flags['no-truncate'] }
-    )
+    cli.table(validatorList, validatorTable, { 'no-truncate': res.flags['no-truncate'] })
   }
 }
