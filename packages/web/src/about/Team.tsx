35248298a6f85a447af2c9aac22c0dbdd4ab5592
Aaron DeRuvo <aaron.deruvo@gmail.com>
2019-12-06 14:32:32 -0800

    Updated front end for fetching contributors

--- packages/web/src/about/Team.tsx
+++ packages/web/src/about/Team.tsx
@@ -2,16 +2,18 @@ import * as React from 'react'
 import LazyLoadFadin from 'react-lazyload-fadein'
 import { Image, ImageURISource, StyleSheet, Text, View } from 'react-native'
 import shuffleSeed from 'shuffle-seed'
-import teamList, { Person } from 'src/about/team/team-list'
-import { H1, H4 } from 'src/fonts/Fonts'
+import { H1 } from 'src/fonts/Fonts'
 import { I18nProps, withNamespaces } from 'src/i18n'
 import BookLayout from 'src/layout/BookLayout'
 import { Cell, GridRow, Spans } from 'src/layout/GridRow'
 import { ScreenProps, ScreenSizes, withScreenSize } from 'src/layout/ScreenSize'
 import AspectRatio from 'src/shared/AspectRatio'
 import Responsive from 'src/shared/Responsive'
-import { colors, fonts, standardStyles, textStyles } from 'src/styles'
-
+import { fonts, standardStyles, textStyles, colors } from 'src/styles'
+import Fetch from 'src/brandkit/common/Fetch'
+import Spinner from 'src/shared/Spinner'
+import { Contributor } from 'src/about/Contributor'
+import External from 'src/icons/External'
 interface Props {
   randomSeed: number
 }
@@ -19,14 +21,21 @@ interface Props {
 export class Team extends React.Component<Props & I18nProps & ScreenProps> {
   render() {
     const { t, randomSeed, screen } = this.props
-    const shuffledTeamList = shuffleSeed.shuffle(teamList, randomSeed)
     const isTablet = screen === ScreenSizes.TABLET
     const isMobile = screen === ScreenSizes.MOBILE
     return (
       <>
         <BookLayout label={t('teamTitle')} startBlock={true}>
           <H1>{t('teamAlternateTitle')}</H1>
-          <Text style={[fonts.p, standardStyles.elementalMargin]}>{t('teamCopy')} </Text>
+          <Text
+            style={[
+              fonts.p,
+              standardStyles.elementalMarginTop,
+              standardStyles.sectionMarginBottomMobile,
+            ]}
+          >
+            {t('teamCopy')}{' '}
+          </Text>
         </BookLayout>
         <GridRow>
           <Cell span={Spans.full} tabletSpan={Spans.full}>
@@ -36,21 +45,34 @@ export class Team extends React.Component<Props & I18nProps & ScreenProps> {
                 isMobile ? styles.photoListAuxMobile : isTablet && styles.photoListAuxTablet,
               ]}
             >
-              {shuffledTeamList.map((person: Person) => (
-                <React.Fragment key={person.name}>
-                  <LazyLoadFadin>
-                    {(onLoad: () => void) => (
-                      <Portrait
-                        name={person.name}
-                        team={person.role}
-                        purpose="Methodically Manages Mischief"
-                        source={{ uri: person.photo }}
-                        onLoad={onLoad}
-                      />
-                    )}
-                  </LazyLoadFadin>
-                </React.Fragment>
-              ))}
+              <Fetch query="/api/contributors">
+                {({ data, loading, error }) => {
+                  if (loading) {
+                    return <Spinner size="medium" color={colors.dark} />
+                  }
+                  if (error) {
+                    return null
+                  }
+                  const shuffledTeamList = shuffleSeed.shuffle(data, randomSeed)
+                  return shuffledTeamList.map((person: Contributor) => (
+                    <React.Fragment key={person.name}>
+                      <LazyLoadFadin>
+                        {(onLoad: () => void) => (
+                          <Portrait
+                            name={person.name}
+                            url={person.url}
+                            team={person.team}
+                            company={person.company}
+                            purpose={person.purpose}
+                            source={{ uri: person.photo }}
+                            onLoad={onLoad}
+                          />
+                        )}
+                      </LazyLoadFadin>
+                    </React.Fragment>
+                  ))
+                }}
+              </Fetch>
             </View>
           </Cell>
         </GridRow>
@@ -63,7 +85,9 @@ interface PortraitProps {
   source: ImageURISource
   name: string
   purpose: string
-  team: string
+  team?: string
+  company: string
+  url?: string
   onLoad?: () => void
 }
 
@@ -72,7 +96,9 @@ const Portrait = React.memo(function _Portrait({
   onLoad,
   name,
   team,
+  company,
   purpose,
+  url,
 }: PortraitProps) {
   return (
     <>
@@ -91,8 +117,18 @@ const Portrait = React.memo(function _Portrait({
           >
             {purpose}
           </Text>
-          <Text style={[fonts.p, textStyles.heavy, standardStyles.elementalMarginTop]}>{name}</Text>
-          <Text style={[fonts.p]}>{team}</Text>
+          <View style={[standardStyles.row, standardStyles.elementalMarginTop]}>
+            <Text style={[fonts.p, textStyles.heavy, styles.name]}>{name}</Text>
+            {url && (
+              <a href={url}>
+                <External size={12} color={colors.dark} />
+              </a>
+            )}
+          </View>
+
+          <Text style={[fonts.p]}>
+            {company} – {team}
+          </Text>
         </View>
       </Responsive>
     </>
@@ -103,32 +139,40 @@ const PHOTO_BACKGROUND = '#E5E2DD'
 
 // @ts-ignore
 const styles = StyleSheet.create({
-  purposeText: { fontSize: 24, lineHeight: 28 },
+  purposeText: { fontSize: 26, lineHeight: 28 },
   container: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'flex-start',
     alignItems: 'center',
   },
-  photoListAuxMobile: { justifyContent: 'center' },
+  photoListAuxMobile: {
+    justifyContent: 'flex-start',
+    minHeight: '80vh',
+  },
   photoListAuxTablet: { justifyContent: 'space-around' },
   imageBackground: { backgroundColor: PHOTO_BACKGROUND, marginBottom: 20 },
   photo: {
     height: '100%',
     width: '100%',
   },
+  name: {
+    marginRight: 5,
+  },
   photoList: {
     flexDirection: 'row',
     flexWrap: 'wrap',
     alignItems: 'center',
     justifyContent: 'space-between',
+    minHeight: '50vh',
   },
   person: {
     flexDirection: 'column',
     margin: 5,
     marginBottom: 30,
-    flex: 1,
-    maxWidth: '100%',
+    width: '80vw',
+    minWidth: 200,
+    maxWidth: 300,
   },
   mediumPerson: {
     flexDirection: 'column',
