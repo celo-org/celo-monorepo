445ae2f282a9aa9aa381a95fdda75ecc66813c7f
Aaron DeRuvo <aaron.deruvo@gmail.com>
2019-12-09 10:58:22 -0800

    Remove unused files

--- packages/web/src/about/images/index.ts
+++ packages/web/src/about/images/index.ts
@@ -1,8 +1,7 @@
 import BeautifulMoneyPreview from './BeautifulMoneyPreview.jpg'
-import mintPlaza from './mint-plaza@2x.png'
 import teamHero from './team-hero@2x.png'
 
 import sacredEconBack from './sacredEconBack.jpg'
 import team from './team.jpg'
 
-export { BeautifulMoneyPreview, mintPlaza, sacredEconBack, team, teamHero }
+export { BeautifulMoneyPreview, sacredEconBack, team, teamHero }
