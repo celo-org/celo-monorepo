cd055fa1379dfd46d176ca8bec88b2373e1f8fe1
Ivan Sorokin <ivan@sorokin.io>
2019-12-10 12:01:52 +0100

    fix tests

---
+++ packages/mobile/src/notifications/SummaryNotification.test.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { Text, View } from 'react-native'
+import * as renderer from 'react-test-renderer'
+import SummaryNotification from 'src/notifications/SummaryNotification'
+
+const props = () => ({
+  image: require('src/images/placeholder.png'),
+  icon: <View />,
+  title: 'Test',
+  items: ['a'],
+  itemRenderer: (item: string, key: number) => {
+    return <Text>{`test-${item}`}</Text>
+  },
+  onReview: jest.fn(),
+})
+
+describe(SummaryNotification, () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<SummaryNotification<string> {...props()} />)
+    expect(tree).toMatchSnapshot()
+  })
+})
