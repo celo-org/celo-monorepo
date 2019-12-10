31608ee3168fb106ac6f8ba6b57c78c6d6ed818a
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2019-12-10 16:53:43 +0100

    migrate sequencer to javascript

--- packages/contractkit/src/test-utils/AlphabeticSequencer.js
+++ packages/contractkit/src/test-utils/AlphabeticSequencer.js
@@ -1,13 +1,9 @@
 const Sequencer = require('@jest/test-sequencer').default
 
 class AlphabeticSequencer extends Sequencer {
-  // @ts-ignore
   sort(tests) {
     const copyTests = Array.from(tests)
-    return copyTests.sort(
-      // @ts-ignore
-      (testA, testB) => (testA.path > testB.path ? 1 : -1)
-    )
+    return copyTests.sort((testA, testB) => (testA.path > testB.path ? 1 : -1))
   }
 }
 
