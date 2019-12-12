67d9a6a9e1f0b25389dda0547db13d6988c16063
Aaron DeRuvo <aaron.deruvo@gmail.com>
2019-12-12 13:52:03 -0800

    This test should run

---
+++ packages/web/src/brandkit/tests/Showcase.test.tsx
@@ -0,0 +1,19 @@
+import * as renderer from 'react-test-renderer'
+import Showcase from 'src/brandkit/common/Showcase'
+
+describe(Showcase, () => {
+  it('renders', () => {
+    const showCase = renderer
+      .create(
+        <Showcase
+          name={'test'}
+          description="Trust, Attest, Verify"
+          loading={false}
+          size={100}
+          uri="example.com"
+        />
+      )
+      .toJSON()
+    expect(showCase).toMatchSnapshot()
+  })
+})
