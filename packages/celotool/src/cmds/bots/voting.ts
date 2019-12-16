c9f21761fe2873e0f36d2f9d0cb84d5418bfbb64
Audrey Penven <audrey@celo.org>
2019-12-16 15:50:52 +0000

    calculate the average score for validator groups

--- packages/celotool/src/cmds/bots/voting.ts
+++ packages/celotool/src/cmds/bots/voting.ts
@@ -2,6 +2,7 @@
 // behavior by validators. This introduces some non-validator stakers into the
 // network that will judge the validator groups, and vote accordingly.
 import { ContractKit, newKit } from '@celo/contractkit'
+import BigNumber from 'bignumber.js'
 import { BotsArgv } from 'src/cmds/bots'
 import { envVar, fetchEnv } from 'src/lib/env-utils'
 import { AccountType, getPrivateKeysFor, privateKeyToAddress } from 'src/lib/generate_utils'
@@ -25,6 +26,7 @@ export const builder = (yargs: Argv) => {
 }
 
 export const handler = async function simulateVoting(argv: SimulateVotingArgv) {
+  console.info('starting voting command')
   try {
     // Set up contract kit instance and instances of all the contracts we'll need
     // to interact with
@@ -38,12 +40,28 @@ export const handler = async function simulateVoting(argv: SimulateVotingArgv) {
     // Maybe this should even be variable, based on how "happy" the staker is with their current vote
     const changeVoteProbability = 1
 
-    // Get the state of the world...
-    //  - what validator groups exist?
-    //  - what are the most recent epoch rewards for each group?
-    //  - group uptimes?
-    //  - what validator groups can accept votes? (this may change as these accounts cast votes)
-    //  - anything else?
+    // sort the validators into groups
+    const validatorGroupScores = new Map<string, BigNumber[]>()
+    for (const valSigner of await election.electValidatorSigners()) {
+      const val = await validators.getValidatorFromSigner(valSigner)
+      const groupAddress = val.affiliation!
+      validatorGroupScores.set(
+        groupAddress,
+        (validatorGroupScores.get(groupAddress) || []).concat(val.score)
+      )
+    }
+
+    const groupAvgScores = new Map<string, BigNumber>()
+    for (const group of validatorGroupScores.keys()) {
+      const scores = validatorGroupScores.get(group)!
+      const avg = scores.reduce((a, b) => a.plus(b), new BigNumber(0)).div(scores.length)
+      groupAvgScores.set(group, avg)
+    }
+
+    const sortedGroups = [...groupAvgScores.keys()].sort((a, b) => {
+      return groupAvgScores.get(b)!.comparedTo(groupAvgScores.get(a)!)
+    })
+    console.info(sortedGroups)
 
     // Get keys + accounts for the voting bot accounts (based on mnemonic, envvar count of bot accounts)
     const votingBotKeys: string[] = getPrivateKeysFor(
@@ -58,13 +76,14 @@ export const handler = async function simulateVoting(argv: SimulateVotingArgv) {
         kit.addAccount(key)
         const account = privateKeyToAddress(key)
         console.info(`handling vote for ${account}`)
-        const groupVotes = await election.getValidatorGroupsVotes()
-        console.info(groupVotes)
+        // const groupVotes = await election.getValidatorGroupsVotes()
+        // console.info(groupVotes)
       }
       //    select new group, based on weighted probability of the "state of the world"
       //    cast new vote
     }
   } catch (error) {
+    console.error(error)
     // log this in a reasonable way
   }
 }
