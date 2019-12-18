a53a2e13bd354436cb503ff5fba30c368b6782b5
Aaron DeRuvo <aaron.deruvo@gmail.com>
2019-12-18 14:36:41 -0800

    Clean up

--- packages/web/__mocks__/react-modal.tsx
+++ packages/web/__mocks__/react-modal.tsx
@@ -1,19 +1,21 @@
 import * as React from 'react'
 
-interface IProps {
+interface Props {
   id: string
   isOpen: boolean
   contentLabel: string
   overlayClassName: string
   className: any
+  parentSelector: any
   shouldCloseOnOverlayClick(): void
   onRequestClose(): void
-  parentSelector: any
   onAfterOpen(): void
 }
 
-export default class MockReactModal extends React.Component<IProps, any> {
-  static setAppElement() {}
+export default class MockReactModal extends React.Component<Props, any> {
+  static setAppElement() {
+    return null
+  }
 
   render() {
     return <div id={this.props.id}>{this.props.isOpen && this.props.children}</div>
