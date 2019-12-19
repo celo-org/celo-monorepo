60bdde7c78995b18c831e9216bc2088bc661bfd7
Justin Foutts <justin.foutts@celo.org>
2019-12-19 14:11:09 -0800

    Add reconstructHeaderArray

--- packages/celotool/src/e2e-tests/slashing_tests.ts
+++ packages/celotool/src/e2e-tests/slashing_tests.ts
@@ -3,6 +3,8 @@
 
 import { ContractKit, newKit } from '@celo/contractkit'
 import { assert } from 'chai'
+import * as rlp from 'rlp'
+import Web3 from 'web3'
 import { getHooks, GethTestConfig, sleep } from './utils'
 
 describe('Slashing tests', function(this: any) {
@@ -36,13 +38,15 @@ describe('Slashing tests', function(this: any) {
     before(async () => {
       await restartGeth()
     })
+
     it('block limit should have been set using governance', async () => {
       this.timeout(0)
       const current = await kit.web3.eth.getBlockNumber()
       const block = await kit.web3.eth.getBlock(current)
       assert.equal(block.gasLimit, 20000000)
     })
-    it('hashHeader should be correct', async () => {
+
+    it('should hash test header correctly', async () => {
       const contract = await kit._web3Contracts.getElection()
       const header = kit.web3.utils.hexToBytes(
         '0xf901f9a07285abd5b24742f184ad676e31f6054663b3529bc35ea2fcad8a3e0f642a46f7a01dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347948888f1f195afa192cfee860698584c030f4c9db1a0ecc60e00b3fe5ce9f6e1a10e5469764daf51f1fe93c22ec3f9a7583a80357217a0d35d334d87c0cc0a202e3756bf81fae08b1575f286c7ee7a3f8df4f0f3afc55da056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421b90100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008302000001832fefd8825208845c47775c80a00000000000000000000000000000000000000000000000000000000000000000880000000000000000'
@@ -50,7 +54,19 @@ describe('Slashing tests', function(this: any) {
       const hash = await contract.methods.hashHeader(header).call()
       assert.equal(hash, '0xf5a450266c77dce47f7698959d8e7019db860ee19a5322b16a853fdf23607100')
     })
-    it('getBlockNumberFromHeader should be correct', async () => {
+
+    it('should hash current header correctly', async () => {
+      const current = await kit.web3.eth.getBlockNumber()
+      const block = await kit.web3.eth.getBlock(current)
+      const contract = await kit._web3Contracts.getElection()
+      const hash = await contract.methods
+        .hashHeader(rlp.encode(reconstructHeaderArray(kit.web3, block)))
+        .call()
+      console.info('Current block hash is: ' + hash)
+      assert.equal(hash, block.hash)
+    })
+
+    it('it should parse blockNumber from test header', async () => {
       const contract = await kit._web3Contracts.getElection()
       const header = kit.web3.utils.hexToBytes(
         '0xf901f9a07285abd5b24742f184ad676e31f6054663b3529bc35ea2fcad8a3e0f642a46f7a01dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347948888f1f195afa192cfee860698584c030f4c9db1a0ecc60e00b3fe5ce9f6e1a10e5469764daf51f1fe93c22ec3f9a7583a80357217a0d35d334d87c0cc0a202e3756bf81fae08b1575f286c7ee7a3f8df4f0f3afc55da056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421b90100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008302000001832fefd8825208845c47775c80a00000000000000000000000000000000000000000000000000000000000000000880000000000000000'
@@ -60,3 +76,23 @@ describe('Slashing tests', function(this: any) {
     })
   })
 })
+
+function reconstructHeaderArray(web3: Web3, block: any) {
+  return [
+    block.parentHash,
+    block.sha3Uncles,
+    block.miner,
+    block.stateRoot,
+    block.transactionsRoot,
+    block.receiptsRoot,
+    block.logsBloom,
+    web3.utils.toHex(block.difficulty),
+    block.number,
+    block.gasLimit,
+    block.gasUsed,
+    block.timestamp,
+    block.extraData,
+    block.mixHash,
+    block.nonce,
+  ]
+}
