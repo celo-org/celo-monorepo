8c934dada906d975fa0a746d6fbe71a0f96a061e
Justin Foutts <justin.foutts@celo.org>
2019-12-19 14:42:48 -0800

    Make a header to double sign

--- packages/celotool/src/e2e-tests/slashing_tests.ts
+++ packages/celotool/src/e2e-tests/slashing_tests.ts
@@ -39,11 +39,14 @@ describe('Slashing tests', function(this: any) {
       await restartGeth()
     })
 
-    it('block limit should have been set using governance', async () => {
+    it('should parse blockNumber from test header', async () => {
       this.timeout(0)
-      const current = await kit.web3.eth.getBlockNumber()
-      const block = await kit.web3.eth.getBlock(current)
-      assert.equal(block.gasLimit, 20000000)
+      const contract = await kit._web3Contracts.getElection()
+      const header = kit.web3.utils.hexToBytes(
+        '0xf901f9a07285abd5b24742f184ad676e31f6054663b3529bc35ea2fcad8a3e0f642a46f7a01dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347948888f1f195afa192cfee860698584c030f4c9db1a0ecc60e00b3fe5ce9f6e1a10e5469764daf51f1fe93c22ec3f9a7583a80357217a0d35d334d87c0cc0a202e3756bf81fae08b1575f286c7ee7a3f8df4f0f3afc55da056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421b90100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008302000001832fefd8825208845c47775c80a00000000000000000000000000000000000000000000000000000000000000000880000000000000000'
+      )
+      const blockNumber = await contract.methods.getBlockNumberFromHeader(header).call()
+      assert.equal(blockNumber, 1)
     })
 
     it('should hash test header correctly', async () => {
@@ -56,23 +59,30 @@ describe('Slashing tests', function(this: any) {
     })
 
     it('should hash current header correctly', async () => {
+      const contract = await kit._web3Contracts.getElection()
       const current = await kit.web3.eth.getBlockNumber()
       const block = await kit.web3.eth.getBlock(current)
-      const contract = await kit._web3Contracts.getElection()
-      const hash = await contract.methods
-        .hashHeader(rlp.encode(reconstructHeaderArray(kit.web3, block)))
-        .call()
-      console.info('Current block hash is: ' + hash)
-      assert.equal(hash, block.hash)
+      const rlpEncodedBlock = rlp.encode(reconstructHeaderArray(kit.web3, block))
+      const blockHash = await contract.methods.hashHeader(rlpEncodedBlock).call()
+      assert.equal(blockHash, block.hash)
     })
 
-    it('it should parse blockNumber from test header', async () => {
+    it('slashing four double signing', async () => {
       const contract = await kit._web3Contracts.getElection()
-      const header = kit.web3.utils.hexToBytes(
-        '0xf901f9a07285abd5b24742f184ad676e31f6054663b3529bc35ea2fcad8a3e0f642a46f7a01dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347948888f1f195afa192cfee860698584c030f4c9db1a0ecc60e00b3fe5ce9f6e1a10e5469764daf51f1fe93c22ec3f9a7583a80357217a0d35d334d87c0cc0a202e3756bf81fae08b1575f286c7ee7a3f8df4f0f3afc55da056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421b90100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008302000001832fefd8825208845c47775c80a00000000000000000000000000000000000000000000000000000000000000000880000000000000000'
+      const current = await kit.web3.eth.getBlockNumber()
+      const block = await kit.web3.eth.getBlock(current)
+      const doubleSignedBlock = await kit.web3.eth.getBlock(current)
+      doubleSignedBlock.timestamp++
+      const rlpEncodedBlock = rlp.encode(reconstructHeaderArray(kit.web3, block))
+      const rlpEncodedDoubleSignedBlock = rlp.encode(
+        reconstructHeaderArray(kit.web3, doubleSignedBlock)
       )
-      const blockNumber = await contract.methods.getBlockNumberFromHeader(header).call()
-      assert.equal(blockNumber, 1)
+      const blockHash = await contract.methods.hashHeader(rlpEncodedBlock).call()
+      const doubleSignedBlockHash = await contract.methods
+        .hashHeader(rlpEncodedDoubleSignedBlock)
+        .call()
+      console.info('Canonical block hash: ' + blockHash)
+      console.info('Double signed block hash: ' + doubleSignedBlockHash)
     })
   })
 })
