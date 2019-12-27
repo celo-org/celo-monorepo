64e7afaa5bd43bd9ba02297b135c40fbe636905d
Justin Foutts <justin.foutts@celo.org>
2019-12-26 16:14:36 -0800

    Update expected gas cost in transfer tests

--- packages/celotool/src/e2e-tests/transfer_tests.ts
+++ packages/celotool/src/e2e-tests/transfer_tests.ts
@@ -584,7 +584,7 @@ describe('Transfer tests', function(this: any) {
         })
 
         describe('Transfer CeloDollars', () => {
-          const evmGasCost = 20303
+          const evmGasCost = 20325
           describe('feeCurrency = CeloDollars >', () => {
             testTransferToken({
               expectedGas: evmGasCost + INTRINSIC_TX_GAS_COST + ADDITIONAL_INTRINSIC_TX_GAS_COST,
