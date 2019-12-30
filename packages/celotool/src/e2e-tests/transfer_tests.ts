2bd727303f614c4fb86fe38c3101536163ca0432
Sami Mäkelä <mrsmkl@gmail.com>
2019-12-30 16:55:31 +0200

    why does this change?

--- packages/celotool/src/e2e-tests/transfer_tests.ts
+++ packages/celotool/src/e2e-tests/transfer_tests.ts
@@ -584,7 +584,7 @@ describe('Transfer tests', function(this: any) {
         })
 
         describe('Transfer CeloDollars', () => {
-          const evmGasCost = 20325
+          const evmGasCost = 20303
           describe('feeCurrency = CeloDollars >', () => {
             testTransferToken({
               expectedGas: evmGasCost + INTRINSIC_TX_GAS_COST + ADDITIONAL_INTRINSIC_TX_GAS_COST,
@@ -660,7 +660,8 @@ describe('Transfer tests', function(this: any) {
         describe('Transfer CeloDollars', () => {
           describe('feeCurrency = CeloDollars >', () => {
             testTransferToken({
-              expectedGas: 75325,
+              expectedGas:
+                20303 + changedIntrinsicGasForAlternativeFeeCurrency + INTRINSIC_TX_GAS_COST,
               transferToken: CeloContract.StableToken,
               feeToken: CeloContract.StableToken,
             })
