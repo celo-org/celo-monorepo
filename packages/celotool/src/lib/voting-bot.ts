9e279d32640f14d2e3fc718759632459e0ba229b
Audrey Penven <audrey@celo.org>
2020-01-02 18:18:28 +0100

    bring envvars into the helm chart

--- packages/celotool/src/lib/voting-bot.ts
+++ packages/celotool/src/lib/voting-bot.ts
@@ -10,10 +10,14 @@ export async function installHelmChart(celoEnv: string) {
 
 function helmParameters(celoEnv: string) {
   return [
+    `--set cronSchedule="${fetchEnv(envVar.VOTING_BOT_CRON_SCHEDULE)}"`,
     `--set domain.name=${fetchEnv(envVar.CLUSTER_DOMAIN_NAME)}`,
     `--set environment=${celoEnv}`,
+    `--set imageRepository=${fetchEnv(envVar.CELOTOOL_DOCKER_IMAGE_REPOSITORY)}`,
+    `--set imageTag=${fetchEnv(envVar.CELOTOOL_DOCKER_IMAGE_TAG)}`,
     `--set mnemonic="${fetchEnv(envVar.MNEMONIC)}"`,
     `--set votingBotCount=${fetchEnv(envVar.VOTING_BOTS)}`,
+    `--set voteChangeProbability="${fetchEnv(envVar.VOTING_BOT_CHANGE_PROBABILITY)}"`,
   ]
 }
 
