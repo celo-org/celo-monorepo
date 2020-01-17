7f240e41da7806bc15c6b0fdbd3e0fe77be24a8b
yourcodesucks <chortik@gmail.com>
2020-01-17 00:34:59 -0500

    - date format - metadata

--- packages/cli/src/commands/network/head.ts
+++ packages/cli/src/commands/network/head.ts
@@ -12,11 +12,11 @@ export default class Head extends BaseCommand {
     const block = await this.web3.eth.getBlock('latest')
     console.log(block.miner)
     let interesting: { [key: string]: string | number | boolean } = {}
-    interesting['timestamp'] = new Date(0).setUTCSeconds(block.timestamp).toLocaleString()
+    interesting['timestamp'] = new Date(new Date(0).setUTCSeconds(block.timestamp)).toUTCString()
     interesting['block number'] = block.number
     interesting['epoch index'] = Math.floor(block.number / 720) // TODO: epoch block duration should come from network:parameters
     interesting['epoch position'] = Math.floor(block.number % 720) // TODO: epoch block duration should come from network:parameters
-    interesting['block validator'] = block.miner
+    interesting['block signer'] = block.miner
 
     printValueMap(interesting)
   }
