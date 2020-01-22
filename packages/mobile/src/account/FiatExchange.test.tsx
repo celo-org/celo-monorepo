230958e97dbc4c68924595a8b469c990c10c6c57
annakaz <annakaz@users.noreply.github.com>
2020-01-22 09:12:14 -0800

    Update fiat exchange test

--- packages/mobile/src/account/FiatExchange.test.tsx
+++ packages/mobile/src/account/FiatExchange.test.tsx
@@ -2,10 +2,10 @@ import * as React from 'react'
 import 'react-native'
 import { Provider } from 'react-redux'
 import * as renderer from 'react-test-renderer'
-import FiatExchange from 'src/account/GoldEducation'
+import FiatExchange from 'src/account/FiatExchange'
 import { createMockStore } from 'test/utils'
 
-describe('GoldEducation', () => {
+describe('FiatExchange', () => {
   it('renders correctly', () => {
     const tree = renderer.create(
       <Provider store={createMockStore({})}>
