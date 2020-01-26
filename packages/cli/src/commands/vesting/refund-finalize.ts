7701ad6c1f9a991a73378352aa79ec540ab28442
evgenipirianov <ep@blocksize-capital.com>
2020-01-26 20:03:26 +0100

    added new cli command

---
+++ packages/cli/src/commands/vesting/refund-finalize.ts
@@ -0,0 +1,46 @@
+import { NULL_ADDRESS } from '@celo/contractkit'
+import { BaseCommand } from '../../base'
+import { newCheckBuilder } from '../../utils/checks'
+import { displaySendTx } from '../../utils/cli'
+import { Flags } from '../../utils/command'
+
+export default class RefundFinalize extends BaseCommand {
+  static description = 'Refund revoker and beneficiary after the vesting has been revoked'
+
+  static flags = {
+    ...BaseCommand.flags,
+    from: Flags.address({ required: true, description: 'Beneficiary of the vesting' }),
+    revoker: Flags.address({ required: true, description: 'Revoker of the vesting' }),
+  }
+
+  static args = []
+
+  static examples = [
+    'refund-finalize --from 0x5409ED021D9299bf6814279A6A1411A7e866A631 --revoker 0x47e172F6CfB6c7D01C1574fa3E2Be7CC73269D95',
+  ]
+
+  async run() {
+    const res = this.parse(RefundFinalize)
+    this.kit.defaultAccount = res.flags.revoker
+    const vestingFactory = await this.kit.contracts.getVestingFactory()
+    const vestingInstance = await vestingFactory.getVestedAt(res.flags.from)
+
+    await newCheckBuilder(this)
+      .addCheck(
+        `No vesting instance found under the given beneficiary ${res.flags.from}`,
+        () => vestingInstance.address !== NULL_ADDRESS
+      )
+      .addCheck(
+        `Vesting instance has a different revoker`,
+        async () => (await vestingInstance.getRevoker()) === res.flags.revoker
+      )
+      .addCheck(
+        `Vesting instance is not revoked yet`,
+        async () => (await vestingInstance.isRevoked()) === true
+      )
+      .runChecks()
+
+    const tx = await vestingInstance.refundAndFinalize()
+    await displaySendTx('refundAndFinalizeTx', tx, { from: await vestingInstance.getRevoker() })
+  }
+}
