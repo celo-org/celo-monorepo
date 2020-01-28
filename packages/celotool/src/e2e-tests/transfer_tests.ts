ae296f7a736ebd015b2c35fc0b9a840bccf4e34b
Mariano Cortesi <mcortesi@gmail.com>
2020-01-28 15:55:42 -0300

    Fix: Use lightest

--- packages/celotool/src/e2e-tests/transfer_tests.ts
+++ packages/celotool/src/e2e-tests/transfer_tests.ts
@@ -164,7 +164,7 @@ describe('Transfer tests', function(this: any) {
   const ToAddress = '0xbBae99F0E1EE565404465638d40827b54D343638'
   const FeeRecipientAddress = '0x4f5f8a3f45d179553e7b95119ce296010f50f6f1'
 
-  const syncModes = ['full', 'fast', 'light', 'ultralight']
+  const syncModes = ['full', 'fast', 'light', 'lightest']
   const gethConfig: GethTestConfig = {
     migrateTo: 18,
     instances: [
