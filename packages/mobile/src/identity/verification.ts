b6166906b161d1d2549e14c5c5a4818a1c3ec49f
J M Rossy <jm.rossy@gmail.com>
2020-01-30 21:44:46 -0500

    Add Enter Codes option to verification fail modal Reduce Escrow payment reclaim time to 1 day

--- packages/mobile/src/identity/verification.ts
+++ packages/mobile/src/identity/verification.ts
@@ -50,7 +50,8 @@ export enum VerificationStatus {
   GettingStatus = 2,
   RequestingAttestations = 3,
   RevealingNumber = 4,
-  Done = 5,
+  RevealAttemptFailed = 5,
+  Done = 6,
 }
 
 export enum CodeInputType {
@@ -470,21 +471,8 @@ function* revealAndCompleteAttestation(
   attestation: ActionableAttestation
 ) {
   const issuer = attestation.issuer
-  Logger.debug(TAG + '@revealAttestation', `Revealing an attestation for issuer: ${issuer}`)
-  CeloAnalytics.track(CustomEventNames.verification_reveal_attestation, { issuer })
 
-  const response = yield call(
-    [attestationsWrapper, attestationsWrapper.revealPhoneNumberToIssuer],
-    e164Number,
-    account,
-    attestation.issuer,
-    attestation.attestationServiceURL
-  )
-  if (!response.ok) {
-    throw new Error(
-      `Error revealing to issuer ${attestation.attestationServiceURL}. Status code: ${response.status}`
-    )
-  }
+  yield call(tryRevealPhoneNumber, attestationsWrapper, account, e164Number, attestation)
 
   const code: AttestationCode = yield call(waitForAttestationCode, issuer)
 
@@ -505,6 +493,41 @@ function* revealAndCompleteAttestation(
 
   yield put(completeAttestationCode())
   Logger.debug(TAG + '@revealAttestation', `Attestation for issuer ${issuer} completed`)
+}
+
+function* tryRevealPhoneNumber(
+  attestationsWrapper: AttestationsWrapper,
+  account: string,
+  e164Number: string,
+  attestation: ActionableAttestation
+) {
+  const issuer = attestation.issuer
+  Logger.debug(TAG + '@tryRevealPhoneNumber', `Revealing an attestation for issuer: ${issuer}`)
+  CeloAnalytics.track(CustomEventNames.verification_reveal_attestation, { issuer })
+
+  try {
+    const response = yield call(
+      [attestationsWrapper, attestationsWrapper.revealPhoneNumberToIssuer],
+      e164Number,
+      account,
+      attestation.issuer,
+      attestation.attestationServiceURL
+    )
+    if (!response.ok) {
+      throw new Error(
+        `Error revealing to issuer ${attestation.attestationServiceURL}. Status code: ${response.status}`
+      )
+    }
+
+    Logger.debug(TAG + '@tryRevealPhoneNumber', `Revealing for issuer ${issuer} successful`)
+  } catch (error) {
+    // This is considered a recoverable error because the user may have received the code in a previous run
+    // So instead of propagating the error, we catch it just update status. This will trigger the modal,
+    // allowing the user to enter codes manually or skip verification.
+    Logger.error(TAG + '@tryRevealPhoneNumber', `Reveal for issuer ${issuer} failed`, error)
+    yield put(showError(ErrorMessages.REVEAL_ATTESTATION_FAILURE))
+    yield put(setVerificationStatus(VerificationStatus.RevealAttemptFailed))
+  }
 }
 
 // Get the code from the store if it's already there, otherwise wait for it
