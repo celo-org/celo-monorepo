7772e5e268630b1c4654b5d03c87cc80f6276dcf
Jean Regisser <jean.regisser@gmail.com>
2020-02-03 15:16:16 +0100

    Workaround victory-native importing 2 different victory-core modules

--- packages/mobile/src/setupE2eEnv.e2e.ts
+++ packages/mobile/src/setupE2eEnv.e2e.ts
@@ -1,5 +1,8 @@
 import Config from 'react-native-config'
-import { TimerContext } from 'victory-core/es'
+// @ts-ignore
+import { TimerContext } from 'victory-core'
+// @ts-ignore
+import { TimerContext as TimerContextES } from 'victory-core/es'
 
 // Inject custom e2e config
 Config.IS_E2E = true
@@ -12,5 +15,11 @@ Config.IS_E2E = true
 // and adjust accordingly.
 // Long term fix would be to disable RN timer tracking within Detox
 // see https://github.com/wix/Detox/issues/1513
+// Also victory-native ends up importing 2 different victory-core
+// so fix both until this is resolved upstream.
 TimerContext._currentValue.transitionTimer.stop()
 TimerContext._currentValue.animationTimer.stop()
+// @ts-ignore
+TimerContextES._currentValue.transitionTimer.stop()
+// @ts-ignore
+TimerContextES._currentValue.animationTimer.stop()
