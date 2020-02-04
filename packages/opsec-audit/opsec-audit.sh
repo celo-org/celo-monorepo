b09434addbfb53ff35f4035d423c2f02b0672b82
Javier Cortejoso <javier.cortejoso@gmail.com>
2020-02-04 19:03:48 +0100

    Some changes to opsec script

--- packages/opsec-audit/opsec-audit.sh
+++ packages/opsec-audit/opsec-audit.sh
@@ -1,29 +1,83 @@
 #!/bin/bash
 tput clear
 trap ctrl_c INT
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+LYNIS_TARFILE=lynis-2.7.5.tar.gz
 
 function ctrl_c() {
-        echo "**Exiting as encountered Ctrl+C"
-	exit 0;
+  echo "**Exiting as encountered Ctrl+C"
+  exit 0
 }
+
+function testcmd() {
+  if command -v "$1" >/dev/null; then
+    return 0
+  else
+    echo "The command $1 is not installed in your system. Please intall it and run the script again"
+    return 1
+  fi
+}
+
+function check_root() {
+  if [[ $EUID -ne 0 ]]; then
+    echo "Please run this script as root or using sudo"
+    exit 1
+  fi
+}
+
+function get_so() {
+  # From https://unix.stackexchange.com/questions/6345/how-can-i-get-distribution-name-and-version-number-in-a-simple-shell-script
+  if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    source /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    source /etc/lsb-release
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    OS=Debian
+    VER=$(cat /etc/debian_version)
+  else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+    VER=$(uname -r)
+  fi
+}
+##
+check_root
+get_so
+
 #
 echo "###############################################"
 echo "Welcome to the Celo Security audit of your linux machine:"
 echo "###############################################"
 echo
 echo "This script will execute speedtest-cli, lynis, perform some extra checks and upload results:"
-echo "Note: it has been tested for Debian/Centos:"
+echo "Note: it has been tested for Debian/Centos."
 echo
 sleep 3
 echo "###############################################"
-echo "You Are $HOSTNAME..:"
+echo "You Are $HOSTNAME, $OS $VER distribution...:"
 echo
 sleep 3
 echo "Starting"
 START=$(date +%s)
-echo -e "\e[0;33m 1. Downloading Lynis////// \e[0m"
-curl https://downloads.cisofy.com/lynis/lynis-2.7.5.tar.gz -o lynis-2.7.5.tar.gz
-tar xfvz lynis-2.7.5.tar.gz -C /usr/local
+if [[ ! -f "/usr/local/lynis/lynis" ]]; then
+  if [[ ! -f "$DIR/$LYNIS_TARFILE" ]]; then
+    echo -e "\e[0;33m 1. Downloading Lynis////// \e[0m"
+    curl -s -f https://downloads.cisofy.com/lynis/$LYNIS_TARFILE -o "$DIR/$LYNIS_TARFILE"
+  fi
+  tar xfvz "$DIR/$LYNIS_TARFILE" -C /usr/local
+fi
 echo 'plugin=tooling' | sudo tee -a /usr/local/lynis/custom.prf > /dev/null
 echo
 echo "###############################################"
@@ -73,11 +127,14 @@ echo "###############################################"
 echo
 echo -e "\e[0;33m 10. Check Password Strength Tools///// \e[0m"
 grep -Ei "suggestion\[\]\=AUTH-9262" /var/log/lynis-report.dat
-echo "DEBIAN"
-cat /etc/pam.d/common-auth
-echo "CentOS-RHEL"
-cat /etc/pam.d/system-auth
-cat /etc/security/pwquality.conf
+if [[ "$OS" == *"Debian"* ]] || [[ "$OS" == *"Ubuntu"* ]]; then
+  echo "DEBIAN"
+  cat /etc/pam.d/common-auth
+elif [[ "$OS" == *"CentOS"* ]] || [[ "$OS" == *"Red Hat"* ]] || [[ "$OS" == *"Fedora"* ]]; then
+  echo "CentOS-RHEL"
+  cat /etc/pam.d/system-auth
+  cat /etc/security/pwquality.conf
+fi
 echo
 echo "###############################################"
 echo
@@ -101,24 +158,28 @@ echo "Check Lynis Output"
 echo
 echo "###############################################"
 echo
-echo -e "\e[0;33m 15. Downloading and Executing Speedtest///// \e[0m"
-curl -Lo speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
-chmod +x speedtest-cli
-./speedtest-cli
+if [[ ! -f "$DIR/speedtest-cli" ]]; then
+  echo -e "\e[0;33m 15a. Downloading Speedtest///// \e[0m"
+  curl -s -f -Lo speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+fi
+echo -e "\e[0;33m 15b. Executing Speedtest///// \e[0m"
+chmod +x $DIR/speedtest-cli
+$DIR/speedtest-cli
 echo
 echo "###############################################"
-echo -e "\e[0;33m 14. Check Ledger///// \e[0m"
+echo -e "\e[0;33m 16. Check Ledger///// \e[0m"
 echo
 echo "###############################################"
 echo
-lsusb -d "2c97:"
+testcmd lsusb && lsusb -d "2c97:"
 echo
-docker exec -it celo-validator geth -exec personal.listWallets attach | grep -B4 "ledger://"
+testcmd docker && docker ps | grep celo-validator && docker exec -it celo-validator geth -exec personal.listWallets attach | grep -B4 "ledger://"
 echo
 echo "###############################################"
 END=$(date +%s)
-DIFF=$(( $END - $START ))
+DIFF=$(( END - START ))
 echo "Script completed in $DIFF seconds :"
 echo
 
-exit 0;
+exit 0
+
