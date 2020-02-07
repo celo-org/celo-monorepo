0d59066e7d6a914263174f76d9625698c6563f1b
jfoutts-celo <57463555+jfoutts-celo@users.noreply.github.com>
2020-02-07 13:02:53 -0800

    contractkit: add slashing wrappers  (#2405)

--- packages/utils/src/address.ts
+++ packages/utils/src/address.ts
@@ -33,3 +33,59 @@ export const isValidPrivateKey = (privateKey: string) =>
 export { isValidAddress, isValidChecksumAddress, toChecksumAddress } from 'ethereumjs-util'
 
 export const NULL_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+export const findAddressIndex = (address: Address, addresses: Address[]) =>
+  addresses.findIndex((x) => eqAddress(x, address))
+
+// Returns an array of indices mapping the entries of oldAddress[] to newAddress[]
+export const mapAddressListOnto = (oldAddress: Address[], newAddress: Address[]) => {
+  const oldAddressIndex: Array<{
+    address: Address
+    index: number
+  }> = oldAddress.map((x: Address, index: number) => ({ address: normalizeAddress(x), index }))
+
+  const newAddressIndex: Array<{
+    address: Address
+    index: number
+  }> = newAddress.map((x: Address, index: number) => ({ address: normalizeAddress(x), index }))
+
+  oldAddressIndex.sort((a, b) => a.address.localeCompare(b.address))
+  newAddressIndex.sort((a, b) => a.address.localeCompare(b.address))
+  const res = [...Array(oldAddress.length).fill(-1)]
+
+  for (let i = 0, j = 0; i < oldAddress.length && j < newAddress.length; ) {
+    const cmp = oldAddressIndex[i].address.localeCompare(newAddressIndex[j].address)
+    if (cmp < 0) {
+      i++
+    } else if (cmp > 0) {
+      j++
+    } else {
+      // Address is present in both lists
+      res[oldAddressIndex[i].index] = newAddressIndex[j].index
+      i++
+      j++
+    }
+  }
+  return res
+}
+
+// Returns data[] reordered by mapAddressListOnto(), and initiaValue for any entry of
+// oldAddress[] not present in newAddress[].
+export function mapAddressListDataOnto<T>(
+  data: T[],
+  oldAddress: Address[],
+  newAddress: Address[],
+  initialValue: T
+): T[] {
+  const res = [...Array(oldAddress.length).fill(initialValue)]
+  if (data.length === 0) {
+    return res
+  }
+  const addressIndexMap = mapAddressListOnto(oldAddress, newAddress)
+  for (let i = 0; i < addressIndexMap.length; i++) {
+    if (addressIndexMap[i] >= 0) {
+      res[addressIndexMap[i]] = data[i]
+    }
+  }
+  return res
+}
