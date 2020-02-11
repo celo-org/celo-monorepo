3eaff515b820a74fc77f67bf7b1eaef616a2d1a6
Victor Graf <victor@celo.org>
2020-02-11 11:33:07 -0800

    adjust migration numbers in e2e tests

--- packages/celotool/src/e2e-tests/transfer_tests.ts
+++ packages/celotool/src/e2e-tests/transfer_tests.ts
@@ -170,7 +170,7 @@ describe('Transfer tests', function(this: any) {
 
   const syncModes = ['full', 'fast', 'light', 'ultralight']
   const gethConfig: GethRunConfig = {
-    migrateTo: 19,
+    migrateTo: 18,
     networkId: 1101,
     network: 'local',
     runPath: TMP_PATH,
