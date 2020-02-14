4005bbf8b159c6c801f04eb69852accc4a6e794b
Aaron DeRuvo <aaron.deruvo@gmail.com>
2020-02-13 16:49:59 -0800

    🖼 Got the “code editor” image in looks so good

--- packages/web/src/dev/Sandbox.tsx
+++ packages/web/src/dev/Sandbox.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import CodeEditor from 'src/dev/CodeEditor'
 import { H2, H3 } from 'src/fonts/Fonts'
 import { NameSpaces, useTranslation } from 'src/i18n'
 import { Cell, GridRow, Spans } from 'src/layout/GridRow'
 import Button, { BTN } from 'src/shared/Button.3'
-import { colors, fonts, standardStyles, textStyles } from 'src/styles'
+import { fonts, standardStyles, textStyles } from 'src/styles'
 
 export default function Sandbox() {
   const { t } = useTranslation(NameSpaces.dev)
@@ -19,23 +20,10 @@ export default function Sandbox() {
           <H3 style={textStyles.invert}>{t('sandbox.overtitle')}</H3>
           <H2 style={[textStyles.invert, standardStyles.elementalMargin]}>{t('sandbox.title')}</H2>
           <Text style={[fonts.p, textStyles.invert]}>{t('sandbox.explainer')}</Text>
-          <View style={standardStyles.centered}>
+          <View style={[standardStyles.centered, standardStyles.blockMarginTablet]}>
             <View style={[standardStyles.blockMarginMobile, styles.codeBox]}>
-              <code style={{ color: colors.gray, padding: 20 }}>const Web3 = require('web3')</code>
-              <code style={{ color: colors.gray, padding: 20 }}>
-                const ContractKit = require('@celo/contractkit')
-              </code>
-              <code style={{ color: colors.gray, padding: 20 }}>
-                const provider = new
-                Web3.providers.HttpProvider(`https://alfajores-forno.celo-testnet.org`)
-              </code>
-              <code style={{ color: colors.gray, padding: 20 }}>
-                const web3 = new Web3(provider)
-              </code>
-              <code style={{ color: colors.gray, padding: 20 }}>
-                const contractKit = ContractKit.newKitFromWeb3(web3)
-              </code>
-              <View style={[{ position: 'absolute', right: 0, bottom: 0 }]}>
+              <CodeEditor />
+              <View style={[styles.absoluteCenter, standardStyles.centered]}>
                 <Button
                   kind={BTN.PRIMARY}
                   href="https://repl.it/@annakaz/contractkit"
@@ -53,10 +41,9 @@ export default function Sandbox() {
 
 const styles = StyleSheet.create({
   codeBox: {
-    backgroundColor: colors.black,
-    height: 400,
-    borderColor: colors.greenScreen,
-    borderWidth: 1,
+    height: '100%',
+    maxHeight: '80vh',
     width: '100%',
   },
+  absoluteCenter: { position: 'absolute', right: 0, left: 0, top: 0, bottom: 0 },
 })
