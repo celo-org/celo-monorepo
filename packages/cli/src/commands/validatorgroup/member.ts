95bf6554afda938e95aba24c96a0e2a2e45aff8e
Keefer Taylor <keefer.t@gmail.com>
2020-02-16 14:21:15 -0500

    Update member.ts

--- packages/cli/src/commands/validatorgroup/member.ts
+++ packages/cli/src/commands/validatorgroup/member.ts
@@ -21,7 +21,7 @@ export default class ValidatorGroupMembers extends BaseCommand {
     }),
     reorder: flags.integer({
       exclusive: ['accept', 'remove'],
-      description: 'Reorder a validator within the members list',
+      description: 'Reorder a validator within the members list. Indices are 0 based.',
     }),
   }
 
