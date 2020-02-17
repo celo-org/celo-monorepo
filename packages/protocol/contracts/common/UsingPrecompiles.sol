b119d97423a3c7d368b0e2d1015c80568109a81e
Sami Mäkelä <mrsmkl@gmail.com>
2020-02-17 13:21:16 +0200

    calculate quorum the same way as minQuorumSize in geth

--- packages/protocol/contracts/common/UsingPrecompiles.sol
+++ packages/protocol/contracts/common/UsingPrecompiles.sol
@@ -234,4 +234,21 @@ contract UsingPrecompiles {
     }
     return x;
   }
+
+  /**
+   * @notice Returns the minimum number of required signers for a given block number.
+   * @dev Computed in Geth as int(math.Ceil(float64(2*valSet.Size()) / 3))
+   */
+  function minQuorumSize(uint256 blockNumber) internal view returns (uint256) {
+    return numberValidatorsInSet(blockNumber).mul(2).add(2).div(3);
+  }
+
+  /**
+   * @notice Computes byzantine quorum from current validator set size
+   * @return Byzantine quorum of validators.
+   */
+  function byzantineQuorumValidatorsInCurrentSet() public view returns (uint256) {
+    return minQuorumSize(block.number);
+  }
+
 }
