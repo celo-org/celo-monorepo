235975769df27def9b485e1cf1262124f69896cd
Alec Schaefer <AlecSchaefer@users.noreply.github.com>
2020-02-18 20:06:57 -0500

    Sets governance thresholds during migration

--- packages/protocol/migrations/22_governance.ts
+++ packages/protocol/migrations/22_governance.ts
@@ -1,5 +1,6 @@
 /* tslint:disable:no-console */
 
+import { constitution } from '@celo/protocol/governanceConstitution'
 import { CeloContractName } from '@celo/protocol/lib/registry-utils'
 import {
   deploymentForCoreContract,
@@ -43,6 +44,55 @@ module.exports = deploymentForCoreContract<GovernanceInstance>(
       artifacts
     )
     await reserve.addSpender(governance.address)
+
+    console.info('Setting constitution thresholds')
+    Object.keys(constitution)
+      .filter((contractName) => contractName !== 'proxy')
+      .forEach(async (contractName) => {
+        const proxy: any = artifacts.require(contractName + 'Proxy')
+        const contract: any = await getDeployedProxiedContract<Truffle.ContractInstance>(
+          contractName,
+          artifacts
+        )
+
+        // Build mapping of function names to selectors
+        // Each function name maps to an array of selectors to account for overloading
+        const selectors: { [index: string]: string[] } = {}
+        proxy.abi
+          .concat(contract.abi)
+          .filter((abiEntry: any) => abiEntry.type === 'function')
+          .forEach((func: any) => {
+            if (typeof selectors[func.name] === 'undefined') {
+              selectors[func.name] = []
+            }
+            selectors[func.name].push(func.signature)
+          })
+
+        const setThresholds = (thresholds: any) => {
+          Object.keys(thresholds)
+            .filter((func) => func !== 'default')
+            .forEach(async (func) => {
+              await Promise.all(
+                selectors[func].map((selector) =>
+                  governance.setConstitution(
+                    contract.address,
+                    selector,
+                    toFixed(thresholds[func]).toFixed()
+                  )
+                )
+              )
+            })
+        }
+        setThresholds(constitution.proxy)
+        setThresholds(constitution[contractName])
+
+        // set default threshold
+        await governance.setConstitution(
+          contract.address,
+          '0x00000000',
+          toFixed(constitution[contractName].default).toFixed()
+        )
+      })
 
     const proxyOwnedByGovernance = ['GoldToken']
     await Promise.all(
