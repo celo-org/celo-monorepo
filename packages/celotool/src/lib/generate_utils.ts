195b501455990a054b54936a2a565417a0407caf
Trevor Porter <trkporter@ucdavis.edu>
2020-02-19 14:27:29 +0100

    Revert "Revert "Removes EpochData (#2361)""
    This reverts commit 1f520f8047f6a1514e1cc0327321e10f80d3b946.

--- packages/celotool/src/lib/generate_utils.ts
+++ packages/celotool/src/lib/generate_utils.ts
@@ -282,8 +282,6 @@ const generateIstanbulExtraData = (validators: Validator[]) => {
           // ParentAggregatedSeal.Round
           new Buffer(0),
         ],
-        // EpochData
-        new Buffer(0),
       ])
       .toString('hex')
   )
