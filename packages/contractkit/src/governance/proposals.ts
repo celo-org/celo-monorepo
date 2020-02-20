10682096ae3c072d4e9ed4467d971d420bad246b
Victor Graf <victor@celo.org>
2020-02-19 16:21:32 -0800

    add docstring to proposal builder and fix unintutive behavior

--- packages/contractkit/src/governance/proposals.ts
+++ packages/contractkit/src/governance/proposals.ts
@@ -20,6 +20,19 @@ export const hotfixToEncodedParams = (kit: ContractKit, proposal: Proposal, salt
 export const hotfixToHash = (kit: ContractKit, proposal: Proposal, salt: Buffer) =>
   keccak256(hotfixToEncodedParams(kit, proposal, salt)) as Buffer
 
+/**
+ * JSON encoding of a proposal transaction.
+ *
+ * Example:
+ * ```json
+ * {
+ *   "contract": "Election",
+ *   "function": "setElectableValidators",
+ *   "args": [ "1", "120" ],
+ *   "value": "0"
+ * }
+ * ```
+ */
 export interface ProposalTransactionJSON {
   contract: CeloContract
   function: string
@@ -28,6 +41,12 @@ export interface ProposalTransactionJSON {
   value: string
 }
 
+/**
+ * Convert a compiled proposal to a human-readble JSON form using network information.
+ * @param kit Contract kit instance used to resolve addresses to contract names.
+ * @param proposal A constructed proposal object.
+ * @returns The JSON encoding of the proposal.
+ */
 export const proposalToJSON = async (kit: ContractKit, proposal: Proposal) => {
   const contractDetails = await obtainKitContractDetails(kit)
   const blockExplorer = new BlockExplorer(kit, contractDetails)
@@ -48,20 +67,38 @@ export const proposalToJSON = async (kit: ContractKit, proposal: Proposal) => {
 }
 
 type ProposalTxParams = Pick<ProposalTransaction, 'to' | 'value'>
+
+/**
+ * Builder class to construct proposals from JSON or transaction objects.
+ */
 export class ProposalBuilder {
   constructor(
     private readonly kit: ContractKit,
     private readonly builders: Array<() => Promise<ProposalTransaction>> = []
   ) {}
 
+  /**
+   * Build calls all of the added build steps and returns the final proposal.
+   * @returns A constructed Proposal object (i.e. a list of ProposalTransaction)
+   */
   build = async () => concurrentMap(4, this.builders, (builder) => builder())
 
+  /**
+   * Converts a Web3 transaction into a proposal transaction object.
+   * @param tx A Web3 transaction object to convert.
+   * @param params Parameters for how the transaction should be executed.
+   */
   fromWeb3tx = (tx: TransactionObject<any>, params: ProposalTxParams): ProposalTransaction => ({
     value: params.value,
     to: params.to,
     input: tx.encodeABI(),
   })
 
+  /**
+   * Adds a transaction to set the implementation on a proxy to the given address.
+   * @param proxyAddress Address of the proxy which should have its implementation set.
+   * @param newImplementationAddress Address of the new contract implementation.
+   */
   addProxyRepointingTx = (proxyAddress: string, newImplementationAddress: string) => {
     this.addWeb3Tx(setImplementationOnProxy(newImplementationAddress), {
       to: proxyAddress,
@@ -69,12 +106,22 @@ export class ProposalBuilder {
     })
   }
 
+  /**
+   * Adds a Web3 transaction to the list for proposal construction.
+   * @param tx A Web3 transaction object to add to the proposal.
+   * @param params Parameters for how the transaction should be executed.
+   */
   addWeb3Tx = (tx: TransactionObject<any>, params: ProposalTxParams) =>
     this.builders.push(async () => this.fromWeb3tx(tx, params))
 
+  /**
+   * Adds a Celo transaction to the list for proposal construction.
+   * @param tx A Celo transaction object to add to the proposal.
+   * @param params Optional parameters for how the transaction should be executed.
+   */
   addTx(tx: CeloTransactionObject<any>, params: Partial<ProposalTxParams> = {}) {
-    const to = tx.defaultParams && tx.defaultParams.to ? tx.defaultParams.to : params.to
-    const value = tx.defaultParams && tx.defaultParams.value ? tx.defaultParams.value : params.value
+    const to = params.to || (tx.defaultParams && tx.defaultParams.to)
+    const value = params.value || (tx.defaultParams && tx.defaultParams.value)
     if (!to || !value) {
       throw new Error("Transaction parameters 'to' and/or 'value' not provided")
     }
@@ -82,6 +129,10 @@ export class ProposalBuilder {
     this.addWeb3Tx(tx.txo, { to, value: valueToString(value.toString()) })
   }
 
+  /**
+   * Adds a JSON encoded proposal transaction to the builder list.
+   * @param tx A JSON encoded proposal transaction.
+   */
   addJsonTx = (tx: ProposalTransactionJSON) =>
     this.builders.push(async () => {
       const contract = await this.kit._web3Contracts.getContract(tx.contract)
