e6f6ee7bc51c7b298a09215210b82792480df17e
Audrey Penven <audrey@celo.org>
2020-02-21 18:39:38 +0100

    more useful error. allow bots to scale up gold

--- packages/celotool/src/lib/voting-bot.ts
+++ packages/celotool/src/lib/voting-bot.ts
@@ -1,6 +1,7 @@
 import { ContractKit, newKit } from '@celo/contractkit'
 import { CeloProvider } from '@celo/contractkit/lib/providers/celo-provider'
 import { getAccountAddressFromPrivateKey } from '@celo/walletkit'
+import BigNumber from 'bignumber.js'
 import { getFornoUrl } from 'src/lib/endpoints'
 import { envVar, fetchEnv } from 'src/lib/env-utils'
 import { AccountType, getPrivateKeysFor } from 'src/lib/generate_utils'
@@ -22,19 +23,22 @@ export async function setupVotingBotAccounts(celoEnv: string) {
   const fornoUrl = getFornoUrl(celoEnv)
   const mnemonic = fetchEnv(envVar.MNEMONIC)
   const numBotAccounts = parseInt(fetchEnv(envVar.VOTING_BOTS), 10)
+  const botStartingBalance = new BigNumber(fetchEnv(envVar.VOTING_BOT_BALANCE))
 
   const kit: ContractKit = newKit(fornoUrl)
   const goldToken = await kit.contracts.getGoldToken()
   const lockedGold = await kit.contracts.getLockedGold()
   const accounts = await kit.contracts.getAccounts()
 
-  const botsWithoutGold: string[] = []
+  const botsWithoutEnoughGold: string[] = []
 
   for (const key of getPrivateKeysFor(AccountType.VOTING_BOT, mnemonic, numBotAccounts)) {
     const botAccount = ensure0x(getAccountAddressFromPrivateKey(key))
     const goldBalance = await goldToken.balanceOf(botAccount)
-    if (goldBalance.isZero()) {
-      botsWithoutGold.push(botAccount)
+    // Assume that running bot accounts will have used some of their gold in transaction fees, so
+    // be tolerant of that decrease
+    if (goldBalance.isLessThan(botStartingBalance.multipliedBy(0.99))) {
+      botsWithoutEnoughGold.push(botAccount)
       continue
     }
 
@@ -46,7 +50,7 @@ export async function setupVotingBotAccounts(celoEnv: string) {
     }
 
     const amountLocked = await lockedGold.getAccountTotalLockedGold(botAccount)
-    const totalAmountToBeLocked = goldBalance.multipliedBy(0.99)
+    const totalAmountToBeLocked = botStartingBalance.multipliedBy(0.99)
 
     if (amountLocked.isLessThan(totalAmountToBeLocked)) {
       const amountToLock = totalAmountToBeLocked.minus(amountLocked).toFixed(0)
@@ -60,8 +64,16 @@ export async function setupVotingBotAccounts(celoEnv: string) {
       console.info(`Locked gold for ${botAccount}`)
     }
   }
-  if (botsWithoutGold.length > 0) {
-    throw new Error(`These bot accounts have no gold. Faucet them, and retry: ${botsWithoutGold}`)
+  if (botsWithoutEnoughGold.length > 0) {
+    console.error(`These bot accounts need to be fauceted: ${botsWithoutEnoughGold}`)
+    console.error('Run the following command to fix this:')
+    throw new Error(
+      `Faucet the bots and try again \n
+      celotool account faucet --gold ${botStartingBalance
+        .div(1000000000000000000)
+        .toFixed()} --dollar 0 -e ${celoEnv} --account ${botsWithoutEnoughGold}
+      `
+    )
   }
   console.info('Finished/confirmed setup of voting bot accounts')
 
