1cf75aea2ce083bf5c3f3f1f35eaf138f973e542
Asa Oines <a@celo.org>
2020-02-23 15:41:09 -0800

    Oops

--- packages/protocol/contracts/governance/LockedGold.sol
+++ packages/protocol/contracts/governance/LockedGold.sol
@@ -76,7 +76,7 @@ contract LockedGold is ILockedGold, ReentrancyGuard, Initializable, UsingRegistr
    * @notice Sets the duration in seconds users must wait before withdrawing gold after unlocking.
    * @param value The unlocking period in seconds.
    */
-  function setUnlockingPeriod(uint256 value) external onlyOwner {
+  function setUnlockingPeriod(uint256 value) public onlyOwner {
     require(value != unlockingPeriod, "Unlocking period not changed");
     unlockingPeriod = value;
     emit UnlockingPeriodSet(value);
