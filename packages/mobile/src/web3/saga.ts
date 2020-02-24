6518576be2fd8c53c50910ba6a3b3cc175d74497
Ivan Sorokin <ivan@sorokin.io>
2020-02-25 02:23:37 +0800

    Fix invites because of different dev bundleId on Android and iOS

--- packages/mobile/src/web3/saga.ts
+++ packages/mobile/src/web3/saga.ts
@@ -20,7 +20,7 @@ import { UNLOCK_DURATION } from 'src/geth/consts'
 import { deleteChainData, stopGethIfInitialized } from 'src/geth/geth'
 import { gethSaga, waitForGethConnectivity } from 'src/geth/saga'
 import { gethStartedThisSessionSelector } from 'src/geth/selectors'
-import { navigate, navigateToError } from 'src/navigator/NavigationService'
+import { navigate, navigateBack, navigateToError } from 'src/navigator/NavigationService'
 import { Screens } from 'src/navigator/Screens'
 import { setCachedPincode } from 'src/pincode/PincodeCache'
 import { restartApp } from 'src/utils/AppRestart'
@@ -343,6 +343,7 @@ export function* unlockAccount(account: string) {
     }
 
     const pincode = yield call(getPincode)
+    navigateBack()
     const fornoMode = yield select(fornoSelector)
     if (fornoMode) {
       if (accountAlreadyAddedInFornoMode) {
