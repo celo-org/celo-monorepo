7c9411f9d7c7fe6b5595ae7184d57358bb402241
Lucas Ege <lucas@celo.org>
2020-02-24 17:01:17 -0800

    Working on local testnet, some style changes incoming

--- packages/protocol/scripts/bash/deploy_release_contracts.sh
+++ packages/protocol/scripts/bash/deploy_release_contracts.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Upgrades all proxyable contracts whose compiled bytecode differs from the
-# bytecode of the contract pointed to by the corresponding proxy.
+# Deploys all grants detailed in `GRANTS_FILE` from the corresponding entity.
 #
 # Flags:
 # -n: Name of the network to upgrade
+# -f: File containing grant information
 
 NETWORK=""
+GRANTS_FILE=""
 
-while getopts 'n:' flag; do
+while getopts 'n:f:' flag; do
   case "${flag}" in
     n) NETWORK="$OPTARG" ;;
+    f) GRANTS_FILE="${OPTARG}" ;;
     *) error "Unexpected option ${flag}" ;;
   esac
 done
 
 [ -z "$NETWORK" ] && echo "Need to set the NETWORK via the -n flag" && exit 1;
+[ -z "$GRANTS_FILE" ] && echo "Need to set the GRANTS_FILE via the -f flag" && exit;
 
 if ! nc -z 127.0.0.1 8545 ; then
   echo "Port 8545 not open"
@@ -26,4 +29,4 @@ fi
 yarn run build && \
 # yarn run truffle-config -n $NETWORK && \
 yarn run truffle exec ./scripts/truffle/deploy_release_contracts.js \
-  --network $NETWORK --build_directory $PWD/build/$NETWORK \
+  --network $NETWORK --grants $GRANTS_FILE --build_directory $PWD/build/$NETWORK \
--- packages/protocol/scripts/truffle/deploy_release_contracts.ts
+++ packages/protocol/scripts/truffle/deploy_release_contracts.ts
@@ -1,45 +1,73 @@
-import BigNumber from 'bignumber.js'
-import { getDeployedProxiedContract } from 'lib/web3-utils'
-import { RegistryInstance, ReleaseGoldContract } from 'types'
+import { getDeployedProxiedContract } from '@celo/protocol/lib/web3-utils'
+import {
+  MultiSigContract,
+  RegistryInstance,
+  ReleaseGoldContract,
+  ReleaseGoldProxyContract,
+} from 'types'
 import fs = require('fs')
-
-interface ReleaseGoldConfig {
-  releaseStartTime: number
-  releaseCliffTime: number
-  numReleasePeriods: number
-  releasePeriod: number
-  amountReleasedPerPeriod: BigNumber
-  revocable: boolean
-  beneficiary: string
-  releaseOwner: string
-  refundAddress: string
-  subjectToLiquidityProvision: boolean
-  initialDistributionPercentage: number
-  canValidate: boolean
-  canVote: boolean
-}
 
 module.exports = async (callback: (error?: any) => number) => {
   try {
+    const argv = require('minimist')(process.argv.slice(2), {
+      string: ['network', 'grants'],
+    })
     const registry = await getDeployedProxiedContract<RegistryInstance>('Registry', artifacts)
+    const Multisig: MultiSigContract = artifacts.require('MultiSig')
     const ReleaseGold: ReleaseGoldContract = artifacts.require('ReleaseGold')
+    const ReleaseGoldProxy: ReleaseGoldProxyContract = artifacts.require('ReleaseGoldProxy')
     const releases = []
     const handleJSONFile = async (err, data) => {
       if (err) {
         throw err
       }
-      const releaseGoldConfigs: ReleaseGoldConfig[] = JSON.parse(data)
+      data = JSON.parse(data)
+      const entity = data['entity']
+      const releaseGoldConfigs = data['grants']
       for (const releaseGoldConfig of releaseGoldConfigs) {
-        const args = []
-        for (const key of Object.keys(releaseGoldConfig)) {
-          args.push(releaseGoldConfig[key])
-        }
-        args.push(registry.address)
-        const releaseGoldInstance = await ReleaseGold.new.apply(args)
-        releases.push([releaseGoldConfig.beneficiary, releaseGoldInstance.address])
+        const releaseStartTime = new Date(releaseGoldConfig['releaseStartTime'])
+        const releaseCliffTime = releaseGoldConfig['releaseCliffTime']
+        const numReleasePeriods = releaseGoldConfig['numReleasePeriods']
+        const releasePeriod = releaseGoldConfig['releasePeriod']
+        const amountReleasedPerPeriod = releaseGoldConfig['amountReleasedPerPeriod']
+        const revocable = releaseGoldConfig['revocable']
+        const beneficiary = releaseGoldConfig['beneficiary']
+        const releaseOwner = releaseGoldConfig['releaseOwner']
+        const refundAddress = releaseGoldConfig['refundAddress']
+        const subjectToLiquidityProvision = releaseGoldConfig['subjectToLiquidityProvision']
+        const initialDistributionPercentage = releaseGoldConfig['initialDistributionPercentage']
+        const canValidate = releaseGoldConfig['canValidate']
+        const canVote = releaseGoldConfig['canVote']
+        const releaseGoldInstance = await ReleaseGold.new(
+          releaseStartTime.getTime() / 1000,
+          releaseCliffTime,
+          numReleasePeriods,
+          releasePeriod,
+          amountReleasedPerPeriod,
+          revocable,
+          beneficiary,
+          releaseOwner,
+          refundAddress,
+          subjectToLiquidityProvision,
+          initialDistributionPercentage,
+          canValidate,
+          canVote,
+          registry.address
+        )
+        const releaseGoldProxyInstance = await ReleaseGoldProxy.new()
+        await releaseGoldProxyInstance._setImplementation(releaseGoldInstance.address)
+        const multiSigInstance = await Multisig.new()
+        await multiSigInstance.initialize([entity, beneficiary], 2)
+        await releaseGoldProxyInstance._transferOwnership(multiSigInstance.address)
+        releases.push([
+          releaseGoldConfig.beneficiary,
+          releaseGoldProxyInstance.address,
+          multiSigInstance.address,
+        ])
       }
+      console.log(releases)
     }
-    fs.readFile('scripts/truffle/releaseGoldContracts.json', handleJSONFile)
+    fs.readFile(argv.grants, handleJSONFile)
   } catch (error) {
     callback(error)
   }
