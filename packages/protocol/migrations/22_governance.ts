001c36ffa55e1d6feb6efaffb82ea44d43bef63c
Alec Schaefer <AlecSchaefer@users.noreply.github.com>
2020-02-24 19:22:05 -0500

    adds test for governance constitutions

--- packages/protocol/migrations/22_governance.ts
+++ packages/protocol/migrations/22_governance.ts
@@ -5,6 +5,7 @@ import { CeloContractName } from '@celo/protocol/lib/registry-utils'
 import {
   deploymentForCoreContract,
   getDeployedProxiedContract,
+  getFunctionSelectorsForContract,
   transferOwnershipOfProxy,
   transferOwnershipOfProxyAndImplementation,
 } from '@celo/protocol/lib/web3-utils'
@@ -46,53 +47,30 @@ module.exports = deploymentForCoreContract<GovernanceInstance>(
     await reserve.addSpender(governance.address)
 
     console.info('Setting constitution thresholds')
-    Object.keys(constitution)
-      .filter((contractName) => contractName !== 'proxy')
-      .forEach(async (contractName) => {
-        const proxy: any = artifacts.require(contractName + 'Proxy')
-        const contract: any = await getDeployedProxiedContract<Truffle.ContractInstance>(
-          contractName,
-          artifacts
-        )
+    await Promise.all(
+      Object.keys(constitution)
+        .filter((contractName) => contractName !== 'proxy')
+        .map(async (contractName) => {
+          const contract: any = await getDeployedProxiedContract<Truffle.ContractInstance>(
+            contractName,
+            artifacts
+          )
 
-        // Build mapping of function names to selectors
-        // Each function name maps to an array of selectors to account for overloading
-        const selectors: { [index: string]: string[] } = {}
-        proxy.abi
-          .concat(contract.abi)
-          .filter((abiEntry: any) => abiEntry.type === 'function')
-          .forEach((func: any) => {
-            if (typeof selectors[func.name] === 'undefined') {
-              selectors[func.name] = []
-            }
-            selectors[func.name].push(func.signature)
-          })
+          const selectors = getFunctionSelectorsForContract(contract, contractName, artifacts)
+          selectors.default = ['0x00000000']
 
-        const setThresholds = (thresholds: any) => {
-          Object.keys(thresholds)
-            .filter((func) => func !== 'default')
-            .forEach(async (func) => {
-              await Promise.all(
+          const thresholds = { ...constitution.proxy, ...constitution[contractName] }
+          await Promise.all(
+            Object.keys(thresholds).map((func) =>
+              Promise.all(
                 selectors[func].map((selector) =>
-                  governance.setConstitution(
-                    contract.address,
-                    selector,
-                    toFixed(thresholds[func]).toFixed()
-                  )
+                  governance.setConstitution(contract.address, selector, toFixed(thresholds[func]))
                 )
               )
-            })
-        }
-        setThresholds(constitution.proxy)
-        setThresholds(constitution[contractName])
-
-        // set default threshold
-        await governance.setConstitution(
-          contract.address,
-          '0x00000000',
-          toFixed(constitution[contractName].default).toFixed()
-        )
-      })
+            )
+          )
+        })
+    )
 
     const proxyOwnedByGovernance = ['GoldToken']
     await Promise.all(
