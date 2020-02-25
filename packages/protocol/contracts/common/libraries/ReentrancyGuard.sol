be0016d905b75beaa16be8565c4932f1d564a81a
Justin Foutts <justin.foutts@celo.org>
2020-02-25 13:17:44 -0800

    Fix indentation

--- packages/protocol/contracts/common/libraries/ReentrancyGuard.sol
+++ packages/protocol/contracts/common/libraries/ReentrancyGuard.sol
@@ -17,12 +17,12 @@ contract ReentrancyGuard {
   }
 
   /**
-     * @dev Prevents a contract from calling itself, directly or indirectly.
-     * Calling a `nonReentrant` function from another `nonReentrant`
-     * function is not supported. It is possible to prevent this from happening
-     * by making the `nonReentrant` function external, and make it call a
-     * `private` function that does the actual work.
-     */
+   * @dev Prevents a contract from calling itself, directly or indirectly.
+   * Calling a `nonReentrant` function from another `nonReentrant`
+   * function is not supported. It is possible to prevent this from happening
+   * by making the `nonReentrant` function external, and make it call a
+   * `private` function that does the actual work.
+   */
   modifier nonReentrant() {
     _guardCounter += 1;
     uint256 localCounter = _guardCounter;
