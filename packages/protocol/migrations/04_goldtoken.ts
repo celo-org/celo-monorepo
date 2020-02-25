312f8da8648c2fe1fe25f974adaf16a9683f4ea2
Nam Chu Hoai <nambrot@googlemail.com>
2020-02-25 14:22:20 -0800

    Remove GoldToken as a FeeCurrencyWhitelist

--- packages/protocol/migrations/04_goldtoken.ts
+++ packages/protocol/migrations/04_goldtoken.ts
@@ -1,10 +1,7 @@
 /* tslint:disable:no-console */
 import { CeloContractName } from '@celo/protocol/lib/registry-utils'
-import {
-  deploymentForCoreContract,
-  getDeployedProxiedContract,
-} from '@celo/protocol/lib/web3-utils'
-import { FeeCurrencyWhitelistInstance, GoldTokenInstance } from 'types'
+import { deploymentForCoreContract } from '@celo/protocol/lib/web3-utils'
+import { GoldTokenInstance } from 'types'
 
 const initializeArgs = async () => {
   return []
@@ -14,12 +11,5 @@ module.exports = deploymentForCoreContract<GoldTokenInstance>(
   web3,
   artifacts,
   CeloContractName.GoldToken,
-  initializeArgs,
-  async (goldToken: GoldTokenInstance) => {
-    console.info('Whitelisting GoldToken as a fee currency')
-    const feeCurrencyWhitelist: FeeCurrencyWhitelistInstance = await getDeployedProxiedContract<
-      FeeCurrencyWhitelistInstance
-    >('FeeCurrencyWhitelist', artifacts)
-    await feeCurrencyWhitelist.addToken(goldToken.address)
-  }
+  initializeArgs
 )
