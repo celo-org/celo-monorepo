2e2b5bf703cfd242d4b90afee1ba55c73e6c9557
Asa Oines <a@celo.org>
2020-02-24 21:38:06 -0800

    prettify

--- packages/protocol/migrationsConfig.js
+++ packages/protocol/migrationsConfig.js
@@ -127,7 +127,7 @@ const DefaultConfig = {
       adjustmentSpeed: 0.1,
     },
     membershipHistoryLength: 60,
-    commissionUpdateDelay: 51840,  // Approximately 3 days
+    commissionUpdateDelay: 51840, // Approximately 3 days
     maxGroupSize: '5',
     // 30 Days divided by 24 to accelerate for Stake off
     slashingPenaltyResetPeriod: (60 * 60 * 24 * 30) / 24,
