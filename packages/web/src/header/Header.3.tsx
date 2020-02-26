36fd292ae5478b1be188868c8ee65146c6c3d150
aaron deruvo <aaron@celo.org>
2020-02-25 17:05:12 -0800

    Header / Menu fix and refactor
    replace custom watching of resize with use of withScreenSize context
    add Hoverable to menu
    Fix issue with scrolling down past open mobile menu

--- packages/web/src/header/Header.3.tsx
+++ packages/web/src/header/Header.3.tsx
@@ -9,15 +9,17 @@ import cssStyles from 'src/header/Header.3.scss'
 import { I18nProps, withNamespaces } from 'src/i18n'
 import MediumLogo from 'src/icons/MediumLogo'
 import Octocat from 'src/icons/Octocat'
+import { ScreenProps, ScreenSizes, withScreenSize } from 'src/layout/ScreenSize'
 import LogoDarkBg from 'src/logos/LogoDarkBg'
 import LogoLightBg from 'src/logos/LogoLightBg'
 import Button, { BTN } from 'src/shared/Button.3'
 import Footer from 'src/shared/Footer'
+import Hoverable from 'src/shared/Hoverable'
 import Link from 'src/shared/Link'
 import menu, { CeloLinks, MAIN_MENU } from 'src/shared/menu-items'
 import OvalCoin from 'src/shared/OvalCoin'
 import Responsive from 'src/shared/Responsive'
-import { DESKTOP_BREAKPOINT, HEADER_HEIGHT } from 'src/shared/Styles'
+import { HEADER_HEIGHT } from 'src/shared/Styles'
 import { colors } from 'src/styles'
 const BlueBanner = dynamic(import('src/header/BlueBanner'))
 const CookieConsent = dynamic(
@@ -41,14 +43,14 @@ interface OwnProps {
   router: Router
 }
 
-type Props = OwnProps & I18nProps
+type Props = OwnProps & I18nProps & ScreenProps
 
 interface State {
-  showDesktopMenu: boolean
   mobileMenuActive: boolean
   menuFaded: boolean
   belowFoldUpScroll: boolean
   isBannerShowing: boolean
+  isHovering: boolean
   bannerHeight: number
 }
 
@@ -71,6 +73,7 @@ export class Header extends React.PureComponent<Props, State> {
     mobileMenuActive: false,
     belowFoldUpScroll: false,
     isBannerShowing: false,
+    isHovering: false,
     bannerHeight: 0,
   }
 
@@ -104,9 +107,6 @@ export class Header extends React.PureComponent<Props, State> {
   }, 200)
 
   componentDidMount() {
-    this.windowResize({ window: Dimensions.get('window') })
-    Dimensions.addEventListener('change', this.windowResize)
-
     this.lastScrollOffset = scrollOffset()
     window.addEventListener('scroll', this.handleScroll)
 
@@ -114,21 +114,9 @@ export class Header extends React.PureComponent<Props, State> {
   }
 
   componentWillUnmount() {
-    Dimensions.removeEventListener('change', this.windowResize)
     window.removeEventListener('scroll', this.handleScroll)
   }
 
-  windowResize = ({ window: { width } }) => {
-    if (width < DESKTOP_BREAKPOINT) {
-      if (this.state.showDesktopMenu) {
-        this.setState({ showDesktopMenu: false })
-      }
-    } else {
-      if (!this.state.showDesktopMenu) {
-        this.setState({ showDesktopMenu: true, mobileMenuActive: false })
-      }
-    }
-  }
   closeMenu = () => {
     this.setState({ mobileMenuActive: false })
   }
@@ -150,7 +138,7 @@ export class Header extends React.PureComponent<Props, State> {
 
   getBackgroundColor = () => {
     if (this.isTranslucent() && !this.state.belowFoldUpScroll) {
-      return 'transparent'
+      return this.state.isHovering ? `rgba(0,0,0,0.5)` : 'transparent'
     }
     return this.isDarkMode() ? colors.dark : colors.white
   }
@@ -167,13 +155,34 @@ export class Header extends React.PureComponent<Props, State> {
     return this.props.router.pathname === menu.ABOUT_US.link && !this.state.belowFoldUpScroll
   }
 
+  setHovering = () => {
+    this.setState({ isHovering: true })
+  }
+  unsetHovering = () => {
+    this.setState({ isHovering: false })
+  }
+
+  willShowHamburger = () => {
+    if (this.props.screen === ScreenSizes.DESKTOP) {
+      return false
+    }
+
+    if (this.state.menuFaded && !this.state.mobileMenuActive) {
+      return false
+    }
+
+    return true
+  }
+
   render() {
-    const { t } = this.props
+    const { t, screen } = this.props
+    const isDesktop = screen === ScreenSizes.DESKTOP
+
     const foreground = this.getForegroundColor()
-    const background =
-      this.state.menuFaded && this.state.showDesktopMenu ? 'transparent' : this.getBackgroundColor()
+    const background = this.state.menuFaded && isDesktop ? 'transparent' : this.getBackgroundColor()
     const hamburger = this.state.mobileMenuActive ? colors.dark : foreground
     const isHomePage = this.props.router.pathname === menu.HOME.link
+
     return (
       <View
         // @ts-ignore
@@ -194,7 +203,6 @@ export class Header extends React.PureComponent<Props, State> {
         {isHomePage && (
           <BlueBanner onVisibilityChange={this.toggleBanner} getHeight={this.setBannerHeight} />
         )}
-
         <View
           // @ts-ignore
           style={[
@@ -231,60 +239,64 @@ export class Header extends React.PureComponent<Props, State> {
                 </View>
               </View>
             </Link>
-            {this.state.showDesktopMenu && (
-              <View
-                style={[
-                  styles.links,
-                  styles.fadeTransition as ViewStyle,
-                  this.state.menuFaded ? styles.menuInvisible : styles.menuVisible,
-                ]}
-              >
-                {menuItems.map((item, index) => (
-                  <View key={index} style={styles.linkWrapper}>
+            {isDesktop && (
+              <Hoverable onHoverIn={this.setHovering} onHoverOut={this.unsetHovering}>
+                <View
+                  style={[
+                    styles.links,
+                    styles.fadeTransition as ViewStyle,
+                    this.state.menuFaded ? styles.menuInvisible : styles.menuVisible,
+                  ]}
+                >
+                  {menuItems.map((item, index) => (
+                    <View key={index} style={styles.linkWrapper}>
+                      <Button
+                        kind={this.isDarkMode() ? BTN.DARKNAV : BTN.NAV}
+                        href={item.link}
+                        text={t(item.name)}
+                      />
+                      {this.props.router.pathname === item.link && (
+                        <View style={styles.activeTab}>
+                          <OvalCoin color={colors.primary} size={10} />
+                        </View>
+                      )}
+                    </View>
+                  ))}
+                  <View style={[styles.linkWrapper]}>
                     <Button
                       kind={this.isDarkMode() ? BTN.DARKNAV : BTN.NAV}
-                      href={item.link}
-                      text={t(item.name)}
+                      href={'https://medium.com/CeloHQ'}
+                      text={t('blog')}
+                      target={'_blank'}
+                      iconRight={<MediumLogo height={20} color={foreground} wrapWithLink={false} />}
                     />
-                    {this.props.router.pathname === item.link && (
-                      <View style={styles.activeTab}>
-                        <OvalCoin color={colors.primary} size={10} />
-                      </View>
-                    )}
                   </View>
-                ))}
-                <View style={[styles.linkWrapper]}>
-                  <Button
-                    kind={this.isDarkMode() ? BTN.DARKNAV : BTN.NAV}
-                    href={'https://medium.com/CeloHQ'}
-                    text={t('blog')}
-                    target={'_blank'}
-                    iconRight={<MediumLogo height={20} color={foreground} wrapWithLink={false} />}
-                  />
+                  <View style={[styles.linkWrapper]}>
+                    <Button
+                      kind={this.isDarkMode() ? BTN.DARKNAV : BTN.NAV}
+                      href={CeloLinks.gitHub}
+                      text={t('github')}
+                      target={'_blank'}
+                      iconRight={
+                        <Octocat size={22} color={this.isDarkMode() ? colors.white : colors.dark} />
+                      }
+                    />
+                  </View>
                 </View>
-                <View style={[styles.linkWrapper]}>
-                  <Button
-                    kind={this.isDarkMode() ? BTN.DARKNAV : BTN.NAV}
-                    href={CeloLinks.gitHub}
-                    text={t('github')}
-                    target={'_blank'}
-                    iconRight={
-                      <Octocat size={22} color={this.isDarkMode() ? colors.white : colors.dark} />
-                    }
-                  />
-                </View>
-              </View>
+              </Hoverable>
             )}
           </View>
         </Responsive>
-        <View style={[styles.menuActive, !this.state.mobileMenuActive && styles.hidden]}>
-          <View style={styles.mobileOpenContainer}>
-            <Footer isVertical={true} currentPage={this.props.router.pathname} />
+        {this.state.mobileMenuActive && (
+          <View style={styles.menuActive}>
+            <View style={styles.mobileOpenContainer}>
+              <Footer isVertical={true} currentPage={this.props.router.pathname} />
+            </View>
           </View>
-        </View>
+        )}
 
-        {!this.state.showDesktopMenu && !this.state.menuFaded && (
-          <View style={[styles.hamburger]}>
+        {this.willShowHamburger() && (
+          <View style={styles.hamburger}>
             <div
               className={`${cssStyles.hamburger} ${cssStyles['hamburger--squeeze']} ${
                 this.state.mobileMenuActive ? cssStyles['is-active'] : ''
@@ -304,7 +316,7 @@ export class Header extends React.PureComponent<Props, State> {
 
 const styles = StyleSheet.create({
   fadeTransition: {
-    transitionProperty: 'opacity',
+    transitionProperty: 'opacity color',
     transitionDuration: '300ms',
   },
   menuVisible: {
@@ -366,7 +378,7 @@ const styles = StyleSheet.create({
     cursor: 'pointer',
   },
   menuActive: {
-    position: 'absolute',
+    position: 'fixed',
     top: 0,
     left: 0,
     right: 0,
@@ -396,7 +408,7 @@ const styles = StyleSheet.create({
     marginRight: 50,
   },
   hamburger: {
-    position: 'absolute',
+    position: 'fixed',
     top: 5,
     right: 5,
   },
@@ -411,4 +423,4 @@ const styles = StyleSheet.create({
   },
 })
 
-export default withNamespaces('common')(withRouter<Props>(Header))
+export default withNamespaces('common')(withScreenSize(withRouter<Props>(Header)))
