4fa4f62f9bfab7b4fbd6f111388b9d853d4a53b6
Sami Mäkelä <mrsmkl@gmail.com>
2020-03-09 12:15:00 +0200

    savig

--- packages/metadata-crawler/src/crawler.ts
+++ packages/metadata-crawler/src/crawler.ts
@@ -1,13 +1,6 @@
-import {
-  ContractKit,
-  newKitFromWeb3,
-  CeloContract,
-  IdentityMetadataWrapper,
-} from '@celo/contractkit'
+import { IdentityMetadataWrapper } from '@celo/contractkit'
 import { Client } from 'pg'
-import Web3 from 'web3'
 
-const CRAWLER_WEB3 = process.env['CRAWLER_WEB3'] || 'http://localhost:8545'
 const CRAWLER_DATABASE = process.env['CRAWLER_DATABASE'] || 'blockscout'
 
 const client = new Client({ database: CRAWLER_DATABASE })
@@ -37,14 +30,6 @@ async function main() {
   for (let i of items) {
     handleItem(i)
   }
-
-  /*
-  const web3 = new Web3(CRAWLER_WEB3)
-  const kit: ContractKit = newKitFromWeb3(web3)
-  const token = await kit.contracts.getStableToken()
-  const oracle = await kit.contracts.getSortedOracles()
-  console.log(await oracle.medianRate(CeloContract.StableToken), token.address, oracle.address)
-  */
 }
 
 main()
