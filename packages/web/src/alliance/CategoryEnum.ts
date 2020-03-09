ba92bb0e1aa929b6f19202bae8b5e511b0163a80
Aaron DeRuvo <aaron.deruvo@gmail.com>
2020-03-09 12:13:12 -0700

    [web] ALLIANCE (Part B - node server side) (#3048)

---
+++ packages/web/src/alliance/CategoryEnum.ts
@@ -0,0 +1,14 @@
+export enum Category {
+  'Build' = 'Build',
+  'Earn' = 'Earn',
+  'Educate' = 'Educate',
+  'Give' = 'Give',
+  'Grow' = 'Grow',
+  'Insure' = 'Insure',
+  'Lend' = 'Lend',
+  'Pay' = 'Pay',
+  'Send' = 'Send',
+  'Save' = 'Save',
+  'Secure' = 'Secure',
+  'Validate' = 'Validate',
+}
