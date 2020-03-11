d3299d979cbce719207e68ca21b5ff582c3115e0
Aitor <1726644+aaitor@users.noreply.github.com>
2020-03-11 15:35:57 +0100

    adding viewDefinition dto

--- packages/contractkit/src/contract-cache.ts
+++ packages/contractkit/src/contract-cache.ts
@@ -6,7 +6,7 @@ import { BlockchainParametersWrapper } from './wrappers/BlockchainParameters'
 import { DoubleSigningSlasherWrapper } from './wrappers/DoubleSigningSlasher'
 import { DowntimeSlasherWrapper } from './wrappers/DowntimeSlasher'
 import { ElectionWrapper } from './wrappers/Election'
-// import { EpochRewardsWrapper } from './wrappers/EpochRewards'
+import { EpochRewardsWrapper } from './wrappers/EpochRewards'
 import { EscrowWrapper } from './wrappers/Escrow'
 import { ExchangeWrapper } from './wrappers/Exchange'
 import { GasPriceMinimumWrapper } from './wrappers/GasPriceMinimum'
@@ -25,7 +25,7 @@ const WrapperFactories = {
   [CeloContract.DoubleSigningSlasher]: DoubleSigningSlasherWrapper,
   [CeloContract.DowntimeSlasher]: DowntimeSlasherWrapper,
   [CeloContract.Election]: ElectionWrapper,
-  // [CeloContract.EpochRewards]?: EpochRewardsWrapper,
+  [CeloContract.EpochRewards]: EpochRewardsWrapper,
   [CeloContract.Escrow]: EscrowWrapper,
   [CeloContract.Exchange]: ExchangeWrapper,
   // [CeloContract.FeeCurrencyWhitelist]: FeeCurrencyWhitelistWrapper,
@@ -52,7 +52,7 @@ interface WrapperCacheMap {
   [CeloContract.DoubleSigningSlasher]?: DoubleSigningSlasherWrapper
   [CeloContract.DowntimeSlasher]?: DowntimeSlasherWrapper
   [CeloContract.Election]?: ElectionWrapper
-  // [CeloContract.EpochRewards]?: EpochRewardsWrapper
+  [CeloContract.EpochRewards]?: EpochRewardsWrapper
   [CeloContract.Escrow]?: EscrowWrapper
   [CeloContract.Exchange]?: ExchangeWrapper
   // [CeloContract.FeeCurrencyWhitelist]?: FeeCurrencyWhitelistWrapper,
@@ -98,9 +98,9 @@ export class WrapperCache {
   getElection() {
     return this.getContract(CeloContract.Election)
   }
-  // getEpochRewards() {
-  //   return this.getContract(CeloContract.EpochRewards)
-  // }
+  getEpochRewards() {
+    return this.getContract(CeloContract.EpochRewards)
+  }
   getEscrow() {
     return this.getContract(CeloContract.Escrow)
   }
