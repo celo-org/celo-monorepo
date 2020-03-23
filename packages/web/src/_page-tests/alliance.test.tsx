8ec0f956366d8b2045636c2b07355a8f9766e88e
aaron deruvo <aaron@celo.org>
2020-03-22 17:08:52 -0700

    add pages which fetch

--- packages/web/src/_page-tests/alliance.test.tsx
+++ packages/web/src/_page-tests/alliance.test.tsx
@@ -1,10 +1,17 @@
 import Alliance from 'pages/alliance'
 import * as React from 'react'
 import * as renderer from 'react-test-renderer'
+import { TestProvider } from 'src/_page-tests/test-utils'
 
 describe('Alliance', () => {
   it('renders', () => {
-    const tree = renderer.create(<Alliance />).toJSON()
+    const tree = renderer
+      .create(
+        <TestProvider>
+          <Alliance />
+        </TestProvider>
+      )
+      .toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
