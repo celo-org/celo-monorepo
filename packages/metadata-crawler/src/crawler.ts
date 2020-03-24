4723971c4cb51aa3ed15b4b6c5759183a948bf1a
Aitor <1726644+aaitor@users.noreply.github.com>
2020-03-24 11:08:46 +0100

    upgrading pg and fixing async

--- packages/metadata-crawler/src/crawler.ts
+++ packages/metadata-crawler/src/crawler.ts
@@ -23,6 +23,7 @@ const client = new Client({
 
 async function jsonQuery(query: string) {
   let res = await client.query(`SELECT json_agg(t) FROM (${query}) t`)
+  // .catch( error => console.error(`Error in jsonQuery: ${error}`))
   return res.rows[0].json_agg
 }
 
@@ -30,15 +31,18 @@ async function jsonQuery(query: string) {
 //   return ''
 // }
 
-async function addUrl(address: string, domain: string, verified: boolean) {
+async function addVerificationClaimToDatabase(address: string, domain: string, verified: boolean) {
   try {
     const query = `INSERT INTO celo_claims (address, type, element, verified, timestamp, inserted_at, updated_at) VALUES 
-        (decode('${address}', 'hex'), 'domain', '${domain}', ${verified}, now(), now(), now()) 
+        (decode($1, 'hex'), 'domain', $2, $3, now(), now(), now()) 
         ON CONFLICT (address, type, element) DO 
-        UPDATE SET verified=${verified}, timestamp=now(), updated_at=now() `
-    console.log(query)
-    await client.query(query)
-    console.log(`Verification flag added to domain ${domain} and address ${address}`)
+        UPDATE SET verified=$3, timestamp=now(), updated_at=now() `
+    const values = [address, domain, verified]
+
+    await client
+      .query(query, values)
+      .catch((error) => console.error(`Database error ${error}\n query: ${query}`))
+      .then(() => console.log(`Verification flag added to domain ${domain} and address ${address}`))
   } catch (err) {
     console.error('Error updating the database', err)
   }
@@ -48,55 +52,55 @@ async function handleItem(item: { url: string; address: string }) {
   try {
     let metadata = await IdentityMetadataWrapper.fetchFromURL(item.url)
     let claims = metadata.filterClaims(ClaimTypes.DOMAIN)
-    console.log('claims', claims)
-    console.log(item.address)
-    claims.map((claim) => {
-      //const alreadyVerified =
-      isClaimAlreadyVerified(item.address, claim.domain).then((alreadyVerified: boolean) => {
-        if (!alreadyVerified) {
-          console.log(`Verifying ${claim.domain} for address ${item.address}`)
-          verifyDomainRecord(
-            JSON.parse(metadata.toString()).meta.signature,
-            item.address,
-            claim.domain,
-            metadata
-          )
-            // verifyClaim(claim, item.address, urlGetter)
-            .then((verified) => {
-              console.log(`Claim = ${claim.domain}`)
-              if (verified === undefined) addUrl(item.address, claim.domain, true)
-              else console.error(verified)
-            })
-          // let verified = (await verifyClaim(claim, item.address, urlGetter)) === undefined
-          // addUrl(item.address, claim.domain, verified)
-        }
-      })
-    })
+
+    const numClaims = claims.length
+    for (let i = 0; i < numClaims; i++) {
+      const claim = claims[i]
+      const alreadyVerified = await isClaimAlreadyVerified(item.address, claim.domain)
+      if (!alreadyVerified) {
+        console.log(`Verifying ${claim.domain} for address ${item.address}`)
+        const verified = await verifyDomainRecord(
+          JSON.parse(metadata.toString()).meta.signature,
+          item.address,
+          claim.domain,
+          metadata
+        ).catch((error) => console.error(`Error in verifyDomainRecord ${error}`))
+        console.debug(`Claim = ${claim.domain}`)
+        if (verified === undefined)
+          await addVerificationClaimToDatabase(item.address, claim.domain, true)
+        else console.error(verified)
+      }
+    }
   } catch (err) {
     console.error('Cannot read metadata', err)
   }
 }
 
 async function isClaimAlreadyVerified(address: string, domain: string): Promise<boolean> {
-  jsonQuery(
-    `SELECT verified FROM celo_claims WHERE address=${address} AND 
-                domain=${domain} AND type='domain' AND timestamp = NOT NULL AND verified=true`
-  ).then((items) => {
-    items = items || []
-    if (items.length > 0) return true
-    return false
-  })
+  const query = `SELECT verified FROM celo_claims WHERE  address=decode('${address}', 'hex') AND
+                element='${domain}' AND type='domain' AND timestamp IS NOT NULL AND verified=true`
+  jsonQuery(query)
+    .catch((error) => console.error(`Error verifying claim in DB: ${error}\n${query}`))
+    .then((items) => {
+      items = items || []
+      if (items.length > 0) return true
+      return false
+    })
   return false
 }
 
 async function main() {
   console.debug('Connecting to: ' + PGHOST)
   await client.connect()
+
+  client.on('error', (e) => {
+    console.log(`Reconnecting after ${e}`)
+    client.connect()
+  })
+
   let items: { address: string; url: string }[] = await jsonQuery(
     `SELECT address, url FROM celo_account WHERE url is NOT NULL `
   )
-  // `SELECT a.address AS address, a.url AS url FROM celo_account AS a, celo_claims AS c WHERE a.address = c.address AND c.timestamp is NULL AND a.url is NOT NULL`
-  // `SELECT address, url FROM celo_account WHERE domain_timestamp is NULL AND url is NOT NULL `)
 
   items = items || []
   items = items.map((a) => ({ ...a, address: normalizeAddress(a.address.substr(2)) }))
