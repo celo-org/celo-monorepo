a3794dada40bbc0c9c7bcc6cc45472857db71048
aaron deruvo <aaron@celo.org>
2020-03-25 11:41:27 -0700

    fix fetch mocks

--- packages/web/src/_page-tests/alliance.test.tsx
+++ packages/web/src/_page-tests/alliance.test.tsx
@@ -1,7 +1,30 @@
+jest.mock('cross-fetch', () => {
+  return async () => ({
+    json: async () => ({
+      articles: [
+        { imgSource: 'test.jpg', href: '/', title: 'Alliance', text: 'Decentralize Together' },
+      ],
+    }),
+  })
+})
 import Alliance from 'pages/alliance'
 import * as React from 'react'
 import * as renderer from 'react-test-renderer'
+import { Category } from 'src/alliance/CategoryEnum'
 import { TestProvider } from 'src/_page-tests/test-utils'
+
+jest.mock('src/alliance/gatherAllies', () => {
+  return (callback) => {
+    callback(
+      Object.keys(Category).map((key: Category) => {
+        return {
+          name: key,
+          records: [],
+        }
+      })
+    )
+  }
+})
 
 describe('Alliance', () => {
   it('renders', () => {
