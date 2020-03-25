daee0bc56405f25e69c495ef90ef64808b348583
aaron deruvo <aaron@celo.org>
2020-03-25 11:48:21 -0700

    fix the fetch mock issue

--- packages/web/src/_page-tests/alliance.test.tsx
+++ packages/web/src/_page-tests/alliance.test.tsx
@@ -1,18 +1,16 @@
-jest.mock('cross-fetch', () => {
-  return async () => ({
-    json: async () => ({
-      articles: [
-        { imgSource: 'test.jpg', href: '/', title: 'Alliance', text: 'Decentralize Together' },
-      ],
-    }),
-  })
-})
 import Alliance from 'pages/alliance'
 import * as React from 'react'
 import * as renderer from 'react-test-renderer'
 import { Category } from 'src/alliance/CategoryEnum'
 import { TestProvider } from 'src/_page-tests/test-utils'
 
+jest.mock('cross-fetch', () => {
+  return async () => ({
+    json: async () => ({
+      articles: [],
+    }),
+  })
+})
 jest.mock('src/alliance/gatherAllies', () => {
   return (callback) => {
     callback(
