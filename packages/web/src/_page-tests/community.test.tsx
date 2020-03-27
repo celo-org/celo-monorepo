52250a00bd6d71f37da8c6679b5b8c2961bb3730
Aaron DeRuvo <aaron.deruvo@gmail.com>
2020-03-27 14:43:48 -0700

    Snapshot the website text (#3190)
    * snapshot now includes human words
    
    * test that translations work
    
    * add pages which fetch
    
    * fix fetch mocks
    
    * fix the fetch mock issue
    
    * linting
    
    Co-authored-by: aaron deruvo <aaron@celo.org>

--- packages/web/src/_page-tests/community.test.tsx
+++ packages/web/src/_page-tests/community.test.tsx
@@ -1,10 +1,25 @@
 import Community from 'pages/community'
 import * as React from 'react'
 import * as renderer from 'react-test-renderer'
+import { TestProvider } from 'src/_page-tests/test-utils'
+
+jest.mock('cross-fetch', () => {
+  return async () => ({
+    json: async () => ({
+      articles: [],
+    }),
+  })
+})
 
 describe('Community', () => {
   it('renders', () => {
-    const tree = renderer.create(<Community />).toJSON()
+    const tree = renderer
+      .create(
+        <TestProvider>
+          <Community />
+        </TestProvider>
+      )
+      .toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
--- packages/web/src/_page-tests/faq.test.tsx
+++ packages/web/src/_page-tests/faq.test.tsx
@@ -1,10 +1,17 @@
 import FAQ from 'pages/faq'
 import * as React from 'react'
 import * as renderer from 'react-test-renderer'
+import { TestProvider } from 'src/_page-tests/test-utils'
 
 describe('FAQ', () => {
   it('renders', () => {
-    const tree = renderer.create(<FAQ />).toJSON()
+    const tree = renderer
+      .create(
+        <TestProvider>
+          <FAQ />
+        </TestProvider>
+      )
+      .toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
