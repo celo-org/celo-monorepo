8b1d4f7bc1349782d44774f6c859b97e25d1c631
J M Rossy <jm.rossy@gmail.com>
2020-03-31 16:01:19 -0400

    Create a Models layer and address other feedback

--- packages/phone-number-privacy/migrations/20200330212224_create-accounts-table.ts
+++ packages/phone-number-privacy/migrations/20200330212224_create-accounts-table.ts
@@ -1,17 +1,17 @@
 import * as Knex from 'knex'
+import { ACCOUNTS_COLUMNS, ACCOUNTS_TABLE } from '../src/models/account'
 
 export async function up(knex: Knex): Promise<any> {
-  return knex.schema.createTable('accounts', (t) => {
-    t.increments('id')
-      .unsigned()
+  return knex.schema.createTable(ACCOUNTS_TABLE, (t) => {
+    t.string(ACCOUNTS_COLUMNS.address)
+      .notNullable()
       .primary()
-    t.dateTime('createdAt').notNullable()
-    t.string('address').notNullable()
-    t.integer('num_lookups').unsigned()
-    t.dateTime('did_matchmaking')
+    t.dateTime(ACCOUNTS_COLUMNS.createdAt).notNullable()
+    t.integer(ACCOUNTS_COLUMNS.numLookups).unsigned()
+    t.dateTime(ACCOUNTS_COLUMNS.didMatchmaking)
   })
 }
 
 export async function down(knex: Knex): Promise<any> {
-  return knex.schema.dropTable('accounts')
+  return knex.schema.dropTable(ACCOUNTS_TABLE)
 }
