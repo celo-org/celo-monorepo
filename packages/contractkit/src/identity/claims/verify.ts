b36cc01671bb789a4638f65bdd1428123f341aea
Aitor <1726644+aaitor@users.noreply.github.com>
2020-04-01 18:14:14 +0200

    linting

--- packages/contractkit/src/identity/claims/verify.ts
+++ packages/contractkit/src/identity/claims/verify.ts
@@ -5,7 +5,7 @@ import { resolveTxt } from 'dns'
 import { Address } from '../../base'
 import { IdentityMetadataWrapper } from '../metadata'
 import { AccountClaim } from './account'
-import { Claim, DomainClaim, DOMAIN_TXT_HEADER, serializeClaim } from './claim'
+import { Claim, DOMAIN_TXT_HEADER, DomainClaim, serializeClaim } from './claim'
 import { verifyKeybaseClaim } from './keybase'
 import { ClaimTypes } from './types'
 
