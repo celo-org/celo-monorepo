31213b413f176bcedcf8b404d790279dbf155ce0
aaron deruvo <aaron@celo.org>
2020-04-06 11:17:20 -0700

    fix tests

--- packages/web/src/_page-tests/experience/icons.test.tsx
+++ packages/web/src/_page-tests/experience/icons.test.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react'
 import * as renderer from 'react-test-renderer'
-
+import { IconData } from 'src/brandkit/IconsPage'
 import Icons from '../../../pages/experience/brand/icons'
+
+const ICONS: IconData[] = [
+  {
+    name: 'Tree',
+    description: 'Organic Technology',
+    preview: 'icon.png',
+    uri: 'example.com/tree-icon.png',
+  },
+  {
+    name: 'Forest',
+    description: 'Ecological Technology',
+    preview: 'icon.png',
+    uri: 'example.com/forest-icon.png',
+  },
+]
 
 describe('Experience/Icons', () => {
   it('renders', () => {
-    const tree = renderer.create(<Icons />).toJSON()
+    const tree = renderer.create(<Icons icons={ICONS} />).toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
