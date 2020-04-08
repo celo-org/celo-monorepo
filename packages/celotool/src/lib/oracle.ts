f8d2ed8c3fb2357a79b4d889f1a28c95762df320
Trevor Porter <trkporter@ucdavis.edu>
2020-04-08 20:02:02 +0100

    Add MVP private key handling

--- packages/celotool/src/lib/oracle.ts
+++ packages/celotool/src/lib/oracle.ts
@@ -1,4 +1,5 @@
 import { envVar, fetchEnv } from 'src/lib/env-utils'
+import { AccountType, getPrivateKeysFor } from 'src/lib/generate_utils'
 import { installGenericHelmChart, removeGenericHelmChart } from 'src/lib/helm_deploy'
 
 const helmChartPath = '../helm-charts/oracle'
@@ -17,12 +18,19 @@ export async function removeHelmRelease(celoEnv: string) {
 }
 
 async function helmParameters(celoEnv: string) {
+  const replicas = parseInt(fetchEnv(envVar.ORACLES), 10)
+  const oraclePrivateKeys = getPrivateKeysFor(
+    AccountType.PRICE_ORACLE,
+    fetchEnv(envVar.MNEMONIC),
+    replicas
+  ).map((pkey) => `0x${pkey}`)
   return [
     `--set environmentName=${celoEnv}`,
     `--set replicas=${fetchEnv(envVar.ORACLES)}`,
     `--set image.repository=${fetchEnv(envVar.ORACLE_DOCKER_IMAGE_REPOSITORY)}`,
     `--set image.tag=${fetchEnv(envVar.ORACLE_DOCKER_IMAGE_TAG)}`,
-    `--set oracle.web3ProviderUrl=http://localhost:8545`,
+    `--set oracle.web3ProviderUrl=https://alfajoresstaging-forno.celo-testnet.org`,
+    `--set oracle.privateKeys={${oraclePrivateKeys.join(',')}}`,
   ]
 }
 
