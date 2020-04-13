c6afa045645f9fe4411643a33821182512e897d7
Asa Oines <a@celo.org>
2020-04-13 18:39:14 -0400

    Prettify

--- packages/protocol/migrations/04_goldtoken.ts
+++ packages/protocol/migrations/04_goldtoken.ts
@@ -1,6 +1,9 @@
 /* tslint:disable:no-console */
 import { CeloContractName } from '@celo/protocol/lib/registry-utils'
-import { deploymentForCoreContract, getDeployedProxiedContract } from '@celo/protocol/lib/web3-utils'
+import {
+  deploymentForCoreContract,
+  getDeployedProxiedContract,
+} from '@celo/protocol/lib/web3-utils'
 import { config } from '@celo/protocol/migrationsConfig'
 import { FreezerInstance, GoldTokenInstance } from 'types'
 
