260e4bf63cb1afff92b659d8c1a1ad657daac3f1
Amy Slawson <amyslawson@MacBook-Pro.local>
2020-04-16 11:37:21 -0700

    used await rather than promise callbacks, removed knex variable

--- packages/phone-number-privacy/src/database/wrappers/number-pairs.ts
+++ packages/phone-number-privacy/src/database/wrappers/number-pairs.ts
@@ -1,8 +1,6 @@
 import { getDatabase } from '../database'
 import { NUMBER_PAIRS_COLUMN, NUMBER_PAIRS_TABLE } from '../models/numberPair'
 
-const knex = getDatabase()
-
 /*
  * Returns contacts who have already matched with the user (a contact-->user mapping exists).
  */
@@ -10,14 +8,13 @@ export async function getNumberPairContacts(
   userPhone: string,
   contactPhones: string[]
 ): Promise<string[]> {
-  return knex(NUMBER_PAIRS_TABLE)
+  const contentPairs = await getDatabase()(NUMBER_PAIRS_TABLE)
     .select(NUMBER_PAIRS_COLUMN.userPhoneHash)
     .where(NUMBER_PAIRS_COLUMN.contactPhoneHash, userPhone)
-    .then((contactPairs) =>
-      contactPairs
-        .map((contractPair) => contractPair[NUMBER_PAIRS_COLUMN.userPhoneHash])
-        .filter((number) => contactPhones.includes(number))
-    )
+
+  return contentPairs
+    .map((contractPair) => contractPair[NUMBER_PAIRS_COLUMN.userPhoneHash])
+    .filter((number) => contactPhones.includes(number))
 }
 
 /*
@@ -35,14 +32,13 @@ export async function setNumberPairContacts(
     }
     rows.push(data)
   }
-
-  return knex
-    .batchInsert(NUMBER_PAIRS_TABLE, rows)
-    .then(() => console.log('successful batch insertion'))
-    .catch((e) => {
-      if (e.code !== '23505') {
-        // ignore duplicate insertion error (23505)
-        console.error('error batch inserting number-paid', e)
-      }
-    }) // TODO (amyslawson) handle error
+  try {
+    await getDatabase().batchInsert(NUMBER_PAIRS_TABLE, rows)
+  } catch (e) {
+    // TODO (amyslawson) handle error
+    if (e.code !== '23505') {
+      // ignore duplicate insertion error (23505)
+      console.error('error batch inserting number-paid', e)
+    }
+  }
 }
