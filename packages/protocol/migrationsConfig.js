55fd2b690d00af7fc42e5878de81057d26b18203
Asa Oines <a@celo.org>
2020-04-21 14:05:58 -0400

    change address

--- packages/protocol/migrationsConfig.js
+++ packages/protocol/migrationsConfig.js
@@ -165,7 +165,7 @@ const DefaultConfig = {
     inflationRate: 1,
     inflationPeriod: 1.5 * YEAR,
     initialBalances: {
-      addresses: ['0x11901cf7eEae1E2644995FB2E47Ce46bC7F33246'],
+      addresses: ['0x989e1a3B344A43911e02cCC609D469fbc15AB1F1'],
       values: ['5000000000000000000000000'],
     },
     // TODO(asa): Needs updating before RC1 deployment
