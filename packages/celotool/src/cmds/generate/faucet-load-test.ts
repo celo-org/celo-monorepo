e478942a98f5bd40906a0869289aa38c8b7b86d3
Javier Cortejoso <javier.cortejoso@gmail.com>
2020-04-21 12:46:09 +0200

    Faucet loadtest using portforward

--- packages/celotool/src/cmds/generate/faucet-load-test.ts
+++ packages/celotool/src/cmds/generate/faucet-load-test.ts
@@ -1,11 +1,13 @@
 /* tslint:disable no-console */
 import { newKit } from '@celo/contractkit'
+import { switchToClusterFromEnv } from 'src/lib/cluster'
 import { convertToContractDecimals } from 'src/lib/contract-utils'
+import { CeloEnvArgv, envVar, fetchEnv } from 'src/lib/env-utils'
 import { AccountType, generateAddress } from 'src/lib/generate_utils'
+import { portForwardAnd } from 'src/lib/port_forward'
 import yargs from 'yargs'
 
-interface faucetLoadTest {
-  mnemonic: string
+interface FaucetLoadTest extends CeloEnvArgv {
   count_from: number
   gold: number
   dollars: number
@@ -20,12 +22,6 @@ export const describe = 'command for fauceting the addresses used for load testi
 
 export const builder = (argv: yargs.Argv) => {
   return argv
-    .option('mnemonic', {
-      type: 'string',
-      description: 'BIP-39 mnemonic',
-      demandOption: 'Please specify a mnemonic from which to derive a public key',
-      alias: 'm',
-    })
     .option('gold', {
       type: 'number',
       description: 'Celo Gold amount to transfer',
@@ -64,36 +60,47 @@ export const builder = (argv: yargs.Argv) => {
     })
 }
 
-export const handler = async (argv: faucetLoadTest) => {
+export const handler = async (argv: FaucetLoadTest) => {
+  await switchToClusterFromEnv()
   const accountType = AccountType.LOAD_TESTING_ACCOUNT
+  const mnemonic = fetchEnv(envVar.MNEMONIC)
 
-  const kit = newKit('http://localhost:8545')
-  const account = (await kit.web3.eth.getAccounts())[0]
-  console.log(`Using account: ${account}`)
-  kit.defaultAccount = account
+  const cb = async () => {
+    const kit = newKit('http://localhost:8545')
+    const account = (await kit.web3.eth.getAccounts())[0]
+    console.log(`Using account: ${account}`)
+    kit.defaultAccount = account
 
-  const [goldToken, stableToken, reserve] = await Promise.all([
-    kit.contracts.getGoldToken(),
-    kit.contracts.getStableToken(),
-    kit.contracts.getReserve(),
-  ])
-  const goldAmount = await convertToContractDecimals(argv.gold, goldToken)
-  const stableTokenAmount = await convertToContractDecimals(argv.dollars, stableToken)
+    const [goldToken, stableToken, reserve] = await Promise.all([
+      kit.contracts.getGoldToken(),
+      kit.contracts.getStableToken(),
+      kit.contracts.getReserve(),
+    ])
+    const goldAmount = await convertToContractDecimals(argv.gold, goldToken)
+    const stableTokenAmount = await convertToContractDecimals(argv.dollars, stableToken)
 
-  for (let i = argv.count_from; i <= argv.count_to; i++) {
-    for (let t = argv.threads_from; t <= argv.threads_to; t++) {
-      const index = parseInt(`${i}${t}`, 10)
-      const address = generateAddress(argv.mnemonic, accountType, index)
-      console.log(
-        `${index} --> Fauceting ${goldAmount.toFixed()} Gold and ${stableTokenAmount.toFixed()} StableToken to ${address}`
-      )
-      if (await reserve.isSpender(account)) {
-        await reserve.transferGold(address, goldAmount.toFixed()).send()
-      } else {
-        await goldToken.transfer(address, goldAmount.toFixed()).send()
+    for (let i = argv.count_from; i <= argv.count_to; i++) {
+      for (let t = argv.threads_from; t <= argv.threads_to; t++) {
+        const index = parseInt(`${i}${t}`, 10)
+        const address = generateAddress(mnemonic, accountType, index)
+        console.log(
+          `${index} --> Fauceting ${goldAmount.toFixed()} Gold and ${stableTokenAmount.toFixed()} StableToken to ${address}`
+        )
+        if (await reserve.isSpender(account)) {
+          await reserve.transferGold(address, goldAmount.toFixed()).send()
+        } else {
+          await goldToken.transfer(address, goldAmount.toFixed()).send()
+        }
+        await stableToken.transfer(address, stableTokenAmount.toFixed()).send()
       }
-      await stableToken.transfer(address, stableTokenAmount.toFixed()).send()
     }
-    // await sleep(100)
+  }
+
+  try {
+    await portForwardAnd(argv.celoEnv, cb)
+  } catch (error) {
+    console.error(`Unable to faucet load-test accounts on ${argv.celoEnv}`)
+    console.error(error)
+    process.exit(1)
   }
 }
