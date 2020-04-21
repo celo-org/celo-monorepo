d3ca5c4e3c5cf64e20755be4f8209a32b5da236d
Lucas Ege <lucas@celo.org>
2020-04-21 10:37:57 -0700

    lint

--- packages/protocol/scripts/truffle/deploy_release_contracts.ts
+++ packages/protocol/scripts/truffle/deploy_release_contracts.ts
@@ -1,6 +1,7 @@
 import { _setInitialProxyImplementation } from '@celo/protocol/lib/web3-utils'
 import BigNumber from 'bignumber.js'
 import chalk from 'chalk'
+import fs = require('fs')
 import * as prompts from 'prompts'
 import {
   ReleaseGoldContract,
@@ -8,7 +9,6 @@ import {
   ReleaseGoldMultiSigProxyContract,
   ReleaseGoldProxyContract,
 } from 'types'
-import fs = require('fs')
 
 let argv: any
 let releases: any
