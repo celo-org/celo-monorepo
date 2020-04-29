bb1e7bc3b9ea72b476bd0d235056f42b06bbfeb3
Amy Slawson <aslawson@users.noreply.github.com>
2020-04-29 13:48:39 -0700

    [phone-number-privacy] min balance for unverified salt callers, require verification for matchmake
    ### Description
    
    - add min balance 10 cents for unverified callers for salt query to receive quota of 2
    - add requirement for phone number verification prior to match making request
    - make caller phone number hash param for salt optional (for pre-attestation case where caller needs their own salt)
    
    ### Other changes
    
    NA
    
    ### Tested
    
    Updated tests for these cases
    
    ### Related issues
    
    - https://github.com/celo-org/celo-labs/issues/448
    
    ### Backwards compatibility
    
    NA

--- packages/phone-number-privacy/test/salt-generation/query-quota.test.ts
+++ packages/phone-number-privacy/test/salt-generation/query-quota.test.ts
@@ -1,3 +1,5 @@
+import BigNumber from 'bignumber.js'
+import { isVerified } from '../../src/common/identity'
 import { getPerformedQueryCount } from '../../src/database/wrappers/account'
 import { getRemainingQueryCount } from '../../src/salt-generation/query-quota'
 import { getContractKit } from '../../src/web3/contracts'
@@ -5,6 +7,7 @@ import {
   ContractRetrieval,
   createMockAttestation,
   createMockContractKit,
+  createMockStableToken,
   createMockWeb3,
 } from '../utils'
 import { mockAccount, mockPhoneNumber } from '../values'
@@ -13,16 +16,22 @@ jest.mock('../../src/web3/contracts')
 const mockGetContractKit = getContractKit as jest.Mock
 jest.mock('../../src/database/wrappers/account')
 const mockPerformedQueryCount = getPerformedQueryCount as jest.Mock
+jest.mock('../../src/common/identity')
+const mockIsVerified = isVerified as jest.Mock
 
 describe(getRemainingQueryCount, () => {
   it('Calculates remaining query count for verified account', async () => {
     const contractKitVerifiedNoTx = createMockContractKit(
       {
         [ContractRetrieval.getAttestations]: createMockAttestation(3, 3),
+        [ContractRetrieval.getStableToken]: createMockStableToken(
+          new BigNumber(200000000000000000)
+        ),
       },
       createMockWeb3(5)
     )
     mockPerformedQueryCount.mockImplementation(() => new Promise((resolve) => resolve(2)))
+    mockIsVerified.mockReturnValue(true)
     mockGetContractKit.mockImplementation(() => contractKitVerifiedNoTx)
     expect(await getRemainingQueryCount(mockAccount, mockPhoneNumber)).toEqual(40)
   })
@@ -30,10 +39,14 @@ describe(getRemainingQueryCount, () => {
     const contractKitVerifiedNoTx = createMockContractKit(
       {
         [ContractRetrieval.getAttestations]: createMockAttestation(0, 0),
+        [ContractRetrieval.getStableToken]: createMockStableToken(
+          new BigNumber(200000000000000000)
+        ),
       },
       createMockWeb3(0)
     )
     mockPerformedQueryCount.mockImplementation(() => new Promise((resolve) => resolve(1)))
+    mockIsVerified.mockReturnValue(false)
     mockGetContractKit.mockImplementation(() => contractKitVerifiedNoTx)
     expect(await getRemainingQueryCount(mockAccount, mockPhoneNumber)).toEqual(1)
   })
@@ -41,10 +54,14 @@ describe(getRemainingQueryCount, () => {
     const contractKitVerifiedNoTx = createMockContractKit(
       {
         [ContractRetrieval.getAttestations]: createMockAttestation(3, 3),
+        [ContractRetrieval.getStableToken]: createMockStableToken(
+          new BigNumber(200000000000000000)
+        ),
       },
       createMockWeb3(100)
     )
     mockPerformedQueryCount.mockImplementation(() => new Promise((resolve) => resolve(10)))
+    mockIsVerified.mockReturnValue(true)
     mockGetContractKit.mockImplementation(() => contractKitVerifiedNoTx)
     expect(await getRemainingQueryCount(mockAccount, mockPhoneNumber)).toEqual(222)
   })
@@ -52,11 +69,42 @@ describe(getRemainingQueryCount, () => {
     const contractKitVerifiedNoTx = createMockContractKit(
       {
         [ContractRetrieval.getAttestations]: createMockAttestation(0, 0),
+        [ContractRetrieval.getStableToken]: createMockStableToken(
+          new BigNumber(200000000000000000)
+        ),
+      },
+      createMockWeb3(100)
+    )
+    mockPerformedQueryCount.mockImplementation(() => new Promise((resolve) => resolve(0)))
+    mockIsVerified.mockReturnValue(false)
+    mockGetContractKit.mockImplementation(() => contractKitVerifiedNoTx)
+    expect(await getRemainingQueryCount(mockAccount, mockPhoneNumber)).toEqual(2)
+  })
+  it('Calculates remaining query count for unverified account without any balance', async () => {
+    const contractKitVerifiedNoTx = createMockContractKit(
+      {
+        [ContractRetrieval.getAttestations]: createMockAttestation(0, 0),
+        [ContractRetrieval.getStableToken]: createMockStableToken(new BigNumber(0)),
+      },
+      createMockWeb3(100)
+    )
+    mockPerformedQueryCount.mockImplementation(() => new Promise((resolve) => resolve(0)))
+    mockIsVerified.mockReturnValue(false)
+    mockGetContractKit.mockImplementation(() => contractKitVerifiedNoTx)
+    expect(await getRemainingQueryCount(mockAccount, mockPhoneNumber)).toEqual(0)
+  })
+  it('No phone number hash when request own phone number', async () => {
+    const contractKitVerifiedNoTx = createMockContractKit(
+      {
+        [ContractRetrieval.getAttestations]: createMockAttestation(0, 0),
+        [ContractRetrieval.getStableToken]: createMockStableToken(
+          new BigNumber(200000000000000000)
+        ),
       },
       createMockWeb3(100)
     )
     mockPerformedQueryCount.mockImplementation(() => new Promise((resolve) => resolve(0)))
     mockGetContractKit.mockImplementation(() => contractKitVerifiedNoTx)
-    expect(await getRemainingQueryCount(mockAccount, mockPhoneNumber)).toEqual(2)
+    expect(await getRemainingQueryCount(mockAccount, undefined)).toEqual(2)
   })
 })
