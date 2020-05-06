b9b77c0ec007b0a56e9d94d09d6a32c6cc16b6d2
aaron deruvo <aaron@celo.org>
2020-05-06 13:17:31 -0700

    remove unused thing

--- packages/web/src/_page-tests/experience/exchange-icons.test.tsx
+++ packages/web/src/_page-tests/experience/exchange-icons.test.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import * as renderer from 'react-test-renderer'
-import { IconData } from 'src/brandkit/IconsPage'
 import ExchangeIcons from '../../../pages/experience/brand/exchange-icons'
 
 describe('Experience/ExchangeIcons', () => {
