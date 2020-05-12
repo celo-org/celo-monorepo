4eb9baf650e86715de79936628b0dc89d7ff37ee
Alex Harley <alexbruceharley@gmail.com>
2020-05-12 11:17:48 +0200

    chore: small logging update

--- scripts/ci_check_if_test_should_run_v2.sh
+++ scripts/ci_check_if_test_should_run_v2.sh
@@ -11,7 +11,7 @@ cd packages/mobile
 CHANGED=$(node -r ts-node/register ../../scripts/check_if_test_should_run_v2.ts --packages ${PACKAGES_TO_CHECK})
 cd -
 if [ $CHANGED = false ] ; then
-  echo "No changes in ${1} - skipping  testing"
+  echo "No changes in ${1} or dependent packages - skipping testing"
   # https://discuss.circleci.com/t/ability-to-return-successfully-from-a-job-before-completing-all-the-next-steps/12969/6
   circleci step halt
   exit 0
