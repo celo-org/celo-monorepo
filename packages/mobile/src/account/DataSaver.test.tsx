553ceeb77dce2cf5d6dc516d89e98b70d14765c1
Connor McEwen <connor.mcewen@gmail.com>
2020-05-13 01:23:03 -0700

    More tests

--- packages/mobile/src/account/DataSaver.test.tsx
+++ packages/mobile/src/account/DataSaver.test.tsx
@@ -3,14 +3,23 @@ import 'react-native'
 import { Provider } from 'react-redux'
 import * as renderer from 'react-test-renderer'
 import DataSaver from 'src/account/DataSaver'
+import { Screens } from 'src/navigator/Screens'
 import { createMockStore } from 'test/utils'
 import { mockNavigation } from 'test/values'
+
+const mockRoute = {
+  name: Screens.DataSaver as Screens.DataSaver,
+  key: '1',
+  params: {
+    promptModalVisible: false,
+  },
+}
 
 describe('DataSaver', () => {
   it('renders correctly', () => {
     const tree = renderer.create(
       <Provider store={createMockStore({})}>
-        <DataSaver navigation={mockNavigation} />
+        <DataSaver navigation={mockNavigation} route={mockRoute} />
       </Provider>
     )
     expect(tree).toMatchSnapshot()
--- packages/mobile/src/language/Language.test.tsx
+++ packages/mobile/src/language/Language.test.tsx
@@ -3,13 +3,25 @@ import 'react-native'
 import { Provider } from 'react-redux'
 import * as renderer from 'react-test-renderer'
 import Language from 'src/language/Language'
-import MockedNavigator from 'test/MockedNavigator'
 import { createMockStore } from 'test/utils'
+
+jest.mock('@react-navigation/native', () => {
+  const { mockNavigation } = require('test/values')
+  const { Screens } = require('src/navigator/Screens')
+  return {
+    useNavigation: () => mockNavigation,
+    useRoute: () => ({
+      name: Screens.Language,
+      key: '1',
+      params: {},
+    }),
+  }
+})
 
 it('renders correctly', () => {
   const tree = renderer.create(
     <Provider store={createMockStore()}>
-      <MockedNavigator component={Language} />
+      <Language />
     </Provider>
   )
   expect(tree).toMatchSnapshot()
