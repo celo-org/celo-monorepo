37c0e657ac63aa38cd35d17e89c32debe80afbec
annakaz <annakaz@users.noreply.github.com>
2020-05-14 10:21:48 -0700

    Update verificationFailedModal with option to retry

--- packages/mobile/src/verify/VerificationFailedModal.tsx
+++ packages/mobile/src/verify/VerificationFailedModal.tsx
@@ -1,11 +1,6 @@
-import TextButton from '@celo/react-components/components/TextButton'
-import colors from '@celo/react-components/styles/colors'
-import fontStyles from '@celo/react-components/styles/fonts'
-import { componentStyles } from '@celo/react-components/styles/styles'
 import * as React from 'react'
 import { useTranslation } from 'react-i18next'
-import { StyleSheet, Text, View } from 'react-native'
-import Modal from 'react-native-modal'
+import { WarningModal } from 'src/components/WarningModal'
 import { Namespaces } from 'src/i18n'
 import { cancelVerification } from 'src/identity/actions'
 import { VerificationStatus } from 'src/identity/verification'
@@ -29,85 +24,40 @@ export function VerificationFailedModal(props: Props) {
     navigateHome()
   }, [props.cancelVerification])
 
+  const onRetry = () => {
+    // TODO retry verification
+  }
+
   const isVisible =
     props.verificationStatus === VerificationStatus.Failed ||
     (props.verificationStatus === VerificationStatus.RevealAttemptFailed && !isDismissed)
 
-  const showRetryWithForno = props.verificationStatus === VerificationStatus.Failed
+  // TODO may need to check
+  const showRetryWithForno = props.verificationStatus === VerificationStatus.Failed // TODO some check to only show modal once
 
   const allowEnterCodes = props.verificationStatus === VerificationStatus.RevealAttemptFailed
 
-  return (
-    <Modal isVisible={isVisible}>
-      <View style={styles.modalContainer}>
-        <Text style={styles.modalHeader}>
-          {showRetryWithForno ? t('retryWithFornoModal.header') : t('failModal.header')}
-        </Text>
-        <Text style={fontStyles.body}>
-          {showRetryWithForno ? t('retryWithFornoModal.body1') : t('failModal.body1')}
-        </Text>
-        <Text style={[fontStyles.body, componentStyles.marginTop10]}>
-          {showRetryWithForno
-            ? t('retryWithFornoModal.body2')
-            : t('failModal.body2') + (allowEnterCodes ? t('failModal.enterCodesBody') : '')}
-        </Text>
-        <View style={styles.modalButtonsContainer}>
-          {allowEnterCodes && (
-            <TextButton onPress={onDismiss} style={styles.modalSkipText}>
-              {t('failModal.enterCodesButton')}
-            </TextButton>
-          )}
-          <TextButton onPress={onSkip} style={styles.modalSkipText}>
-            {t('missingCodesModal.skip')}
-          </TextButton>
-        </View>
-      </View>
-    </Modal>
+  return showRetryWithForno ? (
+    <WarningModal
+      isVisible={isVisible}
+      header={t('retryWithFornoModal.header')}
+      body1={t('retryWithFornoModal.body1')}
+      body2={t('retryWithFornoModal.body2')}
+      continueTitle={t('retryWithFornoModal.retryButton')}
+      cancelTitle={t('skip')} // TODO(anna) make sure translation works
+      onCancel={onSkip}
+      onContinue={onRetry}
+    />
+  ) : (
+    <WarningModal
+      isVisible={isVisible}
+      header={t('failModal.header')}
+      body1={t('promptFornoModal.body')}
+      body2={allowEnterCodes ? t('failModal.enterCodesBody') : undefined}
+      continueTitle={t('promptFornoModal.switchToDataSaver')}
+      cancelTitle={t('global:goBack')}
+      onCancel={allowEnterCodes ? onDismiss : undefined}
+      onContinue={onSkip}
+    />
   )
 }
-
-function RetryVerificationWithFornoModal(props: Props) {
-  const test = 'hi'
-  return (
-    <>
-      <Text style={styles.modalHeader}>{t('retryWithFornoModal.header')}</Text>
-      <Text style={fontStyles.body}>{t('retryWithFornoModal.body1')}</Text>
-      <Text style={[fontStyles.body, componentStyles.marginTop10]}>
-        {t('retryWithFornoModal.body2')}
-      </Text>
-      <View style={styles.modalButtonsContainer}>
-        <TextButton onPress={onSkip} style={styles.modalSkipText}>
-          {t('missingCodesModal.skip')}
-        </TextButton>
-        <TextButton onPress={onRetry} style={styles.modalSkipText}>
-          {t('retryWithFornoModal.retryButton')}
-        </TextButton>
-      </View>
-    </>
-  )
-}
-
-const styles = StyleSheet.create({
-  modalContainer: {
-    backgroundColor: colors.background,
-    padding: 20,
-    marginHorizontal: 10,
-    borderRadius: 4,
-  },
-  modalHeader: {
-    ...fontStyles.h2,
-    ...fontStyles.bold,
-    marginVertical: 15,
-    color: colors.errorRed,
-  },
-  modalButtonsContainer: {
-    marginTop: 25,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-  },
-  modalSkipText: {
-    ...fontStyles.body,
-    ...fontStyles.semiBold,
-  },
-})
