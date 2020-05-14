51f934d76201ac14073d66775e9a2673875eb782
Ivan Sorokin <ivan@sorokin.io>
2020-05-14 14:40:15 +0200

    PR comments

--- packages/mobile/src/components/BackButton.v2.tsx
+++ packages/mobile/src/components/BackButton.v2.tsx
@@ -1,8 +1,33 @@
-import BackButton from '@celo/react-components/components/BackButton.v2'
-import React, { useCallback } from 'react'
+import BackChevron, { Props as BackChevronProps } from '@celo/react-components/icons/BackChevron.v2'
+import variables from '@celo/react-components/styles/variables'
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
 import { navigateBack } from 'src/navigator/NavigationService'
+import { TopBarIconButton, TopBarIconButtonProps } from 'src/navigator/TopBarButton.v2'
 
-export default function MobileBackButton(props: { height?: number; color?: string }) {
-  const goBack = useCallback(() => navigateBack(), [navigateBack])
-  return <BackButton onPress={goBack} {...props} />
+type Props = Omit<TopBarIconButtonProps, 'icon'> & BackChevronProps
+
+function BackButton(props: Props) {
+  return (
+    <View style={styles.container}>
+      <TopBarIconButton
+        {...props}
+        icon={<BackChevron color={props.color} height={props.height} />}
+      />
+    </View>
+  )
 }
+
+BackButton.defaultProps = {
+  onPress: navigateBack,
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingLeft: variables.contentPadding + 6, // 6px from the left padding
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
+
+export default BackButton
--- packages/mobile/src/navigator/TopBarButton.v2.test.tsx
+++ packages/mobile/src/navigator/TopBarButton.v2.test.tsx
@@ -1,11 +1,44 @@
 import * as React from 'react'
-import 'react-native'
-import { render } from 'react-native-testing-library'
-import TopBarButton from 'src/navigator/TopBarButton.v2'
+import { Text } from 'react-native'
+import { fireEvent, render } from 'react-native-testing-library'
+import { TopBarIconButton, TopBarTextButton } from 'src/navigator/TopBarButton.v2'
 
-describe('TopBarButton', () => {
-  it('displays children', () => {
-    const { queryByText } = render(<TopBarButton title={'label'} />)
+const testID = 'button'
+
+describe('TopBarTextButton', () => {
+  it('renders with minimum props', () => {
+    const onPress = jest.fn()
+    const { queryByText } = render(
+      <TopBarTextButton testID={testID} title={'label'} onPress={onPress} />
+    )
     expect(queryByText('label')?.props.children).toEqual('label')
+  })
+
+  it('fires an event when pressed', () => {
+    const onPress = jest.fn()
+    const { queryByTestId } = render(
+      <TopBarTextButton testID={testID} title={'label'} onPress={onPress} />
+    )
+    fireEvent.press(queryByTestId(testID)!)
+    expect(onPress).toBeCalled()
+  })
+})
+
+describe('TopBarIconButton', () => {
+  it('renders with minimum props', () => {
+    const onPress = jest.fn()
+    const { queryByText } = render(
+      <TopBarIconButton testID={testID} icon={<Text>icon</Text>} onPress={onPress} />
+    )
+    expect(queryByText('icon')?.props.children).toEqual('icon')
+  })
+
+  it('fires an event when pressed', () => {
+    const onPress = jest.fn()
+    const { queryByTestId } = render(
+      <TopBarIconButton testID={testID} icon={<Text>icon</Text>} onPress={onPress} />
+    )
+    fireEvent.press(queryByTestId(testID)!)
+    expect(onPress).toBeCalled()
   })
 })
