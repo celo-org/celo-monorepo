c583d72ab0dc6754526ae90a9941ac94b17499dc
Victor Graf <victor@celo.org>
2020-05-15 17:03:59 -0700

    Add local flag to account:list command to avoid needing an external node

--- packages/cli/src/commands/account/list.ts
+++ packages/cli/src/commands/account/list.ts
@@ -1,4 +1,5 @@
 import { CeloProvider } from '@celo/contractkit/lib'
+import { flags } from '@oclif/command'
 import { ParserOutput } from '@oclif/parser/lib/parse'
 import { toChecksumAddress } from 'ethereumjs-util'
 import { BaseCommand } from '../../base'
@@ -8,24 +9,32 @@ export default class AccountList extends BaseCommand {
 
   static flags = {
     ...BaseCommand.flags,
+    local: flags.boolean({
+      default: false,
+      description: 'If set, only show local and hardware wallet accounts',
+    }),
   }
 
   requireSynced = false
 
   async run() {
-    this.parse(AccountList)
+    const res = this.parse(AccountList)
 
     const celoProvider: CeloProvider = this.kit.web3.currentProvider as any
-    const addresses = await this.kit.web3.eth.getAccounts()
+
+    // Retreive accounts from the connected Celo node.
+    const addresses = []
+    if (res.flags.local) {
+      addresses.push(...(await this.kit.web3.eth.getAccounts()))
+    }
+
+    // Get addresses from the local wallet.
     const localAddresses = celoProvider.wallet
       .getAccounts()
       .map((value) => toChecksumAddress(value))
-    let localName = 'Local'
-    const res: ParserOutput<any, any> = this.parse()
 
-    if (res.flags.useLedger) {
-      localName = 'Ledger'
-    }
+    // Display the addresses.
+    const localName = res.flags.useLedger ? 'Ledger' : 'Local'
     console.log('All Addresses: ', addresses)
     console.log(
       'Keystore Addresses: ',
