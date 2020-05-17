5cd6c88a12dfc6dbb034962b43006eda72cbf70d
yorhodes <yorhodes@celo.org>
2020-05-16 17:43:52 -0700

    Update geth signer with unlock features

--- packages/contractkit/src/wallets/geth-wallet.ts
+++ packages/contractkit/src/wallets/geth-wallet.ts
@@ -1,30 +1,105 @@
-import { normalizeAddressWith0x, privateKeyToAddress } from '@celo/utils/lib/address'
-import { provider } from 'web3-core'
+import { ensureLeading0x, privateKeyToAddress } from '@celo/utils/lib/address'
+// @ts-ignore-next-line
+import { account as Account } from 'eth-lib'
+import * as ethUtil from 'ethereumjs-util'
+import { EncodedTransaction, provider, Tx } from 'web3-core'
 import { DefaultRpcCaller, RpcCaller } from '../utils/rpc-caller'
-import { GethSigner } from './signers/geth-signer'
-import { Wallet, WalletBase } from './wallet'
+import { EIP712TypedData } from '../utils/sign-typed-data-utils'
+import { encodeTransaction, rlpEncodedTx } from '../utils/signing-utils'
+import { Wallet } from './wallet'
 
-export class GethWallet extends WalletBase implements Wallet {
-  protected gethSigner: GethSigner
-  protected gethRpcCaller: RpcCaller
+enum GethRpc {
+  signTransaction = 'eth_signTransaction',
+  signTypedData = 'eth_signTypedData',
+  signPersonal = 'personal_sign',
+  unlockPersonal = 'personal_unlockAccount',
+  addPersonal = 'personal_importRawKey',
+}
+
+const currentTimeInSeconds = () => Math.round(Date.now() / 1000)
+
+export class GethWallet implements Wallet {
+  protected rpc: RpcCaller
+  private accounts: Map<string, { unlockedAt: number; duration: number }>
+
   constructor(protected gethProvider: provider) {
-    super()
-    this.gethRpcCaller = new DefaultRpcCaller(gethProvider)
-    this.gethSigner = new GethSigner(this.gethRpcCaller)
+    this.rpc = new DefaultRpcCaller(gethProvider)
+    this.accounts = new Map()
   }
 
-  async addAccount(privateKey: string, passphrase: string) {
-    // Prefix 0x here or else the signed transaction produces dramatically different signer!!!
-    privateKey = normalizeAddressWith0x(privateKey)
-    const accountAddress = normalizeAddressWith0x(privateKeyToAddress(privateKey))
-    if (this.hasAccount(accountAddress)) {
-      return
+  getAccounts = () => Array.from(this.accounts.keys())
+  hasAccount = (address?: string) => this.accounts.has(address!)
+
+  async addAccount(privateKey: string, passphrase: string): Promise<string> {
+    privateKey = ensureLeading0x(privateKey)
+    const address = privateKeyToAddress(privateKey)
+    if (this.hasAccount(address)) {
+      throw new Error(`GethWallet already has address ${address}`)
     }
-    await this.gethRpcCaller.call('personal_importRawKey', [privateKey, passphrase])
-    this.addSigner(accountAddress, this.gethSigner)
+    await this.callRpc(GethRpc.addPersonal, [privateKey, passphrase], address)
+    this.accounts.set(address, { unlockedAt: -1, duration: -1 })
+    return address
   }
 
   async unlockAccount(address: string, passphrase: string, duration: number) {
-    await this.gethRpcCaller.call('personal_unlockAccount', [address, passphrase, duration])
+    this.requireHasAccount(address)
+    await this.callRpc(GethRpc.unlockPersonal, [address, passphrase, duration], true)
+    this.accounts.set(address, { unlockedAt: currentTimeInSeconds(), duration })
+  }
+
+  isAccountUnlocked(address: string) {
+    this.requireHasAccount(address)
+    const unlockedInfo = this.accounts.get(address)!
+    return unlockedInfo.unlockedAt + unlockedInfo.duration > currentTimeInSeconds()
+  }
+
+  async signTransaction(txParams: Tx): Promise<EncodedTransaction> {
+    const address = txParams.from! as string
+    this.requireAccountUnlocked(address)
+    const rlpEncoded = rlpEncodedTx(txParams)
+    const result = await this.callRpc(GethRpc.signTransaction, [txParams])
+    const signature = decodeSig(result)
+    return encodeTransaction(rlpEncoded, signature)
+  }
+
+  async signPersonalMessage(address: string, data: string): Promise<string> {
+    this.requireAccountUnlocked(address)
+    return this.callRpc(GethRpc.signPersonal, [data, address])
+  }
+
+  async signTypedData(address: string, typedData: EIP712TypedData): Promise<string> {
+    this.requireAccountUnlocked(address)
+    return this.callRpc(GethRpc.signTypedData, [typedData, address])
+  }
+
+  private async callRpc(method: GethRpc, params: any[], expectedResp?: any) {
+    const resp = await this.rpc.call(method.toString(), params)
+    if (resp.error) {
+      throw new Error(`GethWallet: Error ${resp.error} during ${method.toString()}`)
+    } else if (expectedResp && expectedResp !== resp.result) {
+      throw new Error(`GethWallet: ${method.toString()} gave unexpected result ${resp.result}`)
+    }
+    return resp.result!
+  }
+
+  private requireHasAccount(address: string) {
+    if (!this.hasAccount(address)) {
+      throw new Error(`GethWallet does not have address ${address}`)
+    }
+  }
+
+  private requireAccountUnlocked(address: string) {
+    if (!this.isAccountUnlocked(address)) {
+      throw new Error(`GethWallet address ${address} is not unlocked`)
+    }
+  }
+}
+
+const decodeSig = (sig: any) => {
+  const [v, r, s] = Account.decodeSignature(sig)
+  return {
+    v: parseInt(v, 16),
+    r: ethUtil.toBuffer(r) as Buffer,
+    s: ethUtil.toBuffer(s) as Buffer,
   }
 }
