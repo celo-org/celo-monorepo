b266beb7f5453521ca71dc894ce732bd7b146bb2
Connor McEwen <connor.mcewen@gmail.com>
2020-05-18 11:08:41 -0700

    Begin switch to drawer

--- packages/mobile/src/navigator/DrawerNavigator.tsx
+++ packages/mobile/src/navigator/DrawerNavigator.tsx
@@ -1,9 +1,6 @@
-import Touchable from '@celo/react-components/components/Touchable'
 import colors from '@celo/react-components/styles/colors'
 import fontStyles from '@celo/react-components/styles/fonts'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs/lib/typescript/src/types'
-import { useNavigation } from '@react-navigation/core'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 import * as React from 'react'
 import { useTranslation } from 'react-i18next'
 import { StyleSheet, View } from 'react-native'
@@ -12,70 +9,18 @@ import ExchangeHomeScreen from 'src/exchange/ExchangeHomeScreen'
 import WalletHome from 'src/home/WalletHome'
 import { Namespaces } from 'src/i18n'
 import GoldTabIcon from 'src/icons/GoldTab'
-import PaymentsIcon from 'src/icons/PaymentsIcon'
-import WalletIcon from 'src/icons/Wallet'
 import { Screens } from 'src/navigator/Screens'
-import { getTabBarActiveNotification, isBackupTooLate } from 'src/redux/selectors'
+import { isBackupTooLate } from 'src/redux/selectors'
 
-const TabNav = createBottomTabNavigator()
+const Drawer = createDrawerNavigator()
 
-function SendIcon() {
-  const tooLate = useSelector(isBackupTooLate)
-  const color = tooLate ? colors.inactive : colors.dark
-
-  return (
-    <View style={styles.alignPaymentIcon}>
-      <PaymentsIcon color={color} />
-    </View>
-  )
-}
-
-const SmartWalletIcon = ({ color }: { color: string }) => {
-  const hasActiveNotifications = useSelector(getTabBarActiveNotification)
-
-  return (
-    <View style={styles.alignWallet}>
-      {hasActiveNotifications && <View style={styles.circle} />}
-      <WalletIcon color={color} />
-    </View>
-  )
-}
-
-function TabBarButtonComponent(props: BottomTabBarButtonProps & { isSend?: boolean }) {
-  const backupTooLate = useSelector(isBackupTooLate)
-  const navigation = useNavigation()
-  let { onPress } = props
-  if (props.isSend) {
-    onPress = () => {
-      navigation.dangerouslyGetParent()?.navigate(Screens.Send)
-    }
-  }
-
-  return (
-    <Touchable
-      // @ts-ignore
-      {...props}
-      // @ts-ignore
-      onPress={onPress}
-      borderless={true}
-      disabled={backupTooLate}
-    >
-      {props.children}
-    </Touchable>
-  )
-}
-
-const SendButton = (props: BottomTabBarButtonProps) => (
-  <TabBarButtonComponent {...props} isSend={true} />
-)
-
-export default function TabNavigator() {
+export default function DrawerNavigator() {
   const { t } = useTranslation(Namespaces.global)
   const backupTooLate = useSelector(isBackupTooLate)
   const inactiveTintColor = backupTooLate ? colors.inactive : colors.dark
 
   return (
-    <TabNav.Navigator
+    <Drawer.Navigator
       initialRouteName={Screens.WalletHome}
       screenOptions={{
         tabBarButton: TabBarButtonComponent,
@@ -87,7 +32,7 @@ export default function TabNavigator() {
         style: styles.tabBar,
       }}
     >
-      <TabNav.Screen
+      <Drawer.Screen
         name={Screens.WalletHome}
         component={WalletHome}
         options={{
@@ -95,7 +40,7 @@ export default function TabNavigator() {
           tabBarIcon: SmartWalletIcon,
         }}
       />
-      <TabNav.Screen
+      <Drawer.Screen
         name={Screens.Send}
         component={View}
         options={{
@@ -104,7 +49,7 @@ export default function TabNavigator() {
           tabBarButton: SendButton,
         }}
       />
-      <TabNav.Screen
+      <Drawer.Screen
         name={Screens.ExchangeHomeScreen}
         component={ExchangeHomeScreen}
         options={{
@@ -112,7 +57,7 @@ export default function TabNavigator() {
           tabBarIcon: ({ color }) => <GoldTabIcon color={color} />,
         }}
       />
-    </TabNav.Navigator>
+    </Drawer.Navigator>
   )
 }
 
