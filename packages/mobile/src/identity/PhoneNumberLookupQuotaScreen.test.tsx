c9e2ba891264f0b304774d01a66470491e050e85
J M Rossy <jm.rossy@gmail.com>
2020-05-20 13:28:28 -0400

    Fix nav issues from react-nav upgrade

--- packages/mobile/src/identity/PhoneNumberLookupQuotaScreen.test.tsx
+++ packages/mobile/src/identity/PhoneNumberLookupQuotaScreen.test.tsx
@@ -3,7 +3,18 @@ import 'react-native'
 import { render } from 'react-native-testing-library'
 import { Provider } from 'react-redux'
 import PhoneNumberLookupQuotaScreen from 'src/identity/PhoneNumberLookupQuotaScreen'
+import { Screens } from 'src/navigator/Screens'
 import { createMockStore } from 'test/utils'
+import { mockNavigation } from 'test/values'
+
+const mockRoute = {
+  name: Screens.PhoneNumberLookupQuota as Screens.PhoneNumberLookupQuota,
+  key: '1',
+  params: {
+    onBuy: jest.fn(),
+    onSkip: jest.fn(),
+  },
+}
 
 describe('PhoneNumberLookupQuotaScreen', () => {
   const store = createMockStore({})
@@ -11,7 +22,7 @@ describe('PhoneNumberLookupQuotaScreen', () => {
   it('renders correctly', () => {
     const { toJSON } = render(
       <Provider store={store}>
-        <PhoneNumberLookupQuotaScreen />
+        <PhoneNumberLookupQuotaScreen navigation={mockNavigation} route={mockRoute} />
       </Provider>
     )
     expect(toJSON()).toMatchSnapshot()
