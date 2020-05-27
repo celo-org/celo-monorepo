8e8003699b0afa95d18a69c9fb86a96d60099715
Erdal Tuleu <etuleu@gmail.com>
2020-05-27 11:36:41 -0700

    remove files pushed by mistake

--- packages/mobile/android/app/src/androidTest/gen/org/celo/mobile/Manifest.java
+++
@@ -1,5 +0,0 @@
-/*___Generated_by_IDEA___*/
-package org.celo.mobile;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {}
