1ae66be5ad0dfd330f579b031401c2dbc5f236df
aaron deruvo <aaron@celo.org>
2020-05-28 13:32:08 -0700

    top bar

--- packages/web/src/experienceKits/common/TopBar.tsx
+++ packages/web/src/experienceKits/common/TopBar.tsx
@@ -5,40 +5,65 @@ import Octocat from 'src/icons/Octocat'
 import { useScreenSize } from 'src/layout/ScreenSize'
 import LogoLightBg from 'src/logos/LogoLightBg'
 import Button, { BTN } from 'src/shared/Button.3'
-import { CeloLinks } from 'src/shared/menu-items'
+import links, { CeloLinks } from 'src/shared/menu-items'
 import { colors, fonts, standardStyles } from 'src/styles'
+import Navigation, { NavigationTheme } from 'src/shared/navigation'
 
 interface Props {
-  title: string
-  link: string
+  current: string
 }
 
-export default function TopBar({ title, link }: Props) {
+export default function TopBar({ current }: Props) {
   const { isMobile } = useScreenSize()
   const { t } = useTranslation(NameSpaces.common)
+  const name = current === links.BRAND.link ? links.BRAND.name : links.EVENTS_KIT.name
   return (
     <View style={standardStyles.centered}>
       <View style={[standardStyles.row, styles.container]}>
-        <a href={link}>
-          <TouchableOpacity style={styles.logo}>
-            <LogoLightBg height={30} />
-            {!isMobile && (
-              <Text
-                // @ts-ignore -- added initial to the aug but it still isnt liking it
-                style={[fonts.h3, styles.title]}
-              >
-                {title}
-              </Text>
-            )}
-          </TouchableOpacity>
-        </a>
-        <Button
-          kind={BTN.NAV}
-          href={CeloLinks.gitHub}
-          text={t('github')}
-          target={'_blank'}
-          iconRight={<Octocat size={22} color={colors.dark} />}
-        />
+        <View style={styles.rowVerticalCenter}>
+          <a href={links.HOME.link}>
+            <TouchableOpacity style={styles.rowVerticalCenter}>
+              <LogoLightBg height={30} />
+            </TouchableOpacity>
+          </a>
+          {!isMobile && (
+            <a href={current}>
+              <TouchableOpacity style={styles.rowVerticalCenter}>
+                <Text
+                  // @ts-ignore -- added initial to the aug but it still isnt liking it
+                  style={[fonts.h3, styles.title]}
+                >
+                  {name}
+                </Text>
+              </TouchableOpacity>
+            </a>
+          )}
+        </View>
+        <View style={styles.rowVerticalCenter}>
+          <View style={styles.kits}>
+            <Navigation
+              style={styles.navLink}
+              text={links.EVENTS_KIT.name}
+              link={links.EVENTS_KIT.link}
+              selected={links.EVENTS_KIT.link === current}
+              theme={NavigationTheme.LIGHT}
+            />
+            <Navigation
+              style={styles.navLink}
+              text={links.BRAND.name}
+              link={links.BRAND.link}
+              selected={links.BRAND.link === current}
+              theme={NavigationTheme.LIGHT}
+            />
+          </View>
+          <Button
+            kind={BTN.NAV}
+            href={CeloLinks.gitHub}
+            text={t('github')}
+            target={'_blank'}
+            iconRight={<Octocat size={22} color={colors.dark} />}
+          />
+        </View>
       </View>
     </View>
   )
@@ -58,8 +83,17 @@ const styles = StyleSheet.create({
     width: '100%',
     flex: 1,
   },
-  logo: {
+  rowVerticalCenter: {
     alignContent: 'center',
     flexDirection: 'row',
+  },
+  kits: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginHorizontal: 30,
+  },
+  navLink: {
+    marginBottom: 0,
+    marginHorizontal: 15,
   },
 })
