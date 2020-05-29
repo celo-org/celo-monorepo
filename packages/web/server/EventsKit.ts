91930f21888c923c394c0749927254878c1c1640
aaron deruvo <aaron@celo.org>
2020-05-29 16:35:48 -0700

    Resources for events

---
+++ packages/web/server/EventsKit.ts
@@ -0,0 +1,71 @@
+import { Attachment, FieldSet, Table } from 'airtable'
+// import getConfig from 'next/config'
+import cache from './cache'
+import airtableInit from './airtable'
+
+const BASE_ID = 'appjKfoHvrO5SZWdd'
+
+interface Fields extends FieldSet {
+  Name: string
+  Preview: Attachment[]
+  Terms: boolean
+  Tags?: string[]
+  Order: number
+  Location: string
+}
+
+export enum Sheets {
+  Planning = 'Planning',
+}
+
+export default async function getAssets(sheet: Sheets) {
+  return cache(`exp-events-${sheet}`, fetchAssets, { args: sheet, minutes: 10 })
+}
+
+async function fetchAssets(sheet: Sheets) {
+  const assets = []
+
+  await getAirtable(sheet)
+    .select({
+      pageSize: 100,
+      filterByFormula: `${TERMS_SIGNED}`,
+      sort: [{ field: 'Order', direction: 'asc' }],
+    })
+    .eachPage((records, fetchNextPage) => {
+      records.forEach((doc) => assets.push(normalize(doc.fields, doc.id)))
+      fetchNextPage()
+    })
+  return assets
+}
+
+function getAirtable(sheet: Sheets): Table<Fields> {
+  return airtableInit(BASE_ID)(sheet) as Table<Fields>
+}
+
+const TERMS_SIGNED = 'Terms=1'
+
+function normalize(asset: Fields, id: string) {
+  return {
+    title: asset.Name,
+    preview: getPreview(asset),
+    uri: getURI(asset),
+    id,
+  }
+}
+export const _normalize = normalize
+
+function getPreview(asset: Fields) {
+  const previewField = asset.Preview
+
+  return (
+    (previewField &&
+      previewField[0] &&
+      previewField[0].thumbnails &&
+      previewField[0].thumbnails.large.url) ||
+    ''
+  )
+}
+
+function getURI(asset: Fields) {
+  return asset.Location
+}
--- packages/web/src/experience/eventkit/PlanningDocs.tsx
+++ packages/web/src/experience/eventkit/PlanningDocs.tsx
@@ -1,93 +1,97 @@
 import * as React from 'react'
+import { useAsync } from 'react-async-hook'
 import { Image, StyleSheet, Text, View } from 'react-native'
-import placeholder from 'src/experience/eventkit/content/tenents/striving-for-beauty.png'
 import External from 'src/icons/External'
 import AspectRatio from 'src/shared/AspectRatio'
 import { colors, fonts, standardStyles } from 'src/styles'
 
-function DocPreview({ source, href, title }) {
+interface Doc {
+  title: string
+  preview: any
+  uri: string
+}
+
+// usually if there is an icon it will jump to the next line by its lonely self, instead we marry it to the last word so that it has a partner
+const MarryLastWordToIcon = React.memo(function _MarryLastWordToIcon({ words }: { words: string }) {
+  const titleParts = words.split(' ')
+  const titleEnd = (
+    <Text
+      // @ts-ignore
+      style={styles.noBreaks}
+    >
+      {titleParts[titleParts.length - 1]} <External size={14} color={colors.dark} />
+    </Text>
+  )
+
+  return (
+    <>
+      {titleParts.splice(0, titleParts.length - 1).join(' ')} {titleEnd}
+    </>
+  )
+})
+
+function DocPreview({ preview, uri, title }: Doc) {
   return (
     <View style={styles.root}>
-      <a href={href}>
+      <a href={uri} target={'_blank'}>
         <AspectRatio ratio={148 / 111} style={styles.image}>
-          <Image source={source} style={standardStyles.image} />
+          <Image source={preview} style={standardStyles.image} />
         </AspectRatio>
       </a>
-      <a href={href}>
+      <a href={uri} target={'_blank'}>
         <Text style={fonts.h6}>
-          {title} <External size={14} color={colors.dark} />
+          <MarryLastWordToIcon words={title} />
         </Text>
       </a>
     </View>
   )
 }
 
-const DOCS = [
-  {
-    source: placeholder,
-    href: '#',
-    title: 'Types of Events',
-  },
-  {
-    source: placeholder,
-    href: '#',
-    title: 'Types of Events',
-  },
-  {
-    source: placeholder,
-    href: '#',
-    title: 'Types of Events',
-  },
-  {
-    source: placeholder,
-    href: '#',
-    title: 'Types of Events',
-  },
-  {
-    source: placeholder,
-    href: '#',
-    title: 'Types of Events',
-  },
-  {
-    source: placeholder,
-    href: '#',
-    title: 'Types of Events',
-  },
-  {
-    source: placeholder,
-    href: '#',
-    title: 'Types of Events',
-  },
-  {
-    source: placeholder,
-    href: '#',
-    title: 'Types of Events',
-  },
-]
+async function fetchDocs() {
+  return (await fetch('/api/experience/assets/planning')).json()
+}
 
 export default function PlanningDocs() {
-  return (
-    <View style={styles.grid}>
-      {DOCS.map((doc) => {
-        return <DocPreview key={doc.title} source={doc.source} href={doc.href} title={doc.title} />
-      })}
-    </View>
-  )
+  const planningDocs = useAsync<Doc[]>(fetchDocs, [])
+
+  if (!planningDocs.result) {
+    return <View style={styles.placeholder} />
+  }
+
+  if (planningDocs.result) {
+    return (
+      <View style={styles.grid}>
+        {planningDocs.result.map((doc) => {
+          console.info(JSON.stringify(doc))
+          return (
+            <DocPreview key={doc.title} preview={doc.preview} uri={doc.uri} title={doc.title} />
+          )
+        })}
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   grid: {
     display: 'grid',
-    gridGap: 15,
+    gridColumnGap: 20,
+    gridRowGap: 10,
     gridTemplateColumns: `repeat(auto-fill, minmax(160px, 1fr))`,
     justifyContent: 'space-between',
   },
   root: { flexBasis: 160, marginVertical: 10 },
   image: {
-    width: '90%',
+    width: '100%',
     marginBottom: 10,
     borderRadius: 4,
     borderWidth: 2,
     borderColor: colors.gray,
+  },
+  placeholder: {
+    minHeight: 300,
+  },
+  noBreaks: {
+    display: 'inline-flex',
   },
 })
