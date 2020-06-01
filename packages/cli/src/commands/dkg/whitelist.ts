cb725c1f4aab0857307ac6e2f75d66aa255bc533
Kobi Gurkan <kobigurk@gmail.com>
2020-06-01 11:17:27 +0300

    fix whitelist

--- packages/cli/src/commands/dkg/whitelist.ts
+++ packages/cli/src/commands/dkg/whitelist.ts
@@ -1,6 +1,5 @@
 import { ensureLeading0x } from '@celo/utils/lib/address'
 import { flags } from '@oclif/command'
-import fs from 'fs'
 import { BaseCommand } from '../../base'
 import { displayWeb3Tx } from '../../utils/cli'
 import { Flags } from '../../utils/command'
@@ -27,8 +26,8 @@ export default class DKGRegister extends BaseCommand {
     const dkg = new web3.eth.Contract(DKG.abi, res.flags.address)
 
     // read the pubkey and publish it
-    const participantAddress = fs.readFileSync(res.flags.participantAddress).toString('hex')
-    await displayWeb3Tx('whitelist', dkg.methods.register(ensureLeading0x(participantAddress)), {
+    const participantAddress = res.flags.participantAddress
+    await displayWeb3Tx('whitelist', dkg.methods.whitelist(ensureLeading0x(participantAddress)), {
       from: res.flags.from,
     })
   }
