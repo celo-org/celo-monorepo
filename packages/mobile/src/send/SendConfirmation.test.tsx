9392bd2a01c4d3bd25de876d68d02e1797757f99
Tarik Bellamine <tarikbellamine@protonmail.com>
2020-06-02 09:28:59 -0700

    wrote tests and updated snapshots

--- packages/mobile/src/send/SendConfirmation.test.tsx
+++ packages/mobile/src/send/SendConfirmation.test.tsx
@@ -19,16 +19,6 @@ const TEST_FEE = new BigNumber(10000000000000000)
 
 jest.mock('src/send/saga')
 
-const storeData = {
-  fees: {
-    estimates: {
-      send: {
-        feeInWei: '1',
-      },
-    },
-  },
-}
-
 const mockedGetSendFee = getSendFee as jest.Mock
 
 const mockScreenProps = getMockStackScreenProps(Screens.SendConfirmation, {
@@ -44,22 +34,27 @@ describe('SendConfirmation', () => {
     mockedGetSendFee.mockClear()
   })
 
-  describe('when commenting', () => {
-    const store = createMockStore(storeData)
-    const getWrapper = () =>
-      render(
-        <Provider store={store}>
-          <SendConfirmation {...mockScreenProps} />
-        </Provider>
-      )
-
-    it('updates the comment/reason', () => {
-      const wrapper = getWrapper()
-      const input = wrapper.getByTestId('commentInput/send')
-      const comment = 'A comment!'
-      fireEvent.changeText(input, comment)
-      expect(wrapper.queryAllByDisplayValue(comment)).toHaveLength(1)
+  it('updates the comment/reason', () => {
+    const store = createMockStore({
+      fees: {
+        estimates: {
+          send: {
+            feeInWei: '1',
+          },
+        },
+      },
     })
+
+    const tree = render(
+      <Provider store={store}>
+        <SendConfirmation {...mockScreenProps} />
+      </Provider>
+    )
+
+    const input = tree.getByTestId('commentInput/send')
+    const comment = 'A comment!'
+    fireEvent.changeText(input, comment)
+    expect(tree.queryAllByDisplayValue(comment)).toHaveLength(1)
   })
 
   it('renders correctly for send payment confirmation', async () => {
