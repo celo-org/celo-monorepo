f97e19fc1311dad65c1039ec97051f93fd7e937e
J M Rossy <jm.rossy@gmail.com>
2020-06-04 16:09:19 -0400

    Remove deprecated verifier and verification-pool-api packages (#3989)
    ### Description
    
    Remove deprecated verifier and verification-pool-api packages. The pool appears to be causing test flakiness.
    Tag created before this commit: https://github.com/celo-org/celo-monorepo/releases/tag/pre-ver-pool-and-app-removal
    
    ### Backwards compatibility
    
    Yes

--- packages/verifier/src/components/HomeScreen/SwitchButton.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import 'react-native'
-import * as renderer from 'react-test-renderer'
-import SwitchButton from 'src/components/HomeScreen/SwitchButton'
-
-it('renders correctly', () => {
-  const tree = renderer.create(<SwitchButton onToggle={jest.fn()} switchStatus={false} />)
-  expect(tree).toMatchSnapshot()
-})
