60f65d54fada74cbe19e8b0bc8ab4334e07885ff
J M Rossy <jm.rossy@gmail.com>
2020-06-03 21:03:52 -0400

    Add snapshot test

--- packages/mobile/src/onboarding/contacts/ImportContactsScreen.test.tsx
+++ packages/mobile/src/onboarding/contacts/ImportContactsScreen.test.tsx
@@ -6,10 +6,10 @@ import ImportContactsScreen from 'src/onboarding/contacts/ImportContactsScreen'
 import { createMockStore } from 'test/utils'
 
 describe('ImportContactsScreen', () => {
-  const store = createMockStore({})
+  const store = createMockStore()
 
   it('renders correctly', () => {
-    const { getByTestId, toJSON } = render(
+    const { toJSON } = render(
       <Provider store={store}>
         <ImportContactsScreen />
       </Provider>
