dec433aa4ddef0074de10c07946fa7589b1b7acb
Javier Cortejoso <javier.cortejoso@gmail.com>
2020-06-04 20:34:26 +0200

    Using victorops rest integration

--- packages/celotool/scripts/notify_victorops.sh
+++ packages/celotool/scripts/notify_victorops.sh
@@ -4,21 +4,19 @@ set -eu
 result=$1
 
 if [ "$result" == "FAILURE" ]; then
+  message_type="CRITICAL"
   fail_modes=$2
-  message=":red_circle: Job $CIRCLE_JOB failed: The syncmodes $fail_modes failed to sync on Mainnet"
+  message="Geth Integration tests: Job $CIRCLE_JOB failed: The syncmodes $fail_modes failed to sync on Mainnet: $CIRCLE_BUILD_URL"
 else
-  message=":tada: Job $CIRCLE_JOB succeeded. All modes could sync successfully on Mainnet"
+  message_type="INFO"
+  message="Geth Integration tests: Job $CIRCLE_JOB succeeded. All modes could sync successfully on Mainnet: $CIRCLE_BUILD_URL"
 fi
 
 payload="{
-    \"entity_type\": \"Build System\",
-    \"event_type\": \"Build\",
-    \"source\": \"CircleCI\",
-    \"summary\": \"Geth syncmode tests to Mainnet\",
-    \"url\": \"$CIRCLE_BUILD_URL\",
-    \"action\": \"test\",
-    \"result\": \"$result\",
-    \"long_message\": \"$message\"
+    \"message_type\": \"$message_type\",
+    \"entity_id\": \"$CIRCLE_BUILD_NUM\",
+    \"entity_display_name\": \"CircleCI JOB $CIRCLE_BUILD_NUM\",
+    \"state_message\": \"$message\"
 }"
 
 curl -H "Content-Type: application/json; charset=UTF-8" -X POST --data "$payload" "$VICTOROPS_NOTIFICATION_URL"
