bc9c6c4d67ae5cc4827300056f8ead4a509fdbf4
Aaron DeRuvo <aaron.deruvo@gmail.com>
2020-06-10 16:49:20 -0700

    Code of Conduct Update / markify (#4065)
    ### Description
    
    Updates Code of Conduct to latest version and moves to a markdown file for easier editing. 
    
    I like to think this brings it closes to the community as community members can now easily submit pull requests to change the code.
    
    ### Other changes
    
    remove code-of-conduct.json
    
    ### Tested
    ![Screen Shot 2020-06-10 at 3 07 40 PM](https://user-images.githubusercontent.com/3814795/84325026-bd3da300-ab2e-11ea-8dd8-c4c8d071bb50.png)
    
    
    ### Related issues
    
    - Fixes #3855

--- packages/web/jest.config.js
+++ packages/web/jest.config.js
@@ -11,6 +11,7 @@ module.exports = {
   moduleNameMapper: {
     '\\.(png|jpg|jpeg|gif)$': '<rootDir>/__mocks__/ImageStub.ts',
     '\\.(css|scss)$': '<rootDir>/__mocks__/ImageStub.ts',
+    '\\.(md)$': '<rootDir>/__mocks__/MarkdownStub.ts',
     'pages/(.*)$': '<rootDir>/pages/$1',
     'src/(.*)$': '<rootDir>/src/$1',
     'public/(.*)$': '<rootDir>/public/$1',
