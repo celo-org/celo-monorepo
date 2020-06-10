eddcbc8c1ab34526d6b5627280d5b47403d356a2
Erdal Tuleu <etuleu@gmail.com>
2020-06-09 22:00:23 -0700

    use typeText as it works on both ios and android

--- packages/mobile/e2e/Transfer.spec.js
+++ packages/mobile/e2e/Transfer.spec.js
@@ -102,8 +102,7 @@ describe('Transfer Works', () => {
       .withTimeout(20000)
 
     await element(by.id('ImportWalletBackupKeyInputField')).tap()
-    await element(by.id('ImportWalletBackupKeyInputField')).replaceText(SAMPLE_BACKUP_KEY)
-    await element(by.id('ImportWalletBackupKeyInputField')).tapReturnKey()
+    await element(by.id('ImportWalletBackupKeyInputField')).typeText(SAMPLE_BACKUP_KEY)
 
     await sleep(4000)
 
