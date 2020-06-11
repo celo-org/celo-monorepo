c17287cfd4a3c7258aabe364e2c9c8a2adc15508
Erdal Tuleu <etuleu@gmail.com>
2020-06-11 16:22:46 -0700

    use image android-28 because 26 has an open gl bug

--- packages/mobile/scripts/install_android_emulator_image.sh
+++ packages/mobile/scripts/install_android_emulator_image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sdkmanager "platform-tools" "platforms;android-26" "extras;intel;Hardware_Accelerated_Execution_Manager" "build-tools;26.0.0" "system-images;android-26;google_apis;x86" "emulator" --verbose
+sdkmanager "platform-tools" "platforms;android-28" "extras;intel;Hardware_Accelerated_Execution_Manager" "build-tools;26.0.0" "system-images;android-28;google_apis;x86" "emulator" --verbose
