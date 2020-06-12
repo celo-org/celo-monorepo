4f8a386da336ca0f2d0d4a292f38cadff15a50b0
Aaron DeRuvo <aaron.deruvo@gmail.com>
2020-06-12 10:23:25 -0700

    Update Exchange Ticker Icons (#4082)
    ### Description
    
    Updates to Exchange Icons due to rename of Celo's native asset. 
    
    ### Tested
    
    ![Screen Shot 2020-06-11 at 3 20 37 PM](https://user-images.githubusercontent.com/3814795/84444759-559f5a80-abf7-11ea-8acc-2c04ff4e7ccd.png)
    
    ![Screen Shot 2020-06-11 at 3 20 43 PM](https://user-images.githubusercontent.com/3814795/84444835-7b2c6400-abf7-11ea-9084-d3a574243db7.png)
    
    
    ### Related issues
    
    - Fixes #4066

--- packages/web/src/experience/brandkit/IconsExchangePage.tsx
+++ packages/web/src/experience/brandkit/IconsExchangePage.tsx
@@ -2,6 +2,7 @@ import { NextPage } from 'next'
 import * as React from 'react'
 import { StyleSheet, View } from 'react-native'
 import Page, { ICONS_PATH } from 'src/experience/brandkit/common/Page'
+import content from 'src/experience/brandkit/content/exchange-icons.md'
 import {
   AssetTypes,
   EXCHANGE_ICONS_PKG_TRACKING,
@@ -9,11 +10,12 @@ import {
 } from 'src/experience/brandkit/tracking'
 import CCLicense from 'src/experience/common/CCLicense'
 import { brandStyles, GAP } from 'src/experience/common/constants'
-import PageHeadline from 'src/experience/common/PageHeadline'
 import IconShowcase from 'src/experience/common/Showcase'
+import Markdown from 'src/experience/Markdown'
 import { NameSpaces, useTranslation } from 'src/i18n'
 import Button, { BTN } from 'src/shared/Button.3'
 import { hashNav } from 'src/shared/menu-items'
+import { standardStyles } from 'src/styles'
 
 const icons = [
   {
@@ -23,38 +25,38 @@ const icons = [
     uri: '/assets/marketplace-icons/icon-celo-dollar-color.zip',
   },
   {
-    name: 'cGLD Exchange Icon',
+    name: 'CELO Exchange Icon',
     description: 'Full Color\n',
-    preview: '/images/marketplace-icons/icon-celo-gold-color-f.svg',
-    uri: '/assets/marketplace-icons/icon-celo-gold-color.zip',
+    preview: '/images/marketplace-icons/icon-celo-CELO-color-f.svg',
+    uri: '/assets/marketplace-icons/icon-celo-CELO-color.zip',
   },
   null,
   {
     name: 'cUSD Exchange Icon',
     description: 'Monochrome\n',
-    preview: '/images/marketplace-icons/icon-celo-dollar-black-f.svg',
-    uri: '/assets/marketplace-icons/icon-celo-dollar-black.zip',
+    preview: '/images/marketplace-icons/icon-celo-dollar-mono-f.svg',
+    uri: '/assets/marketplace-icons/icon-celo-dollar-mono.zip',
     variant: 'circle-white',
   },
   {
-    name: 'cGLD Exchange Icon',
+    name: 'CELO Exchange Icon',
     description: 'Monochrome\n',
-    preview: '/images/marketplace-icons/icon-celo-gold-black-f.svg',
-    uri: '/assets/marketplace-icons/icon-celo-gold-black.zip',
+    preview: '/images/marketplace-icons/icon-celo-CELO-mono-f.svg',
+    uri: '/assets/marketplace-icons/icon-celo-CELO-mono.zip',
     variant: 'circle-white',
   },
   {
     name: 'cUSD Exchange Icon',
     description: 'Reverse Monochrome\n',
-    preview: '/images/marketplace-icons/icon-celo-dollar-white-f.svg',
-    uri: '/assets/marketplace-icons/icon-celo-dollar-white.zip',
+    preview: '/images/marketplace-icons/icon-celo-dollar-reverse-f.svg',
+    uri: '/assets/marketplace-icons/icon-celo-dollar-reverse.zip',
     variant: 'circle-black',
   },
   {
-    name: 'cGLD Exchange Icon',
+    name: 'CELO Exchange Icon',
     description: 'Reverse Monochrome\n',
-    preview: '/images/marketplace-icons/icon-celo-gold-white-f.svg',
-    uri: '/assets/marketplace-icons/icon-celo-gold-white.zip',
+    preview: '/images/marketplace-icons/icon-celo-CELO-reverse-f.svg',
+    uri: '/assets/marketplace-icons/icon-celo-CELO-reverse.zip',
     variant: 'circle-black',
   },
 ]
@@ -91,16 +93,16 @@ function Overview() {
 
   return (
     <View style={styles.container}>
-      <PageHeadline title={t('exchangeIcons.title')} headline={t('exchangeIcons.headline')} />
-
-      <Button
-        kind={BTN.PRIMARY}
-        text={t('logo.overviewBtn')}
-        style={styles.download}
-        onPress={onPressDownload}
-        href="/assets/CeloMarketplaceIcons.zip"
-      />
-
+      <View style={brandStyles.gap}>
+        <Markdown source={content} />
+        <Button
+          kind={BTN.PRIMARY}
+          text={t('logo.overviewBtn')}
+          style={standardStyles.elementalMarginTop}
+          onPress={onPressDownload}
+          href="/assets/CeloMarketplaceIcons.zip"
+        />
+      </View>
       <CCLicense textI18nKey="exchangeIcons.license" />
 
       <View style={styles.root}>
@@ -137,8 +139,5 @@ const styles = StyleSheet.create({
   break: {
     width: '100%',
     display: 'block',
-  },
-  download: {
-    marginLeft: 10,
   },
 })
