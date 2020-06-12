6a51ccdb5f95bccc58e947123ac606314eff155f
Erdal Tuleu <etuleu@gmail.com>
2020-06-12 16:10:39 -0700

    upgrade build tools to latest 30.0.0

--- packages/mobile/scripts/install_android_emulator_image.sh
+++ packages/mobile/scripts/install_android_emulator_image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sdkmanager "platform-tools" "platforms;android-28" "build-tools;26.0.0" "system-images;android-28;google_apis_playstore;x86" "emulator" --verbose
+sdkmanager "platform-tools" "platforms;android-28" "build-tools;30.0.0" "system-images;android-28;google_apis_playstore;x86" "emulator" --verbose
