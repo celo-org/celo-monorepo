24997eb6e6a321f748d12b5b25520d24f7f66c20
Nam Chu Hoai <nambrot@googlemail.com>
2020-06-17 11:49:55 +0200

    Smaller deploy fixes

--- packages/celotool/src/lib/blockscout.ts
+++ packages/celotool/src/lib/blockscout.ts
@@ -89,12 +89,12 @@ async function helmParameters(
     `--set blockscout.metadata_crawler.image.repository=${fetchEnv(
       envVar.BLOCKSCOUT_METADATA_CRAWLER_IMAGE_REPOSITORY
     )}`,
-    `--set blockscout.metadata_crawler.repository.tag=${fetchEnv(
+    `--set blockscout.metadata_crawler.image.tag=${fetchEnv(
       envVar.BLOCKSCOUT_METADATA_CRAWLER_IMAGE_TAG
     )}`,
-    `--set blockscout.metadata_crawler.schedule=${fetchEnv(
+    `--set blockscout.metadata_crawler.schedule="${fetchEnv(
       envVar.BLOCKSCOUT_METADATA_CRAWLER_SCHEDULE
-    )}`,
+    )}"`,
     `--set promtosd.scrape_interval=${fetchEnv(envVar.PROMTOSD_SCRAPE_INTERVAL)}`,
     `--set promtosd.export_interval=${fetchEnv(envVar.PROMTOSD_EXPORT_INTERVAL)}`,
   ]
