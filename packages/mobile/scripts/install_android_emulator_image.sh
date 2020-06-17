8d0f3d6d91032b601ae0008e7b64d6d2929d7bf8
Erdal Tuleu <etuleu@gmail.com>
2020-06-17 12:36:45 -0700

    try 64 bit and android-29

--- packages/mobile/scripts/install_android_emulator_image.sh
+++ packages/mobile/scripts/install_android_emulator_image.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sdkmanager "platform-tools" "platforms;android-28" "extras;intel;Hardware_Accelerated_Execution_Manager" "build-tools;30.0.0" "system-images;android-28;google_apis_playstore;x86" "emulator" --verbose
+sdkmanager "platform-tools" \
+  "platforms;android-29" \
+  "build-tools;30.0.0" \
+  "system-images;android-29;google_apis;x86_64" \
+  "emulator" \
+  --verbose
