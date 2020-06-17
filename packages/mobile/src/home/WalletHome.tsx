b02744b5ab36e42b01816c108fa7795784f7ab94
Tarik Bellamine <tarikbellamine@protonmail.com>
2020-06-16 19:06:38 -0700

    fixed build errors

--- packages/mobile/src/home/WalletHome.tsx
+++ packages/mobile/src/home/WalletHome.tsx
@@ -105,7 +105,6 @@ export class WalletHome extends React.Component<Props> {
     await this.tryImportContacts()
   }
 
-  // TODO: cache recent recipients of txs to speed this up
   tryImportContacts = async () => {
     const { numberVerified, recipientCache } = this.props
 
