4174ba125bce4983fd80194808ac04254a798777
Erdal Tuleu <etuleu@gmail.com>
2020-06-17 19:05:47 -0700

    add haxm

--- packages/mobile/scripts/install_android_emulator_image.sh
+++ packages/mobile/scripts/install_android_emulator_image.sh
@@ -5,5 +5,6 @@ sdkmanager "platform-tools" \
   "platforms;android-29" \
   "build-tools;30.0.0" \
   "system-images;android-29;google_apis;x86_64" \
+  "extras;intel;Hardware_Accelerated_Execution_Manager" \
   "emulator" \
   --verbose
