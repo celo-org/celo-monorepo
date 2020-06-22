c4c81e21aeeb3297cf881213b8c2d2d97226885e
Jean Regisser <jean.regisser@gmail.com>
2020-06-22 14:37:15 +0200

    [Wallet][ReDesign] Verification start screen (#4162)
    ### Description
    
    This implements the [verification start screen redesign](https://www.figma.com/file/zt7aTQ5wuXycIwxq5oAmF9/Wallet--Refresh?node-id=5404%3A450).
    
    Note: this PR depends on #4152, please merge that one first.
    
    ### Other changes
    
    N/A
    
    ### Tested
    
    ![verification-education2](https://user-images.githubusercontent.com/57791/85232663-ff08fc00-b400-11ea-8c71-e13b77bb3d6f.gif)
    <img src="https://user-images.githubusercontent.com/57791/85232587-45119000-b400-11ea-948d-00202faba470.png" width="50%" /><img src="https://user-images.githubusercontent.com/57791/85232589-480c8080-b400-11ea-8174-42c0e58e8aaa.png" width="50%" />
    <img src="https://user-images.githubusercontent.com/57791/85232590-4b077100-b400-11ea-8bc4-eda3cdee8da5.png" width="50%" />
    
    ### Related issues
    
    - Epic #3645
    
    ### Backwards compatibility
    
    Yes.

--- packages/mobile/src/navigator/Navigator.tsx
+++ packages/mobile/src/navigator/Navigator.tsx
@@ -102,7 +102,6 @@ import { ExtractProps } from 'src/utils/typescript'
 import VerificationEducationScreen from 'src/verify/VerificationEducationScreen'
 import VerificationInputScreen from 'src/verify/VerificationInputScreen'
 import VerificationInterstitialScreen from 'src/verify/VerificationInterstitialScreen'
-import VerificationLearnMoreScreen from 'src/verify/VerificationLearnMoreScreen'
 import VerificationLoadingScreen from 'src/verify/VerificationLoadingScreen'
 
 const Stack = createStackNavigator<StackParamList>()
@@ -143,11 +142,7 @@ const verificationScreens = (Navigator: typeof Stack) => {
       <Navigator.Screen
         name={Screens.VerificationEducationScreen}
         component={VerificationEducationScreen}
-        options={nuxNavigationOptions}
-      />
-      <Navigator.Screen
-        name={Screens.VerificationLearnMoreScreen}
-        component={VerificationLearnMoreScreen}
+        options={VerificationEducationScreen.navigationOptions}
       />
       <Navigator.Screen
         name={Screens.VerificationLoadingScreen}
