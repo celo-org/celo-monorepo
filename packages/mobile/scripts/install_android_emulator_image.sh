e45f4c8f6f8294f8112d58a4661f0696832e89ae
Erdal Tuleu <etuleu@gmail.com>
2020-06-23 07:10:28 -0700

    try the exact same conf as the blog post

--- packages/mobile/scripts/install_android_emulator_image.sh
+++ packages/mobile/scripts/install_android_emulator_image.sh
@@ -3,9 +3,8 @@ set -euo pipefail
 
 sdkmanager "platform-tools" \
   "platforms;android-29" \
-  "build-tools;30.0.0" \
+  "build-tools;29.0.2" \
   "system-images;android-29;google_apis;x86_64" \
-  "extras;intel;Hardware_Accelerated_Execution_Manager" \
   "emulator" \
   --verbose
 
