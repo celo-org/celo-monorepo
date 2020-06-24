c2fbc8362e23e38edfc6a34b8f546749a656c511
Erdal Tuleu <etuleu@gmail.com>
2020-06-23 20:41:00 -0700

    try x86

--- packages/mobile/scripts/install_android_emulator_image.sh
+++ packages/mobile/scripts/install_android_emulator_image.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 sdkmanager "platform-tools" \
   "platforms;android-29" \
   "build-tools;29.0.2" \
-  "system-images;android-29;google_apis;x86_64" \
+  "system-images;android-29;google_apis;x86" \
   "emulator" \
   --verbose
