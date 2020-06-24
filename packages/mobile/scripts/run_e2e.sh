db3c83853604bc73b06c08d3fbf39fa20bf5c0ca
Erdal Tuleu <etuleu@gmail.com>
2020-06-24 13:42:09 -0700

    wait for device to start

--- packages/mobile/scripts/run_e2e.sh
+++ packages/mobile/scripts/run_e2e.sh
@@ -133,8 +133,8 @@ if [ $PLATFORM = "android" ]; then
     exit 1
   fi
 
-  echo "Starting the emulator"
-  $ANDROID_HOME/emulator/emulator -avd $VD_NAME -no-boot-anim -netdelay $NET_DELAY &
+  # echo "Starting the emulator"
+  # $ANDROID_HOME/emulator/emulator -avd $VD_NAME -no-boot-anim -netdelay $NET_DELAY &
 
   echo "Waiting for device to connect to Wifi, this is a good proxy the device is ready"
   until [ `adb shell dumpsys wifi | grep "mNetworkInfo" | grep "state: CONNECTED" | wc -l` -gt 0 ]
