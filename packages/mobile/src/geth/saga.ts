843f9969ae2f9302d1485843b37d8976b1c5e43a
Erdal Tuleu <etuleu@gmail.com>
2020-06-24 13:13:05 -0700

    revert geth timeout

--- packages/mobile/src/geth/saga.ts
+++ packages/mobile/src/geth/saga.ts
@@ -25,7 +25,7 @@ import { fornoSelector } from 'src/web3/selectors'
 const gethEmitter = new NativeEventEmitter(NativeModules.RNGeth)
 
 const TAG = 'geth/saga'
-const INIT_GETH_TIMEOUT = 30000
+const INIT_GETH_TIMEOUT = 15000
 const NEW_BLOCK_TIMEOUT = 30000
 const GETH_MONITOR_DELAY = 5000
 
