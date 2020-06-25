bc98ae01ea8db10f2791f87ca2a7ff8837cdd985
Jean Regisser <jean.regisser@gmail.com>
2020-06-25 17:18:47 +0200

    Fix nav bar visible in the onboarding success screen

--- packages/mobile/src/navigator/Navigator.tsx
+++ packages/mobile/src/navigator/Navigator.tsx
@@ -230,7 +230,11 @@ const nuxScreens = (Navigator: typeof Stack) => (
       component={ImportContactsScreen}
       options={ImportContactsScreen.navigationOptions}
     />
-    <Navigator.Screen name={Screens.OnboardingSuccessScreen} component={OnboardingSuccessScreen} />
+    <Navigator.Screen
+      name={Screens.OnboardingSuccessScreen}
+      component={OnboardingSuccessScreen}
+      options={OnboardingSuccessScreen.navigationOptions}
+    />
   </>
 )
 
