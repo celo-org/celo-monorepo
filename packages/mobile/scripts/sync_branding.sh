8afd75884d3afb1a570c1016dc4953c9648169de
Jean Regisser <jean.regisser@gmail.com>
2020-07-01 16:15:56 +0200

    Improve sync branding script

--- packages/mobile/scripts/sync_branding.sh
+++ packages/mobile/scripts/sync_branding.sh
@@ -6,16 +6,20 @@ echo $mobile_root
 cd "$mobile_root"
 
 branding=valora
+# Please update the sha when branding updates are needed
 branding_sha=6d643e3
 
-export GIT_TERMINAL_PROMPT=0
-if [[ ! -e branding/valora ]] && ! git clone git@github.com:clabs-co/valora-app-branding.git branding/valora ; then
-  echo "Couldn't clone private branding. Will use default branding."
-  branding=celo
-else
-  pushd "branding/$branding"
-  git checkout "$branding_sha"
-  popd
+if [[ "$branding" == "valora" ]]; then
+  # prevents git from asking credentials
+  export GIT_TERMINAL_PROMPT=0
+  if [[ ! -e branding/valora ]] && ! git clone git@github.com:clabs-co/valora-app-branding.git branding/valora ; then
+    echo "Couldn't clone private branding. Will use default branding."
+    branding=celo
+  else 
+    pushd "branding/$branding"
+    git checkout "$branding_sha"
+    popd
+  fi
 fi
 
 echo "Using branding/$branding"
