52b93f851035331b298618e919a68d1a407961fc
Nam Chu Hoai <nambrot@googlemail.com>
2020-07-03 15:59:22 +0200

    Do not exit 1 when node is not synced yet

--- packages/attestation-service/src/db.ts
+++ packages/attestation-service/src/db.ts
@@ -1,6 +1,5 @@
 import { ContractKit, newKit } from '@celo/contractkit'
 import { FindOptions, Sequelize } from 'sequelize'
-import { Block } from 'web3-eth'
 import { fetchEnv, getAttestationSignerAddress } from './env'
 import { rootLogger } from './logger'
 import Attestation, { AttestationModel, AttestationStatic } from './models/attestation'
@@ -57,23 +56,8 @@ export async function initializeKit() {
     kit = newKit(fetchEnv('CELO_PROVIDER'))
     // Copied from @celo/cli/src/utils/helpers
     try {
-      const syncProgress = await kit.web3.eth.isSyncing()
-      if (typeof syncProgress === 'boolean' && !syncProgress) {
-        const latestBlock: Block = await kit.web3.eth.getBlock('latest')
-        if (latestBlock && latestBlock.number > 0) {
-          // To catch the case in which syncing has happened in the past,
-          // has stopped, and hasn't started again, check for an old timestamp
-          // on the latest block
-          const ageOfBlock = Date.now() / 1000 - Number(latestBlock.timestamp)
-          if (ageOfBlock > 120) {
-            throw new Error(
-              `Latest block is ${ageOfBlock} seconds old, and syncing is not currently in progress`
-            )
-          }
-        }
-      } else {
-        throw new Error('Node is not synced')
-      }
+      const { ageOfLatestBlock } = await getAgeOfLatestBlock()
+      rootLogger.info({ ageOfLatestBlock }, 'Initializing ContractKit')
     } catch (error) {
       rootLogger.error(
         'Initializing Kit failed, are you running your node and specified it with the "CELO_PROVIDER" env var?. It\' currently set as ' +
