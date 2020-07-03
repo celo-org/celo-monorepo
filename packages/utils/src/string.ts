daa7a5881a9002432ab4b65ad3c11c3ea5e9235b
Nam Chu Hoai <nambrot@googlemail.com>
2020-07-03 18:05:01 +0200

    Add string utils

---
+++ packages/utils/src/string.ts
@@ -0,0 +1,7 @@
+export function appendPath(baseUrl: string, path: string) {
+  const lastChar = baseUrl[baseUrl.length - 1]
+  if (lastChar === '/') {
+    return baseUrl + path
+  }
+  return baseUrl + '/' + path
+}
