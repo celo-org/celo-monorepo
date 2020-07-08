a895c4a32c34cb3e72f901d16d82fca95eecfb19
Cody Born <codyborn@codys-mbp.lan>
2020-07-07 23:24:21 -0700

    Export config-utils

--- packages/phone-number-privacy/common/src/index.ts
+++ packages/phone-number-privacy/common/src/index.ts
@@ -1,2 +1,3 @@
 export { ErrorMessage, WarningMessage } from './interfaces/error-utils'
 export { SignMessageResponse, SignMessageResponseFailure } from './interfaces/SignMessageResponse'
+export { toBool, toNum } from './utils/config-utils'
