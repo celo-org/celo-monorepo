f9faa56ab3ba7302f7d5eb37763d116fa441aa0c
annakaz <annakaz@users.noreply.github.com>
2020-07-16 18:24:16 -0700

    Add wei per unit const

--- packages/notification-service/src/exchange/exchangeQuery.ts
+++ packages/notification-service/src/exchange/exchangeQuery.ts
@@ -6,9 +6,10 @@ import { getContractKit } from '../util/utils'
 
 // Amounts to estimate the exchange rate, as the rate varies based on transaction size
 // A small amount returns a rate closer to the median rate
+const WEI_PER_UNIT = 1000000000000000000
 const SELL_AMOUNTS = {
-  [CURRENCY_ENUM.DOLLAR]: new BigNumber(0.01 * 1000000000000000000), // 0.01 dollar
-  [CURRENCY_ENUM.GOLD]: new BigNumber(0.01 * 1000000000000000000), // 0.01 gold
+  [CURRENCY_ENUM.DOLLAR]: new BigNumber(0.01 * WEI_PER_UNIT), // 0.01 dollar
+  [CURRENCY_ENUM.GOLD]: new BigNumber(0.01 * WEI_PER_UNIT), // 0.01 gold
 }
 
 export async function handleExchangeQuery() {
