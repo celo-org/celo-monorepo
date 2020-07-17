d28f48da30159605a1ec07af303cfc03cf54e8e8
David Michael <david@bisontrails.co>
2020-07-16 20:18:17 -0400

    another test fix

--- packages/cli/src/commands/account/authorize.test.ts
+++ packages/cli/src/commands/account/authorize.test.ts
@@ -21,6 +21,7 @@ testWithGanache('account:authorize cmd', (web3: Web3) => {
       accounts[1],
       '--signature',
       '0x1b9fca4bbb5bfb1dbe69ef1cddbd9b4202dcb6b134c5170611e1e36ecfa468d7b46c85328d504934fce6c2a1571603a50ae224d2b32685e84d4d1a1eebad8452eb',
+      '--ecdsaOnly',
     ])
   })
 
