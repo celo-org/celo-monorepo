6853e3b3083779921fbfe573e3753195fc897e46
annakaz <annakaz@users.noreply.github.com>
2020-07-21 14:16:30 -0700

    Prod moonpay auth changes

--- packages/moonpay-auth/src/index.ts
+++ packages/moonpay-auth/src/index.ts
@@ -1,13 +1,20 @@
 import crypto from 'crypto'
 import * as functions from 'firebase-functions'
-import { MOONPAY_PUBLIC_KEY, MOONPAY_SECRET_KEY, MOONPAY_URL } from './config'
+import {
+  MOONPAY_PUBLIC_KEY_PROD,
+  MOONPAY_PUBLIC_KEY_STAGING,
+  MOONPAY_SECRET_KEY_PROD,
+  MOONPAY_SECRET_KEY_STAGING,
+  MOONPAY_URL_PROD,
+  MOONPAY_URL_STAGING,
+} from './config'
 const URL = require('url').URL
 
-export const signMoonpay = functions.https.onRequest((request, response) => {
+export const signMoonpayStaging = functions.https.onRequest((request, response) => {
   const url =
-    MOONPAY_URL +
+    MOONPAY_URL_STAGING +
     '?apiKey=' +
-    MOONPAY_PUBLIC_KEY +
+    MOONPAY_PUBLIC_KEY_STAGING +
     '&currencyCode=' +
     request.body.currency +
     '&walletAddress=' +
@@ -19,7 +26,33 @@ export const signMoonpay = functions.https.onRequest((request, response) => {
   console.log(`Requested signature for: ${url}`)
 
   const signature = crypto
-    .createHmac('sha256', MOONPAY_SECRET_KEY)
+    .createHmac('sha256', MOONPAY_SECRET_KEY_STAGING)
+    .update(new URL(url).search)
+    .digest('base64')
+
+  const urlWithSignature = `${url}&signature=${encodeURIComponent(signature)}`
+  console.log(`Returning signed URL: ${urlWithSignature}`)
+  response.send(JSON.stringify({ url: urlWithSignature }))
+})
+
+export const signMoonpayProd = functions.https.onRequest((request, response) => {
+  console.log(`Public key (non sensitive): ${MOONPAY_PUBLIC_KEY_PROD}`)
+  const url =
+    MOONPAY_URL_PROD +
+    '?apiKey=' +
+    MOONPAY_PUBLIC_KEY_PROD +
+    '&currencyCode=' +
+    request.body.currency +
+    '&walletAddress=' +
+    request.body.address +
+    '&baseCurrencyCode=' +
+    request.body.fiatCurrency +
+    '&baseCurrencyAmount=' +
+    request.body.fiatAmount
+  console.log(`Requested signature for: ${url}`)
+
+  const signature = crypto
+    .createHmac('sha256', MOONPAY_SECRET_KEY_PROD)
     .update(new URL(url).search)
     .digest('base64')
 
