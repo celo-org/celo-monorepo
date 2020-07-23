celo-org/celo-monorepo

commit 96eadd62145abefb988899be88295e59ab6802fa

[Wallet] Fix Firebase regressions on iOS
### Description

Follow up PR to #3320

This PR fixes crashes on iOS related to the database module.
I had already patched those for `react-native-firebase` v5 (see https://github.com/invertase/react-native-firebase/pull/2734).
Turns out a similar patch is still needed for v6. I will submit a PR upstream too for v6.

Also fixed broken push registration on iOS. There was a new call needed with v6.
I upgraded `react-native-firebase` to v6.7.1 since it changed push registration too, this way we're already on top again (but for how long? 😉).

These issues are reproducible only when using a real device.

### Tested

- App doesn't crash anymore on iOS in release mode.
- Push registration works

I could NOT test push being received successfully as `notification-service` currently has a similar issue as `blockchain-api` did a couple of days ago (fixed in #3583).

### Related issues

- Fixes #3622
- Fixes #3623

### Backwards compatibility

Yes.

packages/mobile/src/firebase/firebase.ts
@@ -85,6 +85,9 @@ export function* initializeCloudMessaging(app: ReactNativeFirebase.Module, addre
     }
   }
 
+  // `registerDeviceForRemoteMessages` must be called before calling `getToken`
+  // Note: `registerDeviceForRemoteMessages` is really only required for iOS and is a no-op on Android
+  yield call([app.messaging(), 'registerDeviceForRemoteMessages'])
   const fcmToken = yield call([app.messaging(), 'getToken'])
   if (fcmToken) {
     yield call(registerTokenToDb, app, address, fcmToken)

packages/mobile/src/firebase/firebase.test.ts
@@ -12,6 +12,7 @@ import { mockAccount2 } from 'test/values'
 
 const hasPermissionMock = jest.fn(() => null)
 const requestPermissionMock = jest.fn(() => null)
+const registerDeviceForRemoteMessagesMock = jest.fn(() => null)
 const getTokenMock = jest.fn(() => null)
 const onTokenRefreshMock = jest.fn(() => null)
 const onMessageMock = jest.fn(() => null)
@@ -26,6 +27,7 @@ const app: any = {
   messaging: () => ({
     hasPermission: hasPermissionMock,
     requestPermission: requestPermissionMock,
+    registerDeviceForRemoteMessages: registerDeviceForRemoteMessagesMock,
     getToken: getTokenMock,
     onTokenRefresh: onTokenRefreshMock,
     setBackgroundMessageHandler,

patches/@react-native-firebase/database+6.7.1.patch
@@ -1,0 +1,104 @@
+patch-package
+--- a/node_modules/@react-native-firebase/database/ios/RNFBDatabase/RNFBDatabaseCommon.h
++++ b/node_modules/@react-native-firebase/database/ios/RNFBDatabase/RNFBDatabaseCommon.h
+@@ -21,6 +21,8 @@
+ 
+ @interface RNFBDatabaseCommon : NSObject
+ 
+++ (dispatch_queue_t)sharedMethodQueue;
++
+ + (FIRDatabase *)getDatabaseForApp
+     :(FIRApp *)firebaseApp
+                              dbURL:(NSString *)dbURL;
+--- a/node_modules/@react-native-firebase/database/ios/RNFBDatabase/RNFBDatabaseCommon.m
++++ b/node_modules/@react-native-firebase/database/ios/RNFBDatabase/RNFBDatabaseCommon.m
+@@ -33,6 +33,16 @@ NSString *const DATABASE_PERSISTENCE_CACHE_SIZE = @"firebase_database_persistenc
+   configSettingsLock = [NSMutableDictionary dictionary];
+ }
+ 
+++ (dispatch_queue_t)sharedMethodQueue
++{
++    static dispatch_queue_t sharedQueue;
++    static dispatch_once_t onceToken;
++    dispatch_once(&onceToken, ^{
++        sharedQueue = dispatch_queue_create("io.invertase.firebase.database", DISPATCH_QUEUE_SERIAL);
++    });
++    return sharedQueue;
++}
++
+ + (FIRDatabase *)getDatabaseForApp:(FIRApp *)firebaseApp dbURL:(NSString *)dbURL {
+ 
+   FIRDatabase *firDatabase;
+--- a/node_modules/@react-native-firebase/database/ios/RNFBDatabase/RNFBDatabaseModule.m
++++ b/node_modules/@react-native-firebase/database/ios/RNFBDatabase/RNFBDatabaseModule.m
+@@ -29,7 +29,7 @@
+ RCT_EXPORT_MODULE();
+ 
+ - (dispatch_queue_t)methodQueue {
+-  return dispatch_queue_create("io.invertase.firebase.database", DISPATCH_QUEUE_SERIAL);
++  return RNFBDatabaseCommon.sharedMethodQueue;
+ }
+ 
+ #pragma mark -
+--- a/node_modules/@react-native-firebase/database/ios/RNFBDatabase/RNFBDatabaseOnDisconnectModule.m
++++ b/node_modules/@react-native-firebase/database/ios/RNFBDatabase/RNFBDatabaseOnDisconnectModule.m
+@@ -29,7 +29,7 @@
+ RCT_EXPORT_MODULE();
+ 
+ - (dispatch_queue_t)methodQueue {
+-  return dispatch_queue_create("io.invertase.firebase.database", DISPATCH_QUEUE_SERIAL);
++  return RNFBDatabaseCommon.sharedMethodQueue;
+ }
+ 
+ #pragma mark -
+--- a/node_modules/@react-native-firebase/database/ios/RNFBDatabase/RNFBDatabaseQueryModule.m
++++ b/node_modules/@react-native-firebase/database/ios/RNFBDatabase/RNFBDatabaseQueryModule.m
+@@ -32,7 +32,7 @@ static NSString *const RNFB_DATABASE_SYNC = @"database_sync_event";
+ RCT_EXPORT_MODULE();
+ 
+ - (dispatch_queue_t)methodQueue {
+-  return dispatch_queue_create("io.invertase.firebase.database", DISPATCH_QUEUE_SERIAL);
++  return RNFBDatabaseCommon.sharedMethodQueue;
+ }
+ 
+ - (id)init {
+--- a/node_modules/@react-native-firebase/database/ios/RNFBDatabase/RNFBDatabaseReferenceModule.m
++++ b/node_modules/@react-native-firebase/database/ios/RNFBDatabase/RNFBDatabaseReferenceModule.m
+@@ -29,7 +29,7 @@
+ RCT_EXPORT_MODULE();
+ 
+ - (dispatch_queue_t)methodQueue {
+-  return dispatch_queue_create("io.invertase.firebase.database", DISPATCH_QUEUE_SERIAL);
++  return RNFBDatabaseCommon.sharedMethodQueue;
+ }
+ 
+ #pragma mark -
+--- a/node_modules/@react-native-firebase/database/ios/RNFBDatabase/RNFBDatabaseTransactionModule.m
++++ b/node_modules/@react-native-firebase/database/ios/RNFBDatabase/RNFBDatabaseTransactionModule.m
+@@ -33,7 +33,7 @@ static NSString *const RNFB_DATABASE_TRANSACTION_EVENT = @"database_transaction_
+ RCT_EXPORT_MODULE();
+ 
+ - (dispatch_queue_t)methodQueue {
+-  return dispatch_queue_create("io.invertase.firebase.database", DISPATCH_QUEUE_SERIAL);
++  return RNFBDatabaseCommon.sharedMethodQueue;
+ }
+ 
+ - (id)init {
+@@ -57,6 +57,8 @@ RCT_EXPORT_METHOD(transactionStart:
+     : (nonnull NSNumber *) transactionId
+     : (BOOL) applyLocally
+ ) {
++  FIRDatabase *firDatabase = [RNFBDatabaseCommon getDatabaseForApp:firebaseApp dbURL:dbURL];
++  FIRDatabaseReference *firDatabaseReference = [RNFBDatabaseCommon getReferenceForDatabase:firDatabase path:path];
+   dispatch_async(_transactionQueue, ^{
+     NSMutableDictionary *transactionState = [NSMutableDictionary new];
+     dispatch_semaphore_t sema = dispatch_semaphore_create(0);
+@@ -64,8 +66,6 @@ RCT_EXPORT_METHOD(transactionStart:
+ #pragma ide diagnostic ignored "err_typecheck_convert_incompatible"
+     transactionState[@"semaphore"] = sema;
+ #pragma clang diagnostic pop
+-    FIRDatabase *firDatabase = [RNFBDatabaseCommon getDatabaseForApp:firebaseApp dbURL:dbURL];
+-    FIRDatabaseReference *firDatabaseReference = [RNFBDatabaseCommon getReferenceForDatabase:firDatabase path:path];
+ 
+     id runTransactionBlock = ^FIRTransactionResult *(FIRMutableData *currentData) {
+       dispatch_barrier_async(_transactionQueue, ^{

commit d181a9de939e7ce421d309b245ef4ed6d275ad99

[Wallet] Fix push permission never asked on iOS
This broke in https://github.com/celo-org/celo-monorepo/pull/3650
because yield call return types are not inferred yet :/

packages/mobile/src/firebase/firebase.ts
@@ -19,6 +19,7 @@ import { WritePaymentRequest } from 'src/firebase/actions'
 import { handleNotification } from 'src/firebase/notifications'
 import { navigateHome } from 'src/navigator/NavigationService'
 import Logger from 'src/utils/Logger'
+import { Awaited } from 'src/utils/typescript'
 
 const TAG = 'firebase/firebase'
 
@@ -82,8 +83,12 @@ export function* initializeCloudMessaging(app: ReactNativeFirebase.Module, addre
   Logger.info(TAG, 'Initializing Firebase Cloud Messaging')
 
   // this call needs to include context: https://github.com/redux-saga/redux-saga/issues/27
-  const enabled = yield call([app.messaging(), 'hasPermission'])
-  if (!enabled) {
+  // Manual type checking because yield calls can't infer return type yet :'(
+  const authStatus: Awaited<ReturnType<
+    FirebaseMessagingTypes.Module['hasPermission']
+  >> = yield call([app.messaging(), 'hasPermission'])
+  Logger.info(TAG, 'Current messaging authorization status', authStatus.toString())
+  if (authStatus === firebase.messaging.AuthorizationStatus.NOT_DETERMINED) {
     try {
       yield call([app.messaging(), 'requestPermission'])
     } catch (error) {
@@ -110,8 +115,8 @@ export function* initializeCloudMessaging(app: ReactNativeFirebase.Module, addre
 
   // Listen for notification messages while the app is open
   const channelOnNotification: EventChannel<NotificationChannelEvent> = eventChannel((emitter) => {
-    const unsuscribe = () => {
-      Logger.info(TAG, 'Notification channel closed, reseting callbacks. This is likely an error.')
+    const unsubscribe = () => {
+      Logger.info(TAG, 'Notification channel closed, resetting callbacks. This is likely an error.')
       app.messaging().onMessage(() => null)
       app.messaging().onNotificationOpenedApp(() => null)
     }
@@ -131,7 +136,7 @@ export function* initializeCloudMessaging(app: ReactNativeFirebase.Module, addre
         stateType: NotificationReceiveState.APP_FOREGROUNDED,
       })
     })
-    return unsuscribe
+    return unsubscribe
   })
   yield spawn(watchFirebaseNotificationChannel, channelOnNotification)
 

packages/mobile/src/utils/typescript.ts
@@ -21,7 +21,6 @@ export type ExtractProps<
  * Usage: array.filter(isPresent)
  * See https://github.com/microsoft/TypeScript/issues/16069#issuecomment-565658443
  */
-
 export function isPresent<T>(t: T | undefined | null | void): t is T {
   return t !== undefined && t !== null
 }
@@ -30,3 +29,10 @@ export function isPresent<T>(t: T | undefined | null | void): t is T {
 export function assertNever(x: never): never {
   throw new Error('Unexpected object: ' + x)
 }
+
+/**
+ * Unwrap type of promise.
+ * This can be removed once we switch to TypeScript 3.9 which directly supports the `awaited` keyword
+ * See https://github.com/microsoft/TypeScript/pull/35998
+ */
+export type Awaited<T> = T extends Promise<infer U> ? U : never

packages/mobile/src/firebase/firebase.test.ts
@@ -1,3 +1,4 @@
+import firebase from '@react-native-firebase/app'
 import { expectSaga } from 'redux-saga-test-plan'
 import { throwError } from 'redux-saga-test-plan/providers'
 import { call, select } from 'redux-saga/effects'
@@ -48,7 +49,10 @@ describe(initializeCloudMessaging, () => {
 
     await expectSaga(initializeCloudMessaging, app, address)
       .provide([
-        [call([app.messaging(), 'hasPermission']), false],
+        [
+          call([app.messaging(), 'hasPermission']),
+          firebase.messaging.AuthorizationStatus.NOT_DETERMINED,
+        ],
         [call([app.messaging(), 'requestPermission']), throwError(errorToRaise)],
         {
           spawn(effect, next) {