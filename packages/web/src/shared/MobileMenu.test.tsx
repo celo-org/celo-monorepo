7acfffeb35f3ef0bad587dfab63860fddc1f4fc9
aaron deruvo <aaron@celo.org>
2020-07-23 10:49:58 -0700

    lint

--- packages/web/src/shared/MobileMenu.test.tsx
+++ packages/web/src/shared/MobileMenu.test.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import * as renderer from 'react-test-renderer'
+import { TestProvider } from 'src/_page-tests/test-utils'
 import menu, { MAIN_MENU } from 'src/shared/menu-items'
 import MobileMenu from 'src/shared/MobileMenu'
-import { TestProvider } from 'src/_page-tests/test-utils'
 
 describe('Main MobileMenu', () => {
   it('renders', () => {
