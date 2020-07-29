afa7a4f2c5c1d3a321c022bb5febd7ffa5c83ccb
aaron deruvo <aaron@celo.org>
2020-07-28 17:39:25 -0700

    with the poem

--- packages/web/src/codename-allegory/Body.tsx
+++ packages/web/src/codename-allegory/Body.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
 import { Image, StyleSheet, View } from 'react-native'
-import AspectRatio from 'src/shared/AspectRatio'
 import Cambio from 'src/codename-allegory/cambio-flower.jpg'
-import { standardStyles, colors } from 'src/styles'
 import RingsGlyph from 'src/logos/RingsGlyph'
+import AspectRatio from 'src/shared/AspectRatio'
+import { colors, standardStyles } from 'src/styles'
+import Poem from 'src/codename-allegory/Poem'
 
 export default function Body() {
   return (
@@ -12,6 +13,7 @@ export default function Body() {
       <AspectRatio ratio={1} style={styles.flower}>
         <Image source={Cambio} style={standardStyles.image} />
       </AspectRatio>
+      <Poem />
     </View>
   )
 }
---
+++ packages/web/src/codename-allegory/Poem.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+import { H4 } from 'src/fonts/Fonts'
+import { fonts, textStyles, standardStyles } from 'src/styles'
+import Fade from 'react-reveal/Fade'
+
+export default function Poem() {
+  return (
+    <View style={styles.root}>
+      <H4 style={[textStyles.italic, standardStyles.elementalMarginBottom]}>As Wealth Flowers</H4>
+      <Text style={fonts.p}>
+        {STANZAS.map((verse) => (
+          <Fade>{verse} </Fade>
+        ))}
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  root: {
+    width: 380,
+  },
+})
+
+const STANZAS = [
+  `The story is
+An invitation, our imagination,
+our story, for these
+our renewed forms
+of prosperity`,
+  `Nourishing, through service
+Awakenings of purpose
+A magic within connection`,
+  `Gathering, together
+in generous abundance,
+held, by
+humbled enchantments of
+Our worth
+For we believe
+in beautiful money
+Seeding, with
+others, these
+New stories
+Of change
+Redeeming systems
+of exchange`,
+  `Making value beautiful
+within hands, within reach
+Committed through practice
+A divine
+gift, deserving
+of our acceptance`,
+  `A worthy wish, to serve
+A wish
+For healing in bloom
+A wish
+For a responsibility shared
+A prayer
+For collective prosperity`,
+  `A story,`,
+  `A blossom of beautiful money`,
+]
