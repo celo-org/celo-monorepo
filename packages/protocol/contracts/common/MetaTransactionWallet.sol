d1e5683f8ebfd9eafb608c8815405b3e31de7805
Asa Oines <a@celo.org>
2020-07-31 10:10:36 -0400

    Upgrade solc

--- packages/protocol/contracts/common/MetaTransactionWallet.sol
+++ packages/protocol/contracts/common/MetaTransactionWallet.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.3;
+pragma solidity ^0.5.12;
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/utils/Address.sol";
