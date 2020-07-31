3961c8d1a4714810b30dcdb015d5cd0aa28ef575
Jean Regisser <jean.regisser@gmail.com>
2020-07-31 13:49:54 +0200

    Show qr code payment reason in the send confirmation screen

--- packages/mobile/src/qrcode/utils.ts
+++ packages/mobile/src/qrcode/utils.ts
@@ -179,7 +179,7 @@ export function* handleBarcode(
         recipient,
         amount,
         reason: qrData.comment,
-        type: TokenTransactionType.Sent,
+        type: TokenTransactionType.PayRequest,
       }
       replace(Screens.SendConfirmation, { transactionData, isFromScan: true })
     } else {
