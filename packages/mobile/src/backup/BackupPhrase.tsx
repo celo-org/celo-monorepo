e239250ecdab81d65880cdc1df0f09e02d116c04
annakaz <annakaz@users.noreply.github.com>
2020-08-03 20:53:42 -0700

    Fix lint error

--- packages/mobile/src/backup/BackupPhrase.tsx
+++ packages/mobile/src/backup/BackupPhrase.tsx
@@ -15,7 +15,7 @@ import BackupPhraseContainer, {
 } from 'src/backup/BackupPhraseContainer'
 import CancelConfirm from 'src/backup/CancelConfirm'
 import { getStoredMnemonic, onGetMnemonicFail } from 'src/backup/utils'
-import i18n, { Namespaces, withTranslation } from 'src/i18n'
+import { Namespaces, withTranslation } from 'src/i18n'
 import { navigate, pushToStack } from 'src/navigator/NavigationService'
 import { Screens } from 'src/navigator/Screens'
 import { TopBarTextButton } from 'src/navigator/TopBarButton.v2'
