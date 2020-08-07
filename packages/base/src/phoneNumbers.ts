1947f14d95aadf6ba58f05ddc7bb40ad895c8332
Pasto <hbandura@gmail.com>
2020-08-06 21:37:21 -0300

    More redirections

--- packages/base/src/phoneNumbers.ts
+++ packages/base/src/phoneNumbers.ts
@@ -14,14 +14,14 @@ const E164_REGEX = /^\+[1-9][0-9]{1,14}$/
 export const getPhoneHash = (
   phoneNumber: string,
   salt: string,
-  sha3: (a: string) => string
+  sha3: (a: string) => string | null
 ): string => {
   if (!phoneNumber || !isE164Number(phoneNumber)) {
     throw Error('Attempting to hash a non-e164 number: ' + phoneNumber)
   }
   const prefix = getIdentifierPrefix(IdentifierType.PHONE_NUMBER)
   const value = prefix + (salt ? phoneNumber + PHONE_SALT_SEPARATOR + salt : phoneNumber)
-  return sha3(value)
+  return sha3(value) as string
 }
 
 export function isE164Number(phoneNumber: string) {
