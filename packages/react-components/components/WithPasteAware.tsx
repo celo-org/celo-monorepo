6f98dbf8b53f1205dbc0329c851f94f7219a4edf
Ivan Sorokin <ivan@sorokin.io>
2020-08-07 20:11:14 +0300

    [Wallet] Add Paste button to Search Input (#4641)
    ### Description
    
    Add Paste icon to search input. This way if address is in clipboard - Paste icon would appear and allow user to quickly enter the address. https://www.figma.com/file/zt7aTQ5wuXycIwxq5oAmF9/Wallet--Refresh?node-id=986%3A4151
    
    ### Other changes
    
    Unify `PasteAware` logic and make it UI implementation agnostic.
    
    ### Tested
    
    iOS Simulator
    
    ### Related issues
    
    - Fixes #4402
    
    ### Backwards compatibility
    
    Yes
    
    ### Demo
    
    https://drive.google.com/file/d/1PlOO2aOq3zrqpu33tOfoxjzszeX5szkj/view?usp=sharing

---
+++ packages/react-components/components/WithPasteAware.tsx
@@ -0,0 +1,96 @@
+// HOC to add a paste button to a text input
+
+import * as React from 'react'
+import { AppState, Clipboard, TextInputProps } from 'react-native'
+
+interface PasteAwareProps {
+  value: string
+  shouldShowClipboard: (value: string) => boolean
+  onChangeText: (text: string) => void
+}
+
+export interface PasteAwareWrappedElementProps {
+  isPasteIconVisible: boolean
+  onPressPaste: () => void
+  onChangeText: (text: string) => void
+}
+
+interface PasteAwareState {
+  isPasteIconVisible: boolean
+  clipboardContent: string | null
+}
+
+export function withPasteAware<P extends TextInputProps>(
+  WrappedView: React.ComponentType<P & PasteAwareWrappedElementProps>
+) {
+  return class WithPasteAware extends React.Component<P & PasteAwareProps> {
+    state: PasteAwareState = {
+      isPasteIconVisible: false,
+      clipboardContent: null,
+    }
+
+    _interval?: number
+    _isMounted = false
+
+    async componentDidMount() {
+      this._isMounted = true
+      AppState.addEventListener('change', this.checkClipboardContents)
+      // TODO: make it work for iOS 14
+      // https://9to5mac.com/2020/06/24/ios-14-clipboard-notifications/
+      this._interval = window.setInterval(async () => {
+        await this.checkClipboardContents()
+      }, 1000) // Every 1s
+      await this.checkClipboardContents()
+    }
+
+    componentWillUnmount() {
+      this._isMounted = false
+      AppState.removeEventListener('change', this.checkClipboardContents)
+      clearInterval(this._interval)
+    }
+
+    checkClipboardContents = async () => {
+      try {
+        const clipboardContent = await Clipboard.getString()
+        if (!this._isMounted) {
+          return
+        }
+
+        const { shouldShowClipboard, value } = this.props
+        if (
+          clipboardContent &&
+          !(value && clipboardContent.toLowerCase().includes(value.toLowerCase())) &&
+          shouldShowClipboard(clipboardContent)
+        ) {
+          this.setState({ isPasteIconVisible: true, clipboardContent })
+        } else {
+          this.setState({ isPasteIconVisible: false, clipboardContent: null })
+        }
+      } catch (error) {
+        console.error('Error checking clipboard contents', error)
+      }
+    }
+
+    onPressPaste = () => {
+      const { clipboardContent } = this.state
+      if (!clipboardContent) {
+        console.error('Attempted to paste but clipboard content empty. Should never happen.')
+        return
+      }
+      this.setState({ isPasteIconVisible: false, clipboardContent: null })
+      this.props.onChangeText(clipboardContent)
+    }
+
+    render() {
+      const { isPasteIconVisible } = this.state
+
+      return (
+        <WrappedView
+          {...this.props}
+          isPasteIconVisible={isPasteIconVisible}
+          onPressPaste={this.onPressPaste}
+        />
+      )
+    }
+  }
+}
