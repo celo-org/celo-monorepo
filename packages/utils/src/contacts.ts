8f7072c27c20ff620bb3d93b1314eae2e7b51ecd
Pasto <hbandura@gmail.com>
2020-08-07 19:43:00 -0300

    forwarded collections and contacts

--- packages/utils/src/contacts.ts
+++ packages/utils/src/contacts.ts
@@ -1,29 +1,12 @@
+import * as base from '@celo/base/lib/contacts'
 import * as Web3Utils from 'web3-utils'
 
-export interface ContactPhoneNumber {
-  label?: string
-  number?: string
-}
-
-export interface MinimalContact {
-  recordID: string
-  displayName?: string
-  phoneNumbers?: ContactPhoneNumber[]
-  thumbnailPath?: string
-}
-
-export const getContactPhoneNumber = (contact: MinimalContact) => {
-  if (!contact) {
-    throw new Error('Invalid contact')
-  }
-
-  if (!contact.phoneNumbers || !contact.phoneNumbers.length) {
-    return null
-  }
-
-  // TODO(Rossy) find the right phone number based on the address
-  return contact.phoneNumbers[0].number
-}
+// Exports moved to @celo/base, forwarding them
+// here for backwards compatibility
+export import ContactPhoneNumber = base.ContactPhoneNumber
+export import MinimalContact = base.MinimalContact
+export import getContactPhoneNumber = base.getContactPhoneNumber
+export import isContact = base.isContact
 
 export const getContactNameHash = (contact: MinimalContact) => {
   if (!contact) {
@@ -31,11 +14,4 @@ export const getContactNameHash = (contact: MinimalContact) => {
   }
 
   return Web3Utils.keccak256(contact.displayName || '')
-}
-
-export function isContact(contactOrNumber: any): contactOrNumber is MinimalContact {
-  if (typeof contactOrNumber === 'object') {
-    return 'recordID' in contactOrNumber
-  }
-  return false
 }
