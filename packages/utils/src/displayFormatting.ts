05859c35f8eb08eb943937cc866dc58f65381c1a
Pasto <hbandura@gmail.com>
2020-08-07 19:57:44 -0300

    Forwarded future and displayFormatting

--- packages/utils/src/displayFormatting.ts
+++ packages/utils/src/displayFormatting.ts
@@ -1,10 +1,5 @@
-export function getErrorMessage(error: Error) {
-  // This replacement is because when the error reaches here, it's been wrapped
-  // by Error: multiple times
-  let errorMsg = error.message || error.name || 'unknown'
-  errorMsg = errorMsg.replace(/Error:/g, '')
-  if (error.stack) {
-    errorMsg += ' in ' + error.stack.substring(0, 100)
-  }
-  return errorMsg
-}
+import * as base from '@celo/base/lib/displayFormatting'
+
+// Exports moved to @celo/base, forwarding them
+// here for backwards compatibility
+export import getErrorMessage = base.getErrorMessage
