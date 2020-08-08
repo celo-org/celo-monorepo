3a21f1d84a92466967295ac2d636d9c6908cf689
Pasto <hbandura@gmail.com>
2020-08-08 04:43:42 -0300

    Update displayFormatting syntax

--- packages/utils/src/displayFormatting.ts
+++ packages/utils/src/displayFormatting.ts
@@ -1,5 +1,3 @@
-import * as base from '@celo/base/lib/displayFormatting'
-
 // Exports moved to @celo/base, forwarding them
 // here for backwards compatibility
-export import getErrorMessage = base.getErrorMessage
+export { getErrorMessage } from '@celo/base/lib/displayFormatting'
