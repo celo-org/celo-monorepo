ff452a26d4b943f41682c593fb85a80596e5d26a
alecps <alec@cLabs.co>
2020-08-09 13:42:01 -0400

    adds comment

--- packages/protocol/test/customHooks.ts
+++ packages/protocol/test/customHooks.ts
@@ -1,5 +1,7 @@
 import { sleep } from '../lib/test-utils'
 
+// Handles flakey `error: Invalid JSON RPC response: ""` error that seems to be caused by port exhaustion in CI.
+// See https://github.com/ethereum/web3.js/issues/3425 and https://github.com/ethereum/web3.js/issues/926.
 export const beforeEachWithRetries = (
   title: string,
   numRetries: number,
