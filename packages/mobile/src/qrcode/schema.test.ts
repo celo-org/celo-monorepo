fc8ce7a3306f4e435fc5d4200e6cfbb8df0ebf8c
Yorke Rhodes <yorhodes@celo.org>
2020-08-12 13:59:11 -0700

    Fix undefined serialization in URIs (#4676)
    * Fix undefined serialization
    
    * Update snapshots
    
    * Add strip undefined helper

--- packages/mobile/src/qrcode/schema.test.ts
+++ packages/mobile/src/qrcode/schema.test.ts
@@ -1,5 +1,5 @@
 import { zeroAddress } from 'ethereumjs-util'
-import { uriDataFromJson } from 'src/qrcode/schema'
+import { UriData, uriDataFromJson, uriDataFromUrl, urlFromUriData } from 'src/qrcode/schema'
 
 const validAddressData = { address: zeroAddress() }
 const validUserData = {
@@ -20,31 +20,65 @@ const validBeamAndGoPaymentData = {
   comment: '92a53156-c0f2-11ea-b3de-0242ac13000',
 }
 
-describe('#qrDataFromJson', () => {
-  const validParse = (...objects: object[]) =>
-    objects.forEach((obj) => uriDataFromJson(JSON.parse(JSON.stringify(obj))))
+describe('qrcode/schema', () => {
+  describe('#uriDataFromJson', () => {
+    const validParse = (...objects: object[]) =>
+      objects.forEach((obj) => uriDataFromJson(JSON.parse(JSON.stringify(obj))))
 
-  it('should parse valid address', () =>
-    validParse(validAddressData, validUserData, validLocalPaymentData))
+    it('should parse valid address', () =>
+      validParse(validAddressData, validUserData, validLocalPaymentData))
 
-  it('should parse valid user data', () => validParse(validUserData))
+    it('should parse valid user data', () => validParse(validUserData))
 
-  it('should parse valid local payment data', () => validParse(validLocalPaymentData))
+    it('should parse valid local payment data', () => validParse(validLocalPaymentData))
 
-  it('should parse valid BeamAndGo payment data', () => validParse(validBeamAndGoPaymentData))
+    it('should parse valid BeamAndGo payment data', () => validParse(validBeamAndGoPaymentData))
 
-  const invalidParse = (...pairs: Array<{ obj: object; s: string }>) =>
-    pairs.forEach((pair) => {
-      try {
-        uriDataFromJson(JSON.parse(JSON.stringify(pair.obj)))
-      } catch (e) {
-        expect(e).toEqual(new Error(pair.s))
-      }
+    const invalidParse = (...pairs: Array<{ obj: object; s: string }>) =>
+      pairs.forEach((pair) => {
+        try {
+          uriDataFromJson(JSON.parse(JSON.stringify(pair.obj)))
+        } catch (e) {
+          expect(e).toEqual(new Error(pair.s))
+        }
+      })
+
+    it('should parse with error on invalid address', () =>
+      invalidParse({
+        obj: { address: zeroAddress().slice(0, -1) },
+        s: 'is not a valid address',
+      }))
+  })
+
+  const data1: Partial<UriData> = {
+    address: '0x8902dBbE62F149841F2b05a63dFE615bD8F69340',
+    displayName: undefined,
+    e164PhoneNumber: undefined,
+  }
+  const url1 = urlFromUriData(data1)
+
+  const data2: Partial<UriData> = {
+    ...data1,
+    displayName: 'Steven Cowrie',
+    e164PhoneNumber: '+254720670799',
+  }
+  const url2 = urlFromUriData(data2)
+
+  describe('#urlFromUriData', () => {
+    it('should strip undefined values', () => {
+      expect(url1).toBe(`celo://wallet/pay?address=${data1.address}`)
     })
 
-  it('should parse with error on invalid address', () =>
-    invalidParse({
-      obj: { address: zeroAddress().slice(0, -1) },
-      s: 'is not a valid address',
-    }))
+    it('should include defined values', () => {
+      const params = new URLSearchParams(Object(data2))
+      expect(url2).toBe(encodeURI(`celo://wallet/pay?${params.toString()}`))
+    })
+  })
+
+  describe('#uriDataFromUrl', () => {
+    it('should parse correctly', () => {
+      uriDataFromUrl(url1)
+      uriDataFromUrl(url2)
+    })
+  })
 })
--- packages/mobile/src/qrcode/schema.ts
+++ packages/mobile/src/qrcode/schema.ts
@@ -29,13 +29,16 @@ export const uriDataFromJson = (obj: object): UriData => {
   }
   return either.right
 }
-export const uriDataFromUrl = (url: string) => uriDataFromJson(parse(url, true).query)
+export const uriDataFromUrl = (url: string) => uriDataFromJson(parse(decodeURI(url), true).query)
 
 enum UriMethod {
   pay = 'pay',
 }
 
+// removes undefined parameters for serialization
+export const stripUndefined = (obj: object) => JSON.parse(JSON.stringify(obj))
+
 export const urlFromUriData = (data: Partial<UriData>, method: UriMethod = UriMethod.pay) => {
-  const params = new URLSearchParams(Object(data))
-  return `celo://wallet/${method.toString()}?${params.toString()}`
+  const params = new URLSearchParams(stripUndefined(data))
+  return encodeURI(`celo://wallet/${method.toString()}?${params.toString()}`)
 }
