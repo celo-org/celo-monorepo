7989a652d102d4b127971d9fa622dc01d2515f58
Gonzalo Nardini <nardinigonzalom@gmail.com>
2020-08-18 21:09:27 -0300

    [Wallet] Witdraw CELO QR scan fixes (#4740)
    ### Description
    
    The withdraw CELO PR (https://github.com/celo-org/celo-monorepo/pull/4646) used the old QR format that was modified by another PR (https://github.com/celo-org/celo-monorepo/pull/4536/files). With the new format, scanning a QR is not returning the correct address. This PR fixes that and adds an error message if an invalid QR is scanned
    
    ### Tested
    
    On an Android physical phone.
    
    ### Related issues
    
    - Fixes #4732 and #4731

--- packages/mobile/src/exchange/WithdrawCeloQrScannerScreen.tsx
+++ packages/mobile/src/exchange/WithdrawCeloQrScannerScreen.tsx
@@ -6,13 +6,18 @@ import { componentStyles } from '@celo/react-components/styles/styles'
 import { StackScreenProps } from '@react-navigation/stack'
 import { memoize } from 'lodash'
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { showError } from 'src/alert/actions'
+import { ErrorMessages } from 'src/app/ErrorMessages'
 import BackButton from 'src/components/BackButton.v2'
+import { ADDRESS_LENGTH } from 'src/exchange/reducer'
 import i18n from 'src/i18n'
 import { nuxNavigationOptions } from 'src/navigator/Headers.v2'
 import { navigateBack } from 'src/navigator/NavigationService'
 import { Screens } from 'src/navigator/Screens'
 import { StackParamList } from 'src/navigator/types'
 import QRScanner from 'src/qrcode/QRScanner'
+import { uriDataFromUrl } from 'src/qrcode/schema'
 import { QrCode } from 'src/send/actions'
 import Logger from 'src/utils/Logger'
 
@@ -22,7 +27,7 @@ type Props = StackScreenProps<StackParamList, Screens.WithdrawCeloQrScannerScree
 
 function fetchAddressFromQrData(data: string): string | null {
   try {
-    return JSON.parse(data).address
+    return uriDataFromUrl(data).address
   } catch (error) {
     return null
   }
@@ -30,14 +35,19 @@ function fetchAddressFromQrData(data: string): string | null {
 
 function WithdrawCeloQrScannerScreen({ route }: Props) {
   const { onAddressScanned } = route.params
+  const dispatch = useDispatch()
 
   const onBarCodeDetected = memoize(
     (qrCode: QrCode) => {
       const qrData = qrCode.data
       Logger.debug(TAG, 'Bar code detected: ' + qrData)
       const address = fetchAddressFromQrData(qrData) || qrData
-      onAddressScanned(address)
-      navigateBack()
+      if (address.startsWith('0x') && address.length === ADDRESS_LENGTH) {
+        onAddressScanned(address)
+        navigateBack()
+      } else {
+        dispatch(showError(ErrorMessages.QR_FAILED_INVALID_ADDRESS))
+      }
     },
     (qrCode) => qrCode.data
   )
