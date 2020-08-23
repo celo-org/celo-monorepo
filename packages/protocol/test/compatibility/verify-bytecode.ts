827fe47658245b587c09140471b525eed47b8440
Marcin Chrzanowski <martin@c-labs.co>
2020-08-23 16:09:14 -0400

    Implement library DFS

--- packages/protocol/test/compatibility/verify-bytecode.ts
+++ packages/protocol/test/compatibility/verify-bytecode.ts
@@ -1,15 +1,47 @@
-import {
-  collectLibraryPositions,
-  collectLibraryAddresses,
-} from '@celo/protocol/lib/compatibility/verify-bytecode'
 import { LibraryLinks, linkLibraries } from '@celo/protocol/lib/bytecode'
-
+import { Artifact } from '@celo/protocol/lib/compatibility/internal'
+import {
+  collectLibraryAddresses,
+  collectLibraryPositions,
+  verifyBytecodesDfs,
+} from '@celo/protocol/lib/compatibility/verify-bytecode'
+import { assertThrowsAsync } from '@celo/protocol/lib/test-utils'
 import { getTestArtifacts } from '@celo/protocol/test/compatibility/common'
+
+import { RegistryInstance } from 'types'
 
 import { assert } from 'chai'
 
-const artifacts = getTestArtifacts('linked_libraries')
-const artifact = artifacts.getArtifactByName('TestContract')
+import truffleContract = require('truffle-contract')
+
+const Registry = artifacts.require('Registry')
+const Proxy = artifacts.require('Proxy')
+
+const makeTruffleContract = (artifact: Artifact) => {
+  const Contract = truffleContract({
+    abi: artifact.abi,
+    unlinked_binary: artifact.bytecode,
+  })
+  Contract.setProvider(web3.currentProvider)
+  Contract.setNetwork('development')
+
+  return Contract
+}
+
+const deployProxiedContract = async (Contract: any, from: string) => {
+  const proxy = await Proxy.new()
+  const contract = await Contract.new({ from })
+  await proxy._setImplementation(contract.address)
+  return Contract.at(proxy.address)
+}
+
+const buildArtifacts = getTestArtifacts('linked_libraries')
+const artifact = buildArtifacts.getArtifactByName('TestContract')
+
+const TestContract = makeTruffleContract(buildArtifacts.getArtifactByName('TestContract'))
+const LinkedLibrary1 = makeTruffleContract(buildArtifacts.getArtifactByName('LinkedLibrary1'))
+const LinkedLibrary2 = makeTruffleContract(buildArtifacts.getArtifactByName('LinkedLibrary2'))
+const LinkedLibrary3 = makeTruffleContract(buildArtifacts.getArtifactByName('LinkedLibrary3'))
 
 describe('#collectLibraryPositions()', () => {
   it('collects the right number of positions for each library', () => {
@@ -51,6 +83,51 @@ describe('#collectLibraryAddresses()', () => {
       assert.throws(() => {
         collectLibraryAddresses(incorrectBytecode, positions)
       })
+    })
+  })
+})
+
+contract('', (accounts) => {
+  let registry: RegistryInstance
+  let library1
+  let library2
+  let library3
+  let testContract
+  beforeEach(async () => {
+    registry = await Registry.new()
+
+    library1 = await deployProxiedContract(LinkedLibrary1, accounts[0])
+    library3 = await deployProxiedContract(LinkedLibrary3, accounts[0])
+    LinkedLibrary2.link('LinkedLibrary3', library3.address)
+    library2 = await deployProxiedContract(LinkedLibrary2, accounts[0])
+
+    TestContract.link('LinkedLibrary1', library1.address)
+    TestContract.link('LinkedLibrary2', library2.address)
+    testContract = await deployProxiedContract(TestContract, accounts[0])
+
+    await registry.setAddressFor('TestContract', testContract.address)
+  })
+
+  describe('verifyBytecodesDfs', () => {
+    it(`doesn't throw on matching contracts`, async () => {
+      await verifyBytecodesDfs(['TestContract'], buildArtifacts, registry, web3)
+      assert(true)
+    })
+
+    it(`throws when a contract's bytecodes don't match`, async () => {
+      const oldBytecode = artifact.deployedBytecode
+      artifact.deployedBytecode = '0x0' + oldBytecode.slice(3, artifact.deployedBytecode.length)
+      await assertThrowsAsync(verifyBytecodesDfs(['TestContract'], buildArtifacts, registry, web3))
+      artifact.deployedBytecode = oldBytecode
+    })
+
+    it(`throws when a library's bytecodes don't match`, async () => {
+      const libraryArtifact = buildArtifacts.getArtifactByName('LinkedLibrary1')
+      const oldBytecode = libraryArtifact.deployedBytecode
+      libraryArtifact.deployedBytecode =
+        oldBytecode.slice(0, 44) + '00' + oldBytecode.slice(46, oldBytecode.length)
+      await assertThrowsAsync(verifyBytecodesDfs(['TestContract'], buildArtifacts, registry, web3))
+      libraryArtifact.deployedBytecode = oldBytecode
     })
   })
 })
