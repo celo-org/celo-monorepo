d6c902b51a55c626927a5e1f952226ba7dd219bb
Marcin Chrzanowski <martin@c-labs.co>
2020-08-24 19:08:21 -0400

    Add script for verifying bytecode

---
+++ packages/protocol/scripts/truffle/verify-bytecode.ts
@@ -0,0 +1,19 @@
+import { getBuildArtifacts } from '@openzeppelin/upgrades'
+
+import { ProxyInstance, RegistryInstance } from 'types'
+
+import { CeloContractName, celoRegistryAddress } from '@celo/protocol/lib/registry-utils'
+import { verifyBytecodesDfs } from '@celo/protocol/lib/compatibility/verify-bytecode'
+
+const Registry: Truffle.Contract<RegistryInstance> = artifacts.require('Registry')
+const Proxy: Truffle.Contract<ProxyInstance> = artifacts.require('Proxy')
+
+module.exports = async (callback: (error?: any) => number) => {
+  try {
+    const registry = await Registry.at(celoRegistryAddress)
+    const buildArtifacts = getBuildArtifacts('./build/contracts')
+    verifyBytecodesDfs(Object.keys(CeloContractName), buildArtifacts, registry, Proxy, web3)
+  } catch (error) {
+    callback(error)
+  }
+}
