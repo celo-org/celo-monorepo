5db2d97b3c3f182f01abab8c5ae4510846d92d69
Marcin Chrzanowski <martin@c-labs.co>
2020-08-24 20:27:47 -0400

    Appease the linter

--- packages/protocol/scripts/truffle/verify-bytecode.ts
+++ packages/protocol/scripts/truffle/verify-bytecode.ts
@@ -2,8 +2,8 @@ import { getBuildArtifacts } from '@openzeppelin/upgrades'
 
 import { ProxyInstance, RegistryInstance } from 'types'
 
-import { CeloContractName, celoRegistryAddress } from '@celo/protocol/lib/registry-utils'
 import { verifyBytecodesDfs } from '@celo/protocol/lib/compatibility/verify-bytecode'
+import { CeloContractName, celoRegistryAddress } from '@celo/protocol/lib/registry-utils'
 
 const Registry: Truffle.Contract<RegistryInstance> = artifacts.require('Registry')
 const Proxy: Truffle.Contract<ProxyInstance> = artifacts.require('Proxy')
