1ffe864a18a6b9ec9d60b95a3ed3693ca2f509ac
codyborn <codyborn@outlook.com>
2020-08-26 17:53:21 -0700

    Add sleep

--- packages/phone-number-privacy/signer/src/key-management/azure-key-provider.ts
+++ packages/phone-number-privacy/signer/src/key-management/azure-key-provider.ts
@@ -4,11 +4,14 @@ import logger from '../common/logger'
 import config from '../config'
 import { KeyProviderBase } from './key-provider-base'
 
+const snooze = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
 export class AzureKeyProvider extends KeyProviderBase {
   public async fetchPrivateKeyFromStore() {
     try {
       const { vaultName, secretName } = config.keystore.azure
 
+      await snooze(7000)
       const keyVaultClient = new AzureKeyVaultClient(vaultName)
       const privateKey = await keyVaultClient.getSecret(secretName)
       this.setPrivateKey(privateKey)
