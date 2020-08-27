2c93134d2da5d7bbf1dc6fccae63fe7abff92b6e
Javier Cortejoso <javier.cortejoso@gmail.com>
2020-08-27 12:22:09 +0200

    Debugging

--- packages/celotool/src/lib/geth.ts
+++ packages/celotool/src/lib/geth.ts
@@ -506,7 +506,7 @@ export const simulateClient = async (
   // Assume the node is accessible via localhost with senderAddress unlocked
   const kit = newKit(web3Provider)
   kit.defaultAccount = senderAddress
-  const gasPriceFixed = 200000000
+  const gasPriceFixed = 10100000
 
   const baseLogMessage: any = {
     loadTestID: index,
