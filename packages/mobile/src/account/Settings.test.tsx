606045bc5e044bbfdb6da380437ec1a095d2aeb8
Ivan Sorokin <ivan@sorokin.io>
2020-08-28 20:37:27 +0200

    [Wallet] Hide CTAs with verification when insufficient balance (#4850)
    ### Description
    
    This hides CTAs with verification when balance is insufficient to retrieve a pepper if it's not cached.
    
    ### Tested
    
    iOS simulator
    
    ### Related issues
    
    - Fixes https://github.com/celo-org/celo-monorepo/issues/4748
    - Fixes https://github.com/celo-org/celo-monorepo/issues/4797
    
    ### Backwards compatibility
    
    Yes

--- packages/mobile/src/account/Settings.test.tsx
+++ packages/mobile/src/account/Settings.test.tsx
@@ -5,6 +5,7 @@ import * as renderer from 'react-test-renderer'
 import Settings from 'src/account/Settings'
 import { Screens } from 'src/navigator/Screens'
 import { createMockStore, getMockStackScreenProps } from 'test/utils'
+import { mockE164Number, mockE164NumberPepper } from 'test/values'
 
 describe('Account', () => {
   beforeAll(() => {
@@ -17,7 +18,16 @@ describe('Account', () => {
 
   it('renders correctly', () => {
     const tree = renderer.create(
-      <Provider store={createMockStore({})}>
+      <Provider
+        store={createMockStore({
+          account: {
+            e164PhoneNumber: mockE164Number,
+          },
+          identity: { e164NumberToSalt: { [mockE164Number]: mockE164NumberPepper } },
+          stableToken: { balance: '0.00' },
+          goldToken: { balance: '0.00' },
+        })}
+      >
         <Settings {...getMockStackScreenProps(Screens.Settings)} />
       </Provider>
     )
@@ -28,11 +38,23 @@ describe('Account', () => {
     const tree = renderer.create(
       <Provider
         store={createMockStore({
+          identity: { e164NumberToSalt: { [mockE164Number]: mockE164NumberPepper } },
+          stableToken: { balance: '0.00' },
+          goldToken: { balance: '0.00' },
           account: {
             devModeActive: true,
+            e164PhoneNumber: mockE164Number,
           },
         })}
       >
+        <Settings {...getMockStackScreenProps(Screens.Settings)} />
+      </Provider>
+    )
+    expect(tree).toMatchSnapshot()
+  })
+  it('renders correctly when verification is not possible', () => {
+    const tree = renderer.create(
+      <Provider store={createMockStore({})}>
         <Settings {...getMockStackScreenProps(Screens.Settings)} />
       </Provider>
     )
