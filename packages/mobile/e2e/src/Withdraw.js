57761b716f8a45286249667981094bb86e61852d
Gonzalo Nardini <gnardini@clabs.co>
2020-08-31 15:49:03 -0300

    Separate withdraw test in steps

--- packages/mobile/e2e/src/Withdraw.js
+++ packages/mobile/e2e/src/Withdraw.js
@@ -1,42 +1,49 @@
-import { enterPinUi } from './utils/utils'
+import { enterPinUi, isElementVisible } from './utils/utils'
 import { DEFAULT_RECIPIENT_ADDRESS } from './utils/consts'
 
 const CELO_TO_SEND = '0.001'
 
 export default Withdraw = () => {
-  it('Wallet Home -> Exchange CELO', async () => {
-    // Open Hamburguer menu and go to CELO screen.
+  it('Go to CELO screen', async () => {
     await element(by.id('Hamburguer')).tap()
     await waitFor(element(by.id('DrawerItem/CELO')))
       .toBeVisible()
-      .withTimeout(10000)
+      .withTimeout(2000)
     await element(by.id('DrawerItem/CELO')).tap()
-    // Go through the education flow.
-    for (let i = 0; i < 4; i++) {
-      await element(by.id('Education/progressButton')).tap()
+  })
+
+  it('Go through onboarding and tap on the withdraw button', async () => {
+    if (await isElementVisible('Education/progressButton')) {
+      for (let i = 0; i < 4; i++) {
+        await element(by.id('Education/progressButton')).tap()
+      }
     }
     await waitFor(element(by.id('WithdrawCELO')))
       .toBeVisible()
       .whileElement(by.id('ExchangeScrollView'))
       .scroll(50, 'down')
-    // Go to the Withdraw Celo screen and fill the data.
     await element(by.id('WithdrawCELO')).tap()
+  })
+
+  it('Fill the information and confirm withdrawal', async () => {
     await waitFor(element(by.id('AccountAddress')))
       .toBeVisible()
-      .withTimeout(10000)
+      .withTimeout(2000)
     await element(by.id('AccountAddress')).replaceText(DEFAULT_RECIPIENT_ADDRESS)
     await element(by.id('CeloAmount')).replaceText(CELO_TO_SEND)
     // Tap review to go to the review screen and confirm.
     await element(by.id('WithdrawReviewButton')).tap()
     await waitFor(element(by.id('ConfirmWithdrawButton')))
       .toBeVisible()
-      .withTimeout(10000)
+      .withTimeout(2000)
     await element(by.id('ConfirmWithdrawButton')).tap()
     // Uncomment if running this file only.
     // await enterPinUi()
-    // Make sure we return to the Exchange CELO screen after confirming.
+  })
+
+  it('Return to the Exchange CELO screen after confirming.', async () => {
     await waitFor(element(by.id('WithdrawCELO')))
       .toBeVisible()
-      .withTimeout(10000)
+      .withTimeout(2000)
   })
 }
