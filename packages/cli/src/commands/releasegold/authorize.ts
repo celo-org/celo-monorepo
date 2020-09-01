33eb3cb129fd78f53505e8ab6fbd6cd549ae7ef1
Victor "Nate" Graf <victor@celo.org>
2020-09-01 16:56:57 -0700

    Prevent accidental rotation of ECDSA validator key without BLS key (#4805)
    * Make it difficult to rotate a validator ECDSA key without rotating the BLS key
    
    * add dependsOn conditions to authorize flags
    
    * rename hasEnoughGold to hasEnoughCelo
    
    * fix reference in locked-gold command
    
    * remove
    
    * fix reference error
    
    * fix usage for unregistered validators
    
    * expect test to reject
    
    * update docs
    
    * Update packages/cli/src/commands/validator/update-bls-public-key.ts
    
    Co-authored-by: Gaston Ponti <pontigaston@gmail.com>
    
    * update docs
    
    Co-authored-by: Gaston Ponti <pontigaston@gmail.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- packages/cli/src/commands/releasegold/authorize.ts
+++ packages/cli/src/commands/releasegold/authorize.ts
@@ -22,10 +22,18 @@ export default class Authorize extends ReleaseGoldBaseCommand {
     blsKey: Flags.blsPublicKey({
       description:
         'The BLS public key that the validator is using for consensus, should pass proof of possession. 96 bytes.',
+      dependsOn: ['blsPop'],
     }),
     blsPop: Flags.blsProofOfPossession({
       description:
         'The BLS public key proof-of-possession, which consists of a signature on the account address. 48 bytes.',
+      dependsOn: ['blsKey'],
+    }),
+    force: flags.boolean({
+      description:
+        'Allow rotation of validator ECDSA key without rotating the BLS key. Only intended for validators with a special reason to do so.',
+      default: false,
+      hidden: true,
     }),
   }
 
@@ -42,9 +50,20 @@ export default class Authorize extends ReleaseGoldBaseCommand {
     const { flags } = this.parse(Authorize)
     const role = flags.role
 
-    await newCheckBuilder(this)
-      .isAccount(this.releaseGoldWrapper.address)
-      .runChecks()
+    // Check that the account is registered on-chain.
+    // Additionally, if the authorization is for a validator, the BLS key must be provided when the
+    // validator is already registered, and cannot be provided if the validator is not registered.
+    // (Because the BLS key is stored on the validator entry, which would not exist yet)
+    // Using the --force flag allows setting the ECDSA key on the validator without the BLS key.
+    const checker = newCheckBuilder(this).isAccount(this.releaseGoldWrapper.address)
+    if (flags.role === 'validator' && !flags.force) {
+      if (flags.blsKey && flags.blsPop) {
+        checker.isValidator(this.releaseGoldWrapper.address)
+      } else {
+        checker.isNotValidator(this.releaseGoldWrapper.address)
+      }
+    }
+    await checker.runChecks()
 
     const accounts = await this.kit.contracts.getAccounts()
     const sig = accounts.parseSignatureOfAddress(
@@ -60,12 +79,12 @@ export default class Authorize extends ReleaseGoldBaseCommand {
     let tx: any
     if (role === 'vote') {
       tx = await this.releaseGoldWrapper.authorizeVoteSigner(flags.signer, sig)
-    } else if (role === 'validator' && flags.blsKey) {
+    } else if (role === 'validator' && flags.blsKey && flags.blsPop) {
       tx = await this.releaseGoldWrapper.authorizeValidatorSignerAndBls(
         flags.signer,
         sig,
         flags.blsKey,
-        flags.blsPop!
+        flags.blsPop
       )
     } else if (role === 'validator') {
       tx = await this.releaseGoldWrapper.authorizeValidatorSigner(flags.signer, sig)
