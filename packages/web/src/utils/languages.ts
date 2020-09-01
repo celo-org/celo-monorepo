f69ec5f0ffe181cd621cd8b393d4381f6a9aa5e9
Isabelle Wei <isabelle.wei@yahoo.ca>
2020-09-01 14:51:55 -0400

    add all user language articles to press page

--- packages/web/src/utils/languages.ts
+++ packages/web/src/utils/languages.ts
@@ -1,12 +1,12 @@
 // https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
 
 export enum Languages {
-  'en' = 'English',
-  'es' = 'Spanish',
-  'ko' = 'Korean',
-  'fr' = 'French',
-  'de' = 'German',
-  'pt' = 'Portuguese',
-  'zh' = 'Chinese',
-  'tr' = 'Turkish',
+  'English' = 'en',
+  'Spanish' = 'es',
+  'Korean' = 'ko',
+  'French' = 'fr',
+  'German' = 'de',
+  'Portuguese' = 'pt',
+  'Chinese' = 'zh',
+  'Turkish' = 'tr',
 }
