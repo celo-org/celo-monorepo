f8744efa0c35d6652c39d8882d1d0e0a5f023233
Nitya Subramanian <nitya@celo.org>
2020-09-01 18:50:52 -0700

    Update packages/mobile/src/i18n.ts
    Co-authored-by: Anna K <anna.kazlauskas@gmail.com>

--- packages/mobile/src/i18n.ts
+++ packages/mobile/src/i18n.ts
@@ -13,6 +13,7 @@ import Logger from 'src/utils/Logger'
 
 const TAG = 'i18n'
 const TOS_LINK_DISPLAY = TOS_LINK.replace(/^https?\:\/\//i, '')
+const TOS_LINK_DISPLAY = TOS_LINK.replace(/^https?\:\/\//i, '')
 
 export enum Namespaces {
   accountScreen10 = 'accountScreen10',
