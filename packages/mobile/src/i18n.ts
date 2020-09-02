dc54efd0320c7865dfe5393864f73167d288632e
Nitya Subramanian <nitya@celo.org>
2020-09-01 18:25:49 -0700

    update TOS Link

--- packages/mobile/src/i18n.ts
+++ packages/mobile/src/i18n.ts
@@ -8,7 +8,7 @@ import {
   withTranslation as withTranslationI18Next,
 } from 'react-i18next'
 import * as RNLocalize from 'react-native-localize'
-import { APP_NAME } from 'src/config'
+import { APP_NAME, TOS_LINK } from 'src/config'
 import Logger from 'src/utils/Logger'
 
 const TAG = 'i18n'
@@ -91,7 +91,7 @@ i18n
     debug: true,
     interpolation: {
       escapeValue: false,
-      defaultVariables: { appName: APP_NAME },
+      defaultVariables: { appName: APP_NAME, tosLink: TOS_LINK },
     },
     missingInterpolationHandler: currencyInterpolator,
   })
