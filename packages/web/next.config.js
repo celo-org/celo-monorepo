4ebeaa0e9361bd492a20d61bae679481f668d42e
aaron deruvo <aaron@celo.org>
2020-09-03 14:34:02 -0700

    not yet

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -5,7 +5,6 @@ const serverEnvConfig = require('./server-env-config')
 module.exports = withImages({
   experimental: {
     granularChunks: true,
-    modern: true,
   },
   publicRuntimeConfig: envConfig,
   serverRuntimeConfig: serverEnvConfig,
