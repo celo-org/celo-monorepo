82c7f8d2101cb79e6ab672a041ae54b0bc5a9d5a
aaron deruvo <aaron@celo.org>
2020-09-04 10:28:33 -0700

    combine imports

--- packages/web/src/fauceting/PhoneInput.test.tsx
+++ packages/web/src/fauceting/PhoneInput.test.tsx
@@ -1,8 +1,7 @@
 import { fireEvent, render } from '@testing-library/react'
 import * as React from 'react'
+import { onPress, TestProvider } from 'src/_page-tests/test-utils'
 import PhoneInput from 'src/fauceting/PhoneInput'
-import { onPress } from 'src/_page-tests/test-utils'
-import { TestProvider } from 'src/_page-tests/test-utils'
 
 describe('PhoneInput', () => {
   describe('on initial', () => {
