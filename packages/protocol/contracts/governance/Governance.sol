54ca188169a9ff0d080b22c2e43f3374c706d263
yorhodes <yorhodes@celo.org>
2020-09-10 16:24:02 -0700

    Open 5052 for parameter minimums

--- packages/protocol/contracts/governance/Governance.sol
+++ packages/protocol/contracts/governance/Governance.sol
@@ -16,7 +16,6 @@ import "../common/UsingPrecompiles.sol";
 import "../common/interfaces/ICeloVersionedContract.sol";
 import "../common/libraries/ReentrancyGuard.sol";
 
-// TODO(asa): Hardcode minimum times for queueExpiry, etc.
 /**
  * @title A contract for making, passing, and executing on-chain governance proposals.
  */
