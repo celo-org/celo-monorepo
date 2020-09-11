bd02bf952a593610c4b704509f983c56e63fb03e
Gaston Ponti <pontigaston@gmail.com>
2020-09-11 10:25:58 -0300

    Fix blockchain api setup jest

--- packages/blockchain-api/setupJest.ts
+++ packages/blockchain-api/setupJest.ts
@@ -2,6 +2,6 @@ import * as fetch from 'jest-fetch-mock'
 
 jest.setMock('node-fetch', fetch)
 
-const customGlobal = global as fetch.GlobalWithFetchMock
+const customGlobal = (global as unknown) as fetch.GlobalWithFetchMock
 customGlobal.fetch = require('jest-fetch-mock')
 customGlobal.fetchMock = global.fetch
