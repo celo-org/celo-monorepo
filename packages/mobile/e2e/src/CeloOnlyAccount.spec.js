7823665645df7152c8e225a8172d26f306e8d08f
Gonzalo Nardini <gnardini@clabs.co>
2020-09-11 17:59:13 -0300

    Remove file

--- packages/mobile/e2e/src/CeloOnlyAccount.spec.js
+++
@@ -1,8 +0,0 @@
-import dismissBanners from './utils/banners'
-import Onboarding from './usecases/Onboarding'
-
-describe('CELO only Account', () => {
-  beforeEach(dismissBanners)
-
-  describe('Onboarding', Onboarding)
-})
