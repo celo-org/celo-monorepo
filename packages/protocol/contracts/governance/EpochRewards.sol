a412743da8cb3fa0b77dd3f7554eb07765883697
Emily Williams <emag3m@gmail.com>
2020-09-11 10:02:58 -0400

    EpochRewards carbon offsetting partner CAN be addr(0)

--- packages/protocol/contracts/governance/EpochRewards.sol
+++ packages/protocol/contracts/governance/EpochRewards.sol
@@ -189,7 +189,6 @@ contract EpochRewards is
    * @return True upon success.
    */
   function setCarbonOffsettingFund(address partner, uint256 value) public onlyOwner returns (bool) {
-    require(partner != address(0), "partner cannot be null address");
     require(partner != carbonOffsettingPartner || value != carbonOffsettingFraction.unwrap());
     require(value < FixidityLib.fixed1().unwrap());
     carbonOffsettingPartner = partner;
