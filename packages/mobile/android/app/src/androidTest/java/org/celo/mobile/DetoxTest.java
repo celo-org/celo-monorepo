133795ba40610ab6bcec74de7fa14e2ad3a20434
Gonzalo Nardini <gnardini@clabs.co>
2020-09-15 12:46:17 -0300

    Fix android detox config

--- packages/mobile/android/app/src/androidTest/java/org/celo/mobile/DetoxTest.java
+++ packages/mobile/android/app/src/androidTest/java/org/celo/mobile/DetoxTest.java
@@ -24,7 +24,7 @@ public class DetoxTest {
     DetoxConfig detoxConfig = new DetoxConfig();
     detoxConfig.idlePolicyConfig.masterTimeoutSec = 90;
     detoxConfig.idlePolicyConfig.idleResourceTimeoutSec = 60;
-    detoxConfig.rnContextLoadTimeoutSec = (com.example.BuildConfig.DEBUG ? 180 : 60);
+    detoxConfig.rnContextLoadTimeoutSec = (org.celo.mobile.BuildConfig.DEBUG ? 180 : 60);
 
     Detox.runTests(mActivityRule, detoxConfig);
   }
