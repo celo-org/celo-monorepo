ee03faa2ff47eb84a4ba8d155a44d10336abf433
aaron deruvo <aaron@celo.org>
2020-09-14 17:03:11 -0700

    move Celo FAQ to contentful so it can be kept up to date

--- packages/web/pages/faq.tsx
+++ packages/web/pages/faq.tsx
@@ -1,2 +1,5 @@
 import FAQ from 'src/privacy/FAQ'
+import getFAQData from 'src/privacy/getFAQData'
 export default FAQ
+
+export const getServerSideProps = getFAQData
---
+++ packages/web/src/privacy/getFAQData.ts
@@ -0,0 +1,29 @@
+import { GetServerSideProps } from 'next'
+import getConfig from 'next/config'
+import { getFAQ } from 'src/utils/contentful'
+
+const { publicRuntimeConfig } = getConfig()
+
+const preview = publicRuntimeConfig.ENV === 'development'
+
+const getServerSideProps: GetServerSideProps = async function getServerSideProp({ query }) {
+  
+  const locale = query.locale || 'en-US'
+  const faqs = await getFAQ({preview, locale})
+  return {
+    props: {
+      title: faqs.fields.title,
+      list: faqs.fields.list.map(item => {
+        return {
+          question: item.fields.question,
+          answer: item.fields.answer,
+          id: item.sys.id
+        }
+      }),
+      id: faqs.sys.id,
+      updatedAt: faqs.sys.updatedAt
+    }
+  }
+}
+
+export default getServerSideProps
