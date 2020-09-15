5f85a02b8dc0a4474fd142a84c9149333929ad4c
Gonzalo Nardini <gnardini@clabs.co>
2020-09-15 02:08:58 -0300

    typo

--- packages/mobile/e2e/src/ResetAccount.js
+++ packages/mobile/e2e/src/ResetAccount.js
@@ -1,18 +1,6 @@
 import { enterPinUiIfNecessary, waitForElementId, sleep } from './utils/utils'
 import { SAMPLE_BACKUP_KEY } from './utils/consts'
 
-// Yes, the try-catch looks weird. It's here to avoid flakiness on the scroll. Without it the scroll
-// line intermittently fails with a ~"Can't find view" error even though the SettingsScrollView is visible.
-// This probably doesn't reduce flakiness 100%, but in practive it reduces it from ~50% to a very
-// small % of the time.
-const scrollToBottomWithRetry = async (elementId) => {
-  try {
-    await element(by.id(elementId)).scrollTo('bottom')
-  } catch (err) {
-    await element(by.id(elementId)).scrollTo('bottom')
-  }
-}
-
 export default ResetAccount = () => {
   it('Reset Account by doing the Account Key quiz', async () => {
     // Go to Settings
@@ -21,7 +9,11 @@ export default ResetAccount = () => {
 
     // Scroll to bottom and start the reset process.
     await waitForElementId('SettingsScrollView')
-    await scrollToBottomWithRetry('SettingsScrollView')
+    // The sleep is here to avoid flakiness on the scroll. Without it the scroll to bottom intermittently fails
+    // with a ~"Can't find view" error even though the SettingsScrollView is visible.
+    // This probably doesn't reduce flakiness 100%, but in practice it reduces it significantly.
+    await sleep(5000)
+    await element(by.id(elementId)).scrollTo('bottom')
     await element(by.id('ResetAccount')).tap()
     await element(by.id('RemoveAccountModal/PrimaryAction')).tap()
 
