1c70e32370b893c24075183d0d26a631297ee5df
Tim Moreton <tim@celo.org>
2020-09-15 15:10:04 -0700

    CLI test cmd now checks delivery status

--- packages/cli/src/commands/identity/test-attestation-service.ts
+++ packages/cli/src/commands/identity/test-attestation-service.ts
@@ -1,6 +1,8 @@
 import { ClaimTypes, IdentityMetadataWrapper } from '@celo/contractkit/lib/identity'
+import { sleep } from '@celo/utils/lib/async'
 import { appendPath } from '@celo/utils/lib/string'
 import { flags as oFlags } from '@oclif/command'
+import { cli } from 'cli-ux'
 import fetch from 'cross-fetch'
 import { BaseCommand } from '../../base'
 import { newCheckBuilder } from '../../utils/checks'
@@ -72,14 +74,16 @@ export default class TestAttestationService extends BaseCommand {
     const signature = await this.kit.web3.eth.sign(phoneNumber + message, address)
 
     try {
-      const response = await fetch(
-        appendPath(attestationServiceUrlClaim.url, 'test_attestations'),
-        {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ phoneNumber, signature, message, provider }),
-        }
-      )
+      const testUrl = appendPath(attestationServiceUrlClaim.url, 'test_attestations')
+      cli.action.start(`Sending request to ${testUrl}`)
+
+      console.info()
+
+      const response = await fetch(testUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ phoneNumber, signature, message, provider }),
+      })
 
       if (!response.ok) {
         console.error('Request was not successful')
@@ -87,7 +91,57 @@ export default class TestAttestationService extends BaseCommand {
         console.error(`Response: ${await response.text()}`)
       }
 
+      cli.action.stop()
+
+      const testRes = JSON.parse(await response.text())
+      if (testRes.success !== 'true') {
+        console.error('Request was not successful')
+        cli.styledJSON(testRes)
+        return
+      }
+
       console.info('Request successful')
+
+      if (testRes.salt) {
+        // Service supports tracking attestation delivery status.
+        const getUrl = appendPath(attestationServiceUrlClaim.url, 'get_attestations')
+        cli.action.start(`Checking for delivery status at ${getUrl}`)
+        let latestGet = null
+        for (let i = 0; i < 6; i++) {
+          await sleep(5 * 1000)
+          const getResponse = await fetch(
+            getUrl +
+              '?' +
+              new URLSearchParams({
+                phoneNumber,
+                salt: testRes.salt,
+                issuer: testRes.issuer,
+                account: testRes.account,
+              }),
+            {
+              method: 'GET',
+              headers: { 'Content-Type': 'application/json' },
+            }
+          )
+
+          if (!getResponse.ok) {
+            console.error('Request was not successful')
+            console.error(`Status: ${getResponse.status}`)
+            console.error(`Response: ${await getResponse.text()}`)
+          }
+
+          latestGet = JSON.parse(await getResponse.text())
+
+          if (latestGet!.status === 'Delivered') {
+            break
+          }
+        }
+
+        cli.action.stop()
+        if (latestGet) {
+          cli.styledJSON(latestGet)
+        }
+      }
     } catch (error) {
       console.error(`Something went wrong`)
       console.error(error)
