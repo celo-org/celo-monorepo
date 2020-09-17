79e54ee70bde026903b9ca2c14d492aafb2dfac0
Marcin Chrzanowski <martin@c-labs.co>
2020-09-17 15:09:10 -0400

    Update comment

--- packages/protocol/lib/bytecode.ts
+++ packages/protocol/lib/bytecode.ts
@@ -24,7 +24,7 @@ export const stripMetadata = (bytecode: string): string => {
   return match[1]
 }
 
-// Maps library names to addresses (formatted without "0x" prefix).
+// Maps library names to their onchain addresses (formatted without "0x" prefix).
 export interface LibraryLinks {
   [name: string]: string
 }
