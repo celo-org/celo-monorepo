57eca5206104e213893dd588276a10c236380591
Jean Regisser <jean.regisser@gmail.com>
2020-10-16 21:29:40 +0200

    [Wallet] Support push notifications with an "open url" semantic (#5413)
    ### Description
    
    This PR adds support for push notifications with an "open url" semantic.
    When the `ou` (Open Url) data field is set, it redirects the user to that url.
    
    ### Other changes
    
    - Fix notification not handled when app is launched because of said notification
    - Fix notification body not displayed
    - Fix banner title color
    - `AlertBanner`/`showMessage` now supports an action parameter
    - Add test for notifications handling
    
    ### Tested
    
    **Example marketing notification**
    
    <Img src="https://user-images.githubusercontent.com/57791/96271233-ed112180-0fcc-11eb-86ec-0c3b58b58e14.png" width="50%" />
    
    **Payment received (before/after)**
    
    <Img src="https://user-images.githubusercontent.com/57791/96271645-6c065a00-0fcd-11eb-80d8-6910a5717dff.png" width="50%" /><Img src="https://user-images.githubusercontent.com/57791/96271339-09ad5980-0fcd-11eb-8b07-2f4f20f7d085.png" width="50%" />
    
    **Banner (before/after)**
    
    <Img src="https://user-images.githubusercontent.com/57791/96271709-7b85a300-0fcd-11eb-8907-0450ad6256c7.png" width="50%" /><Img src="https://user-images.githubusercontent.com/57791/96271524-48431400-0fcd-11eb-92c3-77380f1d3072.png" width="50%" />
    
    ### Related issues
    
    - Fixes #5325
    
    ### Backwards compatibility
    
    Yes

--- packages/mobile/src/alert/reducer.ts
+++ packages/mobile/src/alert/reducer.ts
@@ -7,19 +7,20 @@ export enum ErrorDisplayType {
   'INLINE',
 }
 
-export interface State {
+export type State = {
   type: 'message' | 'error'
   displayMethod: ErrorDisplayType
   message: string
   dismissAfter?: number | null
   buttonMessage?: string | null
+  action?: object | null
   title?: string | null
   underlyingError?: ErrorMessages | null
-}
+} | null
 
 const initialState = null
 
-export const reducer = (state: State | null = initialState, action: ActionTypes): State | null => {
+export const reducer = (state: State = initialState, action: ActionTypes): State => {
   switch (action.type) {
     case Actions.SHOW:
       return {
@@ -28,12 +29,17 @@ export const reducer = (state: State | null = initialState, action: ActionTypes)
         message: action.message,
         dismissAfter: action.dismissAfter,
         buttonMessage: action.buttonMessage,
+        action: action.action,
         title: action.title,
         underlyingError: action.underlyingError,
       }
     case Actions.HIDE:
       return null
     default:
+      if (state?.action === action) {
+        // Hide alert when the alert action is dispatched
+        return null
+      }
       return state
   }
 }
