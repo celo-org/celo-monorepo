celo-org/celo-monorepo

commit aafe6d5f7499e31cbecce251acc5502922864b77

MetaTransactionWallet ContractKit wrapper (#5156)
### Description

Create a contract wrapper for the MetaTransactionWallet.
It also implements a couple of convenience methods that allow for MetaTransaction building, included as a call to it's own `executeTransactions` method in order to create a batch of operations.
These signed meta transactions can then be submitted by anybody.

#### Nonce

One consideration here is whether we want the wrapper to do any fancy logic to keep track of the on-chain nonce of MetaTransactions. I briefly considered it but I think it would just introduce more source of confusion. I think we should thus leave the nonce setting/management at the discretion of the consumer of the wrapper. 

### Tested

Specs included covering a full range of usage.

### Related issues

- Fixes https://github.com/celo-org/komenci/issues/22

### Backwards compatibility

New addition no bw comp concerns.

packages/contractkit/src/base.ts
@@ -16,6 +16,7 @@ export enum CeloContract {
   GoldToken = 'GoldToken',
   Governance = 'Governance',
   LockedGold = 'LockedGold',
+  MetaTransactionWallet = 'MetaTransactionWallet',
   MetaTransactionWalletDeployer = 'MetaTransactionWalletDeployer',
   MultiSig = 'MultiSig',
   Random = 'Random',

packages/contractkit/src/contract-cache.ts
@@ -14,6 +14,7 @@ import { GasPriceMinimumWrapper } from './wrappers/GasPriceMinimum'
 import { GoldTokenWrapper } from './wrappers/GoldTokenWrapper'
 import { GovernanceWrapper } from './wrappers/Governance'
 import { LockedGoldWrapper } from './wrappers/LockedGold'
+import { MetaTransactionWalletWrapper } from './wrappers/MetaTransactionWallet'
 import { MetaTransactionWalletDeployerWrapper } from './wrappers/MetaTransactionWalletDeployer'
 import { MultiSigWrapper } from './wrappers/MultiSig'
 import { ReserveWrapper } from './wrappers/Reserve'
@@ -39,6 +40,7 @@ const WrapperFactories = {
   [CeloContract.LockedGold]: LockedGoldWrapper,
   // [CeloContract.Random]: RandomWrapper,
   // [CeloContract.Registry]: RegistryWrapper,
+  [CeloContract.MetaTransactionWallet]: MetaTransactionWalletWrapper,
   [CeloContract.MetaTransactionWalletDeployer]: MetaTransactionWalletDeployerWrapper,
   [CeloContract.MultiSig]: MultiSigWrapper,
   [CeloContract.Reserve]: ReserveWrapper,
@@ -66,6 +68,7 @@ interface WrapperCacheMap {
   [CeloContract.GoldToken]?: GoldTokenWrapper
   [CeloContract.Governance]?: GovernanceWrapper
   [CeloContract.LockedGold]?: LockedGoldWrapper
+  [CeloContract.MetaTransactionWallet]?: MetaTransactionWalletWrapper
   [CeloContract.MetaTransactionWalletDeployer]?: MetaTransactionWalletDeployerWrapper
   [CeloContract.MultiSig]?: MultiSigWrapper
   // [CeloContract.Random]?: RandomWrapper,
@@ -131,6 +134,9 @@ export class WrapperCache {
   }
   getLockedGold() {
     return this.getContract(CeloContract.LockedGold)
+  }
+  getMetaTransactionWallet(address: string) {
+    return this.getContract(CeloContract.MetaTransactionWallet, address)
   }
   getMetaTransactionWalletDeployer(address: string) {
     return this.getContract(CeloContract.MetaTransactionWalletDeployer, address)

packages/contractkit/src/kit.test.ts
@@ -34,6 +34,7 @@ export function txoStub<T>(): TransactionObjectStub<T> {
     rejectHash: peStub.rejectHash,
     resolveReceipt: peStub.resolveReceipt,
     rejectReceipt: peStub.resolveReceipt,
+    _parent: jest.fn() as any,
   }
   return pe
 }

packages/contractkit/src/kit.ts
@@ -1,8 +1,11 @@
+import { ensureLeading0x, Signature } from '@celo/base'
+import { EIP712TypedData, generateTypedDataHash } from '@celo/utils/lib/sign-typed-data-utils'
+import { parseSignatureWithoutPrefix } from '@celo/utils/lib/signatureUtils'
 import { BigNumber } from 'bignumber.js'
 import debugFactory from 'debug'
 import net from 'net'
 import Web3 from 'web3'
-import { Tx } from 'web3-core'
+import { HttpProvider, Tx } from 'web3-core'
 import { TransactionObject } from 'web3-eth'
 import { AddressRegistry } from './address-registry'
 import { Address, CeloContract, CeloToken } from './base'
@@ -350,6 +353,30 @@ export class ContractKit {
         gas,
       })
     )
+  }
+
+  async signTypedData(signer: string, typedData: EIP712TypedData): Promise<Signature> {
+    const signature = await new Promise<string>((resolve, reject) => {
+      ;(this.web3.currentProvider as Pick<HttpProvider, 'send'>).send(
+        {
+          jsonrpc: '2.0',
+          method: 'eth_signTypedData',
+          params: [signer, typedData],
+        },
+        (error, resp) => {
+          if (error) {
+            reject(error)
+          } else if (resp) {
+            resolve(resp.result as string)
+          } else {
+            reject(new Error('empty-response'))
+          }
+        }
+      )
+    })
+
+    const messageHash = ensureLeading0x(generateTypedDataHash(typedData).toString('hex'))
+    return parseSignatureWithoutPrefix(messageHash, signature, signer)
   }
 
   private fillTxDefaults(tx?: Tx): Tx {

packages/contractkit/src/web3-contract-cache.ts
@@ -15,6 +15,7 @@ import { newGasPriceMinimum } from './generated/GasPriceMinimum'
 import { newGoldToken } from './generated/GoldToken'
 import { newGovernance } from './generated/Governance'
 import { newLockedGold } from './generated/LockedGold'
+import { newMetaTransactionWallet } from './generated/MetaTransactionWallet'
 import { newMetaTransactionWalletDeployer } from './generated/MetaTransactionWalletDeployer'
 import { newMultiSig } from './generated/MultiSig'
 import { newProxy } from './generated/Proxy'
@@ -45,6 +46,7 @@ export const ContractFactories = {
   [CeloContract.GoldToken]: newGoldToken,
   [CeloContract.Governance]: newGovernance,
   [CeloContract.LockedGold]: newLockedGold,
+  [CeloContract.MetaTransactionWallet]: newMetaTransactionWallet,
   [CeloContract.MetaTransactionWalletDeployer]: newMetaTransactionWalletDeployer,
   [CeloContract.MultiSig]: newMultiSig,
   [CeloContract.Random]: newRandom,
@@ -115,6 +117,9 @@ export class Web3ContractCache {
   }
   getLockedGold() {
     return this.getContract(CeloContract.LockedGold)
+  }
+  getMetaTransactionWallet(address: string) {
+    return this.getContract(CeloContract.MetaTransactionWallet, address)
   }
   getMetaTransactionWalletDeployer(address: string) {
     return this.getContract(CeloContract.MetaTransactionWalletDeployer, address)

packages/contractkit/src/wrappers/MetaTransactionWallet.test.ts
@@ -1,0 +1,264 @@
+import { Address, ensureLeading0x, Signature } from '@celo/base'
+import { testWithGanache } from '@celo/dev-utils/lib/ganache-test'
+import MTWContract from '@celo/protocol/build/contracts/MetaTransactionWallet.json'
+import { generateTypedDataHash } from '@celo/utils/lib/sign-typed-data-utils'
+import BigNumber from 'bignumber.js'
+import { newKitFromWeb3 } from '../kit'
+import { GoldTokenWrapper } from './GoldTokenWrapper'
+import {
+  buildMetaTxTypedData,
+  MetaTransactionWalletWrapper,
+  RawTransaction,
+  toRawTransaction,
+} from './MetaTransactionWallet'
+
+const contract = require('@truffle/contract')
+const MetaTransactionWallet = contract(MTWContract)
+
+testWithGanache('MetaTransactionWallet Wrapper', (web3) => {
+  MetaTransactionWallet.setProvider(web3.currentProvider)
+
+  const deployWallet = async (deployer: Address, signer: Address): Promise<Address> => {
+    const instance = await MetaTransactionWallet.new({ from: deployer })
+    await instance.initialize(signer, { from: deployer })
+    return instance.address
+  }
+
+  // Ganache returns 1 in chainId assembly code
+  const chainId = 1
+  const kit = newKitFromWeb3(web3)
+  let accounts: Address[]
+  let walletDeployer: Address
+  let walletSigner: Address
+  let wallet: MetaTransactionWalletWrapper
+  let gold: GoldTokenWrapper
+  let emptyAccounts: Address[]
+  let rando: Address
+
+  beforeAll(async () => {
+    accounts = await web3.eth.getAccounts()
+    walletDeployer = accounts[0]
+    walletSigner = accounts[1]
+    kit.defaultAccount = walletSigner
+    rando = accounts[2]
+    gold = await kit.contracts.getGoldToken()
+  })
+
+  beforeEach(async () => {
+    const walletAddress = await deployWallet(walletDeployer, walletSigner)
+    wallet = await kit.contracts.getMetaTransactionWallet(walletAddress)
+    // Ganache returns 1 in chainId assembly code
+    // @ts-ignore
+    wallet.chainId = () => Promise.resolve(chainId)
+
+    // Give the wallet some funds
+    await gold.transfer(wallet.address, new BigNumber(20e18).toFixed()).sendAndWaitForReceipt()
+    emptyAccounts = [0, 0, 0, 0, 0].map(() => web3.utils.randomHex(20))
+  })
+
+  describe('#executeTransaction', () => {
+    describe('as a rando', () => {
+      it('reverts', async () => {
+        expect.assertions(1)
+        await expect(
+          wallet
+            .executeTransaction(gold.transfer(emptyAccounts[0], 10000).txo)
+            .sendAndWaitForReceipt({ from: rando })
+        ).rejects.toThrow(/Invalid transaction sender/)
+      })
+    })
+
+    describe('as the signer', () => {
+      it('can call contracts', async () => {
+        const value = new BigNumber(1e18)
+        const result = await wallet
+          .executeTransaction(gold.transfer(emptyAccounts[0], value.toFixed()).txo)
+          .sendAndWaitForReceipt()
+        expect(result.status).toBe(true)
+        expect(await gold.balanceOf(emptyAccounts[0])).toEqual(value)
+      })
+    })
+  })
+
+  describe('#executeTransactions', () => {
+    describe('as a rando', () => {
+      it('reverts', async () => {
+        expect.assertions(1)
+        await expect(
+          wallet
+            .executeTransactions([
+              gold.transfer(emptyAccounts[0], 1000).txo,
+              gold.transfer(emptyAccounts[1], 1000).txo,
+            ])
+            .sendAndWaitForReceipt({ from: rando })
+        ).rejects.toThrow(/Invalid transaction sender/)
+      })
+    })
+
+    describe('as the signer', () => {
+      it('can execute transactions', async () => {
+        const walletBalanceBefore = await gold.balanceOf(wallet.address)
+        const value = new BigNumber(1e18)
+        const result = await wallet
+          .executeTransactions([
+            gold.transfer(emptyAccounts[0], value.toFixed()).txo,
+            gold.transfer(emptyAccounts[1], value.toFixed()).txo,
+          ])
+          .sendAndWaitForReceipt()
+        expect(result.status).toBe(true)
+
+        expect(await gold.balanceOf(wallet.address)).toEqual(
+          walletBalanceBefore.minus(value.times(2))
+        )
+        expect(await gold.balanceOf(emptyAccounts[0])).toEqual(value)
+        expect(await gold.balanceOf(emptyAccounts[1])).toEqual(value)
+      })
+    })
+  })
+
+  describe('#getMetaTransactionDigest', () => {
+    it('should match the digest created off-chain', async () => {
+      const metaTransfer = gold.transfer(emptyAccounts[0], 1000).txo
+      const onChainDigest = await wallet.getMetaTransactionDigest(metaTransfer, 0)
+      const typedData = buildMetaTxTypedData(
+        wallet.address,
+        toRawTransaction(metaTransfer),
+        0,
+        chainId
+      )
+      const offChainDigest = ensureLeading0x(generateTypedDataHash(typedData).toString('hex'))
+
+      expect(onChainDigest).toEqual(offChainDigest)
+    })
+  })
+
+  describe('#getMetaTransactionSigner', () => {
+    it('should match what is signed off-chain', async () => {
+      const metaTransfer = gold.transfer(emptyAccounts[0], 1000000).txo
+      const signature = await wallet.signMetaTransaction(metaTransfer, 0)
+      const signer = await wallet.getMetaTransactionSigner(metaTransfer, 0, signature)
+      expect(signer).toEqual(walletSigner)
+    })
+  })
+
+  describe('#signMetaTransaction', () => {
+    describe('with an unlocked account', () => {
+      it('returns a signature', async () => {
+        const signature = await wallet.signMetaTransaction(
+          gold.transfer(emptyAccounts[0], 1000).txo
+        )
+        expect(signature).toBeDefined()
+      })
+    })
+  })
+
+  describe('#executeMetaTransaction', () => {
+    describe('as a rando', () => {
+      it('can execute transactions', async () => {
+        const walletBalanceBefore = await gold.balanceOf(wallet.address)
+        const value = new BigNumber(1e18)
+
+        const metaTransfer = gold.transfer(emptyAccounts[0], value.toFixed()).txo
+        const signature = await wallet.signMetaTransaction(metaTransfer)
+
+        const result = await wallet
+          .executeMetaTransaction(metaTransfer, signature)
+          .sendAndWaitForReceipt({ from: rando })
+        expect(result.status).toBe(true)
+
+        expect(await gold.balanceOf(emptyAccounts[0])).toEqual(value)
+        expect(await gold.balanceOf(wallet.address)).toEqual(walletBalanceBefore.minus(value))
+      })
+
+      it('can batch transactions as a call to self', async () => {
+        const walletBalanceBefore = await gold.balanceOf(wallet.address)
+        const value = new BigNumber(1e18)
+        const metaBatch = wallet.executeTransactions([
+          gold.transfer(emptyAccounts[0], value.toFixed()).txo,
+          gold.transfer(emptyAccounts[1], value.toFixed()).txo,
+          gold.transfer(emptyAccounts[2], value.toFixed()).txo,
+        ]).txo
+
+        const signature = await wallet.signMetaTransaction(metaBatch, 0)
+        const result = await wallet
+          .executeMetaTransaction(metaBatch, signature)
+          .sendAndWaitForReceipt({ from: rando })
+        expect(result.status).toBe(true)
+
+        expect(await gold.balanceOf(wallet.address)).toEqual(
+          walletBalanceBefore.minus(value.times(3))
+        )
+        for (let i = 0; i < 3; i++) {
+          expect(await gold.balanceOf(emptyAccounts[i])).toEqual(value)
+        }
+      })
+    })
+
+    describe('when passed over the wire', () => {
+      it('can hydrate and execute directly', async () => {
+        const walletBalanceBefore = await gold.balanceOf(wallet.address)
+        const value = new BigNumber(1e18)
+        const metaTx = wallet.executeTransactions([
+          gold.transfer(emptyAccounts[0], value.toFixed()).txo,
+          gold.transfer(emptyAccounts[1], value.toFixed()).txo,
+          gold.transfer(emptyAccounts[2], value.toFixed()).txo,
+        ]).txo
+        const signature = await wallet.signMetaTransaction(metaTx, 0)
+        const rawTx = toRawTransaction(metaTx)
+        const payload = JSON.stringify({ rawTx, signature })
+        // Now we're somewhere else:
+        const resp: { rawTx: RawTransaction; signature: Signature } = JSON.parse(payload)
+        const result = await wallet
+          .executeMetaTransaction(resp.rawTx, resp.signature)
+          .sendAndWaitForReceipt({ from: rando })
+        expect(result.status).toBe(true)
+
+        expect(await gold.balanceOf(wallet.address)).toEqual(
+          walletBalanceBefore.minus(value.times(3))
+        )
+        for (let i = 0; i < 3; i++) {
+          expect(await gold.balanceOf(emptyAccounts[i])).toEqual(value)
+        }
+      })
+    })
+  })
+
+  describe('#signAndExecuteMetaTransaction', () => {
+    describe('as a rando', () => {
+      it('can execute transactions', async () => {
+        const walletBalanceBefore = await gold.balanceOf(wallet.address)
+        const value = new BigNumber(1e18)
+
+        const metaTransfer = gold.transfer(emptyAccounts[0], value.toFixed()).txo
+        const tx = await wallet.signAndExecuteMetaTransaction(metaTransfer)
+        const result = await tx.sendAndWaitForReceipt({ from: rando })
+        expect(result.status).toBe(true)
+
+        expect(await gold.balanceOf(emptyAccounts[0])).toEqual(value)
+        expect(await gold.balanceOf(wallet.address)).toEqual(walletBalanceBefore.minus(value))
+      })
+
+      it('can batch transactions as a call to self', async () => {
+        const walletBalanceBefore = await gold.balanceOf(wallet.address)
+        const value = new BigNumber(1e18)
+
+        const tx = await wallet.signAndExecuteMetaTransaction(
+          wallet.executeTransactions([
+            gold.transfer(emptyAccounts[0], value.toFixed()).txo,
+            gold.transfer(emptyAccounts[1], value.toFixed()).txo,
+            gold.transfer(emptyAccounts[2], value.toFixed()).txo,
+          ]).txo
+        )
+        const result = await tx.sendAndWaitForReceipt({ from: rando })
+        expect(result.status).toBe(true)
+
+        expect(await gold.balanceOf(wallet.address)).toEqual(
+          walletBalanceBefore.minus(value.times(3))
+        )
+        for (let i = 0; i < 3; i++) {
+          expect(await gold.balanceOf(emptyAccounts[i])).toEqual(value)
+        }
+      })
+    })
+  })
+})

packages/contractkit/src/wrappers/MetaTransactionWallet.ts
@@ -1,0 +1,290 @@
+import { ensureLeading0x, trimLeading0x } from '@celo/base'
+import { Signature } from '@celo/base/lib/signatureUtils'
+import { EIP712TypedData } from '@celo/utils/lib/sign-typed-data-utils'
+import BigNumber from 'bignumber.js'
+import { TransactionObject } from 'web3-eth'
+import { Address } from '../base'
+import { MetaTransactionWallet } from '../generated/MetaTransactionWallet'
+import {
+  BaseWrapper,
+  CeloTransactionObject,
+  proxyCall,
+  proxySend,
+  stringIdentity,
+  toTransactionObject,
+  valueToInt,
+  valueToString,
+} from './BaseWrapper'
+
+export interface TransactionObjectWithValue<T> {
+  txo: TransactionObject<T>
+  value: BigNumber.Value
+}
+
+export interface RawTransaction {
+  destination: string
+  value: string
+  data: string
+}
+
+export type TransactionInput<T> =
+  | TransactionObject<T>
+  | TransactionObjectWithValue<T>
+  | RawTransaction
+
+/**
+ * Class that wraps the MetaTransactionWallet
+ */
+export class MetaTransactionWalletWrapper extends BaseWrapper<MetaTransactionWallet> {
+  /**
+   * Execute a transaction originating from the MTW
+   * Reverts if the caller is not a signer
+   * @param tx a TransactionInput
+   */
+  public executeTransaction(tx: TransactionInput<any>): CeloTransactionObject<string> {
+    const rawTx = toRawTransaction(tx)
+    return toTransactionObject(
+      this.kit,
+      this.contract.methods.executeTransaction(rawTx.destination, rawTx.value, rawTx.data)
+    )
+  }
+
+  /**
+   * Execute a batch of transactions originating from the MTW
+   * Reverts if the caller is not a signer
+   * @param txs An array of TransactionInput
+   */
+  public executeTransactions(
+    txs: Array<TransactionInput<any>>
+  ): CeloTransactionObject<{ 0: string; 1: string[] }> {
+    const { destinations, values, callData, callDataLengths } = toTransactionBatch(txs)
+
+    return toTransactionObject(
+      this.kit,
+      this.contract.methods.executeTransactions(destinations, values, callData, callDataLengths)
+    )
+  }
+
+  /**
+   * Execute a signed meta transaction
+   * Reverts if meta-tx signer is not a signer for the wallet
+   * @param tx a TransactionInput
+   * @param signature a Signature
+   */
+  public executeMetaTransaction(
+    tx: TransactionInput<any>,
+    signature: Signature
+  ): CeloTransactionObject<string> {
+    const rawTx = toRawTransaction(tx)
+
+    return toTransactionObject(
+      this.kit,
+      this.contract.methods.executeMetaTransaction(
+        rawTx.destination,
+        rawTx.value,
+        rawTx.data,
+        signature.v,
+        signature.r,
+        signature.s
+      )
+    )
+  }
+
+  /**
+   * Signs a meta transaction as EIP712 typed data
+   * @param tx a TransactionWrapper
+   * @param nonce Optional -- will query contract state if not passed
+   * @returns signature a Signature
+   */
+  public async signMetaTransaction(tx: TransactionInput<any>, nonce?: number): Promise<Signature> {
+    if (nonce === undefined) {
+      nonce = await this.nonce()
+    }
+    const typedData = buildMetaTxTypedData(
+      this.address,
+      toRawTransaction(tx),
+      nonce,
+      await this.chainId()
+    )
+    const signer = await this.signer()
+    return this.kit.signTypedData(signer, typedData)
+  }
+
+  /**
+   * Execute a signed meta transaction
+   * Reverts if meta-tx signer is not a signer for the wallet
+   * @param tx a TransactionInput
+   */
+  public async signAndExecuteMetaTransaction(
+    tx: TransactionInput<any>
+  ): Promise<CeloTransactionObject<string>> {
+    const signature = await this.signMetaTransaction(tx)
+    return this.executeMetaTransaction(tx, signature)
+  }
+
+  private getMetaTransactionDigestParams = (
+    tx: TransactionInput<any>,
+    nonce: number
+  ): [string, string, string, number] => {
+    const rawTx = toRawTransaction(tx)
+    return [rawTx.destination, rawTx.value, rawTx.data, nonce]
+  }
+
+  getMetaTransactionDigest = proxyCall(
+    this.contract.methods.getMetaTransactionDigest,
+    this.getMetaTransactionDigestParams,
+    stringIdentity
+  )
+
+  private getMetaTransactionSignerParams = (
+    tx: TransactionInput<any>,
+    nonce: number,
+    signature: Signature
+  ): [string, string, string, number, number, string, string] => {
+    const rawTx = toRawTransaction(tx)
+    return [
+      rawTx.destination,
+      rawTx.value,
+      rawTx.data,
+      nonce,
+      signature.v,
+      signature.r,
+      signature.s,
+    ]
+  }
+  getMetaTransactionSigner = proxyCall(
+    this.contract.methods.getMetaTransactionSigner,
+    this.getMetaTransactionSignerParams,
+    stringIdentity
+  )
+
+  eip712DomainSeparator = proxyCall(this.contract.methods.eip712DomainSeparator)
+  isOwner = proxyCall(this.contract.methods.isOwner)
+  nonce = proxyCall(this.contract.methods.nonce, undefined, valueToInt)
+  private getSigner = proxyCall(this.contract.methods.signer, undefined, stringIdentity)
+
+  transferOwnership: (newOwner: Address) => CeloTransactionObject<void> = proxySend(
+    this.kit,
+    this.contract.methods.transferOwnership
+  )
+
+  setSigner: (newSigner: Address) => CeloTransactionObject<void> = proxySend(
+    this.kit,
+    this.contract.methods.setSigner
+  )
+
+  setEip712DomainSeparator: () => CeloTransactionObject<void> = proxySend(
+    this.kit,
+    this.contract.methods.setEip712DomainSeparator
+  )
+
+  /**
+   * Get and cache the chain ID -- assume it's static for a kit instance
+   * @returns chainId
+   */
+  _chainId?: number
+  private async chainId(): Promise<number> {
+    if (this._chainId === undefined) {
+      this._chainId = await this.kit.web3.eth.net.getId()
+    }
+    return this._chainId
+  }
+
+  /**
+   * Get an cache the signer - it should be static for a Wallet instance
+   * @returns signer
+   */
+  _signer?: Address
+  public async signer() {
+    if (this._signer === undefined) {
+      this._signer = await this.getSigner()
+    }
+    return this._signer
+  }
+}
+
+/**
+ * Turns any possible way to pass in a transaction into the raw values
+ * that are actually required. This is used both internally to normalize
+ * ways in which transactions are passed in but also public in order
+ * for one instance of ContractKit to serialize a meta transaction to
+ * send over the wire and be consumed somewhere else.
+ * @param tx TransactionInput<any> union of all the ways we expect transactions
+ * @returns a RawTransactions that's serializable
+ */
+export const toRawTransaction = (tx: TransactionInput<any>): RawTransaction => {
+  if ('destination' in tx) {
+    return tx
+  } else if ('value' in tx) {
+    return {
+      destination: tx.txo._parent.options.address,
+      data: tx.txo.encodeABI(),
+      value: valueToString(tx.value),
+    }
+  } else {
+    return {
+      destination: tx._parent.options.address,
+      data: tx.encodeABI(),
+      value: '0',
+    }
+  }
+}
+
+/**
+ * Turns an array of transaction inputs into the argument that
+ * need to be passed to the executeTransactions call.
+ * Main transformation is that all the `data` parts of each
+ * transaction in the batch are concatenated and an array
+ * of lengths is constructed.
+ * This is a gas optimisation on the contract.
+ * @param txs Array<TransactionInput<any>> array of txs
+ * @returns Params for the executeTransactions method call
+ */
+export const toTransactionBatch = (
+  txs: Array<TransactionInput<any>>
+): {
+  destinations: string[]
+  values: string[]
+  callData: string
+  callDataLengths: number[]
+} => {
+  const rawTxs: RawTransaction[] = txs.map(toRawTransaction)
+  return {
+    destinations: rawTxs.map((rtx) => rtx.destination),
+    values: rawTxs.map((rtx) => rtx.value),
+    callData: ensureLeading0x(rawTxs.map((rtx) => trimLeading0x(rtx.data)).join('')),
+    callDataLengths: rawTxs.map((rtx) => trimLeading0x(rtx.data).length / 2),
+  }
+}
+
+export const buildMetaTxTypedData = (
+  walletAddress: Address,
+  tx: RawTransaction,
+  nonce: number,
+  chainId: number
+): EIP712TypedData => {
+  return {
+    types: {
+      EIP712Domain: [
+        { name: 'name', type: 'string' },
+        { name: 'version', type: 'string' },
+        { name: 'chainId', type: 'uint256' },
+        { name: 'verifyingContract', type: 'address' },
+      ],
+      ExecuteMetaTransaction: [
+        { name: 'destination', type: 'address' },
+        { name: 'value', type: 'uint256' },
+        { name: 'data', type: 'bytes' },
+        { name: 'nonce', type: 'uint256' },
+      ],
+    },
+    primaryType: 'ExecuteMetaTransaction',
+    domain: {
+      name: 'MetaTransactionWallet',
+      version: '1.1',
+      chainId,
+      verifyingContract: walletAddress,
+    },
+    message: tx ? { ...tx, nonce } : {},
+  }
+}

packages/contractkit/types/web3-celo.d.ts
@@ -1,6 +1,8 @@
 import BigNumber from 'bignumber.js'
 import 'web3-core'
+// tslint:disable-next-line:no-duplicate-imports
 import { Tx } from 'web3-core'
+import { Contract } from 'web3-eth-contract'
 
 declare module 'web3-core' {
   export interface TransactionConfig extends TransactionConfig {
@@ -45,6 +47,7 @@ declare module 'web3-eth' {
     send(tx?: Tx): PromiEvent<T>
     estimateGas(tx?: Tx): Promise<number>
     encodeABI(): string
+    _parent: Contract
   }
 
   export type BlockType = string | number | BN | BigNumber | 'latest' | 'pending' | 'earliest'

commit 47cb67835c49000c75f006ebcb39681298a424e3

Remove MTW from CK registered contracts (#5523)
### Description

Remove MetaTransactionWallet from `RegisteredContracts` which is used in various indexers like `BlockExplorer` and `celocli network:contracts`

Fixes bugs introduced by https://github.com/celo-org/celo-monorepo/pull/5156

### Other changes

_Describe any minor or "drive-by" changes here._

### Tested

![Screen Shot 2020-10-22 at 10 52 37 AM](https://user-images.githubusercontent.com/3020995/96910688-b6b42600-1454-11eb-8c3d-5a931f4603e7.png)

### Related issues

- Fixes #[issue number here]

### Backwards compatibility

_Brief explanation of why these changes are/are not backwards compatible._

packages/contractkit/src/base.ts
@@ -33,7 +33,11 @@ export const ProxyContracts = Object.keys(CeloContract).map((c) => `${c}Proxy`)
 export type CeloToken = CeloContract.GoldToken | CeloContract.StableToken
 
 export const AllContracts = Object.keys(CeloContract) as CeloContract[]
-const AuxiliaryContracts = [CeloContract.MultiSig, CeloContract.MetaTransactionWalletDeployer]
+const AuxiliaryContracts = [
+  CeloContract.MultiSig,
+  CeloContract.MetaTransactionWalletDeployer,
+  CeloContract.MetaTransactionWallet,
+]
 export const RegisteredContracts = AllContracts.filter((v) => !AuxiliaryContracts.includes(v))
 
 export const NULL_ADDRESS = '0x0000000000000000000000000000000000000000' as Address