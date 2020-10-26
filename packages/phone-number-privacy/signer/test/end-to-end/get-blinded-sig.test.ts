0a7f2206f5951d2df134974a3bee0ea9299039bc
Alec Schaefer <alec@cLabs.co>
2020-10-26 16:39:57 -0400

    adds komenci support to ODIS (#5570)

--- packages/phone-number-privacy/signer/test/end-to-end/get-blinded-sig.test.ts
+++ packages/phone-number-privacy/signer/test/end-to-end/get-blinded-sig.test.ts
@@ -1,9 +1,16 @@
+import { newKitFromWeb3 } from '@celo/contractkit'
 import { TestUtils } from '@celo/phone-number-privacy-common'
 import { PhoneNumberUtils } from '@celo/utils'
 import { normalizeAddressWith0x, privateKeyToAddress } from '@celo/utils/lib/address'
 import { serializeSignature, signMessage } from '@celo/utils/lib/signatureUtils'
 import 'isomorphic-fetch'
 import Web3 from 'web3'
+import {
+  getBlindedPhoneNumber,
+  registerWalletAddress,
+  replenishQuota,
+} from '../../../common/src/test/utils'
+
 import config from '../../src/config'
 
 require('dotenv').config()
@@ -18,6 +25,8 @@ const PRIVATE_KEY1 = '0x1234567890abcdef1234567890abcdef1234567890abcdef12345678
 const ACCOUNT_ADDRESS1 = normalizeAddressWith0x(privateKeyToAddress(PRIVATE_KEY1))
 const PRIVATE_KEY2 = '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890fdeccc'
 const ACCOUNT_ADDRESS2 = privateKeyToAddress(PRIVATE_KEY2)
+const PRIVATE_KEY3 = '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890fffff1d'
+const ACCOUNT_ADDRESS3 = normalizeAddressWith0x(privateKeyToAddress(PRIVATE_KEY3))
 const PHONE_NUMBER = '+15555555555'
 const IDENTIFIER = PhoneNumberUtils.getPhoneHash(PHONE_NUMBER)
 const BLINDING_FACTOR = new Buffer('0IsBvRfkBrkKCIW6HV0/T1zrzjQSe8wRyU3PKojCnww=', 'base64')
@@ -25,6 +34,9 @@ const BLINDED_PHONE_NUMBER = getBlindedPhoneNumber(PHONE_NUMBER, BLINDING_FACTOR
 const DEFAULT_FORNO_URL = config.blockchain.provider
 
 const web3 = new Web3(new Web3.providers.HttpProvider(DEFAULT_FORNO_URL))
+const contractkit = newKitFromWeb3(web3)
+contractkit.addAccount(PRIVATE_KEY2)
+contractkit.defaultAccount = ACCOUNT_ADDRESS2
 
 jest.setTimeout(15000)
 
@@ -119,7 +131,7 @@ describe('Running against a deployed service', () => {
     })
 
     it('Returns sig when querying succeeds with unused request', async () => {
-      await replenishQuota(ACCOUNT_ADDRESS2, PRIVATE_KEY2)
+      await replenishQuota(ACCOUNT_ADDRESS2, contractkit)
       const response = await postToSignMessage(
         BLINDED_PHONE_NUMBER,
         PRIVATE_KEY2,
@@ -135,7 +147,7 @@ describe('Running against a deployed service', () => {
     })
 
     it('Returns sig when querying succeeds with used request', async () => {
-      await replenishQuota(ACCOUNT_ADDRESS2, PRIVATE_KEY2)
+      await replenishQuota(ACCOUNT_ADDRESS2, contractkit)
       const response = await postToSignMessage(
         BLINDED_PHONE_NUMBER,
         PRIVATE_KEY2,
@@ -151,7 +163,7 @@ describe('Running against a deployed service', () => {
     })
 
     it('Returns sig when querying succeeds with missing timestamp', async () => {
-      await replenishQuota(ACCOUNT_ADDRESS2, PRIVATE_KEY2)
+      await replenishQuota(ACCOUNT_ADDRESS2, contractkit)
       const response = await postToSignMessage(BLINDED_PHONE_NUMBER, PRIVATE_KEY2, ACCOUNT_ADDRESS2)
       expect(response.status).toBe(200)
     })
@@ -160,6 +172,62 @@ describe('Running against a deployed service', () => {
       const queryCount = await getQuota(PRIVATE_KEY2, ACCOUNT_ADDRESS2, IDENTIFIER)
       expect(queryCount).toEqual(initialQueryCount + 2)
     })
+  })
+})
+
+describe('When using walletAddress', () => {
+  // if these tests are failing, it may just be that the address needs to be fauceted:
+  // celotooljs account faucet --account ACCOUNT_ADDRESS2 --dollar 1 --gold 1 -e <ENV> --verbose
+  let initialQueryCount: number
+  let timestamp: number
+  beforeAll(async () => {
+    // ACCOUNT_ADDRESS2 is the wallet address
+    await registerWalletAddress(ACCOUNT_ADDRESS3, ACCOUNT_ADDRESS2, contractkit)
+    initialQueryCount = await getQuota(PRIVATE_KEY3, ACCOUNT_ADDRESS3, IDENTIFIER)
+    timestamp = Date.now()
+  })
+
+  it('Returns sig when querying succeeds with unused request', async () => {
+    await replenishQuota(ACCOUNT_ADDRESS2, contractkit)
+    const response = await postToSignMessage(
+      BLINDED_PHONE_NUMBER,
+      PRIVATE_KEY3,
+      ACCOUNT_ADDRESS3,
+      timestamp
+    )
+    expect(response.status).toBe(200)
+  })
+
+  it('Returns count when querying with unused request increments query count', async () => {
+    const queryCount = await getQuota(PRIVATE_KEY3, ACCOUNT_ADDRESS3, IDENTIFIER)
+    expect(queryCount).toEqual(initialQueryCount + 1)
+  })
+
+  it('Returns sig when querying succeeds with used request', async () => {
+    await replenishQuota(ACCOUNT_ADDRESS2, contractkit)
+    const response = await postToSignMessage(
+      BLINDED_PHONE_NUMBER,
+      PRIVATE_KEY3,
+      ACCOUNT_ADDRESS3,
+      timestamp
+    )
+    expect(response.status).toBe(200)
+  })
+
+  it('Returns count when querying with used request does not increment query count', async () => {
+    const queryCount = await getQuota(PRIVATE_KEY3, ACCOUNT_ADDRESS3, IDENTIFIER)
+    expect(queryCount).toEqual(initialQueryCount + 1)
+  })
+
+  it('Returns sig when querying succeeds with missing timestamp', async () => {
+    await replenishQuota(ACCOUNT_ADDRESS2, contractkit)
+    const response = await postToSignMessage(BLINDED_PHONE_NUMBER, PRIVATE_KEY3, ACCOUNT_ADDRESS3)
+    expect(response.status).toBe(200)
+  })
+
+  it('Returns count when querying with missing timestamp increments query count', async () => {
+    const queryCount = await getQuota(PRIVATE_KEY3, ACCOUNT_ADDRESS3, IDENTIFIER)
+    expect(queryCount).toEqual(initialQueryCount + 2)
   })
 })
 
