428a47f33efdde8a42f42af72fc7d354a7f4b6a3
codyborn <codyborn@outlook.com>
2020-10-29 14:35:23 -0700

    Remove env vars

--- packages/phone-number-privacy/signer/src/key-management/azure-key-provider.ts
+++ packages/phone-number-privacy/signer/src/key-management/azure-key-provider.ts
@@ -7,13 +7,7 @@ import { KeyProviderBase } from './key-provider-base'
 export class AzureKeyProvider extends KeyProviderBase {
   public async fetchPrivateKeyFromStore() {
     try {
-      const { clientID, clientSecret, tenant, vaultName, secretName } = config.keystore.azure
-
-      // Set environment variables for service principal auth
-      // The lib relies on these
-      process.env.AZURE_CLIENT_ID = clientID
-      process.env.AZURE_CLIENT_SECRET = clientSecret
-      process.env.AZURE_TENANT_ID = tenant
+      const { vaultName, secretName } = config.keystore.azure
 
       const keyVaultClient = new AzureKeyVaultClient(vaultName)
       const privateKey = await keyVaultClient.getSecret(secretName)
