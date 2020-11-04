300d789eb608f4142b0e6dc6dfe2f4e77a18568f
Marcin Chrzanowski <martin@clabs.co>
2020-11-04 16:27:24 +0100

    Add code to detect contracts needing relinking

---
+++ packages/protocol/test/compatibility/library-linking.ts
@@ -0,0 +1,29 @@
+import { assert } from 'chai'
+
+import { reportASTIncompatibilities } from '@celo/protocol/lib/compatibility/ast-code'
+import { reportLibraryLinkingIncompatibilities } from '@celo/protocol/lib/compatibility/library-linking'
+import { getTestArtifacts } from '@celo/protocol/test/compatibility/common'
+
+const testCases = {
+  linked_libraries: getTestArtifacts('linked_libraries'),
+  linked_libraries_upgraded_lib: getTestArtifacts('linked_libraries_upgraded_lib'),
+}
+
+describe('reportLibraryLinkingIncompatibilities', () => {
+  it('detects when a linked library has changed', () => {
+    const codeReport = reportASTIncompatibilities(
+      testCases.linked_libraries,
+      testCases.linked_libraries_upgraded_lib
+    )
+    const libraryLinksReport = reportLibraryLinkingIncompatibilities(
+      {
+        LinkedLibrary1: ['TestContract'],
+        LinkedLibrary2: ['TestContract'],
+        LinkedLibrary3: ['LinkedLibrary2'],
+      },
+      codeReport
+    )
+    assert.deepEqual(libraryLinksReport['TestContract'], ['LinkedLibrary2'])
+    assert.deepEqual(libraryLinksReport['LinkedLibrary2'], ['LinkedLibrary3'])
+  })
+})
