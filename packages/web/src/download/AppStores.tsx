699cdaf6a52014cdd1940498e53de7ecd65cfd70
aaron deruvo <aaron@celo.org>
2020-11-04 10:01:45 -0800

    add back alfajores developer

--- packages/web/src/download/AppStores.tsx
+++ packages/web/src/download/AppStores.tsx
@@ -30,7 +30,7 @@ export default withNamespaces(NameSpaces.download)(
         >
           <Cell span={Spans.half}>
             <Fade distance={'20px'}>
-              <View style={standardStyles.centered}>
+              <View style={styles.account}>
                 <H1 style={[textStyles.center, standardStyles.elementalMargin]} ariaLevel={'2'}>
                   {t('haveAccount')}
                 </H1>
@@ -40,6 +40,7 @@ export default withNamespaces(NameSpaces.download)(
                 <Button
                   size={SIZE.normal}
                   kind={BTN.NAKED}
+                  align={'center'}
                   text={t('learnMore')}
                   href={CeloLinks.gettingStarted}
                 />
@@ -67,13 +68,13 @@ const Logos = withScreenSize(({ screen }: ScreenProps) => {
   return (
     <Cell span={Spans.full} style={[!isMobile && standardStyles.row, styles.stores]}>
       <TouchableOpacity>
-        <a href={CeloLinks.playStoreWallet}>
+        <a href={CeloLinks.playStoreDevWallet}>
           <FadingImage source={playLogo} />
         </a>
       </TouchableOpacity>
       {isMobile || <View style={[standardStyles.verticalLine, styles.verticalLine]} />}
       <TouchableOpacity>
-        <a href={CeloLinks.appStoreWallet}>
+        <a href={CeloLinks.appStoreDevWallet}>
           <FadingImage source={appStoreLogo} />
         </a>
       </TouchableOpacity>
@@ -82,6 +83,11 @@ const Logos = withScreenSize(({ screen }: ScreenProps) => {
 })
 
 const styles = StyleSheet.create({
+  account: {
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+  },
   stores: {
     justifyContent: 'space-evenly',
     alignItems: 'center',
@@ -94,5 +100,4 @@ const styles = StyleSheet.create({
     width: 212,
     height: 103,
   },
-  seedPhrase: { maxWidth: 450 },
 })
