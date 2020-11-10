78a4661906c5d9c2dc867e3d850323c66ff900a1
Emily Williams <emag3m@gmail.com>
2020-11-10 18:25:12 -0500

    option to write to json file

--- packages/cli/src/commands/parseEvents/accountBalances.ts
+++ packages/cli/src/commands/parseEvents/accountBalances.ts
@@ -1,4 +1,5 @@
 import { flags } from '@oclif/command'
+import fs from 'fs'
 import { BaseCommand } from '../../base'
 import { printValueMapRecursive } from '../../utils/cli'
 
@@ -9,6 +10,10 @@ export default class ParseEvents extends BaseCommand {
     ...BaseCommand.flags,
     fromBlock: flags.string({ required: true, description: 'Starting Block' }),
     toBlock: flags.string({ required: true, description: 'Ending Block' }),
+    outputJson: flags.boolean({
+      description: 'Write to JSON file titled with blocknumbers: balances0-100.json',
+    }),
+    startingBalances: flags.string(),
   }
 
   static examples = ['status']
@@ -17,8 +22,20 @@ export default class ParseEvents extends BaseCommand {
     const res = this.parse(ParseEvents)
     const fromBlock = res.flags.fromBlock
     const toBlock = res.flags.toBlock
+    const outputJson = res.flags.outputJson
     const stableToken = await this.kit.contracts.getStableToken()
     const balances = await stableToken.getAccountBalances(fromBlock, toBlock)
+    // Output results to a JSON file if set
+    if (outputJson) {
+      fs.writeFile(
+        `balances${fromBlock}-${toBlock}.json`,
+        JSON.stringify(balances, null, 2),
+        (err) => {
+          if (err) throw err
+        }
+      )
+    }
+    // Print values to console
     printValueMapRecursive(balances)
     console.log('Total accounts reported:', Object.keys(balances).length)
   }
