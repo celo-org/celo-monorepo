6cd01af35c9cb7445f760602e092258eaddba5bf
Tarik Bellamine <tarikbellamine@protonmail.com>
2020-11-17 14:29:29 -0800

    add test

--- packages/mobile/src/exchange/ExchangeConfirmationCard.test.tsx
+++ packages/mobile/src/exchange/ExchangeConfirmationCard.test.tsx
@@ -24,7 +24,19 @@ it('renders correctly with no exchange rate', () => {
   expect(tree).toMatchSnapshot()
 })
 
-it('renders correctly with giant numbers', () => {
+it('renders correctly with larger numbers', () => {
+  const tree = renderer.create(
+    <Provider store={store}>
+      <ExchangeConfirmationCard
+        makerAmount={{ value: '240000.00', currencyCode: 'cUSD', localAmount }}
+        takerAmount={{ value: '180000000', currencyCode: 'cGLD', localAmount }}
+      />
+    </Provider>
+  )
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders correctly with numbers that are over the balance limit', () => {
   const tree = renderer.create(
     <Provider store={store}>
       <ExchangeConfirmationCard
