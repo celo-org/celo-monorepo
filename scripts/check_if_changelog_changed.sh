382288479801d3e03752ccdc511931a32ee13fe3
Medha Kothari <medha@clabs.co>
2020-11-17 19:38:44 -0800

    added utils and base

--- scripts/check_if_changelog_changed.sh
+++ scripts/check_if_changelog_changed.sh
@@ -7,11 +7,9 @@ cd packages/${PACKAGE}
 # latest commit
 LATEST_COMMIT=$(git rev-parse HEAD)
 
-echo "LATEST_COMMIT: ${LATEST_COMMIT}"
-
 # latest commit where path/to/PACKAGE was changed
 CHANGE_COMMIT=$(git log -1 --format=format:%H --full-diff CHANGELOG.md)
-echo "CHANGE_COMMIT: ${CHANGE_COMMIT}"
+cd -
 
 if [[ $CHANGE_COMMIT != $LATEST_COMMIT ]]; then
   echo "Please commit to the ${PACKAGE}/CHANGELOG.md file"
