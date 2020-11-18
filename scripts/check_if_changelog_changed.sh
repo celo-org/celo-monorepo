a6b0556aa90474d93414102b7e04d9c9518e70de
Medha Kothari <medha@clabs.co>
2020-11-17 16:55:00 -0800

    script done

--- scripts/check_if_changelog_changed.sh
+++ scripts/check_if_changelog_changed.sh
@@ -1,19 +1,21 @@
-#!/usr/bin/env bash
 set -euo pipefail
 
-checkIfChangedChangelog() {
-  # get package to check 
-  PACKAGE=${1}
+# get package to check and move its directory
+PACKAGE=${1}
+cd packages/${PACKAGE}
 
-  # latest commit
-  LATEST_COMMIT=$(git rev-parse HEAD)
+# latest commit
+LATEST_COMMIT=$(git rev-parse HEAD)
 
-  # latest commit where path/to/PACKAGE was changed
-  CHANGE_COMMIT=$(git log -1 --format=format:%H --full-diff ${PACKAGE}/CHANGELOG.md)
+echo "LATEST_COMMIT: ${LATEST_COMMIT}"
 
-  if [[ $CHANGE_COMMIT != $LATEST_COMMIT ]]; then
-    echo "Please commit to the ${PACKAGE}/CHANGELOG.md file"
-    exit 1
-  fi
-}
+# latest commit where path/to/PACKAGE was changed
+CHANGE_COMMIT=$(git log -1 --format=format:%H --full-diff CHANGELOG.md)
+echo "CHANGE_COMMIT: ${CHANGE_COMMIT}"
+
+if [[ $CHANGE_COMMIT != $LATEST_COMMIT ]]; then
+  echo "Please commit to the ${PACKAGE}/CHANGELOG.md file"
+  exit 1
+fi
+
 
