f5548d808b744190895d8367322de30ec25a3649
Nam Chu Hoai <nambrot@googlemail.com>
2020-11-18 15:32:55 -0500

    Prototype for single calculation command

---
+++ packages/cli/src/commands/events/parse/calculate-rewards.ts
@@ -0,0 +1,91 @@
+import { flags } from '@oclif/command'
+import { cli } from 'cli-ux'
+import fs from 'fs'
+import { writeFileSync } from 'fs-extra'
+import { EventLog } from 'web3-core'
+import { BaseCommand } from '../../../base'
+import {
+  calculateTimeWeightedAverage,
+  initializeBalancesByBlock,
+  mergeTwo,
+  processAttestationCompletion,
+  processTransfer,
+  RewardsCalculationState,
+} from '../../../utils/events'
+
+export default class CalculateRewards extends BaseCommand {
+  static description = 'Parses Events for data'
+
+  static flags = {
+    ...BaseCommand.flags,
+    attestationEvents: flags.string({
+      required: true,
+      description: 'File containing AttestationCompleted events',
+    }),
+    transferEvents: flags.string({
+      required: true,
+      description: 'File containing Transfer events',
+    }),
+  }
+
+  async run() {
+    const res = this.parse(CalculateRewards)
+    const attestationEvents = JSON.parse(fs.readFileSync(res.flags.attestationEvents, 'utf8'))
+    const transferEvents = JSON.parse(fs.readFileSync(res.flags.transferEvents, 'utf8'))
+    const allEvents: EventLog[] = mergeTwo(attestationEvents, transferEvents)
+
+    // State over time
+    const attestationCompletions = {}
+    const balances = {}
+    const balancesByBlock = {}
+    const state: RewardsCalculationState = {
+      attestationCompletions,
+      balances,
+      balancesByBlock,
+      // TODO: Move these into cmd args
+      blockNumberToStartTracking: 1000000,
+      blockNumberToFinishTracking: 3000000,
+      startedBlockBalanceTracking: false,
+    }
+
+    const progressBar = cli.progress()
+    progressBar.start(allEvents.length, 0)
+
+    allEvents.forEach((event, index) => {
+      progressBar.update(index)
+      if (
+        event.blockNumber >= state.blockNumberToStartTracking &&
+        !state.startedBlockBalanceTracking
+      ) {
+        initializeBalancesByBlock(state)
+        state.startedBlockBalanceTracking = true
+      }
+
+      switch (event.event) {
+        case 'AttestationCompleted':
+          processAttestationCompletion(state, event)
+          break
+        case 'Transfer':
+          processTransfer(state, event)
+          break
+        default:
+          throw new Error('Unknown event')
+      }
+    })
+    progressBar.stop()
+
+    writeFileSync(
+      'rewardsBalances.json',
+      JSON.stringify(
+        calculateTimeWeightedAverage(
+          balancesByBlock,
+          state.blockNumberToStartTracking,
+          state.blockNumberToFinishTracking
+        )
+      )
+    )
+    writeFileSync('rewardsCalculationState.json', JSON.stringify(state))
+
+    console.info('Done')
+  }
+}
--- packages/cli/src/utils/events.ts
+++ packages/cli/src/utils/events.ts
@@ -1,7 +1,42 @@
 import { BigNumber } from 'bignumber.js'
 import { EventLog } from 'web3-core'
 
-export async function attestedAccounts(events: EventLog[], minimum: number): Promise<string[]> {
+export function mergeTwo<T>(arr1: T[], arr2: T[]) {
+  const merged = []
+  let index1 = 0
+  let index2 = 0
+  let current = 0
+
+  while (current < arr1.length + arr2.length) {
+    const isArr1Depleted = index1 >= arr1.length
+    const isArr2Depleted = index2 >= arr2.length
+
+    if (!isArr1Depleted && (isArr2Depleted || arr1[index1] < arr2[index2])) {
+      merged[current] = arr1[index1]
+      index1++
+    } else {
+      merged[current] = arr2[index2]
+      index2++
+    }
+
+    current++
+  }
+
+  return merged
+}
+
+export function initializeBalancesByBlock(state: RewardsCalculationState) {
+  Object.keys(state.balances).forEach((acct) => {
+    if (state.attestationCompletions[acct] < 3) {
+      return
+    }
+    state.balancesByBlock[acct] = {
+      balances: [state.balances[acct]],
+      blocks: [state.blockNumberToStartTracking],
+    }
+  })
+}
+export function attestedAccounts(events: EventLog[], minimum: number): string[] {
   let verifiedAccounts: string[] = []
   let issuers: { [account: string]: string[] } = {}
 
@@ -22,30 +57,95 @@ export async function attestedAccounts(events: EventLog[], minimum: number): Pro
   return verifiedAccounts.filter((acct, index) => verifiedAccounts.indexOf(acct) == index)
 }
 
-export async function accountBalances(
+export function accountBalances(
   events: EventLog[],
   addressFilter: string[] | undefined
-): Promise<{ [address: string]: BigNumber }> {
+): { [address: string]: BigNumber } {
   let balances: { [address: string]: BigNumber } = {}
 
   events.forEach(function(eventlog: EventLog) {
     checkEventType(eventlog, 'Transfer')
-    let amount = eventlog.returnValues.value
-    let to = eventlog.returnValues.to
-    let from = eventlog.returnValues.from
-    balances[to]
-      ? (balances[to] = balances[to].plus(amount))
-      : (balances[to] = new BigNumber(amount))
-    balances[from]
-      ? (balances[from] = balances[from].minus(amount))
-      : (balances[from] = new BigNumber(0)) // just for address 0x0
+    updateBalance(balances, eventlog)
   })
   return addressFilter ? filterObject(balances, addressFilter) : balances
 }
 
-interface BalancesByBlock {
+function updateBalance(balances: Balances, eventLog: EventLog) {
+  const amount = eventLog.returnValues.value
+  const to = eventLog.returnValues.to
+  const from = eventLog.returnValues.from
+  balances[to] ? (balances[to] = balances[to].plus(amount)) : (balances[to] = new BigNumber(amount))
+  balances[from]
+    ? (balances[from] = balances[from].minus(amount))
+    : (balances[from] = new BigNumber(0)) // just for address 0x0
+  return { to, from }
+}
+
+export interface RewardsCalculationState {
+  attestationCompletions: AttestationCompletions
+  balances: Balances
+  balancesByBlock: BalancesByBlockState
+  startedBlockBalanceTracking: boolean
+  blockNumberToStartTracking: number
+  blockNumberToFinishTracking: number
+}
+export interface AttestationCompletions {
+  [account: string]: number
+}
+
+export interface Balances {
+  [account: string]: BigNumber
+}
+
+export interface BalancesByBlockState {
+  [account: string]: BalancesByBlock
+}
+export interface BalancesByBlock {
   balances: BigNumber[]
-  blocks: BigNumber[]
+  blocks: number[]
+}
+
+export function processAttestationCompletion(state: RewardsCalculationState, event: EventLog) {
+  const account = event.returnValues.account
+  state.attestationCompletions[account] = state.attestationCompletions[account]
+    ? state.attestationCompletions[account] + 1
+    : 1
+
+  if (state.startedBlockBalanceTracking && state.attestationCompletions[account] === 3) {
+    // Just completed
+    state.balancesByBlock[account] = {
+      balances: [state.balances[account] || new BigNumber(0)],
+      blocks: [event.blockNumber],
+    }
+  }
+}
+
+export function processTransfer(state: RewardsCalculationState, event: EventLog) {
+  const { to, from } = updateBalance(state.balances, event)
+
+  if (state.startedBlockBalanceTracking) {
+    updateBalanceByBlock(state, from, event.blockNumber)
+    updateBalanceByBlock(state, to, event.blockNumber)
+  }
+}
+
+function updateBalanceByBlock(
+  state: RewardsCalculationState,
+  account: string,
+  blockNumber: number
+) {
+  if (state.attestationCompletions[account] >= 3) {
+    const balancesByBlock = state.balancesByBlock[account]
+    if (balancesByBlock.balances.length > 0) {
+      balancesByBlock.balances.push(state.balances[account])
+      balancesByBlock.blocks.push(blockNumber)
+    } else {
+      state.balancesByBlock[account] = {
+        balances: [state.balances[account]],
+        blocks: [blockNumber],
+      }
+    }
+  }
 }
 
 export async function accountBalancesTWA(
@@ -58,7 +158,7 @@ export async function accountBalancesTWA(
   Object.keys(initialBalances).forEach((acct) => {
     balancesByBlock[acct] = {
       balances: [initialBalances[acct]],
-      blocks: [new BigNumber(fromBlock)],
+      blocks: [fromBlock],
     }
   })
 
@@ -67,7 +167,7 @@ export async function accountBalancesTWA(
     let amount = eventlog.returnValues.value
     let to = eventlog.returnValues.to
     let from = eventlog.returnValues.from
-    let block = new BigNumber(eventlog.blockNumber)
+    let block = eventlog.blockNumber
     if (balancesByBlock[to]) {
       let balances = balancesByBlock[to]
       let prevBalance = balances.balances[balances.balances.length - 1]
@@ -105,7 +205,7 @@ function filterObject(
   }, obj)
 }
 
-function calculateTimeWeightedAverage(
+export function calculateTimeWeightedAverage(
   balancesTWA: { [address: string]: BalancesByBlock },
   fromBlock: number,
   toBlock: number
@@ -116,8 +216,8 @@ function calculateTimeWeightedAverage(
     const blocks = balancesTWA[address].blocks
     const weightedSum = balances.reduce((prevWeightedBalance, balance, i) => {
       const currentBlock = blocks[i]
-      const nextBlock = blocks[i + 1] || new BigNumber(toBlock)
-      const weightedBalance = balance.times(nextBlock.minus(currentBlock))
+      const nextBlock = blocks[i + 1] || toBlock
+      const weightedBalance = balance.times(nextBlock - currentBlock)
       return prevWeightedBalance.plus(weightedBalance)
     }, new BigNumber(0))
     const weightedAverage = weightedSum.dividedBy(new BigNumber(toBlock - fromBlock))
