57696b119596f70b56c461b6046137c4de5d010a
Erdal Tuleu <etuleu@gmail.com>
2020-11-17 16:32:33 -0800

    escape harder

--- packages/mobile/e2e/src/usecases/HandleDeepLinkDappkit.js
+++ packages/mobile/e2e/src/usecases/HandleDeepLinkDappkit.js
@@ -1,6 +1,6 @@
 export default HandleDeepLinkDappkit = () => {
   const DAPPKIT_URL =
-    '"celo://wallet/dappkit?type=sign_tx&requestId=beneficiaryclaim&callback=impactmarket%3A%2F%2F&dappName=impactmarket&txs=W3sidHhEYXRhIjoiMHg0ZTcxZDkyZCIsImVzdGltYXRlZEdhcyI6MTIxMTI3LCJmcm9tIjoiMHhhY2FGQjRGMWQ5RERGMTQwNWViZEJGZWM5NTlBNzI4MTk0QjAyMzhhIiwidG8iOiIweDc0ZTVDNDA1RURFNEUzN2U3ODBEQjk2NTI1NDE1MzhkZDhBNzlBN2QiLCJub25jZSI6MjYsImZlZUN1cnJlbmN5QWRkcmVzcyI6IjB4NzY1REU4MTY4NDU4NjFlNzVBMjVmQ0ExMjJiYjY4OThCOEIxMjgyYSIsInZhbHVlIjoiMCJ9XQ%3D%3D"'
+    '"celo://wallet/dappkit?type=sign_tx\\&requestId=beneficiaryclaim\\&callback=impactmarket%3A%2F%2F\\&dappName=impactmarket\\&txs=W3sidHhEYXRhIjoiMHg0ZTcxZDkyZCIsImVzdGltYXRlZEdhcyI6MTIxMTI3LCJmcm9tIjoiMHhhY2FGQjRGMWQ5RERGMTQwNWViZEJGZWM5NTlBNzI4MTk0QjAyMzhhIiwidG8iOiIweDc0ZTVDNDA1RURFNEUzN2U3ODBEQjk2NTI1NDE1MzhkZDhBNzlBN2QiLCJub25jZSI6MjYsImZlZUN1cnJlbmN5QWRkcmVzcyI6IjB4NzY1REU4MTY4NDU4NjFlNzVBMjVmQ0ExMjJiYjY4OThCOEIxMjgyYSIsInZhbHVlIjoiMCJ9XQ%3D%3D"'
 
   it('Launch app with dappkit deep link', async () => {
     await device.terminateApp()
