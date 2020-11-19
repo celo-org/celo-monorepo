c854dac211bfc6ab89e09d60e7ca6ac51cbfd0d1
Marcin Chrzanowski <martin@clabs.co>
2020-11-19 16:32:24 +0100

    Get and kill ganache PID only if lsof exists

--- packages/protocol/scripts/bash/release-on-devchain.sh
+++ packages/protocol/scripts/bash/release-on-devchain.sh
@@ -45,8 +45,10 @@ startInBgAndWaitForString 'Ganache STARTED' yarn devchain run-tar devchain.tar.g
 git checkout -
 yarn build >> $LOG_FILE
 
-GANACHE_PID=`lsof -i tcp:8545 | tail -n 1 | awk '{print $2}'`
-echo "Network started with PID $GANACHE_PID, if exit 1, you will need to manually stop the process"
+if command -v lsof; then
+    GANACHE_PID=`lsof -i tcp:8545 | tail -n 1 | awk '{print $2}'`
+    echo "Network started with PID $GANACHE_PID, if exit 1, you will need to manually stop the process"
+fi
 
 echo "- Verify bytecode of the network"
 yarn run truffle exec ./scripts/truffle/verify-bytecode.js --network development --build_artifacts $BUILD_DIR/contracts
@@ -63,3 +65,7 @@ yarn truffle exec --network development ./scripts/truffle/make-release.js --buil
 # From verify-release.sh
 echo "- Verify release"
 yarn truffle exec --network development ./scripts/truffle/verify-bytecode.js --build_artifacts build/contracts --proposal ../../proposal.json
+
+if [[ -n $GANACHE_PID ]]; then
+    kill $GANACHE_PID
+fi
