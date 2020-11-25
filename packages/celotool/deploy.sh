3a025c5e8de26880b4c5085939ba5b2ecff7175d
Joshua Gutow <joshua@clabs.co>
2020-11-25 14:01:50 -0800

    Cleanup

--- packages/celotool/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-env=second
-sed -i bak "s/GETH_NODE_DOCKER_IMAGE_TAG=\"[a-zA-Z0-9]*\"/GETH_NODE_DOCKER_IMAGE_TAG=\"$1\"/" ../../.env.$env
-git commit ../../.env.$env -m "Bump geth to $1"
-
- yarn cli deploy destroy testnet -e $env
-# sleep 400
-
-yarn cli deploy initial testnet -e $env
