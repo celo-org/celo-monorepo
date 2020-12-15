3571041fc3833cd47fea1a5fe6170a7211bddb11
Valentin Rodygin <carterqw@gmail.com>
2020-12-15 13:03:12 +0100

    Added fees to the activity feed (#5240)
    Added fees to the activity feed

---
+++ packages/blockchain-api/src/events/Verification.ts
@@ -0,0 +1,34 @@
+import { EventBuilder } from '../helpers/EventBuilder'
+import { EventTypes } from '../schema'
+import { Transaction } from '../transaction/Transaction'
+import { TransactionType } from '../transaction/TransactionType'
+import { Contracts } from '../utils'
+
+export class Verification extends TransactionType {
+  matches(transaction: Transaction): boolean {
+    return (
+      transaction.transfers.length === 1 &&
+      transaction.transfers.containsTransferTo(Contracts.Attestations)
+    )
+  }
+
+  getEvent(transaction: Transaction) {
+    const transfer = transaction.transfers.getTransferTo(Contracts.Attestations)
+
+    if (!transfer) {
+      throw new Error('Transfer to Attestations not found.')
+    }
+
+    return EventBuilder.transferEvent(
+      transaction,
+      transfer,
+      EventTypes.VERIFICATION_FEE,
+      transfer.toAddressHash,
+      transaction.fees
+    )
+  }
+
+  isAggregatable(): boolean {
+    return false
+  }
+}
