dc123cb625dd6ff4353329fa707b2c93212a84d8
Erdal T <etuleu@gmail.com>
2020-12-15 19:12:34 -0800

    fix android build for reanimated

--- packages/mobile/scripts/install-android-tools.sh
+++ packages/mobile/scripts/install-android-tools.sh
@@ -21,6 +21,6 @@ SDKMANAGER=$ANDROID_SDK_ROOT/tools/bin/sdkmanager
 $SDKMANAGER "platform-tools"
 $SDKMANAGER "platforms;android-29"
 $SDKMANAGER "build-tools;29.0.2"
-$SDKMANAGER "ndk-bundle"
+$SDKMANAGER "ndk;22.0.7026061"
 $SDKMANAGER "system-images;android-29;default;x86_64"
 $SDKMANAGER "emulator"
