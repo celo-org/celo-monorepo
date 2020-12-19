c328556e39b4e89321c290e73a2b3eccb2bcdf8d
Yorke Rhodes <yorhodes@celo.org>
2020-12-18 18:10:01 -0800

    Adjust how CLI docs are generated (#5882)
    ### Description
    
    - Make base command flags `hidden`
    - Make generated CLI docs wrap at 88 characters such that gitbook does not hide content
    - Use upstream https://github.com/oclif/dev-cli
    - Add useful oclif plugins
    
    ### Other changes
    
    _Describe any minor or "drive-by" changes here._
    
    ### Tested
    
    from misspelled command
    ![Screen Shot 2020-11-13 at 5 02 13 PM](https://user-images.githubusercontent.com/3020995/99136426-8ede5b00-25d9-11eb-8420-b822cb8f157e.png)
    
    from `yarn preview`
    ![Screen Shot 2020-11-13 at 5 02 41 PM](https://user-images.githubusercontent.com/3020995/99136428-8f76f180-25d9-11eb-9ad6-b133c8949617.png)
    
    ### Related issues
    
    - Fixes #[issue number here]
    
    ### Backwards compatibility
    
    _Brief explanation of why these changes are/are not backwards compatible._

--- packages/cli/src/commands/validator/list.ts
+++ packages/cli/src/commands/validator/list.ts
@@ -17,7 +17,7 @@ export default class ValidatorList extends BaseCommand {
     'List registered Validators, their name (if provided), affiliation, uptime score, and public keys used for validating.'
 
   static flags = {
-    ...BaseCommand.flagsWithoutLocalAddresses(),
+    ...BaseCommand.flags,
     ...(cli.table.flags() as object),
   }
 
