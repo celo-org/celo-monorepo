3ca9ad2fc390a5619649f121af8bf096703ab9b2
Asa Oines <a@celo.org>
2021-01-05 19:22:54 -0500

    Add script

---
+++ packages/protocol/scripts/truffle/deploy_multisig.ts
@@ -0,0 +1,43 @@
+import { retryTx } from '@celo/protocol/lib/proxy-utils'
+import { _setInitialProxyImplementation } from '@celo/protocol/lib/web3-utils'
+import { MultiSigContract, ProxyContract } from 'types'
+
+module.exports = async (callback: (error?: any) => number) => {
+  try {
+    const argv = require('minimist')(process.argv.slice(5), {
+      string: ['from', 'owners', 'required', 'internalRequired'],
+    })
+    const MultSig: MultiSigContract = artifacts.require('MultiSig')
+    const Proxy: ProxyContract = artifacts.require('Proxy')
+
+    console.info('  Deploying MultiSigProxy...')
+    const proxy = await retryTx(Proxy.new, [{ from: argv.from }])
+    console.info('  Deploying MultiSig...')
+    const multiSig = await retryTx(MultiSig.new, [{ from: argv.from }])
+    const initializationTxHash = await _setInitialProxyImplementation(
+      web3,
+      multiSig,
+      proxy,
+      'MultiSig',
+      {
+        from: argv.from,
+        value: null,
+      },
+      argv.owners.split(','),
+      argv.required,
+      argv.internalRequired
+    )
+    await retryTx(proxy._transferOwnership, [
+      proxy.address,
+      {
+        from: argv.from,
+      },
+    ])
+    console.info('Proxy address', proxy.address)
+    console.info('Implementation address', multiSig.address)
+    callback()
+  } catch (e) {
+    console.error('Something went wrong')
+    callback(error)
+  }
+}
