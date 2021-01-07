7a487ce5da4d2da0e36a620b45be183f2c1f18a3
Gonzalo Nardini <nardinigonzalom@gmail.com>
2021-01-07 11:53:18 -0300

    Update packages/mobile/src/firebase/notifications.ts
    Co-authored-by: Jean Regisser <jean.regisser@gmail.com>

--- packages/mobile/src/firebase/notifications.ts
+++ packages/mobile/src/firebase/notifications.ts
@@ -87,7 +87,7 @@ export function* handleNotification(
   message: FirebaseMessagingTypes.RemoteMessage,
   notificationState: NotificationReceiveState
 ) {
-  // See if this is a notification with an open url or webview action (`ou` or `ow` prop in the data)
+  // See if this is a notification with an open url or webview action (`ou` prop in the data)
   const urlToOpen = message.data?.ou
   const openExternal = message.data?.openExternal === 'true'
   const openUrlAction = urlToOpen ? openUrl(urlToOpen, openExternal) : null
