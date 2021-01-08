c2edebe1fda64ce56dec7035e0da3557da776b41
Gonzalo Nardini <nardinigonzalom@gmail.com>
2021-01-08 14:57:42 -0300

    [Wallet] Add host of URI as title to WebViewScreen and fix deep links (#6417)
    ### Description
    
    - The `WebViewScreen` doesn't have a title. Adding the host of the URI as title in the header.
    - When firing a deep link form the WebView, the link was not being fired. Added a handler to support that usecase. In the past I had tested against a custom HTML which did work, but when using a real webpage it did not. It works after this change.
    
    ### Other changes
    - Added an origin param to the CELO rewards link to be able to track usage.
    
    ### Tested
    
    Yes
    
    ### Related issues
    
    - Fixes #6416

--- packages/mobile/src/app/WebViewScreen.tsx
+++ packages/mobile/src/app/WebViewScreen.tsx
@@ -1,35 +1,65 @@
 import colors from '@celo/react-components/styles/colors'
 import { StackScreenProps } from '@react-navigation/stack'
 import * as React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { Platform, StyleSheet, View } from 'react-native'
 import { WebView } from 'react-native-webview'
+import { ShouldStartLoadRequest } from 'react-native-webview/lib/WebViewTypes'
+import { useDispatch } from 'react-redux'
+import { openDeepLink } from 'src/app/actions'
 import i18n from 'src/i18n'
 import { emptyHeader } from 'src/navigator/Headers'
 import { navigateBack } from 'src/navigator/NavigationService'
 import { Screens } from 'src/navigator/Screens'
 import { TopBarTextButton } from 'src/navigator/TopBarButton'
 import { StackParamList } from 'src/navigator/types'
+import { parse } from 'url'
 
 type RouteProps = StackScreenProps<StackParamList, Screens.WebViewScreen>
 type Props = RouteProps
 
-export const webViewScreenNavOptions = {
-  ...emptyHeader,
-  headerLeft: () => (
-    <TopBarTextButton
-      title={i18n.t('global:close')}
-      onPress={navigateBack}
-      titleStyle={styles.close}
-    />
-  ),
+export const webViewScreenNavOptions = ({ route }: RouteProps) => {
+  const { hostname } = parse(route.params.uri)
+  return {
+    ...emptyHeader,
+    headerTitle: hostname || ' ',
+    headerLeft: () => (
+      <TopBarTextButton
+        title={i18n.t('global:close')}
+        onPress={navigateBack}
+        titleStyle={styles.close}
+      />
+    ),
+  }
+}
+
+const shouldUseOpacityHack = () => {
+  if (Platform.OS === 'ios') {
+    return false
+  }
+  return Platform.Version >= 28
 }
 
 function WebViewScreen({ route }: Props) {
   const { uri } = route.params
+  const dispatch = useDispatch()
+
+  const handleLoadRequest = (event: ShouldStartLoadRequest): boolean => {
+    if (event.url.startsWith('celo://')) {
+      dispatch(openDeepLink(event.url))
+      return false
+    }
+    return true
+  }
 
   return (
     <View style={styles.container}>
-      <WebView style={styles.webView} source={{ uri }} />
+      <WebView
+        style={shouldUseOpacityHack() ? styles.webView : {}}
+        originWhitelist={['https://*', 'celo://*']}
+        onShouldStartLoadWithRequest={handleLoadRequest}
+        setSupportMultipleWindows={false}
+        source={{ uri }}
+      />
     </View>
   )
 }
