18acd2e18395fe13500655ed4991bc4e5ed3a226
yorhodes <yorhodes@cLabs.co>
2021-01-08 10:17:59 -0800

    Make sed portable to GNU

--- packages/cli/generate_docs.sh
+++ packages/cli/generate_docs.sh
@@ -2,4 +2,4 @@ set -e
 export COLUMNS=88
 yarn oclif-dev readme --multi --dir=../docs/command-line-interface
 yarn prettier --write ../docs/command-line-interface/*
-sed -i '' '/^- \[/d' ../docs/command-line-interface/*
+sed -i.bak '/^- \[/d' ../docs/command-line-interface/*
