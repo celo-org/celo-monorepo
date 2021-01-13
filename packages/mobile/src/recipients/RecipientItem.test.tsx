e551c198544a1fbb471564df8929d3d9ca2c4ab2
Gonzalo Nardini <gnardini@clabs.co>
2021-01-13 15:11:54 -0300

    Update snapshots

--- packages/mobile/src/recipients/RecipientItem.test.tsx
+++ packages/mobile/src/recipients/RecipientItem.test.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react'
 import 'react-native'
+import { Provider } from 'react-redux'
 import * as renderer from 'react-test-renderer'
 import RecipientItem from 'src/recipients/RecipientItem'
+import { createMockStore } from 'test/utils'
 import { mockRecipient } from 'test/values'
 
 describe(RecipientItem, () => {
   it('renders correctly', () => {
     const tree = renderer.create(
-      <RecipientItem recipient={mockRecipient} onSelectRecipient={jest.fn()} />
+      <Provider store={createMockStore({})}>
+        <RecipientItem recipient={mockRecipient} onSelectRecipient={jest.fn()} />
+      </Provider>
     )
     expect(tree).toMatchSnapshot()
   })
