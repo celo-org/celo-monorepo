c0cb8ee3d1d764e2d94775c92176a62101680347
Martín Volpe <martin.volpe@gmail.com>
2021-01-21 12:05:17 -0300

    Adding cEUR in places

--- packages/protocol/contracts/stability/ExchangeEUR.sol
+++ packages/protocol/contracts/stability/ExchangeEUR.sol
@@ -2,4 +2,12 @@ pragma solidity ^0.5.13;
 
 import "./Exchange.sol";
 
-contract ExchangeEUR is Exchange {}
+contract ExchangeEUR is Exchange {
+  /**
+   * @notice Returns the storage, major, minor, and patch version of the contract.
+   * @return The storage, major, minor, and patch version of the contract.
+   */
+  function getVersionNumber() external pure returns (uint256, uint256, uint256, uint256) {
+    return (1, 0, 0, 0);
+  }
+}
