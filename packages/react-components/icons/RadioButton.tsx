a4986858cdbb04e559e3b19cb706da42f952048e
Michelle Wong <michellewong793@gmail.com>
2021-01-21 14:59:35 -0800

    first attempt at trying to add new conditionals

--- packages/react-components/icons/RadioButton.tsx
+++ packages/react-components/icons/RadioButton.tsx
@@ -1,49 +1,45 @@
-import colors from '@celo/react-components/styles/colors'
-import * as React from 'react'
-import { StyleSheet, View } from 'react-native'
-import Svg, { Circle } from 'svgs'
+import colors from '@celo/react-components/styles/colors';
+import * as React from 'react';
+import { Circle, Svg } from 'svgs';
 
 
 interface Props {
   height?: number
   color?: string
   selected?: boolean
-  disabled?:boolean
+  disabled?: boolean
 }
 
-const styles = StyleSheet.create({
-  circle: {
-    paddingTop: 2,
-    height: 20,
-    width: 20,
-    borderRadius: 12,
-    borderWidth: 2,
-    borderColor: colors.gray4,
-  },
-})
 
 export default class RadioButton extends React.PureComponent<Props> {
   static defaultProps = {
     width: 20,
     height: 20,
     color: colors.greenUI,
-    selected: true, 
+    selected: true,
     disabled: false,
   }
-  
-  
 
   render() {
+    let stroke = undefined;
+    if (this.props.disabled) {
+      stroke = colors.gray2
+    } else if (this.props.selected) {
+      stroke = colors.greenUI
+    } else if (!this.props.selected) {
+      stroke = colors.gray4
+    }
+    const fill = (this.props.selected && !this.props.disabled) ? colors.greenUI : undefined;
     return (
+
       <Svg
         xmlns="http://www.w3.org/2000/svg"
         height={this.props.height}
         width={this.props.height}
         viewBox="0 0 20 20"
       >
-      <Circle cx="10" cy="10" r="9" stroke={this.props.disabled ? colors.gray2 : this.props.color} strokeWidth={2} />  
-      {this.props.selected ? <Circle cx="10" cy="10" r="6" fill={this.props.color} /> : <View style={styles.circle} />
-  }
+        <Circle cx="10" cy="10" r="9" stroke={stroke} strokeWidth={2} />
+        <Circle cx="10" cy="10" r="6" fill={fill} />
       </Svg>
     )
   }
