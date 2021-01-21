fcf52bd57ae67a6fa4746a721ff94ac4b315b122
yorhodes <yorhodes@cLabs.co>
2021-01-21 14:26:55 -0800

    Use release tags in CI and fetch only relevant tags

--- packages/protocol/contracts/stability/StableTokenEUR.sol
+++ packages/protocol/contracts/stability/StableTokenEUR.sol
@@ -2,13 +2,4 @@ pragma solidity ^0.5.13;
 
 import "./StableToken.sol";
 
-contract StableTokenEUR is StableToken {
-  /**
-   * @notice Returns the storage, major, minor, and patch version of the contract.
-   * @return The storage, major, minor, and patch version of the contract.
-   */
-  function getVersionNumber() external pure returns (uint256, uint256, uint256, uint256) {
-    return (1, 1, 0, 0);
-  }
-
-}
+contract StableTokenEUR is StableToken {}
