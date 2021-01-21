412fc8289c0f741b7575e8e47c41f9c1886f18ab
Martín Volpe <martin.volpe@gmail.com>
2021-01-21 11:25:22 -0300

    Add empty contracts

---
+++ packages/protocol/contracts/stability/ExchangeEUR.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.5.13;
+
+import "./Exchange.sol";
+
+contract ExchangeEUR is Exchange {}
---
+++ packages/protocol/contracts/stability/StableTokenEUR.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.5.13;
+
+import "./StableToken.sol";
+
+contract StableTokenEUR is StableToken {}
