929c670f45c451fc9b59742150c6ff53bf543c03
Medha Kothari <medha@clabs.co>
2021-01-22 12:18:33 -0800

    added flags back in

--- packages/cli/src/commands/config/get.ts
+++ packages/cli/src/commands/config/get.ts
@@ -5,6 +5,10 @@ import { readConfig } from '../../utils/config'
 export default class Get extends BaseCommand {
   static description = 'Output network node configuration'
 
+  static flags = {
+    ...BaseCommand.flags,
+  }
+
   requireSynced = false
 
   async run() {
