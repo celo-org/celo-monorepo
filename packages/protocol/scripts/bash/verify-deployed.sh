0c62d26bdd431a0e40bba82aa4a9c929e8095fcb
Nam Chu Hoai <nambrot@googlemail.com>
2021-02-02 10:36:22 +0100

    Copy registry-utils

--- packages/protocol/scripts/bash/verify-deployed.sh
+++ packages/protocol/scripts/bash/verify-deployed.sh
@@ -35,13 +35,21 @@ git fetch origin +'refs/tags/celo-core-contracts*:refs/tags/celo-core-contracts*
 git checkout $BRANCH 2>>$LOG_FILE >> $LOG_FILE
 echo "- Build contract artifacts"
 rm -rf build/contracts
-yarn build >> $LOG_FILE
+yarn build:sol >> $LOG_FILE
 rm -rf $BUILD_DIR && mkdir -p $BUILD_DIR
 mv build/contracts $BUILD_DIR
-
-echo "- Run verification script"
-yarn run truffle exec ./scripts/truffle/verify-bytecode.js --network $NETWORK --build_artifacts $BUILD_DIR/contracts $FORNO
+cp lib/registry-utils.ts $BUILD_DIR/
 
 # Move back to branch from which we started
 echo "- Return to original git commit"
 git checkout - >> $LOG_FILE
+
+cp $BUILD_DIR/registry-utils.ts lib/
+
+echo "- Build verification script"
+yarn build >> $LOG_FILE
+
+echo "- Run verification script"
+yarn run truffle exec ./scripts/truffle/verify-bytecode.js --network $NETWORK --build_artifacts $BUILD_DIR/contracts $FORNO
+
+git checkout lib/registry-utils.ts
