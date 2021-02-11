dc95dfca92c529a8c9f92b896b3a4fa8447f4b8a
Tarik Bellamine <tarikbellamine@protonmail.com>
2021-02-10 17:06:49 -0800

    implement prefill with min tx amount

--- packages/mobile/src/fiatExchanges/RampScreen.tsx
+++ packages/mobile/src/fiatExchanges/RampScreen.tsx
@@ -1,13 +1,15 @@
 import { StackScreenProps } from '@react-navigation/stack'
 import React, { useEffect, useRef } from 'react'
 import { BackHandler, StyleSheet, View } from 'react-native'
-import { WebViewMessageEvent } from 'react-native-webview/lib/WebViewTypes'
 import { useSelector } from 'react-redux'
 import WebView, { WebViewRef } from 'src/components/WebView'
 import { VALORA_LOGO_URL } from 'src/config'
 import { CURRENCY_ENUM } from 'src/geth/consts'
 import config from 'src/geth/networkConfig'
 import i18n from 'src/i18n'
+import { LocalCurrencyCode } from 'src/localCurrency/consts'
+import { convertDollarsToLocalAmount } from 'src/localCurrency/convert'
+import { getLocalCurrencyExchangeRate } from 'src/localCurrency/selectors'
 import { emptyHeader } from 'src/navigator/Headers'
 import { navigate, navigateBack } from 'src/navigator/NavigationService'
 import { Screens } from 'src/navigator/Screens'
@@ -16,6 +18,7 @@ import { StackParamList } from 'src/navigator/types'
 import { currentAccountSelector } from 'src/web3/selectors'
 
 const RAMP_URI = config.rampWidgetUrl
+const MIN_USD_TX_AMOUNT = 15
 
 const navigateHome = () => navigate(Screens.WalletHome)
 
@@ -31,6 +34,15 @@ type Props = RouteProps
 function RampScreen({ route }: Props) {
   const { localAmount, currencyCode, currencyToBuy } = route.params
   const account = useSelector(currentAccountSelector)
+
+  let minTxAmount = MIN_USD_TX_AMOUNT
+
+  if (currencyCode !== LocalCurrencyCode.USD) {
+    const localCurrencyExchangeRate = useSelector(getLocalCurrencyExchangeRate)
+    const localTxMin = convertDollarsToLocalAmount(minTxAmount, localCurrencyExchangeRate)
+    minTxAmount = localTxMin?.toNumber() || MIN_USD_TX_AMOUNT
+  }
+
   const asset = {
     [CURRENCY_ENUM.GOLD]: 'CELO',
     [CURRENCY_ENUM.DOLLAR]: 'CUSD',
@@ -43,7 +55,7 @@ function RampScreen({ route }: Props) {
       &hostAppName=Valora
       &hostLogoUrl=${VALORA_LOGO_URL}
       &fiatCurrency=${currencyCode}
-      &fiatValue=${localAmount}
+      &fiatValue=${localAmount || minTxAmount}
       &finalUrl=${encodeURI(finalUrl)}
     `.replace(/\s+/g, '')
 
@@ -66,15 +78,7 @@ function RampScreen({ route }: Props) {
 
   return (
     <View style={styles.container}>
-      <WebView
-        ref={webview}
-        source={{ uri }}
-        onNavigationStateChange={onNavigationStateChange}
-        onMessage={(event: WebViewMessageEvent) => {
-          console.group(event)
-          console.log(event.nativeEvent)
-        }}
-      />
+      <WebView ref={webview} source={{ uri }} onNavigationStateChange={onNavigationStateChange} />
     </View>
   )
 }
