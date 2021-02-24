0abe0281227360de71750c8a677f38ee6c859af3
Trevor Porter <trkporter@ucdavis.edu>
2021-02-24 14:46:26 +0000

    remove euro from @celo/base/src/currencies

--- packages/sdk/base/src/currencies.ts
+++ packages/sdk/base/src/currencies.ts
@@ -1,7 +1,6 @@
 export enum CURRENCY_ENUM {
   GOLD = 'Celo Gold',
   DOLLAR = 'Celo Dollar',
-  EURO = 'Celo Euro',
 }
 
 interface Currency {
@@ -23,36 +22,25 @@ export const CURRENCIES: CurrencyObject = {
     code: 'cUSD',
     displayDecimals: 2,
   },
-  [CURRENCY_ENUM.EURO]: {
-    symbol: '€',
-    code: 'cEUR',
-    displayDecimals: 2,
-  },
 }
 
 export const resolveCurrency = (label: string): CURRENCY_ENUM => {
-  if (label) {
-    const lowerCaseLabel = label.toLowerCase()
-    if (lowerCaseLabel.includes('dollar')) {
-      return CURRENCY_ENUM.DOLLAR
-    } else if (lowerCaseLabel.includes('euro')) {
-      return CURRENCY_ENUM.EURO
-    } else if (lowerCaseLabel.includes('gold')) {
-      return CURRENCY_ENUM.GOLD
-    }
+  if (label && label.toLowerCase().includes('dollar')) {
+    return CURRENCY_ENUM.DOLLAR
+  } else if (label && label.toLowerCase().includes('gold')) {
+    return CURRENCY_ENUM.GOLD
+  } else {
+    console.info('Unable to resolve currency from label: ' + label)
+    return CURRENCY_ENUM.DOLLAR
   }
-  console.info('Unable to resolve currency from label: ' + label)
-  return CURRENCY_ENUM.DOLLAR
 }
 
 export enum SHORT_CURRENCIES {
   DOLLAR = 'dollar',
-  EURO = 'euro',
   GOLD = 'gold',
 }
 
 export const currencyToShortMap = {
   [CURRENCY_ENUM.DOLLAR]: SHORT_CURRENCIES.DOLLAR,
-  [CURRENCY_ENUM.EURO]: SHORT_CURRENCIES.EURO,
   [CURRENCY_ENUM.GOLD]: SHORT_CURRENCIES.GOLD,
 }
