26c2de8c80a584c209fef29188928a17b7400ca4
yorhodes <yorhodes@cLabs.co>
2021-03-01 12:22:38 -0800

    Make usable with commandline

--- packages/protocol/scripts/make-release-3-changes.ts
+++ packages/protocol/scripts/make-release-3-changes.ts
@@ -6,20 +6,28 @@ import {
 } from '../lib/compatibility/verify-bytecode'
 import { ProposalTx } from './truffle/make-release'
 
-const proposalFile = 'release3_proposal_mock.json'
+const makeRelease3Changes = (releaseProposal: ProposalTx[]) => {
+  const stableTokenEURaddr = getProposedProxyAddress('StableTokenEUR', releaseProposal)
+  const exchangeInitIndex = releaseProposal.findIndex((tx) =>
+    isProxyRepointAndInitForIdTransaction(tx, 'ExchangeEURProxy')
+  )
+  const initCallData = trimLeading0x(releaseProposal[exchangeInitIndex].args[1])
+  const paramPosition = (4 + 32) * 2 // (functionSelector + 1 parameter) * 2 hex
+  releaseProposal[exchangeInitIndex].args[1] = ensureLeading0x(
+    initCallData.slice(0, paramPosition) +
+      trimLeading0x(stableTokenEURaddr).padStart(64, '0') +
+      initCallData.slice(paramPosition + 32 * 2) // offset 1 parameter * 2 hex
+  )
+  return releaseProposal
+}
 
-const releaseProposal: ProposalTx[] = readJsonSync(proposalFile)
-const stableTokenEURaddr = getProposedProxyAddress('StableTokenEUR', releaseProposal)
-const exchangeInitIndex = releaseProposal.findIndex((tx) =>
-  isProxyRepointAndInitForIdTransaction(tx, 'ExchangeEURProxy')
-)
-const initCallData = trimLeading0x(releaseProposal[exchangeInitIndex].args[1])
-const paramPosition = (4 + 32) * 2 // (functionSelector + 1 parameter) * 2 hex
-releaseProposal[exchangeInitIndex].args[1] = ensureLeading0x(
-  initCallData.slice(0, paramPosition) +
-    trimLeading0x(stableTokenEURaddr).padStart(64, '0') +
-    initCallData.slice(paramPosition + 32 * 2) // offset 1 parameter * 2 hex
-)
-const newFile = `modified_${proposalFile}`
-writeJsonSync(newFile, releaseProposal, { spaces: 2 })
-console.log(`Modified proposal written to ${newFile}`)
+try {
+  const argv = require('minimist')(process.argv.slice(2), {
+    string: ['input_proposal', 'output_proposal'],
+  })
+  const releaseProposal: ProposalTx[] = readJsonSync(argv.input_proposal)
+  writeJsonSync(argv.output_proposal, makeRelease3Changes(releaseProposal), { spaces: 2 })
+  console.log(`Modifications made sucessfully; written to ${argv.output_proposal}`)
+} catch (e) {
+  console.error(`Something went wrong: ${e}`)
+}
