6a344c98d6ee403ccc935fac2a1be4970b74f5fb
eelanagaraj <7308464+eelanagaraj@users.noreply.github.com>
2021-03-04 20:42:14 +0100

    Add back parseURL call and add check for chrome browser

--- packages/sdk/dappkit/src/web.ts
+++ packages/sdk/dappkit/src/web.ts
@@ -15,6 +15,10 @@ export { FeeCurrency, requestAccountAddress, requestTxSig, TxParams } from './in
 
 // DappKit Web constants and helpers
 const localStorageKey = 'dappkit-web'
+
+// Ensure this is called on dappkit import
+parseURLOnRender()
+
 // hack to get around dappkit issue where new tabs are opened
 // and the url hash state is not respected (Note this implementation
 // of dappkit doesn't use URL hashes to always force the newtab experience).
@@ -24,10 +28,13 @@ export function parseURLOnRender() {
   if (typeof window !== 'undefined') {
     const params = new URL(window.location.href).searchParams
     if (params.get('type') && params.get('requestId')) {
-      localStorage.setItem(localStorageKey, window.location.href)
-      // TODO: seems like the below line is not getting executed in Chrome on iOS
-      // on iOS though, the newly opened window gets closed and the following window (instead of the previously opened tab) gets opened
-      window.close()
+      // Prevents error when reloading a chrome page with params in the URL
+      if (localStorage) {
+        localStorage.setItem(localStorageKey, window.location.href)
+        // TODO: seems like the below line is not getting executed in Chrome on iOS
+        // on iOS though, the newly opened window gets closed and the following window (instead of the previously opened tab) gets opened
+        window.close()
+      }
     }
   }
 }
