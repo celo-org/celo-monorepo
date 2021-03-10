4645273d36fba1b5719439c0868ab84b471fa486
yorhodes <yorhodes@cLabs.co>
2021-03-09 19:49:51 -0800

    Add more info to network:contracts

--- packages/cli/src/commands/network/contracts.ts
+++ packages/cli/src/commands/network/contracts.ts
@@ -1,5 +1,8 @@
+import { CeloContract } from '@celo/contractkit'
+import { newProxy } from '@celo/contractkit/lib/generated/Proxy'
+import { newICeloVersionedContract } from '@celo/contractkit/lib/generated/ICeloVersionedContract'
 import { BaseCommand } from '../../base'
-import { printValueMap2 } from '../../utils/cli'
+import { printValueMapRecursive } from '../../utils/cli'
 
 export default class Contracts extends BaseCommand {
   static description = 'Lists Celo core contracts and their addesses.'
@@ -9,6 +12,35 @@ export default class Contracts extends BaseCommand {
   }
 
   async run() {
-    printValueMap2(await this.kit.registry.addressMappingWithNotDeployedContracts())
+    const addressMapping = await this.kit.registry.addressMapping()
+    const info: {
+      [name in CeloContract]?: {
+        version: string
+        proxy: string
+        implementation: string
+        balance: any
+      }
+    } = {}
+    for (const [contract, proxy] of addressMapping.entries()) {
+      try {
+        const proxyContract = newProxy(this.kit.web3, proxy)
+        const implementation = await proxyContract.methods._getImplementation().call()
+
+        const versionedContract = newICeloVersionedContract(this.kit.web3, implementation)
+        const version = await versionedContract.methods.getVersionNumber().call()
+        const readableVersion = `${version[0]}.${version[1]}.${version[2]}.${version[3]}`
+
+        const balance = await this.kit.celoTokens.balancesOf(proxy)
+        info[contract] = {
+          version: readableVersion,
+          proxy,
+          implementation,
+          balance,
+        }
+      } catch (e) {
+        console.debug(`Contract ${contract} at address ${proxy} failed with ${e}`)
+      }
+    }
+    printValueMapRecursive(info)
   }
 }
