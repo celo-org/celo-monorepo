40b48b9511ed746896769c381c7f8d11bbb45a57
yorhodes <yorhodes@cLabs.co>
2021-03-11 15:59:29 -0800

    Revert build caching

--- packages/protocol/scripts/bash/release-lib.sh
+++ packages/protocol/scripts/bash/release-lib.sh
@@ -9,16 +9,12 @@ function build_tag() {
   rm -r contracts
   git checkout $BRANCH -- contracts 2>>$LOG_FILE >> $LOG_FILE
 
-  [ -d "build/contracts" ] && mv build/contracts build/contracts_tmp
-  if [ ! -d $BUILD_DIR ]; then
-    echo " - Build contract artifacts at $BUILD_DIR"
-    yarn build:sol >> $LOG_FILE
-    rm -rf $BUILD_DIR && mkdir -p $BUILD_DIR
-    mv build/contracts $BUILD_DIR
-  else
-    echo " - Contract artifacts already built at $BUILD_DIR"
-  fi
-  [ -d "build/contracts_tmp" ] && mv build/contracts_tmp build/contracts
+  echo " - Build contract artifacts at $BUILD_DIR"
+  mv build/contracts build/contracts_tmp
+  yarn build:sol >> $LOG_FILE
+  rm -rf $BUILD_DIR && mkdir -p $BUILD_DIR
+  mv build/contracts $BUILD_DIR
+  mv build/contracts_tmp build/contracts
 
   rm -r contracts
   git checkout - -- contracts 2>>$LOG_FILE >> $LOG_FILE
