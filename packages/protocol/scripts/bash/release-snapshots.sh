d04256a38f4c1ad50f8359038ad3640ab62d5df9
yorhodes <yorhodes@cLabs.co>
2021-03-11 18:04:20 -0800

    Remove commented make-release script

--- packages/protocol/scripts/bash/release-snapshots.sh
+++ packages/protocol/scripts/bash/release-snapshots.sh
@@ -6,9 +6,4 @@ do
         -a "celo-core-contracts-v$(($i - 1)).$NETWORK" \
         -b "celo-core-contracts-v$i.$NETWORK" \
         -r "releaseData/versionReports/release$i-report.json"
-    # yarn make-release -d -n "$NETWORK" \
-    #     -b "celo-core-contracts-v$i.$NETWORK" \
-    #     -i "releaseData/initializationData/release$i.json" \
-    #     -r "release$i-report.json" \
-    #     -p "release$i-proposal.json"
 done
