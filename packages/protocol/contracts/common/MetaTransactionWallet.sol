ab8db9709cb519b4c8c2dc68949fe6f6a9ff2786
Kevin Jue <kevjue@Kevins-MacBook-Pro.local>
2021-03-12 13:54:01 -0800

    set the correct contract version

--- packages/protocol/contracts/common/MetaTransactionWallet.sol
+++ packages/protocol/contracts/common/MetaTransactionWallet.sol
@@ -67,7 +67,7 @@ contract MetaTransactionWallet is
    * @return The storage, major, minor, and patch version of the contract.
    */
   function getVersionNumber() external pure returns (uint256, uint256, uint256, uint256) {
-    return (1, 1, 1, 1);
+    return (1, 1, 1, 0);
   }
 
   /**
