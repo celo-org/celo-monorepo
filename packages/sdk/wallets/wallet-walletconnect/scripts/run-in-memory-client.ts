b53d3a3b0faa6b8f15d798b61c4f6877bf005ab7
Alex Harley <alexbruceharley@gmail.com>
2021-03-23 20:10:59 +0100

    alpha.32

--- packages/sdk/wallets/wallet-walletconnect/scripts/run-in-memory-client.ts
+++ packages/sdk/wallets/wallet-walletconnect/scripts/run-in-memory-client.ts
@@ -1,5 +1,7 @@
+import { newKit } from '@celo/contractkit'
 import { WalletConnectWallet } from '../src'
-;(async function main() {
+
+async function main() {
   const wallet = new WalletConnectWallet({
     connect: {
       metadata: {
@@ -16,4 +18,22 @@ import { WalletConnectWallet } from '../src'
   const uri = await wallet.getUri()
   console.log(uri.toString())
   await wallet.init()
-})()
+
+  const [from] = await wallet.getAccounts()
+  const kit = newKit('https://alfajores-forno.celo-testnet.org', wallet)
+
+  /**
+   * Uncomment to send a test transaction
+   */
+  // const gold = await kit.contracts.getGoldToken()
+  // await gold
+  //   .transfer('0x4132F04EaCfdE9E2b707667A13CB69DbC5BABb68', '1')
+  //   .sendAndWaitForReceipt({ from })
+}
+
+main()
+  .then(() => process.exit())
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
