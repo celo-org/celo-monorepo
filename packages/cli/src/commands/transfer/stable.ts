9e03bff92dace52da98a2b32e83951929b3bc796
Gaston Ponti <pontigaston@gmail.com>
2021-03-24 19:25:00 -0300

    Unify stableToken wording

--- packages/cli/src/commands/transfer/stable.ts
+++ packages/cli/src/commands/transfer/stable.ts
@@ -7,19 +7,19 @@ export default class TransferStable extends TransferStableBase {
 
   static flags = {
     ...TransferStableBase.flags,
-    stable: flags.enum({
+    stableToken: flags.enum({
       options: Object.keys(StableToken),
       description: 'Name of the stable to be transfered',
     }),
   }
 
   static examples = [
-    'stable --from 0xa0Af2E71cECc248f4a7fD606F203467B500Dd53B --to 0x5409ed021d9299bf6814279a6a1411a7e866a631 --value 1000000000000000000',
+    'stable --from 0xa0Af2E71cECc248f4a7fD606F203467B500Dd53B --to 0x5409ed021d9299bf6814279a6a1411a7e866a631 --value 1000000000000000000 --stableToken cUSD',
   ]
 
   async init() {
     const res = this.parse(TransferStable)
-    const stableName = res.flags.stable
+    const stableName = res.flags.stableToken
     this._stableCurrency = StableToken[stableName as keyof typeof StableToken]
     await super.init()
   }
