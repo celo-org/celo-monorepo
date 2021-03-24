85ce045c39add744f34c34fd7723c8e3165c4244
Alex Harley <alexbruceharley@gmail.com>
2021-03-24 14:11:44 +0100

    feat: adding more logging to in memory client

--- packages/sdk/wallets/wallet-walletconnect/scripts/run-in-memory-client.ts
+++ packages/sdk/wallets/wallet-walletconnect/scripts/run-in-memory-client.ts
@@ -13,10 +13,13 @@ async function main() {
     },
     init: {
       relayProvider: 'wss://relay.walletconnect.org',
+      logger: 'error',
     },
   })
   const uri = await wallet.getUri()
-  console.log(uri.toString())
+  console.log(`=== START OUT OF BAND URI ===
+${uri.toString()}
+=== END OUT OF BAND URI ===`)
   await wallet.init()
 
   const [from] = await wallet.getAccounts()
@@ -29,6 +32,20 @@ async function main() {
   // await gold
   //   .transfer('0x4132F04EaCfdE9E2b707667A13CB69DbC5BABb68', '1')
   //   .sendAndWaitForReceipt({ from })
+  // console.log('Transaction sent!')
+
+  /**
+   * Uncomment to sign a test payload
+   */
+  // await kit.connection.sign(ensureLeading0x(Buffer.from('hello').toString()), from)
+  // console.log('Payload signed!')
+
+  /**
+   * Uncomment to hold connection open
+   */
+  // await new Promise((resolve) => setTimeout(resolve, 1000 * 60 * 60 * 24))
+
+  await wallet.close()
 }
 
 main()
