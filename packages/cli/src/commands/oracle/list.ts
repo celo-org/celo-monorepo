5f3a88cdefaaf9e3430399597607652b70034c61
Gaston Ponti <pontigaston@gmail.com>
2021-03-26 15:59:22 -0300

    [CK/cli] cEUR support. CK works with StableTokens not yet deployed (#7524)
    ### Description
    
    Add to the contractKit the possibility of having stable tokens not deployed, without breaking.
    If the net does not have one particular Stable, it won't be showed in the configs or balances.
    cli cEUR support
    
    ### Other changes
    
    Fix all cli network issues
    cli cEUR transfer
    cli exchanges working for cEUR
    cli oracles working for cEUR
    cli pay with different gasToken
    
    ### Tested
    
    Test against alfajores and mainnet (to check if the ck works as it should with a missing contract)
    
    ### Related issues
    
    - Fixes #7472
    - Fixes #7473
    - Fixes #7263
    - Fixes #7264
    - Fixes #7265 
    - Fixes #7266
    
    ### Backwards compatibility
    
    The `config` json, instead of having one entry for every StableToken, or for every Exchange, will have one `exchanges` and one `stableTokens` with all the info there, and those two keys were removed

--- packages/cli/src/commands/oracle/list.ts
+++ packages/cli/src/commands/oracle/list.ts
@@ -1,5 +1,7 @@
 import { CeloContract } from '@celo/contractkit'
+import { stableTokenContractArray } from '@celo/contractkit/lib/base'
 import { BaseCommand } from '../../base'
+import { failWith } from '../../utils/cli'
 
 export default class List extends BaseCommand {
   static description = 'List oracle addresses for a given token'
@@ -13,16 +15,22 @@ export default class List extends BaseCommand {
       name: 'token',
       required: true,
       description: 'Token to list the oracles for',
-      options: [CeloContract.StableToken],
+      options: stableTokenContractArray,
       default: CeloContract.StableToken,
     },
   ]
 
-  static example = ['list StableToken', 'list']
+  static example = ['list StableToken', 'list', 'list StableTokenEUR']
 
   async run() {
     const res = this.parse(List)
     const sortedOracles = await this.kit.contracts.getSortedOracles()
+
+    try {
+      await this.kit.registry.addressFor(res.args.token)
+    } catch {
+      failWith(`The ${res.args.token} contract was not deployed yet`)
+    }
 
     const oracles = await sortedOracles.getOracles(res.args.token)
     console.log(oracles)
