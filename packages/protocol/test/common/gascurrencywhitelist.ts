e17d3a9ea8ce94c2467b953c942905802af98fea
Farad <faradkhademi@gmail.com>
2021-03-30 09:22:27 +0430

    .

--- packages/protocol/test/common/gascurrencywhitelist.ts
+++ packages/protocol/test/common/gascurrencywhitelist.ts
@@ -9,45 +9,31 @@ import {
     RegistryInstance,
     MockSortedOraclesContract,
     MockSortedOraclesInstance,
-    StableTokenContract,
-    StableTokenInstance,
+    MockStableTokenContract,
+    MockStableTokenInstance,
 } from 'types'
 
 const FeeCurrencyWhitelist: FeeCurrencyWhitelistContract = artifacts.require('FeeCurrencyWhitelist')
 const Registry: RegistryContract = artifacts.require('Registry')
 const MockSortedOracles: MockSortedOraclesContract = artifacts.require('MockSortedOracles')
-const StableToken: StableTokenContract = artifacts.require('StableToken')
+const MockStableToken: MockStableTokenContract = artifacts.require('MockStableToken')
 
-// @ts-ignore
-StableToken.numberFormat = 'BigNumber'
+
 
 contract('FeeCurrencyWhitelist', (accounts: string[]) => {
     let feeCurrencyWhitelist: FeeCurrencyWhitelistInstance
     let registry: RegistryInstance
-    let stableToken: StableTokenInstance
+    let mockStableToken: MockStableTokenInstance
     let mockSortedOracles: MockSortedOraclesInstance
 
     const nonOwner = accounts[1]
 
-    const decimals = 18
-    const SECONDS_IN_A_WEEK = 604800
     const goldAmountForRate = new BigNumber('0x10000000000000000')
     const stableAmountForRate = new BigNumber(2).times(goldAmountForRate)
     beforeEach(async () => {
-        stableToken = await StableToken.new()
+        mockStableToken = await MockStableToken.new()
         registry = await Registry.new()
-
-        await stableToken.initialize(
-            'Celo Dollar',
-            'cUSD',
-            decimals,
-            registry.address,
-            fixed1,
-            SECONDS_IN_A_WEEK,
-            [],
-            [],
-            'Exchange'
-        )
+        // await registry.setAddressFor(CeloContractName.StableToken, mockStableToken.address)
 
         mockSortedOracles = await MockSortedOracles.new()
         await registry.setAddressFor(CeloContractName.SortedOracles, mockSortedOracles.address)
@@ -66,23 +52,27 @@ contract('FeeCurrencyWhitelist', (accounts: string[]) => {
         })
     })
 
-    describe('#addToken()', () => {
-        it('should not allow to add a token without an oracle price', async () => {
-            await assertRevert(feeCurrencyWhitelist.addToken(stableToken.address))
+    describe('#addToken() with an invalid oracle price', () => {
+        it('should not allow to add a token with an invalid oracle price', async () => {
+            await assertRevert(feeCurrencyWhitelist.addToken(mockStableToken.address))
+        })
+    })
+
+    describe('#addToken() with a valid oracle price', () => {
+        beforeEach(async () => {
+            await mockSortedOracles.setMedianRate(mockStableToken.address, stableAmountForRate)
+            await mockSortedOracles.setMedianTimestampToNow(mockStableToken.address)
+            await mockSortedOracles.setNumRates(mockStableToken.address, 2)
         })
 
         it('should allow the owner to add a token', async () => {
-            await mockSortedOracles.setMedianRate(stableToken.address, stableAmountForRate)
-            await mockSortedOracles.setMedianTimestampToNow(stableToken.address)
-            await mockSortedOracles.setNumRates(stableToken.address, 2)
-            await feeCurrencyWhitelist.addToken(stableToken.address)
+            await feeCurrencyWhitelist.addToken(mockStableToken.address)
             const tokens = await feeCurrencyWhitelist.getWhitelist()
-            assert.sameMembers(tokens, [stableToken.address])
+            assert.sameMembers(tokens, [mockStableToken.address])
         })
 
         it('should not allow a non-owner to add a token', async () => {
-            await assertRevert(feeCurrencyWhitelist.addToken(stableToken.address, { from: nonOwner }))
+            await assertRevert(feeCurrencyWhitelist.addToken(mockStableToken.address, { from: nonOwner }))
         })
-
     })
 })
