4f94ba35a20b27e3671853d4b74d186887489393
Farad <faradkhademi@gmail.com>
2021-03-30 15:37:09 +0430

    .

--- packages/protocol/migrations/03_whitelist.ts
+++ packages/protocol/migrations/03_whitelist.ts
@@ -1,9 +1,15 @@
 import { CeloContractName } from '@celo/protocol/lib/registry-utils'
 import { deploymentForCoreContract } from '@celo/protocol/lib/web3-utils'
 import { FeeCurrencyWhitelistInstance } from 'types'
+import { config } from '@celo/protocol/migrationsConfig'
+
+const initializeArgs = async (): Promise<any[]> => {
+  return [config.registry.predeployedProxyAddress]
+}
 
 module.exports = deploymentForCoreContract<FeeCurrencyWhitelistInstance>(
   web3,
   artifacts,
-  CeloContractName.FeeCurrencyWhitelist
+  CeloContractName.FeeCurrencyWhitelist,
+  initializeArgs
 )
