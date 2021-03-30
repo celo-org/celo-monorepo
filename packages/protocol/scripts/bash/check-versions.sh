947a176b93e37c10ed18f3d7a765c2f6cd8bd59d
Marcin Chrzanowski <martin@clabs.co>
2021-03-30 18:10:21 +0200

    Rename variables

--- packages/protocol/scripts/bash/check-versions.sh
+++ packages/protocol/scripts/bash/check-versions.sh
@@ -39,11 +39,15 @@ git fetch origin +'refs/tags/celo-core-contracts*:refs/tags/celo-core-contracts*
 source scripts/bash/release-lib.sh
 
 build_tag $OLD_BRANCH $LOG_FILE
-BUILD_DIR_1=$BUILD_DIR
+OLD_BRANCH_BUILD_DIR=$BUILD_DIR
 build_tag $NEW_BRANCH $LOG_FILE
-BUILD_DIR_2=$BUILD_DIR
+NEW_BRANCH_BUILD_DIR=$BUILD_DIR
 
 # Exclude test contracts, mock contracts, contract interfaces, Proxy contracts, inlined libraries,
 # MultiSig contracts, and the ReleaseGold contract.
 CONTRACT_EXCLUSION_REGEX=".*Test|Mock.*|I[A-Z].*|.*Proxy|MultiSig.*|ReleaseGold|MetaTransactionWallet|SlasherUtil|UsingPrecompiles"
-yarn ts-node scripts/check-backward.ts sem_check --old_contracts $BUILD_DIR_1/contracts --new_contracts $BUILD_DIR_2/contracts --exclude $CONTRACT_EXCLUSION_REGEX $REPORT_FLAG
+yarn ts-node scripts/check-backward.ts sem_check \
+    --old_contracts $OLD_BRANCH_BUILD_DIR/contracts \
+    --new_contracts $NEW_BRANCH_BUILD_DIR/contracts \
+    --exclude $CONTRACT_EXCLUSION_REGEX \
+    $REPORT_FLAG
