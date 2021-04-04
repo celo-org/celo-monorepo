a173d87acdbff74d08c9de4b9cb7b9b7a29941ff
alecps <alec@cLabs.co>
2021-04-04 17:59:53 -0500

    adds load test to monitor

--- packages/phone-number-privacy/monitor/src/query.ts
+++ packages/phone-number-privacy/monitor/src/query.ts
@@ -4,13 +4,12 @@ import { AuthSigner } from '@celo/identity/lib/odis/query'
 import { fetchEnv } from '@celo/phone-number-privacy-common'
 import { normalizeAddressWith0x, privateKeyToAddress } from '@celo/utils/lib/address'
 import { LocalWallet } from '@celo/wallet-local'
-import * as functions from 'firebase-functions'
+import { blockchainProvider, network } from './index'
 
 const privateKey = fetchEnv('PRIVATE_KEY')
 const phoneNumber = fetchEnv('PHONE_NUMBER')
 const accountAddress = normalizeAddressWith0x(privateKeyToAddress(privateKey)) // 0x1be31a94361a391bbafb2a4ccd704f57dc04d4bb
-
-const contractKit = newKit(functions.config().blockchain.provider, new LocalWallet())
+const contractKit = newKit(blockchainProvider, new LocalWallet())
 contractKit.connection.addAccount(privateKey)
 contractKit.defaultAccount = accountAddress
 
@@ -19,12 +18,11 @@ export const queryOdisForSalt = () => {
     authenticationMethod: OdisUtils.Query.AuthenticationMethod.WALLET_KEY,
     contractKit,
   }
-
   return OdisUtils.PhoneNumberIdentifier.getPhoneNumberIdentifier(
     phoneNumber,
     accountAddress,
     authSigner,
-    OdisUtils.Query.getServiceContext(functions.config().blockchain.name),
+    OdisUtils.Query.getServiceContext(network),
     undefined,
     'monitor:1.0.0'
   )
---
+++ packages/phone-number-privacy/monitor/src/scripts/runLoadTest.ts
@@ -0,0 +1,27 @@
+import { concurrentLoadTest } from '../test'
+
+const args = process.argv.slice(2)
+
+const printHelpAndExit = () => {
+  console.log('Usage: yarn loadTest <network> <numWorkers>')
+  process.exit(1)
+}
+
+if (args[0] === '--help' || args.length !== 2) {
+  printHelpAndExit()
+}
+
+switch (args[0]) {
+  case 'alfajores':
+    process.env.BLOCKCHAIN_PROVIDER = 'https://alfajores-forno.celo-testnet.org'
+    break
+  case 'mainnet':
+    process.env.BLOCKCHAIN_PROVIDER = 'https://forno.celo.org'
+    break
+  default:
+    printHelpAndExit()
+    break
+}
+process.env.NETWORK = args[0]
+
+concurrentLoadTest(Number(args[1]))
