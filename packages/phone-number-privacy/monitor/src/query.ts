737b019a6112a6b431b4e5a0ab648ec2c26dc510
alecps <alec@cLabs.co>
2021-04-06 11:00:55 -0500

    adds randomized account generation to load test

--- packages/phone-number-privacy/monitor/src/query.ts
+++ packages/phone-number-privacy/monitor/src/query.ts
@@ -2,18 +2,24 @@ import { newKit } from '@celo/contractkit'
 import { OdisUtils } from '@celo/identity'
 import { AuthSigner } from '@celo/identity/lib/odis/query'
 import { fetchEnv } from '@celo/phone-number-privacy-common'
+import { generateKeys, generateMnemonic, MnemonicStrength } from '@celo/utils/lib/account'
 import { normalizeAddressWith0x, privateKeyToAddress } from '@celo/utils/lib/address'
 import { LocalWallet } from '@celo/wallet-local'
 import { blockchainProvider, network } from './index'
 
-const privateKey = fetchEnv('PRIVATE_KEY')
 const phoneNumber = fetchEnv('PHONE_NUMBER')
-const accountAddress = normalizeAddressWith0x(privateKeyToAddress(privateKey)) // 0x1be31a94361a391bbafb2a4ccd704f57dc04d4bb
 const contractKit = newKit(blockchainProvider, new LocalWallet())
-contractKit.connection.addAccount(privateKey)
-contractKit.defaultAccount = accountAddress
 
-export const queryOdisForSalt = () => {
+const newPrivateKey = async () => {
+  const mnemonic = await generateMnemonic(MnemonicStrength.s256_24words)
+  return (await generateKeys(mnemonic)).privateKey
+}
+
+export const queryOdisForSalt = async () => {
+  const privateKey = await newPrivateKey()
+  const accountAddress = normalizeAddressWith0x(privateKeyToAddress(privateKey))
+  contractKit.connection.addAccount(privateKey)
+  contractKit.defaultAccount = accountAddress
   const authSigner: AuthSigner = {
     authenticationMethod: OdisUtils.Query.AuthenticationMethod.WALLET_KEY,
     contractKit,
