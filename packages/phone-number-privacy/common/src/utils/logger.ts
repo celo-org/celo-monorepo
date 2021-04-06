15331da792ca19a2e00fdab8e71104387c0634a2
alecps <alec@cLabs.co>
2021-04-05 22:34:13 -0500

    assign sessionID in combiner if not provided by client

--- packages/phone-number-privacy/common/src/utils/logger.ts
+++ packages/phone-number-privacy/common/src/utils/logger.ts
@@ -2,6 +2,7 @@ import Logger, { createLogger, levelFromName, LogLevelString, stdSerializers } f
 import bunyanDebugStream from 'bunyan-debug-stream'
 import { createStream } from 'bunyan-gke-stackdriver'
 import { NextFunction, Request, Response } from 'express'
+import { WarningMessage } from '../interfaces/error-utils'
 import { fetchEnv, fetchEnvOrDefault } from './config-utils'
 
 const logLevel = fetchEnvOrDefault('LOG_LEVEL', 'info') as LogLevelString
@@ -28,19 +29,29 @@ export const rootLogger: Logger = createLogger({
 })
 
 export function loggerMiddleware(req: Request, res: Response, next?: NextFunction): Logger {
+  const sessionID = req.body.sessionID || genSessionID()
+
   const requestLogger = rootLogger.child({
     endpoint: req.path,
-    sessionID: req.body.sessionID, // May be undefined
-    requestID: genRequestID(),
+    sessionID,
   })
-
   res.locals.logger = requestLogger
+
+  if (!req.body.sessionID) {
+    req.body.sessionID = sessionID
+    requestLogger.info(WarningMessage.MISSING_SESSION_ID)
+    requestLogger.info(
+      { request: req.body },
+      `Request received w/o sessionID, assigning ${sessionID}`
+    )
+  }
+
   if (next) {
     next()
   }
   return requestLogger
 }
 
-export function genRequestID() {
+export function genSessionID() {
   return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
 }
