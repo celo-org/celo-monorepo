a351296eaaea5f087a9f5af92634b18e55059752
alecps <alec@cLabs.co>
2021-04-07 18:18:12 -0500

    fixes load test script

--- packages/phone-number-privacy/monitor/src/query.ts
+++ packages/phone-number-privacy/monitor/src/query.ts
@@ -6,10 +6,15 @@ import { genSessionID } from '@celo/phone-number-privacy-common/lib/utils/logger
 import { generateKeys, generateMnemonic, MnemonicStrength } from '@celo/utils/lib/account'
 import { normalizeAddressWith0x, privateKeyToAddress } from '@celo/utils/lib/address'
 import { LocalWallet } from '@celo/wallet-local'
-import { blockchainProvider, network } from './index'
+import * as functions from 'firebase-functions'
+
+const haveConfig = !!functions.config().blockchain
+const network = () => (haveConfig ? functions.config().blockchain.network : process.env.NETWORK)
+const blockchainProvider = () =>
+  haveConfig ? functions.config().blockchain.provider : process.env.BLOCKCHAIN_PROVIDER
 
 const phoneNumber = fetchEnv('PHONE_NUMBER')
-const contractKit = newKit(blockchainProvider, new LocalWallet())
+const contractKit = newKit(blockchainProvider(), new LocalWallet())
 
 const newPrivateKey = async () => {
   const mnemonic = await generateMnemonic(MnemonicStrength.s256_24words)
@@ -17,6 +22,8 @@ const newPrivateKey = async () => {
 }
 
 export const queryOdisForSalt = async () => {
+  console.log(network()) // tslint:disable-line:no-console
+  console.log(blockchainProvider()) // tslint:disable-line:no-console
   const privateKey = await newPrivateKey()
   const accountAddress = normalizeAddressWith0x(privateKeyToAddress(privateKey))
   contractKit.connection.addAccount(privateKey)
@@ -29,7 +36,7 @@ export const queryOdisForSalt = async () => {
     phoneNumber,
     accountAddress,
     authSigner,
-    OdisUtils.Query.getServiceContext(network),
+    OdisUtils.Query.getServiceContext(network()),
     undefined,
     'monitor:1.0.0',
     undefined,
