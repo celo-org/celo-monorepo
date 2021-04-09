f1ddeec6d4164419d92b6cab5c1d19c64d767958
Asa Oines <a@celo.org>
2021-04-09 15:45:06 -0400

    Update branch

--- packages/protocol/contracts/common/ProxyCloneFactory.sol
+++ packages/protocol/contracts/common/ProxyCloneFactory.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.5.13;
 
-import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-import "./CloneFactory.sol";
-import "./ProxyV2.sol";
 
-contract ProxyCloneFactory is CloneFactory, Ownable {
-  event ProxyCreated(ProxyV2 proxy);
+import "./InitializableProxy.sol";
+
+contract ProxyCloneFactory is Ownable {
+  event ProxyCreated(InitializableProxy proxy);
 
   address public proxyAddress;
 
@@ -17,8 +16,23 @@ contract ProxyCloneFactory is CloneFactory, Ownable {
     proxyAddress = _proxyAddress;
   }
 
+  // Copied from open-zeppelin.
+  // TODO: Upgrade solc version and import from open-zeppelin instead.
+  function clone(address master) internal returns (address instance) {
+    // solhint-disable-next-line no-inline-assembly
+    assembly {
+      let ptr := mload(0x40)
+      mstore(ptr, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
+      mstore(add(ptr, 0x14), shl(0x60, master))
+      mstore(add(ptr, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+      instance := create(0, ptr, 0x37)
+    }
+    require(instance != address(0), "ERC1167: create failed");
+  }
+
   function deploy(address implementation, bytes calldata initCallData) external {
-    ProxyV2 proxy = ProxyV2(createClone(proxyAddress));
+    // Cast to prevent compiler from complaining about the output of clone() not being payable.
+    InitializableProxy proxy = InitializableProxy(address(uint160(clone(proxyAddress))));
     proxy._initialize(address(this));
     proxy._setAndInitializeImplementation(implementation, initCallData);
     // TODO(asa): In the current version of Komenci we transfer proxy ownership to the user.
--- packages/protocol/test/common/onboarding.ts
+++ packages/protocol/test/common/onboarding.ts
@@ -5,9 +5,17 @@ import { ensureLeading0x, trimLeading0x } from '@celo/utils/lib/address'
 import { getDeployedProxiedContract } from '@celo/protocol/lib/web3-utils'
 import { getParsedSignatureOfAddress } from '@celo/protocol/lib/signing-utils'
 import {
+  assumeOwnership,
+  getDerivedKey,
+  getVerificationCodeSignature,
+  KeyOffsets,
+  unlockAndAuthorizeKey,
+} from '@celo/protocol/lib/test-utils'
+import {
+  AccountsContract,
+  AccountsInstance,
   AttestationsTestContract,
   AttestationsTestInstance,
-  AccountsInstance,
   StableTokenInstance,
   MockRandomInstance,
   MockRandomContract,
@@ -22,7 +30,6 @@ import {
   ProxyCloneFactoryInstance,
   RegistryInstance,
 } from 'types'
-import { AttestationUtils } from '@celo/utils'
 
 const executeMetaTransaction = async (
   signer: string,
@@ -45,69 +52,11 @@ const getExecuteMetaTransactionData = async (
     .encodeABI()
 }
 
+const Accounts: AccountsContract = artifacts.require('Accounts')
 const Attestations: AttestationsTestContract = artifacts.require('AttestationsTest')
 const MTW: MetaTransactionWalletContract = artifacts.require('MetaTransactionWallet')
-const MTWDeployer: MetaTransactionWalletDeployerContract = artifacts.require(
-  'MetaTransactionWalletDeployer'
-)
-const Validators: MockValidatorsContract = artifacts.require('MockValidators')
 const Random: MockRandomContract = artifacts.require('MockRandom')
-
 contract('Komenci Onboarding', (_accounts: string[]) => {
-  /*
-   * Helpers for verification
-   */
-  enum KeyOffsets {
-    VALIDATING_KEY_OFFSET,
-    ATTESTING_KEY_OFFSET,
-    NEW_VALIDATING_KEY_OFFSET,
-    VOTING_KEY_OFFSET,
-  }
-
-  // Private keys of each of the 10 miners, in the same order as their addresses in 'accounts'.
-  const accountPrivateKeys: string[] = [
-    '0xf2f48ee19680706196e2e339e5da3491186e0c4c5030670656b0e0164837257d',
-    '0x5d862464fe9303452126c8bc94274b8c5f9874cbd219789b3eb2128075a76f72',
-    '0xdf02719c4df8b9b8ac7f551fcb5d9ef48fa27eef7a66453879f4d8fdc6e78fb1',
-    '0xff12e391b79415e941a94de3bf3a9aee577aed0731e297d5cfa0b8a1e02fa1d0',
-    '0x752dd9cf65e68cfaba7d60225cbdbc1f4729dd5e5507def72815ed0d8abc6249',
-    '0xefb595a0178eb79a8df953f87c5148402a224cdf725e88c0146727c6aceadccd',
-    '0x83c6d2cc5ddcf9711a6d59b417dc20eb48afd58d45290099e5987e3d768f328f',
-    '0xbb2d3f7c9583780a7d3904a2f55d792707c345f21de1bacb2d389934d82796b2',
-    '0xb2fd4d29c1390b71b8795ae81196bfd60293adf99f9d32a0aff06288fcdac55f',
-    '0x23cb7121166b9a2f93ae0b7c05bde02eae50d64449b2cbb42bc84e9d38d6cc89',
-  ]
-
-  async function getVerificationCodeSignature(
-    account: string,
-    issuer: string,
-    identifier: string
-  ): Promise<[number, string, string]> {
-    const privateKey = getDerivedKey(KeyOffsets.ATTESTING_KEY_OFFSET, issuer)
-    const { v, r, s } = AttestationUtils.attestToIdentifier(identifier, account, privateKey)
-    return [v, r, s]
-  }
-
-  const getDerivedKey = (offset: number, address: string) => {
-    const pKey = accountPrivateKeys[_accounts.indexOf(address)]
-    const aKey = Buffer.from(pKey.slice(2), 'hex')
-    aKey.write((aKey[0] + offset).toString(16))
-    return '0x' + aKey.toString('hex')
-  }
-
-  const unlockAndAuthorizeKey = async (offset: number, authorizeFn: any, account: string) => {
-    const key = getDerivedKey(offset, account)
-    const addr = privateKeyToAddress(key)
-    // @ts-ignore
-    await web3.eth.personal.importRawKey(key, 'passphrase')
-    await web3.eth.personal.unlockAccount(addr, 'passphrase', 1000000)
-
-    const signature = await getParsedSignatureOfAddress(web3, account, addr)
-    return authorizeFn(addr, signature.v, signature.r, signature.s, {
-      from: account,
-    })
-  }
-
   let accounts: AccountsInstance
   let stableToken: StableTokenInstance
   let mtw: MetaTransactionWalletInstance
@@ -119,25 +68,31 @@ contract('Komenci Onboarding', (_accounts: string[]) => {
   const selectIssuersWaitBlocks = 4
   const maxAttestations = 20
   const user: string = _accounts[1]
+  // Random hex strings
   const dek = '0x02f2f48ee19680706196e2e339e5da3491186e0c4c5030670656b0e01611111111'
   const identifier = '0x02f2f48ee19680706196e2e339e5da3491186e0c4c5030670656b0e016111111'
   const numAttestations = 3
-  // const attestationFee = web3.utils.toWei(numAttestations * 0.05, 'ether').toString()
+  // TODO(asa): Clean this up
+  // const perAttestationFee = web3.utils.toWei(numAttestations * 0.05, 'ether').toString()
   const perAttestationFee = '50000000000000000'
   const attestationFee = '150000000000000000'
   before(async () => {
-    accounts = await getDeployedProxiedContract('Accounts', artifacts)
     stableToken = await getDeployedProxiedContract('StableToken', artifacts)
     mtw = await MTW.new(false)
     // The komenci version is set as a test contract because we are not using it with a proxy.
     relayermtw = await MTW.new(true)
     await relayermtw.initialize(_accounts[0])
 
-    // Set up the required mocks the will allow verification to work.
+    // Set up the required mocks that will allow verification to work with ganache.
     registry = await getDeployedProxiedContract('Registry', artifacts)
     random = await Random.new()
-    // Note this only works for now because we deleted the governance migration
+    accounts = await Accounts.new()
+    await accounts.initialize(registry.address)
+    // Take ownership of the registry contract to point it to the mocks.
+    await assumeOwnership(['Registry'], _accounts[0])
     await registry.setAddressFor(CeloContractName.Random, random.address)
+    await registry.setAddressFor(CeloContractName.Accounts, accounts.address)
+    const Validators: MockValidatorsContract = artifacts.require('MockValidators')
     const mockValidators = await Validators.new()
     await registry.setAddressFor(CeloContractName.Validators, mockValidators.address)
 
@@ -147,12 +102,14 @@ contract('Komenci Onboarding', (_accounts: string[]) => {
         await unlockAndAuthorizeKey(
           KeyOffsets.VALIDATING_KEY_OFFSET,
           accounts.authorizeValidatorSigner,
-          account
+          account,
+          _accounts
         )
         await unlockAndAuthorizeKey(
           KeyOffsets.ATTESTING_KEY_OFFSET,
           accounts.authorizeAttestationSigner,
-          account
+          account,
+          _accounts
         )
       })
     )
@@ -174,12 +131,15 @@ contract('Komenci Onboarding', (_accounts: string[]) => {
       )
       await attestations.__setValidators(
         _accounts.map((account) =>
-          privateKeyToAddress(getDerivedKey(KeyOffsets.VALIDATING_KEY_OFFSET, account))
+          privateKeyToAddress(getDerivedKey(KeyOffsets.VALIDATING_KEY_OFFSET, account, _accounts))
         )
       )
     })
 
     describe('Current flow', () => {
+      const MTWDeployer: MetaTransactionWalletDeployerContract = artifacts.require(
+        'MetaTransactionWalletDeployer'
+      )
       let mtwDeployer: MetaTransactionWalletDeployerInstance
       before(async () => {
         mtwDeployer = await MTWDeployer.new()
@@ -277,7 +237,12 @@ contract('Komenci Onboarding', (_accounts: string[]) => {
         const issuers = await attestations.getAttestationIssuers(identifier, mtw.address)
         for (let i = 0; i < numAttestations; i++) {
           const issuer = issuers[i]
-          ;[v, r, s] = await getVerificationCodeSignature(mtw.address, issuer, identifier)
+          ;[v, r, s] = await getVerificationCodeSignature(
+            mtw.address,
+            issuer,
+            identifier,
+            _accounts
+          )
           const complete = {
             value: 0,
             destination: attestations.address,
@@ -409,7 +374,12 @@ contract('Komenci Onboarding', (_accounts: string[]) => {
           const issuers = await attestations.getAttestationIssuers(identifier, mtw.address)
           for (let i = 0; i < numAttestations; i++) {
             const issuer = issuers[i]
-            ;[v, r, s] = await getVerificationCodeSignature(mtw.address, issuer, identifier)
+            ;[v, r, s] = await getVerificationCodeSignature(
+              mtw.address,
+              issuer,
+              identifier,
+              _accounts
+            )
             const complete = {
               value: 0,
               destination: attestations.address,
