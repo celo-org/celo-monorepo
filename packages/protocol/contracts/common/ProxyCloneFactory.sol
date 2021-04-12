03088585d74c7b05ad6b25f7d2d064c93bbd64cb
Asa Oines <a@celo.org>
2021-04-12 10:20:21 -0400

    Fix build

--- packages/protocol/contracts/common/ProxyCloneFactory.sol
+++ packages/protocol/contracts/common/ProxyCloneFactory.sol
@@ -31,7 +31,7 @@ contract ProxyCloneFactory is Ownable {
     assembly {
       let ptr := mload(0x40)
       mstore(ptr, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
-      mstore(add(ptr, 0x14), shl(0x60, master))
+      mstore(add(ptr, 0x14), shl(0x60, _proxyAddress))
       mstore(add(ptr, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
       instance := create(0, ptr, 0x37)
     }
