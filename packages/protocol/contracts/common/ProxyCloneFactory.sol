0988d247de524d503cbe0f1b5eac04d31c8220a4
Asa Oines <a@celo.org>
2021-04-15 09:42:53 -0400

    Respond to comments on contract code

--- packages/protocol/contracts/common/ProxyCloneFactory.sol
+++ packages/protocol/contracts/common/ProxyCloneFactory.sol
@@ -5,33 +5,35 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 import "./InitializableProxy.sol";
 
+/**
+ * @title A factory for deploying EIP-1167 clones of upgradable Proxy contracts.
+ */
 contract ProxyCloneFactory is Ownable {
-  address public proxyAddress;
-  event ProxyCreated(InitializableProxy proxy);
-
-  function() external payable {}
+  // The address of the Proxy implementation to clone.
+  address public proxyImplementationAddress;
+  event ProxyCloneCreated(address proxyClone);
 
   /**
-   * @notice Sets the address of the implementation to clone.
-   * @param The address of the implementation to clone.
+   * @notice Sets the address of the Proxy implementation to clone.
+   * @param The address of the Proxy implementation to clone.
    */
-  function setProxyAddress(address _proxyAddress) external onlyOwner {
-    proxyAddress = _proxyAddress;
+  function setImplementationAddress(address _proxyImplementationAddress) external onlyOwner {
+    proxyImplementationAddress = _proxyImplementationAddress;
   }
 
   // TODO: Upgrade solc version and import from open-zeppelin instead.
   /**
-   * @notice Creates an EIP-1167 style clone of the specified `_proxyAddress`.
-   * @param The address of the implementation to clone.
+   * @notice Creates an EIP-1167 style clone of the specified `_proxyImplementationAddress`.
+   * @param The address of the Proxy implementation to clone.
    * @return The address of the clone.
    * @dev Copied from open-zeppelin.
    */
-  function clone(address _proxyAddress) internal returns (address instance) {
+  function clone(address _proxyImplementationAddress) internal returns (address instance) {
     // solhint-disable-next-line no-inline-assembly
     assembly {
       let ptr := mload(0x40)
       mstore(ptr, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
-      mstore(add(ptr, 0x14), shl(0x60, _proxyAddress))
+      mstore(add(ptr, 0x14), shl(0x60, _proxyImplementationAddress))
       mstore(add(ptr, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
       instance := create(0, ptr, 0x37)
     }
@@ -39,18 +41,21 @@ contract ProxyCloneFactory is Ownable {
   }
 
   /**
-   * @notice Creates an EIP-1167 style proxy clone, points to an implementation and initializes.
+   * @notice Creates an EIP-1167 style clone of a Proxy contract, points the Proxy to an
+   *         implementation and initializes it.
    * @param implementation The address to point the proxy to.  
    * @param initCallData The function to call on the implementation and the corresponding args.
    */
   function deploy(address implementation, bytes calldata initCallData) external {
     // Cast to prevent compiler from complaining about the output of clone() not being payable.
-    InitializableProxy proxy = InitializableProxy(address(uint160(clone(proxyAddress))));
-    proxy._initialize(address(this));
-    proxy._setAndInitializeImplementation(implementation, initCallData);
+    InitializableProxy proxyClone = InitializableProxy(
+      address(uint160(clone(proxyImplementationAddress)))
+    );
+    proxyClone._initialize(address(this));
+    proxyClone._setAndInitializeImplementation(implementation, initCallData);
     // TODO(asa): In the current version of Komenci we transfer proxy ownership to the user.
     // Need to decide which to do.
-    proxy._transferOwnership(address(proxy));
-    emit ProxyCreated(proxy);
+    proxyClone._transferOwnership(address(proxyClone));
+    emit ProxyCloneCreated(address(proxyClone));
   }
 }
