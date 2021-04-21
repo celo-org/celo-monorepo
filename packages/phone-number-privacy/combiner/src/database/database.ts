12e43cb714fe2a406897772ecfccfe164a789fda
codyborn <codyborn@outlook.com>
2021-04-21 16:46:21 -0700

    Re-add automigration to combiner db

--- packages/phone-number-privacy/combiner/src/database/database.ts
+++ packages/phone-number-privacy/combiner/src/database/database.ts
@@ -1,5 +1,8 @@
+import { rootLogger as logger } from '@celo/phone-number-privacy-common'
 import knex from 'knex'
+import Knex from 'knex/types'
 import config, { DEV_MODE } from '../config'
+import { NUMBER_PAIRS_COLUMN, NUMBER_PAIRS_TABLE } from './models/numberPair'
 
 const db = knex({
   client: 'pg',
@@ -7,10 +10,41 @@ const db = knex({
   debug: DEV_MODE,
 })
 
+export async function initDatabase(doTestQuery = true) {
+  logger.info('Running Migrations')
+
+  await db.migrate.latest({
+    directory: './dist/migrations',
+    loadExtensions: ['.js'],
+  })
+
+  if (doTestQuery) {
+    await executeTestQuery(db)
+  }
+
+  logger.info('Database initialized successfully')
+}
+
 export function getDatabase() {
   return db
 }
 
 export function getTransaction() {
   return db.transaction()
+}
+
+async function executeTestQuery(_db: Knex) {
+  logger.info('Querying first row')
+  const result = await _db(NUMBER_PAIRS_TABLE).select(NUMBER_PAIRS_COLUMN.userPhoneHash).limit(1)
+
+  if (!result) {
+    throw new Error('No result from count, have migrations been run?')
+  }
+
+  const userPhoneHash = Object.values(result)[0]
+  if (userPhoneHash === undefined || userPhoneHash === null || userPhoneHash === '') {
+    throw new Error('No result from count, have migrations been run?')
+  }
+
+  logger.info(`Found ${userPhoneHash} userPhoneHash`)
 }
