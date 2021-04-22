953bfa8acbebf8298103ee1e7c1f55166199d404
yorhodes <yorhodes@cLabs.co>
2021-04-22 16:51:00 -0700

    Fix cli lint

--- packages/cli/src/test-utils/setup.global.ts
+++ packages/cli/src/test-utils/setup.global.ts
@@ -1,10 +1,9 @@
-import * as path from 'path'
-
+import baseSetup from '@celo/dev-utils/lib/ganache-setup'
 // Has to import the matchers somewhere so that typescript knows the matchers have been made available
 import _unused from '@celo/dev-utils/lib/matchers'
-import baseSetup from '@celo/dev-utils/lib/ganache-setup'
 // @ts-ignore
 import flakeTrackerSetup from '@celo/flake-tracker/src/jest/setup.global.js'
+import * as path from 'path'
 
 // Warning: There should be an unused import of '@celo/dev-utils/lib/matchers' above.
 // If there is not, then your editor probably deleted it automatically.
