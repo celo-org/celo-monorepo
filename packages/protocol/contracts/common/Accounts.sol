4e914a924d2666ea11155178b6013dab5de8050c
Alex Harley <alexbruceharley@gmail.com>
2021-04-22 11:05:49 +0200

    chore: correctly upgrade version

--- packages/protocol/contracts/common/Accounts.sol
+++ packages/protocol/contracts/common/Accounts.sol
@@ -96,7 +96,7 @@ contract Accounts is
    * @return The storage, major, minor, and patch version of the contract.
    */
   function getVersionNumber() external pure returns (uint256, uint256, uint256, uint256) {
-    return (2, 0, 0, 0);
+    return (1, 1, 2, 0);
   }
 
   /**
