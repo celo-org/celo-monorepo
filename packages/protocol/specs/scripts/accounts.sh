6592e89d3424f46330cc1a87c085c0a9da0ee961
Shelly Grossman <shellygr151@gmail.com>
2021-05-20 23:48:23 +0300

    updated accounts

--- packages/protocol/specs/scripts/accounts.sh
+++ packages/protocol/specs/scripts/accounts.sh
@@ -1,6 +1,7 @@
 certoraRun specs/harnesses/AccountsHarness.sol \
 	--verify AccountsHarness:specs/accounts.spec \
 	--optimistic_loop \
+	--loop_iter 2 \
 	--cache celo_accounts \
 	--solc_args "['--evm-version', 'istanbul']" \
 	--msg "Accounts"
