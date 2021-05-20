eae5e712bdd3bed13a1258b3a2086634462b7761
Shelly Grossman <shellygr151@gmail.com>
2021-05-20 03:27:17 +0300

    governance start

--- packages/protocol/specs/scripts/accountsPrivileged.sh
+++ packages/protocol/specs/scripts/accountsPrivileged.sh
@@ -1,5 +1,6 @@
 certoraRun.py specs/harnesses/AccountsHarness.sol \
 	--verify AccountsHarness:specs/accountsPrivileged.spec \
 	--optimistic_loop \
+	--cache celo_accounts \
 	--solc_args "['--evm-version', 'istanbul']" \
 	--msg "Accounts privilege check"
---
+++ packages/protocol/specs/scripts/applyHarness.sh
@@ -0,0 +1,1 @@
+perl -0777 -i -pe 's/private/internal/g' contracts/governance/Governance.sol
