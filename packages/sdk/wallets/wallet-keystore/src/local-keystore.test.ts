b50ac62c5446d45b6e8c4ea20a34bfa0a24c92d2
eelanagaraj <7308464+eelanagaraj@users.noreply.github.com>
2021-06-11 11:31:17 +0200

    Cleaned up some of the impl

--- packages/sdk/wallets/wallet-keystore/src/local-keystore.test.ts
+++ packages/sdk/wallets/wallet-keystore/src/local-keystore.test.ts
@@ -1,26 +1,8 @@
 // import { CeloTx, EncodedTransaction } from '@celo/connect'
-import { trimLeading0x } from '@celo/utils/lib/address'
-import { promises as fspromises } from 'fs'
+import { normalizeAddressWith0x, privateKeyToAddress, trimLeading0x } from '@celo/utils/lib/address'
+import { rmdirSync } from 'fs'
 import path from 'path'
-// import { Encrypt } from '@celo/utils/lib/ecies'
-// import { verifySignature } from '@celo/utils/lib/signatureUtils'
-// import { recoverTransaction, verifyEIP712TypedDataSigner } from '@celo/wallet-base'
-// import Web3 from 'web3'
 import { KeystoreWalletWrapper, LocalKeystore } from './local-keystore'
-
-// const CHAIN_ID = 44378
-
-// Sample data from the official EIP-712 example:
-// https://github.com/ethereum/EIPs/blob/master/assets/eip-712/Example.js
-
-// const PRIVATE_KEY1 = '1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef'
-// const PUBLIC_KEY1 = privateKeyToPublicKey(PRIVATE_KEY1)
-// const ACCOUNT_ADDRESS1 = normalizeAddressWith0x(privateKeyToAddress(PRIVATE_KEY1))
-// const PRIVATE_KEY2 = '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890fdeccc'
-// const ACCOUNT_ADDRESS2 = normalizeAddressWith0x(privateKeyToAddress(PRIVATE_KEY2))
-
-// const FEE_ADDRESS = ACCOUNT_ADDRESS1
-// const CURRENCY_ADDRESS = ACCOUNT_ADDRESS2
 
 // TODO remove later once tests are back to normal speed
 jest.setTimeout(15000)
@@ -32,21 +14,26 @@ const TEST_DIRECTORY =
   '/Users/eelanagaraj/celo/celo-monorepo/packages/sdk/wallets/wallet-keystore/test-keystore-dir'
 
 describe('local keystore tests', () => {
-  // let keystore: LocalKeystore
+  let keystore: LocalKeystore
+  let keystoreTestDir: string
 
-  // beforeEach(() => {
-  //   keystore = new LocalKeystore()
-  // })
-  // test('generate keystore file contents from private key', async () => {
-  //   const x = await keystore.getPrivateKey('', '')
-  //   console.log(x)
-  //   await keystore.importPrivateKey(
-  //     TEST_PK,
-  //     TEST_PASSWORD
-  //   )
-  // })
+  // TODO improve mock the file creation +
 
-  xit('decrypts and returns raw private key from keystore file', async () => {
+  beforeAll(() => {
+    keystoreTestDir = path.join(TEST_DIRECTORY, `test-keystores-${Date.now()}`)
+  })
+
+  beforeEach(() => {
+    // TODO perhaps only create the LocalKeystore for limited cases (speed up static tests)
+    const randString = (Math.random() + 1).toString().substring(2, 10)
+    keystore = new LocalKeystore(path.join(keystoreTestDir, `test-keystore-${randString}`))
+  })
+
+  afterAll(() => {
+    rmdirSync(keystoreTestDir, { recursive: true })
+  })
+
+  it('decrypts and returns raw private key from keystore file', async () => {
     const testPk = await LocalKeystore.getPrivateKeyFromFile(
       path.join(
         TEST_DIRECTORY,
@@ -57,7 +44,7 @@ describe('local keystore tests', () => {
     expect(trimLeading0x(testPk)).toBe(TEST_PK)
   })
 
-  xit('fails to decrypt keystore file with wrong password', async () => {
+  it('fails to decrypt keystore file with wrong password', async () => {
     try {
       await LocalKeystore.getPrivateKeyFromFile(
         path.join(
@@ -72,44 +59,33 @@ describe('local keystore tests', () => {
     }
   })
 
-  xit('gets account address from file', async () => {
-    console.log(
-      (await fspromises.readdir(TEST_DIRECTORY)).map((file) =>
-        LocalKeystore.getAddressFromFile(path.join(TEST_DIRECTORY, file))
-      )
-    )
-    // for (let file of await fspromises.readdir(TEST_DIRECTORY)) {
-    //   console.log(LocalKeystore.getAddressFromFile(path.join(TEST_DIRECTORY, file)))
-    // }
+  it('imports private key into keystore file and lists properly', async () => {
+    await keystore.importPrivateKey(TEST_PK, TEST_PASSWORD)
+    expect(await keystore.listKeystoreAccounts()).toEqual([
+      normalizeAddressWith0x(privateKeyToAddress(TEST_PK)),
+    ])
   })
 })
 
 describe('keystore wallet wrapper tests', () => {
   let keystoreWallet: KeystoreWalletWrapper
+  let keystoreTestDir: string
+
+  beforeAll(() => {
+    keystoreTestDir = path.join(TEST_DIRECTORY, `test-wallet-keystore-${Date.now()}`)
+  })
 
   beforeEach(() => {
-    keystoreWallet = new KeystoreWalletWrapper()
+    const randString = (Math.random() + 1).toString().substring(2, 10)
+    keystoreWallet = new KeystoreWalletWrapper(
+      path.join(keystoreTestDir, `test-keystore-${randString}`)
+    )
   })
 
-  it('imports private key into keystore file and lists properly', async () => {
+  it('imports private key into keystore wallet properly', async () => {
     await keystoreWallet.importPrivateKey(TEST_PK, TEST_PASSWORD)
-    // expect(await keystoreWallet.listKeystoreAccounts()).toEqual([privateKeyToAddress(TEST_PK)])
-    // TODO mock the file creation +
-    // TODO check if a file has been created -->
+    expect(keystoreWallet.getLocalWallet().getAccounts()).toEqual([
+      normalizeAddressWith0x(privateKeyToAddress(TEST_PK)),
+    ])
   })
-
-  // xit('fails to decrypt keystore file with wrong password', async () => {
-  //   try {
-  //     await LocalKeystore.getPrivateKeyFromFile(
-  //       path.join(
-  //         TEST_DIRECTORY,
-  //         'UTC--2021-06-08T15-15-22.742Z--be3908acec362af0382ebc56e06b82ce819b19e8'
-  //       ),
-  //       TEST_PASSWORD + '!'
-  //     )
-  //     throw new Error('Expected exception to be thrown')
-  //   } catch (e) {
-  //     expect(e.message).toBe('Key derivation failed - possibly wrong passphrase')
-  //   }
-  // })
 })
--- packages/sdk/wallets/wallet-keystore/src/local-keystore.ts
+++ packages/sdk/wallets/wallet-keystore/src/local-keystore.ts
@@ -9,7 +9,8 @@ import path from 'path'
 /**
  * Stores and provides access to private keys.
  */
-// TODO convert into an interface (then have filesystem keystore, browser keystore, etc.)
+
+// TODO creating an interface for this as well (then have filesystem keystore, browser keystore, etc.)
 export class LocalKeystore {
   /**
    * Takes in a string private key and saves this as an encrypted keystore file
@@ -17,111 +18,22 @@ export class LocalKeystore {
 
   private _keystoreDir: string
 
-  makeDefaultKeystoreDir(): string {
-    // TODO Figure out how this is done for celocli files, etc.?
-
-    const defaultDir = path.join(
-      '/Users/eelanagaraj/celo/celo-monorepo/packages/sdk/wallets/wallet-keystore/test-keystore-dir',
-      'keystore'
-    )
-    mkdirSync(defaultDir, { recursive: true })
-    return defaultDir
-  }
-
   constructor(keystoreDir?: string) {
-    this._keystoreDir = keystoreDir ? keystoreDir : this.makeDefaultKeystoreDir()
-  }
-
-  // TODO restructure/reorganize this
-  static async importPrivateKey(
-    privateKey: string,
-    password: string,
-    keystoreDir = '/Users/eelanagaraj/celo/celo-monorepo/packages/sdk/wallets/wallet-keystore/test-keystore-dir'
-  ) {
-    const key = Buffer.from(privateKey, 'hex')
-    const wallet = Wallet.fromPrivateKey(key)
-    const keystore = await wallet.toV3String(password)
-    const fileName = wallet.getV3Filename(Date.now())
-    // TODO handle if we add the same account twice -- don't overwrite and only allow one?
-    writeFileSync(path.join(keystoreDir, fileName), keystore)
-    // TODO take in an interface for writing to file storage?
-    // That could then in theory be switched out for browser/file system/mock test writer/etc.
-  }
-
-  static async getPrivateKeyFromFile(keystoreFile: string, password: string): Promise<string> {
-    const keystore = readFileSync(keystoreFile).toString()
-    const pkTest = (await Wallet.fromV3(keystore, password)).getPrivateKeyString()
-    console.log(pkTest)
-    return pkTest
-  }
-
-  static getAddressFromFile(keystoreFile: string): string {
-    // TODO is this possible without actually decrypting the file?
-    const rawKeystore = readFileSync(keystoreFile).toString()
-    try {
-      const address = JSON.parse(rawKeystore).address
-      // console.log(JSON.parse(rawKeystore))
-      // console.log(address)
-      return ensureLeading0x(address)
-    } catch (e) {
-      console.log(e)
-      throw new Error('Unexpected keystore file structure')
-    }
-  }
-
-  // async getPrivateKey(account: string, keystoreDir: string): Promise<string> {
-  //   // Should check keystore file and try to find respective account
-  //   // just to not delete this import for now
-  //   console.log(keystoreDir)
-  //   // console.log(scrypt)
-  //   console.log(account)
-  //   const key = Buffer.from(
-  //     '477651e4d34628765680270958b2e4f4724c505ce2443939ac5363b8a2b129ba',
-  //     'hex'
-  //   )
-  //   const testPassword = 'asdf08-io;k/mü idfu909!p890A*&8'
-  //   const wallet = Wallet.fromPrivateKey(key)
-  //   const v3 = await wallet.toV3String(testPassword)
-  //   // const v3Obj = await wallet.toV3(testPassword)
-  //   const timestamp = Date.now()
-  //   console.log(timestamp)
-  //   console.log(wallet.getV3Filename(timestamp))
-  //   // TODO check whether it's critical that these don't have the BLS signature stuff stored as well or if this is a security (or other) risk
-  //   console.log(v3)
-  //   const pkTest = (await Wallet.fromV3(v3, testPassword)).getPrivateKeyString()
-  //   console.log(pkTest)
-  //   return v3
-  // }
-}
-
-export class KeystoreWalletWrapper {
-  private _keystoreDir: string
-  // TODO make this more permissive?
-  private _localWallet: LocalWallet
-
-  makeDefaultKeystoreDir(): string {
     // TODO Figure out how this is done for celocli files, etc.?
-
-    const defaultDir = path.join(
-      '/Users/eelanagaraj/celo/celo-monorepo/packages/sdk/wallets/wallet-keystore/test-keystore-dir',
-      'keystore'
-    )
-    mkdirSync(defaultDir, { recursive: true })
-    return defaultDir
+    this._keystoreDir = keystoreDir
+      ? keystoreDir
+      : path.join(
+          '/Users/eelanagaraj/celo/celo-monorepo/packages/sdk/wallets/wallet-keystore/test-keystore-dir',
+          'keystore'
+        )
+    // TODO revisit if it makes sense to only make the directory if default...
+    mkdirSync(this._keystoreDir, { recursive: true })
   }
 
-  constructor(keystoreDir?: string) {
-    this._keystoreDir = keystoreDir ? keystoreDir : this.makeDefaultKeystoreDir()
-    this._localWallet = new LocalWallet()
+  getKeystoreDir(): string {
+    return this._keystoreDir
   }
 
-  async importPrivateKey(privateKey: string, password: string) {
-    await LocalKeystore.importPrivateKey(privateKey, password, this._keystoreDir)
-    // // TODO do we also want to add the pk signer right here?
-    // this._localWallet.addAccount(privateKey)
-  }
-
-  // TODO: distinction between keystore accounts and unlocked wallet accounts?
   async listKeystoreAccounts(): Promise<string[]> {
     // TODO fix error handling
     return (await fsPromises.readdir(this._keystoreDir)).map((file) =>
@@ -129,19 +41,70 @@ export class KeystoreWalletWrapper {
     )
   }
 
-  // TODO password default just for initial dev/testing
-  async addAccountFromKeystore(account: string, password: string = 'c#gc6s&UTBO6@nXzx1!U') {
-    // TODO get the available accounts in the keystore
-    // if keystore account does not match (what about duplicated??) --> how does geth manage this?
-    const testKeystoreFile = 'eth-keystore-test'
+  // TODO restructure/reorganize this
+  async importPrivateKey(privateKey: string, password: string) {
+    const key = Buffer.from(privateKey, 'hex')
+    const wallet = Wallet.fromPrivateKey(key)
+    const keystore = await wallet.toV3String(password)
+    const fileName = wallet.getV3Filename(Date.now())
+    // TODO handle if we add the same account twice -- don't overwrite and only allow one?
+    writeFileSync(path.join(this._keystoreDir, fileName), keystore)
+    // TODO take in an interface for writing to file storage?
+    // That could then be switched out for browser/file system/mock test writer/etc.
+  }
 
-    // FOR now just for compiler issues
-    console.log(account)
-    // TODO unlock the actual
-    const privateKey = await LocalKeystore.getPrivateKeyFromFile(
-      path.join(this._keystoreDir, testKeystoreFile),
-      password
-    )
+  static async getPrivateKeyFromFile(keystoreFile: string, password: string): Promise<string> {
+    const keystore = readFileSync(keystoreFile).toString()
+    const pkTest = (await Wallet.fromV3(keystore, password)).getPrivateKeyString()
+    return pkTest
+  }
+
+  // TODO modify this as needed to not require the user to pass in the entire
+  static getAddressFromFile(keystoreFile: string): string {
+    // TODO is this possible without actually decrypting the file?
+    const rawKeystore = readFileSync(keystoreFile).toString()
+    try {
+      const address = JSON.parse(rawKeystore).address
+      return ensureLeading0x(address)
+    } catch (e) {
+      console.log(e)
+      throw new Error('Unexpected keystore file structure')
+    }
+  }
+}
+
+// TODO maybe even separate this out into its own file + tests
+export class KeystoreWalletWrapper {
+  // TODO make this more permissive?
+  private _localKeystore: LocalKeystore
+  private _localWallet: LocalWallet
+
+  constructor(keystoreDir?: string) {
+    this._localKeystore = new LocalKeystore(keystoreDir)
+    this._localWallet = new LocalWallet()
+  }
+
+  async importPrivateKey(privateKey: string, password: string) {
+    await this._localKeystore.importPrivateKey(privateKey, password)
+    // // TODO do we also want to add the pk signer right here?
     this._localWallet.addAccount(privateKey)
   }
+
+  getLocalWallet(): LocalWallet {
+    return this._localWallet
+  }
+  // DO NOT DELETE YET
+  // TODO password default just for initial dev/testing
+  // async addAccountFromKeystore(account: string, password: string = 'c#gc6s&UTBO6@nXzx1!U') {
+  //   // TODO get the available accounts in the keystore
+  //   // if keystore account does not match (what about duplicated??) --> how does geth manage this?
+  //   const testKeystoreFile = 'eth-keystore-test'
+
+  //   // FOR now just for compiler issues
+  //   console.log(account)
+  //   // TODO unlock the actual
+  //   // TODO modify -- Wrapper should not interact with the actual File names !!!!
+  //   const privateKey = await this._localKeystore.getPrivateKeyFromFile(testKeystoreFile, password)
+  //   this._localWallet.addAccount(privateKey)
+  // }
 }
