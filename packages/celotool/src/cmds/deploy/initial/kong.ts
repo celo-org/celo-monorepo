c8fe1a3a9c5e9c179483da1fbdae8f332e8bc909
Javier Cortejoso <javier.cortejoso@gmail.com>
2021-06-11 23:20:14 +0200

    Kong and Konga Ingress

---
+++ packages/celotool/src/cmds/deploy/initial/kong.ts
@@ -0,0 +1,27 @@
+import { InitialArgv } from 'src/cmds/deploy/initial'
+import { switchToClusterFromEnvOrContext } from 'src/lib/cluster'
+import { addContextMiddleware, ContextArgv } from 'src/lib/context-utils'
+import { installKong, installKonga } from 'src/lib/kong'
+
+export const command = 'kong'
+
+export const describe = 'deploy Kong and Konga packages'
+
+export type KongInitialArgv = InitialArgv &
+  ContextArgv & {
+    skipClusterSetup: boolean
+  }
+
+export const builder = (argv: KongInitialArgv) => {
+  return addContextMiddleware(argv).option('skipClusterSetup', {
+    type: 'boolean',
+    description: 'If you know that you can skip the cluster setup',
+    default: false,
+  })
+}
+
+export const handler = async (argv: KongInitialArgv) => {
+  await switchToClusterFromEnvOrContext(argv, true)
+  await installKong(argv.celoEnv)
+  await installKonga()
+}
---
+++ packages/celotool/src/lib/kong.ts
@@ -0,0 +1,95 @@
+import { readFileSync, writeFileSync } from 'fs'
+import { execCmdWithExitOnFailure } from 'src/lib/cmd-utils'
+import { outputIncludes } from 'src/lib/utils'
+import { createNamespaceIfNotExists } from './cluster'
+import { installGenericHelmChart, retrieveIPAddress, upgradeGenericHelmChart } from './helm_deploy'
+
+const kongChartPath = '../helm-charts/kong'
+const kongaChartPath = '../helm-charts/konga'
+
+// One unique kong/a deployment per cluster
+const kongReleaseName = 'kongcelotool'
+const kongNamespace = 'kongcelotool'
+const kongaReleaseName = 'kongacelotool'
+const kongaNamespace = 'kongcelotool'
+
+export async function installKong(celoEnv: string) {
+  await createNamespaceIfNotExists(kongNamespace)
+  await createUpdateKongConfigMap(celoEnv)
+  // Update values in values-clabs.yaml file
+  return installGenericHelmChart(
+    kongNamespace,
+    kongReleaseName,
+    kongChartPath,
+    await kongHelmParamenters(celoEnv),
+    true,
+    `values-clabs.yaml`
+  )
+}
+
+export async function upgradeKong(celoEnv: string) {
+  await createUpdateKongConfigMap(celoEnv)
+  return upgradeGenericHelmChart(
+    kongNamespace,
+    kongReleaseName,
+    kongChartPath,
+    [],
+    `values-clabs.yaml`
+  )
+}
+
+export async function installKonga() {
+  await createNamespaceIfNotExists(kongaNamespace)
+  // Update values in values.yaml file
+  return installGenericHelmChart(kongaNamespace, kongaReleaseName, kongaChartPath, [])
+}
+
+export async function upgradeKonga() {
+  return upgradeGenericHelmChart(kongaNamespace, kongaReleaseName, kongaChartPath, [])
+}
+
+export async function destroyKongAndKonga() {
+  await execCmdWithExitOnFailure(`kubectl delete ns ${kongNamespace} ${kongaNamespace}`)
+}
+
+async function kongHelmParamenters(celoEnv: string) {
+  // GCP Internal infra ips
+  let trustedIPs = '130.211.0.0/22,35.191.0.0/16'
+  const fornoPublicGlobalIp = await retrieveIPAddress(`${celoEnv}-forno-global-address`, 'global')
+  trustedIPs = `${trustedIPs},${fornoPublicGlobalIp}/32`
+  return [
+    `--set kong.extraEnvVars[0].name=KONG_TRUSTED_IPS`,
+    `--set kong.extraEnvVars[0].value='${trustedIPs.replace(/,/g, '\\,')}'`,
+  ]
+}
+
+/**
+ * Creates a configMap with the kong configuration
+ * Configuration is read from a kong config file inside the kong chart folder
+ */
+export async function createUpdateKongConfigMap(celoEnv: string) {
+  let kongConfig = readFileSync(`${kongChartPath}/kong.conf`).toString()
+  // We need to patch this file with the forno public ip as this ip will forward
+  // the requests and need to put in the config file so kong/nginx can consider
+  // that ip as internal
+  let trustedIPs = '130.211.0.0/22,35.191.0.0/16'
+  const fornoPublicGlobalIp = await retrieveIPAddress(`${celoEnv}-forno-global-address`, 'global')
+  trustedIPs = `${trustedIPs},${fornoPublicGlobalIp}/32`
+  const re = '/^trusted_ips = .+$/g'
+  kongConfig.replace(re, `trusted_ips = ${trustedIPs}`)
+  const kongConfigTmpFile = '/tmp/kong.conf'
+  writeFileSync(kongConfigTmpFile, kongConfig)
+  const configMapExists = await outputIncludes(
+    `kubectl get cm -n ${kongNamespace} kong-config || true`,
+    'kong-config'
+  )
+  if (configMapExists) {
+    await execCmdWithExitOnFailure(
+      `kubectl create cm kong-config -n ${kongNamespace} --from-file ${kongConfigTmpFile} -o yaml --dry-run | kubectl replace -f -`
+    )
+  } else {
+    await execCmdWithExitOnFailure(
+      `kubectl create cm kong-config -n ${kongNamespace} --from-file ${kongConfigTmpFile}`
+    )
+  }
+}
