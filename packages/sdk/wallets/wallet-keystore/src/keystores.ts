0cd334cce9cebd499ea18f0b3bd09ba9eb9dd714
eelanagaraj <7308464+eelanagaraj@users.noreply.github.com>
2021-06-15 15:58:04 +0200

    Add preliminary docstrings

--- packages/sdk/wallets/wallet-keystore/src/keystores.ts
+++ packages/sdk/wallets/wallet-keystore/src/keystores.ts
@@ -13,20 +13,43 @@ export enum ErrorMessages {
   UNKNOWN_STRUCTURE = 'Unexpected keystore entry structure',
 }
 
+// TODO refine language around keystore entry / keystore / keystore container ...?
 export abstract class KeystoreBase {
   /**
    * Handles encrytion and transformation between private keys <-> V3Keystore strings
+   * See https://github.com/ethereum/wiki/wiki/Web3-Secret-Storage-Definition
+   * for more details on conventions around Web3 Secret Storage.
    */
 
-  // TODO docstrings
-
-  // Must be implemented by subclass
-  // TODO could make all of these either keystoreName OR address
+  /**
+   * Saves encrypted keystore entry (i.e. to disk, database, ...). Must be implemented by subclass.
+   * @param keystoreName Name of keystore entry to be saved
+   * @param keystore encrypted V3Keystore string entry
+   */
   abstract persistKeystore(keystoreName: string, keystore: string): void
+
+  /**
+   * Returns raw encrypted keystore entry string by name
+   * @param keystoreName Name of keystore entry to retrieve
+   */
   abstract getRawKeystore(keystoreName: string): string
+
+  /**
+   * Gets a list of the names of each entry in the keystore
+   */
   abstract getAllKeystoreNames(): Promise<string[]>
+
+  /**
+   * Removes keystore entry from keystore permanently
+   * @param keystoreName Name of keystore entry to remove
+   */
   abstract removeKeystore(keystoreName: string): void
 
+  /**
+   * Gets the address corresponding to a particular keystore entry
+   * @param keystoreName Name of keystore entry belonging to the address
+   * @returns Account address
+   */
   getAddress(keystoreName: string): string {
     const rawKeystore = this.getRawKeystore(keystoreName)
     try {
@@ -37,12 +60,18 @@ export abstract class KeystoreBase {
       throw new Error(ErrorMessages.UNKNOWN_STRUCTURE)
     }
   }
-
+  /**
+   * Gets a list of all account addresses in the keystore
+   * @returns List of account address strings
+   */
   async listKeystoreAddresses(): Promise<string[]> {
     return Object.keys(await this.getAddressMap())
   }
 
-  // Map addresses to their respective keystore entries (names)
+  /**
+   * Maps account addresses to their respective keystore entries (names)
+   * @returns Record with account addresses as keys, keystore entry names as values
+   */
   async getAddressMap(): Promise<Record<string, string>> {
     // Don't store this to minimize race conditions (file is deleted/added manually)
     const addressToFile: Record<string, string> = {}
@@ -52,6 +81,11 @@ export abstract class KeystoreBase {
     return addressToFile
   }
 
+  /**
+   * Encrypts and stores a private key as a new keystore entry
+   * @param privateKey Private key to encrypted
+   * @param passphrase Secret string to encrypt private key
+   */
   async importPrivateKey(privateKey: string, passphrase: string) {
     // Duplicate entries for the same private key are not permitted
     const address = normalizeAddressWith0x(privateKeyToAddress(privateKey))
@@ -67,6 +101,11 @@ export abstract class KeystoreBase {
     this.persistKeystore(keystoreName, keystore)
   }
 
+  /**
+   * Gets name of keystore entry corresponding to an address
+   * @param address Account address
+   * @returns Name of corresponding keystore entry
+   */
   async getKeystoreName(address: string): Promise<string> {
     const keystoreName = (await this.getAddressMap())[normalizeAddressWith0x(address)]
     if (keystoreName === undefined) {
@@ -75,12 +114,24 @@ export abstract class KeystoreBase {
     return keystoreName
   }
 
+  /**
+   * Gets decrypted (plaintext) private key for an account address
+   * @param address Account address
+   * @param passphrase Secret phrase used to encrypt the private key
+   * @returns
+   */
   async getPrivateKey(address: string, passphrase: string): Promise<string> {
     const rawKeystore = this.getRawKeystore(await this.getKeystoreName(address))
     // TODO do we want to trim leading 0x here? what is the best practice here?
     return (await Wallet.fromV3(rawKeystore, passphrase)).getPrivateKeyString()
   }
 
+  /**
+   * Change secret phrase used to encrypt the private key of an address
+   * @param address Account address
+   * @param oldPassphrase Secret phrase used to encrypt the private key
+   * @param newPassphrase New secret phrase to re-encrypt the private key
+   */
   async changeKeystorePassphrase(address: string, oldPassphrase: string, newPassphrase: string) {
     const keystoreName = await this.getKeystoreName(address)
     const rawKeystore = this.getRawKeystore(keystoreName)
@@ -90,17 +141,22 @@ export abstract class KeystoreBase {
     this.persistKeystore(keystoreName, newKeystore)
   }
 
+  /**
+   * Permanently removes keystore entry from keystore
+   * @param address Account address of keystore to be deleted
+   */
   async deleteKeystore(address: string) {
     this.removeKeystore(await this.getKeystoreName(address))
   }
 }
 
-/**
- * Used for mocking keystore operations in unit tests
- */
 export class InMemoryKeystore extends KeystoreBase {
+  /**
+   * Used for mocking keystore operations in unit tests
+   */
   private _storage: Record<string, string> = {}
 
+  // Implements required abstract class methods
   persistKeystore(keystoreName: string, keystore: string) {
     this._storage[keystoreName] = keystore
   }
@@ -119,8 +175,17 @@ export class InMemoryKeystore extends KeystoreBase {
 }
 
 export class FileKeystore extends KeystoreBase {
+  /**
+   * Implements keystore as a directory on disk
+   * with files for keystore entries
+   */
   private _keystoreDir: string
 
+  /**
+   * Creates (but does not overwrite existing) directory
+   * for containing keystore entries.
+   * @param keystoreDir Path to directory where keystore will be saved
+   */
   constructor(keystoreDir: string) {
     super()
     this._keystoreDir = path.join(keystoreDir, 'keystore')
@@ -131,20 +196,36 @@ export class FileKeystore extends KeystoreBase {
     }
   }
 
+  /**
+   * @returns List of file names (keystore entries) in the keystore
+   */
   async getAllKeystoreNames(): Promise<string[]> {
     return fsPromises.readdir(this._keystoreDir)
   }
 
+  /**
+   * Saves keystore entries as a file in the keystore directory
+   * @param keystoreName File name of keystore entry
+   * @param keystore V3Keystore string entry
+   */
   persistKeystore(keystoreName: string, keystore: string) {
     writeFileSync(path.join(this._keystoreDir, keystoreName), keystore)
   }
 
+  /**
+   * Gets contents of keystore entry file
+   * @param keystoreName File name of keystore entry
+   * @returns V3Keystore string entry
+   */
   getRawKeystore(keystoreName: string): string {
     return readFileSync(path.join(this._keystoreDir, keystoreName)).toString()
   }
 
+  /**
+   * Deletes file keystore entry from directory
+   * @param keystoreName File name of keystore entry to be removed
+   */
   removeKeystore(keystoreName: string) {
-    // TODO test
     return unlinkSync(path.join(this._keystoreDir, keystoreName))
   }
 }
