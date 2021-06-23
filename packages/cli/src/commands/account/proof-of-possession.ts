0419e6304a25e98607a7990221ad66542f9cfc9e
Medha Kothari <medha@clabs.co>
2021-06-23 12:21:00 -0400

    added role to proof of posession

--- packages/cli/src/commands/account/proof-of-possession.ts
+++ packages/cli/src/commands/account/proof-of-possession.ts
@@ -16,16 +16,26 @@ export default class ProofOfPossession extends BaseCommand {
       required: true,
       description: 'Address of the account that needs to prove possession of the signer key.',
     }),
+    role: flags.string({
+      char: 'r',
+      options: ['vote', 'validator', 'attestation'],
+      description: 'Role to delegate',
+      required: true,
+    }),
   }
 
   static examples = [
-    'proof-of-possession --account 0x5409ed021d9299bf6814279a6a1411a7e866a631 --signer 0x6ecbe1db9ef729cbe972c83fb886247691fb6beb',
+    'proof-of-possession --account 0x5409ed021d9299bf6814279a6a1411a7e866a631 --signer 0x6ecbe1db9ef729cbe972c83fb886247691fb6beb --role validator',
   ]
 
   async run() {
     const res = this.parse(ProofOfPossession)
     const accounts = await this.kit.contracts.getAccounts()
-    const pop = await accounts.generateProofOfKeyPossession(res.flags.account, res.flags.signer)
+    const pop = await accounts.generateProofOfKeyPossession(
+      res.flags.account,
+      res.flags.signer,
+      res.flags.role
+    )
     printValueMap({ signature: serializeSignature(pop) })
   }
 }
