2acdccd411e2ce3e982bad7a57fc663f73b721ef
Medha Kothari <medha@clabs.co>
2021-06-29 16:04:09 -0400

    stashing

--- packages/cli/src/commands/account/set-indexed-signer.ts
+++ packages/cli/src/commands/account/set-indexed-signer.ts
@@ -2,7 +2,7 @@ import { serializeSignature } from '@celo/utils/lib/signatureUtils'
 import { BaseCommand } from '../../base'
 import { printValueMap } from '../../utils/cli'
 import { Flags } from '../../utils/command'
-export default class ProofOfPossession extends BaseCommand {
+export default class SetIndexedSigner extends BaseCommand {
   static description =
     'Generate proof-of-possession to be used to authorize a signer. See the "account:authorize" command for more details.'
 
@@ -12,30 +12,26 @@ export default class ProofOfPossession extends BaseCommand {
       required: true,
       description: 'Address of the signer key to prove possession of.',
     }),
-    account: Flags.address({
-      required: true,
-      description: 'Address of the account that needs to prove possession of the signer key.',
-    }),
     role: flags.string({
       char: 'r',
       options: ['vote', 'validator', 'attestation'],
       description: 'Role to delegate',
-      required: false,
+      required: true,
+    }),
+    account: Flags.address({
+      required: true,
+      description: 'Address of the account that needs to prove possession of the signer key.',
     }),
   }
 
   static examples = [
-    'proof-of-possession --account 0x5409ed021d9299bf6814279a6a1411a7e866a631 --signer 0x6ecbe1db9ef729cbe972c83fb886247691fb6beb --role validator',
+    'proof-of-possession --account 0x5409ed021d9299bf6814279a6a1411a7e866a631 --signer 0x6ecbe1db9ef729cbe972c83fb886247691fb6beb',
   ]
 
   async run() {
     const res = this.parse(ProofOfPossession)
     const accounts = await this.kit.contracts.getAccounts()
     const pop = await accounts.generateProofOfKeyPossession(res.flags.account, res.flags.signer)
-    if (res.flags.role) {
-      printValueMap({ signature: serializeSignature(pop, res.flags.role) })
-    } else {
-      printValueMap({ signature: serializeSignature(pop) })
-    }
+    printValueMap({ signature: serializeSignature(pop) })
   }
 }
