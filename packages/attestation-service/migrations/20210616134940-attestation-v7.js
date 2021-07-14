e71112098454baa20ccd0e7e978145bbf6fca48f
Gregório Granado Magalhães <greg.magalhaes@gmail.com>
2021-07-14 10:40:47 -0300

    Fix CI errors

--- packages/attestation-service/migrations/20210616134940-attestation-v7.js
+++ packages/attestation-service/migrations/20210616134940-attestation-v7.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -31,5 +31,5 @@ module.exports = {
       await transaction.rollback()
       throw error
     }
-  }
-};
+  },
+}
