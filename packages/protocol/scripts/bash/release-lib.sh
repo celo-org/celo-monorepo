255044d0c2ce72014baaf593659bab1699b1842d
Yorke Rhodes <yorhodes@cLabs.co>
2021-07-20 16:54:54 -0400

    Implement contract upgrades compatibility in ContractKit (#8308)
    ### Description
    
    - Adds version logic to ContractKit base wrapper to support generic version gatekeeping
    
    ### Other changes
    
    - Modularize and simplify protocol build scripts to only generate artifacts when necessary
    - Modify contractkit build:gen script to support targeting contract releases 
    
    ### Tested
    
    - Tests added for base wrapper version compatibility
    
    ### Related issues
    
    - Fixes #8018
    
    ### Backwards compatibility
    
    Yes
    
    ### Documentation
    
    None

--- packages/protocol/scripts/bash/release-lib.sh
+++ packages/protocol/scripts/bash/release-lib.sh
@@ -18,16 +18,12 @@ function build_tag() {
   [ -d contracts ] && rm -r contracts
   git checkout $BRANCH -- contracts 2>>$LOG_FILE >> $LOG_FILE
 
-  [ -d "build/contracts" ] && mv build/contracts build/contracts_tmp
   if [ ! -d $BUILD_DIR ]; then
     echo " - Build contract artifacts at $BUILD_DIR"
-    yarn build:sol >> $LOG_FILE
-    rm -rf $BUILD_DIR && mkdir -p $BUILD_DIR
-    mv build/contracts $BUILD_DIR
+    BUILD_DIR=$BUILD_DIR yarn build:sol >> $LOG_FILE
   else
     echo " - Contract artifacts already built at $BUILD_DIR"
   fi
-  [ -d "build/contracts_tmp" ] && mv build/contracts_tmp build/contracts
 
   [ -d contracts ] && rm -r contracts
   git checkout - -- contracts 2>>$LOG_FILE >> $LOG_FILE
