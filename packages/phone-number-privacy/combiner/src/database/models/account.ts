aa91c47582b062579613abf9709acdf9f4b345e8
alecps <alec@cLabs.co>
2021-07-27 17:41:41 +0100

    add dekSigner to accounts db model

--- packages/phone-number-privacy/combiner/src/database/models/account.ts
+++ packages/phone-number-privacy/combiner/src/database/models/account.ts
@@ -2,6 +2,7 @@ export const ACCOUNTS_TABLE = 'accounts'
 export enum ACCOUNTS_COLUMNS {
   address = 'address',
   signedUserPhoneNumber = 'signedUserPhoneNumber',
+  dekSigner = 'dekSigner',
   createdAt = 'created_at',
   numLookups = 'num_lookups',
   didMatchmaking = 'did_matchmaking',
@@ -9,13 +10,15 @@ export enum ACCOUNTS_COLUMNS {
 export class Account {
   [ACCOUNTS_COLUMNS.address]: string;
   [ACCOUNTS_COLUMNS.signedUserPhoneNumber]: string;
+  [ACCOUNTS_COLUMNS.dekSigner]: string;
   [ACCOUNTS_COLUMNS.createdAt]: Date = new Date();
   [ACCOUNTS_COLUMNS.didMatchmaking]: Date | null = null
 
-  constructor(address: string, signedUserPhoneNumber?: string) {
+  constructor(address: string, signedUserPhoneNumber?: string, dekSigner?: string) {
     this.address = address
-    if (signedUserPhoneNumber) {
+    if (signedUserPhoneNumber && dekSigner) {
       this.signedUserPhoneNumber = signedUserPhoneNumber
+      this.dekSigner = dekSigner
     }
   }
 }
