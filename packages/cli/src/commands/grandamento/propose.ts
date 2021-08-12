618d2306768abdd815534258ab00d1d71f8e07b4
Martín Volpe <martin.volpe@gmail.com>
2021-08-12 20:04:13 +0200

    Added verifications to propose.ts

--- packages/cli/src/commands/grandamento/propose.ts
+++ packages/cli/src/commands/grandamento/propose.ts
@@ -1,6 +1,7 @@
 import { StableToken } from '@celo/contractkit'
 import { flags } from '@oclif/command'
 import { BaseCommand } from '../../base'
+import { newCheckBuilder } from '../../utils/checks'
 import { displaySendTx } from '../../utils/cli'
 import { Flags } from '../../utils/command'
 import { enumEntriesDupWithLowercase } from '../../utils/helpers'
@@ -34,11 +35,16 @@ export default class Propse extends BaseCommand {
     const grandaMento = await this.kit.contracts.getGrandaMento()
 
     const res = this.parse(Propse)
+    const signer = res.flags.from
     const sellAmount = res.flags.value
     const stableToken = stableTokenOptions[res.flags.stableToken]
     const sellCelo = res.flags.sellCelo
 
+    this.kit.defaultAccount = signer
+
     const tokenToSell = sellCelo ? celoToken : await this.kit.contracts.getStableToken(stableToken)
+
+    await newCheckBuilder(this, signer).hasEnoughErc20(signer, sellAmount, celoToken.address)
 
     await displaySendTx(
       'increaseAllowance',
@@ -53,7 +59,7 @@ export default class Propse extends BaseCommand {
         sellCelo
       ),
       { value: sellAmount.toString() },
-      'ProposalQueued'
+      'Proposed'
     )
   }
 }
