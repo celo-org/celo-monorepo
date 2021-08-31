8684042ac98742fea0f7212e7ee778dfc81467c5
yorhodes <yorhodes@cLabs.co>
2021-08-30 20:00:01 -0700

    Revert change to sdk contract cache

--- packages/sdk/contractkit/src/contract-cache.ts
+++ packages/sdk/contractkit/src/contract-cache.ts
@@ -57,7 +57,7 @@ const WrapperFactories = {
   [CeloContract.Validators]: ValidatorsWrapper,
 }
 
-export type CFType = typeof WrapperFactories
+type CFType = typeof WrapperFactories
 export type ValidWrappers = keyof CFType
 
 interface WrapperCacheMap {
