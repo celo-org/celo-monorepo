c8bba4aa6fd7fc9f859ab85a31ab37a55ea6507b
Marcin Chrzanowski <martin@clabs.co>
2021-08-31 14:20:28 +0200

    Add CI step for Reserve spec

--- packages/protocol/specs/scripts/reserve.sh
+++ packages/protocol/specs/scripts/reserve.sh
@@ -2,5 +2,6 @@ certoraRun contracts/common/Registry.sol specs/harnesses/ReserveHarness.sol \
   --link ReserveHarness:registry=Registry \
   --verify ReserveHarness:specs/reserve.spec \
   --optimistic_loop \
+  --settings -ciMode=true \
   --msg "Reserve" \
   --solc_args "['--evm-version', 'istanbul']"
