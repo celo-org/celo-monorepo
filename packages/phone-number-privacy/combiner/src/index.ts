5e9ad4d1b57469ce06ff3f1da1b2bdbb10a78d1d
codyborn <codyborn@outlook.com>
2021-09-07 14:47:22 -0700

    Set min instance count

--- packages/phone-number-privacy/combiner/src/index.ts
+++ packages/phone-number-privacy/combiner/src/index.ts
@@ -55,6 +55,11 @@ async function meterResponse(
 // EG. curl -v "http://localhost:5000/celo-phone-number-privacy/us-central1/getBlindedMessageSig" -H "Authorization: 0xfc2ee61c4d18b93374fdd525c9de09d01398f7d153d17340b9ae156f94a1eb3237207d9aacb42e7f2f4ee0cf2621ab6d5a0837211665a99e16e3367f5209a56b1b" -d '{"blindedQueryPhoneNumber":"+Dzuylsdcv1ZxbRcQwhQ29O0UJynTNYufjWc4jpw2Zr9FLu5gSU8bvvDJ3r/Nj+B","account":"0xdd18d08f1c2619ede729c26cc46da19af0a2aa7f", "hashedPhoneNumber":"0x8fb77f2aff2ef0343706535dc702fc99f61a5d1b8e46d7c144c80fd156826a77"}' -H 'Content-Type: application/json'
 export const getBlindedMessageSig = functions
   .region('us-central1', 'europe-west3')
+  .runWith({
+    // Keep instances warm for this latency-critical function
+    // @ts-ignore https://firebase.google.com/docs/functions/manage-functions#reduce_the_number_of_cold_starts
+    minInstances: 3,
+  })
   .https.onRequest(async (req, res) =>
     meterResponse(handleGetBlindedMessageSig, req, res, '/getBlindedMessageSig')
   )
@@ -62,6 +67,11 @@ export const getBlindedMessageSig = functions
 // EG. curl -v "http://localhost:5000/celo-phone-number-privacy/us-central1/getContactMatches" -H "Authorization: <SIGNED_BODY>" -d '{"userPhoneNumber": "+99999999999", "contactPhoneNumbers": ["+5555555555", "+3333333333"], "account": "0x117ea45d497ab022b85494ba3ab6f52969bf6812"}' -H 'Content-Type: application/json'
 export const getContactMatches = functions
   .region('us-central1', 'europe-west3')
+  .runWith({
+    // Keep instances warm for this latency-critical function
+    // @ts-ignore https://firebase.google.com/docs/functions/manage-functions#reduce_the_number_of_cold_starts
+    minInstances: 3,
+  })
   .https.onRequest(async (req, res) =>
     meterResponse(handleGetContactMatches, req, res, '/getContactMatches')
   )
