5f55e3bfc79f2ad003625908e579211430c9325a
Marcin Chrzanowski <martin@clabs.co>
2021-09-09 21:41:39 +0200

    Cleanup whitespace

--- packages/protocol/specs/scripts/accounts.sh
+++ packages/protocol/specs/scripts/accounts.sh
@@ -1,8 +1,8 @@
 certoraRun specs/harnesses/AccountsHarness.sol \
-	--verify AccountsHarness:specs/accounts.spec \
-	--optimistic_loop \
-	--loop_iter 2 \
-	--cache accounts \
-	--solc_args "['--evm-version', 'istanbul']" \
+  --verify AccountsHarness:specs/accounts.spec \
+  --optimistic_loop \
+  --loop_iter 2 \
+  --cache accounts \
+  --solc_args "['--evm-version', 'istanbul']" \
   --short_output \
-	--msg "Accounts" 
+  --msg "Accounts"
--- packages/protocol/specs/scripts/lockedgold.sh
+++ packages/protocol/specs/scripts/lockedgold.sh
@@ -1,7 +1,7 @@
 certoraRun specs/harnesses/GoldTokenHarness.sol specs/harnesses/LockedGoldHarness.sol \
-	--link LockedGoldHarness:goldToken=GoldTokenHarness \
-	--verify LockedGoldHarness:specs/lockedGold.spec \
-	--optimistic_loop \
+  --link LockedGoldHarness:goldToken=GoldTokenHarness \
+  --verify LockedGoldHarness:specs/lockedGold.spec \
+  --optimistic_loop \
   --short_output \
-	--msg "LockedGold" \
-	--solc_args "['--evm-version', 'istanbul']"
+  --msg "LockedGold" \
+  --solc_args "['--evm-version', 'istanbul']"
