84b326c67b6f028bc1bdc2697fda204a5c5ec0b8
Emil Hotkowski <emil.hotkowski@gmail.com>
2021-09-09 11:28:53 +0200

    Added tests

---
+++ packages/attestation-service/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  globalSetup: './test/testSetup.ts',
+  preset: 'ts-jest',
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node', './nexmo.ts'],
+}
---
+++ packages/attestation-service/test/testSetup.ts
@@ -0,0 +1,5 @@
+module.exports = () => {
+  process.env.ODIS_PUBLIC_KEY = 'key'
+  process.env.ATTESTATION_SIGNER_ADDRESS = 'signerAddress'
+  process.env.CELO_VALIDATOR_ADDRESS = 'address'
+}
