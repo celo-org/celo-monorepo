5ac686d2d1a9032654475f9d543d61ae814248c9
Emil Hotkowski <emil.hotkowski@gmail.com>
2021-09-13 15:15:26 +0200

    Added working package.json for new tests

--- packages/attestation-service/test/testSetup.ts
+++ packages/attestation-service/test/testSetup.ts
@@ -2,4 +2,5 @@ module.exports = () => {
   process.env.ODIS_PUBLIC_KEY = 'key'
   process.env.ATTESTATION_SIGNER_ADDRESS = 'signerAddress'
   process.env.CELO_VALIDATOR_ADDRESS = 'address'
+  process.env.CELO_PROVIDERS = 'provider'
 }
