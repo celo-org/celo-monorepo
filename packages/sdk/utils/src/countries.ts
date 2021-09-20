89d91249103beffddab670083f8676f665df88de
Alex Harley <alexbruceharley@gmail.com>
2021-09-20 16:38:58 +0200

    1.2.5

--- packages/sdk/utils/src/countries.ts
+++ packages/sdk/utils/src/countries.ts
@@ -79,6 +79,7 @@ export class Countries {
         // are fallback languages 'es-US' and 'es-LA' that are not covered
         const names: CountryNames = {
           'en-us': country.name,
+          // @ts-ignore
           'es-419': esData[country.alpha2],
         }
 
