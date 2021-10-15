9cade72e3c2bf909e7937ffde7fc037dbf60afae
Yorke Rhodes <yorhodes@cLabs.co>
2021-10-15 16:46:49 -0700

    Add network alias for global node flag (#8549)
    ### Description
    
    - Add aliases to `--node` for ease of typing

--- packages/cli/src/base.ts
+++ packages/cli/src/base.ts
@@ -31,6 +31,22 @@ export abstract class BaseCommand extends Command {
       char: 'n',
       description: "URL of the node to run commands against (defaults to 'http://localhost:8545')",
       hidden: true,
+      parse: (nodeUrl) => {
+        switch (nodeUrl) {
+          case 'local':
+          case 'localhost':
+            return 'http://localhost:8545'
+          case 'baklava':
+            return 'https://baklava-forno.celo-testnet.org'
+          case 'alfajores':
+            return 'https://alfajores-forno.celo-testnet.org'
+          case 'mainnet':
+          case 'forno':
+            return 'https://forno.celo.org'
+          default:
+            return nodeUrl
+        }
+      },
     }),
     gasCurrency: flags.enum({
       options: Object.keys(gasOptions),
