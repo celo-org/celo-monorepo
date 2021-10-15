2618f9a335bdf9db62d8163d62bf05230c15f971
codyborn <codyborn@outlook.com>
2021-10-15 16:19:57 -0700

    Revert changes

--- packages/cli/src/commands/identity/get-attestations.test.ts
+++ packages/cli/src/commands/identity/get-attestations.test.ts
@@ -2,6 +2,10 @@ import GetAttestations from './get-attestations'
 
 describe('identity:get-attetstations', () => {
   describe('input validation correctly outputs errors', () => {
+    const consoleOutput: string[] = []
+    const mockedError = (output: string) => consoleOutput.push(output)
+    beforeEach(() => (console.error = mockedError))
+
     it('Fails when neither from, pepper, nor identifier are specified', async () => {
       await expect(GetAttestations.run(['--phoneNumber', '+15555555555'])).rejects.toThrow(
         'Must specify either --from or --pepper or --identifier'
@@ -15,7 +19,11 @@ describe('identity:get-attetstations', () => {
     })
 
     it('Successfully prints identifier when given pepper and number', async () => {
-      expect(true)
+      console.log = jest.fn()
+      await GetAttestations.run(['--phoneNumber', '+15555555555', '--pepper', 'XQke2bjvN7mPt'])
+      expect(console.log).toHaveBeenCalledWith(
+        'Identifier: 0xd9460ae529b2889716c8f1ccebb5efec945adc46fe1e9cd16f6242463e81f37c'
+      )
     })
   })
 })
