b392fb33a2b57aade315af475412122cc3679670
nambrot <nambrot@googlemail.com>
2021-10-19 11:46:38 -0400

    Add Result

--- packages/sdk/transactions-signature/src/parser.ts
+++ packages/sdk/transactions-signature/src/parser.ts
@@ -1,6 +1,21 @@
+import { Err, makeAsyncThrowable, Ok, Result, RootError } from '@celo/base'
 import { Interface, JsonFragment, ParamType, TransactionDescription } from '@ethersproject/abi'
 import fetch from 'cross-fetch'
 import { BigNumberish } from 'ethers'
+
+export enum ParserErrorTypes {
+  FetchAbiError = 'FetchAbiError',
+}
+
+export class FetchAbiError extends RootError<ParserErrorTypes.FetchAbiError> {
+  constructor(error: Error) {
+    super(ParserErrorTypes.FetchAbiError)
+    this.message = error.message
+  }
+}
+
+export type ParserErrors = FetchAbiError
+
 type Address = string
 type BytesAsString = string
 
@@ -14,12 +29,16 @@ interface Transaction {
 export class Parser {
   constructor(public readonly chainId: number) {}
 
-  async parse(tx: Transaction) {
+  async parseAsResult(tx: Transaction): Promise<Result<TransactionDescription, ParserErrors>> {
     const abi = await this.fetchAbiForAddress(tx.to)
-    const abiInterface = new Interface(abi)
+    if (!abi.ok) {
+      return abi
+    }
+    const abiInterface = new Interface(abi.result)
     const data = abiInterface.parseTransaction(tx)
-    return data
+    return Ok(data)
   }
+  parse = makeAsyncThrowable(this.parseAsResult.bind(this))
 
   private formatParam(paramValue: any, paramType: ParamType) {
     if (paramType.arrayChildren) {
@@ -47,16 +66,16 @@ export class Parser {
     return `${functionName}(${inputs.join(', ')})`
   }
 
-  async fetchAbiForAddress(address: Address): Promise<JsonFragment[]> {
+  async fetchAbiForAddress(address: Address): Promise<Result<JsonFragment[], FetchAbiError>> {
     const request = await fetch(
       `https://repo.sourcify.dev/contracts/full_match/${this.chainId}/${address}/metadata.json`
     )
     if (!request.ok) {
-      return []
+      return Err(new FetchAbiError(new Error('Could not fetch ABI')))
     }
 
     const data = await request.json()
     const abi = data.output.abi
-    return abi
+    return Ok(abi)
   }
 }
