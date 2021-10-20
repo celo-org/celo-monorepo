70a6f8a49c687704c7f6d76d3e46b96f9b008462
nambrot <nambrot@googlemail.com>
2021-10-20 10:59:54 -0400

    Remove

--- packages/sdk/transactions-signature/src/types.ts
+++ packages/sdk/transactions-signature/src/types.ts
@@ -1,12 +1,5 @@
 export type Address = string
 export type BytesAsString = string
-
-export enum ProxyTypes {
-  eip1967 = 'eip1967',
-  unstructured = 'unstructured',
-  beacon = 'beacon',
-}
-
 export interface KnownProxy {
   bytecode: string
   location: Address
