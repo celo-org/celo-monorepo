a9320fda274bbac137670224b466e7f6afb727ca
yorhodes <yorhodes@cLabs.co>
2021-10-29 00:34:30 -0700

    Activate stable in integration tests

--- packages/protocol/test/common/integration.ts
+++ packages/protocol/test/common/integration.ts
@@ -637,9 +637,6 @@ contract('Integration: Adding StableToken', (accounts: string[]) => {
     it(`should be impossible to sell CELO`, async () => {
       await goldToken.approve(exchangeAbc.address, sellAmount)
       await assertRevert(exchangeAbc.sell(sellAmount, minBuyAmount, true))
-      // This last case is not relevant, but the test is meant to warn in case the behavior ever changes
-      await goldToken.approve(exchangeAbc.address, sellAmount)
-      await exchangeAbc.sell(sellAmount, 0, true)
     })
 
     it(`should be impossible to sell stable token`, async () => {
@@ -690,6 +687,9 @@ contract('Integration: Adding StableToken', (accounts: string[]) => {
       await reserve.addExchangeSpender(exchangeAbc.address)
       await freezer.unfreeze(stableTokenAbc.address)
       await freezer.unfreeze(exchangeAbc.address)
+
+      // activate stable during mento-activation proposal
+      await exchangeAbc.activateStable()
       // Fee currency can't be tested here, but keep this line for reference
       await feeCurrencyWhitelist.addToken(stableTokenAbc.address)
     })
