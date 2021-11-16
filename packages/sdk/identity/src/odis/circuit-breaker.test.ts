57d3ac5deb5efc116807673ca18396f4f9cc4c08
Victor Graf <victor@clabs.co>
2021-11-16 12:06:05 -0800

    round out tests for the circuit breaker client

--- packages/sdk/identity/src/odis/circuit-breaker.test.ts
+++ packages/sdk/identity/src/odis/circuit-breaker.test.ts
@@ -160,7 +160,7 @@ describe('CircuitBreakerClient', () => {
     })
 
     it('should return an error if the fetch returns an HTTP error status', async () => {
-      fetchMock.mock(mockStatusEndpoint, { status: 500 })
+      fetchMock.mock(mockStatusEndpoint, { status: 501 })
       const result = await client.status()
       expect(result.ok).toBe(false)
       if (result.ok) {
@@ -182,6 +182,101 @@ describe('CircuitBreakerClient', () => {
     it('should return an error if fetch results in an invalid status', async () => {
       fetchMock.mock(mockStatusEndpoint, { status: 200, body: { status: 'invalid status' } })
       const result = await client.status()
+      expect(result.ok).toBe(false)
+      if (result.ok) {
+        return
+      }
+      expect(result.error.errorType).toEqual(CircuitBreakerErrorTypes.SERVICE_ERROR)
+    })
+  })
+
+  describe('.wrapKey()', () => {
+    it('should return an encryption of the given plaintext', async () => {
+      const testData = 'test circuit breaker plaintext'
+      const ciphertext = client.wrapKey(Buffer.from(testData))
+      expect(ciphertext.ok).toBe(true)
+      if (!ciphertext.ok) {
+        return
+      }
+
+      const plaintext = crypto.privateDecrypt(
+        //@ts-ignore support for OAEP hash option, was added in Node 12.9.0.
+        { key: MOCK_CIRCUIT_BREAKER_PRIVATE_KEY, oaepHash: 'sha256' },
+        ciphertext.result
+      )
+      expect(plaintext.toString('utf8')).toEqual(testData)
+    })
+  })
+
+  describe('.unwrapKey()', () => {
+    const testData = 'test circuit breaker plaintext'
+    const wrapResult = client.wrapKey(Buffer.from(testData))
+    if (!wrapResult.ok) {
+      throw new Error('failed to produce test ciphertext for unwrapKey')
+    }
+    const testCiphertext = wrapResult.result
+
+    it('should decrypt the given ciphertext when the service is enabled', async () => {
+      const result = await client.unwrapKey(testCiphertext)
+      expect(result.ok).toBe(true)
+      if (!result.ok) {
+        return
+      }
+      expect(result.result.toString('utf8')).toEqual(testData)
+    })
+
+    it('should return an error status response when the service is disabled', async () => {
+      for (const status of Object.values(CircuitBreakerKeyStatus)) {
+        if (status === CircuitBreakerKeyStatus.ENABLED) {
+          continue
+        }
+
+        mockService!.keyStatus = status
+        const result = await client.unwrapKey(testCiphertext)
+        expect(result.ok).toBe(false)
+        if (result.ok) {
+          continue
+        }
+        expect(result.error.errorType).toEqual(CircuitBreakerErrorTypes.SERVICE_ERROR)
+      }
+    })
+
+    it('should return an error if fetch throws', async () => {
+      fetchMock.mock(mockUnwrapKeyEndpoint, { throws: new Error('fetch error') })
+      const result = await client.unwrapKey(testCiphertext)
+      expect(result.ok).toBe(false)
+      if (result.ok) {
+        return
+      }
+      expect(result.error.errorType).toEqual(CircuitBreakerErrorTypes.FETCH_ERROR)
+    })
+
+    it('should return an error if the fetch returns an HTTP error status', async () => {
+      fetchMock.mock(mockUnwrapKeyEndpoint, { status: 501 })
+      const result = await client.unwrapKey(testCiphertext)
+      expect(result.ok).toBe(false)
+      if (result.ok) {
+        return
+      }
+      expect(result.error.errorType).toEqual(CircuitBreakerErrorTypes.SERVICE_ERROR)
+    })
+
+    it('should return an error if fetch results in invalid json', async () => {
+      fetchMock.mock(mockUnwrapKeyEndpoint, { status: 200, body: '<invalid json>' })
+      const result = await client.unwrapKey(testCiphertext)
+      expect(result.ok).toBe(false)
+      if (result.ok) {
+        return
+      }
+      expect(result.error.errorType).toEqual(CircuitBreakerErrorTypes.SERVICE_ERROR)
+    })
+
+    it('should return an error if fetch results in an invalid plaintext', async () => {
+      fetchMock.mock(mockUnwrapKeyEndpoint, {
+        status: 200,
+        body: { plaintext: '<invalid base64>' },
+      })
+      const result = await client.unwrapKey(testCiphertext)
       expect(result.ok).toBe(false)
       if (result.ok) {
         return
