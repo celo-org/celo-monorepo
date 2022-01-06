e6b4c62781a5f5ffa409caa6f809fdc277ee7fbc
Victor Graf <victor@clabs.co>
2022-01-05 16:22:33 -0800

    remove new code for encrypted backup

--- packages/sdk/encrypted-backup/jestSetup.ts
+++
@@ -1,7 +0,0 @@
-// Setup mock for the fetch API to intercept requests to ODIS and the circuit breaker service.
-// cross-fetch is used by the @celo/identity library.
-const fetchMockSandbox = require('fetch-mock').sandbox()
-jest.mock('cross-fetch', () => fetchMockSandbox)
-
-// @ts-ignore
-global.fetchMock = fetchMockSandbox
