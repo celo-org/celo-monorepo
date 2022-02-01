f94a025719b28ee3925d9b8937a6b647a89b9a5d
Nina Barbakadze <barbakadzeninaa@gmail.com>
2022-02-01 15:02:45 +0100

    Update packages/protocol/contracts/common/Accounts.sol
    Co-authored-by: Martin <martin@clabs.co>

--- packages/protocol/contracts/common/Accounts.sol
+++ packages/protocol/contracts/common/Accounts.sol
@@ -334,7 +334,7 @@ contract Accounts is
 
   /**
    * @notice Gets validator payment delegation settings.
-   * @param address of the validator.
+   * @param account Account of the validator.
    * @return Beneficiary address.
    * @return 24 decimal fraction of payment delegated.
    */
