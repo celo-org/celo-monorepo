0ce006e1c94bdb5a4d07301afdb6d2eff252de77
Nina Barbakadze <ninabarbakadze@macbook-pro-4.localdomain>
2022-02-24 18:53:00 +0100

    added function that add fiat tickers into the collection

--- packages/protocol/contracts/stability/StableTokenRegistry.sol
+++ packages/protocol/contracts/stability/StableTokenRegistry.sol
@@ -1,18 +1,31 @@
 pragma solidity ^0.5.13;
 
-import "./StableTokenEUR.sol";
 import "./StableToken.sol";
+import "./StableTokenEUR.sol";
 import "./StableTokenBRL.sol";
 
 /**
  * @title contract that lists what stable coins are deployed as part of Celo's Stability protocol.
  */
 contract StableTokenRegistry {
-  mapping(string => string) public stableTokenRegistry;
+  mapping(string => string) public stableTokens;
+  string[] public fiatTickers;
 
   /**
-   * @notice Sets initialized == true on implementation contracts
-   * @param test Set to true to skip implementation initialization
-   */
+  * @notice Sets initialized == true on implementation contracts
+  * @param test Set to true to skip implementation initialization
+  */
   constructor(bool test) public Initializable(test) {}
+
+  /**
+  * @notice adds fiat currencies to fiatTickers collection
+  * @param currencyType the type of currency we're trying to push into the collection  
+  */
+  function addFiatTickers(string currencyType) external onlyOwner {
+    //also check if it already exists in the array, if it does then don't add
+    //so I can make sure there are no dublicates
+    require(!stableTokens[currencyType], "Stable token hasn't been issued");
+    stableTokens[currencyType] = true;
+    fiatTickers.push(currencyType);
+  }
 }
