e5703ea7d29770c498e2f3b186cd2f58c855ae10
Nina Barbakadze <ninabarbakadze@macbook-pro-4.localdomain>
2022-03-02 15:36:44 +0100

    updating registry contract

--- packages/protocol/test/stability/stabletokenregistry.ts
+++ packages/protocol/test/stability/stabletokenregistry.ts
@@ -1,30 +1,30 @@
 import { assertRevert } from '@celo/protocol/lib/test-utils'
-import {
-  // RegistryContract,
-  // RegistryInstance,
-  StableTokenRegistryContract,
-  StableTokenRegistryInstance,
-} from 'types'
+import { StableTokenRegistryContract, StableTokenRegistryInstance } from 'types'
 
 const STRC: StableTokenRegistryContract = artifacts.require('StableTokenRegistry')
-// const Registry: RegistryContract = artifacts.require('Registry')
 
 contract('StableTokenRegistry', (accounts: string[]) => {
+  console.log('hi')
   let strc: StableTokenRegistryInstance
 
   // @ts-ignore
   let initializationTime
   beforeEach(async () => {
+    console.log('1')
     strc = await STRC.new(true)
+    console.log('12')
     const response = await strc.initialize(
       ['cUSD', 'cEUR', 'cBRL'],
       ['StableToken', 'StableTokenEUR', 'StableTokenBRL']
     )
+    console.log('123')
     // @ts-ignore
     initializationTime = (await web3.eth.getBlock(response.receipt.blockNumber)).timestamp
+    console.log('1234')
   })
 
   describe('#initialize()', async () => {
+    console.log('12345')
     it('should have set the owner', async () => {
       const owner: string = await strc.owner()
       assert.equal(owner, accounts[0])
