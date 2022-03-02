efb6a313178768b692a7ffd0a128858ba78f44df
Nina Barbakadze <ninabarbakadze@macbook-pro-4.localdomain>
2022-03-02 14:40:02 +0100

    initialize test suit

--- packages/protocol/test/stability/stabletokenregistry.ts
+++ packages/protocol/test/stability/stabletokenregistry.ts
@@ -1,3 +1,42 @@
-contract('StableTokenRegistry', () => {
-  console.log('hi')
+import { assertRevert } from '@celo/protocol/lib/test-utils'
+import {
+  // RegistryContract,
+  // RegistryInstance,
+  StableTokenRegistryContract,
+  StableTokenRegistryInstance,
+} from 'types'
+
+const STRC: StableTokenRegistryContract = artifacts.require('StableTokenRegistry')
+// const Registry: RegistryContract = artifacts.require('Registry')
+
+contract('StableTokenRegistry', (accounts: string[]) => {
+  let strc: StableTokenRegistryInstance
+
+  // @ts-ignore
+  let initializationTime
+  beforeEach(async () => {
+    strc = await STRC.new(true)
+    const response = await strc.initialize(
+      ['cUSD', 'cEUR', 'cBRL'],
+      ['StableToken', 'StableTokenEUR', 'StableTokenBRL']
+    )
+    // @ts-ignore
+    initializationTime = (await web3.eth.getBlock(response.receipt.blockNumber)).timestamp
+  })
+
+  describe('#initialize()', async () => {
+    it('should have set the owner', async () => {
+      const owner: string = await strc.owner()
+      assert.equal(owner, accounts[0])
+    })
+
+    it('should not be callable again', async () => {
+      await assertRevert(
+        strc.initialize(
+          ['cUSD', 'cEUR', 'cBRL'],
+          ['StableToken', 'StableTokenEUR', 'StableTokenBRL']
+        )
+      )
+    })
+  })
 })
