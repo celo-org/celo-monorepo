ca37e78e8ef9222875c051286ba3d7783e410059
Marcin Chrzanowski <martin@clabs.co>
2022-03-03 16:44:38 +0100

    Bump version number

--- packages/protocol/contracts/stability/Reserve.sol
+++ packages/protocol/contracts/stability/Reserve.sol
@@ -89,7 +89,7 @@ contract Reserve is
    * @return The storage, major, minor, and patch version of the contract.
    */
   function getVersionNumber() external pure returns (uint256, uint256, uint256, uint256) {
-    return (1, 1, 2, 1);
+    return (1, 1, 2, 2);
   }
 
   function() external payable {} // solhint-disable no-empty-blocks
