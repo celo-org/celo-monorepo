893c63ae5af2209b899fab2cdbade205a7891761
Aaron DeRuvo <aaron@clabs.co>
2022-03-09 18:06:12 -0800

    fix a test for Web3 cache which i broke by testing the ContractCache

--- packages/sdk/contractkit/src/web3-contract-cache.test.ts
+++ packages/sdk/contractkit/src/web3-contract-cache.test.ts
@@ -1,17 +1,10 @@
 import { Connection } from '@celo/connect'
 import Web3 from 'web3'
-import { AddressRegistry } from './address-registry'
-import { WrapperCache } from './contract-cache'
 import { AllContracts } from './index'
 import { Web3ContractCache } from './web3-contract-cache'
 function newWeb3ContractCache() {
   const connection = new Connection(new Web3('http://localhost:8545'))
-  const web3ContractCache = new Web3ContractCache(connection)
-  const registry = new AddressRegistry(connection)
-  const AnyContractAddress = '0xe832065fb5117dbddcb566ff7dc4340999583e38'
-  jest.spyOn(registry, 'addressFor').mockResolvedValue(AnyContractAddress)
-  const contractCache = new WrapperCache(connection, web3ContractCache, registry)
-  return contractCache
+  return new Web3ContractCache(connection)
 }
 
 describe('getContract()', () => {
@@ -19,7 +12,6 @@ describe('getContract()', () => {
 
   for (const contractName of AllContracts) {
     test(`SBAT get ${contractName}`, async () => {
-      // Why is this testing contractCache and not Web3ContractCache?
       const contract = await contractCache.getContract(contractName)
       expect(contract).not.toBeNull()
       expect(contract).toBeDefined()
