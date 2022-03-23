415e07a95cb22d551782352e0b15a4c011c03880
Nina Barbakadze <ninabarbakadze@macbook-pro-4.localdomain>
2022-03-23 20:11:45 +0100

    unsaved changes

---
+++ packages/protocol/migrations/26_stableToken_registry.ts
@@ -0,0 +1,20 @@
+/* tslint:disable:no-console */
+import { CeloContractName } from '@celo/protocol/lib/registry-utils'
+import { deploymentForCoreContract } from '@celo/protocol/lib/web3-utils'
+import { config } from '@celo/protocol/migrationsConfig'
+import { StableTokenRegistryInstance } from 'types'
+
+const initializeArgs = async (): Promise<any[]> => {
+  return [
+    config.StableTokenRegistry.existingFiatTickers,
+    config.StableTokenRegistry.existingStableTokenContractNames,
+  ]
+}
+
+module.exports = deploymentForCoreContract<StableTokenRegistryInstance>(
+  web3,
+  artifacts,
+  CeloContractName.StableTokenRegistry,
+  initializeArgs,
+  async (StableTokenRegistry: StableTokenRegistryInstance) => {}
+)
