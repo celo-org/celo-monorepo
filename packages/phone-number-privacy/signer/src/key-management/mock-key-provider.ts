619887b9470d2c6e2249d27131275e9758847ddd
Victor Graf <victor@clabs.co>
2022-03-29 15:36:19 -0700

    fix signer build

--- packages/phone-number-privacy/signer/src/key-management/mock-key-provider.ts
+++ packages/phone-number-privacy/signer/src/key-management/mock-key-provider.ts
@@ -1,7 +1,8 @@
 import { Key, KeyProviderBase } from './key-provider-base'
+import { PNP_DEV_SIGNER_PRIVATE_KEY } from '@celo/phone-number-privacy-common/lib/test/values'
 
 export class MockKeyProvider extends KeyProviderBase {
   public async fetchPrivateKeyFromStore(key: Key) {
-    this.setPrivateKey(key, DEV_PRIVATE_KEY) // @victor I can't remember why we deleted DEV_PRIVATE_KEY, do you?
+    this.setPrivateKey(key, PNP_DEV_SIGNER_PRIVATE_KEY)
   }
 }
