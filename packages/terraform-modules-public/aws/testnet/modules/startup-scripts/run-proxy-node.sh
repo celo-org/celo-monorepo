ce5d6a67f00adca8d28e3c6912ed84a7779ff753
Chris Geihsler <chris@geihsler.net>
2022-04-07 12:38:59 -0400

    fix: adjust proxy startup params to match latest baklava docs

--- packages/terraform-modules-public/aws/testnet/modules/startup-scripts/run-proxy-node.sh
+++ packages/terraform-modules-public/aws/testnet/modules/startup-scripts/run-proxy-node.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 CELO_IMAGE=${celo_image}
-NETWORK_ID=${celo_network_id}
 CELO_VALIDATOR_SIGNER_ADDRESS=${validator_signer_address}
 
 NODE_DIRECTORY=/home/ubuntu/celo-proxy-node
@@ -8,11 +7,9 @@ NODE_DIRECTORY=/home/ubuntu/celo-proxy-node
 mkdir $NODE_DIRECTORY
 cd $NODE_DIRECTORY
 
-BOOTNODE_ENODES="$(docker run --rm --entrypoint cat $CELO_IMAGE /celo/bootnodes)"
 PROXY_ADDRESS=${proxy_address}
 echo -n '${proxy_private_key_password}' > .password
 echo -n '${proxy_private_key_file_contents}' > keystore/${proxy_private_key_filename}
-echo -n '${proxy_node_private_key}' > .nodeprivatekey
 
 CLOUDWATCH_LOG_GROUP_NAME=${cloudwatch_log_group_name}
 CLOUDWATCH_LOG_STREAM_NAME=${cloudwatch_log_stream_name}
@@ -23,4 +20,11 @@ else
   DOCKER_LOGGING_PARAMS="--log-driver=awslogs --log-opt awslogs-group=$CLOUDWATCH_LOG_GROUP_NAME --log-opt awslogs-stream=$CLOUDWATCH_LOG_STREAM_NAME"
 fi
 
-docker run -d --name celo-proxy $DOCKER_LOGGING_PARAMS --restart unless-stopped -p 30303:30303 -p 30303:30303/udp -p 30503:30503 -p 30503:30503/udp -v $PWD:/root/.celo $CELO_IMAGE --verbosity 3 --networkid $NETWORK_ID --nousb --syncmode full --proxy.proxy --proxy.proxiedvalidatoraddress $CELO_VALIDATOR_SIGNER_ADDRESS --proxy.internalendpoint :30503 --etherbase $PROXY_ADDRESS --unlock $PROXY_ADDRESS --password /root/.celo/.password --allow-insecure-unlock --bootnodes $BOOTNODE_ENODES --ethstats=${validator_name}@${ethstats_host} --nodekey /root/.celo/.nodeprivatekey
+# Adapted from https://docs.celo.org/getting-started/baklava-testnet/running-a-validator-in-baklava#deploy-a-proxy
+docker run -d --name celo-proxy $DOCKER_LOGGING_PARAMS --restart unless-stopped \
+  -p 30303:30303 -p 30303:30303/udp -p 30503:30503 -p 30503:30503/udp \
+  -v $PWD:/root/.celo $CELO_IMAGE --verbosity 3 --syncmode full \ 
+  --proxy.proxy --proxy.proxiedvalidatoraddress $CELO_VALIDATOR_SIGNER_ADDRESS --proxy.internalendpoint :30503 \ 
+  --etherbase $PROXY_ADDRESS --unlock $PROXY_ADDRESS --password /root/.celo/.password \
+  --allow-insecure-unlock --baklava --datadir /root/.celo \
+  --celostats=${validator_name}@${ethstats_host}
