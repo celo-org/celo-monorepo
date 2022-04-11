7cbe2bd34e912196cf23f4eff28c3e5f489070c1
Eela Nagaraj <7308464+eelanagaraj@users.noreply.github.com>
2022-04-11 17:14:18 +0200

    ASv2 Scaffolding (#9430)
    * Add barebones FederatedAttestations contract, proxy, and interface
    
    * Make TODOs ASv2 specific
    
    * Add FA usages to existing files
    
    * Add skeleton migration file
    
    * Add test scaffolding
    
    * Add CK wrapper and test

--- packages/protocol/lib/test-utils.ts
+++ packages/protocol/lib/test-utils.ts
@@ -375,6 +375,7 @@ export const isSameAddress = (minerAddress, otherAddress) => {
 // TODO(amy): Pull this list from the build artifacts instead
 export const proxiedContracts: string[] = [
   'Attestations',
+  // TODO ASv2 revisit if we need to update test-utils
   'Escrow',
   'GoldToken',
   'Registry',
@@ -386,6 +387,7 @@ export const proxiedContracts: string[] = [
 // TODO(asa): Pull this list from the build artifacts instead
 export const ownedContracts: string[] = [
   'Attestations',
+  // TODO ASv2 revisit if we need to update test-utils
   'Escrow',
   'Exchange',
   'Registry',
