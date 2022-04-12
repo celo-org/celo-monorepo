ba30a1fc04df53596be0411e4d817a059753f85e
Nina Barbakadze <ninabarbakadze@MacBook-Pro-4.local>
2022-04-12 18:18:07 +0200

    incrementing the patch number

--- packages/protocol/contracts/stability/Exchange.sol
+++ packages/protocol/contracts/stability/Exchange.sol
@@ -66,7 +66,7 @@ contract Exchange is
    * @return The storage, major, minor, and patch version of the contract.
    */
   function getVersionNumber() external pure returns (uint256, uint256, uint256, uint256) {
-    return (1, 2, 0, 0);
+    return (1, 2, 0, 1);
   }
 
   /**
