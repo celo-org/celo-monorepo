935ffe16d7e54eaf8625f5971468e18d5cebdb1d
Eela Nagaraj <7308464+eelanagaraj@users.noreply.github.com>
2022-05-05 12:30:31 +0200

    Nit: revocation boolean return
    Co-authored-by: Cody Born <codyborn@outlook.com>

--- packages/protocol/contracts/identity/FederatedAttestations.sol
+++ packages/protocol/contracts/identity/FederatedAttestations.sol
@@ -74,7 +74,7 @@ contract FederatedAttestations is
   function _isRevoked(address signer, uint256 time) internal view returns (bool) {
     require(time >= 0);
     uint256 revokedOn = revokedSigners[signer];
-    return revokedOn > 0 && revokedOn <= time ? true : false;
+    return revokedOn > 0 && revokedOn <= time;
   }
 
   /**
