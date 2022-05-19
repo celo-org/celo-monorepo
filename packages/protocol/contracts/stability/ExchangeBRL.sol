a07ef825311a03e5d9871ef3a0739a928260b8bb
Marcin Chrzanowski <martin@clabs.co>
2022-05-19 11:44:51 +0200

    Update Validators and ExchangeBRL versions

--- packages/protocol/contracts/stability/ExchangeBRL.sol
+++ packages/protocol/contracts/stability/ExchangeBRL.sol
@@ -15,6 +15,6 @@ contract ExchangeBRL is Exchange {
 * @return The storage, major, minor, and patch version of the contract.
 */
   function getVersionNumber() external pure returns (uint256, uint256, uint256, uint256) {
-    return (1, 1, 0, 0);
+    return (1, 2, 0, 0);
   }
 }
