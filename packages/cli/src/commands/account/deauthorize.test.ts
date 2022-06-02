958de4025fd1f9e0366b43cd674bddaa553a9a7d
Dave Carroll <llorrac.evad@gmail.com>
2022-06-02 10:39:57 -0700

    style formatting

--- packages/cli/src/commands/account/deauthorize.test.ts
+++ packages/cli/src/commands/account/deauthorize.test.ts
@@ -20,14 +20,7 @@ testWithGanache('account:deauthorize cmd', (web3: Web3) => {
       '--signature',
       '0x1b9fca4bbb5bfb1dbe69ef1cddbd9b4202dcb6b134c5170611e1e36ecfa468d7b46c85328d504934fce6c2a1571603a50ae224d2b32685e84d4d1a1eebad8452eb',
     ])
-    await Deauthorize.run([
-      '--from',
-      accounts[0],
-      '--role',
-      'attestation',
-      '--signer',
-      accounts[1],
-    ])
+    await Deauthorize.run(['--from', accounts[0], '--role', 'attestation', '--signer', accounts[1]])
   })
 
   test('cannot deauthorize role other than attestation', async () => {
@@ -44,14 +37,7 @@ testWithGanache('account:deauthorize cmd', (web3: Web3) => {
       '0x1b9fca4bbb5bfb1dbe69ef1cddbd9b4202dcb6b134c5170611e1e36ecfa468d7b46c85328d504934fce6c2a1571603a50ae224d2b32685e84d4d1a1eebad8452eb',
     ])
     await expect(
-      Deauthorize.run([
-        '--from',
-        accounts[0],
-        '--role',
-        'vote',
-        '--signer',
-        accounts[1],
-      ])
+      Deauthorize.run(['--from', accounts[0], '--role', 'vote', '--signer', accounts[1]])
     ).rejects.toThrow()
   })
 })
