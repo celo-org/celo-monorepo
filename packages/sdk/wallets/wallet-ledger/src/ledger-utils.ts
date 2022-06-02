05e6c51f5b83270004204b085dfb559d08b09130
Aaron DeRuvo <aaron@clabs.co>
2022-06-02 16:23:23 -0700

    fix: handle more ledger error status code

--- packages/sdk/wallets/wallet-ledger/src/ledger-utils.ts
+++ packages/sdk/wallets/wallet-ledger/src/ledger-utils.ts
@@ -4,8 +4,13 @@ const debug = debugFactory('kit:wallet:ledger')
 
 export function transportErrorFriendlyMessage(error: any) {
   debug('Possible connection lost with the ledger')
-  debug(`Error message: ${error.message}`)
-  if (error.statusCode === 26368 || error.statusCode === 26628 || error.message === 'NoDevice') {
+  debug(`Error message: ${error.statusCode} ${error.message}`)
+  if (
+    error.statusCode === 26368 ||
+    error.statusCode === 26628 ||
+    error.statusCode === 27404 ||
+    error.message === 'NoDevice'
+  ) {
     throw new Error(
       `Possible connection lost with the ledger. Check if still on and connected. ${error.message}`
     )
