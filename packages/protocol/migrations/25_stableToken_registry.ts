5a461c771f9c8c9d70126ed17c6d3277c4adc074
Nina Barbakadze <barbakadzeninaa@gmail.com>
2022-06-15 11:51:38 +0200

    fixed migration error

--- packages/protocol/migrations/25_stableToken_registry.ts
+++ packages/protocol/migrations/25_stableToken_registry.ts
@@ -1,12 +1,14 @@
-/* tslint:disable:no-console */
 import { CeloContractName } from '@celo/protocol/lib/registry-utils'
 import { deploymentForCoreContract } from '@celo/protocol/lib/web3-utils'
 import { StableTokenRegistryInstance } from 'types'
 
-// Add initializeArgs when trying to deploy a the registry with a new stable asset on the test net
+const initializeArgs = async (): Promise<any[]> => {
+  return [[], []]
+}
 
 module.exports = deploymentForCoreContract<StableTokenRegistryInstance>(
   web3,
   artifacts,
-  CeloContractName.StableTokenRegistry
+  CeloContractName.StableTokenRegistry,
+  initializeArgs
 )
