c4c7fbe596aaf0b041d6f7ce581a0a34ba96e785
Eela Nagaraj <7308464+eelanagaraj@users.noreply.github.com>
2022-06-15 22:40:13 +0200

    Add comment explaining registry placeholder

--- packages/protocol/contracts/common/UsingRegistryV2WithStorageSlot.sol
+++ packages/protocol/contracts/common/UsingRegistryV2WithStorageSlot.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.5.13;
 import "./UsingRegistryV2.sol";
 
 contract UsingRegistryV2WithStorageSlot is UsingRegistryV2 {
+  // Placeholder for registry storage var in UsingRegistry and cannot be renamed
+  // without breaking release tooling.
+  // Use `registryContract` (in UsingRegistryV2) for the actual registry address.
   IRegistry public registry;
   /**
    * @notice Returns the storage, major, minor, and patch version of the contract.
