f5ff025dea5a0545735f4c510035c2360a1fc905
isabellewei <isabelle@clabs.co>
2022-06-23 05:03:02 -0400

    [ASv2] update attestation hash (#9659)
    * use eip712 hash for unique attestation hash
    
    * add signer to eip712 typed data
    
    * oops
    
    * lint
    
    * concaatenate strings

--- packages/protocol/contracts/identity/FederatedAttestations.sol
+++ packages/protocol/contracts/identity/FederatedAttestations.sol
@@ -44,7 +44,10 @@ contract FederatedAttestations is
   mapping(bytes32 => bool) public revokedAttestations;
 
   bytes32 public constant EIP712_OWNERSHIP_ATTESTATION_TYPEHASH = keccak256(
-    "OwnershipAttestation(bytes32 identifier,address issuer,address account,uint64 issuedOn)"
+    abi.encodePacked(
+      "OwnershipAttestation(bytes32 identifier,address issuer,",
+      "address account,address signer,uint64 issuedOn)"
+    )
   );
   bytes32 public eip712DomainSeparator;
 
@@ -291,9 +294,7 @@ contract FederatedAttestations is
       getAccounts().attestationSignerToAccount(signer) == issuer,
       "Signer is not a currently authorized AttestationSigner for the issuer"
     );
-    bytes32 structHash = keccak256(
-      abi.encode(EIP712_OWNERSHIP_ATTESTATION_TYPEHASH, identifier, issuer, account, issuedOn)
-    );
+    bytes32 structHash = getUniqueAttestationHash(identifier, issuer, account, signer, issuedOn);
     address guessedSigner = Signatures.getSignerOfTypedDataHash(
       eip712DomainSeparator,
       structHash,
@@ -311,7 +312,17 @@ contract FederatedAttestations is
     address signer,
     uint64 issuedOn
   ) public pure returns (bytes32) {
-    return keccak256(abi.encode(identifier, issuer, account, signer, issuedOn));
+    return
+      keccak256(
+        abi.encode(
+          EIP712_OWNERSHIP_ATTESTATION_TYPEHASH,
+          identifier,
+          issuer,
+          account,
+          signer,
+          issuedOn
+        )
+      );
   }
 
   /**
