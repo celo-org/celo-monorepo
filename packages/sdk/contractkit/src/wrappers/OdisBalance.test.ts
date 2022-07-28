037fc804891ee37f5bad6607132b9589160c79da
Eela Nagaraj <7308464+eelanagaraj@users.noreply.github.com>
2022-07-28 14:50:13 +0200

    Implement OdisBalance CK wrapper and tests

--- packages/sdk/contractkit/src/wrappers/OdisBalance.test.ts
+++ packages/sdk/contractkit/src/wrappers/OdisBalance.test.ts
@@ -1,22 +1,55 @@
 import { testWithGanache } from '@celo/dev-utils/lib/ganache-test'
+import { StableToken } from '../celo-tokens'
 import { newKitFromWeb3 } from '../kit'
 import { OdisBalanceWrapper } from './OdisBalance'
+import { StableTokenWrapper } from './StableTokenWrapper'
 
 testWithGanache('OdisBalance Wrapper', (web3) => {
   const kit = newKitFromWeb3(web3)
   let accounts: string[] = []
   let odisBalance: OdisBalanceWrapper
+  let stableToken: StableTokenWrapper
 
   beforeAll(async () => {
     accounts = await web3.eth.getAccounts()
     kit.defaultAccount = accounts[0]
+    odisBalance = await kit.contracts.getOdisBalance()
+    stableToken = await kit.contracts.getStableToken(StableToken.cUSD)
   })
 
-  describe('TODO OdisBalance', () => {
-    it('TODO OdisBalance', async () => {
-      expect(accounts)
-      odisBalance = await kit.contracts.getOdisBalance()
-      expect(odisBalance)
+  describe('#payInCUSD', () => {
+    const testValue = 10000
+
+    const payAndCheckState = async (sender: string, receiver: string, transferValue: number) => {
+      // Approve cUSD that OdisBalance contract may transfer from sender
+      const approveTx = await stableToken
+        .approve(odisBalance.address, transferValue)
+        .send({ from: sender })
+      await approveTx.waitReceipt()
+
+      const senderBalanceBefore = await stableToken.balanceOf(sender)
+      const tx = await odisBalance.payInCUSD(receiver, transferValue).send({ from: sender })
+      await tx.waitReceipt()
+      const balanceAfter = await stableToken.balanceOf(sender)
+      expect(senderBalanceBefore.minus(balanceAfter)).toEqBigNumber(transferValue)
+      expect(await stableToken.balanceOf(odisBalance.address)).toEqBigNumber(transferValue)
+      expect(await odisBalance.totalPaidCUSD(receiver)).toEqBigNumber(transferValue)
+    }
+
+    it('should allow sender to make a payment on their behalf', async () => {
+      await payAndCheckState(accounts[0], accounts[0], testValue)
+    })
+
+    it('should allow sender to make a payment for another account', async () => {
+      await payAndCheckState(accounts[0], accounts[1], testValue)
+    })
+
+    it('should revert if transfer fails', async () => {
+      const approveTx = await stableToken.approve(odisBalance.address, testValue).send()
+      await approveTx.waitReceipt()
+      expect.assertions(2)
+      await expect(odisBalance.payInCUSD(accounts[0], testValue + 1).send()).rejects.toThrow()
+      expect(await odisBalance.totalPaidCUSD(accounts[0])).toEqBigNumber(0)
     })
   })
 })
--- packages/sdk/contractkit/src/wrappers/OdisBalance.ts
+++ packages/sdk/contractkit/src/wrappers/OdisBalance.ts
@@ -1,4 +1,9 @@
 import { OdisBalance } from '../generated/OdisBalance'
-import { BaseWrapper } from './BaseWrapper'
+import { BaseWrapper, proxyCall, proxySend } from './BaseWrapper'
 
-export class OdisBalanceWrapper extends BaseWrapper<OdisBalance> {}
+export class OdisBalanceWrapper extends BaseWrapper<OdisBalance> {
+  totalPaidCUSD = proxyCall(this.contract.methods.totalPaidCUSD)
+  payInCUSD = proxySend(this.connection, this.contract.methods.payInCUSD)
+}
+
+export type OdisBalanceWrapperType = OdisBalanceWrapper
