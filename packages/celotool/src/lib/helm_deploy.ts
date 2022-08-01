48a7dbbb2ac57f14af4597aa4fc4f20a665d9cd9
Victoria Peschke <victoria.peschke@clabs.co>
2022-08-01 18:18:43 +0200

    Add prometheus community helm charts

--- packages/celotool/src/lib/helm_deploy.ts
+++ packages/celotool/src/lib/helm_deploy.ts
@@ -373,7 +373,11 @@ export async function helmAddAndUpdateRepos() {
     `helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx`
   )
   await execCmdWithExitOnFailure(`helm repo add stable https://charts.helm.sh/stable`)
+  await execCmdWithExitOnFailure(
+    `helm repo add prometheus-community https://prometheus-community.github.io/helm-charts`
+  )
   await execCmdWithExitOnFailure(`helm repo add grafana https://grafana.github.io/helm-charts`)
+
   await execCmdWithExitOnFailure(`helm repo update`)
 }
 
