0d6c386eecf3c03112f8bf878ce03506dd51c76b
pahor167 <47992132+pahor167@users.noreply.github.com>
2022-08-05 15:50:58 +0200

    Update packages/protocol/contracts/common/linkedlists/AddressSortedLinkedList.sol
    Co-authored-by: Martin <martin@clabs.co>

--- packages/protocol/contracts/common/linkedlists/AddressSortedLinkedList.sol
+++ packages/protocol/contracts/common/linkedlists/AddressSortedLinkedList.sol
@@ -88,8 +88,8 @@ library AddressSortedLinkedList {
 
   /**
    * @notice Gets all elements from the doubly linked list.
-   * @return Keys of nn unpacked list of elements from largest to smallest.
-   * @return Values of an unpacked list of elements from largest to smallest.
+   * @return Array of all keys in the list.
+   * @return Values corresponding to keys, which will be ordered largest to smallest.
    */
   function getElements(SortedLinkedList.List storage list)
     public
