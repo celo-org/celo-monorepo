ba24265ae84db66fb72b790bb2c8ff957238c474
pahor167 <pavel.hornak@clabs.co>
2022-08-11 13:33:40 +0200

    Contract kit tests fix

--- packages/sdk/contractkit/src/wrappers/Governance.test.ts
+++ packages/sdk/contractkit/src/wrappers/Governance.test.ts
@@ -76,15 +76,20 @@ testWithGanache('Governance Wrapper', (web3: Web3) => {
       [CeloContract.Random, '0x0000000000000000000000000000000000000001'],
       [CeloContract.Escrow, '0x0000000000000000000000000000000000000002'],
     ]
-    const proposalID = new BigNumber(1)
+    const proposalID = new BigNumber(2)
 
     let proposal: Proposal
     beforeAll(() => (proposal = registryRepointProposal(repoints)))
 
-    const proposeFn = async (proposer: Address) =>
-      governance
+    const proposeFn = async (proposer: Address) => {
+      await governance
         .propose(proposal, 'URL')
         .sendAndWaitForReceipt({ from: proposer, value: minDeposit })
+
+      await governance
+        .propose(proposal, 'URL')
+        .sendAndWaitForReceipt({ from: proposer, value: minDeposit })
+    }
 
     const upvoteFn = async (upvoter: Address, shouldTimeTravel = true) => {
       const tx = await governance.upvote(proposalID, upvoter)
@@ -194,7 +199,7 @@ testWithGanache('Governance Wrapper', (web3: Web3) => {
       await voteFn(accounts[2])
 
       const proposer = await governance.getVoter(accounts[0])
-      expect(proposer.refundedDeposits).toEqBigNumber(minDeposit)
+      expect(proposer.refundedDeposits).toEqual(new BigNumber(minDeposit).multipliedBy(2))
 
       const upvoter = await governance.getVoter(accounts[1])
       const expectedUpvoteRecord = { proposalID, upvotes: new BigNumber(ONE_CGLD) }
@@ -202,7 +207,7 @@ testWithGanache('Governance Wrapper', (web3: Web3) => {
 
       const voter = await governance.getVoter(accounts[2])
       const expectedVoteRecord = { proposalID, votes: new BigNumber(ONE_CGLD), value: 'Yes' }
-      expect(voter.votes[0]).toEqual(expectedVoteRecord)
+      expect(voter.votes[1]).toEqual(expectedVoteRecord)
     })
   })
 
