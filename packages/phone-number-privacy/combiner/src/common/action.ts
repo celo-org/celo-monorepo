01859065473206355030f7e5b5da2b3b94a0f99b
alecps <alec@cLabs.co>
2022-08-24 18:53:18 -0400

    adds another TODO

--- packages/phone-number-privacy/combiner/src/common/action.ts
+++ packages/phone-number-privacy/combiner/src/common/action.ts
@@ -2,6 +2,7 @@ import { OdisRequest } from '@celo/phone-number-privacy-common'
 import { IO } from './io'
 import { Session } from './session'
 
+// TODO(2.0.0, refactor) should files like this and Controller go in the common pkg?
 export interface Action<R extends OdisRequest> {
   readonly io: IO<R>
   perform(session: Session<R>): Promise<void>
