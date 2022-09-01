ba363bc7771fb91a8f149135330369608f40a6a7
alecps <alec@cLabs.co>
2022-09-01 14:14:52 -0400

    add example

--- packages/phone-number-privacy/combiner/src/pnp/endpoints/sign/action.ts
+++ packages/phone-number-privacy/combiner/src/pnp/endpoints/sign/action.ts
@@ -3,7 +3,6 @@ import {
   ErrorType,
   MAX_BLOCK_DISCREPANCY_THRESHOLD,
   SignMessageRequest,
-  SignMessageResponseSuccess,
   WarningMessage,
 } from '@celo/phone-number-privacy-common'
 import { CryptoSession } from '../../../common/crypto-session'
@@ -45,54 +44,130 @@ export class PnpSignAction extends SignAction<SignMessageRequest> {
 
   protected logResponseDiscrepancies(session: CryptoSession<SignMessageRequest>): void {
     // TODO: responses should all already be successes due to CombineAction receiveSuccess
-    const responses = session.responses
-      .filter((res) => res.res.success)
-      .map((res) => res.res) as SignMessageResponseSuccess[]
-    const values = session.responses
-      .map(
-        (response) =>
-          response.res.success && {
-            signer: response.url,
-            blockNumber: response.res.blockNumber,
-            totalQuota: response.res.totalQuota,
-            performedQueryCount: response.res.performedQueryCount,
-          }
-      )
-      .filter((val) => val)
 
-    if (responses.length === 0) {
-      return
-    }
-    const expectedRes = responses[0]
-    responses.forEach((res) => {
-      if (
-        res.blockNumber &&
-        expectedRes.blockNumber &&
-        Math.abs(res.blockNumber - expectedRes.blockNumber) > MAX_BLOCK_DISCREPANCY_THRESHOLD
-      ) {
-        const blockValues = session.responses
-          .map(
-            (response) =>
-              response.res.success && {
-                signer: response.url,
-                blockNumber: response.res.blockNumber,
-                warnings: response.res.warnings,
-              }
-          )
-          .filter((val) => val)
-        session.logger.error({ blockValues }, WarningMessage.INCONSISTENT_SIGNER_BLOCK_NUMBERS)
-        return
-      } else if (res.totalQuota !== expectedRes.totalQuota) {
-        session.logger.error({ values }, WarningMessage.INCONSISTENT_SIGNER_QUOTA_MEASUREMENTS)
-        return
-      } else if (
-        res.performedQueryCount !== expectedRes.performedQueryCount ||
-        res.warnings !== expectedRes.warnings
-      ) {
-        session.logger.warn({ values }, 'Discrepancies in signer quota responses')
-        return
+    const parsedResponses: Array<{
+      signerUrl: string
+      valuesOfInterest: {
+        version: string
+        performedQueryCount: number
+        totalQuota: number
+        blockNumber?: number
+        warnings?: string[]
+      }
+    }> = []
+    session.responses.forEach((response) => {
+      if (response.res.success) {
+        const { version, performedQueryCount, totalQuota, blockNumber, warnings } = response.res
+        parsedResponses.push({
+          signerUrl: response.url,
+          valuesOfInterest: { version, performedQueryCount, totalQuota, blockNumber, warnings },
+        })
       }
     })
+
+    if (parsedResponses.length === 0) {
+      // session.logger.info("No response discrepencies found!")
+      // TODO
+      return
+    }
+
+    let discrepancyFound = false
+    const first = JSON.stringify(parsedResponses[0].valuesOfInterest)
+    for (let i = 1; i < parsedResponses.length; i++) {
+      if (JSON.stringify(parsedResponses[i].valuesOfInterest) !== first) {
+        discrepancyFound = true
+        break
+      }
+    }
+
+    // log all responses if we notice any discrepency to aid with debugging
+    if (discrepancyFound) {
+      session.logger.warn({ parsedResponses }, 'Discrepencies detected in signer responses')
+    }
+
+    // Some values should trigger custom logging if we detect discrepencies beyond certain thresholds
+
+    // blockNumber
+    parsedResponses.forEach((res) => {
+      if (res.valuesOfInterest.blockNumber === undefined) {
+        session.logger.warn(
+          { signerUrl: res.signerUrl },
+          'Signer responded with undefined blockNumber'
+        )
+      }
+    })
+    const sortedDescByBlockNumber = parsedResponses
+      .filter((res) => !!res.valuesOfInterest.blockNumber)
+      .sort((a, b) => a.valuesOfInterest.blockNumber! - b.valuesOfInterest.blockNumber!)
+
+    if (
+      sortedDescByBlockNumber.length &&
+      sortedDescByBlockNumber[0].valuesOfInterest.blockNumber! -
+        sortedDescByBlockNumber[sortedDescByBlockNumber.length - 1].valuesOfInterest.blockNumber! >
+        MAX_BLOCK_DISCREPANCY_THRESHOLD
+    ) {
+      session.logger.error(
+        { sortedDescByBlockNumber },
+        WarningMessage.INCONSISTENT_SIGNER_BLOCK_NUMBERS
+      )
+    }
+
+    // totalQuota
+    const sortedDescByTotalQuota = parsedResponses.sort(
+      (a, b) => a.valuesOfInterest.totalQuota - b.valuesOfInterest.totalQuota
+    )
+
+    if (
+      sortedDescByTotalQuota.length &&
+      sortedDescByTotalQuota[0].valuesOfInterest.totalQuota -
+        sortedDescByTotalQuota[sortedDescByTotalQuota.length - 1].valuesOfInterest.totalQuota >
+        MAX_TOTAL_QUOTA_DISCREPENCY_THRESHOLD
+    ) {
+      session.logger.error({ sortedDescByTotalQuota }, WarningMessage.INCONSISTENT_TOTAL_QUOTA)
+    }
+
+    // const values = session.responses
+    //   .map(
+    //     (response) =>
+    //       response.res.success && {
+    //         signer: response.url,
+    //         blockNumber: response.res.blockNumber,
+    //         totalQuota: response.res.totalQuota,
+    //         performedQueryCount: response.res.performedQueryCount,
+    //       }
+    //   )
+    //   .filter((val) => val)
+
+    // const expectedRes = responses[0]
+    // responses.forEach((res) => {
+    //   if (
+    //     res.blockNumber &&
+    //     expectedRes.blockNumber &&
+    //     Math.abs(res.blockNumber - expectedRes.blockNumber) > MAX_BLOCK_DISCREPANCY_THRESHOLD
+    //   ) {
+    //     const blockValues = session.responses
+    //       .map(
+    //         (response) =>
+    //           response.res.success && {
+    //             signer: response.url,
+    //             blockNumber: response.res.blockNumber,
+    //             warnings: response.res.warnings,
+    //           }
+    //       )
+    //       .filter((val) => val)
+    //     session.logger.error({ blockValues }, WarningMessage.INCONSISTENT_SIGNER_BLOCK_NUMBERS)
+    //     return
+    //   } else if (res.totalQuota !== expectedRes.totalQuota) {
+    //     session.logger.error({ values }, WarningMessage.INCONSISTENT_SIGNER_QUOTA_MEASUREMENTS)
+    //     return
+    //   } else if (
+    //     res.performedQueryCount !== expectedRes.performedQueryCount ||
+    //     res.warnings !== expectedRes.warnings
+    //   ) {
+    //     session.logger.warn({ values }, 'Discrepancies in signer quota responses')
+    //     return
+    //   }
+    // })
   }
 
   protected errorCodeToError(errorCode: number): ErrorType {
