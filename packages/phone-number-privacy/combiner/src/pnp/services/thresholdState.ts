180a8d960a870394e93703444b464c1a96b3499e
Isabelle Wei <isabelle@clabs.co>
2022-09-13 04:09:12 -0400

    handle inconsistent total quota responses

--- packages/phone-number-privacy/combiner/src/pnp/services/thresholdState.ts
+++ packages/phone-number-privacy/combiner/src/pnp/services/thresholdState.ts
@@ -4,7 +4,7 @@ import {
   SignMessageRequest,
 } from '@celo/phone-number-privacy-common'
 import { Session } from '../../common/session'
-import { OdisConfig } from '../../config'
+import { MAX_TOTAL_QUOTA_DISCREPANCY_THRESHOLD, OdisConfig } from '../../config'
 
 // TODO(2.0.0, testing): add unit tests for this and domains equivalent
 // (https://github.com/celo-org/celo-monorepo/issues/9792)
@@ -15,23 +15,35 @@ export class PnpThresholdStateService<R extends PnpQuotaRequest | SignMessageReq
     const signerResponses = session.responses
       .map((signerResponse) => signerResponse.res)
       .filter((res) => res.success) as PnpQuotaStatus[]
-
     const sortedResponses = signerResponses.sort(
-      (a, b) => a.performedQueryCount - b.performedQueryCount
+      (a, b) => a.totalQuota - a.performedQueryCount - (b.totalQuota - b.performedQueryCount)
     )
-
     const threshold = this.config.keys.threshold
-    if (signerResponses.length < threshold) {
-      throw new Error('Insufficient number of successful signer responses')
-      // checking for sufficient number of responses is already done in the action
-      // this is basically checking for sufficient number of successful responses
+
+    const totalQuotaAvg =
+      sortedResponses.reduce((a, b) => a.totalQuota + b.totalQuota) / sortedResponses.length
+    const totalQuotaStDev = Math.sqrt(
+      sortedResponses.map((r) => (r.totalQuota - totalQuotaAvg) ** 2).reduce((a, b) => a + b) /
+        sortedResponses.length
+    )
+    if (totalQuotaStDev > MAX_TOTAL_QUOTA_DISCREPANCY_THRESHOLD) {
+      throw new Error('Signer total quota responses deviate too much to be combined')
+    } else if (totalQuotaStDev > 0) {
+      session.warnings.push(
+        'Inconsistent signer total quota responses, using threshold signer as best guess'
+      )
     }
+
+    // TODO: currently this check is not needed, as checking for sufficient number of responses and
+    // filtering for successes is already done in the action. Consider adding back in based on the
+    // result of https://github.com/celo-org/celo-monorepo/issues/9826
+    // if (signerResponses.length < threshold) {
+    //   throw new Error('Insufficient number of successful signer responses')
+    // }
 
     const thresholdSigner = sortedResponses[threshold - 1]
     return {
       performedQueryCount: thresholdSigner.performedQueryCount,
-      // TODO(2.0.0, refactor) address scenario where total quota is inconsistent between signers
-      // (https://github.com/celo-org/celo-monorepo/issues/9806)
       totalQuota: thresholdSigner.totalQuota,
       blockNumber: thresholdSigner.blockNumber,
     }
