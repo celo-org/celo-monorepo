4164b0aed380605923add0efaeffa39144abbfeb
Alec Schaefer <alec@cLabs.co>
2022-09-14 17:48:49 -0400

    Add codecov to ODIS pkgs  (#9838)
    * add configs for local coverage testing
    
    * fix test
    
    * add coverage thresholds
    
    * move domains test file
    
    * adds extension recommendation
    
    * make add coverage testing to CI

--- packages/phone-number-privacy/combiner/jest.config.js
+++ packages/phone-number-privacy/combiner/jest.config.js
@@ -4,4 +4,11 @@ module.exports = {
   preset: 'ts-jest',
   ...nodeFlakeTracking,
   setupFilesAfterEnv: ['<rootDir>/jest_setup.ts', ...nodeFlakeTracking.setupFilesAfterEnv],
+  coverageReporters: [['lcov', { projectRoot: '../../../' }], 'text'],
+  collectCoverageFrom: ['./src/**'],
+  coverageThreshold: {
+    global: {
+      lines: 59, // TODO(2.0.0) make this higher in testing audit ticket (https://github.com/celo-org/celo-monorepo/issues/9811)
+    },
+  },
 }
--- packages/phone-number-privacy/combiner/test/integration/pnp.test.ts
+++ packages/phone-number-privacy/combiner/test/integration/pnp.test.ts
@@ -917,6 +917,7 @@ describe('pnpService', () => {
           performedQueryCount: 0,
           totalQuota,
           blockNumber: testBlockNumber,
+          warnings: [],
         })
       })
     })
