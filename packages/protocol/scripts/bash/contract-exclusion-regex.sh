a52f77f7a64ed03734f4cb4df166b727c5b304e7
Martin Volpe <martin.volpe@gmail.com>
2022-09-19 17:55:12 +0200

    Only avoid for CR7

--- packages/protocol/scripts/bash/contract-exclusion-regex.sh
+++ packages/protocol/scripts/bash/contract-exclusion-regex.sh
@@ -1,3 +1,12 @@
 # Exclude test contracts, mock contracts, contract interfaces, Proxy contracts, inlined libraries,
 # MultiSig contracts, and the ReleaseGold contract.
-CONTRACT_EXCLUSION_REGEX=".*Test|Mock.*|I[A-Z].*|.*Proxy|MultiSig.*|ReleaseGold|SlasherUtil|UsingPrecompiles|^UsingRegistry"
+
+
+
+CONTRACT_EXCLUSION_REGEX=".*Test|Mock.*|I[A-Z].*|.*Proxy|MultiSig.*|ReleaseGold|SlasherUtil|UsingPrecompiles"
+
+# Before CR7, they had been deployed
+if [ "$OLD_BRANCH" = "core-contracts.v7" ]
+  then
+  CONTRACT_EXCLUSION_REGEX="$CONTRACT_EXCLUSION_REGEX|^UsingRegistry"
+fi
