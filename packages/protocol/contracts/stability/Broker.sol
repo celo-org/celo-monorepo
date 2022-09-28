1d6f13af0eced8d06a3439d0d6ba9acf9e57ac26
nina <barbakadzeninaa@gmail.com>
2022-09-28 18:50:29 +0200

    feat: Implementation of McMint Broker contract (#9848)
    * feat: Broker contract and changes in IBrokerAdmin
    
    * post interface changes;
    
    * post-review changes
    
    * nits
    
    * refactoring swap logic
    
    * tests
    
    * adding functionality for minting and burning
    
    * updated the contract, uncommented tests pass
    
    * IExchangeManager does not receive calls
    
    * Make test work
    
    * getAmountIn and getAmountOut complete
    
    * swap tests not registering calls
    
    * tests pass
    
    * merge
    
    * Delete IExchangeManager.sol
    
    * Update references to 'manager' in comments
    
    * Remove duplicated text on BrokerAdmin interface
    
    * Update interface comments.
    
    * Updated comments
    
    * Update useage of 'manager' in comment.
    
    * Renamed test class
    
    * Switch manager to provider in comments
    
    * Consistent punctuation in comments
    
    * post-merge changes
    
    * adding a condition to make sure that the mapping was updated
    
    * code review updates
    
    * code review updates
    
    Co-authored-by: bowd <bogdan@bowd.io>
    Co-authored-by: Bayological <6872903+bayological@users.noreply.github.com>

---
+++ packages/protocol/contracts/stability/Broker.sol
@@ -0,0 +1,250 @@
+pragma solidity ^0.5.13;
+pragma experimental ABIEncoderV2;
+
+import { IERC20 } from "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import { Ownable } from "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+import { IExchangeProvider } from "./interfaces/IExchangeProvider.sol";
+import { IBroker } from "./interfaces/IBroker.sol";
+import { IBrokerAdmin } from "./interfaces/IBrokerAdmin.sol";
+import { IReserve } from "./interfaces/IReserve.sol";
+import { IStableToken } from "./interfaces/IStableToken.sol";
+
+import { Initializable } from "../common/Initializable.sol";
+
+/**
+ * @title Broker
+ * @notice The broker executes swaps and keeps track of spending limits per pair.
+ */
+contract Broker is IBroker, IBrokerAdmin, Initializable, Ownable {
+  /* ==================== State Variables ==================== */
+
+  address[] public exchangeProviders;
+  mapping(address => bool) public isExchangeProvider;
+
+  // Address of the reserve.
+  IReserve public reserve;
+
+  /* ==================== Constructor ==================== */
+
+  /**
+   * @notice Sets initialized == true on implementation contracts.
+   * @param test Set to true to skip implementation initialization.
+   */
+  constructor(bool test) public Initializable(test) {}
+
+  /**
+   * @notice Allows the contract to be upgradable via the proxy.
+   * @param _exchangeProviders The addresses of the ExchangeProvider contracts.
+   * @param _reserve The address of the Reserve contract.
+   */
+  function initialize(address[] calldata _exchangeProviders, address _reserve)
+    external
+    initializer
+  {
+    _transferOwnership(msg.sender);
+    for (uint256 i = 0; i < _exchangeProviders.length; i++) {
+      addExchangeProvider(_exchangeProviders[i]);
+    }
+    setReserve(_reserve);
+  }
+
+  /* ==================== Mutative Functions ==================== */
+
+  /**
+   * @notice Add an exchange provider to the list of providers.
+   * @param exchangeProvider The address of the exchange provider to add.
+   * @return index The index of the newly added specified exchange provider.
+   */
+  function addExchangeProvider(address exchangeProvider) public onlyOwner returns (uint256 index) {
+    require(!isExchangeProvider[exchangeProvider], "ExchangeProvider already exists in the list");
+    require(exchangeProvider != address(0), "ExchangeProvider address can't be 0");
+    exchangeProviders.push(exchangeProvider);
+    isExchangeProvider[exchangeProvider] = true;
+    emit ExchangeProviderAdded(exchangeProvider);
+    index = exchangeProviders.length - 1;
+  }
+
+  /**
+   * @notice Remove an exchange provider from the list of providers.
+   * @param exchangeProvider The address of the exchange provider to remove.
+   * @param index The index of the exchange provider being removed.
+   */
+  function removeExchangeProvider(address exchangeProvider, uint256 index) public onlyOwner {
+    require(
+      exchangeProviders[index] == exchangeProvider,
+      "index into exchangeProviders list not mapped to an exchangeProvider"
+    );
+    exchangeProviders[index] = exchangeProviders[exchangeProviders.length - 1];
+    exchangeProviders.pop();
+    delete isExchangeProvider[exchangeProvider];
+    emit ExchangeProviderRemoved(exchangeProvider);
+  }
+
+  /**
+   * @notice Set the Mento reserve address.
+   * @param _reserve The Mento reserve address.
+   */
+  function setReserve(address _reserve) public onlyOwner {
+    require(_reserve != address(0), "Reserve address must be set");
+    emit ReserveSet(_reserve, address(reserve));
+    reserve = IReserve(_reserve);
+  }
+
+  /**
+   * @notice Calculate the amount of tokenIn to be sold for a given amountOut of tokenOut
+   * @param exchangeProvider the address of the exchange manager for the pair
+   * @param exchangeId The id of the exchange to use
+   * @param tokenIn The token to be sold
+   * @param tokenOut The token to be bought
+   * @param amountOut The amount of tokenOut to be bought
+   * @return amountIn The amount of tokenIn to be sold
+   */
+  function getAmountIn(
+    address exchangeProvider,
+    bytes32 exchangeId,
+    address tokenIn,
+    address tokenOut,
+    uint256 amountOut
+  ) external returns (uint256 amountIn) {
+    require(isExchangeProvider[exchangeProvider], "ExchangeProvider does not exist");
+    amountIn = IExchangeProvider(exchangeProvider).getAmountIn(
+      exchangeId,
+      tokenIn,
+      tokenOut,
+      amountOut
+    );
+  }
+
+  /**
+   * @notice Calculate the amount of tokenOut to be bought for a given amount of tokenIn to be sold
+   * @param exchangeProvider the address of the exchange manager for the pair
+   * @param exchangeId The id of the exchange to use
+   * @param tokenIn The token to be sold
+   * @param tokenOut The token to be bought
+   * @param amountIn The amount of tokenIn to be sold
+   * @return amountOut The amount of tokenOut to be bought
+   */
+  function getAmountOut(
+    address exchangeProvider,
+    bytes32 exchangeId,
+    address tokenIn,
+    address tokenOut,
+    uint256 amountIn
+  ) external returns (uint256 amountOut) {
+    require(isExchangeProvider[exchangeProvider], "ExchangeProvider does not exist");
+    amountOut = IExchangeProvider(exchangeProvider).getAmountOut(
+      exchangeId,
+      tokenIn,
+      tokenOut,
+      amountIn
+    );
+  }
+
+  /**
+   * @notice Execute a token swap with fixed amountIn.
+   * @param exchangeProvider the address of the exchange provider for the pair.
+   * @param exchangeId The id of the exchange to use.
+   * @param tokenIn The token to be sold.
+   * @param tokenOut The token to be bought.
+   * @param amountIn The amount of tokenIn to be sold.
+   * @param amountOutMin Minimum amountOut to be received - controls slippage.
+   * @return amountOut The amount of tokenOut to be bought.
+   */
+  function swapIn(
+    address exchangeProvider,
+    bytes32 exchangeId,
+    address tokenIn,
+    address tokenOut,
+    uint256 amountIn,
+    uint256 amountOutMin
+  ) external returns (uint256 amountOut) {
+    require(isExchangeProvider[exchangeProvider], "ExchangeProvider does not exist");
+    amountOut = IExchangeProvider(exchangeProvider).swapIn(exchangeId, tokenIn, tokenOut, amountIn);
+    require(amountOut >= amountOutMin, "amountOutMin not met");
+    transferIn(msg.sender, tokenIn, amountIn);
+    transferOut(msg.sender, tokenOut, amountOut);
+    emit Swap(exchangeProvider, exchangeId, msg.sender, tokenIn, tokenOut, amountIn, amountOut);
+  }
+
+  /**
+   * @notice Execute a token swap with fixed amountOut.
+   * @param exchangeProvider the address of the exchange provider for the pair.
+   * @param exchangeId The id of the exchange to use.
+   * @param tokenIn The token to be sold.
+   * @param tokenOut The token to be bought.
+   * @param amountOut The amount of tokenOut to be bought.
+   * @param amountInMax Maximum amount of tokenIn that can be traded.
+   * @return amountIn The amount of tokenIn to be sold.
+   */
+  function swapOut(
+    address exchangeProvider,
+    bytes32 exchangeId,
+    address tokenIn,
+    address tokenOut,
+    uint256 amountOut,
+    uint256 amountInMax
+  ) external returns (uint256 amountIn) {
+    require(isExchangeProvider[exchangeProvider], "ExchangeProvider does not exist");
+    amountIn = IExchangeProvider(exchangeProvider).swapOut(
+      exchangeId,
+      tokenIn,
+      tokenOut,
+      amountOut
+    );
+    require(amountIn <= amountInMax, "amountInMax exceeded");
+    transferIn(msg.sender, tokenIn, amountIn);
+    transferOut(msg.sender, tokenOut, amountOut);
+    emit Swap(exchangeProvider, exchangeId, msg.sender, tokenIn, tokenOut, amountIn, amountOut);
+  }
+
+  /* ==================== Private Functions ==================== */
+
+  /**
+   * @notice Transfer a specified Mento asset to the given address.
+   * If the specified asset is a stable asset it will be minted directly to the address. If
+   * the asset is a collateral asset it will be transferred from the reserve to the given address.
+   * @param to The address receiving the asset.
+   * @param token The asset to transfer.
+   * @param amount The amount of `token` to be transferred.
+   */
+  function transferOut(address payable to, address token, uint256 amount) internal {
+    if (reserve.isStableAsset(token)) {
+      IStableToken(token).mint(to, amount);
+    } else if (reserve.isCollateralAsset(token)) {
+      reserve.transferCollateralAsset(token, to, amount);
+    } else {
+      revert("Token must be stable or collateral assert");
+    }
+  }
+
+  /**
+   * @notice Transfer a specified Mento asset into the reserve or the broker. 
+   * If the specified asset is a stable asset it will be transfered to the broker 
+   * and burned. If the asset is a collateral asset it will be transferred to the reserve.
+   * @param from The address to transfer the asset from.
+   * @param token The asset to transfer.
+   * @param amount The amount of `token` to be transferred. 
+   */
+  function transferIn(address payable from, address token, uint256 amount) internal {
+    if (reserve.isStableAsset(token)) {
+      IERC20(token).transferFrom(from, address(this), amount);
+      IStableToken(token).burn(amount);
+    } else if (reserve.isCollateralAsset(token)) {
+      IERC20(token).transferFrom(from, address(reserve), amount);
+    } else {
+      revert("Token must be stable or collateral assert");
+    }
+  }
+
+  /* ==================== View Functions ==================== */
+
+  /**
+   * @notice Get the list of registered exchange providers.
+   * @dev This can be used by UI or clients to discover all pairs.
+   * @return exchangeProviders the addresses of all exchange providers.
+   */
+  function getExchangeProviders() external view returns (address[] memory) {
+    return exchangeProviders;
+  }
+}
--- packages/protocol/contracts/stability/interfaces/IBrokerAdmin.sol
+++ packages/protocol/contracts/stability/interfaces/IBrokerAdmin.sol
@@ -2,46 +2,47 @@
 pragma solidity ^0.5.13;
 
 /*
- * @title Broker Admin Interface - all these functions should be only callable by the owner
- * @notice The broker is responsible for executing swaps and keeping track of trading limits
+ * @title Broker Admin Interface
+ * @notice Contains admin functions to configure the broker that
+ *         should be only callable by the owner.
  */
 interface IBrokerAdmin {
   /**
-   * @notice Emitted when a ExchangeProvider is added
-   * @param exchangeProvider the address of the ExchangeProvider.
+   * @notice Emitted when an ExchangeProvider is added.
+   * @param exchangeProvider The address of the ExchangeProvider.
    */
   event ExchangeProviderAdded(address indexed exchangeProvider);
 
   /**
-   * @notice Emitted when a ExchangeProvider is removed
-   * @param exchangeProvider the address of the ExchangeProvider.
+   * @notice Emitted when an ExchangeProvider is removed.
+   * @param exchangeProvider The address of the ExchangeProvider.
    */
   event ExchangeProviderRemoved(address indexed exchangeProvider);
 
   /**
-   * @notice Emitted the Reserve is updated
-   * @param newAddress the new address
-   * @param prevAddress the previous address
+   * @notice Emitted when the reserve is updated.
+   * @param newAddress The new address.
+   * @param prevAddress The previous address.
    */
-  event ReserveUpdated(address indexed newAddress, address indexed prevAddress);
+  event ReserveSet(address indexed newAddress, address indexed prevAddress);
 
   /**
-   * @notice Remove a exchange manager at an index
-   * @param exchangeProvider the address of the exchange manager to remove
-   * @param index the index in the exchange managers array
+   * @notice Remove an ExchangeProvider at a specified index.
+   * @param exchangeProvider The address of the ExchangeProvider to remove.
+   * @param index The index in the exchange providers array.
    */
-  function removeExchangeProviders(address exchangeProvider, uint256 index) external;
+  function removeExchangeProvider(address exchangeProvider, uint256 index) external;
 
   /**
-   * @notice Add exchange manager
-   * @param exchangeProvider the address of the exchange manager to add
-   * @return index the index where it was inserted
+   * @notice Add an ExchangeProvider.
+   * @param exchangeProvider The address of the ExchangeProvider to add.
+   * @return index The index where the ExchangeProvider was inserted.
    */
   function addExchangeProvider(address exchangeProvider) external returns (uint256 index);
 
   /**
-   * @notice Set the reserve
-   * @param reserve address
+   * @notice Set the Mento reserve address.
+   * @param reserve The Mento reserve address.
    */
   function setReserve(address reserve) external;
 }
