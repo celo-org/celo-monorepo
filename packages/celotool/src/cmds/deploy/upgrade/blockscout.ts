ea15dc9d195c8240eeff27936b432cdecf93f2b0
Valentin Rodygin <valentin@clabs.co>
2022-09-29 00:07:33 +0200

    Move all Blockscout config parameters to Helm (#9140)

--- packages/celotool/src/cmds/deploy/upgrade/blockscout.ts
+++ packages/celotool/src/cmds/deploy/upgrade/blockscout.ts
@@ -1,20 +1,14 @@
 import sleep from 'sleep-promise'
 import {
   createDefaultIngressIfNotExists,
-  createGrafanaTagAnnotation,
   getInstanceName,
   getReleaseName,
   removeHelmRelease,
   upgradeHelmChart,
 } from 'src/lib/blockscout'
 import { switchToClusterFromEnv } from 'src/lib/cluster'
-import { execCmd } from 'src/lib/cmd-utils'
 import { envVar, fetchEnvOrFallback } from 'src/lib/env-utils'
-import {
-  isCelotoolHelmDryRun,
-  resetCloudSQLInstance,
-  retrieveCloudSQLConnectionInfo,
-} from 'src/lib/helm_deploy'
+import { isCelotoolHelmDryRun, resetCloudSQLInstance } from 'src/lib/helm_deploy'
 import yargs from 'yargs'
 import { UpgradeArgv } from '../../deploy/upgrade'
 
@@ -55,12 +49,6 @@ export const handler = async (argv: BlockscoutUpgradeArgv) => {
   const instanceName = getInstanceName(argv.celoEnv, dbSuffix)
   const helmReleaseName = getReleaseName(argv.celoEnv, dbSuffix)
 
-  const [
-    blockscoutDBUsername,
-    blockscoutDBPassword,
-    blockscoutDBConnectionName,
-  ] = await retrieveCloudSQLConnectionInfo(argv.celoEnv, instanceName, dbSuffix)
-
   if (!isCelotoolHelmDryRun()) {
     if (argv.reset === true) {
       console.info(
@@ -72,32 +60,12 @@ export const handler = async (argv: BlockscoutUpgradeArgv) => {
       console.info('Sleep for 30 seconds to have all connections killed')
       await sleep(30000)
       await resetCloudSQLInstance(instanceName)
-    } else {
-      console.info(`Delete blockscout-migration`)
-      try {
-        const jobName = `${argv.celoEnv}-blockscout${dbSuffix}-migration`
-        await execCmd(`kubectl delete job ${jobName} -n ${argv.celoEnv}`)
-      } catch (error) {
-        console.error(error)
-      }
     }
-  } else {
-    console.info(
-      `Skipping Cloud SQL Database upgrade process (recreation or kubernetes job removal). Please check if you can execute the skipped steps.`
-    )
   }
 
-  await upgradeHelmChart(
-    argv.celoEnv,
-    helmReleaseName,
-    imageTag,
-    blockscoutDBUsername,
-    blockscoutDBPassword,
-    blockscoutDBConnectionName
-  )
+  await upgradeHelmChart(argv.celoEnv, helmReleaseName, imageTag)
 
   if (!isCelotoolHelmDryRun()) {
-    await createGrafanaTagAnnotation(argv.celoEnv, imageTag, dbSuffix)
     await createDefaultIngressIfNotExists(argv.celoEnv, helmReleaseName)
   }
 }
