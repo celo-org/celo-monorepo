5db34d57705c564f34df6c3ee76d6446e66eafb3
sawa <jesse.sawa@gmail.com>
2022-10-06 14:00:47 -0400

    Add Escrow Wrapper types and comments Add ASv2 changes to Escrow Wrapper Add Escrow Wrapper tests

---
+++ packages/sdk/contractkit/src/wrappers/Escrow.test.ts
@@ -0,0 +1,105 @@
+import { testWithGanache } from '@celo/dev-utils/lib/ganache-test'
+import { newKitFromWeb3 } from '../kit'
+import { EscrowWrapper } from './Escrow'
+import { FederatedAttestationsWrapper } from './FederatedAttestations'
+import { StableTokenWrapper } from './StableTokenWrapper'
+import { getParsedSignatureOfAddress } from '@celo/protocol/lib/signing-utils'
+
+testWithGanache('Escrow Wrapper', (web3) => {
+  const kit = newKitFromWeb3(web3)
+  const TEN_CUSD = kit.web3.utils.toWei('10', 'ether')
+
+  let accounts: string[] = []
+  let escrow: EscrowWrapper
+  let federatedAttestations: FederatedAttestationsWrapper
+  let stableTokenContract: StableTokenWrapper
+  let identifier: string
+
+  beforeAll(async () => {
+    escrow = await kit.contracts.getEscrow()
+    stableTokenContract = await kit.contracts.getStableToken()
+    federatedAttestations = await kit.contracts.getFederatedAttestations()
+
+    accounts = await web3.eth.getAccounts()
+    kit.defaultAccount = accounts[0]
+
+    identifier = kit.web3.utils.soliditySha3({
+      t: 'bytes32',
+      v: kit.web3.eth.accounts.create().address,
+    }) as string
+  })
+
+  it('transfer with trusted issuers should set TrustedIssuersPerPayment', async () => {
+    const testPaymentId = kit.web3.eth.accounts.create().address
+    const testIssuedOnTimestamp = Math.floor(new Date().getTime() / 1000)
+
+    await federatedAttestations
+      .registerAttestationAsIssuer(
+        identifier as string,
+        kit.defaultAccount as string,
+        testIssuedOnTimestamp
+      )
+      .send()
+
+    const approveTx = await stableTokenContract.approve(escrow.address as string, TEN_CUSD).send()
+    await approveTx.waitReceipt()
+    await escrow
+      .transferWithTrustedIssuers(
+        identifier,
+        stableTokenContract.address,
+        TEN_CUSD,
+        1000,
+        testPaymentId,
+        1,
+        [kit.defaultAccount as string]
+      )
+      .send()
+
+    const trustedIssuersPerPayment = await escrow.getTrustedIssuersPerPayment(testPaymentId)
+
+    expect(trustedIssuersPerPayment[0]).toEqual(kit.defaultAccount)
+  })
+  it('withdraw should be successful after transferWithTrustedIssuers', async () => {
+    const sender: string = accounts[1]
+    const receiver: string = accounts[2]
+    const withdrawKeyAddress: string = accounts[3]
+    const oneDayInSecs: number = 86400
+    const uniquePaymentIDWithdraw = withdrawKeyAddress
+    const parsedSig = await getParsedSignatureOfAddress(web3, receiver, uniquePaymentIDWithdraw)
+
+    const testIssuedOnTimestamp = Math.floor(new Date().getTime() / 1000)
+    await federatedAttestations
+      .registerAttestationAsIssuer(identifier as string, receiver, testIssuedOnTimestamp)
+      .send()
+
+    const senderBalanceBefore = await stableTokenContract.balanceOf(sender)
+    const receiverBalanceBefore = await stableTokenContract.balanceOf(receiver)
+
+    const approveTx = await stableTokenContract
+      .approve(escrow.address as string, TEN_CUSD)
+      .send({ from: sender })
+    await approveTx.waitReceipt()
+
+    await escrow
+      .transferWithTrustedIssuers(
+        identifier,
+        stableTokenContract.address,
+        TEN_CUSD,
+        oneDayInSecs,
+        uniquePaymentIDWithdraw,
+        1,
+        [kit.defaultAccount as string]
+      )
+      .send({ from: sender })
+
+    await escrow
+      .withdraw(uniquePaymentIDWithdraw, parsedSig.v, parsedSig.r, parsedSig.s)
+      .send({ from: receiver })
+
+    const senderBalanceAfter = await stableTokenContract.balanceOf(sender)
+    const receiverBalanceAfter = await stableTokenContract.balanceOf(receiver)
+
+    expect(senderBalanceBefore.minus(+TEN_CUSD)).toEqual(senderBalanceAfter)
+    expect(receiverBalanceBefore.plus(+TEN_CUSD)).toEqual(receiverBalanceAfter)
+  })
+})
