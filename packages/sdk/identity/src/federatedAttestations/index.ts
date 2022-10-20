6f8b83879d8374ea461f125205a7bca865a9c240
sawa <jesse.sawa@gmail.com>
2022-10-20 17:17:28 -0400

    adds getSignatureForAttestation to identity SDK

---
+++ packages/sdk/identity/src/federatedAttestations/index.ts
@@ -0,0 +1,5 @@
+import * as utils from './signatureUtils'
+
+export const FederatedAttestations = {
+  utils,
+}
--- packages/sdk/identity/src/index.ts
+++ packages/sdk/identity/src/index.ts
@@ -1,1 +1,2 @@
 export { OdisUtils } from './odis'
+export { FederatedAttestations } from './federatedAttestations'
