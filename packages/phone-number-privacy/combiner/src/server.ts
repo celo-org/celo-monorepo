f162569c955a83bf7a083bcd555e6637e568ca9d
Aaron DeRuvo <aaron@clabs.co>
2022-10-21 16:22:40 +0200

    explictly enable OPTIONS (and other methods)

--- packages/phone-number-privacy/combiner/src/server.ts
+++ packages/phone-number-privacy/combiner/src/server.ts
@@ -40,6 +40,7 @@ export function startCombiner(config: CombinerConfig, kit?: ContractKit) {
   // enable cross origin resource sharing from any domain so odis can be interacted with from web apps
   app.use(function (_, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST, OPTIONS')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next()
   })
