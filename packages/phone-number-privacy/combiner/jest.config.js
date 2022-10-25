177b84fc8882b60130c931553ab0ad5184ca4fdb
Eela Nagaraj <7308464+eelanagaraj@users.noreply.github.com>
2022-10-25 15:10:50 +0200

    Bump line coverage to 80 and remove TODO

--- packages/phone-number-privacy/combiner/jest.config.js
+++ packages/phone-number-privacy/combiner/jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   collectCoverageFrom: ['./src/**'],
   coverageThreshold: {
     global: {
-      lines: 59, // TODO(2.0.0) make this higher in testing audit ticket (https://github.com/celo-org/celo-monorepo/issues/9811)
+      lines: 80,
     },
   },
 }
