552efa10d10f57c664e07b2a6542444f8959b5da
alecps <alec@cLabs.co>
2022-11-01 17:31:12 -0400

    sdk changes for combiner e2e tests

--- packages/sdk/identity/src/odis/index.ts
+++ packages/sdk/identity/src/odis/index.ts
@@ -2,10 +2,12 @@ import * as BlsBlindingClient from './bls-blinding-client'
 import * as CircuitBreaker from './circuit-breaker'
 import * as PhoneNumberIdentifier from './phone-number-identifier'
 import * as Query from './query'
+import * as Quota from './quota'
 
 export const OdisUtils = {
   BlsBlindingClient,
   Query,
   PhoneNumberIdentifier,
   CircuitBreaker,
+  Quota,
 }
