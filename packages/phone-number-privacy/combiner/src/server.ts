cb47e9523c6901e8b538a19040b85f10b5efa530
alecps <alec@cLabs.co>
2022-11-09 15:52:15 -0500

    fix combiner authentication bug

--- packages/phone-number-privacy/combiner/src/server.ts
+++ packages/phone-number-privacy/combiner/src/server.ts
@@ -44,7 +44,10 @@ export function startCombiner(config: CombinerConfig, kit: ContractKit) {
   app.use((_, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    )
     next()
   })
 
