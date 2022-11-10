739ed99249a673f290b2af36f00473198e223962
Eela Nagaraj <7308464+eelanagaraj@users.noreply.github.com>
2022-11-10 17:09:00 +0100

    Clean up old env vars

--- packages/celotool/src/lib/odis.ts
+++ packages/celotool/src/lib/odis.ts
@@ -18,7 +18,6 @@ const helmChartPath = '../helm-charts/odis'
  */
 interface ODISSignerKeyVaultConfig {
   vaultName: string
-  secretName: string
   pnpKeyNameBase: string
   pnpKeyLatestVersion: string
   domainsKeyNameBase: string
@@ -62,7 +61,6 @@ const contextODISSignerKeyVaultConfigDynamicEnvVars: {
   [k in keyof ODISSignerKeyVaultConfig]: DynamicEnvVar
 } = {
   vaultName: DynamicEnvVar.ODIS_SIGNER_AZURE_KEYVAULT_NAME,
-  secretName: DynamicEnvVar.ODIS_SIGNER_AZURE_KEYVAULT_SECRET_NAME,
   pnpKeyNameBase: DynamicEnvVar.ODIS_SIGNER_AZURE_KEYVAULT_PNP_KEY_NAME_BASE,
   pnpKeyLatestVersion: DynamicEnvVar.ODIS_SIGNER_AZURE_KEYVAULT_PNP_KEY_LATEST_VERSION,
   domainsKeyNameBase: DynamicEnvVar.ODIS_SIGNER_AZURE_KEYVAULT_DOMAINS_KEY_NAME_BASE,
