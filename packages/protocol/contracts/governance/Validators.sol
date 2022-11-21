8df7f99c97d0bc4819458aa433f3889a7f69c4f7
pahor167 <47992132+pahor167@users.noreply.github.com>
2022-11-21 13:21:00 +0100

    validators version bump

--- packages/protocol/contracts/governance/Validators.sol
+++ packages/protocol/contracts/governance/Validators.sol
@@ -166,7 +166,7 @@ contract Validators is
    * @return Patch version of the contract.
    */
   function getVersionNumber() external pure returns (uint256, uint256, uint256, uint256) {
-    return (1, 2, 0, 3);
+    return (1, 2, 0, 4);
   }
 
   /**
