a081f8c47505494310619f978c6670a924b7f51b
Eela Nagaraj <7308464+eelanagaraj@users.noreply.github.com>
2022-12-02 20:47:08 +0100

    Monitor updates

--- packages/phone-number-privacy/monitor/src/index.ts
+++ packages/phone-number-privacy/monitor/src/index.ts
@@ -1,7 +1,35 @@
+import { CombinerEndpointPNP } from '@celo/phone-number-privacy-common'
 import * as functions from 'firebase-functions'
-import { testQuery } from './test'
+import { testDomainsQuery, testPNPQuery } from './test'
 
-export const odisMonitorScheduleFunction = functions
-  .region('us-central1', 'europe-west3')
+// do not deploy this, since this overwrites the ODIS 1.0 monitor
+// export const odisMonitorScheduleFunction = functions
+//   .region('us-central1', 'europe-west3')
+//   .pubsub.schedule('every 5 minutes')
+//   .onRun(testQuery)
+
+const contextName = functions.config().monitor.context_name
+const blockchainProvider = functions.config().blockchain.provider
+if (!contextName || !blockchainProvider) {
+  throw new Error('blockchain provider and context name must be set in function config')
+}
+
+export const odisMonitorScheduleFunctionLegacyPNP = functions
+  .region('us-central1')
+  // .region('us-central1', 'europe-west3')
   .pubsub.schedule('every 5 minutes')
-  .onRun(testQuery)
+  .onRun(async () =>
+    testPNPQuery(blockchainProvider, contextName, CombinerEndpointPNP.LEGACY_PNP_SIGN)
+  )
+
+export const odisMonitorScheduleFunctionPNP = functions
+  .region('us-central1')
+  // .region('us-central1', 'europe-west3')
+  .pubsub.schedule('every 5 minutes')
+  .onRun(async () => testPNPQuery(blockchainProvider, contextName, CombinerEndpointPNP.PNP_SIGN))
+
+export const odisMonitorScheduleFunctionDomains = functions
+  .region('us-central1')
+  // .region('us-central1', 'europe-west3')
+  .pubsub.schedule('every 5 minutes')
+  .onRun(() => testDomainsQuery(contextName))
