0c4e1c3e5e272d27f0ac16d083bb7252be5da5df
Eela Nagaraj <7308464+eelanagaraj@users.noreply.github.com>
2022-12-05 12:21:02 +0100

    Add yarn deploy commands for all envs

--- packages/phone-number-privacy/monitor/src/index.ts
+++ packages/phone-number-privacy/monitor/src/index.ts
@@ -2,18 +2,13 @@ import { CombinerEndpointPNP } from '@celo/phone-number-privacy-common'
 import * as functions from 'firebase-functions'
 import { testDomainsQuery, testPNPQuery } from './test'
 
-// do not deploy this, since this overwrites the ODIS 1.0 monitor
-// export const odisMonitorScheduleFunction = functions
-//   .region('us-central1', 'europe-west3')
-//   .pubsub.schedule('every 5 minutes')
-//   .onRun(testQuery)
-
 const contextName = functions.config().monitor.context_name
 const blockchainProvider = functions.config().blockchain.provider
 if (!contextName || !blockchainProvider) {
   throw new Error('blockchain provider and context name must be set in function config')
 }
 
+// New functions do not overwrite ODIS 1.0 monitor function.
 export const odisMonitorScheduleFunctionLegacyPNP = functions
   .region('us-central1')
   // .region('us-central1', 'europe-west3')
