4acbefea4c8e3b35e6698f8fd1ab66277200302b
Eela Nagaraj <7308464+eelanagaraj@users.noreply.github.com>
2022-12-13 18:04:22 +0100

    Use dropNullable instead

--- packages/phone-number-privacy/signer/src/common/database/migrations/20221213125526_add-constraint-domain-requests.ts
+++ packages/phone-number-privacy/signer/src/common/database/migrations/20221213125526_add-constraint-domain-requests.ts
@@ -1,10 +1,14 @@
 import { Knex } from 'knex'
 import { DOMAIN_REQUESTS_COLUMNS, DOMAIN_REQUESTS_TABLE } from '../models/domain-request'
 
-export async function up(knex: Knex): Promise<void> {
+export async function up(knex: Knex): Promise<any> {
   return knex.schema.alterTable(DOMAIN_REQUESTS_TABLE, (t) => {
-    t.dateTime(DOMAIN_REQUESTS_COLUMNS.timestamp).notNullable().alter()
+    t.dropNullable(DOMAIN_REQUESTS_COLUMNS.timestamp)
   })
 }
 
-export async function down(_knex: Knex): Promise<void> {}
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema.alterTable(DOMAIN_REQUESTS_TABLE, (t) => {
+    t.setNullable(DOMAIN_REQUESTS_COLUMNS.timestamp)
+  })
+}
