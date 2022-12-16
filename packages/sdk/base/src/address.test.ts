5ad5683f17a656652910da0800489335f72985b7
Aaron DeRuvo <aaron@clabs.co>
2022-12-16 09:25:31 -0800

    fix  isNullAddress check (#10077)
    fix  #10045 isNullAddress returning incorrect result due to 0x prefix differences

--- packages/sdk/base/src/address.test.ts
+++ packages/sdk/base/src/address.test.ts
@@ -1,4 +1,4 @@
-import { getAddressChunks } from './address'
+import { getAddressChunks, isNullAddress } from './address'
 
 describe(getAddressChunks, () => {
   test('splits the address into chunks of 4 chars', async () => {
@@ -14,5 +14,26 @@ describe(getAddressChunks, () => {
       'ce10',
       'ce10',
     ])
+  })
+})
+
+describe(isNullAddress, () => {
+  test('returns true for 0x0000000000000000000000000000000000000000', () => {
+    expect(isNullAddress('0x0000000000000000000000000000000000000000')).toBe(true)
+  })
+  test('returns true for 0000000000000000000000000000000000000000', () => {
+    expect(isNullAddress('0000000000000000000000000000000000000000')).toBe(true)
+  })
+
+  test('returns false for 0xce10ce10ce10ce10ce10ce10ce10ce10ce10ce10', () => {
+    expect(isNullAddress('0xce10ce10ce10ce10ce10ce10ce10ce10ce10ce10')).toBe(false)
+  })
+
+  test('returns false for 0x0x0000000000000000000000000000000000000000', () => {
+    expect(isNullAddress('0x0x0000000000000000000000000000000000000000')).toBe(false)
+  })
+
+  test('returns false for 0x000000000000000000000000000000000000ce10', () => {
+    expect(isNullAddress('0x000000000000000000000000000000000000ce10')).toBe(false)
   })
 })
