71b7e070e0cb1f7ae5f6a99e872a2cf957077714
Martin Volpe <martin.volpe@gmail.com>
2023-01-05 16:37:50 -0300

    Fix version

--- packages/protocol/contracts/common/GoldToken.sol
+++ packages/protocol/contracts/common/GoldToken.sol
@@ -56,7 +56,7 @@ contract GoldToken is
    * @return Patch version of the contract.
    */
   function getVersionNumber() external pure returns (uint256, uint256, uint256, uint256) {
-    return (1, 1, 1, 1);
+    return (1, 1, 2, 1);
   }
 
   /**
