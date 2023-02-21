8572a0f978d1aa01a36775ef4be48c3eafdbb204
Eela Nagaraj <7308464+eelanagaraj@users.noreply.github.com>
2023-02-21 18:06:22 +0100

    Expose getIdentifierHash in @celo/base (#10165)
    * WIP: messy draft of removing getPhoneHash
    
    * Remove attestations file from @celo/base
    
    * wip deleting comments
    
    * wip
    
    * Add tests in base and clean up
    
    * More cleanup
    
    * Build entire monorepo in generate-old-devchain-and-build
    
    * Remove sneaky BUILD_DIR definition from normal CK build and add comment
    
    * Update docstrings and comments

--- packages/sdk/base/src/phoneNumbers.test.ts
+++ packages/sdk/base/src/phoneNumbers.test.ts
@@ -1,6 +1,4 @@
-import { soliditySha3 } from 'web3-utils'
-import { getPhoneHash, isE164Number } from './phoneNumbers'
-const sha3 = (v: string) => soliditySha3({ type: 'string', value: v })
+import { isE164Number } from './phoneNumbers'
 
 const TEST_PHONE_NUMBERS = {
   VALID_US_1: '6282287826',
@@ -30,27 +28,6 @@ const TEST_PHONE_NUMBERS = {
 }
 
 describe('Phone number formatting and utilities', () => {
-  describe('Phone hashing', () => {
-    it('Hashes an valid number without a salt', () => {
-      expect(getPhoneHash(sha3, TEST_PHONE_NUMBERS.VALID_E164, '')).toBe(
-        '0x483128504c69591aed5751690805ba9aad6c390644421dc189f6dbb6e085aadf'
-      )
-    })
-    it('Hashes an valid number with a salt', () => {
-      expect(getPhoneHash(sha3, TEST_PHONE_NUMBERS.VALID_E164, 'abcdefg')).toBe(
-        '0xf08257f6b126597dbd090fecf4f5106cfb59c98ef997644cef16f9349464810c'
-      )
-    })
-    it('Throws for an invalid number', () => {
-      try {
-        getPhoneHash(sha3, TEST_PHONE_NUMBERS.VALID_US_1, '')
-        fail('expected an error')
-      } catch (error) {
-        // Error expected
-      }
-    })
-  })
-
   describe('Other phone helper methods', () => {
     it('checks if number is e164', () => {
       // @ts-ignore
