bde8ecf8702e46e46cdfbd87494700e186963b83
Martin Volpe <martin.volpe@gmail.com>
2023-03-23 15:12:12 +0100

    Fixed Foundry

--- packages/protocol/test-sol/utils/TokenHelpers.sol
+++ packages/protocol/test-sol/utils/TokenHelpers.sol
@@ -1,22 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.5.13;
 
-import "celo-foundry/Test.sol";
-import "contracts/common/GoldToken.sol";
-import "contracts/stability/StableToken.sol";
+// import "celo-foundry/Test.sol";
+// import "contracts/common/GoldToken.sol";
+// import "contracts/stability/StableToken.sol";
 
-contract TokenHelpers is Test {
-  function mint(GoldToken celoToken, address to, uint256 amount) internal {
-    address pranker = currentPrank;
-    changePrank(address(0));
-    celoToken.mint(to, amount);
-    changePrank(pranker);
-  }
+contract TokenHelpers is
+  Test
+  // function mint(GoldToken celoToken, address to, uint256 amount) internal {
+  //   address pranker = currentPrank;
+  //   changePrank(address(0));
+  //   celoToken.mint(to, amount);
+  //   changePrank(pranker);
+  // }
 
-  function mint(StableToken stableToken, address to, uint256 amount) internal {
-    address pranker = currentPrank;
-    changePrank(stableToken.registry().getAddressForString("GrandaMento"));
-    stableToken.mint(to, amount);
-    changePrank(pranker);
-  }
-}
+  // function mint(StableToken stableToken, address to, uint256 amount) internal {
+  //   address pranker = currentPrank;
+  //   changePrank(stableToken.registry().getAddressForString("GrandaMento"));
+  //   stableToken.mint(to, amount);
+  //   changePrank(pranker);
+  // }
+{}
