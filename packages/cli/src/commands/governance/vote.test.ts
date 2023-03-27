802ec487c2ef76e7436c2d9b4c5319e7fabd190f
pahor167 <47992132+pahor167@users.noreply.github.com>
2023-03-27 13:30:30 +0200

    Revert "Fixed CLI governance:vote test (#10191)"
    This reverts commit d89656d535f9b17ce6d1c8484d1384cf2909055b.

--- packages/cli/src/commands/governance/vote.test.ts
+++ packages/cli/src/commands/governance/vote.test.ts
@@ -41,6 +41,7 @@ testWithGanache('governance:vote cmd', (web3: Web3) => {
     ])
     await testLocally(Register, ['--from', accounts[0]])
     await testLocally(Lock, ['--from', accounts[0], '--value', '100'])
+    await timeTravel(expConfig.approvalStageDuration, web3)
   })
 
   test('can vote yes', async () => {
