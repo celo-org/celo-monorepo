47dc6d963c38e2348eeed8848bede414f2a45069
boqdan <304771+bowd@users.noreply.github.com>
2023-03-27 09:25:51 +0100

    Improve non-core contract CGP decoding with proxies (#10203)
    This is a follow-up PR to #10019, which improved the tooling around transactions targeting non-core contracts in celocli by using Sourcify to query for metadata. While testing the upcoming Mento upgrade (MU01), we realized that two pieces of missing functionality around proxy initialization are stopping us from decoding the proposal.

--- packages/sdk/explorer/src/base.ts
+++ packages/sdk/explorer/src/base.ts
@@ -1,11 +1,17 @@
 import { concurrentMap } from '@celo/base/lib/async'
-import { AbiItem, Address } from '@celo/connect'
+import { ABIDefinition, AbiItem, Address } from '@celo/connect'
 import { CeloContract, ContractKit } from '@celo/contractkit'
 
 export interface ContractDetails {
   name: string
   address: Address
   jsonInterface: AbiItem[]
+  isCore: boolean
+}
+
+export interface ContractMapping {
+  details: ContractDetails
+  fnMapping: Map<string, ABIDefinition>
 }
 
 export const getContractDetailsFromContract = async (
@@ -18,6 +24,7 @@ export const getContractDetailsFromContract = async (
     name: celoContract,
     address: address ?? contract.options.address,
     jsonInterface: contract.options.jsonInterface,
+    isCore: true,
   }
 }
 
