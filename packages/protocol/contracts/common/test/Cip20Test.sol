b2d05d40de0bec606f5dc51fd90ac6c41626926b
Karl Bartel <karl.bartel@clabs.co>
2023-04-03 09:01:41 +0200

    Add test contracts for CIP-20, CIP-30 and CIP-31

---
+++ packages/protocol/contracts/common/test/Cip20Test.sol
@@ -0,0 +1,37 @@
+//SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity >=0.5.10;
+
+import "../CIP20Lib.sol";
+
+contract Cip20Test {
+  using CIP20Lib for bytes;
+
+  function sha3_256(bytes calldata input) external view returns (bytes memory) {
+    return input.sha3_256();
+  }
+
+  function sha3_512(bytes calldata input) external view returns (bytes memory) {
+    return input.sha3_512();
+  }
+
+  function keccak512(bytes calldata input) external view returns (bytes memory) {
+    return input.keccak512();
+  }
+
+  function sha2_512(bytes calldata input) external view returns (bytes memory) {
+    return input.sha2_512();
+  }
+
+  function blake2sWithConfig(bytes32 config, bytes calldata key, bytes calldata preimage)
+    external
+    view
+    returns (bytes memory)
+  {
+    return CIP20Lib.blake2sWithConfig(config, key, preimage);
+  }
+
+  // default settings, no key
+  function blake2s(bytes calldata input) external view returns (bytes memory) {
+    return input.blake2s();
+  }
+}
