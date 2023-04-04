44f1e1ea023468c2f32c100ccbceb3e892083253
Karl Bartel <karl.bartel@clabs.co>
2023-04-04 14:34:58 +0200

    Fix wrong lib usage in BLS12Passthrough

--- packages/protocol/contracts/common/libraries/test/BLS12Passthrough.sol
+++ packages/protocol/contracts/common/libraries/test/BLS12Passthrough.sol
@@ -209,7 +209,7 @@ contract BLS12_381Passthrough is Common {
       input[i].scalar = ref.indexUint(idx + 0x80, 32);
     }
 
-    return B12_377Lib.g1MultiExp(input).serializeG1();
+    return B12_381Lib.g1MultiExp(input).serializeG1();
   }
 
   function g2Add(bytes calldata args) external view returns (bytes memory) {
