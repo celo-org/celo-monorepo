a8c32886d69301d98179bfa180369cda963de7ff
soloseng <102702451+soloseng@users.noreply.github.com>
2023-04-06 14:55:07 -0400

    Merge branch 'soloseng/ast-version_fix'

--- packages/protocol/test/compatibility/ast-version.ts
+++ packages/protocol/test/compatibility/ast-version.ts
@@ -1,30 +1,27 @@
-// TODO: (soloseng): find source of error: <Error: Cannot find module 'c-kzg'>\
-// issue may be comming from @ethereumjs/vm package runCall
+import { getContractVersion } from '@celo/protocol/lib/compatibility/ast-version'
+import { DEFAULT_VERSION_STRING } from '@celo/protocol/lib/compatibility/version'
+import { getTestArtifacts } from '@celo/protocol/test/compatibility/common'
+import { assert } from 'chai'
 
-// import { getContractVersion } from '@celo/protocol/lib/compatibility/ast-version'
-// import { DEFAULT_VERSION_STRING } from '@celo/protocol/lib/compatibility/version'
-// import { getTestArtifacts } from '@celo/protocol/test/compatibility/common'
-// import { assert } from 'chai'
-
-// const testCases = {
-//   original: getTestArtifacts('original'),
-//   versioned: getTestArtifacts('versioned'),
-// }
+const testCases = {
+  original: getTestArtifacts('original'),
+  versioned: getTestArtifacts('versioned'),
+}
 
 describe('#getContractVersion()', () => {
   describe('when the contract implements getVersionNumber()', () => {
-    //   xit('returns the correct version number', async () => {
-    //     const version = await getContractVersion(
-    //       testCases.versioned.getArtifactByName('TestContract')
-    //     )
-    //     assert.equal(version.toString(), '1.2.3.4')
-    //   })
+    it('returns the correct version number', async () => {
+      const version = await getContractVersion(
+        testCases.versioned.getArtifactByName('TestContract')
+      )
+      assert.equal(version.toString(), '1.2.3.4')
+    })
   })
 
   describe('when the contract does not implement getVersionNumber()', () => {
-    // xit('returns the default version number', async () => {
-    //   const version = await getContractVersion(testCases.original.getArtifactByName('TestContract'))
-    //   assert.equal(version.toString(), DEFAULT_VERSION_STRING)
-    // })
+    it('returns the default version number', async () => {
+      const version = await getContractVersion(testCases.original.getArtifactByName('TestContract'))
+      assert.equal(version.toString(), DEFAULT_VERSION_STRING)
+    })
   })
 })
