9c47cbe9fa9b6e617681db22b4d677392089e1f4
Martín Volpe <volpe@clabs.co>
2023-06-09 15:14:56 +0200

    Removing Mento contracts (#10124)

--- packages/protocol/migrations/10_0_exchange_USD.ts
+++ packages/protocol/migrations/10_0_exchange_USD.ts
@@ -7,7 +7,9 @@ import {
 } from '@celo/protocol/lib/web3-utils'
 import { config } from '@celo/protocol/migrationsConfig'
 import { toFixed } from '@celo/utils/lib/fixidity'
-import { ExchangeInstance, FreezerInstance } from 'types'
+import { FreezerInstance } from 'types'
+import { ExchangeInstance } from 'types/mento'
+import { MENTO_PACKAGE } from '../contractPackages'
 
 const initializeArgs = async (): Promise<any[]> => {
   return [
@@ -34,5 +36,6 @@ module.exports = deploymentForCoreContract<ExchangeInstance>(
       await freezer.freeze(exchange.address)
     }
     await exchange.activateStable()
-  }
+  },
+  MENTO_PACKAGE
 )
