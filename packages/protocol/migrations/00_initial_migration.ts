97d034e6dff71cc36a31fc981960cafaaeaaa298
Martín Volpe <martin.volpe@gmail.com>
2023-06-09 13:51:20 +0200

    mini lint

--- packages/protocol/migrations/00_initial_migration.ts
+++ packages/protocol/migrations/00_initial_migration.ts
@@ -6,8 +6,8 @@ module.exports = async (deployer: any, network: any) => {
   deployer.deploy(Migrations)
 
   const currentNetwork = { ...networks[network], name: network }
-  // tslint:disable-next-line
 
+  // tslint:disable-next-line
   console.log('Current network is', JSON.stringify(currentNetwork))
   // Instad of setting this in a singleton, it could have been set in every migration
   // but it would have required quite a lot of refactoring
