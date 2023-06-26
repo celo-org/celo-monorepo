f7b56b8d79652e0ce4d7528c6edcd4f2b0891411
Martín Volpe <volpe@clabs.co>
2023-06-26 19:15:16 +0200

    Update GoldToken.sol

--- packages/protocol/contracts/common/GoldToken.sol
+++ packages/protocol/contracts/common/GoldToken.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.5.13;
 
+// just retrigger
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
