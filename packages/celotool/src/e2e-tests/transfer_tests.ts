02812d7a4801b2bf97bd650036eb9336f48c5a56
Karl Bartel <karl.bartel@clabs.co>
2023-06-27 14:05:40 +0200

    transfer_tests: adapt to UltraGreen CIP-52

--- packages/celotool/src/e2e-tests/transfer_tests.ts
+++ packages/celotool/src/e2e-tests/transfer_tests.ts
@@ -203,7 +203,7 @@ describe('Transfer tests', function (this: any) {
 
   let currentGethInstance: GethInstanceConfig
 
-  let governanceAddress: string // set later on using the contract itself
+  let feeHandlerAddress: string // set later on using the contract itself
   const validatorAddress = '0x47e172f6cfb6c7d01c1574fa3e2be7cc73269d95'
   const DEF_FROM_PK = 'f2f48ee19680706196e2e339e5da3491186e0c4c5030670656b0e0164837257d'
   const FromAddress = '0x5409ed021d9299bf6814279a6a1411a7e866a631'
@@ -284,17 +284,9 @@ describe('Transfer tests', function (this: any) {
       3
     )
 
-    governanceAddress = (await kit._web3Contracts.getGovernance()).options.address
-    // The tests below check the balance of the governance contract (i.e. the community fund)
+    feeHandlerAddress = (await kit._web3Contracts.getFeeHandler()).options.address
+    // The tests below check the balance of the FeeHandler contract (See UltraGreen CIP-52)
     // before and after transactions to verify the correct amount has been received from the fees.
-    // This causes flakiness due to the fund also receiving epoch rewards (if the epoch change is
-    // between the blocks the balance checker uses as its before and after the test will fail due
-    // to the unexpected change from the epoch rewards).
-    // To avoid this, we set the community fund's fraction of epoch rewards to zero.
-    // Another option would have been to make the epoch size large enough so no epoch changes happen
-    // during the test.
-    const epochRewards = await kit._web3Contracts.getEpochRewards()
-    await epochRewards.methods.setCommunityRewardFraction(0).send({ from: validatorAddress })
 
     // Give the account we will send transfers as sufficient gold and dollars.
     const startBalance = TransferAmount.times(800)
@@ -579,7 +571,7 @@ describe('Transfer tests', function (this: any) {
         toAddress,
         txFeeRecipientAddress,
         gatewayFeeRecipientAddress,
-        governanceAddress,
+        feeHandlerAddress,
       ]
       balances = await newBalanceWatcher(kit, accounts)
 
@@ -607,7 +599,7 @@ describe('Transfer tests', function (this: any) {
         if (kit.celoTokens.isStableToken(feeToken)) {
           assert(
             txRes.events.find(
-              (a) => eqAddress(a.to, governanceAddress) && eqAddress(a.from, fromAddress)
+              (a) => eqAddress(a.to, feeHandlerAddress) && eqAddress(a.from, fromAddress)
             )
           )
         }
@@ -651,8 +643,8 @@ describe('Transfer tests', function (this: any) {
     it(`should increment the gateway fee recipient's ${feeToken} balance by the gateway fee`, () =>
       assertEqualBN(balances.delta(gatewayFeeRecipientAddress, feeToken), txRes.fees.gateway))
 
-    it(`should increment the infrastructure fund's ${feeToken} balance by the base portion of the gas fee`, () =>
-      assertEqualBN(balances.delta(governanceAddress, feeToken), txRes.fees.base))
+    it(`should increment the FeeHandler's ${feeToken} balance by the base portion of the gas fee`, () =>
+      assertEqualBN(balances.delta(feeHandlerAddress, feeToken), txRes.fees.base))
 
     it(`should increment the tx fee recipient's ${feeToken} balance by the rest of the gas fee`, () => {
       assertEqualBN(balances.delta(txFeeRecipientAddress, feeToken), txRes.fees.tip)
@@ -877,7 +869,7 @@ describe('Transfer tests', function (this: any) {
                 FromAddress,
                 ToAddress,
                 gatewayFeeRecipientAddress,
-                governanceAddress,
+                feeHandlerAddress,
               ])
 
               await inflationManager.setInflationRateForNextTransfer(new BigNumber(2))
@@ -926,11 +918,11 @@ describe('Transfer tests', function (this: any) {
               )
             })
 
-            it("should halve the infrastructure fund's Celo Dollar balance then increment it by the base portion of the gas fees", () => {
+            it("should halve the FeeHandler's Celo Dollar balance then increment it by the base portion of the gas fees", () => {
               assertEqualBN(
                 balances
-                  .current(governanceAddress, StableToken.cUSD)
-                  .minus(balances.initial(governanceAddress, StableToken.cUSD).idiv(2)),
+                  .current(feeHandlerAddress, StableToken.cUSD)
+                  .minus(balances.initial(feeHandlerAddress, StableToken.cUSD).idiv(2)),
                 expectedFees.base
               )
             })
