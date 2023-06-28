29d3a0a01ddc15d56902dc7f38462c29e05690d1
Martín Volpe <volpe@clabs.co>
2023-06-28 16:00:00 +0200

    Update GoldToken.sol

--- packages/protocol/contracts/common/GoldToken.sol
+++ packages/protocol/contracts/common/GoldToken.sol
@@ -10,6 +10,7 @@ import "./Initializable.sol";
 import "./interfaces/ICeloToken.sol";
 import "../common/interfaces/ICeloVersionedContract.sol";
 
+// trigger CI, don't break lint
 contract GoldToken is
   Initializable,
   CalledByVm,
