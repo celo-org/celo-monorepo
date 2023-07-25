643bcf2656fb90c3fa885a79c938d4d3f3b6eb8a
soloseng <102702451+soloseng@users.noreply.github.com>
2023-07-25 16:43:27 -0400

    removed flake-tracker

--- packages/phone-number-privacy/combiner/jest.config.js
+++ packages/phone-number-privacy/combiner/jest.config.js
@@ -1,9 +1,5 @@
-const { nodeFlakeTracking } = require('@celo/flake-tracker/src/jest/config.js')
-
 module.exports = {
   preset: 'ts-jest',
-  ...nodeFlakeTracking,
-  setupFilesAfterEnv: [...nodeFlakeTracking.setupFilesAfterEnv],
   coverageReporters: [['lcov', { projectRoot: '../../../' }], 'text'],
   collectCoverageFrom: ['./src/**'],
   coverageThreshold: {
--- packages/sdk/identity/jest.config.js
+++ packages/sdk/identity/jest.config.js
@@ -1,11 +1,8 @@
-const { nodeFlakeTracking } = require('@celo/flake-tracker/src/jest/config.js')
-
 module.exports = {
   preset: 'ts-jest',
-  ...nodeFlakeTracking,
   rootDir: './src/',
   testMatch: ['<rootDir>/**/?(*.)+(spec|test).ts?(x)'],
-  setupFilesAfterEnv: ['@celo/dev-utils/lib/matchers', ...nodeFlakeTracking.setupFilesAfterEnv],
+  setupFilesAfterEnv: ['@celo/dev-utils/lib/matchers'],
   globalSetup: '<rootDir>/test-utils/setup.global.ts',
   globalTeardown: '<rootDir>/test-utils/teardown.global.ts',
   verbose: true,
