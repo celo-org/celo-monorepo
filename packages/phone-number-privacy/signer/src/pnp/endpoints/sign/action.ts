581bb979472e5b3d429a465ddb4d7b41d681d9a0
jessan <107156407+jessan-clabs@users.noreply.github.com>
2023-08-03 11:10:25 -0700

    Rough sketch of signer database cleanup

--- packages/phone-number-privacy/signer/src/pnp/endpoints/sign/action.ts
+++ packages/phone-number-privacy/signer/src/pnp/endpoints/sign/action.ts
@@ -34,77 +34,16 @@ export class PnpSignAction implements Action<SignMessageRequest> {
   ): Promise<void> {
     // Compute quota lookup, update, and signing within transaction
     // so that these occur atomically and rollback on error.
-    await this.db.transaction(async (trx) => {
-      const pnpSignHandler = async () => {
-        const quotaStatus = await this.quota.getQuotaStatus(session, trx)
+    const hasQuota = true;  // TODO - check cached data on this.
 
-        let isDuplicateRequest = false
-        try {
-          isDuplicateRequest = await getRequestExists(
-            this.db,
-            this.requestsTable,
-            session.request.body.account,
-            session.request.body.blindedQueryPhoneNumber,
-            session.logger,
-            trx
-          )
-        } catch (err) {
-          session.logger.error(err, 'Failed to check if request already exists in db')
+        if (!hasQuota) {
+             this.io.sendFailure(
+               WarningMessage.EXCEEDED_QUOTA, 
+               403, 
+               session.response, 
+               "some quota status -- don't hit DB or node")
+             return
         }
-
-        if (isDuplicateRequest) {
-          Counters.duplicateRequests.inc()
-          session.logger.info(
-            'Request already exists in db. Will service request without charging quota.'
-          )
-          session.errors.push(WarningMessage.DUPLICATE_REQUEST_TO_GET_PARTIAL_SIG)
-        } else {
-          // In the case of a database connection failure, performedQueryCount will be -1
-          if (quotaStatus.performedQueryCount === -1) {
-            this.io.sendFailure(
-              ErrorMessage.DATABASE_GET_FAILURE,
-              500,
-              session.response,
-              quotaStatus
-            )
-            return
-          }
-          // In the case of a blockchain connection failure, totalQuota will be -1
-          if (quotaStatus.totalQuota === -1) {
-            if (this.io.shouldFailOpen) {
-              // We fail open and service requests on full-node errors to not block the user.
-              // Error messages are stored in the session and included along with the signature in the response.
-              quotaStatus.totalQuota = Number.MAX_SAFE_INTEGER
-              session.logger.warn(
-                { warning: ErrorMessage.FAILURE_TO_GET_TOTAL_QUOTA },
-                ErrorMessage.FAILING_OPEN
-              )
-              Counters.requestsFailingOpen.inc()
-            } else {
-              session.logger.warn(
-                { warning: ErrorMessage.FAILURE_TO_GET_TOTAL_QUOTA },
-                ErrorMessage.FAILING_CLOSED
-              )
-              Counters.requestsFailingClosed.inc()
-              this.io.sendFailure(ErrorMessage.FULL_NODE_ERROR, 500, session.response, quotaStatus)
-              return
-            }
-          }
-
-          // TODO(after 2.0.0) add more specific error messages on DB and key version
-          // https://github.com/celo-org/celo-monorepo/issues/9882
-          // quotaStatus is updated in place; throws on failure to update
-          const { sufficient } = await this.quota.checkAndUpdateQuotaStatus(
-            quotaStatus,
-            session,
-            trx
-          )
-          if (!sufficient) {
-            this.io.sendFailure(WarningMessage.EXCEEDED_QUOTA, 403, session.response, quotaStatus)
-            return
-          }
-        }
-
         const key: Key = {
           version:
             getRequestKeyVersion(session.request, session.logger) ??
@@ -122,7 +61,6 @@ export class PnpSignAction implements Action<SignMessageRequest> {
           return
         } catch (err) {
           session.logger.error({ err })
-          quotaStatus.performedQueryCount--
           this.io.sendFailure(
             ErrorMessage.SIGNATURE_COMPUTATION_FAILURE,
             500,
@@ -130,10 +68,83 @@ export class PnpSignAction implements Action<SignMessageRequest> {
             quotaStatus
           )
           // Note that errors thrown after rollback will have no effect, hence doing this last
-          await trx.rollback()
           return
         }
       }
+  
+    // Do this quota update stuff stuff async at the end, only if signiture success.
+    // Update the "cached" value at the end.
+    // async updateQuota (don't block)    
+    // await this.db.transaction(async (trx) => {
+    //   const pnpSignHandler = async () => {
+    //     const quotaStatus = await this.quota.getQuotaStatus(session, trx)
+
+    //     let isDuplicateRequest = false
+    //     try {
+    //       isDuplicateRequest = await getRequestExists(
+    //         this.db,
+    //         this.requestsTable,
+    //         session.request.body.account,
+    //         session.request.body.blindedQueryPhoneNumber,
+    //         session.logger,
+    //         trx
+    //       )
+    //     } catch (err) {
+    //       session.logger.error(err, 'Failed to check if request already exists in db')
+    //     }
+
+    //     if (isDuplicateRequest) {
+    //       Counters.duplicateRequests.inc()
+    //       session.logger.info(
+    //         'Request already exists in db. Will service request without charging quota.'
+    //       )
+    //       session.errors.push(WarningMessage.DUPLICATE_REQUEST_TO_GET_PARTIAL_SIG)
+    //     } else {
+    //       // In the case of a database connection failure, performedQueryCount will be -1
+    //       if (quotaStatus.performedQueryCount === -1) {
+    //         this.io.sendFailure(
+    //           ErrorMessage.DATABASE_GET_FAILURE,
+    //           500,
+    //           session.response,
+    //           quotaStatus
+    //         )
+    //         return
+    //       }
+    //       // In the case of a blockchain connection failure, totalQuota will be -1
+    //       if (quotaStatus.totalQuota === -1) {
+    //         if (this.io.shouldFailOpen) {
+    //           // We fail open and service requests on full-node errors to not block the user.
+    //           // Error messages are stored in the session and included along with the signature in the response.
+    //           quotaStatus.totalQuota = Number.MAX_SAFE_INTEGER
+    //           session.logger.warn(
+    //             { warning: ErrorMessage.FAILURE_TO_GET_TOTAL_QUOTA },
+    //             ErrorMessage.FAILING_OPEN
+    //           )
+    //           Counters.requestsFailingOpen.inc()
+    //         } else {
+    //           session.logger.warn(
+    //             { warning: ErrorMessage.FAILURE_TO_GET_TOTAL_QUOTA },
+    //             ErrorMessage.FAILING_CLOSED
+    //           )
+    //           Counters.requestsFailingClosed.inc()
+    //           this.io.sendFailure(ErrorMessage.FULL_NODE_ERROR, 500, session.response, quotaStatus)
+    //           return
+    //         }
+    //       }
+
+    //       // TODO(after 2.0.0) add more specific error messages on DB and key version
+    //       // https://github.com/celo-org/celo-monorepo/issues/9882
+    //       // quotaStatus is updated in place; throws on failure to update
+    //X       const { sufficient } = await this.quota.checkAndUpdateQuotaStatus(
+    //X        quotaStatus,
+    //X         session,
+    // X        trx
+    //       )
+    // X      if (!sufficient) {
+    //  X       this.io.sendFailure(WarningMessage.EXCEEDED_QUOTA, 403, session.response, quotaStatus)
+    //   X      return
+    //    X   }
+    // }
       await timeout(pnpSignHandler, [], this.config.timeout, timeoutError)
     })
   }
