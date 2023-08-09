7c12e3c42b83b289b1653985d0be6a8cdf1dd724
alvarof2 <fernandez.f.alvaro@gmail.com>
2023-08-09 09:37:22 +0200

    knex instrumentation

--- packages/phone-number-privacy/signer/src/pnp/endpoints/sign/action.ts
+++ packages/phone-number-privacy/signer/src/pnp/endpoints/sign/action.ts
@@ -36,7 +36,7 @@ export class PnpSignAction implements Action<SignMessageRequest> {
     session: PnpSession<SignMessageRequest>,
     timeoutError: symbol
   ): Promise<void> {
-    tracer.startActiveSpan('pnpSignIO - Perform', async (span) => {
+    tracer.startActiveSpan('pnpSignIO - perform', async (span) => {
       span.addEvent('Calling transaction')
       // Compute quota lookup, update, and signing within transaction
       // so that these occur atomically and rollback on error.
