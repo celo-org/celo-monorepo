df423f91cfc9a110dcea251a7b6dbbaad327d3b6
alvarof2 <fernandez.f.alvaro@gmail.com>
2023-08-16 15:09:39 +0200

    Combiner tracing

---
+++ packages/phone-number-privacy/combiner/src/tracing.ts
@@ -0,0 +1,59 @@
+/*tracing.js*/
+import { JaegerExporter } from '@opentelemetry/exporter-jaeger'
+import { registerInstrumentations } from '@opentelemetry/instrumentation'
+import { Resource } from '@opentelemetry/resources'
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base'
+import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
+
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
+// const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express')
+// const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http')
+
+const options = {
+  tags: [],
+  endpoint: process.env.TRACER_ENDPOINT,
+}
+
+// Optionally register instrumentation libraries
+registerInstrumentations({
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      '@opentelemetry/instrumentation-http': {
+        startIncomingSpanHook: (req) => {
+          delete req.headers.traceparent
+          delete req.headers[`x-cloud-trace-context`]
+          delete req.headers[`grpc-trace-bin`]
+
+          return {}
+        },
+      },
+    }),
+  ],
+})
+
+const resource = Resource.default().merge(
+  new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: process.env.TRACING_SERVICE_NAME,
+    [SemanticResourceAttributes.SERVICE_VERSION]: '0.1.0',
+  })
+)
+
+const provider = new NodeTracerProvider({
+  resource,
+})
+const exporter = new JaegerExporter(options)
+const processor = new BatchSpanProcessor(exporter)
+provider.addSpanProcessor(processor)
+
+provider.register()
+
+// await exporter.shutdown();
+
+// process.stdin.resume();//so the program will not close instantly
+
+async function exit() {
+  await exporter.forceFlush()
+}
+
+module.exports = { exit }
