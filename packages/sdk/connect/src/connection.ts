f492e1935e8af63f6913a35f5dcb11f26d6df571
Aaron DeRuvo <aaron@clabs.co>
2023-08-18 15:05:56 -0500

    logging

--- packages/sdk/connect/src/connection.ts
+++ packages/sdk/connect/src/connection.ts
@@ -1,11 +1,11 @@
 import { ensureLeading0x, toChecksumAddress } from '@celo/utils/lib/address'
 import { EIP712TypedData, generateTypedDataHash } from '@celo/utils/lib/sign-typed-data-utils'
-import { parseSignatureWithoutPrefix, Signature } from '@celo/utils/lib/signatureUtils'
+import { Signature, parseSignatureWithoutPrefix } from '@celo/utils/lib/signatureUtils'
 import { bufferToHex } from '@ethereumjs/util'
 import debugFactory from 'debug'
 import Web3 from 'web3'
 import { AbiCoder } from './abi-types'
-import { assertIsCeloProvider, CeloProvider } from './celo-provider'
+import { CeloProvider, assertIsCeloProvider } from './celo-provider'
 import {
   Address,
   Block,
@@ -32,9 +32,9 @@ import {
   outputCeloTxReceiptFormatter,
 } from './utils/formatter'
 import { hasProperty } from './utils/provider-utils'
-import { getRandomId, HttpRpcCaller, RpcCaller } from './utils/rpc-caller'
+import { HttpRpcCaller, RpcCaller, getRandomId } from './utils/rpc-caller'
 import { TxParamsNormalizer } from './utils/tx-params-normalizer'
-import { toTxResult, TransactionResult } from './utils/tx-result'
+import { TransactionResult, toTxResult } from './utils/tx-result'
 import { ReadOnlyWallet } from './wallet'
 
 const debugGasEstimation = debugFactory('connection:gas-estimation')
@@ -423,7 +423,7 @@ export class Connection {
   gasPrice = async (feeCurrency?: Address): Promise<string> => {
     // Required otherwise is not backward compatible
     const parameter = feeCurrency ? [feeCurrency] : []
-
+    console.info('looking up gas price with', parameter)
     // Reference: https://eth.wiki/json-rpc/API#eth_gasprice
     const response = await this.rpcCaller.call('eth_gasPrice', parameter)
     const gasPriceInHex = response.result.toString()
