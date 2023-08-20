97e951a206347f5a6732f578d8cce4ff0d93c760
alecps <alec@cLabs.co>
2023-08-19 22:22:16 -0400

    (to revert) mock db return values for testing

--- packages/phone-number-privacy/signer/src/common/database/wrappers/request.ts
+++ packages/phone-number-privacy/signer/src/common/database/wrappers/request.ts
@@ -1,46 +1,48 @@
-import { ErrorMessage } from '@celo/phone-number-privacy-common'
+// import { ErrorMessage } from '@celo/phone-number-privacy-common'
 import Logger from 'bunyan'
 import { Knex } from 'knex'
-import { config } from '../../../config'
-import {
-  PnpSignRequestRecord,
-  REQUESTS_COLUMNS,
-  REQUESTS_TABLE,
-  toPnpSignRequestRecord,
-} from '../models/request'
-import { doMeteredSql } from '../utils'
+// import { config } from '../../../config'
+// import {
+//   PnpSignRequestRecord,
+//   // REQUESTS_COLUMNS,
+//   REQUESTS_TABLE,
+//   toPnpSignRequestRecord,
+// } from '../models/request'
+// import { doMeteredSql } from '../utils'
 
 export async function getRequestExists( // TODO try insert, if primary key error, then duplicate request
-  db: Knex,
-  account: string,
-  blindedQuery: string,
-  logger: Logger
+  _db: Knex,
+  _account: string,
+  _blindedQuery: string,
+  _logger: Logger
 ): Promise<boolean> {
-  logger.debug(`Checking if request exists for account: ${account}, blindedQuery: ${blindedQuery}`)
-  return doMeteredSql('getRequestExists', ErrorMessage.DATABASE_GET_FAILURE, logger, async () => {
-    const existingRequest = await db<PnpSignRequestRecord>(REQUESTS_TABLE)
-      .where({
-        [REQUESTS_COLUMNS.address]: account,
-        [REQUESTS_COLUMNS.blindedQuery]: blindedQuery, // TODO are we using the primary key correctly??
-      })
-      .first()
-      .timeout(config.db.timeout)
-    return !!existingRequest // TODO use EXISTS query??
-  })
+  return Promise.resolve(false)
+  // logger.debug(`Checking if request exists for account: ${account}, blindedQuery: ${blindedQuery}`)
+  // return doMeteredSql('getRequestExists', ErrorMessage.DATABASE_GET_FAILURE, logger, async () => {
+  //   const existingRequest = await db<PnpSignRequestRecord>(REQUESTS_TABLE)
+  //     .where({
+  //       [REQUESTS_COLUMNS.address]: account,
+  //       [REQUESTS_COLUMNS.blindedQuery]: blindedQuery, // TODO are we using the primary key correctly??
+  //     })
+  //     .first()
+  //     .timeout(config.db.timeout)
+  //   return !!existingRequest // TODO use EXISTS query??
+  // })
 }
 
 export async function insertRequest(
-  db: Knex,
-  account: string,
-  blindedQuery: string,
-  logger: Logger,
-  trx?: Knex.Transaction
+  _db: Knex,
+  _account: string,
+  _blindedQuery: string,
+  _logger: Logger,
+  _trx?: Knex.Transaction
 ): Promise<void> {
-  logger.debug(`Storing salt request for: ${account}, blindedQuery: ${blindedQuery}`)
-  return doMeteredSql('insertRequest', ErrorMessage.DATABASE_INSERT_FAILURE, logger, async () => {
-    const sql = db<PnpSignRequestRecord>(REQUESTS_TABLE)
-      .insert(toPnpSignRequestRecord(account, blindedQuery))
-      .timeout(config.db.timeout)
-    await (trx != null ? sql.transacting(trx) : sql)
-  })
+  return
+  // logger.debug(`Storing salt request for: ${account}, blindedQuery: ${blindedQuery}`)
+  // return doMeteredSql('insertRequest', ErrorMessage.DATABASE_INSERT_FAILURE, logger, async () => {
+  //   const sql = db<PnpSignRequestRecord>(REQUESTS_TABLE)
+  //     .insert(toPnpSignRequestRecord(account, blindedQuery))
+  //     .timeout(config.db.timeout)
+  //   await (trx != null ? sql.transacting(trx) : sql)
+  // })
 }
