577bee26e9f096245ad207ad31006b3ea4840196
alecps <alec@cLabs.co>
2023-08-23 18:30:56 -0400

    fix lint errors

--- packages/phone-number-privacy/signer/src/common/database/wrappers/domain-state.ts
+++ packages/phone-number-privacy/signer/src/common/database/wrappers/domain-state.ts
@@ -4,9 +4,9 @@ import Logger from 'bunyan'
 import { Knex } from 'knex'
 import { config } from '../../../config'
 import {
-  DomainStateRecord,
   DOMAIN_STATE_COLUMNS,
   DOMAIN_STATE_TABLE,
+  DomainStateRecord,
   toDomainStateRecord,
 } from '../models/domain-state'
 import { doMeteredSql } from '../utils'
