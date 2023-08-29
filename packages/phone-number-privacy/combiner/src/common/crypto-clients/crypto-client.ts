39369e29dd56cfc81e577904e4fba6b9f04e27b8
alecps <alec@cLabs.co>
2023-08-29 14:42:03 -0400

    misc cosmetic changes from review

--- packages/phone-number-privacy/combiner/src/common/crypto-clients/crypto-client.ts
+++ packages/phone-number-privacy/combiner/src/common/crypto-clients/crypto-client.ts
@@ -15,7 +15,6 @@ export abstract class CryptoClient {
   /**
    * Returns true if the number of valid signatures is enough to perform a combination
    */
-  // TODO (mcortesi) remove
   public hasSufficientSignatures(): boolean {
     return this.allSignaturesLength >= this.keyVersionInfo.threshold
   }
