66b1fb3755f90fd73593b40e2a121ed8343e9c3b
Gaston Ponti <ponti@clabs.co>
2023-09-07 00:45:33 -0300

    Refactor combiner (#10533)
    * Refactor combiner
    
    * Remove combiner timeouts
    
    * fix lint
    
    ---------
    
    Co-authored-by: Alec Schaefer <alec@cLabs.co>

--- packages/phone-number-privacy/combiner/src/index.ts
+++ packages/phone-number-privacy/combiner/src/index.ts
@@ -1,4 +1,3 @@
-import { getContractKitWithAgent } from '@celo/phone-number-privacy-common'
 import * as functions from 'firebase-functions'
 import config from './config'
 import { startCombiner } from './server'
@@ -12,5 +11,6 @@ export const combiner = functions
     // Defined check required for running tests vs. deployment
     minInstances: functions.config().service ? Number(functions.config().service.min_instances) : 0,
   })
-  .https.onRequest(startCombiner(config, getContractKitWithAgent(config.blockchain)))
+  .https.onRequest(startCombiner(config))
+
 export * from './config'
