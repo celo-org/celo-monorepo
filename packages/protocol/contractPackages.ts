a4a9e637ac009b6635ae7516816dd90f66078e86
Martín Volpe <martin.volpe@gmail.com>
2023-09-14 20:06:12 -0300

    Added issue contractPackages.ts

--- packages/protocol/contractPackages.ts
+++ packages/protocol/contractPackages.ts
@@ -45,14 +45,10 @@ export const SOLIDITY_08_PACKAGE: ContractPackage = {
   contracstFolder: '',
   folderPath: '',
   name: '0.8',
-  proxiesPath: '/', // Proxies are still with 0.8 contracts
-  contracts: [
-    // TODO make this automatic?
-    'GasPriceMinimum',
-  ],
-  proxyContracts: [
-    // TODO make this automatic?
-    'GasPriceMinimumProxy',
-  ],
+  proxiesPath: '/', // Proxies are still with 0.5 contracts
+  // Proxies shouldn't have to be added to a list manually
+  // https://github.com/celo-org/celo-monorepo/issues/10555
+  contracts: ['GasPriceMinimum'],
+  proxyContracts: ['GasPriceMinimumProxy'],
   truffleConfig: 'truffle-config0.8.js',
 }
