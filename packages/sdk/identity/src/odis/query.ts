4df1a492c26d392da418e60afe447e2d0cbce32a
soloseng <102702451+soloseng@users.noreply.github.com>
2023-09-21 12:27:38 -0400

    deployed gen2 on alfajores

--- packages/sdk/identity/src/odis/query.ts
+++ packages/sdk/identity/src/odis/query.ts
@@ -58,13 +58,13 @@ export const ODIS_STAGING_CONTEXT: ServiceContext = {
 }
 
 export const ODIS_ALFAJORES_CONTEXT_PNP: ServiceContext = {
-  odisUrl: 'https://us-central1-celo-phone-number-privacy.cloudfunctions.net/combiner',
+  odisUrl: 'https://us-central1-celo-phone-number-privacy.cloudfunctions.net/combinerGen2',
   odisPubKey:
     'kPoRxWdEdZ/Nd3uQnp3FJFs54zuiS+ksqvOm9x8vY6KHPG8jrfqysvIRU0wtqYsBKA7SoAsICMBv8C/Fb2ZpDOqhSqvr/sZbZoHmQfvbqrzbtDIPvUIrHgRS0ydJCMsA',
 }
 
 export const ODIS_ALFAJORES_CONTEXT_DOMAINS: ServiceContext = {
-  odisUrl: 'https://us-central1-celo-phone-number-privacy.cloudfunctions.net/combiner',
+  odisUrl: 'https://us-central1-celo-phone-number-privacy.cloudfunctions.net/combinerGen2',
   odisPubKey:
     '+ZrxyPvLChWUX/DyPw6TuGwQH0glDJEbSrSxUARyP5PuqYyP/U4WZTV1e0bAUioBZ6QCJMiLpDwTaFvy8VnmM5RBbLQUMrMg5p4+CBCqj6HhsMfcyUj8V0LyuNdStlCB',
 }
