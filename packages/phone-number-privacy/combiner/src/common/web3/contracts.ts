6c1e09bb88dc969a9a4e55d9a453387e5d9921bc
alvarof2 <fernandez.f.alvaro@gmail.com>
2023-09-29 02:08:09 +0200

    Add fullNodeLatency Histogram

--- packages/phone-number-privacy/combiner/src/common/web3/contracts.ts
+++ packages/phone-number-privacy/combiner/src/common/web3/contracts.ts
@@ -2,17 +2,22 @@ import { ContractKit } from '@celo/contractkit'
 import { getDataEncryptionKey } from '@celo/phone-number-privacy-common'
 import Logger from 'bunyan'
 import config from '../../config'
+import { Counters, Histograms, newMeter } from '../metrics'
 
 export async function getDEK(kit: ContractKit, logger: Logger, account: string): Promise<string> {
-  return getDataEncryptionKey(
-    account,
-    kit,
-    logger,
-    config.phoneNumberPrivacy.fullNodeTimeoutMs,
-    config.phoneNumberPrivacy.fullNodeRetryCount,
-    config.phoneNumberPrivacy.fullNodeRetryDelayMs
-  ).catch((err) => {
-    logger.error({ err, account }, 'failed to get on-chain DEK for account')
-    throw err
-  })
+  const _meter = newMeter(Histograms.fullNodeLatency, 'getDataEncryptionKey')
+  return _meter(() =>
+    getDataEncryptionKey(
+      account,
+      kit,
+      logger,
+      config.phoneNumberPrivacy.fullNodeTimeoutMs,
+      config.phoneNumberPrivacy.fullNodeRetryCount,
+      config.phoneNumberPrivacy.fullNodeRetryDelayMs
+    ).catch((err) => {
+      logger.error({ err, account }, 'failed to get on-chain DEK for account')
+      Counters.blockchainErrors.inc()
+      throw err
+    })
+  )
 }
