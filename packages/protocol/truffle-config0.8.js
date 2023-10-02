09dcaee1dbfe8f7df0c56c810512e1d69d3270e0
pahor167 <47992132+pahor167@users.noreply.github.com>
2023-10-02 12:29:53 +0200

    Verify smart contracts

--- packages/protocol/truffle-config0.8.js
+++ packages/protocol/truffle-config0.8.js
@@ -3,11 +3,15 @@ const SOLC_VERSION = '0.8.19'
 const parent = require('./truffle-config-parent.js')
 const flakeTrackingConfig = require('@celo/flake-tracker/src/mocha/config.js')
 const networks = { ...parent.networks }
+const { celoScanApiKey } = require('./.env.json')
 
 console.log(`Using truffle version for Solidity ${SOLC_VERSION}`)
 
 module.exports = {
-  plugins: ['truffle-security', 'truffle-plugin-blockscout-verify'],
+  plugins: ['truffle-plugin-verify'],
+  api_keys: {
+    celoscan: celoScanApiKey,
+  },
   compilers: {
     solc: {
       version: SOLC_VERSION,
@@ -30,7 +34,10 @@ if (process.argv.includes('--gas')) {
         },
       },
     },
-    plugins: ['truffle-security', 'truffle-plugin-blockscout-verify'],
+    plugins: ['truffle-plugin-verify'],
+    api_keys: {
+      celoscan: celoScanApiKey,
+    },
     networks,
     reporter: 'eth-gas-reporter',
     reporterOptions: {
