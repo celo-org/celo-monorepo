bc85433fd97cd75a2e5f66816e08beb6ea01951c
Martín Volpe <volpe@clabs.co>
2023-10-18 19:55:10 +0200

    Create 26_01_uniswap_fee_handler_seller.ts

---
+++ packages/protocol/migrations_ts/26_01_uniswap_fee_handler_seller.ts
@@ -0,0 +1,14 @@
+import { CeloContractName } from '@celo/protocol/lib/registry-utils'
+import { deploymentForCoreContract } from '@celo/protocol/lib/web3-utils'
+import { UniswapFeeHandlerSellerInstance } from 'types'
+
+const initializeArgs = async () => {
+  return []
+}
+
+module.exports = deploymentForCoreContract<UniswapFeeHandlerSellerInstance>(
+  web3,
+  artifacts,
+  CeloContractName.MentoFeeHandlerSeller,
+  initializeArgs
+)
