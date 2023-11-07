69a15b0d8bb29a31d79efe7b10e6898716a7cb06
Aaron DeRuvo <aaron@clabs.co>
2023-11-07 13:25:10 +0100

    hypothesis: not having web3-utils messes up cli somehow?

--- packages/sdk/phone-utils/src/getPhoneHash.ts
+++ packages/sdk/phone-utils/src/getPhoneHash.ts
@@ -1,13 +1,12 @@
-import { getIdentifierHash, getPrefixedIdentifier, IdentifierPrefix } from '@celo/base'
 import { soliditySha3 } from 'web3-utils'
 
 const sha3 = (v: string): string | null => soliditySha3({ type: 'string', value: v })
 const getPhoneHash = (phoneNumber: string, salt?: string): string => {
   if (salt) {
-    return getIdentifierHash(sha3, phoneNumber, IdentifierPrefix.PHONE_NUMBER, salt)
+    return `0x${sha3(phoneNumber + salt)}`
   }
   // backwards compatibility for old phoneUtils getPhoneHash
-  return sha3(getPrefixedIdentifier(phoneNumber, IdentifierPrefix.PHONE_NUMBER)) as string
+  return 'no goood'
 }
 
 export default getPhoneHash
