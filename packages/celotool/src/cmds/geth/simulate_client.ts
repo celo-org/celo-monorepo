7c6b4f08a43b83661f998477658c16f9aa038174
Valentin Rodygin <valentin@clabs.co>
2023-11-07 15:22:12 +0100

    Updated the description

--- packages/celotool/src/cmds/geth/simulate_client.ts
+++ packages/celotool/src/cmds/geth/simulate_client.ts
@@ -64,7 +64,7 @@ export const builder = () => {
     })
     .options('test-mode', {
       type: 'string',
-      description: 'Load test mode: mixed transaction types, big calldatas, or simple transfers',
+      description: 'Load test mode: mixed transaction types, big calldatas, simple transfers paid in CELO or transfers paid in cUSD',
       choices: [TestMode.Mixed, TestMode.Data, TestMode.Transfer, TestMode.StableTransfer],
       default: TestMode.Mixed,
     })
