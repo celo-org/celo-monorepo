7c73e235a34872f01d58ad997e17b86017df74ac
Aaron DeRuvo <aaron@clabs.co>
2023-11-07 13:39:21 +0100

    remove web3-utils

--- packages/sdk/phone-utils/src/getPhoneHash.ts
+++ packages/sdk/phone-utils/src/getPhoneHash.ts
@@ -1,9 +1,6 @@
-import { soliditySha3 } from 'web3-utils'
-
-const sha3 = (v: string): string | null => soliditySha3({ type: 'string', value: v })
 const getPhoneHash = (phoneNumber: string, salt?: string): string => {
   if (salt) {
-    return `0x${sha3(phoneNumber + salt)}`
+    return `0x${phoneNumber + salt}`
   }
   // backwards compatibility for old phoneUtils getPhoneHash
   return 'no goood'
