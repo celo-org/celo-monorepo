17937f84183d8dcc527306321a880e442c7a63c0
Aaron DeRuvo <aaron@clabs.co>
2023-11-07 17:03:02 +0100

    if this works something is seriously wrong

--- packages/sdk/phone-utils/src/getPhoneHash.ts
+++ packages/sdk/phone-utils/src/getPhoneHash.ts
@@ -1,6 +1,6 @@
 // Remove this function causes the most weird error on celocli tests
-const getPhoneHash = (_number: string, _salt?: string) => {
-  throw new Error('Do not use getPhoneHash from @celo/phone-utils')
+const getPhoneHash = (phoneNumber: string, salt?: string): string => {
+  return `0x${phoneNumber + salt}`
 }
 
 export default getPhoneHash
