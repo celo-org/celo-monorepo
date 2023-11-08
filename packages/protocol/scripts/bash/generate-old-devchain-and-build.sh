64b68893a95fcfaecffb059a4daf9bb581127e0b
pahor167 <47992132+pahor167@users.noreply.github.com>
2023-11-08 11:18:13 +0100

    revert to original

--- packages/protocol/scripts/bash/generate-old-devchain-and-build.sh
+++ packages/protocol/scripts/bash/generate-old-devchain-and-build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
-set -x
 
 source ./scripts/bash/utils.sh
 
@@ -30,46 +29,32 @@ done
 [ -z "$BRANCH" ] && echo "Need to set the branch via the -b flag" && exit 1;
 [ -z "$BUILD_DIR" ] && BUILD_DIR=$(echo build/$(echo $BRANCH | sed -e 's/\//_/g'));
 
-echo "- Checkout2 source code at $BRANCH"
-# Fetching also tags so we can checkout if $BRACH references a tag
-git fetch origin +"$BRANCH" --tags --force >> $LOG_FILE 2>&1
-rm -vrf packages/protocol/lib/memview.sol/* # has to be mentined explicitly as it is not a submodule in previous releases
-ls -la packages/protocol/lib/memview.sol
-git checkout -f --recurse-submodules $BRANCH >> $LOG_FILE 2>&1
+echo "- Checkout source code at $BRANCH"
+git fetch origin +"$BRANCH" 2>>$LOG_FILE >> $LOG_FILE
+git checkout $BRANCH 2>>$LOG_FILE >> $LOG_FILE
 
 echo "- Build monorepo (contract artifacts, migrations, + all dependencies)"
 cd ../..
 
 # Using `yarn reset` to remove node_modules before re-installing using the node version of 
 # the previous release branch. This is useful when node version between branches are incompatible
-
+yarn run reset >> $LOG_FILE
 # build entire monorepo to account for any required dependencies.
 yarn install >> $LOG_FILE
+yarn run clean >> $LOG_FILE
 # in release v8 and earlier, @celo/contractkit automatically uses set RELEASE_TAG
 # when building, which fails if this differs from `package/protocol`'s build directory.
 RELEASE_TAG="" yarn build >> $LOG_FILE
 cd packages/protocol
 
-# echo "- Create local network"
-# if [ -z "$GRANTS_FILE" ]; then
-#   yarn devchain generate-tar "$PWD/devchain.tar.gz" >> $LOG_FILE
-# else
-#   yarn devchain generate-tar "$PWD/devchain.tar.gz" --release_gold_contracts $GRANTS_FILE >> $LOG_FILE
-# fi
-# rm -rf $BUILD_DIR && mkdir -p $BUILD_DIR
-# mv build/contracts* $BUILD_DIR
-# mv "$PWD/devchain.tar.gz" $BUILD_DIR/.
+echo "- Create local network"
+if [ -z "$GRANTS_FILE" ]; then
+  yarn devchain generate-tar "$PWD/devchain.tar.gz" >> $LOG_FILE
+else
+  yarn devchain generate-tar "$PWD/devchain.tar.gz" --release_gold_contracts $GRANTS_FILE >> $LOG_FILE
+fi
+rm -rf $BUILD_DIR && mkdir -p $BUILD_DIR
+mv build/contracts $BUILD_DIR
+mv "$PWD/devchain.tar.gz" $BUILD_DIR/.
 
-# Forcefully remove all submodules and reinitialize them after checkout
-git submodule deinit -f --all
-while IFS= read -r line; do
-    # Extract the submodule path which is the second part of the line
-    submodule_path=$(echo "$line" | cut -d ' ' -f 2)
-    # Remove the submodule directory
-    echo "removing $submodule_path"
-    rm -rf "$submodule_path"
-done < <(git config --file .gitmodules --get-regexp path)
-rm -rf packages/protocol/lib/memview.sol # has to be mentined explicitly as it is not a submodule in previous releases
-rm -rf .git/modules/*
-git checkout -f --recurse-submodules -
-git submodule update --init --recursive
+git checkout -
