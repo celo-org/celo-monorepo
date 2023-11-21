b41948bd78c974644dd1adf930cccefe6b4aaf57
Aaron DeRuvo <aaron@clabs.co>
2023-11-21 18:32:07 +0200

    use new devchain

--- packages/cli/src/test-utils/setup.global.ts
+++ packages/cli/src/test-utils/setup.global.ts
@@ -1,6 +1,5 @@
 import baseSetup from '@celo/dev-utils/lib/ganache-setup'
 // Has to import the matchers somewhere so that typescript knows the matchers have been made available
-import _unused from '@celo/dev-utils/lib/matchers'
 import * as path from 'path'
 
 // Warning: There should be an unused import of '@celo/dev-utils/lib/matchers' above.
@@ -9,7 +8,7 @@ import * as path from 'path'
 export default async function globalSetup() {
   const chainDataPath = path.join(path.dirname(require.resolve('@celo/celo-devchain')), '../chains')
   // v10 refers to core contract release 10
-  await baseSetup(path.resolve(chainDataPath), 'v10.tar.gz', {
+  await baseSetup(path.resolve(chainDataPath), 'devchain.tar.gz', {
     from_targz: true,
   })
 }
