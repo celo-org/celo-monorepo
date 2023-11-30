17cb1d904896291e6a9d6042f00a7f3b747322bf
soloseng <102702451+soloseng@users.noreply.github.com>
2023-11-30 17:15:54 -0500

    created test contract

---
+++ packages/protocol/test-sol/common/GasPriceMinimum.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.7 <0.8.20;
+
+import "celo-foundry/Test.sol";
+import "forge-std/console.sol";
+
+// Contract to test
+import "../../contracts-0.8/common/GasPriceMinimum.sol";
+
+contract GasPriceMinimumTest is Test {}
