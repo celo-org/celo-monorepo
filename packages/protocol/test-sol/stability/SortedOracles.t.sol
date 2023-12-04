e91c7de6f22e7809280b1ed10908aac3d4ea7106
Martín Volpe <volpe@clabs.co>
2023-12-04 13:19:27 +0100

    Delete console.log

--- packages/protocol/test-sol/stability/SortedOracles.t.sol
+++ packages/protocol/test-sol/stability/SortedOracles.t.sol
@@ -7,7 +7,6 @@ import { SortedOracles } from "../../contracts/stability/SortedOracles.sol";
 import "../../contracts/common/FixidityLib.sol";
 import "../../contracts/common/linkedlists/AddressSortedLinkedListWithMedian.sol";
 import "../../contracts/common/linkedlists/SortedLinkedListWithMedian.sol";
-import "forge-std/console.sol";
 
 contract SortedOraclesTest is Test {
   using FixidityLib for FixidityLib.Fraction;
