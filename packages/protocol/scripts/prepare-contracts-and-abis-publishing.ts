bb9eabfe934dab7eeb4abaf3a5ef14992677ed7a
Leszek Stachowski <leszek.stachowski@clabs.co>
2023-12-04 17:29:02 +0100

    comment out temporarily

--- packages/protocol/scripts/prepare-contracts-and-abis-publishing.ts
+++ packages/protocol/scripts/prepare-contracts-and-abis-publishing.ts
@@ -106,10 +106,10 @@ function prepareTargetTypesExports() {
   targets.forEach((target) => {
     // We don't need package.json for type declarations
     if (target != BuildTarget.TYPES) {
-      fs.copyFileSync(
-        path.join(ABIS_PACKAGE_SRC_DIR, `package-${target}.json`),
-        path.join(ABIS_DIST_DIR, target, 'package.json')
-      )
+      // fs.copyFileSync(
+      //   path.join(ABIS_PACKAGE_SRC_DIR, `package-${target}.json`),
+      //   path.join(ABIS_DIST_DIR, target, 'package.json')
+      // )
     }
 
     const filePaths = lsRecursive(path.join(ABIS_DIST_DIR, target))
