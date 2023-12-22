c3b5621ae2998c392be15cf2aa7328e4ca8eec4b
Martín Volpe <martin.volpe@gmail.com>
2023-12-22 19:28:42 +0100

    Finished tests

--- packages/protocol/test-sol/constants.sol
+++ packages/protocol/test-sol/constants.sol
@@ -5,6 +5,9 @@ contract Constants {
   uint256 public constant FIXED1 = 1e24;
   uint256 constant YEAR = 365 * 24 * 60 * 60;
 
+  // uint256 constant EPOCH_SIZE = (24*60*60)/5;
+  uint256 constant EPOCH_SIZE = 100; // TODO roll back
+
   // contract names
   string constant ElectionContract = "Election";
   string constant SortedOraclesContract = "SortedOracles";
