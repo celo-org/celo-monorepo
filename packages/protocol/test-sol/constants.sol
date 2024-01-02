7d016b2662ae33f90d8ae512932d3127609bd1c6
Martín Volpe <martin.volpe@gmail.com>
2024-01-02 14:11:51 +0100

    fixed constants

--- packages/protocol/test-sol/constants.sol
+++ packages/protocol/test-sol/constants.sol
@@ -4,9 +4,7 @@ pragma solidity ^0.5.13;
 contract Constants {
   uint256 public constant FIXED1 = 1e24;
   uint256 constant YEAR = 365 * 24 * 60 * 60;
-
-  // uint256 constant EPOCH_SIZE = (24*60*60)/5;
-  uint256 constant EPOCH_SIZE = 100; // TODO roll back
+  uint256 constant EPOCH_SIZE = (24 * 60 * 60) / 5;
 
   // contract names
   string constant ElectionContract = "Election";
