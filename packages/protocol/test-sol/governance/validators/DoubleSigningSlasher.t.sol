ff38e4b69e800192b84662accfab171e11206caf
soloseng <102702451+soloseng@users.noreply.github.com>
2024-01-05 10:39:12 -0500

    Using separate DoubleSigningSlasherTest contract for foundry & truffle

--- packages/protocol/test-sol/governance/validators/DoubleSigningSlasher.t.sol
+++ packages/protocol/test-sol/governance/validators/DoubleSigningSlasher.t.sol
@@ -10,7 +10,46 @@ import "@celo-contracts/common/Registry.sol";
 import "@celo-contracts/common/Accounts.sol";
 import "@celo-contracts/governance/test/MockValidators.sol";
 import "@celo-contracts/governance/test/MockLockedGold.sol";
-import "@celo-contracts/governance/test/DoubleSigningSlasherTest.sol";
+import "@celo-contracts/governance/DoubleSigningSlasher.sol";
+import "@celo-contracts/governance/test/MockUsingPrecompiles.sol";
+
+// pragma solidity ^0.5.13;
+// pragma experimental ABIEncoderV2;
+
+// import "../DoubleSigningSlasher.sol";
+// import "./MockUsingPrecompiles.sol";
+
+contract DoubleSigningSlasherTest is DoubleSigningSlasher(true), MockUsingPrecompiles {
+  struct SlashParams {
+    address signer;
+    uint256 index;
+    bytes headerA;
+    bytes headerB;
+    uint256 groupMembershipHistoryIndex;
+    address[] validatorElectionLessers;
+    address[] validatorElectionGreaters;
+    uint256[] validatorElectionIndices;
+    address[] groupElectionLessers;
+    address[] groupElectionGreaters;
+    uint256[] groupElectionIndices;
+  }
+
+  function mockSlash(SlashParams calldata slashParams) external {
+    slash(
+      slashParams.signer,
+      slashParams.index,
+      slashParams.headerA,
+      slashParams.headerB,
+      slashParams.groupMembershipHistoryIndex,
+      slashParams.validatorElectionLessers,
+      slashParams.validatorElectionGreaters,
+      slashParams.validatorElectionIndices,
+      slashParams.groupElectionLessers,
+      slashParams.groupElectionGreaters,
+      slashParams.groupElectionIndices
+    );
+  }
+}
 
 contract DoubleSigningSlasherBaseTest is Test {
   using FixidityLib for FixidityLib.Fraction;
