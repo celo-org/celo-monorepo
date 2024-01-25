72118da4d2314330c937dee33c36b0f8dadbe4d2
Aaron DeRuvo <aaron@clabs.co>
2024-01-25 11:23:22 +0200

    investigate intermediate value is not iterable

--- packages/protocol/test/common/integration.ts
+++ packages/protocol/test/common/integration.ts
@@ -49,7 +49,10 @@ enum VoteValue {
 }
 
 async function getGroups(election: ElectionInstance) {
-  const [lst1, lst2] = await election.getTotalVotesForEligibleValidatorGroups()
+  const response = await election.getTotalVotesForEligibleValidatorGroups()
+  console.info('response', response)
+  const lst1 = response[0]
+  const lst2 = response[1]
   return zip(
     (address, value) => {
       return { address, value }
@@ -212,8 +215,9 @@ contract('Integration: Governance slashing', (accounts: string[]) => {
     })
 
     it('should increment the vote totals', async () => {
-      const [yes, ,] = await governance.getVoteTotals(proposalId)
-      assertEqualBN(yes, value)
+      const response = await governance.getVoteTotals(proposalId)
+      console.error('response', response)
+      assertEqualBN(response[0], value)
     })
   })
 
@@ -386,8 +390,9 @@ contract('Integration: Governance', (accounts: string[]) => {
     })
 
     it('should increment the vote totals', async () => {
-      const [yes, ,] = await governance.getVoteTotals(proposalId)
-      assertEqualBN(yes, value)
+      const response = await governance.getVoteTotals(proposalId)
+      console.error('response', response)
+      assertEqualBN(response[0], value)
     })
   })
 
