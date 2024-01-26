171512a51805b9a4901332a4f670a97eff347c8a
soloseng <102702451+soloseng@users.noreply.github.com>
2024-01-26 14:46:31 -0500

    ++ address to pubkey helper

---
+++ packages/protocol/test-sol/utils/ECDSAHelper.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.17;
+
+import "@0xcyphered/SECP256K1.sol";
+
+contract ECDSAHelper {
+  function addressToPublicKey(bytes32 message, uint8 _v, bytes32 _r, bytes32 _s)
+    public
+    pure
+    returns (bytes memory)
+  {
+    string memory header = "\x19Ethereum Signed Message:\n32";
+    bytes32 _message = keccak256(abi.encodePacked(header, message));
+    (uint256 x, uint256 y) = SECP256K1.recover(
+      uint256(_message),
+      _v - 27,
+      uint256(_r),
+      uint256(_s)
+    );
+    return abi.encodePacked(x, y);
+  }
+}
