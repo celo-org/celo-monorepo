d47d41df2f8e5e9e65e15bbb15be69b74e5b8930
pahor167 <47992132+pahor167@users.noreply.github.com>
2024-02-21 11:23:40 +0100

    SortedOracles sourced from Mento core

--- packages/protocol/lib/compatibility/ignored-contracts-v9.ts
+++ packages/protocol/lib/compatibility/ignored-contracts-v9.ts
@@ -10,19 +10,18 @@ export const ignoredContractsV9 = [
   'StableTokenRegistry',
   'Reserve',
   'ReserveSpenderMultiSig',
+]
 
-  'SortedOracles' 
- // Note: Sorted Oracles is a Celo Core Contract
-  // but as it has also been modified and deployed but the Mento team.
-  // We currently need work to be able to upgrade it again:
-  // https://github.com/celo-org/celo-monorepo/issues/10435
+export const ignoredContractsV9Only = [
+  'SortedOracles'
+  // In CR9 the SortedOracles contract was deployed by Mento team, in CR10 we redeployed it ourselves
 ]
 
 export function getReleaseVersion(tag: string) {
   const regexp = /core-contracts.v(?<version>.*[0-9])/gm
   const matches = regexp.exec(tag)
   const version = parseInt(matches?.groups?.version ?? '0', 10)
-  if ((version) == 0){
+  if ((version) == 0) {
     throw `Tag doesn't have the correct format ${tag}`
   }
   return version
--- packages/protocol/scripts/bash/verify-deployed.sh
+++ packages/protocol/scripts/bash/verify-deployed.sh
@@ -32,4 +32,31 @@ done
 source scripts/bash/release-lib.sh
 build_tag $BRANCH $LOG_FILE
 
+if [ "$BRANCH" = "core-contracts.v10" ]; then
+  if [ ! -d "build/mento" ]; then
+    mkdir -p build/mento
+    cd build/mento
+    git clone --depth 1 --branch v2.2.1 https://github.com/mento-protocol/mento-core.git
+    cd mento-core
+    yarn 
+    forge install
+    forge build
+  else 
+    cd build/mento/mento-core
+  fi
+
+  # Replace the bytecode of the SortedOracles contracts with the bytecode from Mento core v2.2.1
+  orig_value_SortedOracles=$(jq -r '.deployedBytecode.object' out/SortedOracles.sol/SortedOracles.json)
+  substring_to_replace='__$e9f4a9f9de32ce6d7070252f1b707ecbd2$__'
+  replacement='__AddressSortedLinkedListWithMedian_____'
+  value_SortedOracles="${orig_value_SortedOracles//$substring_to_replace/$replacement}"
+  jq --arg value "$value_SortedOracles" '.deployedBytecode = $value' ../../../$BUILD_DIR/contracts/SortedOracles.json > "temp.json" && mv "temp.json" ../../../$BUILD_DIR/contracts/SortedOracles.json
+
+  # Replace the bytecode of the AddressSortedLinkedListWithMedian contract with the bytecode from Mento core v2.2.1
+  value_AddressSortedLinkedListWithMedian=$(jq -r '.deployedBytecode.object' out/AddressSortedLinkedListWithMedian.sol/AddressSortedLinkedListWithMedian.json)
+  jq --arg value "$value_AddressSortedLinkedListWithMedian" '.deployedBytecode = $value' ../../../$BUILD_DIR/contracts/AddressSortedLinkedListWithMedian.json > temp.json && mv temp.json ../../../$BUILD_DIR/contracts/AddressSortedLinkedListWithMedian.json
+
+  cd ../../../
+fi
+
 yarn run truffle exec ./scripts/truffle/verify-bytecode.js --network $NETWORK --build_artifacts $BUILD_DIR/contracts  --branch $BRANCH --librariesFile "libraries.json" $FORNO
