3f0176403410d7f714bc4f731449e5265d4d7c96
Martín Volpe <volpe@clabs.co>
2024-02-28 11:30:14 -0700

    Natspec IFeeCurrencyAdapter

--- packages/protocol/contracts-0.8/stability/interfaces/IFeeCurrencyAdapter.sol
+++ packages/protocol/contracts-0.8/stability/interfaces/IFeeCurrencyAdapter.sol
@@ -1,23 +1,51 @@
 // SPDX-License-Identifier: LGPL-3.0-only
-pragma solidity >=0.8.7 <0.8.20;
 
 interface IFeeCurrencyAdapter {
+  /**
+   * @return The address of the adapted token.
+   */
   function getAdaptedToken() external view returns (address);
 
+  /**
+   * @return The multiplier that should be used when upscaling and downscaling.
+   */
   function digitDifference() external view returns (uint96);
 
+  /**
+   * @return returns the amount that it's debited after calling debitGasFees().
+   */
   function debited() external view returns (uint256);
 
+  /**
+   * @return The name of the adapted token.
+   */
   function name() external view returns (string memory);
 
+  /**
+   * @return The symbol of adapted token.
+   */
   function symbol() external view returns (string memory);
 
-  function expectedDecimals() external view returns (uint8);
-
+  /**
+   * @return Returns the decimals expected by the vm.
+   */
   function decimals() external view returns (uint8);
 
+  /**
+   * @return same as decimals.
+   */
+  function expectedDecimals() external view returns (uint8);
+
+
+  /**
+   * @notice See IFeeCurrency.
+   */
   function debitGasFees(address from, uint256 value) external;
 
+
+  /**
+   * @notice See IFeeCurrency.
+   */
   function creditGasFees(
     address refundRecipient,
     address tipRecipient,
