4a35759291520450231e7c94e91d52679f0c2e3e
pahor167 <47992132+pahor167@users.noreply.github.com>
2024-03-05 11:26:33 +0100

    Election tests migration (#10871)
    * Election up till activate
    
    * ElectionValidatorSigners mostly done
    
    * ElectionValidatorSigners done
    
    * Last test batch missing
    
    * All tests are working
    
    * removal of election ts tests
    
    * compilation CI fix
    
    * utils added
    
    * constants added
    
    * minor refactor
    
    * Update packages/protocol/test-sol/voting/Election.t.sol
    
    Co-authored-by: soloseng <102702451+soloseng@users.noreply.github.com>
    
    * Update packages/protocol/test-sol/voting/Election.t.sol
    
    Co-authored-by: soloseng <102702451+soloseng@users.noreply.github.com>
    
    * Update packages/protocol/test-sol/voting/Election.t.sol
    
    Co-authored-by: soloseng <102702451+soloseng@users.noreply.github.com>
    
    * Update packages/protocol/test-sol/voting/Election.t.sol
    
    Co-authored-by: soloseng <102702451+soloseng@users.noreply.github.com>
    
    * Update packages/protocol/test-sol/voting/Election.t.sol
    
    Co-authored-by: soloseng <102702451+soloseng@users.noreply.github.com>
    
    * Update packages/protocol/test-sol/voting/Election.t.sol
    
    Co-authored-by: soloseng <102702451+soloseng@users.noreply.github.com>
    
    * Update packages/protocol/test-sol/voting/Election.t.sol
    
    Co-authored-by: soloseng <102702451+soloseng@users.noreply.github.com>
    
    * Update packages/protocol/test-sol/voting/Election.t.sol
    
    Co-authored-by: soloseng <102702451+soloseng@users.noreply.github.com>
    
    * Update packages/protocol/test-sol/voting/Election.t.sol
    
    Co-authored-by: soloseng <102702451+soloseng@users.noreply.github.com>
    
    * Update packages/protocol/test-sol/voting/Election.t.sol
    
    Co-authored-by: soloseng <102702451+soloseng@users.noreply.github.com>
    
    * Update packages/protocol/test-sol/voting/Election.t.sol
    
    Co-authored-by: soloseng <102702451+soloseng@users.noreply.github.com>
    
    * buildable
    
    * PR comments 2
    
    * Missing tests added
    
    * Split of the tests
    
    ---------
    
    Co-authored-by: soloseng <102702451+soloseng@users.noreply.github.com>

--- packages/protocol/test-sol/constants.sol
+++ packages/protocol/test-sol/constants.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.5.13;
 // This contract is only required for Solidity 0.5
 contract Constants {
   uint256 public constant FIXED1 = 1e24;
-  uint256 constant DAY = 24 * 60 * 60;
-  uint256 constant WEEK = 7 * DAY;
-  uint256 constant YEAR = 365 * DAY;
+  uint256 public constant DAY = 24 * 60 * 60;
+  uint256 public constant WEEK = DAY * 7;
+  uint256 public constant YEAR = 365 * DAY;
+
   uint256 constant EPOCH_SIZE = DAY / 5;
 
   // contract names
