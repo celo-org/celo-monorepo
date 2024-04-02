3b75dabd4555aba453aae98afedab0e8331fc351
soloseng <102702451+soloseng@users.noreply.github.com>
2024-04-02 12:10:32 -0400

    remove old unused test contract

--- packages/protocol/contracts/governance/test/DoubleSigningSlasherTest.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.5.13;
-
-import "../DoubleSigningSlasher.sol";
-import "./MockUsingPrecompiles.sol";
-
-contract DoubleSigningSlasherTest is DoubleSigningSlasher(true), MockUsingPrecompiles {}
