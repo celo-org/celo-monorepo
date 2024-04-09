1f2f80b24e998d97cc682b2d3486a654f636ae71
Martín Volpe <martin.volpe@gmail.com>
2024-04-09 15:43:16 +0200

    fix Ci by changing folder of tests

--- packages/protocol/test-sol/integration/Integration.t.sol
+++ packages/protocol/test-sol/integration/Integration.t.sol
@@ -8,23 +8,21 @@ import "forge-std/console.sol";
 import "@celo-contracts/common/interfaces/IRegistry.sol";
 
 contract IntegrationTest is Test {
-    address constant registryAddress = address(0x000000000000000000000000000000000000ce10);
-    address account1 = actor('account1');
-    address account2 = actor('account2');
-    IRegistry registry = IRegistry(registryAddress);
+  address constant registryAddress = address(0x000000000000000000000000000000000000ce10);
+  address account1 = actor("account1");
+  address account2 = actor("account2");
+  IRegistry registry = IRegistry(registryAddress);
 
-  function setUp() public{
+  function setUp() public {}
 
-  }
-
-  function test_hello() public{
+  function test_hello() public {
     console.log("Works!");
     // registry.setAddressFor("registry", address(1));
     console.log("print:");
     console.logAddress(registry.getAddressForStringOrDie("GoldToken"));
   }
 
-  function test_transfer() public{
+  function test_transfer() public {
     GoldToken goldToken = GoldToken(registry.getAddressForStringOrDie("GoldToken"));
     vm.deal(account1, 1 wei);
     vm.prank(account1);
@@ -32,7 +30,5 @@ contract IntegrationTest is Test {
     assertEq(goldToken.balanceOf(account1), 1);
   }
 
-  function test_precompile() public {
-    
-  }
+  function test_precompile() public {}
 }
