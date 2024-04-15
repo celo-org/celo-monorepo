0586aa5a3238ea06e83b0c09565c81b53a87e445
pahor167 <hornak.pavel@gmail.com>
2024-04-15 13:43:39 +0200

    L2 migration infra

---
+++ packages/protocol/test-sol/common/IsL2.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.7 <0.8.20;
+
+import "celo-foundry-8/Test.sol";
+
+import "@celo-contracts/common/FixidityLib.sol";
+
+import "@celo-contracts/common/interfaces/IRegistry.sol";
+
+// Contract to test
+import "@celo-contracts-8/common/IsL2.sol";
+
+contract IsL2CheckBase is Test {
+  address constant proxyAdminAddress = 0x4200000000000000000000000000000000000018;
+
+  IsL2Check isL2Check;
+
+  function setUp() public virtual {
+    isL2Check = new IsL2Check();
+  }
+}
+
+contract IsL2Check_IsL2Test is IsL2CheckBase {
+  function testIsL2() public {
+    assertFalse(isL2Check.IsL2());
+  }
+
+  function testIsL1() public {
+    assertTrue(isL2Check.IsL1());
+  }
+
+  function helper_WhenProxyAdminAddressIsSet() public {
+    deployCodeTo("Registry.sol", abi.encode(false), proxyAdminAddress);
+  }
+
+  function testIsL2_WhenProxyAdminSet() public {
+    helper_WhenProxyAdminAddressIsSet();
+    assertTrue(isL2Check.IsL2());
+  }
+
+  function testIsL1_WhenProxyAdminSet() public {
+    helper_WhenProxyAdminAddressIsSet();
+    assertFalse(isL2Check.IsL1());
+  }
+}
