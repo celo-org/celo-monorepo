2cea3347a75354bbaa44b1567dc59a4aa5bcb467
soloseng <102702451+soloseng@users.noreply.github.com>
2024-04-19 10:40:25 -0400

    revert version ∆

--- packages/protocol/contracts/governance/LockedGold.sol
+++ packages/protocol/contracts/governance/LockedGold.sol
@@ -122,7 +122,7 @@ contract LockedGold is
   * @return Patch version of the contract.
   */
   function getVersionNumber() external pure returns (uint256, uint256, uint256, uint256) {
-    return (1, 1, 5, 0);
+    return (1, 1, 4, 0);
   }
 
   /**
