d0ce2253e06fcb9d1dc63316013d7f1696fdd957
soloseng <102702451+soloseng@users.noreply.github.com>
2024-05-16 15:12:41 -0400

    updated interface

--- packages/protocol/contracts/common/interfaces/IMintGoldSchedule.sol
+++ packages/protocol/contracts/common/interfaces/IMintGoldSchedule.sol
@@ -2,5 +2,19 @@
 pragma solidity >=0.5.13 <0.9.0;
 
 interface IMintGoldSchedule {
-  function mintAccordingToSchedule() external;
+  /**
+   * @notice Mints CELO to the beneficiaries according to the predefined schedule.
+   */
+  function mintAccordingToSchedule() external returns (bool);
+
+  /**
+   * @return The currently mintable amount.
+   */
+  function getMintableAmount() external returns (uint256);
+
+  /**
+   * @notice Returns the target Gold supply according to the target schedule.
+   * @return The target Gold supply according to the target schedule.
+   */
+  function getTargetGoldTotalSupply() external returns (uint256, uint256, uint256);
 }
