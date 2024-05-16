c42946b97e406ccaa321efc2d49bf7296fce8ee6
pahor167 <hornak.pavel@gmail.com>
2024-05-16 15:10:35 +0200

    removal of MentoFeeCurrencyAdapterV1

--- packages/protocol/contractPackages.ts
+++ packages/protocol/contractPackages.ts
@@ -48,7 +48,7 @@ export const SOLIDITY_08_PACKAGE = {
   proxiesPath: '/', // Proxies are still with 0.5 contracts
   // Proxies shouldn't have to be added to a list manually
   // https://github.com/celo-org/celo-monorepo/issues/10555
-  contracts: ['GasPriceMinimum', 'FeeCurrencyDirectory', 'MentoFeeCurrencyAdapterV1'],
+  contracts: ['GasPriceMinimum', 'FeeCurrencyDirectory'],
   proxyContracts: [
     'GasPriceMinimumProxy',
     'FeeCurrencyDirectoryProxy',
