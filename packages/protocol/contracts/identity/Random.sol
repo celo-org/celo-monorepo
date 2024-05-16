59d6cfea472cdd343a339863ea8dcda2f855274e
soloseng <102702451+soloseng@users.noreply.github.com>
2024-05-16 11:07:21 -0400

    fixed version

--- packages/protocol/contracts/identity/Random.sol
+++ packages/protocol/contracts/identity/Random.sol
@@ -97,7 +97,7 @@ contract Random is
    * @return Patch version of the contract.
    */
   function getVersionNumber() external pure returns (uint256, uint256, uint256, uint256) {
-    return (1, 1, 1, 2);
+    return (1, 1, 2, 0);
   }
 
   /**
