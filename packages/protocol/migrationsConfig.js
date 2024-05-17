3ad97e0cb322bffeb30ab23af729a96e058aa4b9
soloseng <102702451+soloseng@users.noreply.github.com>
2024-05-17 10:55:42 -0400

    ++ ts migrations

--- packages/protocol/migrationsConfig.js
+++ packages/protocol/migrationsConfig.js
@@ -136,6 +136,11 @@ const DefaultConfig = {
   lockedGold: {
     unlockingPeriod: 3 * DAY,
   },
+  mintGoldSchedule: {
+    communityRewardFraction: 1 / 4,
+    carbonOffsettingPartner: '0xce10d577295d34782815919843a3a4ef70dc33ce',
+    carbonOffsettingFraction: 1 / 1000,
+  },
   oracles: {
     reportExpiry: 5 * MINUTE,
   },
---
+++ packages/protocol/migrations_ts/30_mintGoldSchedule.ts
@@ -0,0 +1,21 @@
+import { CeloContractName } from '@celo/protocol/lib/registry-utils'
+import { deploymentForCoreContract } from '@celo/protocol/lib/web3-utils'
+import { config } from '@celo/protocol/migrationsConfig'
+import { toFixed } from '@celo/utils/lib/fixidity'
+import { MintGoldScheduleInstance } from 'types'
+
+const initializeArgs = async () => {
+  return [
+    toFixed(config.mintGoldSchedule.communityRewardFraction).toFixed(),
+    config.mintGoldSchedule.carbonOffsettingPartner,
+    toFixed(config.mintGoldSchedule.carbonOffsettingFraction).toFixed(),
+    config.registry.predeployedProxyAddress,
+  ]
+}
+
+module.exports = deploymentForCoreContract<MintGoldScheduleInstance>(
+  web3,
+  artifacts,
+  CeloContractName.MintGoldSchedule,
+  initializeArgs
+)
