b5d8c15b19771c44303856f4f452b03f04c45cdc
soloseng <102702451+soloseng@users.noreply.github.com>
2024-05-22 09:27:48 -0400

    ++ version

--- packages/protocol/contracts/governance/EpochRewards.sol
+++ packages/protocol/contracts/governance/EpochRewards.sol
@@ -267,7 +267,7 @@ contract EpochRewards is
    * @return Patch version of the contract.
    */
   function getVersionNumber() external pure returns (uint256, uint256, uint256, uint256) {
-    return (1, 1, 1, 1);
+    return (1, 1, 2, 0);
   }
 
   /**
