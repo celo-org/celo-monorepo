23432eb217ad6c9039d63b1e6b59fadf17382ae3
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-06-06 09:48:59 +0100

    chore(migrations_sol): adds MVP script to run e2e tests

---
+++ packages/protocol/migrations_sol/run_e2e_tests_in_anvil.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Generate and run devchain
+echo "Generating and running devchain before running e2e tests..."
+source $PWD/migrations_sol/create_and_migrate_anvil_devchain.sh
+
+# Run e2e tests
+echo "Running e2e tests..."
+forge test \
+-vvv \
+--match-contract TestE2ESetCurrencyConfig \
+--fork-url http://127.0.0.1:$ANVIL_PORT
+
+# helper kill anvil
+# kill $(lsof -i tcp:$ANVIL_PORT | tail -n 1 | awk '{print $2}')
+
+echo "Killing Anvil"
+if [[ -n $ANVIL_PID ]]; then
+    kill $ANVIL_PID
+fi
