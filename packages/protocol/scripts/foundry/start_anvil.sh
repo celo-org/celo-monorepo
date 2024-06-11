b16e70c4a102f8c1fc727c83a8d2ed2f5162a8e3
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-06-11 13:58:09 +0200

    docs(scripts/foundry): adds more links to `package.json`
    Adds `repository` and `directory` to `package.json`.
    This ensures npmjs.org displays hyperlinks to the github repository.

--- packages/protocol/scripts/foundry/start_anvil.sh
+++ packages/protocol/scripts/foundry/start_anvil.sh
@@ -8,7 +8,7 @@ mkdir -p $ANVIL_FOLDER
 echo "Anvil state will be saved to $ANVIL_FOLDER"
 
 # create package.json
-echo "{\"name\": \"@celo/devchain-anvil\",\"version\": \"1.0.0\",\"description\": \"Anvil based devchain that contains core smart contracts of celo\",\"author\":\"Celo\",\"license\": \"LGPL-3.0\"}" > $TMP_FOLDER/package.json
+echo "{\"name\": \"@celo/devchain-anvil\",\"version\": \"1.0.0\",\"repository\": { \"url\": \"https://github.com/celo-org/celo-monorepo\", \"directory\": \"packages/protocol/migrations_sol\" },\"homepage\": \"https://github.com/celo-org/celo-monorepo/blob/master/packages/protocol/migrations_sol/README.md\",\"description\": \"Anvil based devchain that contains core smart contracts of celo\",\"author\":\"Celo\",\"license\": \"LGPL-3.0\"}" > $TMP_FOLDER/package.json
 
 cp $PWD/migrations_sol/README.md $TMP_FOLDER/README.md
 
