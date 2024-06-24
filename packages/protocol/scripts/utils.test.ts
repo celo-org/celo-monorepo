a01f9eaa72cdb36889eb982215b095c4e98de706
soloseng <102702451+soloseng@users.noreply.github.com>
2024-06-24 19:48:14 +0200

    Update npm package versioning script to be compatible with other packages (#11041)
    * updated script to allow for versioning devchain-anvil package
    
    * run in PR as well
    
    * reorg workflow to install node
    
    * fixed utils test
    
    * ++ workflow dispatch

--- packages/protocol/scripts/utils.test.ts
+++ packages/protocol/scripts/utils.test.ts
@@ -1,12 +1,7 @@
 import { expect } from '@jest/globals'
 import { execSync } from 'child_process'
 import { SemVer } from 'semver'
-import {
-  determineNextVersion,
-  getReleaseTypeFromSemVer,
-  isValidNpmTag,
-  isValidPrereleaseIdentifier,
-} from './utils'
+import { determineNextVersion, getReleaseTypeFromSemVer, isValidNpmTag } from './utils'
 
 jest.mock('child_process', () => ({
   execSync: jest.fn(),
@@ -24,18 +19,7 @@ describe('utils', () => {
   describe('determineNextVersion()', () => {
     const execSyncMock = execSync as jest.Mock
 
-    it('determines "latest" release type and extracts version from the git tag directly for generic v12', () => {
-      const nextVersion = determineNextVersion(
-        'core-contracts.v12',
-        'release/core-contracts/12',
-        '@celo/contracts',
-        'alpha'
-      )
-
-      expect(retrieveReleaseInformation(nextVersion!)).toEqual(['12.0.0', 'latest'])
-    })
-
-    it('determines "post-audit" release type and extracts version from "post-audit" git tag directly', () => {
+    it('determines "latest" release type and extracts version from "post-audit" git tag directly', () => {
       const nextVersion = determineNextVersion(
         'core-contracts.v11.2.3.post-audit',
         'release/core-contracts/11.0.0',
@@ -43,10 +27,7 @@ describe('utils', () => {
         'alpha'
       )
 
-      expect(retrieveReleaseInformation(nextVersion!)).toEqual([
-        '11.2.3-post-audit.0',
-        'post-audit',
-      ])
+      expect(retrieveReleaseInformation(nextVersion)).toEqual(['11.2.3', 'latest'])
     })
 
     it('determines "pre-audit" release type and extracts version from "pre-audit" git tag directly', () => {
@@ -57,34 +38,7 @@ describe('utils', () => {
         'alpha'
       )
 
-      expect(retrieveReleaseInformation(nextVersion!)).toEqual(['11.2.3-pre-audit.0', 'pre-audit'])
-    })
-
-    it('uses the tag directly when a semver compliant prerelease identifier is used', () => {
-      const nextVersion = determineNextVersion(
-        'core-contracts.v12.post-audit',
-        'master',
-        '@celo/contracts',
-        ''
-      )
-
-      expect(nextVersion).not.toBeNull()
-      expect(retrieveReleaseInformation(nextVersion!)).toEqual([
-        '12.0.0-post-audit.0',
-        'post-audit',
-      ])
-    })
-
-    it('falls back to alpha when non semver compliant prerelease identifier is used', () => {
-      const nextVersion = determineNextVersion(
-        'core-contracts.v12.post_audit_2',
-        'master',
-        '@celo/contracts',
-        ''
-      )
-
-      expect(nextVersion).not.toBeNull()
-      expect(retrieveReleaseInformation(nextVersion!)).toEqual(['12.0.0-alpha.0', 'alpha'])
+      expect(retrieveReleaseInformation(nextVersion)).toEqual(['11.2.3-pre-audit.0', 'pre-audit'])
     })
 
     it('determines for release git branch when major version matches', () => {
@@ -103,9 +57,7 @@ describe('utils', () => {
         'npm view @celo/contracts@canary version',
         expect.anything()
       )
-
-      expect(nextVersion).not.toBeNull()
-      expect(retrieveReleaseInformation(nextVersion!)).toEqual(['11.2.4-canary.0', 'canary'])
+      expect(retrieveReleaseInformation(nextVersion)).toEqual(['11.2.4-canary.0', 'canary'])
     })
 
     it("determines for release git branch when major version doesn't match", () => {
@@ -124,9 +76,7 @@ describe('utils', () => {
         'npm view @celo/contracts@canary version',
         expect.anything()
       )
-
-      expect(nextVersion).not.toBeNull()
-      expect(retrieveReleaseInformation(nextVersion!)).toEqual(['12.0.0-canary.0', 'canary'])
+      expect(retrieveReleaseInformation(nextVersion)).toEqual(['12.0.0-canary.0', 'canary'])
     })
 
     it("determines based on manually provided npm tag that already exists, doesn't fallback to 'canary'", () => {
@@ -147,9 +97,7 @@ describe('utils', () => {
         'npm view @celo/contracts@alpha version',
         expect.anything()
       )
-
-      expect(nextVersion).not.toBeNull()
-      expect(retrieveReleaseInformation(nextVersion!)).toEqual(['10.2.4-alpha.1', 'alpha'])
+      expect(retrieveReleaseInformation(nextVersion)).toEqual(['10.2.4-alpha.1', 'alpha'])
     })
 
     it("determines based on manually provided npm tag that doesn't exist yet, fallback to 'canary'", () => {
@@ -180,8 +128,7 @@ describe('utils', () => {
         expect.anything()
       )
 
-      expect(nextVersion).not.toBeNull()
-      expect(retrieveReleaseInformation(nextVersion!)).toEqual(['10.1.2-alpha.0', 'alpha'])
+      expect(retrieveReleaseInformation(nextVersion)).toEqual(['10.1.2-alpha.0', 'alpha'])
     })
 
     it("doesn't determine anything when wrong tag is provided", () => {
@@ -202,46 +149,14 @@ describe('utils', () => {
     })
   })
 
-  describe('tags', () => {
-    it('recognizes valid tags and prerelease identifiers', () => {
+  describe('isValidNpmTag()', () => {
+    it('recognizes valid tags', () => {
       expect(isValidNpmTag('latest')).toEqual(true)
-      expect(isValidPrereleaseIdentifier('latest')).toEqual(true)
-
       expect(isValidNpmTag('alpha')).toEqual(true)
-      expect(isValidPrereleaseIdentifier('alpha')).toEqual(true)
-
       expect(isValidNpmTag('beta')).toEqual(true)
-      expect(isValidPrereleaseIdentifier('beta')).toEqual(true)
-
       expect(isValidNpmTag('canary')).toEqual(true)
-      expect(isValidPrereleaseIdentifier('canary')).toEqual(true)
-
       expect(isValidNpmTag('rc')).toEqual(true)
-      expect(isValidPrereleaseIdentifier('rc')).toEqual(true)
-
       expect(isValidNpmTag('valid-tag')).toEqual(true)
-      expect(isValidPrereleaseIdentifier('valid-tag')).toEqual(true)
-
-      expect(isValidNpmTag('alfajores')).toEqual(true)
-      expect(isValidPrereleaseIdentifier('alfajores')).toEqual(true)
-
-      expect(isValidNpmTag('alfajores2')).toEqual(true)
-      expect(isValidPrereleaseIdentifier('alfajores2')).toEqual(true)
-
-      expect(isValidNpmTag('alfajores3')).toEqual(true)
-      expect(isValidPrereleaseIdentifier('alfajores3')).toEqual(true)
-
-      expect(isValidNpmTag('baklava')).toEqual(true)
-      expect(isValidPrereleaseIdentifier('baklava')).toEqual(true)
-
-      expect(isValidNpmTag('post-audit')).toEqual(true)
-      expect(isValidPrereleaseIdentifier('post-audit')).toEqual(true)
-
-      expect(isValidNpmTag('post_audit')).toEqual(true)
-      expect(isValidPrereleaseIdentifier('post_audit')).toEqual(false)
-
-      expect(isValidNpmTag('post_audit_2')).toEqual(true)
-      expect(isValidPrereleaseIdentifier('post_audit_2')).toEqual(false)
     })
 
     it('recognizes invalid tags', () => {
