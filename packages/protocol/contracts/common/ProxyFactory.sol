0ec269bf2a3d074cad2c5d39541c1760d8868c66
Martín Volpe <martin.volpe@gmail.com>
2024-06-27 16:22:05 +0200

    Added bytecodes to the repo

--- packages/protocol/contracts/common/ProxyFactory.sol
+++ packages/protocol/contracts/common/ProxyFactory.sol
@@ -1,25 +1,23 @@
 pragma solidity ^0.5.13;
 
-// import "@celo-contracts/common/interfaces/IProxy.sol";
 import "./Proxy.sol";
 import "./interfaces/IProxyFactory.sol";
 
 contract ProxyFactory is IProxyFactory {
+  // Deploys a new proxy contract and transfers ownership to the provided address
   function deployProxy(address owner) external returns (address) {
     return _deployProxy(owner);
   }
 
+  // Deploys a new proxy contract and transfers ownership to the sender
   function deployProxy() external returns (address) {
     return _deployProxy(msg.sender);
   }
 
+  // Deploys a new proxy contract and transfers ownership to the provided address
   function _deployProxy(address owner) private returns (address) {
     Proxy proxy = new Proxy();
     proxy._transferOwnership(owner);
     return address(proxy);
   }
-
-  // function deployArbitraryByteCode(address owner, bytes calldata initCode) external returns (address) {
-  //   return _deployArbitraryByteCode(owner, initCode);
-  // }
 }
--- packages/protocol/contracts/common/ProxyFactory08.sol
+++ packages/protocol/contracts/common/ProxyFactory08.sol
@@ -1,32 +1,37 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-import "@openzeppelin/contracts8/utils/Create2.sol";
 import "@celo-contracts/common/interfaces/IProxy.sol";
 
+import "@openzeppelin/contracts8/utils/Create2.sol";
+
 contract ProxyFactory08 {
+  // Deploys a new bytecode and transfers ownership to the provided address
+  // Calls with the same initCode that results in the same bytecode, sender and salt
+  // will revert as two contracts can not get deployed to the same address
   function deployArbitraryByteCode(
-    uint256 amount,
+    uint256 value,
     address owner,
     uint256 _salt,
     bytes memory initCode
   ) external returns (address) {
-    return _deployArbitraryByteCode(amount, owner, _salt, initCode);
+    return _deployArbitraryByteCode(value, owner, _salt, initCode);
   }
 
   function _deployArbitraryByteCode(
-    uint256 amount,
+    uint256 value,
     address owner,
     uint256 _salt,
     bytes memory initCode
   ) private returns (address) {
-    // will only deploy proxies
     address deployedContract = Create2.deploy(
-      amount,
+      value,
       keccak256(abi.encode(_salt, msg.sender)),
       abi.encodePacked(initCode)
     );
 
+    // function will fail if contract doesn't implement
+    // _transferOwnership function
     IProxy proxy = IProxy(deployedContract);
     proxy._transferOwnership(owner);
 
