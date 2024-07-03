f51c10ac55621fba6eef257865aec7104ff1bc64
soloseng <102702451+soloseng@users.noreply.github.com>
2024-07-03 13:48:57 -0400

    Soloseng/publishing-version-fix (#11105)
    * set override the default version
    
    * ++ npm tag in workflow trigger
    
    * PR feedback

--- packages/protocol/scripts/utils.ts
+++ packages/protocol/scripts/utils.ts
@@ -4,11 +4,6 @@ import { SemVer } from 'semver'
 
 const DAILY_RELEASE_TAG = 'canary'
 const WORKING_RELEASE_BRANCH_PREFIX = 'release/core-contracts/'
-const TAG_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]*[a-zA-Z0-9]$/
-
-// prerelease part extracted from official semver regex (can be found on https://semver.org/)
-const PRERELEASE_IDENTIFIER_REGEX =
-  /^(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*$/
 
 export type Exports = Record<
   string,
@@ -17,16 +12,6 @@ export type Exports = Record<
 
 export type JSON = Record<string, string | boolean | Exports>
 
-// versioning is done by either
-// A Git tag of the form `core-contracts.vX` (e.g. `core-contracts.v12`) === latest release
-// Otherwise if on the release branch a canary tagged release well be created bumping
-// the canary version by one (e.g. `@celo/contracts@11.0.0-canary.1`)
-// if not on a release branch a dry-run will be done unless an NPM_TAG is provided
-// in which case we will try to fetch the last published version with that tag and bump or use the canary to get major and start versioning from there the new tag at 0
-//  (e.g. `@celo/contracts@11.0.0@custom-tag.0`)
-// It also supports git tags starting with core-contracts.vX-ANY to derive custom npm tags,
-// for example `core-contracts.v11.2.3.post-audit` will result in determining
-// the next version as `11.2.3-post-audit.0` and `post-audit` npm tag
 export const determineNextVersion = (
   gitTag: string,
   gitBranch: string,
@@ -34,20 +19,15 @@ export const determineNextVersion = (
   npmTag: string
 ): SemVer | null => {
   let nextVersion: SemVer | null = null
-  const matchesReleaseTag = gitTag.match(/^core-contracts.v([0-9]+)$/)
-  // eslint-disable-next-line
-  const matchesAnyTag = gitTag.match(/core-contracts.v([0-9\.]+)\.(.+)/)
+  const matchesReleaseTag = gitTag.match(/core-contracts.v(.+).post-audit/)
+  const matchesPreAuditTag = gitTag.match(/core-contracts.v(.+).pre-audit/)
 
   if (matchesReleaseTag) {
     nextVersion = getVersionFromGitTag(matchesReleaseTag)
-  } else if (matchesAnyTag && isValidNpmTag(matchesAnyTag[2])) {
-    const tagFromGitTag = matchesAnyTag[2]
-    const isSemverCompliantPreReleaseTag = isValidPrereleaseIdentifier(tagFromGitTag)
-    const tag = isSemverCompliantPreReleaseTag ? tagFromGitTag : 'alpha'
-    const tempVersion = getVersionFromGitTag(matchesAnyTag)
-
+  } else if (matchesPreAuditTag) {
+    const tempVersion = getVersionFromGitTag(matchesPreAuditTag)
     nextVersion = new SemVer(
-      `${tempVersion.major}.${tempVersion.minor}.${tempVersion.patch}-${tag}.0`
+      `${tempVersion.major}.${tempVersion.minor}.${tempVersion.patch}-pre-audit.0`
     )
   } else if (gitBranch.startsWith(WORKING_RELEASE_BRANCH_PREFIX)) {
     const lastVersion = getPreviousVersion(npmPackage, DAILY_RELEASE_TAG, 'latest')
@@ -71,11 +51,7 @@ export const determineNextVersion = (
 }
 
 export function isValidNpmTag(tag?: string) {
-  return tag?.match(TAG_REGEX) !== null
-}
-
-export function isValidPrereleaseIdentifier(identifier: string) {
-  return PRERELEASE_IDENTIFIER_REGEX.test(identifier)
+  return tag?.match(/^[a-zA-Z]{1,}[a-zA-Z-]*[a-zA-Z]{1,}$/) !== null
 }
 
 // get the previous version for this tag or if not exists find the previous for the fallback
