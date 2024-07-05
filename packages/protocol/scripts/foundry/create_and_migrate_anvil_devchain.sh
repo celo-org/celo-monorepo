05f3532387936fe8073d34b4076dfd673a3f0f16
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-07-05 15:00:33 +0100

    chore(workflows): debugging `jq` CI error

--- packages/protocol/scripts/foundry/create_and_migrate_anvil_devchain.sh
+++ packages/protocol/scripts/foundry/create_and_migrate_anvil_devchain.sh
@@ -36,6 +36,7 @@ time forge build $LIBRARY_FLAGS
 source $PWD/scripts/foundry/deploy_precompiles.sh
 
 echo "Setting Registry Proxy"
+echo "DEBUGGING $(ls -l)"
 PROXY_DEPLOYED_BYTECODE=$(jq -r '.deployedBytecode' $PWD/build/contracts/Proxy.json)
 cast rpc anvil_setCode $REGISTRY_ADDRESS $PROXY_DEPLOYED_BYTECODE --rpc-url $ANVIL_RPC_URL
 
