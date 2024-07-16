d6822c358f9df088724b0e6f5dae97f4e6231258
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-07-16 16:38:36 +0100

    refactor(OdisPayments.t.sol): use constants

--- packages/protocol/test-sol/unit/identity/OdisPayments.t.sol
+++ packages/protocol/test-sol/unit/identity/OdisPayments.t.sol
@@ -3,12 +3,14 @@ pragma solidity ^0.5.13;
 pragma experimental ABIEncoderV2;
 
 import "celo-foundry/Test.sol";
+import { TestConstants } from "@test-sol/constants.sol";
+
 import "@celo-contracts/identity/OdisPayments.sol";
 import { StableToken } from "@mento-core/contracts/StableToken.sol";
 import "@celo-contracts/common/Registry.sol";
 import "@celo-contracts/common/Freezer.sol";
 
-contract OdisPaymentsFoundryTest is Test {
+contract OdisPaymentsFoundryTest is Test, TestConstants {
   uint256 FIXED1 = 1000000000000000000000000;
   uint256 SECONDS_IN_A_DAY = 60 * 60 * 24;
   uint256 startingBalanceCUSD = 1000;
@@ -24,11 +26,9 @@ contract OdisPaymentsFoundryTest is Test {
   event PaymentMade(address indexed account, uint256 valueInCUSD);
 
   function setUp() public {
-    address registryAddress = 0x000000000000000000000000000000000000ce10;
+    deployCodeTo("Registry.sol", abi.encode(false), REGISTRY_ADDRESS);
 
-    deployCodeTo("Registry.sol", abi.encode(false), registryAddress);
-
-    registry = Registry(registryAddress);
+    registry = Registry(REGISTRY_ADDRESS);
     freezer = new Freezer(true);
     odisPayments = new OdisPayments(true);
     stableToken = new StableToken(true);
