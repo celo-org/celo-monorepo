3334cdc34d197bcfb58de15314d47a9b860e77c4
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-07-22 18:36:21 +0100

    test(UniswapFeeHandlerSeller.t.sol): adds test for setting router to zero address
    All tests pass as expected ✅
    
    ```sh
    forge test -vvvv \
    --match-path "test-sol/unit/common/UniswapFeeHandlerSeller.t.sol"
    ```

--- packages/protocol/test-sol/unit/common/UniswapFeeHandlerSeller.t.sol
+++ packages/protocol/test-sol/unit/common/UniswapFeeHandlerSeller.t.sol
@@ -13,6 +13,7 @@ contract UniswapFeeHandlerSellerTest is Test, TestConstants {
   address TOKEN_ADDRESS = actor("Arbitrary Token Address");
   address ROUTER_ADDRESS = actor("Arbitrary Router Address");
   address NON_OWNER_ADDRESS = actor("Arbitrary Non-Owner");
+  address ZERO_ADDRESS = address(0);
 
   // Contract instance
   UniswapFeeHandlerSeller uniswapFeeHandlerSeller;
@@ -35,5 +36,12 @@ contract UniswapFeeHandlerSellerTest_SetRouter is UniswapFeeHandlerSellerTest {
 
     vm.expectRevert("Ownable: caller is not the owner");
     uniswapFeeHandlerSeller.setRouter(TOKEN_ADDRESS, ROUTER_ADDRESS);
+  }
+
+  function test_SetRouter_ShouldRevertWhen_SettingRouterToZeroAddress() public {
+    vm.prank(uniswapFeeHandlerSeller.owner());
+
+    vm.expectRevert("Router can't be address zero");
+    uniswapFeeHandlerSeller.setRouter(TOKEN_ADDRESS, ZERO_ADDRESS);
   }
 }
