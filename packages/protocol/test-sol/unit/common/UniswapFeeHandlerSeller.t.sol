e3a28a60f29ec363f9c5da4da7f70a839b881dd7
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-07-22 18:22:26 +0100

    test(UniswapFeeHandlerSeller.t.sol): adds MVP test outline
    First test passes as expected ✅
    
    ```sh
    forge test -vvvv \
    --match-path "test-sol/unit/common/UniswapFeeHandlerSeller.t.sol"
    ```

---
+++ packages/protocol/test-sol/unit/common/UniswapFeeHandlerSeller.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.5.13;
+pragma experimental ABIEncoderV2;
+
+// Helper contracts
+import { Test } from "celo-foundry/Test.sol";
+import { TestConstants } from "@test-sol/constants.sol";
+
+import { UniswapFeeHandlerSeller } from "@celo-contracts/common/UniswapFeeHandlerSeller.sol";
+
+contract UniswapFeeHandlerSellerTest is Test, TestConstants {
+  // Actors
+  address TOKEN_ADDRESS = actor("Arbitrary Token Address");
+  address ROUTER_ADDRESS = actor("Arbitrary Router Address");
+
+  // Contract instance
+  UniswapFeeHandlerSeller uniswapFeeHandlerSeller;
+
+  function setUp() public {
+    uniswapFeeHandlerSeller = new UniswapFeeHandlerSeller(true);
+  }
+}
+
+contract UniswapFeeHandlerSellerTest_SetRouter is UniswapFeeHandlerSellerTest {
+  function test_SetRouter_ShouldSucceedWhen_CalledByOwner() public {
+    vm.prank(uniswapFeeHandlerSeller.owner());
+    uniswapFeeHandlerSeller.setRouter(TOKEN_ADDRESS, ROUTER_ADDRESS);
+
+    assertEq(uniswapFeeHandlerSeller.getRoutersForToken(TOKEN_ADDRESS)[0], ROUTER_ADDRESS);
+  }
+}
