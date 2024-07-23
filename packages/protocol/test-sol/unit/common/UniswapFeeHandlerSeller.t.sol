a7ca20f3d37e6ea27e553835df886ccf007b5301
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-07-23 15:26:12 +0100

    test(FeeHandlerSeller.t.sol): adds test for large list and applies linting

--- packages/protocol/test-sol/unit/common/UniswapFeeHandlerSeller.t.sol
+++ packages/protocol/test-sol/unit/common/UniswapFeeHandlerSeller.t.sol
@@ -8,18 +8,16 @@ import { TestConstants } from "@test-sol/constants.sol";
 
 import { UniswapFeeHandlerSeller } from "@celo-contracts/common/UniswapFeeHandlerSeller.sol";
 
-import { console2 as console } from "celo-foundry/Test.sol";
-
 contract UniswapFeeHandlerSellerTest is Test, TestConstants {
   // Actors
-  address TOKEN_ADDRESS = actor("Arbitrary Token Address");
+  address ZERO_ADDRESS = address(0);
+  address UNISWAP_FEE_HANDLER_SELLER_OWNER_ADDRESS;
+  address NON_OWNER_ADDRESS = actor("Arbitrary Non-Owner");
+  address ARBITRARY_TOKEN_ADDRESS = actor("Arbitrary Token Address");
   address ARBITRARY_ROUTER_ADDRESS_A = actor("Arbitrary Router Address A");
   address ARBITRARY_ROUTER_ADDRESS_B = actor("Arbitrary Router Address B");
   address ARBITRARY_ROUTER_ADDRESS_C = actor("Arbitrary Router Address C");
   address ARBITRARY_ROUTER_ADDRESS_D = actor("Arbitrary Router Address D");
-  address NON_OWNER_ADDRESS = actor("Arbitrary Non-Owner");
-  address UNISWAP_FEE_HANDLER_SELLER_OWNER_ADDRESS;
-  address ZERO_ADDRESS = address(0);
 
   // Contract instance
   UniswapFeeHandlerSeller uniswapFeeHandlerSeller;
@@ -33,10 +31,10 @@ contract UniswapFeeHandlerSellerTest is Test, TestConstants {
 contract UniswapFeeHandlerSellerTest_SetRouter is UniswapFeeHandlerSellerTest {
   function test_SetRouter_ShouldSucceedWhen_CalledByOwner() public {
     vm.prank(UNISWAP_FEE_HANDLER_SELLER_OWNER_ADDRESS);
-    uniswapFeeHandlerSeller.setRouter(TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_A);
+    uniswapFeeHandlerSeller.setRouter(ARBITRARY_TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_A);
 
     assertEq(
-      uniswapFeeHandlerSeller.getRoutersForToken(TOKEN_ADDRESS)[0],
+      uniswapFeeHandlerSeller.getRoutersForToken(ARBITRARY_TOKEN_ADDRESS)[0],
       ARBITRARY_ROUTER_ADDRESS_A
     );
   }
@@ -45,24 +43,24 @@ contract UniswapFeeHandlerSellerTest_SetRouter is UniswapFeeHandlerSellerTest {
     vm.prank(NON_OWNER_ADDRESS);
 
     vm.expectRevert("Ownable: caller is not the owner");
-    uniswapFeeHandlerSeller.setRouter(TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_A);
+    uniswapFeeHandlerSeller.setRouter(ARBITRARY_TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_A);
   }
 
   function test_SetRouter_ShouldRevertWhen_SettingRouterToZeroAddress() public {
     vm.prank(UNISWAP_FEE_HANDLER_SELLER_OWNER_ADDRESS);
 
     vm.expectRevert("Router can't be address zero");
-    uniswapFeeHandlerSeller.setRouter(TOKEN_ADDRESS, ZERO_ADDRESS);
+    uniswapFeeHandlerSeller.setRouter(ARBITRARY_TOKEN_ADDRESS, ZERO_ADDRESS);
   }
 
   function test_SetRouter_ShouldRevertWhen_SettingMoreRoutesThanMaxAllowed() public {
     vm.startPrank(UNISWAP_FEE_HANDLER_SELLER_OWNER_ADDRESS);
-    uniswapFeeHandlerSeller.setRouter(TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_A);
-    uniswapFeeHandlerSeller.setRouter(TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_B);
-    uniswapFeeHandlerSeller.setRouter(TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_C); // Max number of routers
+    uniswapFeeHandlerSeller.setRouter(ARBITRARY_TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_A);
+    uniswapFeeHandlerSeller.setRouter(ARBITRARY_TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_B);
+    uniswapFeeHandlerSeller.setRouter(ARBITRARY_TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_C); // Max number of routers
 
     vm.expectRevert("Max number of routers reached");
-    uniswapFeeHandlerSeller.setRouter(TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_D); // Attempt to set one more router
+    uniswapFeeHandlerSeller.setRouter(ARBITRARY_TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_D); // Attempt to set one more router
     vm.stopPrank();
   }
 }
@@ -72,37 +70,47 @@ contract UniswapFeeHandlerSellerTest_RemoveRouter is UniswapFeeHandlerSellerTest
     super.setUp();
 
     vm.prank(UNISWAP_FEE_HANDLER_SELLER_OWNER_ADDRESS);
-    uniswapFeeHandlerSeller.setRouter(TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_A);
+    uniswapFeeHandlerSeller.setRouter(ARBITRARY_TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_A);
   }
 
   function test_RemoveRouter_ShouldSucceedWhen_CalledByOwner() public {
     vm.prank(UNISWAP_FEE_HANDLER_SELLER_OWNER_ADDRESS);
-    uniswapFeeHandlerSeller.removeRouter(TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_A);
+    uniswapFeeHandlerSeller.removeRouter(ARBITRARY_TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_A);
 
-    assertEq(uniswapFeeHandlerSeller.getRoutersForToken(TOKEN_ADDRESS).length, 0);
+    assertEq(uniswapFeeHandlerSeller.getRoutersForToken(ARBITRARY_TOKEN_ADDRESS).length, 0);
   }
 
-  // function test_RemoveRouter_ShouldSucceedWhen_ListIsLarge() public {
-  //   address[] memory EXPECTED_ROUTERS;
+  function test_RemoveRouter_ShouldSucceedWhen_ListIsLarge() public {
+    address[2] memory EXPECTED_ROUTERS;
 
-  //   vm.startPrank(UNISWAP_FEE_HANDLER_SELLER_OWNER_ADDRESS);
-  //   uniswapFeeHandlerSeller.setRouter(TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_A);
-  //   uniswapFeeHandlerSeller.setRouter(TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_B);
-  //   uniswapFeeHandlerSeller.setRouter(TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_C);
-  //   uniswapFeeHandlerSeller.removeRouter(TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_C);
-  //   vm.stopPrank();
+    vm.startPrank(UNISWAP_FEE_HANDLER_SELLER_OWNER_ADDRESS);
+    uniswapFeeHandlerSeller.setRouter(ARBITRARY_TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_A);
+    uniswapFeeHandlerSeller.setRouter(ARBITRARY_TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_B);
+    uniswapFeeHandlerSeller.setRouter(ARBITRARY_TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_C);
+    uniswapFeeHandlerSeller.removeRouter(ARBITRARY_TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_C);
+    vm.stopPrank();
 
-  //   EXPECTED_ROUTERS[0] = ARBITRARY_ROUTER_ADDRESS_A;
-  //   EXPECTED_ROUTERS[1] = ARBITRARY_ROUTER_ADDRESS_B;
+    EXPECTED_ROUTERS[0] = ARBITRARY_ROUTER_ADDRESS_A;
+    EXPECTED_ROUTERS[1] = ARBITRARY_ROUTER_ADDRESS_B;
 
-  //   assertEq(uniswapFeeHandlerSeller.getRoutersForToken(TOKEN_ADDRESS).length, EXPECTED_ROUTERS.length);
-  //   assertEq(uniswapFeeHandlerSeller.getRoutersForToken(TOKEN_ADDRESS), EXPECTED_ROUTERS);
-  // }
+    assertEq(
+      uniswapFeeHandlerSeller.getRoutersForToken(ARBITRARY_TOKEN_ADDRESS).length,
+      EXPECTED_ROUTERS.length
+    );
+    assertEq(
+      uniswapFeeHandlerSeller.getRoutersForToken(ARBITRARY_TOKEN_ADDRESS)[0],
+      EXPECTED_ROUTERS[0]
+    ); // Can't use assertEq for arrays
+    assertEq(
+      uniswapFeeHandlerSeller.getRoutersForToken(ARBITRARY_TOKEN_ADDRESS)[1],
+      EXPECTED_ROUTERS[1]
+    );
+  }
 
   function test_RemoveRouter_ShouldRevertWhen_CalledByNonOwner() public {
     vm.prank(NON_OWNER_ADDRESS);
 
     vm.expectRevert("Ownable: caller is not the owner");
-    uniswapFeeHandlerSeller.removeRouter(TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_A);
+    uniswapFeeHandlerSeller.removeRouter(ARBITRARY_TOKEN_ADDRESS, ARBITRARY_ROUTER_ADDRESS_A);
   }
 }
