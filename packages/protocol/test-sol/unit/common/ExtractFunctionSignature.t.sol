a3e8d56427a50bd1bcad186d362ddfea3ca0f472
pahor167 <hornak.pavel@gmail.com>
2024-07-25 14:28:04 +0200

    extract function signature

--- packages/protocol/test-sol/unit/common/ExtractFunctionSignature.t.sol
+++ packages/protocol/test-sol/unit/common/ExtractFunctionSignature.t.sol
@@ -1,47 +1,47 @@
-// // SPDX-License-Identifier: LGPL-3.0-only
-// pragma solidity ^0.5.13;
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.8.7 <0.8.20;
 
-// import "celo-foundry/Test.sol";
+import "celo-foundry-8/Test.sol";
 
-// import "@celo-contracts/common/ExtractFunctionSignature.sol";
+import "@celo-contracts/common/ExtractFunctionSignature.sol";
 
-// contract ExtractFunctionSignatureWrapper {
-//   function extractFunctionSignature(bytes memory input) public pure returns (bytes4) {
-//     return ExtractFunctionSignature.extractFunctionSignature(input);
-//   }
-// }
+contract ExtractFunctionSignatureWrapper {
+  function extractFunctionSignature(bytes memory input) public pure returns (bytes4) {
+    return ExtractFunctionSignature.extractFunctionSignature(input);
+  }
+}
 
-// contract TestTransactions {
-//   mapping(uint256 => uint256) public values;
+contract TestTransactions {
+  mapping(uint256 => uint256) public values;
 
-//   function getValue(uint256 key) external view returns (uint256) {
-//     return values[key];
-//   }
+  function getValue(uint256 key) external view returns (uint256) {
+    return values[key];
+  }
 
-//   function setValue(uint256 key, uint256 value, bool shouldSucceed) external {
-//     require(shouldSucceed);
-//     values[key] = value;
-//   }
-// }
+  function setValue(uint256 key, uint256 value, bool shouldSucceed) external {
+    require(shouldSucceed);
+    values[key] = value;
+  }
+}
 
-// contract ExtractFunctionSignatureTest is Test {
-//   ExtractFunctionSignatureWrapper extractFunctionSignature;
-//   TestTransactions testTransactions;
+contract ExtractFunctionSignatureTest is Test {
+  ExtractFunctionSignatureWrapper extractFunctionSignature;
+  TestTransactions testTransactions;
 
-//   function setUp() public {
-//     extractFunctionSignature = new ExtractFunctionSignatureWrapper();
-//     testTransactions = new TestTransactions();
-//   }
+  function setUp() public {
+    extractFunctionSignature = new ExtractFunctionSignatureWrapper();
+    testTransactions = new TestTransactions();
+  }
 
-//   function test_ExtractFunctionSignature() public {
-//     // Arrange
-//     bytes memory data = abi.encodeWithSignature("setValue(uint256,uint256,bool)", 1, 1, true);
-//     bytes4 expectedSignature = bytes4(keccak256("setValue(uint256,uint256,bool)"));
+  function test_ExtractFunctionSignature() public {
+    // Arrange
+    bytes memory data = abi.encodeWithSignature("setValue(uint256,uint256,bool)", 1, 1, true);
+    bytes4 expectedSignature = bytes4(keccak256("setValue(uint256,uint256,bool)"));
 
-//     // Act
-//     bytes4 result = extractFunctionSignature.extractFunctionSignature(data);
+    // Act
+    bytes4 result = extractFunctionSignature.extractFunctionSignature(data);
 
-//     // Assert
-//     assertEq(result, expectedSignature);
-//   }
-// }
+    // Assert
+    assertEq(result, expectedSignature);
+  }
+}
