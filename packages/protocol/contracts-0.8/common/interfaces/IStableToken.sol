ae477a2d12642d293462883c338b2b579282662f
pahor167 <hornak.pavel@gmail.com>
2024-07-26 10:29:21 +0200

    lint

--- packages/protocol/contracts-0.8/common/interfaces/IStableToken.sol
+++ packages/protocol/contracts-0.8/common/interfaces/IStableToken.sol
@@ -12,6 +12,20 @@ interface IStableToken {
 
     function setInflationParameters(uint256, uint256) external;
 
+    function initialize(
+        string calldata _name,
+        string calldata _symbol,
+        uint8 _decimals,
+        address registryAddress,
+        uint256 inflationRate,
+        uint256 inflationFactorUpdatePeriod,
+        address[] calldata initialBalanceAddresses,
+        uint256[] calldata initialBalanceValues,
+        string calldata exchangeIdentifier
+    ) external;
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
     function valueToUnits(uint256) external view returns (uint256);
 
     function unitsToValue(uint256) external view returns (uint256);
@@ -28,18 +42,4 @@ interface IStableToken {
 
     // NOTE: duplicated with IERC20.sol, remove once interface inheritance is supported.
     function balanceOf(address) external view returns (uint256);
-
-    function initialize(
-        string calldata _name,
-        string calldata _symbol,
-        uint8 _decimals,
-        address registryAddress,
-        uint256 inflationRate,
-        uint256 inflationFactorUpdatePeriod,
-        address[] calldata initialBalanceAddresses,
-        uint256[] calldata initialBalanceValues,
-        string calldata exchangeIdentifier
-    ) external;
-
-    function transfer(address recipient, uint256 amount) external returns (bool);
 }
--- packages/protocol/contracts/common/interfaces/IReserve.sol
+++ packages/protocol/contracts/common/interfaces/IReserve.sol
@@ -25,15 +25,7 @@ interface IReserve {
 
     function transferExchangeGold(address payable, uint256) external returns (bool);
 
-    function getReserveGoldBalance() external view returns (uint256);
-
-    function getUnfrozenReserveGoldBalance() external view returns (uint256);
-
     function getOrComputeTobinTax() external returns (uint256, uint256);
-
-    function getTokens() external view returns (address[] memory);
-
-    function getReserveRatio() external view returns (uint256);
 
     function addExchangeSpender(address) external;
 
@@ -42,4 +34,12 @@ interface IReserve {
     function addSpender(address) external;
 
     function removeSpender(address) external;
+
+    function getReserveGoldBalance() external view returns (uint256);
+
+    function getUnfrozenReserveGoldBalance() external view returns (uint256);
+
+    function getTokens() external view returns (address[] memory);
+
+    function getReserveRatio() external view returns (uint256);
 }
