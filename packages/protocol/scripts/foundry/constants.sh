df87e3fd448683917e48ff3c507c3cebc5a95041
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-07-26 15:00:00 +0100

    refactor(constant.sh): removes anvil private key (not used anymore)

--- packages/protocol/scripts/foundry/constants.sh
+++ packages/protocol/scripts/foundry/constants.sh
@@ -1,8 +1,6 @@
 # Anvil accounts
 export FROM_ACCOUNT_NO_ZERO="f39Fd6e51aad88F6F4ce6aB8827279cffFb92266" # Anvil default account (1)
 export FROM_ACCOUNT="0x$FROM_ACCOUNT_NO_ZERO" # Anvil default account (1)
-export FROM_ACCOUNT_PRIVATE_KEY_NO_ZERO="ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80" # Anvil default account (1)
-export FROM_ACCOUNT_PRIVATE_KEY="0x$FROM_ACCOUNT_PRIVATE_KEY_NO_ZERO" # Anvil default account (1)
 
 # Anvil configurations (Source: https://book.getfoundry.sh/reference/anvil/)
 export ANVIL_PORT=8546
