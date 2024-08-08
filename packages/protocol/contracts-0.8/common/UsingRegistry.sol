43a6bb272ae92c6ac6c16a83d0a825bca9fffeda
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-08-08 16:00:56 +0100

    feat(UsingRegistry): adds EpochRewards

--- packages/protocol/contracts-0.8/common/UsingRegistry.sol
+++ packages/protocol/contracts-0.8/common/UsingRegistry.sol
@@ -18,8 +18,9 @@ import "../../contracts/governance/interfaces/IValidators.sol";
 import "../../contracts/stability/interfaces/ISortedOracles.sol";
 import "../../contracts/common/interfaces/IFeeCurrencyWhitelist.sol";
 import "../../contracts/governance/interfaces/IElection.sol";
-
 import "../../contracts/common/interfaces/IFeeHandlerSeller.sol";
+import "../../contracts/governance/interfaces/IEpochRewards.sol";
+
 
 contract UsingRegistry is Ownable {
   // solhint-disable state-visibility
@@ -29,6 +30,7 @@ contract UsingRegistry is Ownable {
   bytes32 constant DOUBLE_SIGNING_SLASHER_REGISTRY_ID =
     keccak256(abi.encodePacked("DoubleSigningSlasher"));
   bytes32 constant ELECTION_REGISTRY_ID = keccak256(abi.encodePacked("Election"));
+  bytes32 constant EPOCH_REWARDS_REGISTRY_ID = keccak256(abi.encodePacked("EpochRewards"));
   bytes32 constant EXCHANGE_REGISTRY_ID = keccak256(abi.encodePacked("Exchange"));
   bytes32 constant FEE_CURRENCY_WHITELIST_REGISTRY_ID =
     keccak256(abi.encodePacked("FeeCurrencyWhitelist"));
@@ -123,6 +125,11 @@ contract UsingRegistry is Ownable {
   function getElection() internal view returns (IElection) {
     return IElection(registry.getAddressForOrDie(ELECTION_REGISTRY_ID));
   }
+
+  function getEpochRewards() internal view returns (IEpochRewards) {
+    return IEpochRewards(registry.getAddressForOrDie(EPOCH_REWARDS_REGISTRY_ID));
+  }
+
   function getGovernance() internal view returns (IGovernance) {
     return IGovernance(registry.getAddressForOrDie(GOVERNANCE_REGISTRY_ID));
   }
