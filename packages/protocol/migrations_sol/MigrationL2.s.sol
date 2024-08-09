1b25ad0525c0cb2cda1d41d64bc32202f6cce080
Arthur Gousset <46296830+arthurgousset@users.noreply.github.com>
2024-08-09 11:35:13 +0100

    refactor: uses `L2Migration.s.sol` and simplifies migration script (#11177)
    * refactor(constants.sh): renames target contract variable for better readability
    
    * fix(interfaces/ICeloDistributionSchedule): removes 1 argument from `activate` function
    
    As far as I can see, this interface was not defined correctly and had an argument too much.
    
    * refactor(MigrationL2.s.sol): moves `activate` call to script and refactors bash file
    
    Left two TODO comments.
    
    * refactor(constants): uses `DEPLOYER_ACCOUNT`
    
    * refactor(constant.sh): removes anvil private key (not used anymore)
    
    * refactor(Migrations.s.sol): add `setupUsingRegistry()` to improve readability
    
    * style(scripts/foundry): indent lines that don't start a command
    
    * feat(IEpochRewards): adds new interface
    
    * feat(UsingRegistry): adds EpochRewards
    
    * refactor(MigrationL2): gets fractions from EpochRewards.sol
    
    * chore(MigrationL2): delete console.log import
    
    * chore(MigrationL2): deletes `TODO` comment
    
    * style(protocol): linting
    
    * Test celo-monorepo.yml trigger filters
    
    * Remove useless workflow triggers
    
    * Run on repository runners
    
    ---------
    
    Co-authored-by: Javier Cortejoso <javier.cortejoso@gmail.com>

---
+++ packages/protocol/migrations_sol/MigrationL2.s.sol
@@ -0,0 +1,42 @@
+pragma solidity >=0.8.7 <0.8.20;
+
+import { Script } from "forge-std-8/Script.sol";
+import { MigrationsConstants } from "@migrations-sol/constants.sol";
+
+import "@celo-contracts/common/FixidityLib.sol";
+import "@celo-contracts-8/common/UsingRegistry.sol";
+
+contract MigrationL2 is Script, MigrationsConstants, UsingRegistry {
+  using FixidityLib for FixidityLib.Fraction;
+
+  /**
+   * Entry point of the script
+   */
+  function run() external {
+    vm.startBroadcast(DEPLOYER_ACCOUNT);
+
+    setupUsingRegistry();
+    activateCeloDistributionSchedule();
+
+    vm.stopBroadcast();
+  }
+
+  function setupUsingRegistry() public {
+    _transferOwnership(DEPLOYER_ACCOUNT);
+    setRegistry(REGISTRY_ADDRESS);
+  }
+
+  function activateCeloDistributionSchedule() public {
+    uint256 l2StartTime = 1721909903 - 5; // Arbitrarily 5 seconds before last black
+    uint256 communityRewardFraction = getEpochRewards().getCommunityRewardFraction();
+    address carbonOffsettingPartner = 0x22579CA45eE22E2E16dDF72D955D6cf4c767B0eF;
+    uint256 carbonOffsettingFraction = getEpochRewards().getCarbonOffsettingFraction();
+
+    getCeloDistributionSchedule().activate(
+      l2StartTime,
+      communityRewardFraction,
+      carbonOffsettingPartner,
+      carbonOffsettingFraction
+    );
+  }
+}
