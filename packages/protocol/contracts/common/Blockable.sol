b2fbd507c126f10b7d4766f8c68626a115e3311b
Martín Volpe <martin.volpe@gmail.com>
2024-08-16 16:25:46 -0300

    blocker and blockable implemented

--- packages/protocol/contracts/common/Blockable.sol
+++ packages/protocol/contracts/common/Blockable.sol
@@ -2,25 +2,38 @@ pragma solidity >=0.5.13 <0.9.0;
 
 import "./interfaces/IBlockable.sol";
 import "./interfaces/IBlocker.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract Blockable is IBlockable {
+contract Blockable is IBlockable, Ownable {
   IBlocker blockedBy;
 
+  // isBlocked will default to false if blockable not set
+  function isBlocked() external view returns (bool) {
+    // require(address(blockedBy) != address(0), "Blocker contract not set");
+    return _isBlocked();
+  }
+
+  function _isBlocked() internal view returns (bool) {
+    if (address(blockedBy) == address(0)) {
+      return false;
+    }
+    return blockedBy.isBlocked();
+  }
+
+  modifier onlyWhenNotBlocked() {
+    require(!_isBlocked(), "Contract is blocked from performin this action");
+    _;
+  }
+
+  function getBlockedbyContract() external view returns (address) {
+    return address(blockedBy);
+  }
 
   function _setBlockedBy(address _blockedBy) internal {
     blockedBy = IBlocker(_blockedBy);
   }
 
-  function setBlockedByContract(address _blockedBy) external {
+  function setBlockedByContract(address _blockedBy) external onlyOwner {
     _setBlockedBy(_blockedBy);
   }
-
-  function isBlocked() external view returns (bool){
-    returns false;
-  }
-  
-  function getBlockedbyContract() external view returns (address){
-    returns address(0);
-  }
-
 }
--- packages/protocol/contracts/common/interfaces/IBlockable.sol
+++ packages/protocol/contracts/common/interfaces/IBlockable.sol
@@ -3,5 +3,5 @@ pragma solidity >=0.5.13 <0.9.0;
 interface IBlockable {
   function isBlocked() external view returns (bool);
   function getBlockedbyContract() external view returns (address);
-  function setBlockedByContract(address _blockedBy) external
+  function setBlockedByContract(address _blockedBy) external;
 }
