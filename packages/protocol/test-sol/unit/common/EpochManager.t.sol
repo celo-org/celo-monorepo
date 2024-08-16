0b473c231b9f5c3b23a2d28d01e3d8728d344434
pahor167 <hornak.pavel@gmail.com>
2024-08-16 15:18:00 +0200

    First few tests

--- packages/protocol/test-sol/unit/common/EpochManager.t.sol
+++ packages/protocol/test-sol/unit/common/EpochManager.t.sol
@@ -3,31 +3,56 @@ pragma solidity >=0.8.7 <0.8.20;
 
 import "celo-foundry-8/Test.sol";
 import "@celo-contracts-8/common/EpochManager.sol";
+import { TestConstants } from "@test-sol/constants.sol";
 
-contract EpochManagerTest is Test {
+import "@celo-contracts/stability/test/MockSortedOracles.sol";
+
+contract EpochManagerTest is Test, TestConstants {
   EpochManager epochManager;
+  MockSortedOracles sortedOracles;
+  address epochManagerInitializer;
+  address carbonOffsettingPartner;
+  address communityRewardFund;
 
   uint256 firstEpochNumber = 100;
   uint256 firstEpochBlock = 100;
   address[] firstElected;
 
+  IRegistry registry;
+
   function setUp() public virtual {
     epochManager = new EpochManager(true);
-     firstElected.push(actor("validator1"));
-     firstElected.push(actor("validator2"));
+    sortedOracles = new MockSortedOracles();
 
+
+
+    firstElected.push(actor("validator1"));
+    firstElected.push(actor("validator2"));
+
+    epochManagerInitializer = actor("initializer");
+    carbonOffsettingPartner = actor("carbonOffsettingPartner");
+    communityRewardFund = actor("communityRewardFund");
+
+    deployCodeTo("Registry.sol", abi.encode(false), REGISTRY_ADDRESS);
+    registry = IRegistry(REGISTRY_ADDRESS);
+
+    registry.setAddressFor("EpochManagerInitializer", epochManagerInitializer);
+    registry.setAddressFor("SortedOracles", address(sortedOracles));
+
+
+    epochManager.initialize(REGISTRY_ADDRESS, 10, carbonOffsettingPartner, communityRewardFund);
   }
 }
 
 contract EpochManagerinitializeSystem is EpochManagerTest {
 
   function test_processCanBeStarted() public virtual{
-    // vm.prank(initializerAddress);
+    vm.prank(epochManagerInitializer);
     epochManager.initializeSystem(firstEpochNumber, firstEpochBlock, firstElected);
   }
 
   function test_Reverts_processCannotBeStartedAgain() public virtual {
-    // vm.prank(initializerAddress);
+    vm.startPrank(epochManagerInitializer);
     epochManager.initializeSystem(firstEpochNumber, firstEpochBlock, firstElected);
     vm.expectRevert("Epoch system already initialized");
     epochManager.initializeSystem(firstEpochNumber, firstEpochBlock, firstElected);
@@ -35,7 +60,7 @@ contract EpochManagerinitializeSystem is EpochManagerTest {
   }
 
   function test_Reverts_WhenSystemInitializedByOtherContract() public virtual {
-    vm.expectRevert("Not the initializer");
+    vm.expectRevert("msg.sender is not Initializer");
     epochManager.initializeSystem(firstEpochNumber, firstEpochBlock, firstElected);
 
   }
