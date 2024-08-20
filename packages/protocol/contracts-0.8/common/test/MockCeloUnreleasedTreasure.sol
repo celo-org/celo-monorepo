b8f55844c689cb7b5e2ac2e6ac01cc233bf33894
soloseng <102702451+soloseng@users.noreply.github.com>
2024-08-20 10:34:06 -0400

    -- logging

--- packages/protocol/contracts-0.8/common/test/MockCeloUnreleasedTreasure.sol
+++ packages/protocol/contracts-0.8/common/test/MockCeloUnreleasedTreasure.sol
@@ -3,13 +3,9 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "../../../contracts/common/interfaces/ICeloUnreleasedTreasure.sol";
 
-import "forge-std-8/console2.sol";
-
 /**
  * @title A mock CeloDistributionSchedule for testing.
  */
 contract MockCeloUnreleasedTreasure is ICeloUnreleasedTreasure {
-  function release(address to, uint256 amount) external {
-    console2.log("Released");
-  }
+  function release(address to, uint256 amount) external {}
 }
