9ba9e31763caf23431dc3861cc150137bf276392
pahor167 <hornak.pavel@gmail.com>
2024-08-21 15:52:41 +0200

    Validators to 0.8 config

--- packages/protocol/contractPackages.ts
+++ packages/protocol/contractPackages.ts
@@ -48,12 +48,13 @@ export const SOLIDITY_08_PACKAGE = {
   proxiesPath: '/', // Proxies are still with 0.5 contracts
   // Proxies shouldn't have to be added to a list manually
   // https://github.com/celo-org/celo-monorepo/issues/10555
-  contracts: ['GasPriceMinimum', 'FeeCurrencyDirectory', 'CeloDistributionSchedule'],
+  contracts: ['GasPriceMinimum', 'FeeCurrencyDirectory', 'CeloDistributionSchedule', 'Validators'],
   proxyContracts: [
     'GasPriceMinimumProxy',
     'FeeCurrencyDirectoryProxy',
     'MentoFeeCurrencyAdapterV1',
     'CeloDistributionScheduleProxy',
+    'ValidatorsProxy',
   ],
   truffleConfig: 'truffle-config0.8.js',
 } satisfies ContractPackage
--- packages/protocol/contracts-0.8/stability/CeloFeeCurrencyAdapterOwnable.sol
+++ packages/protocol/contracts-0.8/stability/CeloFeeCurrencyAdapterOwnable.sol
@@ -9,15 +9,4 @@ contract CeloFeeCurrencyAdapterOwnable is FeeCurrencyAdapterOwnable {
    * @param test Set to true to skip implementation initialization
    */
   constructor(bool test) FeeCurrencyAdapterOwnable(test) {}
-
-  /**
-   * @notice Returns the storage, major, minor, and patch version of the contract.
-   * @return Storage version of the contract.
-   * @return Major version of the contract.
-   * @return Minor version of the contract.
-   * @return Patch version of the contract.
-   */
-  function getVersionNumber() external pure returns (uint256, uint256, uint256, uint256) {
-    return (1, 1, 0, 0);
-  }
 }
