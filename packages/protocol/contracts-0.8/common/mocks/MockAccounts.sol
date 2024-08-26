78c53037a2f7b5d7e6d84fa9c6ba870674a0303f
Marcin Chrzanowski <martin@clabs.co>
2024-08-26 22:49:18 +0200

    Use relative import

--- packages/protocol/contracts-0.8/common/mocks/MockAccounts.sol
+++ packages/protocol/contracts-0.8/common/mocks/MockAccounts.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.7 <0.8.20;
 
-import "@celo-contracts/common/FixidityLib.sol";
+import "../../../contracts/common/FixidityLib.sol";
 
 contract MockAccounts {
   using FixidityLib for FixidityLib.Fraction;
