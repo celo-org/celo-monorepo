fc278cbf50e4868bfc3fde036a7e69c2b7b2973c
Martín Volpe <martin.volpe@gmail.com>
2024-09-17 16:15:47 +0200

    Force compilation of validator

---
+++ packages/protocol/test-sol/unit/governance/mock/CompileValidatorMock.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.7 <0.8.20;
+
+import "celo-foundry-8/Test.sol";
+
+// here only to forge compile of  ValidatorsMock
+import "./ValidatorsMock.sol";
+
+contract DowntimeSlasherMock is Test {
+  function test_nop() public {}
+}
