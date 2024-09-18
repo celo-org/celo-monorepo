f564dcea7ff1faa55736a61be34a5ac7c8040a5f
soloseng <102702451+soloseng@users.noreply.github.com>
2024-09-18 17:52:05 -0400

    -- celoToken balance check

--- packages/protocol/contracts-0.8/common/EpochManager.sol
+++ packages/protocol/contracts-0.8/common/EpochManager.sol
@@ -128,11 +128,6 @@ contract EpochManager is
       address(registry.getAddressForOrDie(CELO_UNRELEASED_TREASURE_REGISTRY_ID)).balance > 0,
       "CeloUnreleasedTreasury not yet funded."
     );
-    require(
-      getCeloToken().balanceOf(registry.getAddressForOrDie(CELO_UNRELEASED_TREASURE_REGISTRY_ID)) >
-        0,
-      "CeloUnreleasedTreasury not yet funded."
-    );
     require(!systemAlreadyInitialized(), "Epoch system already initialized");
     require(firstEpochNumber > 0, "First epoch number must be greater than 0");
     require(firstEpochBlock > 0, "First epoch block must be greater than 0");
