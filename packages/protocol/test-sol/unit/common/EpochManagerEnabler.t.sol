1cef7013b54c84ee48c3b4321559ef8c4f1f238c
Martín Volpe <martin.volpe@gmail.com>
2024-09-19 18:27:02 +0200

    fix migrations

--- packages/protocol/test-sol/unit/common/EpochManagerEnabler.t.sol
+++ packages/protocol/test-sol/unit/common/EpochManagerEnabler.t.sol
@@ -2,11 +2,8 @@
 pragma solidity >=0.8.7 <0.8.20;
 
 import "celo-foundry-8/Test.sol";
-
 import "@celo-contracts-8/common/EpochManagerEnabler.sol";
-
 import "@celo-contracts/stability/test/MockSortedOracles.sol";
-
 import "@celo-contracts/common/interfaces/IRegistry.sol";
 
 import { EPOCH_SIZEPRE_COMPILE_ADDRESS, EpochSizePrecompile } from "@test-sol/precompiles/EpochSizePrecompile.sol";
