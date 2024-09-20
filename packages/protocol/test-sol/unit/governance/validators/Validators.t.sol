aef2fce22797a8957c8dfaba45d2c38d13964ccb
pahor167 <hornak.pavel@gmail.com>
2024-09-20 11:10:16 +0200

    test fixes

--- packages/protocol/test-sol/unit/governance/validators/Validators.t.sol
+++ packages/protocol/test-sol/unit/governance/validators/Validators.t.sol
@@ -1209,13 +1209,6 @@ contract ValidatorsTest_Affiliate_WhenGroupAndValidatorMeetLockedGoldRequirement
     assertEq(affiliation, group);
   }
 
-  function test_Reverts_WhenL2_WhenAffiliatingWithRegisteredValidatorGroup() public {
-    _whenL2();
-    vm.prank(validator);
-    vm.expectRevert("This method is no longer supported in L2.");
-    validators.affiliate(group);
-  }
-
   function test_Emits_ValidatorAffiliatedEvent() public {
     vm.expectEmit(true, true, true, true);
     emit ValidatorAffiliated(validator, group);
@@ -1286,13 +1279,6 @@ contract ValidatorsTest_Affiliate_WhenValidatorIsAlreadyAffiliatedWithValidatorG
     validators.affiliate(otherGroup);
     (, , address affiliation, , ) = validators.getValidator(validator);
     assertEq(affiliation, otherGroup);
-  }
-
-  function test_ShouldRevert_WhenL2_WhenValidatorNotMemberOfThatValidatorGroup() public {
-    _whenL2();
-    vm.prank(validator);
-    vm.expectRevert("This method is no longer supported in L2.");
-    validators.affiliate(otherGroup);
   }
 
   function test_Emits_ValidatorDeaffiliatedEvent_WhenValidatorNotMemberOfThatValidatorGroup()
@@ -2086,16 +2072,6 @@ contract ValidatorsTest_AddMember is ValidatorsTest {
     vm.prank(group);
     vm.expectRevert("This method is no longer supported in L2.");
     validators.addFirstMember(validator, address(0), address(0));
-  }
-
-  function test_Reverts_AddMemberToTheList_WhenL2() public {
-    _whenL2();
-    address[] memory expectedMembersList = new address[](1);
-    expectedMembersList[0] = validator;
-
-    vm.prank(group);
-    vm.expectRevert("This method is no longer supported in L2.");
-    validators.addMember(validator);
   }
 
   function test_ShouldUpdateGroupSizeHistory() public {
