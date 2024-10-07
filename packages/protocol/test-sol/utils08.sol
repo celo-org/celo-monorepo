d7c35734a88abb487029f565ceec63a1317a309d
soloseng <102702451+soloseng@users.noreply.github.com>
2024-10-07 06:50:40 -0400

    Get epoch info of specified epoch (#11231)

--- packages/protocol/test-sol/utils08.sol
+++ packages/protocol/test-sol/utils08.sol
@@ -1,8 +1,9 @@
 pragma solidity >=0.5.13 <0.9.0;
 
 import "celo-foundry-8/Test.sol";
+import { TestConstants } from "@test-sol/constants.sol";
 
-contract Utils08 {
+contract Utils08 is TestConstants {
   uint256 public constant secondsInOneBlock = 5;
 
   function timeTravel(Vm vm, uint256 timeDelta) public {
@@ -20,12 +21,20 @@ contract Utils08 {
     timeTravel(vm, timeDelta);
   }
 
-  // This function can be also found in OpenZeppelin's library, but in a newer version than the one
-  function compareStrings(string memory a, string memory b) public pure returns (bool) {
-    return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+  // XXX: this function only increases the block number and timestamp, but does not actually change epoch.
+  // XXX: you must start and finish epoch processing to change epochs.
+  function travelEpochL2(Vm vm) public {
+    uint256 blocksInEpoch = L2_BLOCK_IN_EPOCH;
+    blockTravel(vm, blocksInEpoch);
+    timeTravel(vm, DAY);
   }
 
   function whenL2(Vm vm) public {
     vm.etch(0x4200000000000000000000000000000000000018, abi.encodePacked(bytes1(0x01)));
+  }
+
+  // This function can be also found in OpenZeppelin's library, but in a newer version than the one
+  function compareStrings(string memory a, string memory b) public pure returns (bool) {
+    return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
   }
 }
