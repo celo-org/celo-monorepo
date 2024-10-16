d990645e22f042e72a37612a50114f0b67490f0a
Martín Volpe <martin.volpe@gmail.com>
2024-10-16 19:21:56 +0200

    Fix version number

--- packages/protocol/contracts/common/FeeHandlerSeller.sol
+++ packages/protocol/contracts/common/FeeHandlerSeller.sol
@@ -103,4 +103,15 @@ contract FeeHandlerSeller is Ownable, Initializable, UsingRegistry {
 
     return address(getSortedOracles());
   }
+
+  /**
+   * @notice Returns the storage, major, minor, and patch version of the contract.
+   * @return Storage version of the contract.
+   * @return Major version of the contract.
+   * @return Minor version of the contract.
+   * @return Patch version of the contract.
+   */
+  function getVersionNumber() external pure returns (uint256, uint256, uint256, uint256) {
+    return (1, 1, 1, 0);
+  }
 }
