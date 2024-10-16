55afe8ab90912790bb62fdece20d39785a004273
Marcin Chrzanowski <martin@clabs.co>
2024-10-16 12:33:20 +0200

    Cleanup ProxyFactory08

--- packages/protocol/contracts-0.8/common/ProxyFactory08.sol
+++ packages/protocol/contracts-0.8/common/ProxyFactory08.sol
@@ -5,33 +5,33 @@ import "../../contracts/common/interfaces/IProxy.sol";
 
 import "@openzeppelin/contracts8/utils/Create2.sol";
 
+/**
+ * @title Used for deploying Proxy contracts using Create2.
+ */
 contract ProxyFactory08 {
-  // Deploys a new bytecode and transfers ownership to the provided address
-  // Calls with the same initCode that results in the same bytecode, sender and salt
-  // will revert as two contracts can not get deployed to the same address
+  /**
+   * @notice Deploys a new bytecode and transfers ownership to the provided address.
+   * @param value Amount of Celo to transfer to the new contract.
+   * @param owner The proxy owner to set.
+   * @param _salt The Create2 salt to use.
+   * @param initCode The contract init code to use for deployment.
+   * @dev Calls with initCode that results in the same bytecode, sender, and salt
+   * will revert as two contracts cannot get deployed to the same address.
+   * @dev Assumes the deployed contract is a Proxy with a `_transferOwnership`
+   * function that will be called. Will revert if that call reverts.
+   */
   function deployArbitraryByteCode(
     uint256 value,
     address owner,
     uint256 _salt,
     bytes memory initCode
   ) external returns (address) {
-    return _deployArbitraryByteCode(value, owner, _salt, initCode);
-  }
-
-  function _deployArbitraryByteCode(
-    uint256 value,
-    address owner,
-    uint256 _salt,
-    bytes memory initCode
-  ) private returns (address) {
     address deployedContract = Create2.deploy(
       value,
       keccak256(abi.encode(_salt, msg.sender)),
       abi.encodePacked(initCode)
     );
 
-    // function will fail if contract doesn't implement
-    // _transferOwnership function
     IProxy proxy = IProxy(deployedContract);
     proxy._transferOwnership(owner);
 
