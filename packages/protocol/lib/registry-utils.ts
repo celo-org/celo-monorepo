cbeef28c38e318090133a87dd905d2dee5de8566
Aaron DeRuvo <aaron@clabs.co>
2024-11-27 16:32:28 +0100

    move contract list to its own file for easy publishing

--- packages/protocol/lib/registry-utils.ts
+++ packages/protocol/lib/registry-utils.ts
@@ -6,58 +6,15 @@
  * an environment.
  */
 
+import { CeloContractName, celoRegistryAddress } from "@celo/protocol/lib/contracts-list";
 import { ContractPackage, MENTO_PACKAGE, SOLIDITY_08_PACKAGE } from "../contractPackages";
-
-export const celoRegistryAddress = '0x000000000000000000000000000000000000ce10'
-
-export enum CeloContractName {
-  Accounts = 'Accounts',
-  Attestations = 'Attestations',
-  BlockchainParameters = 'BlockchainParameters',
-  CeloToken = 'CeloToken',
-  DoubleSigningSlasher = 'DoubleSigningSlasher',
-  DowntimeSlasher = 'DowntimeSlasher',
-  Election = 'Election',
-  EpochRewards = 'EpochRewards',
-  EpochManagerEnabler = 'EpochManagerEnabler',
-  EpochManager = 'EpochManager',
-  ScoreManager = 'ScoreManager',
-  Escrow = 'Escrow',
-  Exchange = 'Exchange',
-  ExchangeEUR = 'ExchangeEUR',
-  ExchangeBRL = 'ExchangeBRL',
-  FederatedAttestations = 'FederatedAttestations',
-  FeeHandler = 'FeeHandler',
-  MentoFeeHandlerSeller = 'MentoFeeHandlerSeller',
-  FeeCurrencyWhitelist = 'FeeCurrencyWhitelist',
-  Freezer = 'Freezer',
-  GasPriceMinimum = 'GasPriceMinimum',
-  FeeCurrencyDirectory = 'FeeCurrencyDirectory',
-  GoldToken = 'GoldToken',
-  Governance = 'Governance',
-  GovernanceSlasher = 'GovernanceSlasher',
-  GovernanceApproverMultiSig = 'GovernanceApproverMultiSig',
-  GrandaMento = 'GrandaMento',
-  LockedGold = 'LockedGold',
-  LockedCelo = 'LockedCelo',
-  CeloUnreleasedTreasury = 'CeloUnreleasedTreasury',
-  OdisPayments = 'OdisPayments',
-  Random = 'Random',
-  Reserve = 'Reserve',
-  ReserveSpenderMultiSig = 'ReserveSpenderMultiSig',
-  SortedOracles = 'SortedOracles',
-  StableToken = 'StableToken',
-  StableTokenEUR = 'StableTokenEUR',
-  StableTokenBRL = 'StableTokenBRL',
-  TransferWhitelist = 'TransferWhitelist',
-  UniswapFeeHandlerSeller = 'UniswapFeeHandlerSeller',
-  Validators = 'Validators',
-}
 
 export const usesRegistry = [
   CeloContractName.Reserve,
   CeloContractName.StableToken,
 ]
+
+export { CeloContractName, celoRegistryAddress };
 
 export const hasEntryInRegistry: ContractPackage[] = [
   {
