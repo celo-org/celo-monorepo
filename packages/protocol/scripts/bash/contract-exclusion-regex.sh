297b85ed0d37dff9dd1f572c974e27bddff416bd
Martín Volpe <martin.volpe@gmail.com>
2025-01-16 19:31:34 +0100

    Added more things to the regex

--- packages/protocol/scripts/bash/contract-exclusion-regex.sh
+++ packages/protocol/scripts/bash/contract-exclusion-regex.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Exclude test contracts, mock contracts, contract interfaces, Proxy contracts, inlined libraries,
 # MultiSig contracts, and the ReleaseGold contract.
-CONTRACT_EXCLUSION_REGEX=".*Test|Mock.*|I[A-Z].*|.*Proxy|MultiSig.*|ReleaseGold|SlasherUtil|UsingPrecompiles|CeloFeeCurrencyAdapterOwnable|FeeCurrencyAdapter|FeeCurrencyAdapterOwnable"
+CONTRACT_EXCLUSION_REGEX=".*Test|Mock.*|I[A-Z].*|.*Proxy|MultiSig.*|ReleaseGold|SlasherUtil|UsingPrecompiles|CeloFeeCurrencyAdapterOwnable|FeeCurrencyAdapter|FeeCurrencyAdapterOwnable|IsL2Check|Blockable"
 
 echo "BRANCH: $BRANCH"
 
