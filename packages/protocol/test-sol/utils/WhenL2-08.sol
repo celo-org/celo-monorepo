388a65acef797b03fbd062e95782045cab0f6cd3
soloseng <102702451+soloseng@users.noreply.github.com>
2025-01-24 11:45:40 -0500

    Soloseng/L2-epoch-manager-enabler-test (#11311)
    * WIP inheritance issues ?
    
    * fixing compilation
    
    * removed mento from UsingRegistryV2 in 08
    
    * using local precompile handler
    
    * fixed unit test inheritance conflicts
    
    * removed shadowed var
    
    * passing devchain e2e
    
    * passing migration test
    
    * override Utils08 setup and cleanup
    
    * change Utils08 filename
    
    * passing devchain
    
    * reorg TestWithUtils08
    
    * Passing L2 epochManagerEnabler test
    
    * renamed to force update
    
    * using capital letter

--- packages/protocol/test-sol/utils/WhenL2-08.sol
+++ packages/protocol/test-sol/utils/WhenL2-08.sol
@@ -9,3 +9,15 @@ contract WhenL2 is TestWithUtils08 {
     whenL2WithEpochManagerInitialization();
   }
 }
+contract WhenL2NoInitialization is TestWithUtils08 {
+  function setUp() public virtual override {
+    super.setUp();
+    whenL2WithoutEpochManagerInitialization();
+  }
+}
+contract WhenL2NoCapture is TestWithUtils08 {
+  function setUp() public virtual override {
+    super.setUp();
+    whenL2WithoutEpochCapture();
+  }
+}
