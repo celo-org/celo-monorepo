5db9e8ae1b8a26564a137783d3aa392439b2ef4d
pahor167 <47992132+pahor167@users.noreply.github.com>
2025-05-06 08:25:08 +0200

    Epoch manager e2e for vote/active/revoke/unlock in between epoch switching (#11391)
    * E2E active/vote revoke in between epoch switching
    
    * PR comments
    
    * PR comments 2
    
    * PR comments 3
    
    * remove tmp anvil files when e2e done
    
    * assertApproxEqAbs
    
    * PR comments 4 :-)
    
    * prettify
    
    * comment fix

--- packages/protocol/contracts/governance/interfaces/IElection.sol
+++ packages/protocol/contracts/governance/interfaces/IElection.sol
@@ -2,64 +2,109 @@
 pragma solidity >=0.5.13 <0.9.0;
 
 interface IElection {
-  function vote(address, uint256, address, address) external returns (bool);
-  function activate(address) external returns (bool);
-  function revokeActive(address, uint256, address, address, uint256) external returns (bool);
-  function revokeAllActive(address, address, address, uint256) external returns (bool);
-  function revokePending(address, uint256, address, address, uint256) external returns (bool);
-  function markGroupIneligible(address) external;
-  function markGroupEligible(address, address, address) external;
-  function allowedToVoteOverMaxNumberOfGroups(address) external returns (bool);
+  function vote(
+    address group,
+    uint256 value,
+    address lesser,
+    address greater
+  ) external returns (bool);
+  function activate(address group) external returns (bool);
+  function revokeActive(
+    address group,
+    uint256 value,
+    address lesser,
+    address greater,
+    uint256 index
+  ) external returns (bool);
+  function revokeAllActive(
+    address group,
+    address lesser,
+    address greater,
+    uint256 index
+  ) external returns (bool);
+  function revokePending(
+    address group,
+    uint256 value,
+    address lesser,
+    address greater,
+    uint256 index
+  ) external returns (bool);
+  function markGroupIneligible(address group) external;
+  function markGroupEligible(address group, address lesser, address greater) external;
+  function allowedToVoteOverMaxNumberOfGroups(address account) external returns (bool);
   function forceDecrementVotes(
-    address,
-    uint256,
-    address[] calldata,
-    address[] calldata,
-    uint256[] calldata
+    address account,
+    uint256 value,
+    address[] calldata lessers,
+    address[] calldata greaters,
+    uint256[] calldata indices
   ) external returns (uint256);
   function setAllowedToVoteOverMaxNumberOfGroups(bool flag) external;
 
   // only owner
-  function setElectableValidators(uint256, uint256) external returns (bool);
-  function setMaxNumGroupsVotedFor(uint256) external returns (bool);
-  function setElectabilityThreshold(uint256) external returns (bool);
+  function setElectableValidators(uint256 min, uint256 max) external returns (bool);
+  function setMaxNumGroupsVotedFor(uint256 _maxNumGroupsVotedFor) external returns (bool);
+  function setElectabilityThreshold(uint256 threshold) external returns (bool);
 
   // only VM
-  function distributeEpochRewards(address, uint256, address, address) external;
+  function distributeEpochRewards(
+    address group,
+    uint256 value,
+    address lesser,
+    address greater
+  ) external;
 
   // view functions
   function electValidatorSigners() external view returns (address[] memory);
   function electValidatorAccounts() external view returns (address[] memory);
-  function electNValidatorSigners(uint256, uint256) external view returns (address[] memory);
-  function electNValidatorAccounts(uint256, uint256) external view returns (address[] memory);
+  function electNValidatorSigners(
+    uint256 minElectableValidators,
+    uint256 maxElectableValidators
+  ) external view returns (address[] memory);
+  function electNValidatorAccounts(
+    uint256 minElectableValidators,
+    uint256 maxElectableValidators
+  ) external view returns (address[] memory);
   function getElectableValidators() external view returns (uint256, uint256);
   function getElectabilityThreshold() external view returns (uint256);
-  function getNumVotesReceivable(address) external view returns (uint256);
+  function getNumVotesReceivable(address group) external view returns (uint256);
   function getTotalVotes() external view returns (uint256);
   function getActiveVotes() external view returns (uint256);
-  function getTotalVotesByAccount(address) external view returns (uint256);
-  function getPendingVotesForGroupByAccount(address, address) external view returns (uint256);
-  function getActiveVotesForGroupByAccount(address, address) external view returns (uint256);
-  function getTotalVotesForGroupByAccount(address, address) external view returns (uint256);
-  function getActiveVoteUnitsForGroupByAccount(address, address) external view returns (uint256);
-  function getTotalVotesForGroup(address) external view returns (uint256);
-  function getActiveVotesForGroup(address) external view returns (uint256);
-  function getPendingVotesForGroup(address) external view returns (uint256);
-  function getGroupEligibility(address) external view returns (bool);
+  function getTotalVotesByAccount(address account) external view returns (uint256);
+  function getPendingVotesForGroupByAccount(
+    address group,
+    address account
+  ) external view returns (uint256);
+  function getActiveVotesForGroupByAccount(
+    address group,
+    address account
+  ) external view returns (uint256);
+  function getTotalVotesForGroupByAccount(
+    address group,
+    address account
+  ) external view returns (uint256);
+  function getActiveVoteUnitsForGroupByAccount(
+    address group,
+    address account
+  ) external view returns (uint256);
+  function getTotalVotesForGroup(address group) external view returns (uint256);
+  function getActiveVotesForGroup(address group) external view returns (uint256);
+  function getPendingVotesForGroup(address group) external view returns (uint256);
+  function getGroupEligibility(address group) external view returns (bool);
   function getGroupEpochRewardsBasedOnScore(
     address group,
     uint256 totalEpochRewards,
     uint256 groupScore
   ) external view returns (uint256);
-  function getGroupsVotedForByAccount(address) external view returns (address[] memory);
+  function getGroupsVotedForByAccount(address account) external view returns (address[] memory);
   function getEligibleValidatorGroups() external view returns (address[] memory);
   function getTotalVotesForEligibleValidatorGroups()
     external
     view
     returns (address[] memory, uint256[] memory);
   function getCurrentValidatorSigners() external view returns (address[] memory);
-  function canReceiveVotes(address, uint256) external view returns (bool);
-  function hasActivatablePendingVotes(address, address) external view returns (bool);
+  function canReceiveVotes(address group, uint256 value) external view returns (bool);
+  function hasActivatablePendingVotes(address account, address group) external view returns (bool);
   function validatorSignerAddressFromCurrentSet(uint256 index) external view returns (address);
   function numberValidatorsInCurrentSet() external view returns (uint256);
   function owner() external view returns (address);
