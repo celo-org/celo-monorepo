df1dc27fd3eaee5f5bc98d1822e99fcd10db82a7
pahor167 <hornak.pavel@gmail.com>
2025-05-09 17:45:23 +0200

    PR comments

--- packages/protocol/contracts-0.8/common/SuperBridgeETHWrapper.sol
+++ packages/protocol/contracts-0.8/common/SuperBridgeETHWrapper.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-only
-pragma solidity ^0.8.15;
+pragma solidity >=0.8.7 <0.8.20;
 
 import "../../contracts/common/Initializable.sol";
-import "@openzeppelin/contracts8/access/Ownable.sol";
 import "./interfaces/IStandardBridge.sol";
 import "./interfaces/IWETH.sol";
 
