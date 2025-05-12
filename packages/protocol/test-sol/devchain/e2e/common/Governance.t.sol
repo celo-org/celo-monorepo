18edc80bb5d28b4b64b9a5e6585931459400405e
Mc01 <marcin.majchrzak@clabs.co>
2025-05-12 21:14:25 +0200

    Do not rely on ffi mode.

--- packages/protocol/test-sol/devchain/e2e/common/Governance.t.sol
+++ packages/protocol/test-sol/devchain/e2e/common/Governance.t.sol
@@ -7,6 +7,7 @@ import { stdJson } from "forge-std-8/StdJson.sol";
 
 // Local imports
 import { StringUtils } from "@celo-contracts/common/libraries/StringUtils.sol";
+import { SelectorParser } from "@celo-contracts/common/test/SelectorParser.sol";
 
 // Test imports
 import { Devchain, IGovernance } from "@test-sol/devchain/e2e/utils.sol";
@@ -32,6 +33,7 @@ contract E2E_Election is Devchain {
 contract E2E_Governance is Devchain {
   using stdJson for string;
   using StringUtils for string;
+  using SelectorParser for string;
 
   // test cases
   struct ConstitutionCase {
@@ -62,57 +64,58 @@ contract E2E_Governance is Devchain {
 
   function setUp() public virtual override {
     // get contracts from constitution
-    string memory json_ = vm.readFile("./governanceConstitution.json");
-    string[] memory contractNames_ = vm.parseJsonKeys(json_, "");
+    string memory constitutionJson_ = vm.readFile("./governanceConstitution.json");
+    string[] memory contractNames_ = vm.parseJsonKeys(constitutionJson_, "");
 
     // vars for looping
-    string memory contract_;
+    string memory contractName_;
     address address_;
-    string memory function_;
+    string memory functionName_;
     bytes4 selector_;
     uint256 threshold_;
+    string memory selectorJson_;
+    string[] memory functionsWithTypes_;
+    string[] memory functionNames_;
 
     // loop over contract names
     for (uint256 i = 0; i < contractNames_.length; i++) {
-      contract_ = contractNames_[i];
-      if (contract_.compareStrings("proxy")) {
+      contractName_ = contractNames_[i];
+      if (contractName_.equals("proxy")) {
         // skip proxy address
         continue;
       } else {
         // set address from registry
-        address_ = registryContract.getAddressForStringOrDie(contract_);
+        address_ = registryContract.getAddressForStringOrDie(contractName_);
       }
 
-      // get all functions for contract
-      string[] memory functionNames_ = vm.parseJsonKeys(json_, string.concat(".", contract_));
+      // load selectors for given contract from file
+      selectorJson_ = vm.readFile(string.concat("./.tmp/selectors/", contractName_, ".json"));
+
+      // get function names with types
+      functionsWithTypes_ = vm.parseJsonKeys(selectorJson_, "");
+
+      // get functions names from constitution for contract
+      functionNames_ = vm.parseJsonKeys(constitutionJson_, string.concat(".", contractName_));
 
       // loop over function names
       for (uint256 j = 0; j < functionNames_.length; j++) {
-        function_ = functionNames_[j];
-        if (function_.compareStrings("default")) {
+        functionName_ = functionNames_[j];
+        if (functionName_.equals("default")) {
           // use empty selector as default
           selector_ = hex"00000000";
         } else {
-          // use forge inspect through ffi to determine selector for function name
-          string[] memory functionToSelector_ = new string[](4);
-          functionToSelector_[0] = "bash";
-          functionToSelector_[1] = "-c";
-          functionToSelector_[2] = string.concat(
-            "forge inspect ",
-            contract_,
-            " methods | grep ",
-            function_,
-            " | awk -F'|' '{print $3}' | awk '{$1=$1;print}'"
-          );
-          selector_ = bytes4(vm.ffi(functionToSelector_));
+          // retrieve selector from selector JSON
+          selector_ = selectorJson_.getSelector(functionsWithTypes_, functionName_, vm);
         }
 
         // determine treshold from constitution
-        threshold_ = json_.readUint(string.concat(".", contract_, ".", function_));
+        threshold_ = constitutionJson_.readUint(
+          string.concat(".", contractName_, ".", functionName_)
+        );
 
         // push new test case
         constitutionCases.push(
-          ConstitutionCase(contract_, address_, function_, selector_, threshold_)
+          ConstitutionCase(contractName_, address_, functionName_, selector_, threshold_)
         );
       }
     }
