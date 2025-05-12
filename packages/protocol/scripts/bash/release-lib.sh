7c25797c14f2a45b24e7c3ec7725d172faffa0b6
Marcin Chrzanowski <martin@clabs.co>
2025-05-12 14:32:48 +0200

    First attempt building Foundry

--- packages/protocol/scripts/bash/release-lib.sh
+++ packages/protocol/scripts/bash/release-lib.sh
@@ -34,3 +34,40 @@ function build_tag() {
   [ -d contracts-0.8 ] && rm -r contracts-0.8
   git restore --source $CURRENT_HASH --staged --worktree contracts* 2>>$LOG_FILE >> $LOG_FILE
 }
+
+# USAGE: build_tag_foundry <branch> <log file>
+# This function:
+# 1. checks out the given branch
+# 2. builds contracts with Foundry
+# 3. returns to original branch
+# piping output of any commands to the specified log file.
+# Sets $BUILD_DIR to the directory where resulting build artifacts may be found.
+function build_tag_foundry() {
+  local BRANCH="$1"
+  local LOG_FILE="$2"
+
+  echo "Writing logs to $LOG_FILE"
+
+  local CURRENT_HASH=`git log -n 1 --oneline | cut -c 1-9`
+
+  git fetch origin +'refs/tags/core-contracts.v*:refs/tags/core-contracts.v*' >> $LOG_FILE
+  echo " - Checkout contracts source code at $BRANCH"
+  BUILD_DIR=$(echo out-$(echo $BRANCH | sed -e 's/\//_/g'))
+  [ -d contracts ] && rm -r contracts
+  [ -d contracts-0.8 ] && rm -r contracts-0.8
+  [ -d test-sol ] && rm -r test-sol
+
+  git restore --source $BRANCH contracts* test-sol 2>>$LOG_FILE >> $LOG_FILE
+
+  if [ ! -d $BUILD_DIR ]; then
+    echo " - Build contract artifacts at $BUILD_DIR"
+    forge build --out $BUILD_DIR --ast >> $LOG_FILE
+  else
+    echo " - Contract artifacts already built at $BUILD_DIR"
+  fi
+
+  [ -d contracts ] && rm -r contracts
+  [ -d contracts-0.8 ] && rm -r contracts-0.8
+  [ -d test-sol ] && rm -r test-sol
+  git restore --source $CURRENT_HASH --staged --worktree contracts* test-sol 2>>$LOG_FILE >> $LOG_FILE
+}
