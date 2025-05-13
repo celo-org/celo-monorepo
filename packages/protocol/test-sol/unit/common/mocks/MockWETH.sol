2189755691eb8ef06c7aed7685cdc0b18e70fde1
pahor167 <47992132+pahor167@users.noreply.github.com>
2025-05-13 13:35:06 +0200

    Super bridge ETH wrapper (#11343)
    * Super bridge WETH wrapper
    
    * Cleanup & prettify
    
    * added ability to update the addresses
    
    * added recepient and gas limit as input parameters
    
    * Smart contract is now immutable
    
    * Deployment
    
    * lint
    
    * PR comments
    
    * PR comments 2
    
    * removal of duplicate

---
+++ packages/protocol/test-sol/unit/common/mocks/MockWETH.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.8.7 <0.8.20;
+
+import "@openzeppelin/contracts8/token/ERC20/ERC20.sol";
+
+contract MockWETH is ERC20 {
+  event Deposit(address indexed dst, uint wad);
+  event Withdrawal(address indexed src, uint wad);
+
+  uint256 public totalDeposited;
+
+  constructor() ERC20("Mock Wrapped Ether", "MWETH") {}
+
+  function deposit() external payable {
+    _mint(msg.sender, msg.value);
+    totalDeposited += msg.value;
+    emit Deposit(msg.sender, msg.value);
+  }
+
+  function withdraw(uint256 wad) external {
+    require(balanceOf(msg.sender) >= wad, "MockWETH: insufficient balance");
+    totalDeposited -= wad;
+    _burn(msg.sender, wad);
+    payable(msg.sender).transfer(wad);
+    emit Withdrawal(msg.sender, wad);
+  }
+}
