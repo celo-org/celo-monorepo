1808367db752ccdb6b6edc71513e6af448f1d427
Marcin Chrzanowski <martin@clabs.co>
2025-06-04 12:06:01 +0200

    Set API key via env

--- packages/protocol/scripts/foundry/upgrade/fork_l1.sh
+++ packages/protocol/scripts/foundry/upgrade/fork_l1.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+[ -z "$ALCHEMY_API_KEY" ] && echo "Need to set the ALCHEMY_API_KEY via env" && exit 1;
+
 anvil \
   --port 8545 \
-  --fork-url https://eth-holesky.g.alchemy.com/v2/ \
+  --fork-url https://eth-holesky.g.alchemy.com/v2/$ALCHEMY_API_KEY \
   --fork-chain-id 17000 \
   --fork-block-number 3904335
