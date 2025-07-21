f56657c21660b2bed80068bab7cdc6327c9011de
pahor167 <47992132+pahor167@users.noreply.github.com>
2025-07-21 21:07:36 +0200

    Deregister validator Epoch manager e2e test (#11443)

--- packages/protocol/test-sol/unit/governance/validators/mocks/CompileValidatorIntegrationMock.t.sol
+++ packages/protocol/test-sol/unit/governance/validators/mocks/CompileValidatorIntegrationMock.t.sol
@@ -4,8 +4,8 @@ pragma solidity >=0.8.7 <0.8.20;
 import "celo-foundry-8/Test.sol";
 import "forge-std/console.sol";
 
-// here only to forge compile of  ValidatorsMock
-import "@test-sol/unit/governance/validators/mocks/ValidatorsMock.sol";
+// here only to force compile of ValidatorsMock
+import "@test-sol/unit/governance/validators/mocks/ValidatorsCompile.sol";
 
 contract CompileValidatorIntegrationMock is Test {
   function test_nop() public view {
