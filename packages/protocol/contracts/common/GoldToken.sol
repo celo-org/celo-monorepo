4b55d16e1d7d0651abc61aa8f261895834cc00f2
Martín Volpe <git@martinvol.pe>
2025-07-21 16:06:36 -0300

    Fixed gold token versions

--- packages/protocol/contracts/common/GoldToken.sol
+++ packages/protocol/contracts/common/GoldToken.sol
@@ -211,7 +211,7 @@ contract GoldToken is
    * @return Patch version of the contract.
    */
   function getVersionNumber() external pure returns (uint256, uint256, uint256, uint256) {
-    return (1, 3, 0, 0);
+    return (1, 2, 0, 0);
   }
 
   /**
