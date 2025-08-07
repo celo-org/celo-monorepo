b0546992a91d466b340d42eaf0ae6873a7dba6e0
Mc01 <marcin.majchrzak@clabs.co>
2025-08-07 10:36:38 +0200

    Bump version.

--- packages/protocol/contracts-0.8/common/EpochManager.sol
+++ packages/protocol/contracts-0.8/common/EpochManager.sol
@@ -595,7 +595,7 @@ contract EpochManager is
    * @return Patch version of the contract.
    */
   function getVersionNumber() external pure returns (uint256, uint256, uint256, uint256) {
-    return (1, 1, 0, 1);
+    return (1, 2, 0, 0);
   }
 
   /**
