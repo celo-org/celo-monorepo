42199c5e4aa28e6364abd2ff1ff33882ec428550
Marcin Chrzanowski <martin@clabs.co>
2025-08-21 23:17:45 +0200

    Fix layout filtering

--- packages/protocol/lib/compatibility/ast-layout.ts
+++ packages/protocol/lib/compatibility/ast-layout.ts
@@ -193,8 +193,14 @@ export const reportLayoutIncompatibilities = (oldArtifactsSet: BuildArtifacts[],
   for (const newArtifacts of newArtifactsSets) {
     const reports = newArtifacts.listArtifacts()
       .filter((newArtifact: any) => {
-        const coreContractPathPattern = /^(project:\/)?contracts(-0\.8)?\//
-        return coreContractPathPattern.test(newArtifact.ast.absolutePath)
+        // Matches all Truffle project artifacts (core contracts and test resource contracts)
+        const truffleProjectContractPathPattern = /^project:/
+        // Matches Foundry core contracts
+        const foundryCoreContractPathPattern = /^contracts(-0\.8)?\//
+        // Matches Foundry test resource contracts
+        const foundryTestContractPathPattern = /^test-sol-resources/
+        const path = newArtifact.ast.absolutePath
+        return truffleProjectContractPathPattern.test(path) || foundryCoreContractPathPattern.test(path) || foundryTestContractPathPattern.test(path)
       })
       .map((newArtifact: any) => {
       for (const oldArtifacts of oldArtifactsSet) {
