29e4fb45f188c5de7e25dd7bb8fc46e116023d5d
Martin <martin@clabs.co>
2025-09-19 13:25:07 +0200

    Detruffle check-versions script (#11490)

--- packages/protocol/lib/compatibility/internal.ts
+++ packages/protocol/lib/compatibility/internal.ts
@@ -1,6 +1,33 @@
-import { Contract as ZContract } from '@openzeppelin/upgrades'
+import { BuildArtifacts, Contract as ZContract } from '@openzeppelin/upgrades'
 const Web3 = require('web3')
 const web3 = new Web3(null)
+
+// Foundry build artifacts do not have a `.contractName` field, so we get it from the
+// `ContractDefinition` expression in the AST.
+const getContractNameFromDefinition = (artifact: Artifact): string => {
+  for (let i = 0; i < artifact.ast.nodes.length; i++) {
+    const node = artifact.ast.nodes[i]
+    if (node.nodeType === 'ContractDefinition') {
+      return node.name
+    }
+  }
+  console.error("Name not found in artifact AST")
+  return ''
+}
+
+export const getContractName = (artifact: Artifact): string => {
+  if (artifact.contractName) {
+    return artifact.contractName
+  } else {
+    return getContractNameFromDefinition(artifact)
+  }
+}
+
+export const getArtifactByName = (contractName: string, artifacts: BuildArtifacts): Artifact => {
+  return artifacts.listArtifacts().find(artifact =>
+    getContractName(artifact) === contractName
+  )
+}
 
 // getStorageLayout needs an oz-sdk Contract class instance. This class is a
 // subclass of Contract from web3-eth-contract, with an added .schema member and
@@ -8,15 +35,20 @@ const web3 = new Web3(null)
 //
 // Couldn't find an easy way of getting one just from contract artifacts. But
 // for getStorageLayout we really only need .schema.ast and .schema.contractName.
-export function makeZContract(artifact: any): ZContract {
+export function makeZContract(artifact: Artifact): ZContract {
   const web3Contract = new web3.eth.Contract(artifact.abi)
   // @ts-ignore
   const contract = web3Contract as Contract
   // @ts-ignore
   contract.schema = {}
   contract.schema.ast = artifact.ast
-  contract.schema.contractName = artifact.contractName
-  contract.schema.deployedBytecode = artifact.deployedBytecode
+  contract.contractName = getContractName(artifact)
+  contract.schema.contractName = contract.contractName
+  if (typeof artifact.deployedBytecode === "string") {
+    contract.schema.deployedBytecode = artifact.deployedBytecode
+  } else {
+    contract.schema.deployedBytecode = artifact.deployedBytecode.object
+  }
   return contract
 }
 
@@ -27,7 +59,7 @@ export interface Artifact {
   bytecode: string
   compiler: any
   contractName: string
-  deployedBytecode: string
+  deployedBytecode: (string | { object: string })
   deployedSourceMap: string
   fileName: string
   legacyAST?: any
