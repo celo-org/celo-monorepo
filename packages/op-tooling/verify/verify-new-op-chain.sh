a7037abb851e70be4cc21f2b78223af56b859630
Mc01 <marcin.majchrzak@clabs.co>
2025-10-10 13:51:12 +0200

    CR.

--- packages/op-tooling/verify/verify-new-op-chain.sh
+++ packages/op-tooling/verify/verify-new-op-chain.sh
@@ -33,25 +33,22 @@ esac
 # OPCM_GTA="0x6d5f8c3a8a84cc32e70097aaf2f2aa5ba6955857"
 # OPCM_DEPLOYER="0xc7d9db993b9b61410299073fe748f5217bb8904a"
 # OPCM_UPGRADER="0x6c8fc16bf0b1665cdb9fed485250088b67f873c1"
-# DWI="0xe8249b2cffc3f71e433918c5267c71bf1e1fdc1e" # impl for DelayedWETH
-# OPI="0x229ac4d29814249ba4830eb0e5b133df664ce4d7" # impl for OptimismPortal
-# SCI="0x1edd39f1662fa3f3c4003b013e899c2cff976377" # impl for SystemConfig
+# DW="0x082f5f58b664cd1d51f9845fee322aba2ced9cba" # proxy for DelayedWETH
+# OP="0x44ae3d41a335a7d05eb533029917aad35662dcc2" # proxy for OptimismPortal
+# SC="0x760a5f022c9940f4a074e0030be682f560d29818" # proxy for SystemConfig
 # LCDM="0x70b0e58e6039831954ede2ea1e9ef8a51680e4fd" # proxy for L1CrossDomainMessenger
-# LCDMI="0xC1dd01079a4358aEc262AD5080239542433d077a" # impl for L1CrossDomainMessenger
 # LEB="0xb8c8dcbccd0f7c5e7a2184b13b85d461d8711e96" # proxy for L2ERC721Bridge
-# LEBI="0xEf32Aa47df0800B8619d0522FA82a68DD4b9A8D7" # impl for L1ERC721Bridge
 # LSB="0xec18a3c30131a0db4246e785355fbc16e2eaf408" # proxy for L1StandardBridge
-# LSBI="0x4063C3824D993784A169470e05DACC1b8501D972" # impl for L1StandardBridge
-# OMEFI="0xd6e36ca5ef4babe6f890534bd8479b9561c22f94" # impl for OptimismMintableERC20Factory
-# DGFI="0x0468d6dfbcb060cea717459a4026339d60fb34d9" # impl for DisputeGameFactory
-# ASRI="0xe8e958be5a891ff9aac5410c3923dbafd99174bb" # impl for AnchorStateRegistry
-# SUI="0x1b8ca63db2e3e37c1def34f24e4c88ed422bd7c1" # impl for SuperchainConfig
-# PVI="0x9a7Ca01B64cE656B927248aF08692Ed2714c68e0" # impl for ProtocolVersions
+# OMEF="0x261be2ed7241fed9c746e0b5dff3a4a335991377" # proxy for OptimismMintableERC20Factory
+# DGF="0x57c45d82d1a995f1e135b8d7edc0a6bb5211cfaa" # proxy for DisputeGameFactory
+# ASR="0xd73ba8168a61f3e917f0930d5c0401aa47e269d6" # proxy for AnchorStateRegistry
+# SU="0x31bEef32135c90AE8E56Fb071B3587de289Aaf77" # proxy for SuperchainConfig
+# PV="0x0e2d45F3393C3A02ebf285F998c5bF990A1541cd" # proxy for ProtocolVersions
 # PA="0xf7d7a3d3bb8abb6829249b3d3ad3d525d052027e" # ProxyAdmin
 # AM="0x8f0c6fc85a53551d87899ac2a5af2b48c793eb63" # AddressManager
-# PDG=0x5a4feaeb665f8049ac1e0714f82a11532d47ae49 # PermissionedDisputeGame
-# CSUI="0x00cdf709c093702c8019889e7df32d1735b80355" # impl for CeloSuperchainConfig
-# CT="0x93ec064ad109077d42b1581c8fa4e8eba34b2d13" # impl for CeloToken
+# PDG="0x5a4feaeb665f8049ac1e0714f82a11532d47ae49" # PermissionedDisputeGame
+# CSU="0x5c34140a1273372211bd75184ccc9e434b38d86b" # proxy for CeloSuperchainConfig
+# CT="0x3c7011fd5e6aed460caa4985cf8d8caba435b092" # proxy for CeloToken
 
 # L2 contracts
 # L2_LMP="0x4200000000000000000000000000000000000000" # proxy for LegacyMessagePasser
@@ -140,12 +137,16 @@ verify() {
     echo "----------------------------------------"
 }
 
+get_impl() {
+  IMPL_SLOT="0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc" # keccak256("eip1967.proxy.implementation")
+  export IMPL_ADDRESS_B32=$(cast storage $1 $IMPL_SLOT -r $RPC_URL)
+  export IMPL_ADDRESS=$(cast parse-bytes32-address $IMPL_ADDRESS_B32)
+}
+
 verify_proxy() {
-    IMPL_SLOT="0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc" # keccak256("eip1967.proxy.implementation")
-    IMPL_ADDRESS_B32=$(cast storage $1 $IMPL_SLOT -r $RPC_URL)
-    IMPL_ADDRESS=$(cast parse-bytes32-address $IMPL_ADDRESS_B32)
-    echo "Proxy: $1 ImplB32: $IMPL_ADDRESS_B32 Impl: $IMPL_ADDRESS"
-    verify $IMPL_ADDRESS ${@:2}
+  get_impl $1
+  echo "Proxy: $1 ImplB32: $IMPL_ADDRESS_B32 Impl: $IMPL_ADDRESS"
+  verify $IMPL_ADDRESS ${@:2}
 }
 
 if [ "$NETWORK" = "l1" ]; then
@@ -159,33 +160,39 @@ if [ "$NETWORK" = "l1" ]; then
     IMPLS=$(cast call $OPCM_CONTAINER "implementations()((address,address,address,address,address,address,address,address,address,address,address,address))" -r $RPC_URL)
     IMPLS="${IMPLS// /}" # Remove spaces
     echo "OPCM Implementations: $IMPLS"
+    get_impl $CSU
+    CSUI=$IMPL_ADDRESS
+    echo "CeloSuperchainConfig Impl: $CSUI"
+    get_impl $PV
+    PVI=$IMPL_ADDRESS
+    echo "ProtocolVersions Impl: $PVI"
     verify $OPCM_CONTAINER OPContractsManagerContractsContainer "constructor((address,address,address,address,address,address,address,address,address),(address,address,address,address,address,address,address,address,address,address,address,address))" "$BLUEPRINTS" "$IMPLS"
     verify $OPCM_GTA OPContractsManagerGameTypeAdder "constructor(address)" $OPCM_CONTAINER # TODO: unverified
     verify $OPCM_DEPLOYER OPContractsManagerDeployer "constructor(address)" $OPCM_CONTAINER # TODO: unverified
     verify $OPCM_UPGRADER OPContractsManagerUpgrader "constructor(address)" $OPCM_CONTAINER # TODO: unverified
     verify $OPCM OPContractsManager "constructor(address,address,address,address,address,address,string,address)" $OPCM_GTA $OPCM_DEPLOYER $OPCM_UPGRADER $CSUI $PVI $PA $RELEASE $DEPLOYER # TODO: unverified
-    DELAY_WETH=$(cast call $DWI "delay()(uint256)" -r $RPC_URL)
+    DELAY_WETH=$(cast call $DW "delay()(uint256)" -r $RPC_URL)
     echo "Delayed WETH delay: $DELAY_WETH"
-    verify $DWI DelayedWETH "constructor(uint256)" $DELAY_WETH
-    PROOF_MATURITY=$(cast call $OPI "proofMaturityDelaySeconds()(uint256)" -r $RPC_URL)
+    verify_proxy $DW DelayedWETH "constructor(uint256)" $DELAY_WETH
+    PROOF_MATURITY=$(cast call $OP "proofMaturityDelaySeconds()(uint256)" -r $RPC_URL)
     echo "Optimism Portal proof maturity: $PROOF_MATURITY"
-    GAME_FINALITY=$(cast call $OPI "disputeGameFinalityDelaySeconds()(uint256)" -r $RPC_URL)
+    GAME_FINALITY=$(cast call $OP "disputeGameFinalityDelaySeconds()(uint256)" -r $RPC_URL)
     echo "Optimism Portal game finality: $GAME_FINALITY"
-    verify $OPI OptimismPortal2 "constructor(uint256,uint256)" $PROOF_MATURITY $GAME_FINALITY
-    verify $SCI SystemConfig
-    verify $LCDMI L1CrossDomainMessenger
-    verify $LEBI L1ERC721Bridge
-    verify $LSBI L1StandardBridge
-    verify $OMEFI OptimismMintableERC20Factory
-    verify $DGFI DisputeGameFactory
-    verify $ASRI AnchorStateRegistry
-    verify $SUI SuperchainConfig
-    verify $PVI ProtocolVersions
+    verify_proxy $OP OptimismPortal2 "constructor(uint256,uint256)" $PROOF_MATURITY $GAME_FINALITY
+    verify_proxy $LCDM L1CrossDomainMessenger
+    verify_proxy $LEB L1ERC721Bridge
+    verify_proxy $LSB L1StandardBridge
+    verify_proxy $CSU CeloSuperchainConfig
+    verify_proxy $PV ProtocolVersions
+    verify_proxy $SU SuperchainConfig
+    verify_proxy $SC SystemConfig
+    verify_proxy $OMEF OptimismMintableERC20Factory
+    verify_proxy $ASR AnchorStateRegistry
+    verify_proxy $DGF DisputeGameFactory
+    verify_proxy $CT CeloTokenL1
     verify $PA ProxyAdmin
     verify $AM AddressManager
     verify $PDG PermissionedDisputeGame
-    verify $CSUI CeloSuperchainConfig # TODO: unverified
-    verify $CT CeloTokenL1
     # end verifying contracts
     echo ">>> [L1] Finished verifying contracts on $NETWORK"
 else
