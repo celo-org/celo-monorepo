86e1cc97955205ba57d2ad9e5f6029d8af1f3ca7
Martín Volpe <git@martinvol.pe>
2025-10-13 19:02:33 +0200

    Added dependency to build

--- packages/protocol/contracts/Import05Dependencies.sol
+++ packages/protocol/contracts/Import05Dependencies.sol
@@ -1,20 +1,23 @@
 pragma solidity ^0.5.13;
 
-// this file only exists so that foundry compiles this contracts
+// / this file only exists so that foundry compiles this contracts
 import { Proxy } from "./common/Proxy.sol";
-import { ProxyFactory } from ".//common/ProxyFactory.sol";
-import { GoldToken } from ".//common/GoldToken.sol";
-import { Accounts } from ".//common/Accounts.sol";
-import { Election } from ".//governance/Election.sol";
-import { Governance } from ".//governance/Governance.sol";
-import { LockedGold } from ".//governance/LockedGold.sol";
-import { GovernanceApproverMultiSig } from ".//governance/GovernanceApproverMultiSig.sol";
-import { Escrow } from ".//identity/Escrow.sol";
-import { FederatedAttestations } from ".//identity/FederatedAttestations.sol";
-import { SortedOracles } from ".//stability/SortedOracles.sol";
+import { ProxyFactory } from "./common/ProxyFactory.sol";
+import { GoldToken } from "./common/GoldToken.sol";
+import { Accounts } from "./common/Accounts.sol";
+import { Election } from "./governance/Election.sol";
+import { Governance } from "./governance/Governance.sol";
+import { LockedGold } from "./governance/LockedGold.sol";
+import { GovernanceApproverMultiSig } from "./governance/GovernanceApproverMultiSig.sol";
+import { Escrow } from "./identity/Escrow.sol";
+import { FederatedAttestations } from "./identity/FederatedAttestations.sol";
+import { SortedOracles } from "./stability/SortedOracles.sol";
 
-// import { IEpochManager } from ".//common/interfaces/IEpochManager.sol";
-// import { IValidators } from ".//governance/interfaces/IValidators.sol";
-// import ".//common/interfaces/ICeloUnreleasedTreasury.sol";
+import { ReserveSpenderMultiSig } from "../lib/mento-core/contracts/ReserveSpenderMultiSig.sol";
+import { Reserve } from "../lib/mento-core/contracts/Reserve.sol";
+import { StableToken } from "../lib/mento-core/contracts/StableToken.sol";
+import { StableTokenEUR } from "../lib/mento-core/contracts/StableTokenEUR.sol";
+import { StableTokenBRL } from "../lib/mento-core/contracts/StableTokenBRL.sol";
+import { Exchange } from "../lib/mento-core/contracts/Exchange.sol";
 
 contract Import05 {}
