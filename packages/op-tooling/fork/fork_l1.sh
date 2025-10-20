106c13141d2c7c1705d322fef72dba55190b6045
Mc01 <marcin.majchrzak@clabs.co>
2025-10-20 10:42:33 +0200

    Allow arbitrary RPC for forking.

--- packages/op-tooling/fork/fork_l1.sh
+++ packages/op-tooling/fork/fork_l1.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-[ -z "${ALCHEMY_API_KEY:-}" ] && echo "Need to set the ALCHEMY_API_KEY via env" && exit 1;
 [ -z "${NETWORK:-}" ] && echo "Need to set the NETWORK via env" && exit 1;
 [ -z "${BLOCK_NUMBER:-}" ] && echo "Need to set the BLOCK_NUMBER via env" && exit 1;
+
+if [ -z "${RPC_URL:-}" ]; then
+  [ -z "${ALCHEMY_API_KEY:-}" ] && echo "Need to set the ALCHEMY_API_KEY via env" && exit 1;
+  RPC_URL="https://eth-$NETWORK.g.alchemy.com/v2/$ALCHEMY_API_KEY"
+fi
 
 # Check network
 case $NETWORK in
@@ -26,6 +30,6 @@ esac
 
 anvil \
   --port 8545 \
-  --fork-url https://eth-$NETWORK.g.alchemy.com/v2/$ALCHEMY_API_KEY \
+  --fork-url $RPC_URL \
   --fork-chain-id $CHAIN_ID \
   --fork-block-number $BLOCK_NUMBER
