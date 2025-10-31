2b57a208a7fa0293d34c598826513d76b90c420c
Mc01 <marcin.majchrzak@clabs.co>
2025-10-31 12:15:20 +0100

    Add Chaos V2.

--- packages/op-tooling/withdrawal/build_proof/config.ts
+++ packages/op-tooling/withdrawal/build_proof/config.ts
@@ -4,6 +4,7 @@ import { sepolia, holesky, celoAlfajores } from 'viem/chains'
 import { publicActionsL1, publicActionsL2, walletActionsL1, walletActionsL2 } from 'viem/op-stack'
 import { celoBaklava } from './celo_baklava.ts'
 import { celoSepolia } from './celo_sepolia.ts'
+import { celoChaosV2 } from './celo_chaos_v2.ts'
 import { PRIVATE_KEY, NETWORK } from './env.ts'
 
 export const account = privateKeyToAccount(`0x${PRIVATE_KEY}`)
@@ -20,7 +21,7 @@ if (NETWORK === 'alfajores' || NETWORK === 'baklava') {
     chain: holesky,
     transport: http(),
   }).extend(walletActionsL1())
-} else if (NETWORK === 'sepolia') {
+} else if (NETWORK === 'sepolia' || NETWORK === 'chaos_v2') {
   l1.public = createPublicClient({
     chain: sepolia,
     transport: http(),
@@ -71,8 +72,19 @@ if (NETWORK === 'alfajores') {
     chain: celoSepolia,
     transport: http(),
   }).extend(walletActionsL2())
+} else if (NETWORK === 'chaos_v2') {
+  l2.public = createPublicClient({
+    chain: celoChaosV2,
+    transport: http(),
+  }).extend(publicActionsL2())
+
+  l2.wallet = createWalletClient({
+    account,
+    chain: celoChaosV2,
+    transport: http(),
+  }).extend(walletActionsL2())
 } else {
-  throw new Error('Unsupported network. Supported networks are: alfajores, baklava, sepolia')
+  throw new Error('Unsupported network. Supported networks are: alfajores, baklava, sepolia, chaos_v2')
 }
 export const publicClientL2 = l2.public
 export const walletClientL2 = l2.wallet
