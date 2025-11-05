bf22f3d16cc2e3762828e2c10f927939bbcd6127
Mc01 <marcin.majchrzak@clabs.co>
2025-11-05 16:24:25 +0100

    Change fork l2 to sepolia.

--- packages/op-tooling/fork/fork_l2.sh
+++ packages/op-tooling/fork/fork_l2.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 anvil \
   --port 8546 \
-  --fork-url https://alfajores-forno.celo-testnet.org \
-  --fork-chain-id 44787 \
-  --fork-block-number 47029145
+  --fork-url https://forno.celo-sepolia.celo-testnet.org \
+  --fork-chain-id 11142220 \
+  --fork-block-number 9055818
