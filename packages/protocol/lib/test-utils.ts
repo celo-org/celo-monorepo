5af58c22705cb4b31a3766b60e0f92e0eabbbae1
Martín Volpe <git@martinvol.pe>
2025-11-06 18:05:25 +0100

    fix lib tests

--- packages/protocol/lib/test-utils.ts
+++ packages/protocol/lib/test-utils.ts
@@ -1,7 +1,7 @@
 import chai from 'chai';
 import chaiSubset from 'chai-subset';
 // eslint-disable-next-line: ordered-imports
-import { spawn } from 'child_process';
+import { spawn, SpawnOptions } from 'child_process';
 
 /* eslint:disabled ordered-imports: 0 */
 
@@ -11,6 +11,27 @@ chai.use(chaiSubset)
 export const EPOCH = 100
 
 
+async function isPortOpen(host: string, port: number) {
+  return (await execCmd('nc', ['-z', host, port.toString()], { silent: true })) === 0
+}
+
+
+function execCmd(cmd: string, args: string[], options?: SpawnOptions & { silent?: boolean }) {
+  return new Promise<number>(async (resolve, reject) => {
+    const { silent, ...spawnOptions } = options || { silent: false }
+    if (!silent) {
+      console.debug('$ ' + [cmd].concat(args).join(' '))
+    }
+    const process = spawn(cmd, args, { ...spawnOptions, stdio: silent ? 'ignore' : 'inherit' })
+    process.on('close', (code) => {
+      try {
+        resolve(code)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  })
+}
 
 export const assertThrowsAsync = async (promise: any, errorMessage: string = '') => {
   let failed = false
@@ -21,6 +42,25 @@ export const assertThrowsAsync = async (promise: any, errorMessage: string = '')
   }
 
   assert.strictEqual(true, failed, errorMessage)
+}
+
+function delay(time) {
+  return new Promise((resolve) => setTimeout(resolve, time))
+}
+
+
+export async function waitForPortOpen(host: string, port: number, seconds: number) {
+  console.info(`Waiting for ${host}:${port} to open for ${seconds}s`);
+  const deadline = Date.now() + seconds * 1000
+  do {
+    if (await isPortOpen(host, port)) {
+      await delay(60000) // extra 60s just to give ganache extra time to startup
+      console.info(`Port ${host}:${port} opened`)
+      return true
+    }
+  } while (Date.now() < deadline)
+  console.info("Port was not opened in time");
+  return false
 }
 
 
