60dc602c311abad6576b91e3147ad66b588b93f1
Martín Volpe <volpe@clabs.co>
2025-11-10 16:58:39 +0100

    remove unused dependencies  (#11565)

--- packages/protocol/scripts/bash/init_network.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# Runs all truffle migrations in protocol/migrations/
-#
-# Flags:
-# -n: Name of the network to migrate to
-
-yarn run devchain:migrate -r "$@" && yarn run truffle:set-block-gas-limit "$@"
