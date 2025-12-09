6de87a0b88d2e74c1e8d709a14689c41e4ba5691
Martín Volpe <git@martinvol.pe>
2025-12-09 15:59:25 -0300

    Upload anvil artifacts for devchain, instead of ganache (#11568)

--- packages/protocol/migrations_sol/constants.sol
+++ packages/protocol/migrations_sol/constants.sol
@@ -37,4 +37,87 @@ contract MigrationsConstants is TestConstants {
     "MentoFeeHandlerSeller",
     "UniswapFeeHandlerSeller"
   ];
+
+  string[26] contractsInRegistryPath = [
+    string.concat("out-truffle-compat/", "Accounts", ".sol/", "Accounts", ".json"),
+    string.concat(
+      "out-truffle-compat/",
+      "BlockchainParameters",
+      ".sol/",
+      "BlockchainParameters",
+      ".json"
+    ),
+    string.concat(
+      "out-truffle-compat-0.8/",
+      "CeloUnreleasedTreasury",
+      ".sol/",
+      "CeloUnreleasedTreasury",
+      ".json"
+    ),
+    string.concat("out-truffle-compat/", "CeloToken", ".sol/", "CeloToken", ".json"),
+    string.concat(
+      "out-truffle-compat/",
+      "DoubleSigningSlasher",
+      ".sol/",
+      "DoubleSigningSlasher",
+      ".json"
+    ),
+    string.concat("out-truffle-compat/", "DowntimeSlasher", ".sol/", "DowntimeSlasher", ".json"),
+    string.concat("out-truffle-compat/", "Election", ".sol/", "Election", ".json"),
+    string.concat("out-truffle-compat/", "EpochRewards", ".sol/", "EpochRewards", ".json"),
+    string.concat(
+      "out-truffle-compat-0.8/",
+      "EpochManagerEnabler",
+      ".sol/",
+      "EpochManagerEnabler",
+      ".json"
+    ),
+    string.concat("out-truffle-compat-0.8/", "EpochManager", ".sol/", "EpochManager", ".json"),
+    string.concat("out-truffle-compat/", "Escrow", ".sol/", "Escrow", ".json"),
+    string.concat(
+      "out-truffle-compat/",
+      "FederatedAttestations",
+      ".sol/",
+      "FederatedAttestations",
+      ".json"
+    ),
+    string.concat(
+      "out-truffle-compat-0.8/",
+      "FeeCurrencyDirectory",
+      ".sol/",
+      "FeeCurrencyDirectory",
+      ".json"
+    ),
+    string.concat("out-truffle-compat/", "FeeHandler", ".sol/", "FeeHandler", ".json"),
+    string.concat("out-truffle-compat/", "Freezer", ".sol/", "Freezer", ".json"),
+    string.concat("out-truffle-compat/", "Governance", ".sol/", "Governance", ".json"),
+    string.concat(
+      "out-truffle-compat/",
+      "GovernanceSlasher",
+      ".sol/",
+      "GovernanceSlasher",
+      ".json"
+    ),
+    string.concat("out-truffle-compat/", "LockedGold", ".sol/", "LockedGold", ".json"),
+    string.concat("out-truffle-compat/", "OdisPayments", ".sol/", "OdisPayments", ".json"),
+    string.concat("out-truffle-compat/", "Random", ".sol/", "Random", ".json"),
+    string.concat("out-truffle-compat/", "Registry", ".sol/", "Registry", ".json"),
+    string.concat("out-truffle-compat-0.8/", "ScoreManager", ".sol/", "ScoreManager", ".json"),
+    string.concat("out-truffle-compat/", "SortedOracles", ".sol/", "SortedOracles", ".json"),
+    string.concat("out-truffle-compat/", "Validators", ".sol/", "Validators", ".json"),
+    string.concat(
+      "out-truffle-compat/",
+      "MentoFeeHandlerSeller",
+      ".sol/",
+      "MentoFeeHandlerSeller",
+      ".json"
+    ),
+    string.concat(
+      "out-truffle-compat/",
+      "UniswapFeeHandlerSeller",
+      ".sol/",
+      "UniswapFeeHandlerSeller",
+      ".json"
+    )
+  ];
 }
