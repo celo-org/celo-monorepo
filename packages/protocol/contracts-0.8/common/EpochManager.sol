a9b7271873b3d65d21dae159a144a348143885f3
Pavel Hornak <pavel.hornak@clabs.co>
2026-04-07 12:06:14 +0200

    fix: revert version bump to 1.1.0.3 (release diff is against v14 baseline)

--- packages/protocol/contracts-0.8/common/EpochManager.sol
+++ packages/protocol/contracts-0.8/common/EpochManager.sol
@@ -614,7 +614,7 @@ contract EpochManager is
    * @return Patch version of the contract.
    */
   function getVersionNumber() external pure returns (uint256, uint256, uint256, uint256) {
-    return (1, 1, 0, 4);
+    return (1, 1, 0, 3);
   }
 
   /**
