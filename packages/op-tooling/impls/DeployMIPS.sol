7bad321e63eba09f3dda7b9cfb99c786f432ee4c
Ocheretovich <ocheretovich@gmail.com>
2026-05-07 13:59:50 +0300

    fix: add InvalidOracleAddress error declaration

--- packages/op-tooling/impls/DeployMIPS.sol
+++ packages/op-tooling/impls/DeployMIPS.sol
@@ -15,6 +15,7 @@ contract DeployMIPS is Script {
   // CHALLENGE_PERIOD (optional) - challenge period for a new PreimageOracle if PREIMAGE_ORACLE is not provided
 
   error MissingEnvVars();
+  error InvalidOracleAddress();
 
   function run() external {
     address oracle_ = vm.envOr("PREIMAGE_ORACLE", address(0));
@@ -22,7 +23,7 @@ contract DeployMIPS is Script {
     uint256 challengePeriod_ = vm.envOr("CHALLENGE_PERIOD", uint256(0));
 
     if (oracle_ != address(0) && oracle_.code.length == 0) {
-      revert MissingEnvVars();
+      revert InvalidOracleAddress();
     }
 
     if (oracle_ == address(0) && !(minProposalSize_ > 0 && challengePeriod_ > 0)) {
